92acd42e3dcf4c7f872f344326c9c8727eda7391
aric <none@none>
2006-08-06 13:52:56 +0000

    Use gnm_random_graph - this example should have been changed during naming change of random graphs. Fixes #58.
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%40358

--- doc/examples/erdos_renyi.py
+++ doc/examples/erdos_renyi.py
@@ -1,12 +1,16 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python
 """
-Create an Erdos-Renyi random graph and report some properties.
+Create an G{n,m} random graph with n nodes and m edges
+and report some properties.
+
+This graph is sometimes called the Erdős-Rényi graph
+but is different from G{n,p} or binomial_graph which is also
+sometimes called the Erdős-Rényi graph.
 """
 __author__ = """Aric Hagberg (hagberg@lanl.gov)"""
-__date__ = "$Date: 2004-11-03 08:11:09 -0700 (Wed, 03 Nov 2004) $"
 __credits__ = """"""
-__revision__ = "$Revision: 503 $"
-#    Copyright (C) 2004 by 
+#    Copyright (C) 2004-2006 by 
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
@@ -14,17 +18,18 @@ __revision__ = "$Revision: 503 $"
 #    http://www.gnu.org/copyleft/lesser.html
 
 from networkx import *
+import sys
 
 n=10 # 10 nodes
 m=20 # 20 edges
 
-G=erdos_renyi_graph(n,m)
+G=gnm_random_graph(n,m)
 
 # some properties
 print "node degree clustering"
 for v in nodes(G):
     print v,degree(G,v),clustering(G,v)
 
-# print the adjacency list 
-write_adjlist(G)
+# print the adjacency list to terminal 
+write_adjlist(G,sys.stdout)
 
