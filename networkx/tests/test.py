8880bc607f6fa58f3e85a3546982e695263a7171
aric <none@none>
2006-11-10 04:40:05 +0000

    remove Numeric import from tests: must use numpy
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%40437

--- networkx/tests/test.py
+++ networkx/tests/test.py
@@ -20,17 +20,14 @@ def all():
         tests+=glob.glob("generators/*.txt")
         #tests+=glob.glob("drawing/*.txt")  
 
-    # skip some tests if we don't have numpy or Numeric        
+    # skip some tests if we don't have numpy 
     try:
         import numpy
     except ImportError:
-        try:
-            import Numeric
-        except ImportError:
-            print "Neither numpy or Numeric found."
-            print "Skipping tests of spectrum.py and threshold.py"
-            tests.remove('spectrum.txt')
-            tests.remove('threshold.txt')
+        print "numpy not found"
+        print "skipping tests of spectrum.py and threshold.py"
+        tests.remove('spectrum.txt')
+        tests.remove('threshold.txt')
 #            tests=[t for t in tests if 'spectrum.txt' not in t \
 #                    if 'threshold.txt' not in t]
 
