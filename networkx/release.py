e86f8c81ab394579172e32b402dcb27e50a4cf05
aric <none@none>
2008-01-13 18:16:52 +0000

    Update documentation and release.py for networkx-0.36
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%40714

--- networkx/release.py
+++ networkx/release.py
@@ -1,6 +1,6 @@
 """Release data for NetworkX."""
 
-#    Copyright (C) 2004-2007 by 
+#    Copyright (C) 2004-2008 by 
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
@@ -10,7 +10,7 @@
 __author__ = """Aric Hagberg (hagberg@lanl.gov)\nPieter Swart (swart@lanl.gov)\nDan Schult (dschult@colgate.edu)"""
 
 name = 'networkx'
-version = '0.36-svn'
+version = '0.36'
 
 description = "Python package for creating and manipulating graphs and networks"
 
