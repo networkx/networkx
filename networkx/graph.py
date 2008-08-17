e60f2222974c18f70f68dc045263048d4e5669df
aric <none@none>
2008-08-17 15:50:58 +0000

    udpate News and Credits for networkx-0.37
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%40836

--- networkx/graph.py
+++ networkx/graph.py
@@ -56,7 +56,7 @@ will add new nodes as required.
 
 """
 __author__ = """Aric Hagberg (hagberg@lanl.gov)\nPieter Swart (swart@lanl.gov)\nDan Schult(dschult@colgate.edu)"""
-#    Copyright (C) 2004-206 by 
+#    Copyright (C) 2004-2008 by 
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
