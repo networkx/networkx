50f220f419e56804eaa82cc22d4aa66504cf2e0c
aric <none@none>
2008-11-11 18:19:47 +0000

    add __init__.py in tests directory pacakges, update test/run.py for nose
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%40937

--- networkx/tests/run.py
+++ networkx/tests/run.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 from os import path
 import sys
+from nose.config import Config
 
-
-def run():
+def run(args=None):
     import networkx
     path=networkx.__path__
+#    config = Config()
     test_suite=None
     try:
         import nose
@@ -14,7 +15,13 @@ def run():
 http://somethingaboutorange.com/mrl/projects/nose""" 
         sys.exit(1)
     print "path",path
-    nose.run(defaultTest=path)
+    print args
+#    print "config",config
+#    nose.run(defaultTest=path,config=config)
+
+    nose.run(argv=args)
 
 if __name__ == "__main__":
-    run()
+    import sys
+    print sys.argv
+    run(sys.argv)
