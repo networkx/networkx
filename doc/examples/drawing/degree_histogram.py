f2f6940ae19aeb3bbfcbd47d2e2c0cd889b673f2
aric <none@none>
2008-11-11 20:54:10 +0000

    prettier degree rank example
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%40943

--- doc/examples/drawing/degree_histogram.py
+++ doc/examples/drawing/degree_histogram.py
@@ -14,30 +14,29 @@ except:
 
 import networkx as nx
 
-z=nx.create_degree_sequence(1000,nx.utils.powerlaw_sequence)
+z=nx.create_degree_sequence(100,nx.utils.powerlaw_sequence,exponent=2.1)
 nx.is_valid_degree_sequence(z)
 
 print "Configuration model"
 G=nx.configuration_model(z)  # configuration model
 
-degree_sequence=nx.degree(G) # degree sequence
-print "Degree sequence", degree_sequence
+degree_sequence=sorted(nx.degree(G),reverse=True) # degree sequence
+#print "Degree sequence", degree_sequence
 dmax=max(degree_sequence)
-print "Degree histogram, max degree:",dmax
-#h,bins=mlab.hist(degree_sequence,bins=dmax)
-h,bins=matplotlib.numpy.histogram(degree_sequence,bins=dmax,new=True)
 
-# draw x-y plot
-hmax=max(h)
-plt.axis([1,dmax,1,hmax]) # set ranges
-#x=compress(h,bins)    # remove bins with zero entries
-#y=compress(h,h)       # remove corresponding entries
-x=bins.compress(h)
-y=h.compress(h)
-plt.loglog(x,y,'bo')
-plt.title("Degree distribution")
-plt.xlabel("degree")
-plt.ylabel("number of nodes")
+plt.loglog(degree_sequence,'b-',marker='o')
+plt.title("Degree rank plot")
+plt.ylabel("degree")
+plt.xlabel("rank")
+
+# draw graph in inset 
+plt.axes([0.45,0.45,0.45,0.45])
+Gcc=nx.connected_component_subgraphs(G)[0]
+pos=nx.spring_layout(Gcc)
+plt.axis('off')
+nx.draw_networkx_nodes(Gcc,pos,node_size=20)
+nx.draw_networkx_edges(Gcc,pos,alpha=0.2)
+
 plt.savefig("degree_histogram.png")
 plt.show()
 
