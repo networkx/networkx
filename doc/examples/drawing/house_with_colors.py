d5e80930c2b7a3cd0e586cf7463a7f1127594cdb
aric <none@none>
2008-11-11 21:00:19 +0000

    make sure house graph is upright
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%40944

--- doc/examples/drawing/house_with_colors.py
+++ doc/examples/drawing/house_with_colors.py
@@ -12,8 +12,15 @@ except:
 import networkx as nx
 
 G=nx.house_graph()
-pos=nx.spring_layout(G)
-nx.draw(G,pos,node_size=2000,alpha=0.8,with_labels=False)
-nx.draw(G,pos,node_size=1000,nodelist=[0,1,2,3],node_color='b') # blue
+# explicitly set positions
+pos={0:(0,0),
+     1:(1,0),
+     2:(0,1),
+     3:(1,1),
+     4:(0.5,2.0)}
+
+nx.draw_networkx_nodes(G,pos,node_size=2000,nodelist=[4])
+nx.draw_networkx_nodes(G,pos,node_size=3000,nodelist=[0,1,2,3],node_color='b')
+nx.draw_networkx_edges(G,pos,alpha=0.5,width=6)
 plt.savefig("house_with_colors.png") # save as png
 plt.show() # display
