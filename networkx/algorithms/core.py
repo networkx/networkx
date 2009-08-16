255853efdc88c3e5e5b238092d8ce2a19257a764
dschult <none@none>
2009-08-16 01:31:16 +0000

    Update core.py to improve speed greatly. This code is by Jason Grout   jason-sage@creativetrax.com Good work Jason!
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401394

--- networkx/algorithms/core.py
+++ networkx/algorithms/core.py
@@ -13,38 +13,41 @@ __author__ = """Dan Schult(dschult@colgate.edu)"""
 __all__ = ['find_cores']
 
 def find_cores(G,with_labels=True):
-   """Return the core number for each vertex.
+    """Return the core number for each vertex.
 
-   See: arXiv:cs.DS/0310049 by Batagelj and Zaversnik
+    See: arXiv:cs.DS/0310049 by Batagelj and Zaversnik
 
-   If with_labels is True a dict is returned keyed by node to the core number. 
-   If with_labels is False a list of the core numbers is returned.
-   """
-   # compute the degrees of each vertex
-   degrees=G.degree(with_labels=True)
-   
-   # sort verticies by degree
-   verts= [node for deg,node in sorted( (d,n) for n,d in degrees.iteritems() )]
-   
-   # Set up initial guesses for core and lists of neighbors.
-   core= degrees
-   nbrs={}
-   for v in verts:
-       nbrs[v]=G.neighbors(v)
-   
-   # form vertex core building up from smallest
-   for v in verts:
-       for u in nbrs[v]:
-          if core[u] > core[v]:
-             nbrs[u].remove(v)
-             posu=verts.index(u)
-             while core[verts[posu]]==core[u]:
-                 posu -= 1
-             verts.remove(u)
-             verts.insert(posu+1,u)
-             core[u] -= 1
-   if with_labels:
-      return core
-   else:
-      return core.values()
+    If with_labels is True a dict is returned keyed by node to the core number.
+    If with_labels is False a list of the core numbers is returned.
+    """
+    # compute the degrees of each vertex
+    degrees=G.degree(with_labels=True)
+    # sort verticies by degree
+    verts= sorted( degrees.keys(), key=lambda x: degrees[x])
+    bin_boundaries=[0]
+    curr_degree=0
+    for i,v in enumerate(verts):
+        if degrees[v]>curr_degree:
+            bin_boundaries.extend([i]*(degrees[v]-curr_degree))
+            curr_degree=degrees[v]
+    vert_pos = dict((v,pos) for pos,v in enumerate(verts))
+    # Set up initial guesses for core and lists of neighbors.
+    core= degrees
+    nbrs=dict((v,set(G.neighbors(v))) for v in G)
+    # form vertex core building up from smallest
+    for v in verts:
+        for u in nbrs[v]:
+            if core[u] > core[v]:
+                nbrs[u].remove(v)
+                pos=vert_pos[u]
+                bin_start=bin_boundaries[core[u]]
+                vert_pos[u]=bin_start
+                vert_pos[verts[bin_start]]=pos
+                verts[bin_start],verts[pos]=verts[pos],verts[bin_start]
+                bin_boundaries[core[u]]+=1
+                core[u] -= 1
+    if with_labels:
+        return core
+    else:
+        return core.values()
 
