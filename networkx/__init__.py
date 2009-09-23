63854e5fb117080c27694f48c23c673e3f5111ef
aric <none@none>
2009-09-23 13:58:53 +0000

    Raise ImportError instead of sys.exit() if Python version < 2.4. Fixes #281
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401466

--- networkx/__init__.py
+++ networkx/__init__.py
@@ -32,8 +32,8 @@ Using
 #
 import sys
 if sys.version_info[:2] < (2, 4):
-    print "Python version 2.4 or later is required for NetworkX (%d.%d detected)." %  sys.version_info[:2]
-    sys.exit(-1)
+    raise ImportError("Python version 2.4 or later is required for NetworkX (%d.%d detected)." %  sys.version_info[:2])
+
 del sys
 
 # Release data
