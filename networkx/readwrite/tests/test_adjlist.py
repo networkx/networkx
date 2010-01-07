3b6eb8e44ef245f31831cf90e157557d1400ddd5
aric <none@none>
2010-01-07 00:18:10 +0000

    Fix failing adjlist test
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401500

--- networkx/readwrite/tests/test_adjlist.py
+++ networkx/readwrite/tests/test_adjlist.py
@@ -8,14 +8,12 @@ import networkx as nx
 
 def test_read_multiline_adjlist_1():
     # Unit test for https://networkx.lanl.gov/trac/ticket/252
-    s = """
-        # New source node
-        1 2
-        # Neighbors list
-        2 1
-        3 1 
-        """
-
+    s = """# comment line
+1 2
+# comment line
+2
+3
+"""
     import StringIO
     strIO = StringIO.StringIO(s)
 
