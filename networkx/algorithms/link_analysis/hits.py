42afbb0564ce6f4c070ffc2c24d0c91b94707c76
aric <none@none>
2010-04-06 14:31:03 +0000

    Hits algorithms now return dictionaries. Update documentation.
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401664

--- networkx/algorithms/link_analysis/hits.py
+++ networkx/algorithms/link_analysis/hits.py
@@ -1,5 +1,8 @@
+"""
+Hubs and authorities analysis of graph structure.
+"""
 #!/usr/bin/env python
-#    Copyright (C) 2008 by 
+#    Copyright (C) 2008-2010 by 
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
@@ -14,10 +17,14 @@ __all__ = ['hits','hits_numpy','hits_scipy','authority_matrix','hub_matrix']
 def hits(G,max_iter=100,tol=1.0e-8,nstart=None):
     """Return HITS hubs and authorities values for nodes.
 
+    The HITS algorithm computes two numbers for a node. 
+    Authorities estimates the node value based on the incoming links.
+    Hubs estimates the node value based on outgoing links.
+
     Parameters
     -----------
     G : graph
-      A networkx graph 
+      A NetworkX graph 
        
     max_iter : interger, optional
       Maximum number of iterations in power method.
@@ -50,17 +57,19 @@ def hits(G,max_iter=100,tol=1.0e-8,nstart=None):
     algorithm does not check if the input graph is directed and will
     execute on undirected graphs.
 
-    For an overview see:
-    A. Langville and C. Meyer, "A survey of eigenvector methods of web
-    information retrieval."  http://citeseer.ist.psu.edu/713792.html
-
+    References
+    ----------
+    .. [1] A. Langville and C. Meyer, 
+       "A survey of eigenvector methods of web information retrieval."  
+       http://citeseer.ist.psu.edu/713792.html
+    .. [2] Jon Kleinberg, 
+       Authoritative sources in a hyperlinked environment
+       Journal of the ACM 46 (5): 604-32, 1999. 
+       doi:10.1145/324133.324140. 
+       http://www.cs.cornell.edu/home/kleinber/auth.pdf.
     """
-    import networkx
     if type(G) == networkx.MultiGraph or type(G) == networkx.MultiDiGraph:
         raise Exception("hits() not defined for graphs with multiedges.")
-
-#    if not G.weighted:
-#        raise Exception("hits(): input graph must be weighted")
 
     # choose fixed starting vector if not given
     if nstart is None:
@@ -71,8 +80,8 @@ def hits(G,max_iter=100,tol=1.0e-8,nstart=None):
         s=1.0/sum(h.values())
         for k in x: h[k]*=s
     nnodes=G.number_of_nodes()
-    # power iteration: make up to max_iter iterations        
-    for i in range(max_iter):
+    i=0
+    while True: # power iteration: make up to max_iter iterations
         hlast=h
         h=dict.fromkeys(hlast.keys(),0)
         a=dict.fromkeys(hlast.keys(),0)
@@ -94,71 +103,170 @@ def hits(G,max_iter=100,tol=1.0e-8,nstart=None):
         # check convergence, l1 norm            
         err=sum([abs(h[n]-hlast[n]) for n in h])
         if err < tol:
-            return h,a
-
-    raise NetworkXError("HITS: power iteration failed to converge in %d iterations."%(i+1))
+            break
+        if i>max_iter:
+            raise NetworkXError(\
+            "HITS: power iteration failed to converge in %d iterations."%(i+1))
+        i+=1
+    return h,a
 
 
 def authority_matrix(G,nodelist=None):
     """Return the HITS authority matrix."""
-    import networkx
     M=networkx.to_numpy_matrix(G,nodelist=nodelist)
     return M.T*M
 
 def hub_matrix(G,nodelist=None):
     """Return the HITS hub matrix."""
-    import networkx
     M=networkx.to_numpy_matrix(G,nodelist=nodelist)
     return M*M.T
 
 
-def hits_numpy(G,max_iter=100,tol=1.0e-6,nodelist=None):
-    """Return a NumPy array of the hubs and authorities."""
-    import numpy
-    import networkx
-    M=networkx.to_numpy_matrix(G,nodelist=nodelist)
-    (n,m)=M.shape # should be square
-    A=M.T*M # authority matrix
-    x=numpy.ones((n,1))/n
-    # power iteration on authority matrix
-    for i in range(max_iter):
-        xlast=x
-        x=numpy.dot(A,x)
-        x=x/x.sum()
-        # check convergence, l1 norm            
-        err=numpy.abs(x-xlast).sum()
-        if err < n*tol:
-            a=numpy.asarray(x).flatten()
-            # h=M*a
-            h=numpy.asarray(numpy.dot(M,a)).flatten()
-            h/=h.sum()
-            return h,a
+def hits_numpy(G):
+    """Return HITS hubs and authorities values for nodes.
 
-    raise NetworkXError("page_rank: power iteration failed to converge in %d iterations."%(i+1))
+    The HITS algorithm computes two numbers for a node. 
+    Authorities estimates the node value based on the incoming links.
+    Hubs estimates the node value based on outgoing links.
+
+    Parameters
+    -----------
+    G : graph
+      A NetworkX graph 
+
+    Returns
+    -------
+    (hubs,authorities) : two-tuple of dictionaries
+       Two dictionaries keyed by node containing the hub and authority
+       values.
+
+    Examples
+    --------
+    >>> G=nx.path_graph(4)
+    >>> h,a=nx.hits(G)
+
+    Notes
+    -----
+    The eigenvector calculation uses NumPy's interface to LAPACK.
+
+    The HITS algorithm was designed for directed graphs but this
+    algorithm does not check if the input graph is directed and will
+    execute on undirected graphs.
+
+    References
+    ----------
+    .. [1] A. Langville and C. Meyer, 
+       "A survey of eigenvector methods of web information retrieval."  
+       http://citeseer.ist.psu.edu/713792.html
+    .. [2] Jon Kleinberg, 
+       Authoritative sources in a hyperlinked environment
+       Journal of the ACM 46 (5): 604-32, 1999. 
+       doi:10.1145/324133.324140. 
+       http://www.cs.cornell.edu/home/kleinber/auth.pdf.
+    """
+    try:
+        import numpy as np
+    except ImportError:
+        raise ImportError(\
+            "hits_numpy() requires NumPy: http://scipy.org/")
+    H=networkx.hub_matrix(G,G.nodes())
+    e,ev=np.linalg.eig(H)
+    m=e.argsort()[-1] # index of maximum eigenvalue
+    h=np.array(ev[:,m]).flatten()
+    A=networkx.authority_matrix(G,G.nodes())
+    e,ev=np.linalg.eig(A)
+    m=e.argsort()[-1] # index of maximum eigenvalue
+    a=np.array(ev[:,m]).flatten()
+    hubs=dict(zip(G.nodes(),h/h.sum()))
+    authorities=dict(zip(G.nodes(),a/a.sum()))
+    return hubs,authorities
 
 
-def hits_scipy(G,max_iter=100,tol=1.0e-6,nodelist=None):
-    """Return a SciPy sparse array of the hubs and authorities."""
-    import numpy
-    import scipy.sparse
-    import networkx
-    M=networkx.to_scipy_sparse_matrix(G,nodelist=nodelist)
+def hits_scipy(G,max_iter=100,tol=1.0e-6):
+    """Return HITS hubs and authorities values for nodes.
+
+    The HITS algorithm computes two numbers for a node. 
+    Authorities estimates the node value based on the incoming links.
+    Hubs estimates the node value based on outgoing links.
+
+    Parameters
+    -----------
+    G : graph
+      A NetworkX graph 
+       
+    max_iter : interger, optional
+      Maximum number of iterations in power method.
+
+    tol : float, optional
+      Error tolerance used to check convergence in power method iteration.
+
+    nstart : dictionary, optional
+      Starting value of each node for power method iteration.
+
+    Returns
+    -------
+    (hubs,authorities) : two-tuple of dictionaries
+       Two dictionaries keyed by node containing the hub and authority
+       values.
+
+    Examples
+    --------
+    >>> G=nx.path_graph(4)
+    >>> h,a=nx.hits(G)
+
+    Notes
+    -----
+    This implementation uses SciPy sparse matrices.
+
+    The eigenvector calculation is done by the power iteration method
+    and has no guarantee of convergence.  The iteration will stop
+    after max_iter iterations or an error tolerance of
+    number_of_nodes(G)*tol has been reached.
+
+    The HITS algorithm was designed for directed graphs but this
+    algorithm does not check if the input graph is directed and will
+    execute on undirected graphs.
+
+    References
+    ----------
+    .. [1] A. Langville and C. Meyer, 
+       "A survey of eigenvector methods of web information retrieval."  
+       http://citeseer.ist.psu.edu/713792.html
+    .. [2] Jon Kleinberg, 
+       Authoritative sources in a hyperlinked environment
+       Journal of the ACM 46 (5): 604-632, 1999. 
+       doi:10.1145/324133.324140. 
+       http://www.cs.cornell.edu/home/kleinber/auth.pdf.
+    """
+    try:
+        import scipy.sparse
+        import numpy as np
+    except ImportError:
+        raise ImportError(\
+            "hits_scipy() requires SciPy: http://scipy.org/")
+    M=networkx.to_scipy_sparse_matrix(G,nodelist=G.nodes())
     (n,m)=M.shape # should be square
     A=M.T*M # authority matrix
     x=scipy.ones((n,1))/n  # initial guess
     # power iteration on authority matrix
-    for i in range(max_iter):
+    i=0
+    while True:
         xlast=x
         x=A*x
         x=x/x.sum()
         # check convergence, l1 norm            
         err=scipy.absolute(x-xlast).sum()
-        if err < n*tol:
-            a=numpy.asarray(x).flatten()
-            # h=M*a
-            h=numpy.asarray(M*a).flatten()
-            h/=h.sum()
-            return h,a
+        if err < tol:
+            break
+        if i>max_iter:
+            raise NetworkXError(\
+            "HITS: power iteration failed to converge in %d iterations."%(i+1))
+        i+=1
 
-    raise NetworkXError("page_rank: power iteration failed to converge in %d iterations."%(i+1))
+    a=np.asarray(x).flatten()
+    # h=M*a
+    h=np.asarray(M*a).flatten()
+    hubs=dict(zip(G.nodes(),h/h.sum()))
+    authorities=dict(zip(G.nodes(),a/a.sum()))
+    return hubs,authorities
 
--- networkx/algorithms/link_analysis/tests/test_hits.py
+++ networkx/algorithms/link_analysis/tests/test_hits.py
@@ -22,71 +22,35 @@ class TestHITS:
            
         G.add_edges_from(edges,weight=1)
         self.G=G
-        self.G.h=[0.000000, 0.000000, 0.211325, 
-                  0.211325, 0.211325, 0.366025]
-
-        self.G.a=[0.000000, 0.000000, 0.000000, 
-                  0.133975, 0.366025, 0.500000]        
+        self.G.a=dict(zip(G,[0.000000, 0.000000, 0.366025,
+                             0.133975, 0.500000, 0.000000]))
+        self.G.h=dict(zip(G,[ 0.366025, 0.000000, 0.211325, 
+                              0.000000, 0.211325, 0.211325]))
 
 
     def test_hits(self):
         G=self.G
         h,a=networkx.hits(G,tol=1.e-08)
-        for (x,y) in zip(sorted(h.values()),self.G.h):
-            assert_almost_equal(x,y,places=5)
-        for (x,y) in zip(sorted(a.values()),self.G.a):
-            assert_almost_equal(x,y,places=5)
+        for n in G:
+            assert_almost_equal(h[n],G.h[n],places=4)
+        for n in G:
+            assert_almost_equal(a[n],G.a[n],places=4)
 
-
-    def test_numpy_hits(self):
+    def test_hits_numpy(self):
         G=self.G
-        try:
-            import numpy
-        except ImportError:
-            raise SkipTest('numpy not available.')
-        
-        h,a=networkx.hits_numpy(G,tol=1.e-08)
-        for (x,y) in zip(sorted(h),self.G.h):
-            assert_almost_equal(x,y,places=5)
-        for (x,y) in zip(sorted(a),self.G.a):
-            assert_almost_equal(x,y,places=5)
+        h,a=networkx.hits_numpy(G)
+        for n in G:
+            assert_almost_equal(h[n],G.h[n],places=4)
+        for n in G:
+            assert_almost_equal(a[n],G.a[n],places=4)
 
-    def test_hubs_authority_matrix(self):
+
+    def test_hits_scipy(self):
         G=self.G
-        try:
-            import numpy
-            import numpy.linalg
-        except ImportError:
-            raise SkipTest('numpy not available.')
-
-        H=networkx.hub_matrix(G,nodelist=None)
-        e,ev=numpy.linalg.eig(H)
-        m=e.argsort()[-1] # index of maximum eigenvalue
-        h=numpy.array(ev[:,m]).flatten()
-
-        A=networkx.authority_matrix(G,nodelist=None)
-        e,ev=numpy.linalg.eig(A)
-        m=e.argsort()[-1] # index of maximum eigenvalue
-        a=numpy.array(ev[:,m]).flatten()
-        h=h/h.sum()
-        a=a/a.sum()
         h,a=networkx.hits_scipy(G,tol=1.e-08)
-        for (x,y) in zip(sorted(h),self.G.h):
-            assert_almost_equal(x,y,places=5)
-        for (x,y) in zip(sorted(a),self.G.a):
-            assert_almost_equal(x,y,places=5)
-
-    def test_scipy_hits(self):
-        G=self.G
-        try:
-            import scipy
-        except ImportError:
-            raise SkipTest('scipy not available.')
-        h,a=networkx.hits_scipy(G,tol=1.e-08)
-        for (x,y) in zip(sorted(h),self.G.h):
-            assert_almost_equal(x,y,places=5)
-        for (x,y) in zip(sorted(a),self.G.a):
-            assert_almost_equal(x,y,places=5)
-
+        for n in G:
+            assert_almost_equal(h[n],G.h[n],places=4)
+        for n in G:
+            assert_almost_equal(a[n],G.a[n],places=4)
 
 
