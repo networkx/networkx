906f6603a523896173bb83bcffc945f03fb47bf3
aric <none@none>
2010-04-17 22:15:47 +0000

    Update test runner so networkx.test() tests all of NetworkX (you need the nose test package).
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401707

--- networkx/tests/test.py
+++ networkx/tests/test.py
@@ -1,82 +1,23 @@
 #!/usr/bin/env python
-
-import glob
-import os
 import sys
-import doctest
-import unittest
-
-
-def all():
-    import networkx
-    testdirs=['.','algorithms','classes','drawing','generators',
-              'linalg','readwrite','algorithms/isomorphism',
-              'algorithms/traversal']
-    try:
-        from pkg_resources import resource_filename, resource_listdirfoo
-        tests=[]
-        for d in testdirs:
-            
-            tests.extend([resource_filename(networkx.__name__, 
-                                            "%s/tests/%s"%(d,t))
-                          for t in resource_listdir("networkx","%s/tests"%d)
-                          if t.endswith("txt")])
-    except:
-        import networkx
-        base=os.path.dirname(networkx.__file__)
-        tests=[]
-        for d in testdirs:
-            tests+=glob.glob(os.path.join(base,d,"tests","*.txt"))
-
-    print tests
-
-    skiplist=[]        
-
-    try:
-        import numpy
-    except ImportError:
-        print "numpy not found: skipping tests"
-        skiplist.extend(['spectrum.txt','threshold.txt'])
-    try:
-        import yaml
-    except ImportError:
-        print "yaml not found: skipping tests"
-        skiplist.extend(['nx_yaml.txt'])
-    try:
-        import pyparsing
-    except ImportError:
-        print "pyparsing not found: skipping tests"
-        skiplist.extend(['gml.txt'])
-    try:
-        import pydot
-    except ImportError:
-        print "pydot not found: skipping tests"
-        skiplist.extend(['nx_pydot.txt'])
-    try:
-        import pygraphviz
-    except ImportError:
-        print "pygraphviz not found: skipping tests"
-        skiplist.extend(['nx_agraph.txt'])
-    try:
-        import matplotlib
-        import pylab
-    except ImportError:
-        print "matplotlib not found: skipping tests"
-        skiplist.extend(['nx_pylab.txt'])
-
-
-    tests=[t for t in tests if os.path.basename(t) not in skiplist]
-
-    suite = unittest.TestSuite()
-    for t in tests:
-        s = doctest.DocFileSuite(t,module_relative=False)
-        suite.addTest(s)
-    return suite
+from os import path
 
 def run():
-    runner = unittest.TextTestRunner()
-    runner.run(all())
-    
+    try:
+        import nose
+    except ImportError:
+        raise ImportError(\
+            "The nose package is needed to run the NetworkX tests.")
 
-if __name__ == "__main__":
+    print "Running NetworkX tests:"
+    
+    nx_install_dir=path.join(path.dirname(__file__), path.pardir,path.pardir)
+    argv=[' ','--verbosity=1',
+          '-w',nx_install_dir,
+          '--with-doctest',
+          '--doctest-extension=txt']
+    nose.run(argv=argv)
+
+if __name__=="__main__":
     run()
+
