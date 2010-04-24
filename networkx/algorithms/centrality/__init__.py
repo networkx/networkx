294f1bf6fa829c1ae6f6c71b8823f32e666b671c
aric <none@none>
2010-04-24 20:26:59 +0000

    Change filename to avoid namespace conflict
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401732

--- networkx/algorithms/centrality/__init__.py
+++ networkx/algorithms/centrality/__init__.py
@@ -1,7 +1,7 @@
 from betweenness import *
 from betweenness_subset import *
 from closeness import *
-from degree import *
+from degree_alg import *
 from eigenvector import *
 from load import *
 from current_flow_closeness import *
@@ -9,7 +9,7 @@ from current_flow_betweenness import *
 from current_flow_betweenness_subset import *
 import betweenness
 import closeness
-import degree
+import degree_alg
 import eigenvector
 import load
 import current_flow_betweenness
