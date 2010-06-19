e5af04d723a890062c7c33f85b4960ead87709a3
aric <none@none>
2010-06-19 16:52:44 +0000

    Switch gml test to nose class.
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401749

---
+++ networkx/readwrite/tests/test_gml.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+import copy
+import unittest
+from nose.tools import *
+from nose import SkipTest
+import networkx
+
+class TestGraph(object):
+    @classmethod
+    def setupClass(cls):
+        global pyparsing
+        try:
+            import pyparsing
+        except ImportError:
+            raise SkipTest('gml test: pyparsing not available.')
+
+    def setUp(self):
+        self.simple_data="""Creator me
+graph [
+ comment "This is a sample graph"
+ directed 1
+ IsPlanar 1
+ pos  [ x 0 y 1 ]
+ node [
+   id 1
+   label "Node 1"
+   pos [ x 1 y 1 ] 
+ ]
+ node [
+    id 2
+    pos [ x 1 y 2 ] 
+    label "Node 2" 
+    ]
+  node [
+    id 3
+    label "Node 3"
+    pos [ x 1 y 3 ] 
+  ]
+  edge [
+    source 1
+    target 2
+    label "Edge from node 1 to node 2"
+    color [ line 'blue'   thickness 3 ] 
+  ]
+  edge [
+    source 2
+    target 3
+    label "Edge from node 2 to node 3"
+  ]
+  edge [
+    source 3
+    target 1 label
+    "Edge from node 3 to node 1"
+  ]
+]
+"""
+    def test_parse_gml(self):
+        G=networkx.parse_gml(self.simple_data)
+        assert_equals(sorted(G.nodes()),\
+                          ['Node 1', 'Node 2', 'Node 3'])
+        assert_equals( [e for e in sorted(G.edges(data=True))],\
+                           [('Node 1', 'Node 2', 
+                             {'color': {'line': 'blue', 'thickness': 3},
+                              'label': 'Edge from node 1 to node 2'}), 
+                            ('Node 2', 'Node 3', 
+                             {'label': 'Edge from node 2 to node 3'}), 
+                            ('Node 3', 'Node 1', {'label': 'Edge from node 3 to node 1'})])
+
+
+    def test_read_gml(self):
+        import os,tempfile
+        (fd,fname)=tempfile.mkstemp()
+        fh=open(fname,'w')
+        fh.write(self.simple_data)
+        fh.close()
+        Gin=networkx.read_gml(fname)
+        G=networkx.parse_gml(self.simple_data)
+        assert_equals( sorted(G.nodes(data=True)), sorted(Gin.nodes(data=True)))
+        assert_equals( sorted(G.edges(data=True)), sorted(Gin.edges(data=True)))
+        os.close(fd)
+        os.unlink(fname)
+
