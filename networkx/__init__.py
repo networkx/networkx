4d3e750c5cd91d33cc446a05c0ece4867de37b85
aric <none@none>
2010-07-31 01:18:48 +0000

    Move some dates to 2010 in documentation
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401806

--- networkx/__init__.py
+++ networkx/__init__.py
@@ -21,7 +21,7 @@ Using
    >>> print G.edges()
    [(1, 2)]
 """
-#    Copyright (C) 2004-2008 by 
+#    Copyright (C) 2004-2010 by 
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
--- networkx/release.py
+++ networkx/release.py
@@ -1,6 +1,6 @@
 """Release data for NetworkX."""
 
-#    Copyright (C) 2004-2008 by 
+#    Copyright (C) 2004-2010 by 
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
