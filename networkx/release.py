e8b4ab263fca6e4e4c0ec057210f7a72df26da1e
aric <none@none>
2010-08-01 13:06:06 +0000

    Update label dev version 1.3
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401818

--- networkx/release.py
+++ networkx/release.py
@@ -63,11 +63,11 @@ def get_svn_revision():
 
 
 name = 'networkx'
-version = '1.2'
+version = '1.3'
 
 # Declare current release as a development release.
 # Change to False before tagging a release; then change back.
-dev = False
+dev = True
 
 revision = None
 if dev:
