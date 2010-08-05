d86cf0104ae6e0d079916051385a11961f0f45ac
loicseguin <none@none>
2010-08-05 18:40:00 +0000

    Ported examples/basic. Refs #384
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401848

--- examples/basic/properties.py
+++ examples/basic/properties.py
@@ -15,35 +15,34 @@ G = lollipop_graph(4,6)
 
 pathlengths=[]
 
-print "source vertex {target:length, }"
+print("source vertex {target:length, }")
 for v in G.nodes():
     spl=single_source_shortest_path_length(G,v)
-    print v,spl
+    print('%s %s' % (v,spl))
     for p in spl.values():
         pathlengths.append(p)
 
-print
-print "average shortest path length ", sum(pathlengths)/len(pathlengths)
+print('')
+print("average shortest path length %s" % (sum(pathlengths)/len(pathlengths)))
 
 # histogram of path lengths 
 dist={}
 for p in pathlengths:
-    if dist.has_key(p):
+    if p in dist:
         dist[p]+=1
     else:
         dist[p]=1
 
-print
-print "length #paths"
+print('')
+print("length #paths")
 verts=dist.keys()
-verts.sort()
-for d in verts:
-    print d,dist[d]
+for d in sorted(verts):
+    print('%s %d' % (d,dist[d]))
 
-print "radius: ",radius(G)
-print "diameter: ",diameter(G)
-print "eccentricity: ",eccentricity(G)
-print "center: ",center(G)
-print "periphery: ",periphery(G)
-print "density: ", density(G)
+print("radius: %d" % radius(G))
+print("diameter: %d" % diameter(G))
+print("eccentricity: %s" % eccentricity(G))
+print("center: %s" % center(G))
+print("periphery: %s" % periphery(G))
+print("density: %s" % density(G))
 
--- examples/basic/read_write.py
+++ examples/basic/read_write.py
@@ -13,7 +13,10 @@ __author__ = """Aric Hagberg (hagberg@lanl.gov)"""
 from networkx import *
 import sys
 G=grid_2d_graph(5,5)  # 5x5 grid
-write_adjlist(G,sys.stdout) # write adjacency list to screen
+try: # Python 2.6+
+    write_adjlist(G,sys.stdout) # write adjacency list to screen
+except TypeError: # Python 3.x
+    write_adjlist(G,sys.stdout.buffer) # write adjacency list to screen
 # write edgelist to grid.edgelist
 write_edgelist(G,path="grid.edgelist",delimiter=":") 
 # read edgelist from grid.edgelist
