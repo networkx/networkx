05b2d2da5fb87c873c6da1149bb5029f201196fe
loicseguin <none@none>
2010-08-07 05:20:09 +0000

    Added exception classes. Fixes #389
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401858

--- networkx/exception.py
+++ networkx/exception.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 **********
 Exceptions
@@ -6,7 +7,7 @@ Exceptions
 Base exceptions and errors for NetworkX.
     
 """
-__author__ = """Aric Hagberg (hagberg@lanl.gov)\nPieter Swart (swart@lanl.gov)\nDan Schult(dschult@colgate.edu)"""
+__author__ = """Aric Hagberg (hagberg@lanl.gov)\nPieter Swart (swart@lanl.gov)\nDan Schult(dschult@colgate.edu)\nLoïc Séguin-C. <loicseguin@gmail.com>"""
 #    Copyright (C) 2004-2008 by 
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
@@ -29,4 +30,20 @@ class NetworkXPointlessConcept(NetworkXException):
 In Graphs and Combinatorics Conference, George Washington University.
 New York: Springer-Verlag, 1973.
 """
+
+class NetworkXAlgorithmError(NetworkXException):
+    """Exception for unexpected termination of algorithms."""
+
+class NetworkXUnfeasible(NetworkXAlgorithmError):
+    """Exception raised by algorithms trying to solve a problem
+    instance that has no feasible solution."""
+
+class NetworkXNoPath(NetworkXUnfeasible):
+    """Exception for algorithms that should return a path when running
+    on graphs where such a path does not exist."""
+
+class NetworkXUnbounded(NetworkXAlgorithmError):
+    """Exception raised by algorithms trying to solve a maximization
+    or a minimization problem instance that is unbounded."""
+
 
