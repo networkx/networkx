6aabcc917e4c1b220924b3cd3cf3e0712ce79ae6
aric <none@none>
2010-08-17 20:12:50 +0000

    Update pajek documentation and tests
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401912

--- networkx/readwrite/pajek.py
+++ networkx/readwrite/pajek.py
@@ -2,94 +2,86 @@
 *****
 Pajek
 *****
-
 Read graphs in Pajek format.
 
 See http://vlado.fmf.uni-lj.si/pub/networks/pajek/doc/draweps.htm
 for format information.
 
-This implementation handles only directed and undirected graphs including
+This implementation handles directed and undirected graphs including
 those with self loops and parallel edges.  
-
 """
 __author__ = """Aric Hagberg (hagberg@lanl.gov)"""
-#    Copyright (C) 2008 by 
+#    Copyright (C) 2008-2010 by 
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
 #    All rights reserved.
 #    BSD license.
 import networkx as nx
-from networkx.utils import is_string_like,_get_fh
+from networkx.utils import is_string_like,_get_fh,make_str
 
-__all__ = ['read_pajek', 'parse_pajek', 'write_pajek']
+__all__ = ['read_pajek', 'parse_pajek', 'generate_pajek', 'write_pajek']
 
-def write_pajek(G, path):
-    """Write in Pajek format to path.
+def generate_pajek(G):
+    """Generate lines in Pajek graph format.
 
     Parameters
     ----------
     G : graph
-       A networkx graph
-    path : file or string
-       File or filename to write.  
-       Filenames ending in .gz or .bz2 will be compressed.
+       A Networkx graph
 
-    Examples
-    --------
-    >>> G=nx.path_graph(4)
-    >>> nx.write_pajek(G, "test.net")
+    References
+    ----------
+    See http://vlado.fmf.uni-lj.si/pub/networks/pajek/doc/draweps.htm
+    for format information.
     """
-    fh=_get_fh(path,mode='w')
-
     if G.name=='': 
-        name="NetworkX"
+        name='NetworkX'
     else:
         name=G.name
-    fh.write("*network %s\n"%name)
+    yield '*network %s\n'%name
 
     # write nodes with attributes
-    fh.write("*vertices %s\n"%(G.order()))
+    yield '*vertices %s\n'%(G.order())
     nodes = G.nodes()
     # make dictionary mapping nodes to integers
     nodenumber=dict(zip(nodes,range(1,len(nodes)+1))) 
     for n in nodes:
-        na=G.node[n].copy()
-        x=na.pop('x',0.0)
-        y=na.pop('y',0.0)
-        id=int(na.pop('id',nodenumber[n]))
+        na=G.node.get(n,{})
+        x=na.get('x',0.0)
+        y=na.get('y',0.0)
+        id=int(na.get('id',nodenumber[n]))
         nodenumber[n]=id
-        shape=na.pop('shape','ellipse')
-        fh.write("%d \"%s\" %f %f %s "%(id,n,float(x),float(y),shape))
+        shape=na.get('shape','ellipse')
+        yield ' '.join(map(make_str,(id,n,x,y,shape)))
         for k,v in na.items():
-            fh.write("%s %s "%(k,v))
-        fh.write("\n")                               
-        
+            yield '%s %s '%(k,v)
+        yield '\n'
+
     # write edges with attributes         
     if G.is_directed():
-        fh.write("*arcs\n")
+        yield '*arcs\n'
     else:
-        fh.write("*edges\n")
+        yield '*edges\n'
     for u,v,edgedata in G.edges(data=True):
         d=edgedata.copy()
         value=d.pop('weight',1.0) # use 1 as default edge value
-        fh.write("%d %d %f "%(nodenumber[u],nodenumber[v],float(value)))
+        yield ' '.join(map(make_str,(nodenumber[u],nodenumber[v],value)))
         for k,v in d.items():
             if is_string_like(v):
                 # add quotes to any values with a blank space
                 if " " in v: 
                     v="\"%s\""%v
-            fh.write("%s %s "%(k,v))
-        fh.write("\n")                               
-#    fh.close()
-
-def read_pajek(path):
-    """Read graph in Pajek format from path. 
-
-    Returns a MultiGraph or MultiDiGraph.
+            yield '%s %s '%(k,v)
+        yield '\n'
+        
+def write_pajek(G, path, encoding='UTF-8'):
+    """Write graph in Pajek format to path.
 
     Parameters
     ----------
+    G : graph
+       A Networkx graph
     path : file or string
        File or filename to write.  
        Filenames ending in .gz or .bz2 will be compressed.
@@ -98,22 +90,57 @@ def read_pajek(path):
     --------
     >>> G=nx.path_graph(4)
     >>> nx.write_pajek(G, "test.net")
+
+    References
+    ----------
+    See http://vlado.fmf.uni-lj.si/pub/networks/pajek/doc/draweps.htm
+    for format information.
+    """
+    fh=_get_fh(path, 'wb')
+    for line in generate_pajek(G):
+        fh.write(line.encode(encoding))
+
+def read_pajek(path,encoding='UTF-8'):
+    """Read graph in Pajek format from path. 
+
+    Parameters
+    ----------
+    path : file or string
+       File or filename to write.  
+       Filenames ending in .gz or .bz2 will be uncompressed.
+
+    Returns
+    -------
+    G : NetworkX MultiGraph or MultiDiGraph.
+
+    Examples
+    --------
     >>> G=nx.read_pajek("test.net")
 
     To create a Graph instead of a MultiGraph use
 
     >>> G1=nx.Graph(G)
 
-
+    References
+    ----------
+    See Http://vlado.fmf.uni-lj.si/pub/networks/pajek/doc/draweps.htm
+    for format information.
     """
-    fh=_get_fh(path,mode='r')        
-    G=parse_pajek(fh)
-    return G
+    fh=_get_fh(path, 'rb')
+    lines = (line.decode(encoding) for line in fh)
+    return parse_pajek(lines)
 
-def parse_pajek(lines,edge_attr=True):
-    """Parse pajek format graph from string or iterable.
+def parse_pajek(lines):
+    """Parse Pajek format graph from string or iterable.
 
-    Primarily used as a helper for read_pajek().
+    Parameters
+    ----------
+    lines : string or iterable
+       Data in Pajek format.
+
+    Returns
+    -------
+    G : NetworkX graph
 
     See Also
     --------
@@ -123,7 +150,7 @@ def parse_pajek(lines,edge_attr=True):
     import shlex
     if is_string_like(lines): lines=iter(lines.split('\n'))
     lines = iter([line.rstrip('\n') for line in lines])
-    G=nx.MultiDiGraph() # are multiedges allowed in Pajek?
+    G=nx.MultiDiGraph() # are multiedges allowed in Pajek? assume yes
     directed=True # assume this is a directed network for now
     while lines:
         try:
@@ -137,14 +164,16 @@ def parse_pajek(lines,edge_attr=True):
             nodelabels={}
             l,nnodes=l.split()
             for i in range(int(nnodes)):
-                splitline=shlex.split(next(lines))
+                splitline=shlex.split(str(next(lines)))
                 id,label=splitline[0:2]
                 G.add_node(label)
                 nodelabels[id]=label
                 G.node[label]={'id':id}
                 try: 
                     x,y,shape=splitline[2:5]
-                    G.node[label].update({'x':x,'y':y,'shape':shape})
+                    G.node[label].update({'x':float(x),
+                                          'y':float(y),
+                                          'shape':shape})
                 except:
                     pass
                 extra_attr=zip(splitline[5::2],splitline[6::2])
@@ -154,7 +183,7 @@ def parse_pajek(lines,edge_attr=True):
                # switch from digraph to graph
                 G=nx.MultiGraph(G)
             for l in lines:
-                splitline=shlex.split(l)
+                splitline=shlex.split(str(l))
                 ui,vi=splitline[0:2]
                 u=nodelabels.get(ui,ui)
                 v=nodelabels.get(vi,vi)
