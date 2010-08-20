e396beb5a02d2da638dc015847da168692219037
aric <none@none>
2010-08-20 13:53:10 +0000

    Change API of minimum_spanning_tree() to return a tree or forest (instead of edges) and add minimum_spanning_edges().  Fixes #402
    --HG--
    extra : convert_revision : svn%3A3ed01bd8-26fb-0310-9e4c-ca1a4053419f/networkx/trunk%401919

--- networkx/algorithms/mst.py
+++ networkx/algorithms/mst.py
@@ -2,22 +2,26 @@
 Computes minimum spanning tree of a weighted graph.
 
 """
-#    Copyright (C) 2009 by 
+#    Copyright (C) 2009-2010 by 
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
 #    All rights reserved.
 #    BSD license.
 
-__all__ = ['mst', 'kruskal_mst','minimum_spanning_tree']
+__all__ = ['mst', 'kruskal_mst',
+           'minimum_spanning_edges',
+           'minimum_spanning_tree']
 
 import networkx as nx
 
-def minimum_spanning_tree(G):
-    """Generate a minimum spanning tree of an undirected weighted graph.
+def minimum_spanning_edges(G):
+    """Generate edges in a minimum spanning forest of an undirected 
+    weighted graph.
 
     A minimum spanning tree is a subgraph of the graph (a tree)
-    with the minimum sum of edge weights.
+    with the minimum sum of edge weights.  A spanning forest is a
+    union of the spanning trees for each connected component of the graph.
 
     Parameters
     ----------
@@ -33,7 +37,7 @@ def minimum_spanning_tree(G):
     --------
     >>> G=nx.cycle_graph(4)
     >>> G.add_edge(0,3,weight=2) # assign weight 2 to edge 0-3
-    >>> mst=nx.minimum_spanning_tree(G) # a generator of MST edges
+    >>> mst=nx.minimum_spanning_edges(G) # a generator of MST edges
     >>> edgelist=list(mst) # make a list of the edges
     >>> print(sorted(edgelist))
     [(0, 1, {'weight': 1}), (1, 2, {'weight': 1}), (2, 3, {'weight': 1})]
@@ -58,12 +62,60 @@ def minimum_spanning_tree(G):
     # implement once UnionFind exists, and second, because the only slow
     # part (the sort) is sped up by being built in to Python.
     from networkx.utils import UnionFind
+
+    if G.is_directed():
+        raise NetworkXError(\
+            "Mimimum spanning tree not defined for directed graphs.")
+
     subtrees = UnionFind()
     edges = sorted((G[u][v].get('weight',1),u,v) for u in G for v in G[u])
     for W,u,v in edges:
         if subtrees[u] != subtrees[v]:
             yield (u,v,{'weight':W})
             subtrees.union(u,v)
+
+
+def minimum_spanning_tree(G):
+    """Generate a minimum spanning tree or forest of an undirected 
+    weighted graph.
+
+    A minimum spanning tree is a subgraph of the graph (a tree) with
+    the minimum sum of edge weights.
+
+    If the graph is not connected a spanning forest is constructed.  A
+    spanning forest is a union of the spanning trees for each
+    connected component of the graph.
+
+
+    Parameters
+    ----------
+    G : NetworkX Graph
+    
+    Returns
+    -------
+    G : NetworkX Graph
+       A minimum spanning tree or forest. 
+    
+    Examples
+    --------
+    >>> G=nx.cycle_graph(4)
+    >>> G.add_edge(0,3,weight=2) # assign weight 2 to edge 0-3
+    >>> T=minimum_spanning_tree(G)
+    >>> print(sorted(T.edges(data=True)))
+    [(0, 1, {'weight': 1}), (1, 2, {'weight': 1}), (2, 3, {'weight': 1})]
+
+    Notes
+    -----
+    Uses Kruskal's algorithm.
+
+    If the graph edges do not have a weight attribute a default weight of 1
+    will be assigned.
+    """
+    T=nx.Graph(nx.minimum_spanning_edges(G))
+    # Add isolated nodes
+    if len(T)!=len(G):
+        T.add_nodes_from([n for n,d in G.degree().items() if d==0])
+    return T
 
 kruskal_mst=minimum_spanning_tree
 mst=minimum_spanning_tree
--- networkx/algorithms/tests/test_mst.py
+++ networkx/algorithms/tests/test_mst.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 from nose.tools import *
-import networkx
+import networkx as nx
 
 class TestMST:
 
     def setUp(self):
     # example from Wikipedia: http://en.wikipedia.org/wiki/Kruskal's_algorithm
-        G=networkx.Graph() 
+        G=nx.Graph() 
         edgelist = [(0,3,[('weight',5)]),
                     (0,1,[('weight',7)]),
                     (1,3,[('weight',9)]),
@@ -31,9 +31,27 @@ class TestMST:
         self.tree_edgelist=sorted((sorted((u, v))[0], sorted((u, v))[1], d)
                                   for u,v,d in tree_edgelist)
 
-    def test_kruskal_mst(self):
-        edgelist=sorted(networkx.kruskal_mst(self.G))
+    def test_mst(self):
+        T=nx.minimum_spanning_tree(self.G)
+        assert_equal(T.edges(data=True),self.tree_edgelist)
+
+    def test_mst_edges(self):
+        edgelist=sorted(nx.minimum_spanning_edges(self.G))
         assert_equal(edgelist,self.tree_edgelist)
 
+    def test_mst_disconnected(self):
+        G=nx.Graph()
+        G.add_path([1,2])
+        G.add_path([10,20])
+        T=nx.minimum_spanning_tree(G)
+        assert_equal(sorted(T.edges()),[(1, 2), (20, 10)])
+        assert_equal(sorted(T.nodes()),[1, 2, 10, 20])
 
+    def test_mst_isolate(self):
+        G=nx.Graph()
+        G.add_nodes_from([1,2])
+        T=nx.minimum_spanning_tree(G)
+        assert_equal(sorted(T.nodes()),[1, 2])
+        assert_equal(sorted(T.edges()),[])
 
+        
