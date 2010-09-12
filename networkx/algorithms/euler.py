3fc64b3c163feab70165fe776791361913a8623b
Aric Hagberg <aric.hagberg@gmail.com>
2010-09-12 07:49:29 -0600

    Clean up documentation and comments

--- networkx/algorithms/euler.py
+++ networkx/algorithms/euler.py
@@ -22,7 +22,8 @@ def is_eulerian(G):
 
     Parameters
     ----------
-    G : NetworkX graph
+    G : graph
+       A NetworkX Graph
 
     Examples
     --------
@@ -39,11 +40,11 @@ def is_eulerian(G):
     (or strongly connected for directed graphs).
     """
     if G.is_directed():
-        # Every node of must have equal in degree and out degree
+        # Every node must have equal in degree and out degree
         for n in G.nodes_iter():
             if G.in_degree(n) != G.out_degree(n):
                return False
-        # Must be a strongly connected graph
+        # Must be strongly connected
         if not nx.is_strongly_connected(G):
             return False
     else:
@@ -51,7 +52,7 @@ def is_eulerian(G):
         for v,d in G.degree_iter():
             if d % 2 != 0:
                 return False
-        # Must be a connected graph
+        # Must be connected
         if not nx.is_connected(G):
             return False
     return True
@@ -65,8 +66,8 @@ def eulerian_circuit(G, source=None):
 
     Parameters
     ----------
-    G : NetworkX graph
-
+    G : graph
+       A NetworkX Graph
     source : node, optional
        Starting node for circuit.
 
@@ -75,15 +76,14 @@ def eulerian_circuit(G, source=None):
     edges : generator
        A generator that produces edges in the Eulerian circuit.
 
-    Examples
+    Raises
+    ------
+    NetworkXError
+       If the graph is not Eulerian.
+
+    See Also
     --------
-    >>> G=nx.complete_graph(3)
-    >>> list(nx.eulerian_circuit(G))
-    [(0, 1), (1, 2), (2, 0)]
-    >>> list(nx.eulerian_circuit(G,source=1)) 
-    [(1, 0), (0, 2), (2, 1)]
-    >>> [u for u,v in nx.eulerian_circuit(G)]  # nodes in circuit
-    [0, 1, 2]
+    is_eulerian
 
     Notes
     -----
@@ -94,6 +94,16 @@ def eulerian_circuit(G, source=None):
     .. [1] Fleury, "Deux problemes de geometrie de situation", 
        Journal de mathematiques elementaires (1883), 257-261.
     .. [2] http://en.wikipedia.org/wiki/Eulerian_path
+
+    Examples
+    --------
+    >>> G=nx.complete_graph(3)
+    >>> list(nx.eulerian_circuit(G))
+    [(0, 1), (1, 2), (2, 0)]
+    >>> list(nx.eulerian_circuit(G,source=1)) 
+    [(1, 0), (0, 2), (2, 1)]
+    >>> [u for u,v in nx.eulerian_circuit(G)]  # nodes in circuit
+    [0, 1, 2]
     """
     if not is_eulerian(G):
         raise nx.NetworkXError("G is not Eulerian.")
@@ -106,19 +116,19 @@ def eulerian_circuit(G, source=None):
     else:
         v = source
 
-    while g.size()>0:
+    while g.size() > 0:
         n = v   
-        nbrs = sorted([v for u,v in g.edges(n)])
         # sort nbrs here to provide stable ordering of alternate cycles
+        nbrs = sorted([v for u,v in g.edges(n)])
         for v in nbrs:
-            g.remove_edge(n, v)
+            g.remove_edge(n,v)
             bridge = not nx.is_connected(g.to_undirected())
             if bridge:
-                g.add_edge(n, v)  # add this edge back and try another
+                g.add_edge(n,v)  # add this edge back and try another
             else:
                 break  # this edge is good, break the for loop 
         if bridge:
-            g.remove_edge(n, v)            
+            g.remove_edge(n,v)            
             g.remove_node(n)
         yield (n,v)
 
