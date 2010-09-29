9c14cdaea87ce1158013917b7dc3af5f24e83379
Aric Hagberg <aric.hagberg@gmail.com>
2010-09-28 21:44:30 -0600

    Faster construction of words graph. Improve code style. Addresses #440.

--- examples/graph/words.py
+++ examples/graph/words.py
@@ -1,104 +1,82 @@
 """
-SGBWords() returns an undirected graph over the 5757 5-letter
-words in the datafile words_dat.txt.  Two words are connected by an edge
+Words/Ladder Graph
+------------------
+Generate  an undirected graph over the 5757 5-letter words in the 
+datafile words_dat.txt.gz.  Two words are connected by an edge
 if they differ in one letter, resulting in 14,135 edges. This example
-is described in Section 1.1 in Knuth's book [1,2].
+is described in Section 1.1 in Knuth's book [1]_,[2]_.
 
-References.
+References
 ----------
-
-[1] Donald E. Knuth,
-    "The Stanford GraphBase: A Platform for Combinatorial Computing",
-    ACM Press, New York, 1993.
-[2] http://www-cs-faculty.stanford.edu/~knuth/sgb.html
-
+.. [1] Donald E. Knuth,
+   "The Stanford GraphBase: A Platform for Combinatorial Computing",
+   ACM Press, New York, 1993.
+.. [2] http://www-cs-faculty.stanford.edu/~knuth/sgb.html
 """
-
-__author__ = """Brendt Wohlberg\nAric Hagberg (hagberg@lanl.gov)"""
-__date__ = "$Date: 2005-04-01 07:56:04 -0700 (Fri, 01 Apr 2005) $"
-__credits__ = """"""
-__revision__ = ""
-#    Copyright (C) 2004 by 
+__author__ = """\n""".join(['Aric Hagberg (hagberg@lanl.gov)',
+                            'Brendt Wohlberg',
+                            'hughdbrown@yahoo.com'])
+#    Copyright (C) 2004-2010 by 
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
 #    All rights reserved.
 #    BSD license.
 
-from networkx import *
-import re
-import sys
-
-__author__ = """"""
-__date__ = ""
-__credits__ = """"""
-__version__ = ""
-# $Header$
-
+import networkx as nx
 
 #-------------------------------------------------------------------
 #   The Words/Ladder graph of Section 1.1
 #-------------------------------------------------------------------
-
-def _notComment(line):
-    return not(line.startswith('*'))
-
-
-def _wdist(a,b):
-    """ Return simple edit distance between two words a and b. """
-
-    d=abs(len(a)-len(b))
-    for k in range(0,min(len(a),len(b))):
-        if a[k] != b[k]:
-            d = d + 1
-    return d
-
-
-def words_graph():
-    """ Return the words example graph from the Stanford GraphBase"""
-
-    import sys
-    # open file words_dat.txt.gz
-    import gzip
-    fh=gzip.open('words_dat.txt.gz','r')
-
-    G = Graph(name="words")
-    sys.stderr.write("Loading words_dat.txt: ")
-    for line in fh.readlines():
-        line = line.decode()
-        if line.startswith("*"):
-            continue
-        w=line[0:5]
-        G.add_node(w)
-    nwords=number_of_nodes(G)
-    words=G.nodes()
-    for k in range(0,nwords):
-        if (k%100==0): 
-            sys.stderr.softspace=0
-            sys.stderr.write(".")
-        for l in range(k+1,nwords):
-            if _wdist(words[k],words[l]) == 1:
-                G.add_edge(words[k],words[l])
-    print()
+def generate_graph(words):
+    from string import ascii_lowercase as lowercase
+    G = nx.Graph(name="words")
+    lookup = dict((c,lowercase.index(c)) for c in lowercase)
+    def edit_distance_one(word):
+        for i in range(len(word)):
+            left, c, right = word[0:i], word[i], word[i+1:]
+            j = lookup[c] # lowercase.index(c)
+            for cc in lowercase[j+1:]:
+                yield left + cc + right
+    candgen = ((word, cand) for word in sorted(words) 
+               for cand in edit_distance_one(word) if cand in words)
+    G.add_nodes_from(words)
+    for word, cand in candgen:
+        G.add_edge(word, cand)
     return G
 
+def words_graph():
+    """Return the words example graph from the Stanford GraphBase"""
+    import gzip
+    fh=gzip.open('words_dat.txt.gz','r')
+    words=set()
+    for line in fh.readlines():
+        line = line.decode()
+        if line.startswith('*'):
+            continue
+        w=str(line[0:5])
+        words.add(w)
+    return generate_graph(words)
 
 if __name__ == '__main__':
     from networkx import *
     G=words_graph()
     print("Loaded words_dat.txt containing 5757 five-letter English words.")
     print("Two words are connected if they differ in one letter.")
-    print("graph has %d nodes with %d edges"
+    print("Graph has %d nodes with %d edges"
           %(number_of_nodes(G),number_of_edges(G)))
-
-    sp=shortest_path(G, 'chaos', 'order')
-    print("shortest path between 'chaos' and 'order' is:\n%s" % sp)
-
-    sp=shortest_path(G, 'nodes', 'graph')
-    print("shortest path between 'nodes' and 'graph' is:\n%s" % sp)
-
-    sp=shortest_path(G, 'pound', 'marks')
-    print("shortest path between 'pound' and 'marks' is:\n%s" % sp)
-
     print("%d connected components" % number_connected_components(G))
+
+    for (source,target) in [('chaos','order'),
+                            ('nodes','graph'),
+                            ('pound','marks')]:
+        sp=shortest_path(G, source, target)
+        print("Shortest path between %s and %s is"%(source,target))
+        if sp is False:
+            print("None")
+        else:
+            for n in sp:
+                print(n)
+
+
 
