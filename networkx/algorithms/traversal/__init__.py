0b4aa42944bcd2af5c478eb0126e45b99ab8658e
Aric Hagberg <aric.hagberg@gmail.com>
2010-10-17 13:43:00 -0600

    Add breadth-first search algorithm and related.

--- networkx/algorithms/traversal/__init__.py
+++ networkx/algorithms/traversal/__init__.py
@@ -1,2 +1,4 @@
 import networkx.algorithms.traversal.depth_first_search
 from networkx.algorithms.traversal.depth_first_search import *
+import networkx.algorithms.traversal.breadth_first_search
+from networkx.algorithms.traversal.breadth_first_search import *
---
+++ networkx/algorithms/traversal/breadth_first_search.py
@@ -0,0 +1,52 @@
+"""
+==================
+Breadth-first search 
+==================
+
+Basic algorithms for breadth-first searching.
+"""
+__author__ = """\n""".join(['Aric Hagberg <hagberg@lanl.gov>'])
+
+__all__ = ['bfs_edges', 'bfs_tree',
+           'bfs_predecessors', 'bfs_successors']
+
+import networkx as nx
+from collections import defaultdict
+
+def bfs_edges(G,source):
+    """Produce edges in a breadth-first-search starting at source."""
+    # Based on http://www.ics.uci.edu/~eppstein/PADS/BFS.py
+    # by D. Eppstein, July 2004.
+    visited=set([source])
+    stack = [(source,iter(G[source]))]
+    while stack:
+        parent,children = stack[0]
+        try:
+            child = children.next()
+            if child not in visited:
+                yield parent,child
+                visited.add(child)
+                stack.append((child,iter(G[child])))
+        except StopIteration:
+            stack.pop(0)
+
+
+def bfs_tree(G, source):
+    """Return directed tree of breadth-first-search from source."""
+    return nx.DiGraph(bfs_edges(G,source))
+
+
+def bfs_predecessors(G, source):
+    """Return dictionary of predecessors in breadth-first-search from source."""
+    return dict((t,s) for s,t in bfs_edges(G,source))
+
+
+def bfs_successors(G, source):
+    """Return dictionary of successors in breadth-first-search from source."""
+    d=defaultdict(list)
+    for k, v in bfs_edges(G,source):
+        d[k].append(v)
+    return dict(d)
+
+
+
