ae0b4db8f0e754eccd1c8b19022958b1f2e84372
Aric Hagberg <aric.hagberg@gmail.com>
2010-10-18 08:37:35 -0600

    Add tests for dfs source=None Addresses #418

--- networkx/algorithms/traversal/tests/test_dfs.py
+++ networkx/algorithms/traversal/tests/test_dfs.py
@@ -9,23 +9,31 @@ class TestDFS:
         G=nx.Graph()
         G.add_edges_from([(0,1),(1,2),(1,3),(2,4),(3,4)])
         self.G=G
+        # simple graph, disconnected
+        D=nx.Graph()
+        D.add_edges_from([(0,1),(2,3)])
+        self.D=D
 
 
     def test_preorder_nodes(self):
         assert_equal(list(nx.dfs_preorder_nodes(self.G,source=0)),
                      [0, 1, 2, 4, 3])
+        assert_equal(list(nx.dfs_preorder_nodes(self.D)),[0, 1, 2, 3])
 
     def test_postorder_nodes(self):
         assert_equal(list(nx.dfs_postorder_nodes(self.G,source=0)),
                      [3, 4, 2, 1, 0])
+        assert_equal(list(nx.dfs_postorder_nodes(self.D)),[1, 0, 3, 2])
 
     def test_successor(self):
         assert_equal(nx.dfs_successors(self.G,source=0),
                      {0: [1], 1: [2], 2: [4], 4: [3]})
+        assert_equal(nx.dfs_successors(self.D), {0: [1], 2: [3]})
 
     def test_predecessor(self):
         assert_equal(nx.dfs_predecessors(self.G,source=0),
                      {1: 0, 2: 1, 3: 4, 4: 2})
+        assert_equal(nx.dfs_predecessors(self.D), {1: 0, 3: 2})
 
     def test_dfs_tree(self):
         T=nx.dfs_tree(self.G,source=0)
@@ -35,9 +43,16 @@ class TestDFS:
     def test_dfs_edges(self):
         edges=nx.dfs_edges(self.G,source=0)
         assert_equal(list(edges),[(0, 1), (1, 2), (2, 4), (4, 3)])
+        edges=nx.dfs_edges(self.D)
+        assert_equal(list(edges),[(0, 1), (2, 3)])
 
     def test_dfs_labeled_edges(self):
         edges=list(nx.dfs_labeled_edges(self.G,source=0))
         forward=[(u,v) for (u,v,d) in edges if d['dir']=='forward']
-        assert_equal(forward,[(0, 1), (1, 2), (2, 4), (4, 3)])
+        assert_equal(forward,[(0,0), (0, 1), (1, 2), (2, 4), (4, 3)])
 
+    def test_dfs_labeled_disconnected_edges(self):
+        edges=list(nx.dfs_labeled_edges(self.D))
+        forward=[(u,v) for (u,v,d) in edges if d['dir']=='forward']
+        assert_equal(forward,[(0, 0), (0, 1), (2, 2), (2, 3)])
+    
