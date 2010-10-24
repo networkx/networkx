5684edf4e3e637f15e57922ee1d34ef5eb8fbf76
Loïc Séguin-C. <loicseguin@gmail.com>
2010-10-23 23:20:00 -0400

    Added Prim's algorithm for mst. Fixes #421

--- networkx/algorithms/mst.py
+++ networkx/algorithms/mst.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Computes minimum spanning tree of a weighted graph.
 
@@ -6,14 +7,17 @@ Computes minimum spanning tree of a weighted graph.
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
+#    Loïc Séguin-C. <loicseguin@gmail.com>
 #    All rights reserved.
 #    BSD license.
 
 __all__ = ['mst', 'kruskal_mst',
            'minimum_spanning_edges',
-           'minimum_spanning_tree']
+           'minimum_spanning_tree',
+           'prim_mst_edges', 'prim_mst']
 
 import networkx as nx
+from heapq import heappop, heappush
 
 def minimum_spanning_edges(G,weight='weight',data=True):
     """Generate edges in a minimum spanning forest of an undirected 
@@ -130,4 +134,125 @@ def minimum_spanning_tree(G,weight='weight'):
 
 kruskal_mst=minimum_spanning_tree
 mst=minimum_spanning_tree
+
+
+def prim_mst_edges(G, weight = 'weight', data = True):
+    """Generate edges in a minimum spanning forest of an undirected 
+    weighted graph.
+
+    A minimum spanning tree is a subgraph of the graph (a tree)
+    with the minimum sum of edge weights.  A spanning forest is a
+    union of the spanning trees for each connected component of the graph.
+
+    Parameters
+    ----------
+    G : NetworkX Graph
+    
+    weight : string
+       Edge data key to use for weight (default 'weight').
+
+    data : bool, optional
+       If True yield the edge data along with the edge.
+       
+    Returns
+    -------
+    edges : iterator
+       A generator that produces edges in the minimum spanning tree.
+       The edges are three-tuples (u,v,w) where w is the weight.
+    
+    Examples
+    --------
+    >>> G=nx.cycle_graph(4)
+    >>> G.add_edge(0,3,weight=2) # assign weight 2 to edge 0-3
+    >>> mst=nx.prim_mst_edges(G,data=False) # a generator of MST edges
+    >>> edgelist=list(mst) # make a list of the edges
+    >>> print(sorted(edgelist))
+    [(0, 1), (1, 2), (2, 3)]
+
+    Notes
+    -----
+    Uses Prim's algorithm.
+
+    If the graph edges do not have a weight attribute a default weight of 1
+    will be used.
+    """
+
+    if G.is_directed():
+        raise NetworkXError(
+            "Mimimum spanning tree not defined for directed graphs.")
+
+    nodes = G.nodes()
+
+    while True:
+        if len(nodes) == 0:
+            break
+        u = nodes.pop(0)
+        frontier = []
+        visited = [u]
+        for u, v in G.edges(u):
+            heappush(frontier, (G[u][v].get(weight, 1), u, v))
+
+        while len(frontier) > 0:
+            W, u, v = heappop(frontier)
+            if v in visited:
+                continue
+            visited.append(v)
+            nodes.remove(v)
+            for v, w  in G.edges(v):
+                if not w in visited:
+                    heappush(frontier, (G[v][w].get(weight, 1), v, w))
+            if data:
+                yield u, v, G[u][v]
+            else:
+                yield u, v
+
+
+def prim_mst(G, weight = 'weight'):
+    """Return a minimum spanning tree or forest of an undirected 
+    weighted graph.
+
+    A minimum spanning tree is a subgraph of the graph (a tree) with
+    the minimum sum of edge weights.
+
+    If the graph is not connected a spanning forest is constructed.  A
+    spanning forest is a union of the spanning trees for each
+    connected component of the graph.
+
+    Parameters
+    ----------
+    G : NetworkX Graph
+    
+    weight : string
+       Edge data key to use for weight (default 'weight').
+
+    Returns
+    -------
+    G : NetworkX Graph
+       A minimum spanning tree or forest. 
+    
+    Examples
+    --------
+    >>> G=nx.cycle_graph(4)
+    >>> G.add_edge(0,3,weight=2) # assign weight 2 to edge 0-3
+    >>> T=nx.prim_mst(G)
+    >>> print(sorted(T.edges(data=True)))
+    [(0, 1, {}), (1, 2, {}), (2, 3, {})]
+
+    Notes
+    -----
+    Uses Prim's algorithm.
+
+    If the graph edges do not have a weight attribute a default weight of 1
+    will be used.
+    """
+
+    T=nx.Graph(nx.prim_mst_edges(G,weight=weight,data=True))
+    # Add isolated nodes
+    if len(T)!=len(G):
+        T.add_nodes_from([n for n,d in G.degree().items() if d==0])
+    # Add node and graph attributes as shallow copy
+    for n in T:
+        T.node[n]=G.node[n].copy()
+    T.graph=G.graph.copy()
+    return T
 
--- networkx/algorithms/tests/test_mst.py
+++ networkx/algorithms/tests/test_mst.py
@@ -79,3 +79,55 @@ class TestMST:
         T=nx.minimum_spanning_tree(G,weight='distance')
         assert_equal(sorted(T.edges()),[(1,3),(2,3)])
         assert_equal(sorted(T.nodes()),[1,2,3,13])
+
+    def test_prim_mst(self):
+        T=nx.prim_mst(self.G)
+        assert_equal(T.edges(data=True),self.tree_edgelist)
+
+    def test_prim_mst_edges(self):
+        edgelist=sorted(nx.prim_mst_edges(self.G))
+        edgelist=sorted((sorted((u, v))[0], sorted((u, v))[1], d)
+                                  for u,v,d in edgelist)
+        assert_equal(edgelist,self.tree_edgelist)
+
+    def test_prim_mst_disconnected(self):
+        G=nx.Graph()
+        G.add_path([1,2])
+        G.add_path([10,20])
+        T=nx.prim_mst(G)
+        assert_equal(sorted(T.edges()),[(1, 2), (20, 10)])
+        assert_equal(sorted(T.nodes()),[1, 2, 10, 20])
+
+    def test_prim_mst_isolate(self):
+        G=nx.Graph()
+        G.add_nodes_from([1,2])
+        T=nx.prim_mst(G)
+        assert_equal(sorted(T.nodes()),[1, 2])
+        assert_equal(sorted(T.edges()),[])
+
+    def test_prim_mst_attributes(self):
+        G=nx.Graph()
+        G.add_edge(1,2,weight=1,color='red',distance=7)
+        G.add_edge(2,3,weight=1,color='green',distance=2)
+        G.add_edge(1,3,weight=10,color='blue',distance=1)
+        G.add_node(13,color='purple')
+        G.graph['foo']='bar'
+        T=nx.prim_mst(G)
+        assert_equal(T.graph,G.graph)
+        assert_equal(T.node[13],G.node[13])
+        assert_equal(T.edge[1][2],G.edge[1][2])
+
+    def test_prim_mst_edges_specify_weight(self):
+        G=nx.Graph()
+        G.add_edge(1,2,weight=1,color='red',distance=7)
+        G.add_edge(1,3,weight=30,color='blue',distance=1)
+        G.add_edge(2,3,weight=1,color='green',distance=1)
+        G.add_node(13,color='purple')
+        G.graph['foo']='bar'
+        T=nx.prim_mst(G)
+        assert_equal(sorted(T.nodes()),[1,2,3,13])
+        assert_equal(sorted(T.edges()),[(1,2),(2,3)])
+        T=nx.prim_mst(G,weight='distance')
+        assert_equal(sorted(T.edges()),[(1,3),(2,3)])
+        assert_equal(sorted(T.nodes()),[1,2,3,13])
+
