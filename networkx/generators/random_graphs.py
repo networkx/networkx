5e6b6a2c1996e973ddb2d660bb25c34c22cf9297
Aric Hagberg <aric.hagberg@gmail.com>
2011-03-11 18:41:23 -0700

    Use more pythonic style in fast_gnp Addresses #520

--- networkx/generators/random_graphs.py
+++ networkx/generators/random_graphs.py
@@ -78,38 +78,38 @@ def fast_gnp_random_graph(n, p, seed=None, directed=False):
        "Efficient generation of large random networks",
        Phys. Rev. E, 71, 036113, 2005.
     """
-    G=empty_graph(n)
+    G = empty_graph(n)
     G.name="fast_gnp_random_graph(%s,%s)"%(n,p)
 
     if not seed is None:
         random.seed(seed)
 
-    if p<=0 or p>=1:
+    if p <= 0 or p >= 1:
         return nx.gnp_random_graph(n,p)
 
-    v=1  # Nodes in graph are from 0,n-1 (this is the second node index).
-    w=-1
-    lp=math.log(1.0-p)  
+    v = 1  # Nodes in graph are from 0,n-1 (this is the second node index).
+    w = -1
+    lp = math.log(1.0 - p)  
 
     if directed:
-        while v<n:
-            lr=math.log(1.0-random.random())
-            w=w+1+int(lr/lp)
-            while w>=n and v<n:
-                w=w-n
-                v=v+1
-            if v<n:
-                G.add_edge(v,w)
+        while v < n:
+            lr = math.log(1.0 - random.random())
+            w = w + 1 + int(lr/lp)
+            while  w>= n and v < n:
+                w = w - n
+                v = v + 1
+            if v < n:
+                G.add_edge(v, w)
         return G
     else:
-        while v<n:
-            lr=math.log(1.0-random.random())
-            w=w+1+int(lr/lp)
-            while w>=v and v<n:
-                w=w-v
-                v=v+1
-            if v<n:
-                G.add_edge(v,w)
+        while v < n:
+            lr = math.log(1.0 - random.random())
+            w = w + 1 + int(lr/lp)
+            while w >= v and v < n:
+                w = w - v
+                v = v + 1
+            if v < n:
+                G.add_edge(v, w)
         return G
 
 
