d4245fafdefe155164277f2f5c72db2936739e4e
Aric Hagberg <aric.hagberg@gmail.com>
2011-03-22 19:16:52 -0600

    Update isolates functions to work with directed graphs. Fixes #526

--- networkx/algorithms/isolate.py
+++ networkx/algorithms/isolate.py
@@ -2,18 +2,16 @@
 """
 Functions for identifying isolate (degree zero) nodes.
 """
-#    Copyright (C) 2004-2010 by 
+#    Copyright (C) 2004-2011 by 
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
 #    All rights reserved.
 #    BSD license.
-
+import networkx as nx
 __author__ = """\n""".join(['Drew Conway <drew.conway@nyu.edu>',
                             'Aric Hagberg <hagberg@lanl.gov>'])
 __all__=['is_isolate','isolates']
-
-import networkx as nx
 
 def is_isolate(G,n):
     """Determine of node n is an isolate (degree zero).  
@@ -40,7 +38,7 @@ def is_isolate(G,n):
     >>> nx.is_isolate(G,3)
     True
     """
-    return len(G[n])==0 
+    return G.degree(n)==0 
 
 def isolates(G):
     """Return list of isolates in the graph.
@@ -59,16 +57,21 @@ def isolates(G):
     
     Examples
     --------
-    >>> G=nx.Graph()
+    >>> G = nx.Graph()
     >>> G.add_edge(1,2)
     >>> G.add_node(3)
     >>> nx.isolates(G)
     [3]
-    
+
     To remove all isolates in the graph use
     >>> G.remove_nodes_from(nx.isolates(G))
     >>> G.nodes()
     [1, 2]
-    """
-    return [n for n in G if len(G[n])==0]
 
+    For digraphs isolates have zero in-degree and zero out_degre
+    >>> G = nx.DiGraph([(0,1),(1,2)])
+    >>> G.add_node(3)
+    >>> nx.isolates(G)
+    [3]
+    """
+    return [n for (n,d) in G.degree_iter() if d==0]
