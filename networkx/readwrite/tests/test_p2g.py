c4170c852e4f0f346676a357ddb45fe8fa9ec790
Aric Hagberg <aric.hagberg@gmail.com>
2011-12-26 15:17:52 -0700

    put back p2g tests

--- networkx/readwrite/tests/test_p2g.py
+++ networkx/readwrite/tests/test_p2g.py
@@ -29,4 +29,36 @@ c
 """
         bytesIO = io.BytesIO(s)
         G = read_p2g(bytesIO)
-(G.edges(),H.edges())
+        assert_equal(G.name,'name')
+        assert_equal(sorted(G),['a','b','c'])
+        edges = [(str(u),str(v)) for u,v in G.edges()]
+        assert_edges_equal(G.edges(),[('a','c'),('a','b'),('c','a'),('c','c')])
+
+    def test_write_p2g(self):
+        s=b"""foo
+3 2
+1
+1 
+2
+2 
+3
+
+"""
+        fh=io.BytesIO()
+        G=nx.DiGraph()
+        G.name='foo'
+        G.add_edges_from([(1,2),(2,3)])
+        write_p2g(G,fh)
+        fh.seek(0)
+        r=fh.read()
+        assert_equal(r,s)
+
+    def test_write_read_p2g(self):
+        fh=io.BytesIO()
+        G=nx.DiGraph()
+        G.name='foo'
+        G.add_edges_from([('a','b'),('b','c')])
+        write_p2g(G,fh)
+        fh.seek(0)
+        H=read_p2g(fh)
+        assert_edges_equal(G.edges(),H.edges())
