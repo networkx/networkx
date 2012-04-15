220954f89695e6caa5c8150f58fa0c6bc090ba91
Aric Hagberg <aric.hagberg@gmail.com>
2012-04-15 16:14:15 -0600

    Add dominating set problem. Addresses #714

--- networkx/algorithms/approximation/__init__.py
+++ networkx/algorithms/approximation/__init__.py
@@ -1,3 +1,4 @@
 from networkx.algorithms.approximation.clique import *
+from networkx.algorithms.approximation.dominating_set import *
 from networkx.algorithms.approximation.ramsey import *
 from networkx.algorithms.approximation.vertex_cover import *
