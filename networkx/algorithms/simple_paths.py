aee56cf84b8faa30cd88556c0ba1219fa9333acc
Aric Hagberg <aric.hagberg@gmail.com>
2012-06-01 15:33:10 -0600

    Optimized all_simple_paths.  Return empty list if cutoff is < 1. Addresses #713

--- networkx/algorithms/simple_paths.py
+++ networkx/algorithms/simple_paths.py
@@ -4,11 +4,12 @@
 #    All rights reserved.
 #    BSD license.
 import networkx as nx
-__author__ = """\n""".join(['Sérgio Nery Simões <sergionery@gmail.com>'])
+__author__ = """\n""".join(['Sérgio Nery Simões <sergionery@gmail.com>',
+                            'Aric Hagberg <aric.hagberg@gmail.com>'])
 __all__ = ['all_simple_paths']
 
 def all_simple_paths(G, source, target, cutoff=None):
-    """Genereate all simple paths in the graph G from source to target.
+    """Generate all simple paths in the graph G from source to target.
 
     Parameters
     ----------
@@ -52,24 +53,58 @@ def all_simple_paths(G, source, target, cutoff=None):
     shortest_path
     """
     if G.is_multigraph():
-        def neighbors(G,n):
-            return (v for u,v in G.edges(n))
+        return _all_simple_paths_multigraph(G, source, target, cutoff=cutoff)
     else:
-        def neighbors(G,n):
-            return iter(G[n])
+        return _all_simple_paths_graph(G, source, target, cutoff=cutoff)
+
+def _all_simple_paths_graph(G, source, target, cutoff=None):
     if cutoff is None:
         cutoff = len(G)-1
+    if cutoff < 1:
+        return
     visited = [source]
-    stack = [neighbors(G,source)]
+    stack = [iter(G[source])]
     while stack:
         children = stack[-1]
         child = next(children, None)
         if child is None:
             stack.pop()
             visited.pop()
-        elif len(visited) <= cutoff:
+        elif len(visited) < cutoff:
             if child == target:
                 yield visited + [target]
             elif child not in visited:
                 visited.append(child)
-                stack.append(neighbors(G,child))
+                stack.append(iter(G[child]))
+        else: #len(visited) == cutoff:
+            if child == target or target in children:
+                yield visited + [target]
+            stack.pop()
+            visited.pop()
+
+
+def _all_simple_paths_multigraph(G, source, target, cutoff=None):
+    if cutoff is None:
+        cutoff = len(G)-1
+    if cutoff < 1:
+        return
+    visited = [source]
+    stack = [(v for u,v in G.edges(source))]
+    while stack:
+        children = stack[-1]
+        child = next(children, None)
+        if child is None:
+            stack.pop()
+            visited.pop()
+        elif len(visited) < cutoff:
+            if child == target:
+                yield visited + [target]
+            elif child not in visited:
+                visited.append(child)
+                stack.append((v for u,v in G.edges(child)))
+        else: #len(visited) == cutoff:
+            count = ([child]+list(children)).count(target)
+            for i in range(count):
+                yield visited + [target]
+            stack.pop()
+            visited.pop()
--- networkx/algorithms/tests/test_simple_paths.py
+++ networkx/algorithms/tests/test_simple_paths.py
@@ -52,3 +52,10 @@ def test_hamiltonian_path():
     paths = [list(p) for p in hamiltonian_path(G,0)]
     exact = [[0]+list(p) for p in permutations([1,2,3],3) ]
     assert_equal(sorted(paths),sorted(exact))
+
+def test_cutoff_zero():
+    G = nx.path_graph(4)
+    paths = nx.all_simple_paths(G,0,3,cutoff=0)
+    assert_equal(list(list(p) for p in paths),[])
+    paths = nx.all_simple_paths(nx.MultiGraph(G),0,3,cutoff=0)
+    assert_equal(list(list(p) for p in paths),[])
