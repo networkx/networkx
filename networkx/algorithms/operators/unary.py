77cb1530867a28bd22a63f4e50ed5ed671679ed3
chebee7i <chebee7i@gmail.com>
2012-07-26 19:13:19 -0700

    Add unary reverse() operator.

--- networkx/algorithms/operators/unary.py
+++ networkx/algorithms/operators/unary.py
@@ -10,7 +10,7 @@ from networkx.utils import is_string_like
 __author__ = """\n""".join(['Aric Hagberg (hagberg@lanl.gov)',
                            'Pieter Swart (swart@lanl.gov)',
                            'Dan Schult(dschult@colgate.edu)'])
-__all__ = ['complement']
+__all__ = ['complement', 'reverse']
 
 def complement(G, name=None):
     """Return the graph complement of G.
@@ -44,3 +44,44 @@ def complement(G, name=None):
                        for n2 in G if n2 not in nbrs
                        if n != n2) )
     return R
+
+def reverse(G, copy=True):
+    """Return the reverse directed graph of G.
+
+    Parameters
+    ----------
+    G : directed graph
+        A NetworkX directed graph
+    copy : bool
+        If True, then a new graph is returned. If False, then the graph is
+        reversed in place.
+
+    Returns
+    -------
+    H : directed graph
+        The reversed G.
+
+    """
+    from copy import deepcopy
+
+    if not G.is_directed():
+        raise nx.NetworkXError("Cannot reverse an undirected graph.")
+
+    if copy:
+        H = G.__class__(name="Reverse of ({0})".format(G.name))
+        H.add_nodes_from(G)
+        if G.is_multigraph():
+            rev_edges = [ (v,u,k,deepcopy(d))
+                          for u,v,k,d in G.edges(keys=True, data=True) ]
+        else:
+            rev_edges = [ (v,u,deepcopy(d)) for u,v,d in G.edges(data=True) ]
+        H.add_edges_from(rev_edges)
+        H.graph = deepcopy(G.graph)
+        H.node = deepcopy(G.node)
+    else:
+        G.pred, G.succ = G.succ, G.pred
+        G.adj = G.succ
+        H = G
+
+    return H
+
