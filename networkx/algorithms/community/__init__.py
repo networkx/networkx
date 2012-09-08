801b25867b35b827bea31bcaefc7bc143f12426f
Moritz Emanuel Beber <m.beber@jacobs-university.de>
2012-09-08 19:24:56 +0200

    Adds Kernighan--Lin bipartition algorithm.

--- networkx/algorithms/community/__init__.py
+++ networkx/algorithms/community/__init__.py
@@ -1,4 +1,5 @@
 from networkx.algorithms.community.asyn_lpa import *
 from networkx.algorithms.community.centrality import *
+from networkx.algorithms.community.kernighan_lin import *
 from networkx.algorithms.community.kclique import *
 from networkx.algorithms.community.quality import *
