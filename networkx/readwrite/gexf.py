24e26ad77d23704d3104c899ce7b5d4417da1527
Ramiro Gomez <web@ramiro.org>
2012-10-05 12:20:15 +0200

    set default to 100 for alpha channel in viz:color

--- networkx/readwrite/gexf.py
+++ networkx/readwrite/gexf.py
@@ -696,7 +696,7 @@ class GEXFReader(GEXF):
                 viz['color']={'r':int(color.get('r')),
                               'g':int(color.get('g')),
                               'b':int(color.get('b')),
-                              'a':float(color.get('a')),
+                              'a':float(color.get('a', 100)),
                               }
 
         size=node_xml.find("{%s}size"%self.NS_VIZ)
