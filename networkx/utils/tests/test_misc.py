b32097175a058bacd7a5a664a532e644d10c09b0
Aric Hagberg <aric.hagberg@gmail.com>
2012-12-29 07:24:32 -0700

    Add converter from dictionary of values to 1d numpy array. Modify dict_to_numpy_array to handle both dict of values and dict of dict of values. Addresses #787

--- networkx/utils/tests/test_misc.py
+++ networkx/utils/tests/test_misc.py
@@ -32,6 +32,40 @@ def test_random_number_distribution():
     z=pareto_sequence(20,exponent=1.5)
     z=discrete_sequence(20,distribution=[0,0,0,0,1,1,1,1,2,2,3])
 
+class TestNumpyArray(object):
+    numpy=1 # nosetests attribute, use nosetests -a 'not numpy' to skip test
+    @classmethod
+    def setupClass(cls):
+        global numpy
+        global assert_equal
+        global assert_almost_equal
+        try:
+            import numpy
+            from numpy.testing import assert_equal,assert_almost_equal
+        except ImportError:
+             raise SkipTest('NumPy not available.')
+
+    def test_dict_to_numpy_array1(self):
+        d = {'a':1,'b':2}
+        a = dict_to_numpy_array1(d)
+        assert_equal(a, numpy.array([1,2]))
+        a = dict_to_numpy_array1(d, mapping = {'b':0,'a':1})
+        assert_equal(a, numpy.array([2,1]))
+
+    def test_dict_to_numpy_array2(self):
+        d = {'a': {'a':1,'b':2},
+             'b': {'a':10,'b':20}}
+        a = dict_to_numpy_array(d)
+        assert_equal(a, numpy.array([[1,2],[10,20]]))
+        a = dict_to_numpy_array2(d, mapping = {'b':0,'a':1})
+        assert_equal(a, numpy.array([[20,10],[2,1]]))
 
 
-
+    def test_dict_to_numpy_array(self):
+        d = {'a': {'a':1,'b':2},
+             'b': {'a':10,'b':20}}
+        a = dict_to_numpy_array(d)
+        assert_equal(a, numpy.array([[1,2],[10,20]]))
+        d = {'a':1,'b':2}
+        a = dict_to_numpy_array1(d)
+        assert_equal(a, numpy.array([1,2]))
