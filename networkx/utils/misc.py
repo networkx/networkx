ee72ceeeae7e9d92846e1900ae2aa559bf02c6d1
chebee7i <chebee7i@gmail.com>
2013-01-08 01:18:30 -0600

    Add get_fobj() to networkx.utils

--- networkx/utils/misc.py
+++ networkx/utils/misc.py
@@ -97,7 +97,7 @@ def default_opener(filename):
     """
     cmds = {'darwin': ['open'],
             'linux2': ['xdg-open'],
-            'win32': ['cmd.exe', '/C', 'start', '']}
+            'win32': ['cmd.exe', '/c', 'start', '']}
     cmd = cmds[sys.platform] + [filename]
     subprocess.call(cmd)
 
@@ -149,3 +149,39 @@ def dict_to_numpy_array1(d,mapping=None):
         i = mapping[k1]
         a[i] = value
     return a
+
+def get_fobj(fname, mode='w+'):
+    """Obtain a proper file object.
+
+    Parameters
+    ----------
+    fname : string, file object, file descriptor
+        If a string or file descriptor, then we create a file object. If *fname*
+        is a file object, then we do nothing and ignore the specified *mode*
+        parameter.
+    mode : str
+        The mode of the file to be opened.
+
+    Returns
+    -------
+    fobj : file object
+        The file object.
+    close : bool
+        If *fname* was a string, then *close* will be *True* to signify that 
+        the file object should be closed after writing to it. Otherwise, *close*
+        will be *False* signifying that the user, in essence, created the file
+        object already and that subsequent operations should not close it.
+
+    """
+    if is_string_like(fname):
+        fobj = open(fname, mode)
+        close = True
+    elif hasattr(fname, 'write'):
+        # fname is a file-like object, perhaps a StringIO (for example)
+        fobj = fname
+        close = False
+    else:
+        # assume it is a file descriptor
+        fobj = os.fdopen(fname, mode)
+        close = False
+    return fobj, close
