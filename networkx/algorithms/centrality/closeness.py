f14effbe2219a78c17278c929c049f90dc66122e
Aric Hagberg <aric.hagberg@gmail.com>
2013-01-26 09:24:51 -0700

    Updates in closeness centrality.
    Addresses #824.
    Remove distance=True optin (use distance='weight' instead).
    Clarify docs and math notation.
    Cleanup whitespace and formatting.

--- networkx/algorithms/centrality/closeness.py
+++ networkx/algorithms/centrality/closeness.py
@@ -2,7 +2,7 @@
 Closeness centrality measures.
 
 """
-#    Copyright (C) 2004-2010 by 
+#    Copyright (C) 2004-2013 by
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Pieter Swart <swart@lanl.gov>
@@ -10,40 +10,43 @@ Closeness centrality measures.
 #    BSD license.
 import functools
 import networkx as nx
-__author__ = "\n".join(['Aric Hagberg (hagberg@lanl.gov)',
+__author__ = "\n".join(['Aric Hagberg <aric.hagberg@gmail.com>',
                         'Pieter Swart (swart@lanl.gov)',
                         'Sasha Gutfraind (ag362@cornell.edu)'])
 __all__ = ['closeness_centrality']
 
 
-def closeness_centrality(G, v=None, distance=None, normalized=True):
+def closeness_centrality(G, u=None, distance=None, normalized=True):
     r"""Compute closeness centrality for nodes.
 
-    Closeness centrality [1]_ of a node `u` is a measure based on the 
-    inverse of the sum of geodesic distances from `u` to all other nodes.
-    The raw sum of distances is dependent on graph order, thus 
-    closeness measure is normalized by the sum of minimum possible 
-    distances: `n - 1`.
+    Closeness centrality [1]_ of a node `u` is the reciprical of the
+    sum of the shortest path distances from `u` to all `n-1` other nodes.
+    Since the sum of distances is dependent on number of nodes in the
+    graph, closeness is normalized by the sum of minimum possible
+    distances `n-1`.
 
     .. math::
 
-        C(u) = \left [\frac{\sum_{v=1}^{n} d(v, u)}{n - 1} \right]^{-1} 
-             = \frac{n - 1}{\sum_{v=1}^{n} d(v, u)}
+        C(u) = \left [\frac{\sum_{v=1}^{n} d(v, u)}{n - 1} \right]^{-1}
+             = \frac{n - 1}{\sum_{v=1}^{n} d(v, u)},
 
-    Where `d(v, u)` is the shortest path distance between `v` and `u`.
+    where `d(v, u)` is the shortest-path distance between `v` and `u`,
+    and `n` is the number of nodes in the graph.
+
     Notice that higher values of closeness indicate higher centrality.
 
     Parameters
     ----------
     G : graph
-      A networkx graph 
-    v : node, optional
-      Return only the value for node v
-    distance : string key, optional (default=None)
-      Use specified edge key as edge distance. 
-      If True, use 'weight' as the edge key.
-    normalized : bool, optional      
-      If True (default) normalize by the graph size.
+      A NetworkX graph
+    u : node, optional
+      Return only the value for node u
+    distance : edge attribute key, optional (default=None)
+      Use the specified edge attribute as the edge distance in shortest 
+      path calculations
+    normalized : bool, optional
+      If True (default) normalize by the number of nodes in the connected
+      part of the graph.
 
     Returns
     -------
@@ -57,45 +60,44 @@ def closeness_centrality(G, v=None, distance=None, normalized=True):
 
     Notes
     -----
-    The closeness centrality is normalized to `\frac{n-1}{order(G)-1}`
-    where `n` is the number of nodes in the connected part of graph 
-    containing the node.  If the graph is not completely connected, this
-    algorithm computes the closeness centrality for each connected
-    part separately.
+    The closeness centrality is normalized to `(n-1)/(|G|-1)` where
+    `n` is the number of nodes in the connected part of graph
+    containing the node.  If the graph is not completely connected,
+    this algorithm computes the closeness centrality for each
+    connected part separately.
 
     References
     ----------
-    .. [1] Freeman, L.C., 1979. Centrality in networks: I. Conceptual clarification. 
-           Social Networks 1, 215--239.
-           http://www.soc.ucsb.edu/faculty/friedkin/Syllabi/Soc146/Freeman78.PDF
+    .. [1] Freeman, L.C., 1979. Centrality in networks: I.
+       Conceptual clarification.  Social Networks 1, 215--239.
+       http://www.soc.ucsb.edu/faculty/friedkin/Syllabi/Soc146/Freeman78.PDF
 
     """
     if distance is not None:
-        if distance is True: distance='weight'
-        path_length=functools.partial(nx.single_source_dijkstra_path_length,
-                                      weight=distance)
+        # use Dijkstra's algorithm with specified attribute as edge weight 
+        path_length = functools.partial(nx.single_source_dijkstra_path_length,
+                                        weight=distance)
     else:
-        path_length=nx.single_source_shortest_path_length
+        path_length = nx.single_source_shortest_path_length
 
-    if v is None:
-        nodes=G.nodes()
+    if u is None:
+        nodes = G.nodes()
     else:
-        nodes=[v]
-    closeness_centrality={}
+        nodes = [u]
+    closeness_centrality = {}
 
     for n in nodes:
-        sp=path_length(G,n)
-        totsp=sum(sp.values())
+        sp = path_length(G,n)
+        totsp = sum(sp.values())
         if totsp > 0.0 and len(G) > 1:
-            closeness_centrality[n]= (len(sp)-1.0) / totsp
+            closeness_centrality[n] = (len(sp)-1.0) / totsp
             # normalize to number of nodes-1 in connected part
             if normalized:
-                s=(len(sp)-1.0) / ( len(G) - 1 )
+                s = (len(sp)-1.0) / ( len(G) - 1 )
                 closeness_centrality[n] *= s
-        else:                                                                
-            closeness_centrality[n]=0.0           
-    if v is not None:
-        return closeness_centrality[v]
+        else:
+            closeness_centrality[n] = 0.0
+    if u is not None:
+        return closeness_centrality[u]
     else:
         return closeness_centrality
-
