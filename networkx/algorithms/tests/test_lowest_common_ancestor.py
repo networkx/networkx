c4ab1b79cbb79982dff5b29268fe0e5910b4c311
Alex Roper <aroper@umich.edu>
2013-05-05 20:38:23 -0400

    Added main 'does it work' test for the dag algorithm.

--- networkx/algorithms/tests/test_lowest_common_ancestor.py
+++ networkx/algorithms/tests/test_lowest_common_ancestor.py
@@ -9,11 +9,6 @@ def get_pair(dictionary, n1, n2):
   else:
     return dictionary[n2, n1]
 
-def has_same_pairs(d1, d2):
-  for (a, b) in set(((min(pair), max(pair))
-                     for pair in d1.keys() + d2.keys())):
-    assert_equal(get_pair(d1, a, b), get_pair(d2, a, b))
-
 class TestTreeLCA:
 
   def setUp(self):
@@ -40,6 +35,12 @@ class TestTreeLCA:
 
     self.gold = gold
 
+  @staticmethod
+  def assert_has_same_pairs(d1, d2):
+    for (a, b) in set(((min(pair), max(pair))
+                       for pair in d1.keys() + d2.keys())):
+      assert_equal(get_pair(d1, a, b), get_pair(d2, a, b))
+
 
   def test_tree_all_pairs_lowest_common_ancestor1(self):
     """Specifying the root is optional."""
@@ -55,12 +56,9 @@ class TestTreeLCA:
     """Specifying no pairs same as specifying all."""
     all_pairs = itertools.chain(itertools.combinations(self.DG.nodes(), 2),
                                 ((node, node) for node in self.DG.nodes_iter()))
-    for (a, b), lca in nx.tree_all_pairs_lowest_common_ancestor(self.DG, 0,
-                                                                all_pairs):
-      if (a, b) in self.ans:
-        assert_equal(self.ans[a, b], lca)
-      else:
-        assert_equal(self.ans[b, a], lca)
+
+    ans = dict(nx.tree_all_pairs_lowest_common_ancestor(self.DG, 0, all_pairs))
+    self.assert_has_same_pairs(ans, self.ans)
 
   def test_tree_all_pairs_lowest_common_ancestor4(self):
     """Gives the right answer."""
@@ -85,7 +83,7 @@ class TestTreeLCA:
     ans = dict(nx.tree_all_pairs_lowest_common_ancestor(self.DG, 1))
     gold = dict(((pair, lca) for (pair, lca) in self.gold.iteritems()
                  if all((n in (1, 3, 4) for n in pair))))
-    has_same_pairs(gold, ans)
+    self.assert_has_same_pairs(gold, ans)
 
   def test_tree_all_pairs_lowest_common_ancestor7(self):
     """Works on disconnected nodes."""
@@ -99,3 +97,88 @@ class TestTreeLCA:
     
     assert_raises(nx.NetworkXError, list,
                   nx.tree_all_pairs_lowest_common_ancestor(G))
+
+  def test_tree_all_pairs_lowest_common_ancestor8(self):
+    """Raises right errors if not a tree."""
+    # Cycle
+    G = nx.DiGraph()
+    G.add_cycle((1, 2))
+    assert_raises(nx.NetworkXError, list,
+                  nx.tree_all_pairs_lowest_common_ancestor(G))
+
+    # DAG
+    G = nx.DiGraph([(0, 2), (1, 2)])
+    assert_raises(nx.NetworkXError, list,
+                  nx.tree_all_pairs_lowest_common_ancestor(G))
+
+class TestDAGLCA:
+
+  def setUp(self):
+    self.DG = nx.DiGraph()
+    self.DG.add_path((0, 1, 2, 3))
+    self.DG.add_path((0, 4, 3))
+    self.DG.add_path((0, 5, 6, 8, 3))
+    self.DG.add_path((5, 7, 8))
+    self.DG.add_edge(6, 2)
+    self.DG.add_edge(7, 2)
+
+    self.root_distance = nx.shortest_path_length(self.DG, source=0)
+
+    self.gold = {(1, 1): 1,
+                 (1, 2): 1,
+                 (1, 3): 1,
+                 (1, 4): 0,
+                 (1, 5): 0,
+                 (1, 6): 0,
+                 (1, 7): 0,
+                 (1, 8): 0,
+                 (2, 2): 2,
+                 (2, 3): 2,
+                 (2, 4): 0,
+                 (2, 5): 5,
+                 (2, 6): 6,
+                 (2, 7): 7,
+                 (2, 8): 7,
+                 (3, 3): 3,
+                 (3, 4): 4,
+                 (3, 5): 5,
+                 (3, 6): 6,
+                 (3, 7): 7,
+                 (3, 8): 8,
+                 (4, 4): 4,
+                 (4, 5): 0,
+                 (4, 6): 0,
+                 (4, 7): 0,
+                 (4, 8): 0,
+                 (5, 5): 5,
+                 (5, 6): 5,
+                 (5, 7): 5,
+                 (5, 8): 5,
+                 (6, 6): 6,
+                 (6, 7): 5,
+                 (6, 8): 6,
+                 (7, 7): 7,
+                 (7, 8): 7,
+                 (8, 8): 8}
+    self.gold.update((((0, n), 0) for n in self.DG.nodes_iter()))
+
+  def assert_lca_dicts_same(self, d1, d2, G_root=None):
+    """Checks if d1 and d2 contain the same set of pairs and that they have
+    a node at the same distance from root for each. G_root is None
+    (to use self.DG) or (G, root) to use."""
+    if G_root is None:
+      G = self.DG
+      root_distance = self.root_distance
+    else:
+      G, root = G_root
+      root_distance = nx.shortest_path_length(G, source=root)
+
+    for (a, b) in set(((min(pair), max(pair))
+                       for pair in d1.keys() + d2.keys())):
+      assert_equal(root_distance[get_pair(d1, a, b)],
+                   root_distance[get_pair(d2, a, b)])
+
+  def test_all_pairs_lowest_common_ancestor1(self):
+    """Produces the correct results."""
+    self.assert_lca_dicts_same(dict(nx.all_pairs_lowest_common_ancestor(self.DG)),
+                               self.gold)
