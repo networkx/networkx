d38de1f4df8c50917fb2b82ea15e3f816d59e942
Dan Schult <dschult@colgate.edu>
2013-06-17 17:17:51 -0400

    Create nonrecursive function for simple_cycles
    Copied simple_cycles to recursive_simple_cycles and added a new
    simple_cycles that is the same algorithm but not recursive.  Two
    changes in the API for simple_cycle are that
    1) it now returns a generator instead of returning a list.
    2) Cycles no longer repeat the first node as the last.  The last
    node in the list is assumed to connect to the first node on the list.
    
    Tests added to compare output of simple_cycles and recursive_simple_cycles
    
    Thanks to erikasantos and friedsoap for instigating this and suggesting
    helpful changes (which I incorporated).  This addresses #874 #888 #889

--- networkx/algorithms/cycles.py
+++ networkx/algorithms/cycles.py
@@ -13,8 +13,9 @@ import networkx as nx
 from networkx.utils import *
 from collections import defaultdict
 
-__all__ = ['cycle_basis','simple_cycles']
+__all__ = ['cycle_basis','simple_cycles','recursive_simple_cycles']
 __author__ = "\n".join(['Jon Olav Vik <jonovik@gmail.com>',
+                        'Dan Schult <dschult@colgate.edu>',
                         'Aric Hagberg <hagberg@lanl.gov>'])
 
 @not_implemented_for('directed')
@@ -103,6 +104,128 @@ def simple_cycles(G):
     Two elementary circuits are distinct if they are not cyclic permutations
     of each other.
 
+    This is a nonrecursive, iterator/generator version of Johnson's
+    algorithm [1]_.  There may be better algorithms for some cases [2]_ [3]_.
+
+    Parameters
+    ----------
+    G : NetworkX DiGraph
+       A directed graph
+
+    Returns
+    -------
+    cycle_generator: generator
+       A generator that produces elementary cycles of the graph.  Each cycle is
+       a list of nodes with the first and last nodes being the same.
+
+    Examples
+    --------
+    >>> G = nx.DiGraph([(0, 0), (0, 1), (0, 2), (1, 2), (2, 0), (2, 1), (2, 2)])
+    >>> list(nx.simple_cycles(G))
+    [[0], [0, 1, 2], [0, 2], [1, 2], [2]]
+
+    Notes
+    -----
+    The implementation follows pp. 79-80 in [1]_.
+
+    The time complexity is O((n+e)(c+1)) for n nodes, e edges and c
+    elementary circuits.
+
+    To filter the cycles so that they don't include certain nodes or edges,
+    copy your graph and eliminate those nodes or edges before calling.
+    >>> copyG = G.copy()
+    >>> copyG.remove_nodes_from([1])
+    >>> copyG.remove_edges_from([(0,1)])
+    >>> list(nx.simple_cycles(G)
+    [[0], [0, 2], [2]]
+
+    References
+    ----------
+    .. [1] Finding all the elementary circuits of a directed graph.
+       D. B. Johnson, SIAM Journal on Computing 4, no. 1, 77-84, 1975.
+       http://dx.doi.org/10.1137/0204007
+
+    .. [2] Enumerating the cycles of a digraph: a new preprocessing strategy.
+       G. Loizou and P. Thanish, Information Sciences, v. 27, 163-182, 1982.
+
+    .. [3] A search strategy for the elementary cycles of a directed graph.
+       J.L. Szwarcfiter and P.E. Lauer, BIT NUMERICAL MATHEMATICS,
+       v. 16, no. 2, 192-204, 1976.
+
+    See Also
+    --------
+    cycle_basis
+    """
+    def _unblock(thisnode,blocked,B):
+        stack=set([thisnode])
+        while stack:
+            node=stack.pop()
+            if node in blocked:
+                blocked.remove(node)
+                stack.update(B[node])
+                B[node].clear()
+
+    # Johnson's algorithm requires some ordering of the nodes.
+    # We assign the arbitrary ordering given by the strongly connected comps
+    # There is no need to track the ordering as each node removed as processed.
+    subG=G.copy()   # save the actual graph so we can mutate it here
+    sccs = nx.strongly_connected_components(subG)
+    while sccs:
+        scc=sccs.pop(0)
+        # order of scc determines ordering of nodes
+        startnode = scc.pop(0)
+        # Processing node runs "circuit" routine from recursive version
+        path=[startnode]
+        blocked = set() # vertex: blocked from search?
+        closed = set() # nodes involved in a cycle
+        blocked.add(startnode)
+        B=defaultdict(set) # graph portions that yield no elementary circuit
+        stack=[ (startnode,list(subG[startnode])) ]  # subG gives component nbrs
+        while stack:
+            thisnode,nbrs = stack[-1]
+            if nbrs:
+                nextnode = nbrs.pop()
+#                    print thisnode,nbrs,":",nextnode,blocked,B,path,stack,startnode
+#                    f=raw_input("pause")
+                if nextnode == startnode:
+                    yield path[:]
+                    closed.update(path)
+#                        print "Found a cycle",path,closed
+                elif nextnode not in blocked:
+                    path.append(nextnode)
+                    stack.append( (nextnode,list(subG[nextnode])) )
+                    blocked.add(nextnode)
+                    continue
+            # done with nextnode... look for more neighbors
+            if not nbrs:  # no more nbrs
+                if thisnode in closed:
+                    _unblock(thisnode,blocked,B)
+                else:
+                    for nbr in G[thisnode]:
+                        if thisnode not in B[nbr]:
+                            B[nbr].add(thisnode)
+                stack.pop()
+#                assert path[-1]==thisnode
+                path.pop()
+        # done processing this node
+        subG.remove_node(startnode)
+        H=subG.subgraph(scc)  # make smaller to avoid work in SCC routine
+        sccs.extend(nx.strongly_connected_components(H))
+
+
+@not_implemented_for('undirected')
+def recursive_simple_cycles(G):
+    """Find simple cycles (elementary circuits) of a directed graph.
+
+    A simple cycle, or elementary circuit, is a closed path where no
+    node appears twice, except that the first and last node are the same.
+    Two elementary circuits are distinct if they are not cyclic permutations
+    of each other.
+
+    This version uses a recursive algorithm to build a list of cycles.
+    You should probably use the iterator version caled simple_cycles().
+    Warning: This recursive version uses lots of RAM!
+
     Parameters
     ----------
     G : NetworkX DiGraph
@@ -137,7 +260,7 @@ def simple_cycles(G):
 
     See Also
     --------
-    cycle_basis
+    simple_cycles, cycle_basis
     """
     # Jon Olav Vik, 2010-08-09
     def _unblock(thisnode):
@@ -153,7 +276,7 @@ def simple_cycles(G):
         blocked[thisnode] = True
         for nextnode in component[thisnode]: # direct successors of thisnode
             if nextnode == startnode:
-                result.append(path + [startnode])
+                result.append(path[:])
                 closed = True
             elif not blocked[nextnode]:
                 if circuit(nextnode, startnode, component):
