28ee150c72ed86618e81f461cfbb3c1757a7c7fe
jac2130 <jac2130@columbia.edu>
2013-06-26 14:55:34 -0600

    improved the hamming distance based on a comment by Aric Hagberg.

--- hamming.py
+++ hamming.py
@@ -1,18 +1,13 @@
-def hamming_distance(G1, G2):
+def hamming_distance(G, H):
     '''
-    This Function calculates the Hamming Distance for two potentially directed graphs.
+    This function computes the Hamming Distance between two (possibly directed) graphs;
+    this improved version was suggested by Aric Hagberg.
     '''
-    import numpy as np
-
-    for node in G1.nodes():
-        if node not in G2.nodes():
-            G2.add_node(node)
-
-    for node in G2.nodes():
-        if node not in G1.nodes():
-            G1.add_node(node)
-
-    A1=np.array(nx.to_numpy_matrix(G1))
-    A2=np.array(nx.to_numpy_matrix(G2))
-
-    return sum(ch1!=ch2 for ch1, ch2 in zip(list(A1.flatten()), list(A2.flatten())))
+    count = 0
+    for e in G.edges_iter():
+        if not H.has_edge(*e):
+            count+=1
+    for e in H.edges_iter():
+        if not G.has_edge(*e):
+            count+=1
+    return count
