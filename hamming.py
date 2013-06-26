0a6dd77b99b5936260d62f2afd777bc9155dcc9e
jac2130 <jac2130@columbia.edu>
2013-06-26 16:50:01 -0600

    adding a generalized hamming distance for differences between weighted graphs and a potentially different cost for non-relations and zero relations.

--- hamming.py
+++ hamming.py
@@ -11,3 +11,25 @@ def hamming_distance(G, H):
         if not G.has_edge(*e):
             count+=1
     return count
+
+def generalized_hd(G, H, no_edge_cost=0.5, max_relation=2):
+    '''
+    This function computes a generalized Hamming Distance, of a (possibly weighted and directed) graph, where not having a relation
+    has a potentially different cost than having a zero relation
+    '''
+    count=0
+    for e in G.edges_iter():
+        if H.has_edge(*e):
+            try: count+= abs(nx.get_edge_attributes(G, 'weight')[e]-nx.get_edge_attributes(H, 'weight')[e])
+            except: print '%s does not have a weight!' % str(e)
+        else:
+            try: count+= (abs(nx.get_edge_attributes(G, 'weight')[e]) + no_edge_cost)
+            except: print '%s does not have a weight!' % str(e)
+
+    #And now for the edges that are in H but not in G:
+    for e in H.edges_iter():
+        if not G.has_edge(*e):
+            try: count+= (abs(nx.get_edge_attributes(H, 'weight')[e]) + no_edge_cost)
+            except: print '%s does not have a weight!' % str(e)
+
+    return count
