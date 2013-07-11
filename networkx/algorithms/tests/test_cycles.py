0a3ab1e0edf671c5afb90826ce880012c9f8aee7
Dan Schult <dschult@colgate.edu>
2013-07-11 11:43:26 -0400

    Revert cycles to repeat starting node at the end.

--- networkx/algorithms/tests/test_cycles.py
+++ networkx/algorithms/tests/test_cycles.py
@@ -49,7 +49,7 @@ class TestCycles:
     def test_simple_cycles(self):
         G = nx.DiGraph([(0, 0), (0, 1), (0, 2), (1, 2), (2, 0), (2, 1), (2, 2)])
         c=sorted(nx.simple_cycles(G))
-        ca=[[0], [0, 1, 2], [0, 2], [1, 2], [2]]
+        ca=[[0, 0], [0, 1, 2, 0], [0, 2, 0], [1, 2, 1], [2, 2]]
         for (a,b) in zip(c,ca):
             assert_true(self.is_cyclic_permutation(a,b))
 
@@ -68,12 +68,12 @@ class TestCycles:
         G = nx.DiGraph()
         G.add_path([1,2,3,1])
         c=sorted(nx.simple_cycles(G))
-        assert_equal(c,[[1,2,3]])
+        assert_equal(c,[[1,2,3,1]])
         G.add_path([10,20,30,10])
         c=sorted(nx.simple_cycles(G))
-        ca=[[1,2,3],[10,20,30]]
+        ca=[[1,2,3,1],[10,20,30,10]]
         for (a,b) in zip(c,ca):
-            assert_true(self.is_cyclic_permutation(a,b))
+            assert_true(self.is_cyclic_permutation(a[:-1],b[:-1]))
 
     def test_simple_cycles_empty(self):
         G = nx.DiGraph()
@@ -117,5 +117,7 @@ class TestCycles:
             cc=sorted(nx.simple_cycles(G))
             rcc=sorted(nx.recursive_simple_cycles(G))
             assert_equal(len(cc),len(rcc))
+            print cc
+            print rcc
             for c in cc:
-                assert_true(any(self.is_cyclic_permutation(c,rc) for rc in rcc))
+                assert_true(any(self.is_cyclic_permutation(c[:-1],rc[:-1]) for rc in rcc))
