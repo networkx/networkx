b854b78c7f40b78bb3586c9c4286d97c4f9cdfd7
Aric Hagberg <aric.hagberg@gmail.com>
2013-11-09 10:49:41 -0700

    Drop official Python3.1 support
    But pretty much everything will work OK.  Some small issues with xml.etree are annoying to support specially for Python3.1.

--- networkx/release.py
+++ networkx/release.py
@@ -238,7 +238,6 @@ classifiers = [
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries :: Python Modules',
