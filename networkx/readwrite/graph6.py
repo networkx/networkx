941fa2a54cb7987da95643d196cf224d2f40c4eb
Aric Hagberg <aric.hagberg@gmail.com>
2013-12-15 09:45:55 -0700

    Add docuementation, formatting

--- networkx/readwrite/graph6.py
+++ networkx/readwrite/graph6.py
@@ -1,8 +1,6 @@
-"""
-**************
-Graph6
-**************
-Read graphs in graph6 format.
+"""Graph6
+
+Read and write graphs in graph6 format.
 
 Format
 ------
@@ -10,7 +8,6 @@ Format
 "graph6 and sparse6 are formats for storing undirected graphs in a
 compact manner, using only printable ASCII characters. Files in these
 formats have text type and contain one line per graph."
-http://cs.anu.edu.au/~bdm/data/formats.html
 
 See http://cs.anu.edu.au/~bdm/data/formats.txt for details.
 """
@@ -23,19 +20,44 @@ See http://cs.anu.edu.au/~bdm/data/formats.txt for details.
 #    Tomas Gavenciak <gavento@ucw.cz>
 #    All rights reserved.
 #    BSD license.
-__author__ = """\n""".join(['Tomas Gavenciak <gavento@ucw.cz>',
-                            'Aric Hagberg <aric.hagberg@lanl.gov'
-                            ])
-__all__ = ['read_graph6', 'parse_graph6',
-           'generate_graph6', 'write_graph6']
 import networkx as nx
 from networkx.exception import NetworkXError
 from networkx.utils import open_file, not_implemented_for
+__author__ = """\n""".join(['Tomas Gavenciak <gavento@ucw.cz>',
+                            'Aric Hagberg <aric.hagberg@lanl.gov'])
+__all__ = ['read_graph6', 'parse_graph6', 'generate_graph6', 'write_graph6']
 
-def parse_graph6(str):
+def parse_graph6(string):
     """Read a simple undirected graph in graph6 format from string.
 
-    Returns a single Graph.
+    Parameters
+    ----------
+    string : string
+       Data in graph6 format
+
+    Returns
+    -------
+    G : Graph
+
+    Raises
+    ------
+    NetworkXError
+        If the string is unable to be parsed in graph6 format
+
+    Examples
+    --------
+    >>> G = nx.parse_graph6('A_')
+    >>> sorted(G.edges())
+    [(0, 1)]
+
+    See Also
+    --------
+    generate_graph6, read_graph6, write_graph6
+
+    References
+    ----------
+    Graph6 specification:
+    http://cs.anu.edu.au/~bdm/data/formats.txt for details.
     """
     def bits():
         """Return sequence of individual bits from 6-bit-per-value
@@ -44,9 +66,9 @@ def parse_graph6(str):
             for i in [5,4,3,2,1,0]:
                 yield (d>>i)&1
 
-    if str.startswith('>>graph6<<'):
-        str = str[10:]
-    data = graph6_to_data(str)
+    if string.startswith('>>graph6<<'):
+        string = string[10:]
+    data = graph6_to_data(string)
     n, data = data_to_n(data)
     nd = (n*(n-1)//2 + 5) // 6
     if len(data) != nd:
@@ -56,14 +78,46 @@ def parse_graph6(str):
     G=nx.Graph()
     G.add_nodes_from(range(n))
     for (i,j),b in zip([(i,j) for j in range(1,n) for i in range(j)], bits()):
-        if b: G.add_edge(i,j)
+        if b:
+            G.add_edge(i,j)
+
     return G
 
 @open_file(0,mode='rt')
 def read_graph6(path):
     """Read simple undirected graphs in graph6 format from path.
 
-    Returns a list of Graphs, one for each non-empty line of the file.
+    Parameters
+    ----------
+    path : file or string
+       File or filename to write.
+
+    Returns
+    -------
+    G : Graph or list of Graphs
+       If the file contains multple lines then a list of graphs is returned
+
+    Raises
+    ------
+    NetworkXError
+        If the string is unable to be parsed in graph6 format
+
+    Examples
+    --------
+    >>> import io
+    >>> file = io.StringIO('A_')
+    >>> G = nx.read_graph6(file)
+    >>> sorted(G.edges())
+    [(0, 1)]
+
+    See Also
+    --------
+    generate_graph6, parse_graph6, write_graph6
+
+    References
+    ----------
+    Graph6 specification:
+    http://cs.anu.edu.au/~bdm/data/formats.txt for details.
     """
     glist = []
     for line in path:
@@ -76,13 +130,50 @@ def read_graph6(path):
     else:
         return glist
 
-@not_implemented_for('directed')
+@not_implemented_for('directed','multigraph')
 def generate_graph6(G, nodes = None, header=True):
-    """Generate graph6 format description of a simple undirected graph.
+    """Generate graph6 format string from a simple undirected graph.
 
-    The format does not support edge or vetrtex labels and multiedges.
-    Optional graph6 format prefix is controlled by ``header``.
-    Returns an ascii string.
+    Parameters
+    ----------
+    G : Graph (undirected)
+
+    nodes: list or iterable
+       Nodes are labeled 0...n-1 in the order provided.  If None the ordering
+       given by G.nodes() is used.
+
+    header: bool
+       If True add '>>graph6<<' string to head of data
+
+    Returns
+    -------
+    s : string
+       String in graph6 format
+
+    Raises
+    ------
+    NetworkXError
+        If the graph is directed or has parallel edges
+
+    Examples
+    --------
+    >>> G = nx.Graph([(0, 1)])
+    >>> nx.generate_graph6(G)
+    '>>graph6<<A_'
+
+    See Also
+    --------
+    generate_graph6, parse_graph6, write_graph6
+
+    Notes
+    -----
+    The format does not support edge or node labels, parallel edges or
+    self loops.  If self loops are present they are silently ignored.
+
+    References
+    ----------
+    Graph6 specification:
+    http://cs.anu.edu.au/~bdm/data/formats.txt for details.
     """
     if nodes is not None:
         ns = list(nodes)
@@ -114,12 +205,53 @@ def generate_graph6(G, nodes = None, header=True):
 
 
 @open_file(1, mode='wt')
-def write_graph6(G, path, header=True):
-    """Write simple undirected graphs to given path in graph6 format,
-    one per line.
+def write_graph6(G, path, nodes = None, header=True):
+    """Write a simple undirected graph to path in graph6 format.
 
-    Writes graph6 header with every graph by default.
-    See ``generate_graph6`` for details.
+    Parameters
+    ----------
+    G : Graph (undirected)
+
+    path : file or string
+       File or filename to write.
+
+    nodes: list or iterable
+       Nodes are labeled 0...n-1 in the order provided.  If None the ordering
+       given by G.nodes() is used.
+
+    header: bool
+       If True add '>>graph6<<' string to head of data
+
+    Returns
+    -------
+    s : string
+       String in graph6 format
+
+    Raises
+    ------
+    NetworkXError
+        If the graph is directed or has parallel edges
+
+    Examples
+    --------
+    >>> G = nx.Graph([(0, 1)])
+    >>> import io
+    >>> file = io.StringIO()
+    >>> nx.write_graph6(G, file)
+
+    See Also
+    --------
+    generate_graph6, parse_graph6, write_graph6
+
+    Notes
+    -----
+    The format does not support edge or node labels, parallel edges or
+    self loops.  If self loops are present they are silently ignored.
+
+    References
+    ----------
+    Graph6 specification:
+    http://cs.anu.edu.au/~bdm/data/formats.txt for details.
     """
     path.write(generate_graph6(G, header=header))
 
