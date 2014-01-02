7489f38f519ebb9218c0a40acf29fa000aa7c866
huston <h@graphalchemist.com>
2014-01-02 14:52:42 -0800

    refactored to allow optional u, v... added suggestions for normalizations...

--- networkx/algorithms/centrality/dispersion.py
+++ networkx/algorithms/centrality/dispersion.py
@@ -1,52 +1,98 @@
-import networkx as nx
 from itertools import combinations
 
+__author__ = "\n".join(['Ben Edwards (bedwards@cs.unm.edu)',
+                        'Huston Hedinger (h@graphalchemist.com)',
+                        'Dan Schult (dschult@colgate.edu)'])
 
-__author__ = """Huston Hedinger (h@graphalchemist.com)"""
-__all__ = ['dispersion_centrality']
+__all__ = ['dispersion']
 
 
-def dispersion_centrality(G, u, v=None, exclude_nodes=None, normalized=True):
-    """ A python implementation of 'dispersion' as defined by Lars Backstrom
-    and Jon Kleinberg here: http://arxiv.org/pdf/1310.6753v1.pdf
+def dispersion(G, u=None, v=None, normalized=True, alpha=1.0, b=0.0, c=0.0):
+    r""" A python implementation of 'dispersion' as defined by Lars Backstrom
+    and Jon Kleinberg [1]_.
     
+    Where a link between two actors ('u' and 'v') has a high dispersion when their mutual 
+    ties ('s' and 't') are not well connected with each other.
+    
+    .. math::
+
     Parameters
     ----------
     G : graph
       A NetworkX graph
-    u : node, required
-      the ego node of the network
+    u : node, optional
+      the source node for the dispersion score (e.g. ego node of the network)
     v : node, optional
-      Return only the dispersion value for node v
-    exclude_nodes : list of nodes, optional
-      Exclude nodes from score
-    normalized : bool, optional
+      the target node for the dispersion score if specified
+    
+    Note: when not specifying 'u' or 'v' on larger networks can take some time.
+    Typical usage would be to run dispersion on ego network (G_u) with u specified 
+
+    normalized : bool
       If True (default) normalize by the embededness of the nodes (u and v).
+    
+    Returns
+    -------
+    nodes : dictionary
+       If u or v is specified, returns a dictionary of nodes with dispersion score 
+       for all "target" nodes
+       If neither u or v is specified, returns a dictionary of dictionaries for all nodes 
+       'u' in the graph with a dispersion score for each node 'v'.
+
+    References
+    ----------
+    .. [1] Romantic Partnerships and the Dispersion of Social Ties:
+        A Network Analysis of Relationship Status on Facebook.
+        Lars Backstrom, Jon Kleinberg.
+        http://arxiv.org/pdf/1310.6753v1.pdf
+    
     """
 
-    G_u = G
-    dispersion = dict.fromkeys(G_u, 0)
-    u_nbrs = set(G[u])
-
-    for v in u_nbrs:
-        ST = set(n for n in G[v] if n in u_nbrs)
+    def _dispersion(G_u, u, v):
+        """ dispersion for all nodes 'v' in a ego network G_u of node 'u' """
+        u_nbrs = set(G_u[u])
+        ST = set(n for n in G_u[v] if n in u_nbrs)
         set_uv=set([u,v])
         possib = combinations(ST, 2)
         total = 0
-        for p in possib:
-            s = p[0]
-            t = p[1]
-            nbrs_s = u_nbrs.intersection(G[s]) - set_uv
-            if nbrs_s.isdisjoint(G[t]):
+        for (s,t) in possib:
+            nbrs_s = u_nbrs.intersection(G_u[s]) - set_uv
+            if nbrs_s.isdisjoint(G_u[t]):
                 total += 1
         embededness = len(ST)
-    
+
         if normalized == True:
-            if embededness != 0:
-                norm_disp = (total/embededness)
-            elif embededness == 0:
-                norm_disp = total
-            dispersion[v] = {'norm_disp' : norm_disp}
+            if embededness + c != 0:
+                norm_disp = ((total + b)**alpha)/(embededness + c)
+            else:
+                norm_disp = (total+b)**alpha
+            dispersion = norm_disp
+
         else:
-            dispersion[v] = {'abs_disp': total}
-    return dispersion
+            dispersion = total
+
+        return dispersion
+
+    if u is None:
+        # v and u are not specified
+        if v is None:
+            results = dict.fromkeys(G, {})
+            for u in G:
+                for v in G[u]:
+                    results[u][v] = _dispersion(G, u, v)
+        # u is not specified, but v is
+        else:
+            results = dict.fromkeys(G, {})
+            for u in G:
+                results[u] = _dispersion(G, u, v)
+    else:
+        # u is specified with no target v
+        if v is None:
+            results = dict.fromkeys(G, {})
+            for v in G:
+                results[v] = _dispersion(G, u, v)
+        # both u and v are specified
+        else:
+            results = {v : _dispersion(G, u, v)}
+
+    return results
