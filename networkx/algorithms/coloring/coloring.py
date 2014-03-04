704319b33be57905d3c8ca03bd71f07e3ae59720
Jan Aagaard Meier <janzeh@gmail.com>
2014-03-04 17:45:22 +0100

    Slight code cleanup, and added the CS coloring method

--- networkx/algorithms/coloring/coloring.py
+++ networkx/algorithms/coloring/coloring.py
@@ -18,223 +18,233 @@ GIS-Color
 
 from heapq import heappush, heappop
 import networkx as nx
+import sys
 
 __author__ = "\n".join(["Christian Olsson <chro@itu.dk>",
-						"Jan Aagaard Meier <jmei@itu.dk>",
-						"Henrik Haugbølle <hhau@itu.dk>"])
+                        "Jan Aagaard Meier <jmei@itu.dk>",
+                        "Henrik Haugbølle <hhau@itu.dk>"])
 __all__ = ['coloring']
 
 
 def interchange(G):
-	""" Not implemented """
+    """ Not implemented """
 
 def strategy_maxdegree(G):
-	queue = []
-	for n in G.nodes(): # take each node of the graph ...
-		heappush(queue, (len(G.neighbors(n)) * -1, n)) # ... and push it onto the priority queue using its reneighbour degree as priority (max as high priorty)
-	
-	while len(queue): # iterate the priority queue until empty
-		(priority, node) = heappop(queue)
-		yield node
+    queue = []
+    for n in G.nodes(): # take each node of the graph ...
+        heappush(queue, (len(G.neighbors(n)) * -1, n)) # ... and push it onto the priority queue using its reneighbour degree as priority (max as high priorty)
+    
+    while len(queue): # iterate the priority queue until empty
+        (priority, node) = heappop(queue)
+        yield node
 
 def strategy_mindegree(G):
-		queue = []
-		for n in G.nodes(): # take each node of the graph ...
-			heappush(queue, (len(G.neighbors(n)), n)) # ... and push it onto the priority queue using its neighbour degree as priority (min as high priority)
-		
-		while len(queue): # iterate the priority queue until empty
-			(priority, node) = heappop(queue)
-			yield node
-			
+        queue = []
+        for n in G.nodes(): # take each node of the graph ...
+            heappush(queue, (len(G.neighbors(n)), n)) # ... and push it onto the priority queue using its neighbour degree as priority (min as high priority)
+        
+        while len(queue): # iterate the priority queue until empty
+            (priority, node) = heappop(queue)
+            yield node
+            
 
+"""
+Greedy independent set ordering (GIS). Generates a maximum independent set of nodes, and assigns color C to all nodes in this set. This set of nodes is now
+removed from the graph, and the algorithm runs 
+"""
 def strategy_gis(G, colors):
-	while len(colors) < len(G):
-		available_g = G.copy()
+    len_g = len(G)
+    no_colored = 0
 
-		for node in available_g.nodes():
-			if node in colors:
-				available_g.remove_node(node)
+    while no_colored < len_g: # While there are uncolored nodes 
+        available_g = G.copy()
 
-		while available_g.number_of_nodes() > 0:
-			degree = available_g.degree()
-			
-			# Finding the minimum degree, http://stackoverflow.com/a/12343826/800016
-			v = list(degree.values())
-		 	k = list(degree.keys())
-		 	node = k[v.index(min(v))]
-		 	yield node
+        # Find all uncolored nodes
+        for node in available_g.nodes():
+            if node in colors:
+                available_g.remove_node(node)
 
-		 	available_g.remove_nodes_from(available_g.neighbors(node) + [node])
+        while available_g.number_of_nodes() > 0: # While there are still vertices available
+            degree = available_g.degree()
+            
+            # Pick the one with minimal degree in available
+            # Finding the minimum degree, http://stackoverflow.com/a/12343826/800016
+            v = list(degree.values())
+            k = list(degree.keys())
+            node = k[v.index(min(v))]
+            yield node # assign color to v
 
-		
+            no_colored += 1
+            available_g.remove_nodes_from(available_g.neighbors(node) + [node]) # Remove v and its neighbors from available
+
+"""
+Connected sequential ordering (CS). Yield nodes in such an order, that each node, except the first one, has at least one neighbour in the preceeding sequence.
+The sequence can be generated using both BFS and DFS search
+"""
+def strategy_cs(G, traversal='bfs'):
+    source = G.nodes()[0]
+
+    yield source # Pick the first node as source
+
+    if traversal == 'bfs':
+        tree = nx.bfs_edges(G, source)
+    else:
+        tree = nx.dfs_edges(G, source)
+
+    for (_, end) in tree:
+        yield end # Then yield nodes in the order traversed by either BFS or DFS
+    
+        
 def coloring(G, strategy='maxdegree', interchange=False, returntype='dict'):
-	queue = [] # our priority queue
-	colors = dict() # dictionary to keep track of the colors of the nodes
-	sets = [] # list of sets
+    colors = dict() # dictionary to keep track of the colors of the nodes
+    sets = [] # list of sets
 
-	# the type returned from strategies should probably be python generators
-	if strategy == 'maxdegree':
-		nodes = strategy_maxdegree(G)
-	elif strategy == 'mindegree':
-		nodes = strategy_mindegree(G)
-	elif strategy == 'gis':
-		nodes = strategy_gis(G, colors)
-	else:
-		print 'Strategy ' + strategy + ' does not exist.'
-	
-	for node in nodes:
-		neighbourColors = set() # set to keep track of colors of neighbours
+    # the type returned from strategies should probably be python generators
+    if strategy == 'maxdegree':
+        nodes = strategy_maxdegree(G)
+    elif strategy == 'mindegree':
+        nodes = strategy_mindegree(G)
+    elif strategy == 'gis':
+        nodes = strategy_gis(G, colors)
+    elif strategy == 'cs' or strategy == 'cs-bfs':
+        nodes = strategy_cs(G)
+    elif strategy == 'cs-dfs':
+        nodes = strategy_cs(G, traversal='dfs')
+    else:
+        print 'Strategy ' + strategy + ' does not exist.'
+        return colors
 
-		for neighbour in G.neighbors(node): # iterate through the neighbours of the node
-			if neighbour in colors: # if the neighbour has been assigned a color ...
-				neighbourColors.add(colors[neighbour]) # ... put it into the neighbour color set
+    for node in nodes:
+        neighbourColors = set() # set to keep track of colors of neighbours
 
-		i = 0 # initialize first potentially available color
-		color = -1 # initialize non-existant color (-1)
-		while color == -1: # loop over all possible colors, until a vacant has been found
-			if i in neighbourColors: # check if the color is already occupied by a neighbour
-				if interchange == True:
-					print "Interchange is not implemented"
-					
-				i = i + 1 # ... if that's the case, move to next color and reiterate
-				
-			else:
-				color = i # ... if the color is vacant, choose it as the node's color
+        for neighbour in G.neighbors(node): # iterate through the neighbours of the node
+            if neighbour in colors: # if the neighbour has been assigned a color ...
+                neighbourColors.add(colors[neighbour]) # ... put it into the neighbour color set
 
-		colors[node] = color # assign the node the newly found color
+        i = 0 # initialize first potentially available color
+        color = -1 # initialize non-existant color (-1)
+        while color == -1: # loop over all possible colors, until a vacant has been found
+            if i in neighbourColors: # check if the color is already occupied by a neighbour
+                if interchange == True:
+                    print "Interchange is not implemented"
+                    
+                i = i + 1 # ... if that's the case, move to next color and reiterate
+                
+            else:
+                color = i # ... if the color is vacant, choose it as the node's color
 
-		if returntype == 'sets': # only maintain the list of sets, if the desired return type is 'set'
-			if len(sets) <= i: # ensure that a set has been initialize at the 'color'/index of the list
-				sets.append(set()) # ... if not, do it
-			
-			sets[i].add(node) # add the node to the respective set
+        colors[node] = color # assign the node the newly found color
 
-	if returntype == 'sets': # determine desired return type
-		return sets
-	else:
-		return colors
+        if returntype == 'sets': # only maintain the list of sets, if the desired return type is 'set'
+            if len(sets) <= i: # ensure that a set has been initialize at the 'color'/index of the list
+                sets.append(set()) # ... if not, do it
+            
+            sets[i].add(node) # add the node to the respective set
 
-
-
-gis_shtc = nx.Graph()
-gis_shtc.add_nodes_from([1,2,3,4])
-gis_shtc.add_edges_from([
-	(1,2),
-	(2,3),
-	(3,4)
-])
-
-gis_htc = nx.Graph()
-gis_htc.add_nodes_from([1,2,3,4,5,6])
-gis_htc.add_edges_from([
-	(1,5),
-	(2,5),
-	(3,6),
-	(4,6),
-	(5,6)
-])
-
-print coloring(gis_shtc, strategy='gis')
+    if returntype == 'sets': # determine desired return type
+        return sets
+    else:
+        return colors
 
 
 
 # 
 # def max_degree(G, rtype='dict'):
-# 	queue = [] # our priority queue
-# 	colors = dict() # dictionary to keep track of the colors of the nodes
-# 	sets = [] # list of sets
+#   queue = [] # our priority queue
+#   colors = dict() # dictionary to keep track of the colors of the nodes
+#   sets = [] # list of sets
 # 
-# 	for n in G.nodes(): # take each node of the graph ...
-# 		heappush(queue, (len(G.neighbors(n)) * -1, n)) # ... and push it onto the priority queue using its neighbour degree as priority
+#   for n in G.nodes(): # take each node of the graph ...
+#       heappush(queue, (len(G.neighbors(n)) * -1, n)) # ... and push it onto the priority queue using its neighbour degree as priority
 # 
-# 	while len(queue): # iterate the priority queue until empty
-# 		(priority, node) = heappop(queue) # choose the element from the priority queue with highest degree
+#   while len(queue): # iterate the priority queue until empty
+#       (priority, node) = heappop(queue) # choose the element from the priority queue with highest degree
 # 
-# 		neighbourColors = set() # set to keep track of colors of neighbours
+#       neighbourColors = set() # set to keep track of colors of neighbours
 # 
-# 		for neighbour in G.neighbors(node): # iterate through the neighbours of the node
-# 			if neighbour in colors: # if the neighbour has been assigned a color ...
-# 				neighbourColors.add(colors[neighbour]) # ... put it into the neighbour color set
+#       for neighbour in G.neighbors(node): # iterate through the neighbours of the node
+#           if neighbour in colors: # if the neighbour has been assigned a color ...
+#               neighbourColors.add(colors[neighbour]) # ... put it into the neighbour color set
 # 
-# 		i = 0 # initialize first potentially available color
-# 		color = -1 # initialize non-existant color (-1)
-# 		while color == -1: # loop over all possible colors, until a vacant has been found
-# 			if i in neighbourColors: # check if the color is already occupied by a neighbour
-# 				i = i + 1 # ... if that's the case, move to next color and reiterate
-# 			else:
-# 				color = i # ... if the color is vacant, choose it as the node's color
+#       i = 0 # initialize first potentially available color
+#       color = -1 # initialize non-existant color (-1)
+#       while color == -1: # loop over all possible colors, until a vacant has been found
+#           if i in neighbourColors: # check if the color is already occupied by a neighbour
+#               i = i + 1 # ... if that's the case, move to next color and reiterate
+#           else:
+#               color = i # ... if the color is vacant, choose it as the node's color
 # 
-# 		colors[node] = color # assign the node the newly found color
+#       colors[node] = color # assign the node the newly found color
 # 
-# 		if rtype == 'sets': # only maintain the list of sets, if the desired return type is 'set'
-# 			if len(sets) <= i: # ensure that a set has been initialize at the 'color'/index of the list
-# 				sets.append(set()) # ... if not, do it
-# 			
-# 			sets[i].add(node) # add the node to the respective set
+#       if rtype == 'sets': # only maintain the list of sets, if the desired return type is 'set'
+#           if len(sets) <= i: # ensure that a set has been initialize at the 'color'/index of the list
+#               sets.append(set()) # ... if not, do it
+#           
+#           sets[i].add(node) # add the node to the respective set
 # 
-# 	if rtype == 'sets': # determine desired return type
-# 		return sets
-# 	else:
-# 		return colors
+#   if rtype == 'sets': # determine desired return type
+#       return sets
+#   else:
+#       return colors
 # 
 # def max_degree_sets(G):
-# 	"""Performs a greedy coloring of the graph using the First Fit algorithm
-# 	and maximum degree ordering.
+#   """Performs a greedy coloring of the graph using the First Fit algorithm
+#   and maximum degree ordering.
 # 
-# 	The result returned is a list of sets, containing nodes. Each set 
-# 	corresponds to a color of the contained nodes. In other words,
-# 	all nodes contained in a given set, is of same color.
+#   The result returned is a list of sets, containing nodes. Each set 
+#   corresponds to a color of the contained nodes. In other words,
+#   all nodes contained in a given set, is of same color.
 # 
-# 	Parameters
-# 	----------
-# 	G : NetworkX graph
+#   Parameters
+#   ----------
+#   G : NetworkX graph
 # 
-# 	Examples
-# 	--------
-# 	>>> G=nx.graph()
-# 	>>> G.add_edges_from([(1,2),(1,3)])
-# 	>>> print(nx.max_degree_sets(G))
-# 	[set([1]), set([2,3])]
+#   Examples
+#   --------
+#   >>> G=nx.graph()
+#   >>> G.add_edges_from([(1,2),(1,3)])
+#   >>> print(nx.max_degree_sets(G))
+#   [set([1]), set([2,3])]
 # 
-# 	>>> G=nx.graph()
-# 	>>> G.add_edges_from([(1,2),(1,3),(2,3)])
-# 	>>> print(nx.max_degree_sets(G))
-# 	[set([1]), set([2]), set([3])]
+#   >>> G=nx.graph()
+#   >>> G.add_edges_from([(1,2),(1,3),(2,3)])
+#   >>> print(nx.max_degree_sets(G))
+#   [set([1]), set([2]), set([3])]
 # 
-# 	See Also
-# 	--------
-# 	(the 4 other algorithms, that we have not implemented yet)
+#   See Also
+#   --------
+#   (the 4 other algorithms, that we have not implemented yet)
 # 
-# 	"""
-# 	return max_degree(G, rtype='sets')
+#   """
+#   return max_degree(G, rtype='sets')
 # 
 # def max_degree_dict(G):
-# 	"""Performs a greedy coloring of the graph using the First Fit algorithm
-# 	and maximum degree ordering.
+#   """Performs a greedy coloring of the graph using the First Fit algorithm
+#   and maximum degree ordering.
 # 
-# 	The returned result is a dictionary, where keys corresponds to nodes, and
-# 	values corresponds to colors. A color is represented by an integer from 0
-# 	to the number of unique colors used.
+#   The returned result is a dictionary, where keys corresponds to nodes, and
+#   values corresponds to colors. A color is represented by an integer from 0
+#   to the number of unique colors used.
 # 
-# 	Parameters
-# 	----------
-# 	G : NetworkX graph
+#   Parameters
+#   ----------
+#   G : NetworkX graph
 # 
-# 	Examples
-# 	--------
-# 	>>> G=nx.graph()
-# 	>>> G.add_edges_from([(1,2),(1,3)])
-# 	>>> print(nx.max_degree_dict(G))
-# 	{1: 0, 2: 1, 3: 1}
+#   Examples
+#   --------
+#   >>> G=nx.graph()
+#   >>> G.add_edges_from([(1,2),(1,3)])
+#   >>> print(nx.max_degree_dict(G))
+#   {1: 0, 2: 1, 3: 1}
 # 
-# 	>>> G=nx.graph()
-# 	>>> G.add_edges_from([(1,2),(1,3),(2,3)])
-# 	>>> print(nx.max_degree_dict(G))
-# 	{1: 0, 2: 1, 3: 2}
+#   >>> G=nx.graph()
+#   >>> G.add_edges_from([(1,2),(1,3),(2,3)])
+#   >>> print(nx.max_degree_dict(G))
+#   {1: 0, 2: 1, 3: 2}
 # 
-# 	See Also
-# 	--------
-# 	(the 4 other algorithms, that we have not implemented yet)
+#   See Also
+#   --------
+#   (the 4 other algorithms, that we have not implemented yet)
 # 
-# 	"""
-# 	return max_degree(G, rtype='dict')
+#   """
+#   return max_degree(G, rtype='dict')
---
+++ networkx/algorithms/coloring/tests/test_coloring.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""Greedy coloring test suite.
+
+Run with nose: nosetests -v test_coloring.py
+"""
+
+__author__ = "\n".join(["Christian Olsson <chro@itu.dk>",
+                        "Jan Aagaard Meier <jmei@itu.dk>",
+                        "Henrik Haugbølle <hhau@itu.dk>"])
+
+import networkx as nx
+from nose.tools import *
+
+class TestColoring:
+    def test_gis_shtc(self):
+        gis_shtc = nx.Graph()
+        gis_shtc.add_nodes_from([1, 2, 3, 4])
+        gis_shtc.add_edges_from([
+            (1, 2),
+            (2, 3),
+            (3, 4)
+        ])
+
+        coloring = nx.coloring(gis_shtc, strategy='gis', interchange=False, returntype='sets')
+        assert_true(len(coloring) <= 3)
+
+    def test_gis_htc(self):
+        gis_htc = nx.Graph()
+        gis_htc.add_nodes_from([1, 2, 3, 4, 5, 6])
+        gis_htc.add_edges_from([
+            (1, 5),
+            (2, 5),
+            (3, 6),
+            (4, 6),
+            (5, 6)
+        ])
+
+        coloring = nx.coloring(gis_htc, strategy='gis', interchange=False, returntype='sets')
+        assert_equal(len(coloring), 3)
+
+    def test_cs_shtc(self):
+        cs_shtc = nx.Graph()
+        cs_shtc.add_nodes_from([1, 2, 3, 4, 5])
+        cs_shtc.add_edges_from([
+            (1, 2),
+            (1, 5),
+            (2, 3),
+            (2, 4),
+            (3, 4),
+            (4, 5)
+        ])
+
+        coloring = nx.coloring(cs_shtc, strategy='cs', interchange=False, returntype='sets')
+        assert_true(len(coloring) <= 3)
+
+
+    def test_cs_htc(self):
+        pass
+
+        
