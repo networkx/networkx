f357efb9439658ed14906233e1424efe2bc7a635
Jordi Torrents <jordi.t21@gmail.com>
2014-04-06 17:11:17 +0200

    Try to refactor interface to flow algorithms for connectivity and cuts.
    This is a first try on how to refactor the interface to flow algorithms for playing nice with the flow based connectivity and cut algorithms. The changes are:
    
    1. Each maximum flow implementation is now in its own file in the flow directory. Each implementation provides at least 3 functions: `name_impl_value`, `name_impl_flow` and `name_impl_residual`. They return the value of the maximum flow, the flow dict, and the residual network. For backwards compatibility ther is a function `name_impl` that returns a tuple with the flow value and the flow dict.
    
    2. The functions max_flow and min_cut accept now a parameter for the maximum flow value function to use, with a global default configuration. This is the same approach taken in #1095 for connectivity and cuts functions.
    
    I think that having access to the residual network can be useful in general and not only for internal use, at least for a fraction of NetworkX users. Maybe we should set up the flow package as the bipartite package, in which only a few functions are imported in the global namespace and if the user wants to access to all its functionality they have to explicitly import it.
    
    See #1096 and #1095 for discussion.

---
+++ networkx/algorithms/flow/ford_fulkerson.py
@@ -0,0 +1,464 @@
+# -*- coding: utf-8 -*-
+"""
+Ford-Fulkerson maximum flow shortest augmenting path algorithm on 
+capacitated graphs.
+"""
+
+__author__ = """Loïc Séguin-C. <loicseguin@gmail.com>"""
+# Copyright (C) 2010 Loïc Séguin-C. <loicseguin@gmail.com>
+# All rights reserved.
+# BSD license.
+
+import networkx as nx
+
+__all__ = ['ford_fulkerson',
+           'ford_fulkerson_flow',
+           'ford_fulkerson_value',
+           'ford_fulkerson_residual']
+
+
+def ford_fulkerson_impl(G, s, t, capacity='capacity'):
+    """Find a maximum single-commodity flow using the Ford-Fulkerson
+    algorithm.
+    
+    This function returns both the value of the maximum flow and the 
+    residual network resulting after finding the maximum flow, the 
+    residual network has edges with capacity equal to the capacity 
+    of the edge in the original network minus the flow that went 
+    throught that edge. Notice that it can happen that a flow 
+    from v to u is allowed in the auxiliary network, though disallowed 
+    in the original network. A dictionary with infinite capacity edges 
+    can be found as an attribute of the auxiliary network.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        Edges of the graph are expected to have an attribute called
+        'capacity'. If this attribute is not present, the edge is
+        considered to have infinite capacity.
+
+    s : node
+        Source node for the flow.
+
+    t : node
+        Sink node for the flow.
+
+    capacity: string
+        Edges of the graph G are expected to have an attribute capacity
+        that indicates how much flow the edge can support. If this
+        attribute is not present, the edge is considered to have
+        infinite capacity. Default value: 'capacity'.
+
+    Returns
+    -------
+    flow_value : integer, float
+        Value of the maximum flow, i.e., net outflow from the source.
+
+    residual : DiGraph
+        Residual network after finding the maximum flow. 
+        A dictionary with infinite capacity edges can be found as 
+        an attribute of this network: auxiliary.graph['inf_capacity_flows']
+
+    Raises
+    ------
+    NetworkXError
+        The algorithm does not support MultiGraph and MultiDiGraph. If
+        the input graph is an instance of one of these two classes, a
+        NetworkXError is raised.
+
+    NetworkXUnbounded
+        If the graph has a path of infinite capacity, the value of a 
+        feasible flow on the graph is unbounded above and the function
+        raises a NetworkXUnbounded.
+
+    Notes
+    -----
+    This algorithm uses Edmonds-Karp-Dinitz path selection rule which
+    guarantees a running time of `O(nm^2)` for `n` nodes and `m` edges.
+
+    """
+    if G.is_multigraph():
+        raise nx.NetworkXError(
+                'MultiGraph and MultiDiGraph not supported (yet).')
+
+    if s not in G:
+        raise nx.NetworkXError('node %s not in graph' % str(s))
+    if t not in G:
+        raise nx.NetworkXError('node %s not in graph' % str(t))
+
+    auxiliary = _create_auxiliary_digraph(G, capacity=capacity)
+    inf_capacity_flows = auxiliary.graph['inf_capacity_flows']
+
+    flow_value = 0   # Initial feasible flow.
+
+    # As long as there is an (s, t)-path in the auxiliary digraph, find
+    # the shortest (with respect to the number of arcs) such path and
+    # augment the flow on this path.
+    while True:
+        try:
+            path_nodes = nx.bidirectional_shortest_path(auxiliary, s, t)
+        except nx.NetworkXNoPath:
+            break
+
+        # Get the list of edges in the shortest path.
+        path_edges = list(zip(path_nodes[:-1], path_nodes[1:]))
+
+        # Find the minimum capacity of an edge in the path.
+        try:
+            path_capacity = min([auxiliary[u][v][capacity]
+                                for u, v in path_edges
+                                if capacity in auxiliary[u][v]])
+        except ValueError: 
+            # path of infinite capacity implies no max flow
+            raise nx.NetworkXUnbounded(
+                    "Infinite capacity path, flow unbounded above.")
+        
+        flow_value += path_capacity
+
+        # Augment the flow along the path.
+        for u, v in path_edges:
+            edge_attr = auxiliary[u][v]
+            if capacity in edge_attr:
+                edge_attr[capacity] -= path_capacity
+                if edge_attr[capacity] == 0:
+                    auxiliary.remove_edge(u, v)
+            else:
+                inf_capacity_flows[(u, v)] += path_capacity
+
+            if auxiliary.has_edge(v, u):
+                if capacity in auxiliary[v][u]:
+                    auxiliary[v][u][capacity] += path_capacity
+            else:
+                auxiliary.add_edge(v, u, {capacity: path_capacity})
+    
+    auxiliary.graph['inf_capacity_flows'] = inf_capacity_flows
+    return flow_value, auxiliary
+
+
+def _create_auxiliary_digraph(G, capacity='capacity'):
+    """Initialize an auxiliary digraph and dict of infinite capacity
+    edges for a given graph G.
+    Ignore edges with capacity <= 0.
+    """
+    auxiliary = nx.DiGraph()
+    auxiliary.graph['algorithm'] = 'ford_fulkerson'
+    auxiliary.add_nodes_from(G)
+    inf_capacity_flows = {}
+    if nx.is_directed(G):
+        for edge in G.edges(data = True):
+            if capacity in edge[2]:
+                if edge[2][capacity] > 0:
+                    auxiliary.add_edge(*edge)
+            else:
+                auxiliary.add_edge(*edge)
+                inf_capacity_flows[(edge[0], edge[1])] = 0
+    else:
+        for edge in G.edges(data = True):
+            if capacity in edge[2]:
+                if edge[2][capacity] > 0:
+                    auxiliary.add_edge(*edge)
+                    auxiliary.add_edge(edge[1], edge[0], edge[2])
+            else:
+                auxiliary.add_edge(*edge)
+                auxiliary.add_edge(edge[1], edge[0], edge[2])
+                inf_capacity_flows[(edge[0], edge[1])] = 0
+                inf_capacity_flows[(edge[1], edge[0])] = 0
+
+    auxiliary.graph['inf_capacity_flows'] = inf_capacity_flows
+    return auxiliary
+
+
+def _create_flow_dict(G, H, capacity='capacity'):
+    """Creates the flow dict of dicts on G corresponding to the
+    auxiliary digraph H and infinite capacity edges flows
+    inf_capacity_flows.
+    """
+    inf_capacity_flows = H.graph['inf_capacity_flows']
+    flow = dict([(u, {}) for u in G])
+
+    if G.is_directed():
+        for u, v in G.edges_iter():
+            if H.has_edge(u, v):
+                if capacity in G[u][v]:
+                    flow[u][v] = max(0, G[u][v][capacity] - H[u][v][capacity])
+                elif G.has_edge(v, u) and not capacity in G[v][u]:
+                    flow[u][v] = max(0, inf_capacity_flows[(u, v)] -
+                                     inf_capacity_flows[(v, u)])
+                else:
+                    flow[u][v] = max(0, H[v].get(u, {}).get(capacity, 0) -
+                                     G[v].get(u, {}).get(capacity, 0))
+            else:
+                flow[u][v] = G[u][v][capacity]
+
+    else: # undirected
+        for u, v in G.edges_iter():
+            if H.has_edge(u, v):
+                if capacity in G[u][v]:
+                    flow[u][v] = abs(G[u][v][capacity] - H[u][v][capacity])
+                else:
+                    flow[u][v] = abs(inf_capacity_flows[(u, v)] -
+                                     inf_capacity_flows[(v, u)])
+            else:
+                flow[u][v] = G[u][v][capacity]
+            flow[v][u] = flow[u][v]
+
+    return flow
+
+
+def ford_fulkerson(G, s, t, capacity='capacity'):
+    """Find a maximum single-commodity flow using the Ford-Fulkerson
+    algorithm.
+    
+    This algorithm uses Edmonds-Karp-Dinitz path selection rule which
+    guarantees a running time of `O(nm^2)` for `n` nodes and `m` edges.
+
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        Edges of the graph are expected to have an attribute called
+        'capacity'. If this attribute is not present, the edge is
+        considered to have infinite capacity.
+
+    s : node
+        Source node for the flow.
+
+    t : node
+        Sink node for the flow.
+
+    capacity: string
+        Edges of the graph G are expected to have an attribute capacity
+        that indicates how much flow the edge can support. If this
+        attribute is not present, the edge is considered to have
+        infinite capacity. Default value: 'capacity'.
+
+    Returns
+    -------
+    flow_value : integer, float
+        Value of the maximum flow, i.e., net outflow from the source.
+
+    flow_dict : dictionary
+        Dictionary of dictionaries keyed by nodes such that
+        flow_dict[u][v] is the flow edge (u, v).
+
+    Raises
+    ------
+    NetworkXError
+        The algorithm does not support MultiGraph and MultiDiGraph. If
+        the input graph is an instance of one of these two classes, a
+        NetworkXError is raised.
+
+    NetworkXUnbounded
+        If the graph has a path of infinite capacity, the value of a 
+        feasible flow on the graph is unbounded above and the function
+        raises a NetworkXUnbounded.
+
+    Examples
+    --------
+    >>> import networkx as nx
+    >>> G = nx.DiGraph()
+    >>> G.add_edge('x','a', capacity=3.0)
+    >>> G.add_edge('x','b', capacity=1.0)
+    >>> G.add_edge('a','c', capacity=3.0)
+    >>> G.add_edge('b','c', capacity=5.0)
+    >>> G.add_edge('b','d', capacity=4.0)
+    >>> G.add_edge('d','e', capacity=2.0)
+    >>> G.add_edge('c','y', capacity=2.0)
+    >>> G.add_edge('e','y', capacity=3.0)
+    >>> flow, F = nx.ford_fulkerson(G, 'x', 'y')
+    >>> flow
+    3.0
+    """
+    flow_value, residual = ford_fulkerson_impl(G, s, t, capacity=capacity)
+    flow_dict = _create_flow_dict(G, residual, capacity=capacity)
+    return flow_value, flow_dict
+
+
+def ford_fulkerson_value(G, s, t, capacity='capacity'):
+    """Find the value of a maximum single-commodity flow using the 
+    Ford-Fulkerson algorithm.
+    
+    This algorithm uses Edmonds-Karp-Dinitz path selection rule which
+    guarantees a running time of `O(nm^2)` for `n` nodes and `m` edges.
+
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        Edges of the graph are expected to have an attribute called
+        'capacity'. If this attribute is not present, the edge is
+        considered to have infinite capacity.
+
+    s : node
+        Source node for the flow.
+
+    t : node
+        Sink node for the flow.
+
+    capacity: string
+        Edges of the graph G are expected to have an attribute capacity
+        that indicates how much flow the edge can support. If this
+        attribute is not present, the edge is considered to have
+        infinite capacity. Default value: 'capacity'.
+
+    Returns
+    -------
+    flow_value : integer, float
+        Value of the maximum flow, i.e., net outflow from the source.
+
+    Raises
+    ------
+    NetworkXError
+        The algorithm does not support MultiGraph and MultiDiGraph. If
+        the input graph is an instance of one of these two classes, a
+        NetworkXError is raised.
+
+    NetworkXUnbounded
+        If the graph has a path of infinite capacity, the value of a 
+        feasible flow on the graph is unbounded above and the function
+        raises a NetworkXUnbounded.
+
+    Examples
+    --------
+    >>> import networkx as nx
+    >>> G = nx.DiGraph()
+    >>> G.add_edge('x','a', capacity=3.0)
+    >>> G.add_edge('x','b', capacity=1.0)
+    >>> G.add_edge('a','c', capacity=3.0)
+    >>> G.add_edge('b','c', capacity=5.0)
+    >>> G.add_edge('b','d', capacity=4.0)
+    >>> G.add_edge('d','e', capacity=2.0)
+    >>> G.add_edge('c','y', capacity=2.0)
+    >>> G.add_edge('e','y', capacity=3.0)
+    >>> flow_value = nx.ford_fulkerson_value(G, 'x', 'y')
+    >>> flow_value
+    3.0
+    """
+    flow_value, residual = ford_fulkerson_impl(G, s, t, capacity=capacity)
+    return flow_value
+
+
+def ford_fulkerson_flow(G, s, t, capacity='capacity'):
+    """Return a maximum flow for a single-commodity flow problem.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        Edges of the graph are expected to have an attribute called
+        'capacity'. If this attribute is not present, the edge is
+        considered to have infinite capacity.
+
+    s : node
+        Source node for the flow.
+
+    t : node
+        Sink node for the flow.
+
+    capacity: string
+        Edges of the graph G are expected to have an attribute capacity
+        that indicates how much flow the edge can support. If this
+        attribute is not present, the edge is considered to have
+        infinite capacity. Default value: 'capacity'.
+
+    Returns
+    -------
+    flow_dict : dictionary
+        Dictionary of dictionaries keyed by nodes such that
+        flow_dict[u][v] is the flow edge (u, v).
+
+    Raises
+    ------
+    NetworkXError
+        The algorithm does not support MultiGraph and MultiDiGraph. If
+        the input graph is an instance of one of these two classes, a
+        NetworkXError is raised.
+
+    NetworkXUnbounded
+        If the graph has a path of infinite capacity, the value of a 
+        feasible flow on the graph is unbounded above and the function
+        raises a NetworkXUnbounded.
+
+    Examples
+    --------
+    >>> import networkx as nx
+    >>> G = nx.DiGraph()
+    >>> G.add_edge('x','a', capacity=3.0)
+    >>> G.add_edge('x','b', capacity=1.0)
+    >>> G.add_edge('a','c', capacity=3.0)
+    >>> G.add_edge('b','c', capacity=5.0)
+    >>> G.add_edge('b','d', capacity=4.0)
+    >>> G.add_edge('d','e', capacity=2.0)
+    >>> G.add_edge('c','y', capacity=2.0)
+    >>> G.add_edge('e','y', capacity=3.0)
+    >>> F = nx.ford_fulkerson_flow(G, 'x', 'y')
+    >>> for u, v in sorted(G.edges_iter()):
+    ...     print('(%s, %s) %.2f' % (u, v, F[u][v]))
+    ... 
+    (a, c) 2.00
+    (b, c) 0.00
+    (b, d) 1.00
+    (c, y) 2.00
+    (d, e) 1.00
+    (e, y) 1.00
+    (x, a) 2.00
+    (x, b) 1.00
+
+    """
+    flow_value, residual = ford_fulkerson_impl(G, s, t, capacity=capacity)
+    return _create_flow_dict(G, residual, capacity=capacity)
+
+
+def ford_fulkerson_residual(G, s, t, capacity='capacity'):
+    """Find the value of a maximum single-commodity flow using the 
+    Ford-Fulkerson algorithm.
+    
+    This function returns the residual network resulting after finding 
+    the maximum flow, the residual network has edges with capacity equal
+    to the capacity of the edge in the original network minus the flow 
+    that went throught that edge. Notice that it can happen that a flow 
+    from v to u is allowed in the auxiliary network, though disallowed 
+    in the original network. A dictionary with infinite capacity edges 
+    can be found as an attribute of the auxiliary network.
+
+    This algorithm uses Edmonds-Karp-Dinitz path selection rule which
+    guarantees a running time of `O(nm^2)` for `n` nodes and `m` edges.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        Edges of the graph are expected to have an attribute called
+        'capacity'. If this attribute is not present, the edge is
+        considered to have infinite capacity.
+
+    s : node
+        Source node for the flow.
+
+    t : node
+        Sink node for the flow.
+
+    capacity: string
+        Edges of the graph G are expected to have an attribute capacity
+        that indicates how much flow the edge can support. If this
+        attribute is not present, the edge is considered to have
+        infinite capacity. Default value: 'capacity'.
+
+    Returns
+    -------
+    Residual Network : NetworkX Graph
+        The residual network after computing the maximum flow.
+
+    Raises
+    ------
+    NetworkXError
+        The algorithm does not support MultiGraph and MultiDiGraph. If
+        the input graph is an instance of one of these two classes, a
+        NetworkXError is raised.
+
+    NetworkXUnbounded
+        If the graph has a path of infinite capacity, the value of a 
+        feasible flow on the graph is unbounded above and the function
+        raises a NetworkXUnbounded.
+
+    """
+    flow_value, residual = ford_fulkerson_impl(G, s, t, capacity=capacity)
+    return residual
