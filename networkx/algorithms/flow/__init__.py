ae4519a26e372f8a27a7e460fc08d409c5bafe55
ysitu <ysitu@users.noreply.github.com>
2014-04-09 05:31:26 -0400

    Modify handling of module conflict in shortest augmenting path maxflow

--- networkx/algorithms/flow/__init__.py
+++ networkx/algorithms/flow/__init__.py
@@ -1,6 +1,9 @@
-from networkx.algorithms.flow.maxflow import *
-from networkx.algorithms.flow.mincost import *
-from networkx.algorithms.flow.preflow_push import *
-from networkx.algorithms.flow.shortest_augmenting_path import *
+from . import maxflow, mincost, preflow_push, shortest_augmenting_path
 
-del utils
+__all__ = sum([maxflow.__all__, mincost.__all__, preflow_push.__all__,
+               shortest_augmenting_path.__all__], [])
+
+from .maxflow import *
+from .mincost import *
+from .preflow_push import *
+from .shortest_augmenting_path import *
