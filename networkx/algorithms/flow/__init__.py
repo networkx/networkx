d0d52a68707394f915a683b95f050752311e97a0
ysitu <ysitu@users.noreply.github.com>
2014-04-16 17:32:37 -0400

    Add capacity scaling minimum cost flow algorithm

--- networkx/algorithms/flow/__init__.py
+++ networkx/algorithms/flow/__init__.py
@@ -1,10 +1,12 @@
-from . import maxflow, mincost, network_simplex, preflow_push, shortest_augmenting_path
+from . import maxflow, mincost, capacity_scaling, network_simplex, preflow_push, shortest_augmenting_path
 
-__all__ = sum([maxflow.__all__, mincost.__all__, network_simplex.__all__,
-               preflow_push.__all__, shortest_augmenting_path.__all__], [])
+__all__ = sum([maxflow.__all__, mincost.__all__, capacity_scaling.__all__,
+               network_simplex.__all__, preflow_push.__all__,
+               shortest_augmenting_path.__all__], [])
 
 from .maxflow import *
 from .mincost import *
+from .capacity_scaling import *
 from .network_simplex import *
 from .preflow_push import *
 from .shortest_augmenting_path import *
