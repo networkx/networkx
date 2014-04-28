4b94844fae179ca55a76a90f16df749af631f76f
Kemal Maulana <kemskems12@gmail.com>
2014-04-28 16:21:27 +0900

    Refined documentation

--- networkx/algorithms/link_prediction.py
+++ networkx/algorithms/link_prediction.py
@@ -7,23 +7,61 @@ from networkx.utils.decorators import *
 @not_implemented_for('directed')
 @not_implemented_for('multigraph')
 def common_neighbors(G, u, v):
-    """Count the number of common neighbors of two nodes."""
+    """Count the number of common neighbors of u and v.
+
+    Parameters
+    ----------
+    G : graph
+        A NetworkX undirected graph.
+    u,v : nodes
+        Nodes in the graph.
+
+    Returns
+    -------
+    value : int
+        The number of common neighbors of u and v.
+
+    Examples
+    --------
+    >>> G = nx.complete_graph(5)
+    >>> nx.common_neighbors(G, 0, 1)
+    3
+    """
     return len(_common_neighbors(G, u, v))
 
 
 @not_implemented_for('directed')
 @not_implemented_for('multigraph')
 def resource_allocation_index(G, u, v):
-    """Compute the resource allocation index (RA) of two nodes.
+    """Compute the resource allocation index of u and v.
 
-    RA index of two nodes is defined as the sum of reciprocals of the
-    degree of all their common neighbors.
+    Resource allocation index of nodes u and v is defined as the sum of
+    reciprocals of the degree of all the common neighbors w of u and v.
+
+    Parameters
+    ----------
+    G : graph
+        A NetworkX undirected graph.
+    u,v : nodes
+        Nodes in the graph.
+
+    Returns
+    -------
+    value : float
+        The resource allocation index of u and v.
+
+    Examples
+    --------
+    >>> G = nx.complete_graph(5)
+    >>> nx.resource_allocation_index(G, 0, 1)
+    0.75
 
     References
     ----------
-    Lü, Linyuan and Zhou, Tao. Link prediction in complex networks:
-    A survey. In Physica A, (390) 6: 11501170, Year 2011.
-    http://arxiv.org/pdf/1010.0725v1.pdf
+    .. [1] T. Zhou, L. L¨u, Y.-C. Zhang.
+       Predicting missing links via local information.
+       Eur. Phys. J. B 71 (2009) 623.
+       http://arxiv.org/pdf/0901.0553.pdf
     """
     return sum(1 / G.degree(w) for w in _common_neighbors(G, u, v))
 
@@ -34,16 +72,44 @@ def cn_soundarajan_hopcroft(G, u, v):
     """Count the number of common neighbors using community information.
 
     One is added to the count for each common neighbor that belongs
-    to the same community as the given nodes.
+    to the same community as u and v.
+
+    Parameters
+    ----------
+    G : graph
+        A NetworkX undirected graph.
+    u, v : nodes
+        Nodes in the graph.
+
+    Returns
+    -------
+    value : int
+        The number of common neighbors between u and v plus bonus for
+        each common neighbor belonging to the same community as u and v.
+
+    Notes
+    -----
+    The community information is defined as the information of which
+    community each node belongs to. This information should be stored
+    as the nodes attribute with 'community' as the name.
+
+    Examples
+    --------
+    >>> import networkx as nx
+    >>> G = nx.path_graph(3)
+    >>> G.node[0]['community'] = 0
+    >>> G.node[1]['community'] = 0
+    >>> G.node[2]['community'] = 0
+    >>> nx.cn_soundarajan_hopcroft(G, 0, 2)
+    2
 
     References
     ----------
-    Sucheta Soundarajan and John Hopcroft. 2012. Using community
-    information to improve the precision of link prediction methods.
-    In Proceedings of the 21st international conference companion on
-    World Wide Web (WWW '12 Companion). ACM, New York, NY, USA, 607-608.
-    DOI=10.1145/2187980.2188150
-    http://doi.acm.org/10.1145/2187980.2188150
+    .. [1] Sucheta Soundarajan and John Hopcroft.
+       Using community information to improve the precision of link prediction methods.
+       In Proceedings of the 21st international conference companion on
+       World Wide Web (WWW '12 Companion). ACM, New York, NY, USA, 607-608.
+       http://doi.acm.org/10.1145/2187980.2188150
     """
     Cu = _community(G, u)
     Cv = _community(G, v)
@@ -59,19 +125,48 @@ def cn_soundarajan_hopcroft(G, u, v):
 def ra_index_soundarajan_hopcroft(G, u, v):
     """Compute the RA index of two nodes using community information.
 
-    RA index is defined as the sum of reciprocals of the degree of all
-    all common neighbors of the two nodes. However, this function only
-    considers common neighbors belonging to the same community as the
-    given nodes.
+    Resource allocation index of two nodes is defined as the sum of
+    reciprocals of the degree of all their common neighbors. However,
+    this function only considers common neighbors belonging to the same
+    community as u and v.
+
+    Parameters
+    ----------
+    G : graph
+        A NetworkX undirected graph.
+    u, v : nodes
+        Nodes in the graph.
+
+    Returns
+    -------
+    value : float
+        The resource allocation index of u and v considering only
+        common neighbors that belong to the same community as u and v.
+
+    Notes
+    -----
+    The community information is defined as the information of which
+    community each node belongs to. This information should be stored
+    as the nodes attribute with 'community' as the name.
+
+    Examples
+    --------
+    >>> G = nx.Graph()
+    >>> G.add_edges_from([(0, 1), (0, 2), (1, 3), (2, 3)])
+    >>> G.node[0]['community'] = 0
+    >>> G.node[1]['community'] = 0
+    >>> G.node[2]['community'] = 1
+    >>> G.node[3]['community'] = 0
+    >>> nx.ra_index_soundarajan_hopcroft(G, 0, 3)
+    0.5
 
     References
     ----------
-    Sucheta Soundarajan and John Hopcroft. 2012. Using community
-    information to improve the precision of link prediction methods.
-    In Proceedings of the 21st international conference companion on
-    World Wide Web (WWW '12 Companion). ACM, New York, NY, USA, 607-608.
-    DOI=10.1145/2187980.2188150
-    http://doi.acm.org/10.1145/2187980.2188150
+    .. [1] Sucheta Soundarajan and John Hopcroft.
+       Using community information to improve the precision of link prediction methods.
+       In Proceedings of the 21st international conference companion on
+       World Wide Web (WWW '12 Companion). ACM, New York, NY, USA, 607-608.
+       http://doi.acm.org/10.1145/2187980.2188150
     """
     Cu = _community(G, u)
     Cv = _community(G, v)
@@ -85,24 +180,56 @@ def ra_index_soundarajan_hopcroft(G, u, v):
 @not_implemented_for('directed')
 @not_implemented_for('multigraph')
 def within_inter_cluster(G, u, v, delta=0.001):
-    """Compute the ratio of within and inter cluster common neighbor
+    """Compute the ratio of within and inter cluster common neighbor.
 
-    If a common neighbor belongs to the same community with the two
-    nodes, it is considered as within cluster common neighbor.
-    Otherwise, it is considered as inter cluster common neighbor. The
-    ratio between the two of them is the WIC measure. Based on
-    Valverde-Rebaza, et al (2012).
+    If a common neighbor w belongs to the same community with u and v,
+    w is considered as within cluster common neighbor of u and v.
+    Otherwise, it is considered as inter cluster common neighbor of u
+    and v. The ratio between the size of the set of within and inter
+    cluster common neighbors is defined as the WIC measure. [1]_
+
+    Parameters
+    ----------
+    G : graph
+        A NetworkX undirected graph.
+    u, v : nodes
+        Nodes in the graph.
+    delta : float, optional
+        Value to prevent division by zero in case there is no inter
+        cluster common neighbor of u and v. See [1]_ for details.
+
+    Returns
+    -------
+    value : float
+        The WIC measure of u and v.
+
+    Notes
+    -----
+    The community information is defined as the information of which
+    community each node belongs to. This information should be stored
+    as the nodes attribute with 'community' as the name.
+
+    Examples
+    --------
+    >>> G = nx.Graph()
+    >>> G.add_edges_from([(0, 1), (0, 2), (0, 3), (1, 4), (2, 4), (3, 4)])
+    >>> G.node[0]['community'] = 0
+    >>> G.node[1]['community'] = 1
+    >>> G.node[2]['community'] = 0
+    >>> G.node[3]['community'] = 0
+    >>> G.node[4]['community'] = 0
+    >>> nx.within_inter_cluster(G, 0, 4)
+    1.9980019980019983
+    >>> nx.within_inter_cluster(G, 0, 4, delta=0.5)
+    1.3333333333333333
 
     References
     ----------
-    Jorge Carlos Valverde-Rebaza and Alneu de Andrade Lopes. 2012.
-    Link prediction in complex networks based on cluster information.
-    In Proceedings of the 21st Brazilian conference on Advances in
-    Artificial Intelligence (SBIA'12), Leliane N. Barros, Marcelo
-    Finger, Aurora T. Pozo, Gustavo A. Gimenénez-Lugo, and Marcos
-    Castilho (Eds.). Springer-Verlag, Berlin, Heidelberg, 92-101.
-    DOI=10.1007/978-3-642-34459-6_10
-    http://dx.doi.org/10.1007/978-3-642-34459-6_10
+    .. [1] Jorge Carlos Valverde-Rebaza and Alneu de Andrade Lopes.
+       Link prediction in complex networks based on cluster information.
+       In Proceedings of the 21st Brazilian conference on Advances in
+       Artificial Intelligence (SBIA'12)
+       http://dx.doi.org/10.1007/978-3-642-34459-6_10
     """
     if delta <= 0:
         raise NetworkXAlgorithmError('Delta must be greater than zero')
