ecc3541df1fc79a1331ef288cfb1ac7818c35c9c
chebee7i <chebee7i@gmail.com>
2014-05-13 21:44:37 -0500

    Add smoke test when there is no mapping.

--- networkx/utils/tests/test_misc.py
+++ networkx/utils/tests/test_misc.py
@@ -80,6 +80,9 @@ class TestNumpyArray(object):
         a = dict_to_numpy_array1(d, mapping={'b':0, 'a':1})
         assert_allclose(a, numpy.array([2,1]))
 
+        a = dict_to_numpy_array1(d)
+        assert_allclose(a.sum(), 3)
+
     def test_dict_to_numpy_array2(self):
         d = {'a': {'a':1,'b':2},
              'b': {'a':10,'b':20}}
@@ -88,7 +91,10 @@ class TestNumpyArray(object):
         a = dict_to_numpy_array2(d, mapping=mapping)
         assert_allclose(a, numpy.array([[20,10],[2,1]]))
 
-    def test_dict_to_numpy_array(self):
+        a = dict_to_numpy_array2(d)
+        assert_allclose(a.sum(), 33)
+
+    def test_dict_to_numpy_array_a(self):
         d = {'a': {'a':1,'b':2},
              'b': {'a':10,'b':20}}
 
@@ -100,7 +106,16 @@ class TestNumpyArray(object):
         a = dict_to_numpy_array(d, mapping=mapping)
         assert_allclose(a, numpy.array([[20,10],[2,1]]))
 
+        a = dict_to_numpy_array2(d)
+        assert_allclose(a.sum(), 33)
+
+    def test_dict_to_numpy_array_b(self):
         d = {'a':1,'b':2}
+
         mapping = {'a': 0, 'b': 1}
         a = dict_to_numpy_array(d, mapping=mapping)
         assert_allclose(a, numpy.array([1,2]))
+
+        a = dict_to_numpy_array1(d)
+        assert_allclose(a.sum(), 3)
+
