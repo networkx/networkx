25ca0eabb790be418925d5d54946ab7068c8cc25
Kemal Maulana <kemskems12@gmail.com>
2014-05-15 23:10:28 +0900

    Updated __all__

--- networkx/algorithms/link_prediction.py
+++ networkx/algorithms/link_prediction.py
@@ -11,6 +11,9 @@ from networkx.exception import *
 from networkx.utils.decorators import *
 
 __all__ = ['resource_allocation_index',
+           'jaccard_coefficient',
+           'adamic_adar_index',
+           'preferential_attachment',
            'cn_soundarajan_hopcroft',
            'ra_index_soundarajan_hopcroft',
            'within_inter_cluster']
