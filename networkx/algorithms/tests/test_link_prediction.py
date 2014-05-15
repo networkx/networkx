f069da99a4c862181d74ed623a2a301e2544bbe4
Kemal Maulana <kemskems12@gmail.com>
2014-05-15 22:30:57 +0900

    Added tests for preferential attachment

--- networkx/algorithms/tests/test_link_prediction.py
+++ networkx/algorithms/tests/test_link_prediction.py
@@ -173,6 +173,60 @@ class TestAdamicAdarIndex():
         self.test(G, 0, 0, 3 / math.log(3))
 
 
+class TestPreferentialAttachment():
+    def setUp(self):
+        self.func = lp.preferential_attachment
+        def test_func(G, u, v, expected):
+            result = self.func(G, u, v)
+            assert_equal(result, expected)
+        self.test = test_func
+
+    def test_K5(self):
+        G = nx.complete_graph(5)
+        self.test(G, 0, 1, 16)
+
+    def test_P3(self):
+        G = nx.path_graph(3)
+        self.test(G, 0, 1, 2)
+
+    def test_S4(self):
+        G = nx.star_graph(4)
+        self.test(G, 0, 2, 4)
+
+    @raises(NetworkXNotImplemented)
+    def test_digraph(self):
+        G = nx.DiGraph()
+        G.add_edges_from([(0, 1), (1, 2)])
+        self.func(G, 0, 2)
+
+    @raises(NetworkXNotImplemented)
+    def test_multigraph(self):
+        G = nx.MultiGraph()
+        G.add_edges_from([(0, 1), (1, 2)])
+        self.func(G, 0, 2)
+
+    @raises(NetworkXNotImplemented)
+    def test_multidigraph(self):
+        G = nx.MultiDiGraph()
+        G.add_edges_from([(0, 1), (1, 2)])
+        self.func(G, 0, 2)
+
+    @raises(NetworkXError)
+    def test_nonexistent_node1(self):
+        G = nx.complete_graph(5)
+        self.func(G, 'A', 0)
+
+    @raises(NetworkXError)
+    def test_nonexistent_node2(self):
+        G = nx.complete_graph(5)
+        self.func(G, 0, 'A')
+
+    def test_zero_degrees(self):
+        G = nx.Graph()
+        G.add_nodes_from([0, 1])
+        self.test(G, 0, 1, 0)
+
+
 class TestCNSoundarajanHopcroft():
     def setUp(self):
         self.func = lp.cn_soundarajan_hopcroft
