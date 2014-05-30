8e9ad0a9bf32b926ef181c1e5b2808f26ad036d9
ysitu <ysitu@users.noreply.github.com>
2014-05-30 14:45:45 -0400

    Add classifier for Python 3.4 support

--- networkx/release.py
+++ networkx/release.py
@@ -240,6 +240,7 @@ classifiers = [
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Information Analysis',
