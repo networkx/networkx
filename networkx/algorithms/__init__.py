70e1faa844ca09ab33f191a5a99660ffa5cbdd84
ysitu <ysitu@users.noreply.github.com>
2014-08-09 22:20:12 -0400

    Add algorithm for computing immediate dominators

--- networkx/algorithms/__init__.py
+++ networkx/algorithms/__init__.py
@@ -11,6 +11,7 @@ from networkx.algorithms.core import *
 from networkx.algorithms.cycles import *
 from networkx.algorithms.dag import *
 from networkx.algorithms.distance_measures import *
+from networkx.algorithms.dominance import *
 from networkx.algorithms.dominating import *
 from networkx.algorithms.hierarchy import *
 from networkx.algorithms.matching import *
