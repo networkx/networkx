f90f52ded658ca4efbd1e8baea63e64d6d68f3cf
ysitu <ysitu@users.noreply.github.com>
2014-08-10 00:06:15 -0400

    Add algorithm for computing dominance frontiers

--- networkx/algorithms/dominance.py
+++ networkx/algorithms/dominance.py
@@ -11,7 +11,7 @@ from functools import reduce
 import networkx as nx
 from networkx.utils import not_implemented_for
 
-__all__ = ['immediate_dominators']
+__all__ = ['immediate_dominators', 'dominance_frontiers']
 
 
 @not_implemented_for('undirected')
@@ -85,3 +85,58 @@ def immediate_dominators(G, start):
                 changed = True
 
     return idom
+
+
+def dominance_frontiers(G, start):
+    """Returns the dominance frontiers of all nodes of a directed graph.
+
+    Parameters
+    ----------
+    G : a DiGraph or MultiDiGraph
+        The graph where dominance is to be computed.
+
+    start : node
+        The start node of dominance computation.
+
+    Returns
+    -------
+    df : dict keyed by nodes
+        A dict containing the dominance frontiers of each node reachable from
+        ``start`` as lists.
+
+    Raises
+    ------
+    NetworkXNotImplemented
+        If ``G`` is undirected.
+
+    NetworkXError
+        If ``start`` is not in ``G``.
+
+    Examples
+    --------
+    >>> G = nx.DiGraph([(1, 2), (1, 3), (2, 5), (3, 4), (4, 5)])
+    >>> sorted((u, sorted(df)) for u, df in nx.dominance_frontiers(G, 1).items())
+    [(1, []), (2, [5]), (3, [5]), (4, [5]), (5, [])]
+
+    References
+    ----------
+    ..[1] K. D. Cooper, T. J. Harvey, and K. Kennedy.
+          A simple, fast dominance algorithm.
+          Software Practice & Experience, 4:110, 2001.
+    """
+    idom = nx.immediate_dominators(G, start)
+
+    df = {u: [] for u in idom}
+
+    for u in idom:
+        if len(G.pred[u]) - int(u in G.pred[u]) >= 2:
+            p = set()
+            for v in G.pred[u]:
+                while v != idom[u] and v not in p:
+                    p.add(v)
+                    v = idom[v]
+            p.discard(u)
+            for v in p:
+                df[v].append(u)
+
+    return df
