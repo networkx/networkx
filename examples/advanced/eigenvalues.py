939a1059911bb71bfad3695903ab4ded8d2029ae
Aric Hagberg <aric.hagberg@gmail.com>
2014-09-22 17:37:45 -0600

    fix eigenvalue example

--- examples/advanced/eigenvalues.py
+++ examples/advanced/eigenvalues.py
@@ -1,48 +1,20 @@
 #!/usr/bin/env python
 """
 Create an G{n,m} random graph and compute the eigenvalues.
-
-Requires numpy or LinearAlgebra package from Numeric Python.
-
-Uses optional pylab plotting to produce histogram of eigenvalues.
-
+Requires numpy and matplotlib.
 """
-__author__ = """Aric Hagberg (hagberg@lanl.gov)"""
-__credits__ = """"""
-#    Copyright (C) 2004-2006 by 
-#    Aric Hagberg <hagberg@lanl.gov>
-#    Dan Schult <dschult@colgate.edu>
-#    Pieter Swart <swart@lanl.gov>
-#    All rights reserved.
-#    BSD license.
+import networkx as nx
+import numpy.linalg
+import matplotlib.pyplot as plt
 
-from networkx import *
-try:
-    import numpy.linalg
-    eigenvalues=numpy.linalg.eigvals
-except ImportError:
-    raise ImportError("numpy can not be imported.")
+n = 1000 # 1000 nodes
+m = 5000 # 5000 edges
+G = nx.gnm_random_graph(n,m)
 
-try:
-    from pylab import *
-except:
-    pass
-
-n=1000 # 1000 nodes
-m=5000 # 5000 edges
-
-G=gnm_random_graph(n,m)
-
-L=generalized_laplacian(G)
-e=eigenvalues(L)
+L = nx.normalized_laplacian_matrix(G)
+e = numpy.linalg.eigvals(L.A)
 print("Largest eigenvalue:", max(e))
 print("Smallest eigenvalue:", min(e))
-# plot with matplotlib if we have it
-# shows "semicircle" distribution of eigenvalues 
-try:
-    hist(e,bins=100) # histogram with 100 bins
-    xlim(0,2)  # eigenvalues between 0 and 2
-    show()
-except:
-    pass
-
+plt.hist(e,bins=100) # histogram with 100 bins
+plt.xlim(0,2)  # eigenvalues between 0 and 2
+plt.show()
