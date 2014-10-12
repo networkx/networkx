dba3ec33f36a47d4079f90d508a84934fac7d7ae
ysitu <ysitu@users.noreply.github.com>
2014-10-12 03:21:49 -0400

    Adjust GML test cases

--- networkx/readwrite/tests/test_gml.py
+++ networkx/readwrite/tests/test_gml.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from __future__ import unicode_literals
 
+from ast import literal_eval
 import io
 from nose.tools import *
 from nose import SkipTest
-import networkx
+import networkx as nx
+from networkx.readwrite.gml import literal_stringizer, literal_destringizer
+import os
+import tempfile
+
+try:
+    unichr
+except NameError:
+    unichr = chr
+
 
 class TestGraph(object):
-    @classmethod
-    def setupClass(cls):
-        global pyparsing
-        try:
-            import pyparsing
-        except ImportError:
-            try:
-                import matplotlib.pyparsing as pyparsing
-            except:
-                raise SkipTest('gml test: pyparsing not available.')
 
     def setUp(self):
-        self.simple_data="""Creator me
+        self.simple_data = """Creator me
+Version xx
 graph [
  comment "This is a sample graph"
  directed 1
@@ -60,40 +60,39 @@ graph [
   ]
 ]
 """
+
     def test_parse_gml(self):
-        G=networkx.parse_gml(self.simple_data,relabel=True)
-        assert_equals(sorted(G.nodes()),\
-                          ['Node 1', 'Node 2', 'Node 3'])
-        assert_equals( [e for e in sorted(G.edges())],\
-                           [('Node 1', 'Node 2'),
-                            ('Node 2', 'Node 3'),
-                            ('Node 3', 'Node 1')])
+        G = nx.parse_gml(self.simple_data, label='label')
+        assert_equals(sorted(G.nodes()),
+                      ['Node 1', 'Node 2', 'Node 3'])
+        assert_equals([e for e in sorted(G.edges())],
+                      [('Node 1', 'Node 2'),
+                       ('Node 2', 'Node 3'),
+                       ('Node 3', 'Node 1')])
 
-        assert_equals( [e for e in sorted(G.edges(data=True))],\
-                           [('Node 1', 'Node 2',
-                             {'color': {'line': 'blue', 'thickness': 3},
-                              'label': 'Edge from node 1 to node 2'}),
-                            ('Node 2', 'Node 3',
-                             {'label': 'Edge from node 2 to node 3'}),
-                            ('Node 3', 'Node 1',
-                             {'label': 'Edge from node 3 to node 1'})])
-
+        assert_equals([e for e in sorted(G.edges(data=True))],
+                      [('Node 1', 'Node 2',
+                        {'color': {'line': 'blue', 'thickness': 3},
+                         'label': 'Edge from node 1 to node 2'}),
+                       ('Node 2', 'Node 3',
+                        {'label': 'Edge from node 2 to node 3'}),
+                       ('Node 3', 'Node 1',
+                        {'label': 'Edge from node 3 to node 1'})])
 
     def test_read_gml(self):
-        import os,tempfile
-        (fd,fname)=tempfile.mkstemp()
-        fh=open(fname,'w')
+        (fd, fname) = tempfile.mkstemp()
+        fh = open(fname, 'w')
         fh.write(self.simple_data)
         fh.close()
-        Gin=networkx.read_gml(fname,relabel=True)
-        G=networkx.parse_gml(self.simple_data,relabel=True)
-        assert_equals( sorted(G.nodes(data=True)), sorted(Gin.nodes(data=True)))
-        assert_equals( sorted(G.edges(data=True)), sorted(Gin.edges(data=True)))
+        Gin = nx.read_gml(fname, label='label')
+        G = nx.parse_gml(self.simple_data, label='label')
+        assert_equals(sorted(G.nodes(data=True)), sorted(Gin.nodes(data=True)))
+        assert_equals(sorted(G.edges(data=True)), sorted(Gin.edges(data=True)))
         os.close(fd)
         os.unlink(fname)
 
     def test_relabel_duplicate(self):
-        data="""
+        data = """
 graph
 [
 	label	""
@@ -112,46 +111,23 @@ graph
 """
         fh = io.BytesIO(data.encode('UTF-8'))
         fh.seek(0)
-        assert_raises(networkx.NetworkXError,networkx.read_gml,fh,relabel=True)
-
-    def test_bool(self):
-        G=networkx.Graph()
-        G.add_node(1,on=True)
-        G.add_edge(1,2,on=False)
-        data = '\n'.join(list(networkx.generate_gml(G)))
-        answer ="""graph [
-  node [
-    id 0
-    label "1"
-    on 1
-  ]
-  node [
-    id 1
-    label "2"
-  ]
-  edge [
-    source 0
-    target 1
-    on 0
-  ]
-]"""
-        assert_equal(data,answer)
-
+        assert_raises(
+            nx.NetworkXError, nx.read_gml, fh, label='label')
 
     def test_tuplelabels(self):
         # https://github.com/networkx/networkx/pull/1048
         # Writing tuple labels to GML failed.
-        G = networkx.Graph()
-        G.add_edge((0,1), (1,0))
-        data = '\n'.join(list(networkx.generate_gml(G)))
+        G = nx.Graph()
+        G.add_edge((0, 1), (1, 0))
+        data = '\n'.join(nx.generate_gml(G, stringizer=literal_stringizer))
         answer = """graph [
   node [
     id 0
-    label "(0, 1)"
+    label "(0,1)"
   ]
   node [
     id 1
-    label "(1, 0)"
+    label "(1,0)"
   ]
   edge [
     source 0
@@ -160,27 +136,69 @@ graph
 ]"""
         assert_equal(data, answer)
 
-
     def test_quotes(self):
         # https://github.com/networkx/networkx/issues/1061
         # Encoding quotes as HTML entities.
-        import tempfile
-        G = networkx.path_graph(1)
+        G = nx.path_graph(1)
         # This is a unicode string (due to the __future__ import)
         # It was decoded from utf-8 since that the encoding of this file.
-        attr = 'This is "quoted" and this is a copyright: ©'  # u'\xa9'
+        attr = 'This is "quoted" and this is a copyright: ' + unichr(169)
         G.node[0]['demo'] = attr
         fobj = tempfile.NamedTemporaryFile()
-        networkx.write_gml(G, fobj)
+        nx.write_gml(G, fobj)
         fobj.seek(0)
         # Should be bytes in 2.x and 3.x
-        data = fobj.read().strip()
-        answer = b"""graph [
+        data = fobj.read().strip().decode('ascii')
+        answer = """graph [
   name "path_graph(1)"
   node [
     id 0
-    label "0"
-    demo "This is &quot;quoted&quot; and this is a copyright: &#169;"
+    label 0
+    demo "This is &#34;quoted&#34; and this is a copyright: &#169;"
   ]
 ]"""
         assert_equal(data, answer)
+
+    def test_graph_types(self):
+        for directed in [None, False, True]:
+            for multigraph in [None, False, True]:
+                gml = 'graph ['
+                if directed is not None:
+                    gml += ' directed ' + str(int(directed))
+                if multigraph is not None:
+                    gml += ' multigraph ' + str(int(multigraph))
+                gml += ' ]'
+                G = nx.parse_gml(gml)
+                assert_equal(bool(directed), G.is_directed())
+                assert_equal(bool(multigraph), G.is_multigraph())
+                gml = 'graph [\n'
+                if directed is True:
+                    gml += '  directed 1\n'
+                if multigraph is True:
+                    gml += '  multigraph 1\n'
+                gml += ']'
+                assert_equal(gml, '\n'.join(nx.generate_gml(G)))
+
+    def test_data_types(self):
+        data = [10 ** 20, -2e33, "'", '"&&amp;&&#34;"',
+                {(b'\xfd',): '\x7f', unichr(0x4444): (1, 2)}]
+        try:
+            data.append(unichr(0x14444))  # fails under IronPython
+        except ValueError:
+            data.append(unichr(0x1444))
+        try:
+            data.append(literal_eval('{2.3j, 1 - 2.3j, ()}'))  # fails under Python 2.7
+        except ValueError:
+            data.append([2.3j, 1 - 2.3j, ()])
+        G = nx.Graph()
+        G.name = data
+        G.graph['data'] = data
+        G.add_node(0, int=-1, data=dict(data=data))
+        G.add_edge(0, 0, float=-2.5, data=data)
+        gml = '\n'.join(nx.generate_gml(G, stringizer=literal_stringizer))
+        G = nx.parse_gml(gml, destringizer=literal_destringizer)
+        assert_equal(data, G.name)
+        assert_equal(dict(name=data, data=data), G.graph)
+        assert_equal(G.nodes(data=True),
+                     [(0, dict(int=-1, data=dict(data=data)))])
+        assert_equal(G.edges(data=True), [(0, 0, dict(float=-2.5, data=data))])
