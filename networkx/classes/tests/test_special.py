18afd60e46d6ea87fd540c847594db3cea363086
Dan Schult <dschult@colgate.edu>
2014-10-12 15:16:32 -0400

    tweaks to make ThinGraph work with G[u][v]['weight']

--- networkx/classes/tests/test_special.py
+++ networkx/classes/tests/test_special.py
@@ -22,13 +22,14 @@ class OrderedGraphTester(TestGraph):
 
 class ThinGraphTester(TestGraph):
     def setUp(self):
+        all_edge_dict = {'weight' : 1}
         def graph_factory(data,**attr):
             g=networkx.SpecialGraph(data,
-                    edge_attr_dict_factory=lambda :None,**attr)
+                    edge_attr_dict_factory=lambda :all_edge_dict,**attr)
             return g
         self.Graph=graph_factory
         # build dict-of-dict-of-dict K3
-        ed1,ed2,ed3 = (None,None,None)
+        ed1,ed2,ed3 = (all_edge_dict,all_edge_dict,all_edge_dict)
         self.k3adj={0: {1: ed1, 2: ed2},
                     1: {0: ed1, 2: ed3},
                     2: {0: ed2, 1: ed3}}
@@ -61,13 +62,14 @@ class OrderedDiGraphTester(TestDiGraph):
 
 class ThinDiGraphTester(TestDiGraph):
     def setUp(self):
+        all_edge_dict = {'weight' : 1}
         def graph_factory(data,**attr):
             g=networkx.SpecialDiGraph(data,
-                    edge_attr_dict_factory=lambda :None,**attr)
+                    edge_attr_dict_factory=lambda :all_edge_dict,**attr)
             return g
         self.Graph=graph_factory
         # build dict-of-dict-of-dict K3
-        ed1,ed2,ed3 = (None,None,None)
+        ed1,ed2,ed3 = (all_edge_dict,all_edge_dict,all_edge_dict)
         self.k3adj={0: {1: ed1, 2: ed2},
                     1: {0: ed1, 2: ed3},
                     2: {0: ed2, 1: ed3}}
