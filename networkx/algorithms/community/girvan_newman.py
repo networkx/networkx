ef8ded8d95c9f628ee6a73037e97b61135db587a
Aitor Almeida <aitoralmeida@gmail.com>
2014-10-14 12:04:48 +0200

    Some changes according to the comments of the push request:  used the not_implemented for decorator instead of rising an exception, used len(G) instead of len(G.nodes()), changed the return type to a list, used min instead of sort and some other minor changes

--- networkx/algorithms/community/girvan_newman.py
+++ networkx/algorithms/community/girvan_newman.py
@@ -2,9 +2,12 @@
 """
 Created on Thu Oct 09 16:31:16 2014
 
-Bases on the algorithm described in: 
-  - Newman, Mark EJ, and Michelle Girvan. "Finding and evaluating community structure 
-    in networks." Physical review E 69.2 (2004): 026113.
+Bases on the algorithm described in:
+  - Girvan, Michelle, and Mark EJ Newman. "Community structure in social and 
+    biological networks." Proceedings of the National Academy of Sciences 99.12 
+    (2002): 7821-7826.
+  - Newman, Mark EJ, and Michelle Girvan. "Finding and evaluating community 
+    structure in networks." Physical review E 69.2 (2004): 026113.
   - http://en.wikipedia.org/wiki/Girvan%E2%80%93Newman_algorithm
 
 The community structure finding algorithm follows this steps:
@@ -15,30 +18,29 @@ The community structure finding algorithm follows this steps:
     
 The result is a dendogram where the leaves are the individual nodes.
 
-The algorithm works on undirected networks with only a single node type and does
-not take it account edge weights.
+The algorithm works on undirected networks with only a single node type and 
+does not take it account edge weights.
 
 @author: Aitor Almeida (aitoralmeida@gmail.com)
 All rights reserved.
 BSD license.
 """
 import networkx as nx
-import operator
+from networkx.utils.decorators import not_implemented_for
 
+__author__ = """Aitor Almeida (aitoralmeida@gmail.com)"""
 __all__ = ["girvan_newman_communities"]
 
+@not_implemented_for('directed')
 def girvan_newman_communities(original_graph, preserve_original = True):    
-    # The algorithm is divisive and the edges of the graph are going to be deleted.
-    # preserve_original allows to make a copy of the original graph so it is not
-    # destroyed, but this way the algorithm uses more memory.    
+    # The algorithm is divisive and the edges of the graph are going to be 
+    # deleted. preserve_original allows to make a copy of the original graph so 
+    # it is not destroyed, but this way the algorithm uses more memory.    
     
-    # Only works on undirected networks
-    if original_graph.is_directed():
-        raise nx.NetworkXNotImplemented('not implemented for directed type')
-   
-    if len(original_graph.nodes()) < 2:
-        raise nx.NetworkXError('there must be more than 1 nodes to find communities')
-          
+    if len(original_graph) < 2:
+        raise nx.NetworkXError('''there must be more than one node to find 
+                                communities''')
+         
     if preserve_original:
         G = original_graph.copy()
     else:
@@ -46,38 +48,38 @@ def girvan_newman_communities(original_graph, preserve_original = True):
     return _girvan_newman_communities_rec(G)    
     
 def _girvan_newman_communities_rec(G):    
-    # Recursively find the communities in the graph. The final result is a dendogram
-    # where the leaves are the individual nodes.
+    # Recursively find the communities in the graph. The final result is a 
+    # dendogram where the leaves are the individual nodes.
     
     # 1.- Calculate the betweenness for all the edges
     connected_components = list(nx.connected_component_subgraphs(G))
-       
-    while len(connected_components) < 2: # Repeat until new communities are found
-        # 2.- Find the edge with the highest score and remove it from the network
+    
+    # Repeat until new communities are found
+    while len(connected_components) < 2: 
+        # 2.- Find the edge with the highest score and remove it 
+        #     from the network
         _remove_highest_betweenness_edge(G)
         # 3.- Recalculate the betweenness
         connected_components = list(nx.connected_component_subgraphs(G))
         
     for component in connected_components:
-        yield set(component.nodes())
+        yield component.nodes()
         # 4.- Repeat from 2 until there are no more edges
-        if not len(component.edges()) == 0:            
+        if component.size() != 0:            
             for x in _girvan_newman_communities_rec(component):
                 yield x
 
 def _remove_highest_betweenness_edge(G):
     # Finds the edge with the highest node betweenness centrality and removes
-    # it from the graph. If a several nodes have the same betweenness edge value
-    # one of them is selected randomly (not actually random, depends on how the
-    # sorted built-in shorts the items).
+    # it from the graph. If a several nodes have the same betweenness edge 
+    # value one of them is selected randomly (not actually random, depends on 
+    # how the max built-in shorts the items).
     
     # Find highest betweenness edge   
     edge_betweenness = nx.edge_betweenness_centrality(G)
-    shorted_edges = sorted(edge_betweenness.items(), key=operator.itemgetter(1), reverse = True)
-    highest_betweenness_edge = shorted_edges[0][0]            
+    highest_betweenness_edge = max(edge_betweenness, key=edge_betweenness.get)
     
     # Remove the edge
     G.remove_edge(*highest_betweenness_edge)    
-        
-    
+
             
