aa7930df044aa405565c6333ed1f6f4e7bee4e75
alexnikleo <alexnikleo@gmail.com>
2015-03-03 16:18:21 +0300

    changed doc string for compose function

--- networkx/algorithms/operators/binary.py
+++ networkx/algorithms/operators/binary.py
@@ -293,7 +293,6 @@ def compose(G, H, name=None):
     """Return a new graph of G composed with H.
 
     Composition is the simple union of the node sets and edge sets.
-    The node sets of G and H need not be disjoint.
 
     Parameters
     ----------
