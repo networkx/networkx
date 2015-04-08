90d958897e3b29a33ebf352e55498e72a2959ace
Jordi Torrents <jordi.t21@gmail.com>
2015-04-08 04:01:44 +0200

    Add shortest_simple_paths function.
    This function is based on Yen's algorithm for finding k shortest paths.
    It generates all simple paths between source and target starting from
    the shortest one. See #762 and #793 for prior work on this and discussion.
    
    This implementation is based on Andrey's work on #762. It generalizes his
    approach there to the weighted case, as in Greg's implementation posted
    on #793.
    
    Regarding the discussion on #762 about how to do the filtering of nodes
    and edges efficiently, this PR adds private modifications of the functions
    `bidirectional_shortest_path` and `bidirectional_dijkstra` (that accept
    containers with nodes and edges to exclude during the SP search) for its
    exclusive use.
    
    This still needs work, especially on documentation and tests. I'm pushing
    it so we can have the discussion with the code available.
    
    One thing to discuss is whether or not to include an one liner function
    to get the k shortest paths. Could be something like this:
    
    ```python
    from itertools import islice
    def k_shortest_paths(G, source, target, k, weight=None):
            return list(islice(nx.shortest_simple_paths(G, source, target, weight=weight), k))
    ```
    
    I think that, given that `all_simple_paths` is way faster in computing
    all paths, the principal use of `shortest_simple_paths` will be to get
    the k best/shortest paths. So it will be a more user friendly interface
    if we add it. However, we could also add this function as example in the
    `shortest_simple_paths` docstring. I'm ok either way. Thoughts?

--- networkx/algorithms/tests/test_simple_paths.py
+++ networkx/algorithms/tests/test_simple_paths.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
+import random
 from nose.tools import *
-import networkx as nx
 
+import networkx as nx
+from networkx import convert_node_labels_to_integers as cnlti
+from networkx.algorithms.simple_paths import _bidirectional_shortest_path
+
+# Tests for all_simple_paths
 def test_all_simple_paths():
     G = nx.path_graph(4)
     paths = nx.all_simple_paths(G,0,3)
@@ -71,3 +76,80 @@ def test_target_missing():
     G = nx.Graph()
     G.add_path([1,2,3])
     paths = list(nx.all_simple_paths(nx.MultiGraph(G),1,4))
+
+# Tests for shortest_simple_paths
+def test_shortest_simple_paths():
+    G = cnlti(nx.grid_2d_graph(4, 4), first_label=1, ordering="sorted")
+    paths = nx.shortest_simple_paths(G, 1, 12)
+    assert_equal(next(paths), [1, 2, 3, 4, 8, 12])
+    assert_equal(next(paths), [1, 5, 6, 7, 8, 12])
+    assert_equal([len(path) for path in nx.shortest_simple_paths(G, 1, 12)],
+                 sorted([len(path) for path in nx.all_simple_paths(G, 1, 12)]))
+
+def test_shortest_simple_paths_directed():
+    G = nx.cycle_graph(7, create_using=nx.DiGraph())
+    paths = nx.shortest_simple_paths(G, 0, 3)
+    assert_equal([path for path in paths], [[0, 1, 2, 3]])
+
+def test_Greg_Bernstein():
+    g1 = nx.Graph()
+    g1.add_nodes_from(["N0", "N1", "N2", "N3", "N4"])
+    g1.add_edge("N4", "N1", weight=10.0, capacity=50, name="L5")
+    g1.add_edge("N4", "N0", weight=7.0, capacity=40, name="L4")
+    g1.add_edge("N0", "N1", weight=10.0, capacity=45, name="L1")
+    g1.add_edge("N3", "N0", weight=10.0, capacity=50, name="L0")
+    g1.add_edge("N2", "N3", weight=12.0, capacity=30, name="L2")
+    g1.add_edge("N1", "N2", weight=15.0, capacity=42, name="L3")
+    solution = [['N1', 'N0', 'N3'], ['N1', 'N2', 'N3'], ['N1', 'N4', 'N0', 'N3']]
+    result = list(nx.shortest_simple_paths(g1, 'N1', 'N3', weight='weight'))
+    assert_equal(result, solution)
+
+def test_weighted_shortest_simple_path():
+    def cost_func(path):
+        return sum(G.edge[u][v]['weight'] for (u, v) in zip(path, path[1:]))
+    G = nx.complete_graph(5)
+    weight = {(u, v): random.random() for (u, v) in G.edges()}
+    nx.set_edge_attributes(G, 'weight', weight)
+    cost = 0
+    for path in nx.shortest_simple_paths(G, 0, 3, weight='weight'):
+        this_cost = cost_func(path)
+        assert_true(cost <= this_cost)
+        cost = this_cost
+
+def test_bidirectional_shortest_path_restricted():
+    grid = cnlti(nx.grid_2d_graph(4,4), first_label=1, ordering="sorted")
+    cycle = nx.cycle_graph(7)
+    directed_cycle = nx.cycle_graph(7, create_using=nx.DiGraph())
+    length, path = _bidirectional_shortest_path(cycle, 0, 3)
+    assert_equal(path, [0, 1, 2, 3])
+    length, path = _bidirectional_shortest_path(cycle, 0, 3, ignore_nodes = [1])
+    assert_equal(path, [0, 6, 5, 4, 3])
+    length, path = _bidirectional_shortest_path(grid, 1, 12)
+    assert_equal(path, [1, 2, 3, 4, 8, 12])
+    length, path = _bidirectional_shortest_path(grid, 1, 12, ignore_nodes = [2])
+    assert_equal(path, [1, 5, 6, 10, 11, 12])
+    length, path = _bidirectional_shortest_path(grid, 1, 12, ignore_nodes = [2, 6])
+    assert_equal(path, [1, 5, 9, 10, 11, 12])
+    length, path = _bidirectional_shortest_path(grid, 1, 12,
+                                        ignore_nodes = [2, 6],
+                                        ignore_edges = [(10, 11)])
+    assert_equal(path, [1, 5, 9, 10, 14, 15, 16, 12])
+    length, path = _bidirectional_shortest_path(directed_cycle, 0, 3)
+    assert_equal(path, [0, 1, 2, 3])
+    assert_raises(
+        nx.NetworkXNoPath,
+        _bidirectional_shortest_path,
+        directed_cycle,
+        0, 3,
+        ignore_nodes=[1],
+    )
+    length, path = _bidirectional_shortest_path(directed_cycle, 0, 3,
+                                        ignore_edges = [(2, 1)])
+    assert_equal(path, [0, 1, 2, 3])
+    assert_raises(
+        nx.NetworkXNoPath,
+        _bidirectional_shortest_path,
+        directed_cycle,
+        0, 3, 
+        ignore_edges = [(1, 2)],
+    )
