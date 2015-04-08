1a2c08d99ae65d997b629826e98f9f85872ee57f
JGab <jean.gabriel.young@gmail.com>
2015-04-08 11:03:56 -0400

    Modularity matrix implemented

---
+++ networkx/linalg/modularitymatrix.py
@@ -0,0 +1,77 @@
+"""Modularity matrix of graphs.
+"""
+#    Copyright (C) 2004-2015 by
+#    Aric Hagberg <hagberg@lanl.gov>
+#    Dan Schult <dschult@colgate.edu>
+#    Pieter Swart <swart@lanl.gov>
+#    All rights reserved.
+#    BSD license.
+import networkx as nx
+from networkx.utils import not_implemented_for
+__author__ = "\n".join(['Aric Hagberg <aric.hagberg@gmail.com>',
+                        'Pieter Swart (swart@lanl.gov)',
+                        'Dan Schult (dschult@colgate.edu)',
+                        'Jean-Gabriel Young <jean.gabriel.young@gmail.com>'])
+__all__ = ['modularity_matrix']
+
+
+@not_implemented_for('directed')
+def modularity_matrix(G, nodelist=None, weight='weight'):
+    """Return the modularity matrix of G.
+
+    The modularity matrix is the matrix B = A - <A>, where A is the adjacency
+    matrix and <A> is the average adjacency matrix, assuming that the graph
+    is descibred by the configuration model.
+
+    Parameters
+    ----------
+    G : graph
+       A NetworkX graph
+
+    nodelist : list, optional
+       The rows and columns are ordered according to the nodes in nodelist.
+       If nodelist is None, then the ordering is produced by G.nodes().
+
+    weight : string or None, optional (default='weight')
+       The edge data key used to compute each value in the matrix.
+       If None, then each edge has weight 1.
+
+    Returns
+    -------
+    B : SciPy sparse matrix
+      The modularity matrix of G.
+
+    Notes
+    -----
+    For MultiGraph/MultiDiGraph, the edges weights are summed.
+
+    See Also
+    --------
+    to_numpy_matrix
+    adjacency_matrix
+    laplacian_matrix
+
+    References
+    ----------
+    .. [1] M. E. J. Newman, "Modularity and community structure in networks",
+       Proc. Natl. Acad. Sci. USA, vol. 103, pp. 8577–8582, 2006.
+    """
+    if nodelist is None:
+        nodelist = G.nodes()
+    A = nx.to_scipy_sparse_matrix(G, nodelist=nodelist, weight=weight,
+                                  format='csr')
+    r, s = A.shape
+    k = A.sum(axis=1)
+    m = G.number_of_edges()
+    # Expected adjacency matrix
+    X = k[nodelist] * k[nodelist].transpose() / (2 * m)
+    return A - X
+
+
+# fixture for nose tests
+def setup_module(module):
+    from nose import SkipTest
+    try:
+        import numpy
+    except:
+        raise SkipTest("NumPy not available")
