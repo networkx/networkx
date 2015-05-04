27783955f570c8f32985822bb67bb66eab9aa496
Thodoris Sotiropoulos <theosotr@windowslive.com>
2015-05-05 00:55:17 +0300

    Add simulated annealing algorithm
    Add a metaheuristic local search algorithm for solving TSP

--- networkx/algorithms/applications/tests/test_tsp.py
+++ networkx/algorithms/applications/tests/test_tsp.py
@@ -3,25 +3,34 @@ import networkx as nx
 
 
 def _setUp(self):
-    self.unsymmetric = nx.DiGraph()
-    self.unsymmetric.add_weighted_edges_from({('A', 'B', 3),
-                                              ('A', 'C', 17), ('A', 'D', 14), ('B', 'A', 3),
-                                              ('B', 'C', 12), ('B', 'D', 16), ('C', 'A', 13),
-                                              ('C', 'B', 12), ('C', 'D', 4), ('D', 'A', 14),
-                                              ('D', 'B', 15), ('D', 'C', 2)})
+    self.DG = nx.DiGraph()
+    self.DG.add_weighted_edges_from({('A', 'B', 3),
+                                     ('A', 'C', 17), ('A', 'D', 14), ('B', 'A', 3),
+                                     ('B', 'C', 12), ('B', 'D', 16), ('C', 'A', 13),
+                                     ('C', 'B', 12), ('C', 'D', 4), ('D', 'A', 14),
+                                     ('D', 'B', 15), ('D', 'C', 2)})
+    self.DG2 = nx.DiGraph()
+    self.DG2.add_weighted_edges_from({('A', 'B', 3),
+                                      ('A', 'C', 17), ('A', 'D', 14), ('B', 'A', 30),
+                                      ('B', 'C', 2), ('B', 'D', 16), ('C', 'A', 33),
+                                      ('C', 'B', 32), ('C', 'D', 34), ('D', 'A', 14),
+                                      ('D', 'B', 15), ('D', 'C', 2)})
     self.unweightedUG = nx.complete_graph(5, nx.Graph())
     self.unweightedDG = nx.complete_graph(5, nx.DiGraph())
     self.notCompletedUG = nx.Graph()
     self.notCompletedUG.add_weighted_edges_from({(0, 1, 1), (1, 2, 3)})
     self.notCompletedDG = nx.DiGraph()
     self.notCompletedDG.add_weighted_edges_from({(0, 1, 1), (1, 2, 3)})
-    self.unsymmetricSelfLoops = nx.DiGraph()
-    self.unsymmetricSelfLoops.add_weighted_edges_from({('A', 'B', 3),
-                                                       ('A', 'C', 17), ('A', 'D', 14), ('B', 'A', 3),
-                                                       ('B', 'C', 12), ('B', 'D', 16), ('C', 'A', 13),
-                                                       ('C', 'B', 12), ('C', 'D', 4), ('D', 'A', 14),
-                                                       ('D', 'B', 15), ('D', 'C', 2), ('A', 'A', 2),
-                                                       ('B', 'B', 13), ('C', 'C', 2), ('D', 'D', 3)})
+    self.UG = nx.Graph()
+    self.UG.add_weighted_edges_from({('A', 'B', 3),
+                                     ('A', 'C', 17), ('A', 'D', 14),
+                                     ('B', 'C', 12), ('B', 'D', 16),
+                                     ('C', 'D', 4)})
+    self.UG2 = nx.Graph()
+    self.UG2.add_weighted_edges_from({('A', 'B', 1),
+                                      ('A', 'C', 15), ('A', 'D', 5),
+                                      ('B', 'C', 16), ('B', 'D', 8),
+                                      ('C', 'D', 3)})
 
 
 def validate_solution(comp, cost, exp_comp, exp_cost):
@@ -29,20 +38,86 @@ def validate_solution(comp, cost, exp_comp, exp_cost):
     assert_equal(cost, exp_cost)
 
 
-class TestGreedyTSP:
+def validate_symmetric_solution(comp, cost, exp_comp, exp_cost):
+    assert_true(comp == exp_comp or comp == exp_comp[::-1])
+    assert_equal(cost, exp_cost)
 
+
+class TestGreedyTSP:
     setUp = _setUp
 
     def test_greedy(self):
-        output = nx.greedy_tsp(self.unsymmetric, 'D')
-        validate_solution(output[0], output[1], ['D', 'C', 'B', 'A'], 17.0)
-        output2 = nx.greedy_tsp(self.unsymmetricSelfLoops, 'D')
-        validate_solution(output2[0], output2[1], ['D', 'C', 'B', 'A'], 17.0)
+        output = nx.greedy_tsp(self.DG, 'D')
+        validate_solution(output[0], output[1], ['D', 'C', 'B', 'A', 'D'], 31.0)
+
+        output = nx.greedy_tsp(self.DG2, 'D')
+        validate_solution(output[0], output[1], ['D', 'C', 'B', 'A', 'D'], 78.0)
+
+        output = nx.greedy_tsp(self.UG, 'D')
+        validate_solution(output[0], output[1], ['D', 'C', 'B', 'A', 'D'], 33.0)
+
+        output = nx.greedy_tsp(self.UG2, 'D')
+        validate_solution(output[0], output[1], ['D', 'C', 'A', 'B', 'D'], 27.0)
 
     def test_not_completed_graph(self):
-        assert_raises(nx.NetworkXError, nx.greedy_tsp, self.notCompletedUG, 0)
-        assert_raises(nx.NetworkXError, nx.greedy_tsp, self.notCompletedDG, 0)
+        assert_raises(nx.NetworkXError, nx.greedy_tsp,
+                      self.notCompletedUG, 0)
+        assert_raises(nx.NetworkXError, nx.greedy_tsp,
+                      self.notCompletedDG, 0)
 
     def test_not_weighted_graph(self):
-        assert_raises(nx.NetworkXError, nx.greedy_tsp, self.unweightedUG, 0)
-        assert_raises(nx.NetworkXError, nx.greedy_tsp, self.unweightedDG, 0)
+        assert_raises(nx.NetworkXError, nx.greedy_tsp,
+                      self.unweightedUG, 0)
+        assert_raises(nx.NetworkXError, nx.greedy_tsp,
+                      self.unweightedDG, 0)
+
+
+class TestSimulatedAnnealingTSP:
+    setUp = _setUp
+
+    def test_simulated_annealing(self):
+        output = nx.simulated_annealing_tsp(self.DG, 'D')
+        validate_solution(output[0], output[1], ['D', 'C', 'B', 'A', 'D'], 31.0)
+
+        output = nx.simulated_annealing_tsp(self.DG2, 'D')
+        validate_solution(output[0], output[1], ['D', 'A', 'B', 'C', 'D'], 53.0)
+        output = nx.simulated_annealing_tsp(self.DG2, 'D', move='1-0')
+        validate_solution(output[0], output[1], ['D', 'A', 'B', 'C', 'D'], 53.0)
+        output = nx.simulated_annealing_tsp(self.UG2, 'D')
+        validate_symmetric_solution(output[0], output[1], ['D', 'C', 'B', 'A', 'D'], 25.0)
+        output = nx.simulated_annealing_tsp(self.UG2, 'D', move='1-0')
+        validate_symmetric_solution(output[0], output[1], ['D', 'C', 'B', 'A', 'D'], 25.0)
+
+        initial_sol = ['D', 'B', 'A', 'C', 'D']
+        output = nx.simulated_annealing_tsp(self.DG, 'D', sol=initial_sol)
+        validate_solution(output[0], output[1], ['D', 'C', 'B', 'A', 'D'], 31.0)
+        initial_sol = ['D', 'A', 'C', 'B', 'D']
+        output = nx.simulated_annealing_tsp(self.DG, 'D', move='1-0',
+                                            sol=initial_sol)
+        validate_solution(output[0], output[1], ['D', 'C', 'B', 'A', 'D'], 31.0)
+
+        output = nx.simulated_annealing_tsp(self.UG, 'D')
+        validate_solution(output[0], output[1], ['D', 'C', 'B', 'A', 'D'], 33.0)
+
+    def test_not_completed_graph(self):
+        assert_raises(nx.NetworkXError, nx.simulated_annealing_tsp,
+                      self.notCompletedUG, 0, sol=[])
+        assert_raises(nx.NetworkXError, nx.simulated_annealing_tsp,
+                      self.notCompletedDG, 0, sol=[])
+
+    def test_not_weighted_graph(self):
+        assert_raises(nx.NetworkXError, nx.simulated_annealing_tsp,
+                      self.unweightedUG, 0, sol=[])
+        assert_raises(nx.NetworkXError, nx.simulated_annealing_tsp,
+                      self.unweightedDG, 0, sol=[])
+
+    def test_simulated_annealing_fails(self):
+        output = nx.simulated_annealing_tsp(self.DG2, 'D', move='1-0', a=1,
+                                            inner_iter=1, outer_iter=1)
+        assert_true(output[1] > 53.0)
+
+        initial_sol = ['D', 'A', 'B', 'C', 'D']
+        output = nx.simulated_annealing_tsp(self.DG, 'D', move='1-0', a=1,
+                                            inner_iter=1, outer_iter=1,
+                                            sol=initial_sol)
+        assert_true(output[1] > 31.0)
--- networkx/algorithms/applications/tsp.py
+++ networkx/algorithms/applications/tsp.py
@@ -28,9 +28,9 @@ http://en.wikipedia.org/wiki/Travelling_salesman_problem
 """
 import networkx as nx
 from operator import itemgetter
-from random import choice, randint
+from random import choice, randint, random
 
-__all__ = ['greedy_tsp']
+__all__ = ['greedy_tsp', 'simulated_annealing_tsp']
 
 
 def greedy_tsp(G, source, weight='weight'):
@@ -75,9 +75,9 @@ def greedy_tsp(G, source, weight='weight'):
     >>>                           ('D', 'B', 15), ('D', 'C', 2)})
     >>> sol = nx.greedy_tsp(G, 'D')
     >>> sol[0]
-    [('D', 'C'), ('C', 'B'), ('B', 'A')]
+    ['D', 'C', 'B', 'A', 'D']
     >>> sol[1]
-    17.0
+    31.0
 
     Notes
     -----
@@ -107,6 +107,8 @@ def greedy_tsp(G, source, weight='weight'):
         cost += dist
         nodelist.remove(next_visitor)
         source = next_visitor
+    sol.append(sol[0])
+    cost += G.edge[sol[-2]][sol[0]][weight]
     return sol, cost
 
 
@@ -117,19 +119,7 @@ def _is_completed(G):
     :param G: A networkX Graph
     :return: True if graph is completed; False otherwise.
     """
-    _remove_self_loops(G)
-    V = G.number_of_nodes()
-    E = G.number_of_edges()
-    return E == V * (V - 1)
-
-
-def _remove_self_loops(G):
-    """
-    Remove self loops on the graph.
-
-    :param G: A networkX Graph.
-    """
-    [G.remove_edge(node, node) for node in G.nodes() if G.has_edge(node, node)]
+    return all(G.has_edge(u, v) for u in G.nodes() for v in G.nodes() if u != v)
 
 
 def _select_next(G, source, nodes, weight='weight'):
@@ -149,3 +139,182 @@ def _select_next(G, source, nodes, weight='weight'):
     visitors = [(node, G.edge[source][node][weight])
                 for node in nodes]
     return sorted(visitors, key=itemgetter(1))[0]
+
+
+def simulated_annealing_tsp(G, source, temp=100, move='1-1', outer_iter=10,
+                            inner_iter=100, a=0.01, sol=None, weight='weight'):
+    """Finds the route that salesman has to visit in order
+    to minimize total distance and total distance using a
+    simulated annealing algorithm.
+
+    In essence, function returns a cycle given a source point
+    which total cost of cycle is minimized.
+
+    Parameters
+    ----------
+    G: A complete weighted graph.
+
+    source : node
+        Starting node
+
+    temp : int, optional (default=100)
+        An algorithm's parameter. It represents the initial
+        temperature of algorithm
+
+    move : string, optional (default='1-1')
+        Move to be applied in a solution to generate a
+        neighbor solution.
+
+    outer_iter : int, optional (default=10)
+        Number of consecutive iterations of outer loop
+        that cost of best solution is not decreased
+
+    inner_iter : int, optional (default=100)
+        Number of times that inner loop is being executed.
+
+    a : float between (0, 1), optional (default=0.01)
+        Percentage of temperature decrease in each iteration
+        of outer loop
+
+    sol : list, optional (default=None)
+        Initial solution contains the sequence that nodes
+        must be visited.
+
+    weight: string, optional (default='weight')
+        Edge data key corresponding to the edge weight
+
+    Raises
+    ------
+    NetworkXError
+        If graph is not either complete or weighed,
+        algorithm raises an exception.
+
+    Returns
+    -------
+    sol : list, cost : float
+        Returns the route (list of nodes) that salesman
+        has to follow to minimize total cost, and total
+        cost of algorithm's solution.
+
+    Examples
+    --------
+    >>> import networkx as nx
+    >>> G = nx.DiGraph()
+    >>> G.add_weighted_edges_from({('A', 'B', 3),
+    >>>                           ('A', 'C', 17), ('A', 'D', 14), ('B', 'A', 3),
+    >>>                           ('B', 'C', 12), ('B', 'D', 16), ('C', 'A', 13),
+    >>>                           ('C', 'B', 12), ('C', 'D', 4), ('D', 'A', 14),
+    >>>                           ('D', 'B', 15), ('D', 'C', 2)})
+    >>> sol = nx.simulated_annealing_tsp(G, 'D')
+    >>> sol[0]
+    ['D', 'C', 'B', 'A', 'D']
+    >>> sol[1]
+    31.0
+    >>> sol = nx.simulated_annealing_tsp(G, 'D', sol=['D', 'B', 'A', 'C', 'D'])
+    >>> sol[0]
+    ['D', 'C', 'B', 'A', 'D']
+    >>> sol[1]
+    31.0
+
+    Notes
+    -----
+    Simulated Annealing is a metaheuristic local search algorithm.
+    The main characteristic of this algorithm is that it accepts
+    even solutions which lead to the increase of the cost in order
+    to escape from low quality local optimal solutions.
+
+    This algorithm needs an initial solution. This solution can be
+    constructed by a simple greedy algorithm. At every iteration, it
+    selects thoughtfully a neighbor solution.
+    Consider c(x) cost of current solution and c(x') cost of
+    neighbor solution.
+    If c(x') - c(x) <= 0 then neighbor solution becomes current
+    solution for the next iteration. Otherwise, algorithm accepts
+    neighbor solution to become current solution for the next iteration
+    with probability p = exp - ([c(x') - c(x)] / temp).
+
+    Temp is parameter of algorithm and represents temperature in every
+    iteration.
+
+    For more information and how algorithm is inspired see:
+    http://en.wikipedia.org/wiki/Simulated_annealing
+    """
+    if sol is None:
+        # Construct an initial solution using a greedy algorithm.
+        sol, cost = greedy_tsp(G, source, weight=weight)
+
+    else:
+        # Calculate the cost of initial solution and make the essential checks for graph.
+
+        if not _is_completed(G):
+            raise nx.NetworkXError('Given graph is not completed.')
+        if not nx.is_weighted(G, weight=weight):
+            raise nx.NetworkXError('Given graph is not weighted.')
+        cost = _calculate_cost(G, sol, weight=weight)
+
+    count = 0
+    best_sol = list(sol)
+    best_cost = cost
+    while count <= outer_iter and temp > 0:
+        count += 1
+        for i in range(0, inner_iter):
+            adj_sol = _apply_move(sol, move)
+            adj_cost = _calculate_cost(G, adj_sol, weight=weight)
+            delta = adj_cost - cost
+            if delta <= 0:
+
+                # Set current solution the adjacent solution.
+                sol = list(adj_sol)
+                cost = adj_cost
+
+                if cost < best_cost:
+                    count = 0
+                    best_sol = list(sol)
+                    best_cost = cost
+            else:
+                import math
+
+                # Accept even a worse solution with probability p.
+                p = math.exp(- (delta / temp))
+                if p >= random():
+                    sol = list(adj_sol)
+                    cost = adj_cost
+
+        temp -= temp * a
+
+    return best_sol, best_cost
+
+
+def _apply_move(sol, move):
+    """
+    Apply a move to a solution to generate a neighbor solution.
+
+    :param sol: Current solution (list of nodes)
+    :param move: Move to be applied
+    :return: A neighbor solution
+    """
+    a = randint(1, len(sol) - 2)
+    listb = range(1, a) + range(a + 1, len(sol) - 2)
+    b = choice(listb)
+    if move == '1-1':
+        sol[a], sol[b] = sol[b], sol[a]
+    elif move == '1-0':
+        sol.insert(b, sol.pop(a))
+    return sol
+
+
+def _calculate_cost(G, sol, weight='weight'):
+    """
+    Calculate cost (distance) of a solution (list of nodes)
+
+    For example, if we have the following solution:
+    ['D', 'A', 'C', 'D']
+    Cost of solution is calculated as follows:
+    weight('D', 'A') + weight('A', 'C') + weight('C', 'D')
+
+    :param G: A NetoworkX graph
+    :param sol: List of nodes. Sequence that nodes must be visited.
+    :param weight: Edge data key corresponding to the edge weight
+    :return: float, cost (dist) of solution
+    """
+    return sum(G.edge[sol[i]][sol[i + 1]][weight] for i in range(0, len(sol) - 1))
