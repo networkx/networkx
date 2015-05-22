78d9d3499a8bee51256ab16246aa0df3979ed3d6
Aitor <aitoralmeida@gmail.com>
2015-05-22 19:09:36 +0200

    added an aditional test for single node communities

--- networkx/algorithms/community/tests/test_label_propagation.py
+++ networkx/algorithms/community/tests/test_label_propagation.py
@@ -28,16 +28,48 @@ def test_one_node():
 
 def test_simple_communities():
     test = nx.Graph()
+    # community 1
     test.add_edge('a', 'c')
     test.add_edge('a', 'd')
     test.add_edge('d', 'c')
+    # community 2
     test.add_edge('b', 'e')
     test.add_edge('e', 'f')
     test.add_edge('f', 'b')
+    
     # The expected communities are:    
     ground_truth = set([frozenset(['a', 'c', 'd']), 
                         frozenset(['b', 'e', 'f'])])
     
+    communities = label_propagation.label_propagation_communities(test)
+    result = set()
+    for c in communities:
+        result.add(frozenset(c))  
+    assert_equal(result, ground_truth)
+    
+def test_unconnected_communities():
+    test = nx.Graph()
+    # community 1
+    test.add_edge('a', 'b')
+    test.add_edge('c', 'b')
+    test.add_edge('a', 'c')
+    # community 2
+    test.add_edge('d', 'e')
+    test.add_edge('e', 'f')
+    test.add_edge('f', 'd')
+    # connection from community 1 to community 2
+    test.add_edge('a', 'd')
+    # community 3
+    test.add_edge('w', 'y')
+    # community 4 with only a single node
+    G.add_node('z')
+    
+    # The expected communities are:    
+    ground_truth = set([frozenset(['z']), 
+                        frozenset(['a', 'c', 'b']), 
+                        frozenset(['e', 'd', 'f']), 
+                        frozenset(['y', 'w'])])
+                        
     communities = label_propagation.label_propagation_communities(test)
     result = set()
     for c in communities:
