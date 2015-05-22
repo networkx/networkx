ee4a4314f26a4431d6e398c00951fcba072004a3
Tom Boothby <tomas.boothby@gmail.com>
2015-05-22 11:18:58 -0700

    resolves issue #1542

--- networkx/algorithms/flow/preflowpush.py
+++ networkx/algorithms/flow/preflowpush.py
@@ -102,7 +102,7 @@ def preflow_push_impl(G, s, t, capacity, residual, global_relabel_freq,
             push(s, u, flow)
 
     # Partition nodes into levels.
-    levels = [Level() for i in range(2 * n - 1)]
+    levels = [Level() for i in range(2 * n)]
     for u in R:
         if u != s and u != t:
             level = levels[R_node[u]['height']]
