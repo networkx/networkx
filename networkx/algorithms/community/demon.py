9743d1aafae1d9ee12e4d9ee9cad3917bd1d79cb
Aitor Almeida <aitoralmeida@gmail.com>
2015-05-27 11:13:44 +0200

    cleaned the code

--- networkx/algorithms/community/demon.py
+++ networkx/algorithms/community/demon.py
@@ -1,10 +1,10 @@
-#-*- coding: utf-8 -*-
-#    Copyright (C) 2015 
+# -*- coding: utf-8 -*-
+#    Copyright (C) 2015
 #    All rights reserved.
 #    BSD license.
 
 """
-Flat Merge version of DEMON (Democratic Estimate of the Modular Organization of 
+Flat Merge version of DEMON (Democratic Estimate of the Modular Organization of
 a Network) algorithm as described in:
 
 Michele Coscia, Giulio Rossetti, Fosca Giannotti, Dino Pedreschi:
@@ -12,7 +12,7 @@ DEMON: a local-first discovery method for overlapping communities.
 KDD 2012:615-623
 
 DEMON uses a local-first approach to community discovery were each node
-democratically votes for the communities that it sees in its surroundings. It 
+democratically votes for the communities that it sees in its surroundings. It
 provides a deterministic, fully incremental method for community discovery that
 has a limited time complexity.
 
@@ -26,11 +26,12 @@ import random
 
 __all__ = ["demon_communities"]
 
+
 def demon_communities(G, epsilon=0.25, min_community_size=3, weight=None):
-    """Finds the communities in a graph using the label Democratic Estimate of 
-    the Modular Organization of a Network (DEMON) algorithm[1]_. This method 
-    uses a local-first approach to community discovery were each node 
-    democratically votes for the communities that it sees in its surroundings. 
+    """Finds the communities in a graph using the label Democratic Estimate of
+    the Modular Organization of a Network (DEMON) algorithm[1]_. This method
+    uses a local-first approach to community discovery were each node
+    democratically votes for the communities that it sees in its surroundings.
     Communities in this algorithm can be overlapping
     
     Algorithm:
@@ -39,26 +40,26 @@ def demon_communities(G, epsilon=0.25, min_community_size=3, weight=None):
     Ensure: set of overlapping communities C'
         
     1: for all v in V do:
-    2:   e <- EgoMinusEgo(v, G)    
+    2:   e <- EgoMinusEgo(v, G)
     3:   C(v) <- LabelPropagation(e)
     4:   for all C in C'(v) do:
     5:     C <- C union v
     6:     C' <- Merge(C', C, v)
     7:   end for
     8: end for
-    9: return C' 
+    9: return C'
        
     Parameters
     ----------
     G : graph
-        An NetworkX graph. 
+        An NetworkX graph.
     
     epsilon: real
-        The tolerance required in order to merge communities. The epsilon 
-        factor is introduced to vary the percentage of common elements provided 
-        from each couple of communities: epsilon = 0 ensure that two 
-        communities are merged only if one of them is a proper subset of the 
-        other, on the other hand with a value of epsilon = 1 even communities 
+        The tolerance required in order to merge communities. The epsilon
+        factor is introduced to vary the percentage of common elements provided
+        from each couple of communities: epsilon = 0 ensure that two
+        communities are merged only if one of them is a proper subset of the
+        other, on the other hand with a value of epsilon = 1 even communities
         that do not share a single node are merged together
     
     min_community_size: integer
@@ -66,30 +67,28 @@ def demon_communities(G, epsilon=0.25, min_community_size=3, weight=None):
     
     weight: String
         If the graph is weighted, the edge parameter representing the weight
-     
-    
+        
     Returns
     -------
     communities : list
-        List of overlapping communities (sets) 
-    
-  
+        List of overlapping communities (sets)
+     
     References
     ----------
     .. [1] Michele Coscia, Giulio Rossetti, Fosca Giannotti, Dino Pedreschi:
            DEMON: a local-first discovery method for overlapping communities.
            KDD 2012:615-623.
-    """        
+    """
     
     for n in G.nodes():
-            G.node[n]['communities'] = [n]                   
+            G.node[n]['communities'] = [n]
     
     all_communities = []
 
     for ego in nx.nodes(G):
         # EgoMinusEgo and LabelPropagation phase
         ego_minus_ego = nx.ego_graph(G, ego, 1, False)
-        community_to_nodes = _overlapping_label_propagation(ego_minus_ego, ego, weight)   
+        community_to_nodes = _overlapping_label_propagation(ego_minus_ego, ego, weight)
         
         # merging phase
         for c in community_to_nodes.keys():
@@ -100,7 +99,8 @@ def demon_communities(G, epsilon=0.25, min_community_size=3, weight=None):
     for c in all_communities:
         yield c
 
-def _overlapping_label_propagation(ego_minus_ego, ego, weight, max_iteration=100):
+
+def _overlapping_label_propagation(ego_minus_ego, ego, weight, max_iter=100):
     """
     Label propagation algorithm based on [2]_ used to find communities in the
     EgoMinusEgo Graph.
@@ -108,7 +108,7 @@ def _overlapping_label_propagation(ego_minus_ego, ego, weight, max_iteration=100
     Parameters
     ----------
     ego_minus_ego : graph
-        ego network minus its center 
+        ego network minus its center
         
     ego : string
         ego network cente
@@ -116,7 +116,7 @@ def _overlapping_label_propagation(ego_minus_ego, ego, weight, max_iteration=100
     weight : string
         If the graph is weighted, the edge parameter representing the weight
     
-    max_iteration : integer
+    max_iter : integer
         number of desired iteration for the label propagation
         
     Returns
@@ -126,24 +126,18 @@ def _overlapping_label_propagation(ego_minus_ego, ego, weight, max_iteration=100
     
     References
     ----------
-    .. [2] Raghavan, Usha Nandini, Réka Albert, and Soundar Kumara. "Near 
-           linear time algorithm to detect community structures in large-scale 
-           networks." Physical Review E 76.3 (2007): 036106. 
+    .. [2] Raghavan, Usha Nandini, Réka Albert, and Soundar Kumara. "Near
+           linear time algorithm to detect community structures in large-scale
+           networks." Physical Review E 76.3 (2007): 036106.
     
     """
-    t = 0
-
     old_node_to_coms = {}
 
-    while t < max_iteration:
-        t += 1
-
+    for t in range(max_iter):
         node_to_coms = {}
 
         nodes = nx.nodes(ego_minus_ego)
         random.shuffle(nodes)
-
-#        count = -len(nodes)
 
         for n in nodes:
             label_freq = {}
@@ -153,10 +147,6 @@ def _overlapping_label_propagation(ego_minus_ego, ego, weight, max_iteration=100
             if len(n_neighbors) < 1:
                 continue
 
-#            if count == 0:
-#                t += 1
-
-            # compute the frequency of the labels
             for nn in n_neighbors:
 
                 communities_nn = [nn]
@@ -179,16 +169,14 @@ def _overlapping_label_propagation(ego_minus_ego, ego, weight, max_iteration=100
                             label_freq[nn_c] = 1
 
             # first run, random choosing of the communities among the neighbors labels
-            if t == 1:
+            if t == 0:
                 if not len(n_neighbors) == 0:
                     r_label = random.sample(label_freq.keys(), 1)
                     ego_minus_ego.node[n]['communities'] = r_label
                     old_node_to_coms[n] = r_label
-#                count += 1
-#                continue
 
             # choosing the majority
-            else:                      
+            else:
                 max_freq = max(label_freq.values())
                 labels = [l for l in label_freq if label_freq[l] == max_freq]
 
@@ -197,8 +185,6 @@ def _overlapping_label_propagation(ego_minus_ego, ego, weight, max_iteration=100
                 if n not in old_node_to_coms or not set(node_to_coms[n]) == set(old_node_to_coms[n]):
                     old_node_to_coms[n] = node_to_coms[n]
                     ego_minus_ego.node[n]['communities'] = labels
-
-        t += 1
 
     # build the communities reintroducing the ego
     community_to_nodes = {}
@@ -209,12 +195,13 @@ def _overlapping_label_propagation(ego_minus_ego, ego, weight, max_iteration=100
         c_n = ego_minus_ego.node[n]['communities']
 
         for c in c_n:
-            if c in community_to_nodes: 
+            if c in community_to_nodes:
                 community_to_nodes[c].append(n)
             else:
                 community_to_nodes[c] = [n, ego]
 
     return community_to_nodes
+
 
 def _merge_communities(communities, actual_community, epsilon):
     """
@@ -235,7 +222,7 @@ def _merge_communities(communities, actual_community, epsilon):
     Returns
     -------
     communities : list
-        Merged communities   
+        Merged communities
     """
 
     # if the community is already present return
@@ -261,25 +248,26 @@ def _merge_communities(communities, actual_community, epsilon):
 
     return communities
 
+
 def _generalized_inclusion(c1, c2, epsilon):
     """
-    As explained in [1]_: 
-    "Two  communities C and I are  merged  if  and  only  if  at most the 
-    epsilon % of the smaller one is not included in the bigger one;  in  this  
-    case, C and I are removed from the community list and their union is added 
-    to the result set. The epsilon factor is introduced to vary the percentage 
-    of common elements provided from each couple of communities: epsilon = 0 
-    ensure that two communities are merged only if one of them is a proper 
-    subset of the other, on the other hand with a value of epsilon = 1 even 
+    As explained in [1]_:
+    "Two  communities C and I are  merged  if  and  only  if  at most the
+    epsilon % of the smaller one is not included in the bigger one;  in  this
+    case, C and I are removed from the community list and their union is added
+    to the result set. The epsilon factor is introduced to vary the percentage
+    of common elements provided from each couple of communities: epsilon = 0
+    ensure that two communities are merged only if one of them is a proper
+    subset of the other, on the other hand with a value of epsilon = 1 even
     communities that do not share a single node are merged together"
     
     Parameters
     ----------
     c1 : set
-        Community 1. 
+        Community 1.
         
     c2 : set
-        Community 2. 
+        Community 2.
     
     epsilon: real
         The tolerance required in order to merge communities
@@ -287,7 +275,7 @@ def _generalized_inclusion(c1, c2, epsilon):
     Returns
     -------
     union : set
-        Union of both communities if the similarity is greater than epsilon 
+        Union of both communities if the similarity is greater than epsilon
     
     References
     ----------
@@ -299,46 +287,10 @@ def _generalized_inclusion(c1, c2, epsilon):
     smaller_set = min(len(c1), len(c2))
     
     if len(intersection) > 0 and smaller_set > 0:
-        inclusion_pct = len(intersection) / smaller_set 
+        inclusion_pct = len(intersection) / smaller_set
 
         if inclusion_pct > epsilon:
             union = c2 | c1
             return union
             
     return None
-
-
-if __name__ == '__main__':
-    
-    test = nx.Graph()
-    
-    # community 1
-    test.add_edge('a', 'b')
-    test.add_edge('c', 'b')
-    test.add_edge('a', 'c')
-    test.add_edge('a', 'd')
-    test.add_edge('b', 'd')
-    test.add_edge('c', 'd')
-    
-    # community 2
-    test.add_edge('e', 'f')
-    test.add_edge('e', 'g')
-    test.add_edge('e', 'h')
-    test.add_edge('f', 'g')
-    test.add_edge('f', 'h')
-    test.add_edge('h', 'g')
-
-    # connection from community 1 to community 2
-    test.add_edge('a', 'middle')
-    test.add_edge('e', 'middle')
-    
-    
-    print 'starting'
-    communities = demon_communities(nx.karate_club_graph(), 0.25, 3, None)
-    for c in communities:
-        print c
-    print 'FIN'
- 
-    
-    
-    
--- networkx/algorithms/community/tests/test_demon.py
+++ networkx/algorithms/community/tests/test_demon.py
@@ -38,12 +38,15 @@ def test_simple_communities():
         result.add(frozenset(c))  
     assert_equal(result, ground_truth)
     
+
 def test_karate():
     test = nx.karate_club_graph()
     
     # The expected communities are:    
     ground_truth = set([frozenset([0, 16, 4, 5, 6, 10]), 
-                        frozenset([0, 1, 2, 3, 7, 8, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 26, 27, 28, 29, 30, 31, 32, 33])])
+                        frozenset([0, 1, 2, 3, 7, 8, 12, 13, 14, 15, 17, 18, 
+                                   19, 20, 21, 22, 23, 26, 27, 28, 29, 30, 31, 
+                                   32, 33])])
     
     communities = demon.demon_communities(test, 0.25, 3, None)
     result = set()
@@ -51,6 +54,7 @@ def test_karate():
         result.add(frozenset(c))  
     assert_equal(result, ground_truth)
     
+
 def test_min_number_of_nodes():
     test = nx.Graph()
     
@@ -67,31 +71,27 @@ def test_min_number_of_nodes():
     test.add_edge('e', 'h')
     test.add_edge('f', 'h')   
 
-    
-    #******************* min number 3 *******************
+    #******************* min_community_size = 3 *******************
 
     communities1 = demon.demon_communities(test, 0.25, 3, None)   
     
     # The expected communities are:    
     ground_truth1 = set([frozenset(['a', 'c', 'b', 'd'])])   
-    
-    
+     
     result1 = set()
     for c in communities1:
         result1.add(frozenset(c))  
         
     assert_equal(result1, ground_truth1)
-    
-    
-    #******************* min number 2 *******************
+      
+    #******************* min_community_size = 2 *******************
     
     communities2 = demon.demon_communities(test, 0.25, 2, None)   
     
     # The expected communities are:    
     ground_truth2 = set([frozenset(['a', 'b', 'c', 'd']), 
                         frozenset(['h', 'e', 'f'])])  
-    
-    
+      
     result2 = set()
     for c in communities2:
         print c
