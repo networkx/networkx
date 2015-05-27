5f4fa5a3fa8366fa9b5ee60521be4adbfd6c801c
Aitor Almeida <aitoralmeida@gmail.com>
2015-05-27 09:17:44 +0200

    added some extra tests

--- networkx/algorithms/community/tests/test_demon.py
+++ networkx/algorithms/community/tests/test_demon.py
@@ -38,3 +38,63 @@ def test_simple_communities():
         result.add(frozenset(c))  
     assert_equal(result, ground_truth)
     
+def test_karate():
+    test = nx.karate_club_graph()
+    
+    # The expected communities are:    
+    ground_truth = set([frozenset([0, 16, 4, 5, 6, 10]), 
+                        frozenset([0, 1, 2, 3, 7, 8, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 26, 27, 28, 29, 30, 31, 32, 33])])
+    
+    communities = demon.demon_communities(test, 0.25, 3, None)
+    result = set()
+    for c in communities:
+        result.add(frozenset(c))  
+    assert_equal(result, ground_truth)
+    
+def test_min_number_of_nodes():
+    test = nx.Graph()
+    
+    # community 1
+    test.add_edge('a', 'b')
+    test.add_edge('c', 'b')
+    test.add_edge('a', 'c')
+    test.add_edge('a', 'd')
+    test.add_edge('b', 'd')
+    test.add_edge('c', 'd')
+    
+    # community 2
+    test.add_edge('e', 'f')
+    test.add_edge('e', 'h')
+    test.add_edge('f', 'h')   
+
+    
+    #******************* min number 3 *******************
+
+    communities1 = demon.demon_communities(test, 0.25, 3, None)   
+    
+    # The expected communities are:    
+    ground_truth1 = set([frozenset(['a', 'c', 'b', 'd'])])   
+    
+    
+    result1 = set()
+    for c in communities1:
+        result1.add(frozenset(c))  
+        
+    assert_equal(result1, ground_truth1)
+    
+    
+    #******************* min number 2 *******************
+    
+    communities2 = demon.demon_communities(test, 0.25, 2, None)   
+    
+    # The expected communities are:    
+    ground_truth2 = set([frozenset(['a', 'b', 'c', 'd']), 
+                        frozenset(['h', 'e', 'f'])])  
+    
+    
+    result2 = set()
+    for c in communities2:
+        print c
+        result2.add(frozenset(c))  
+        
+    assert_equal(result2, ground_truth2)
