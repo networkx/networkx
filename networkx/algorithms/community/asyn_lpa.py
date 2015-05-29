7a755b4e61e8964cfc7d1c4a7872833306b38053
Aitor Almeida <aitoralmeida@gmail.com>
2015-05-29 10:35:55 +0200

    Added more tests. Improved the documentation

--- networkx/algorithms/community/asyn_lpa.py
+++ networkx/algorithms/community/asyn_lpa.py
@@ -6,15 +6,27 @@
 import random
 import networkx as nx
 
+
 def asyn_lpa_communities(G, weight=None):
     """
-    Asynchornous label propagation algorithm for community detection described
-    on [1]_. The algorithm follows these steps:
+    Asynchronous label propagation algorithm for community detection described
+    on [1]_. The algorithm is non-deterministic and the found communities may
+    vary on different executions.
+    
+    The algorithm follows these steps:
         
         1. Initialize the labels at all nodes in the network giving them unique
         identifiers.
-        2. 
+        2. Set t=1.
+        3. Arrange the nodes in the network in a random order and set it to X.
+        4. For each x in X chosen in that specific order, get the label 
+        occurring with the highest frecuency among neighbors. Ties are broken
+        uniformly randomly.
+        5. If every node has a label that the maximun number of their neighbors
+        have, then stop the algorithm. Else set t = t+1 and got to (3).
 
+    Modified to be able to take into account the edge weights when assigning
+    the labels to one node.
     
     Parameters
     ----------
@@ -22,7 +34,8 @@ def asyn_lpa_communities(G, weight=None):
         Network to be processed
         
     weight : string
-        If the graph is weighted, the edge parameter representing the weight
+        If the graph is weighted, the edge parameter representing the weight.
+        If left to None edge weights are not taken into account.
          
     Returns
     -------
@@ -48,9 +61,11 @@ def asyn_lpa_communities(G, weight=None):
             
             if len(n_neighbors) < 1:
                 continue
-            
-            label_freq = {}            
-            # Get label frecuencies
+                                 
+            # Get label frecuencies. Depending on the order they are processed
+            # in some nodes with be in t and others in t-1, making the
+            # algorithm asynchronous
+            label_freq = {}               
             for neighbor in n_neighbors:
                 n_label = labels[neighbor]
                 if n_label in label_freq:
@@ -64,19 +79,17 @@ def asyn_lpa_communities(G, weight=None):
                     else:
                         label_freq[n_label] = 1   
                         
-            # Chose the label with the highest frecuency
-            # If more than 1 label has the highest frecuency choose one 
-            # randomly
+            # Chose the label with the highest frecuency. If more than 1 label
+            # has the highest frecuency choose one randomly
             max_freq = max(label_freq.values())
             best_labels = [label for label, freq in label_freq.items() if freq == max_freq]
             new_label = random.choice(best_labels)
             labels[node] = new_label
             # Continue until all nodes have a label that is best than other 
-            # neighbour labels
+            # neighbour labels (only one label has max_freq for each node)
             cont = cont or not (len(best_labels) == 1)
             
-    
-    #return the communities        
+    # return the communities        
     communities = {l for l in labels.values()}
     for c in communities:
         yield {n for n in labels if labels[n] == c}
--- networkx/algorithms/community/tests/test_asyn_lpa.py
+++ networkx/algorithms/community/tests/test_asyn_lpa.py
@@ -2,6 +2,36 @@
 from nose.tools import assert_equal
 from networkx import asyn_lpa, Graph
 
+
+def test_empty_graph():
+    # empty graph
+    test = Graph()    
+    
+    # ground truth
+    ground_truth = set()
+                        
+    communities = asyn_lpa.asyn_lpa_communities(test)
+    result = set()
+    for c in communities:
+        result.add(frozenset(c))  
+    assert_equal(result, ground_truth)
+    
+
+def test_single_node():
+    test = Graph()
+    
+    test.add_node('a')
+    
+    # ground truth
+    ground_truth = set([frozenset(['a'])])
+                        
+    communities = asyn_lpa.asyn_lpa_communities(test)
+    result = set()
+    for c in communities:
+        result.add(frozenset(c))  
+    assert_equal(result, ground_truth)
+
+
 def test_simple_communities():
     test = Graph()
     
@@ -24,14 +54,42 @@ def test_simple_communities():
     for c in communities:
         result.add(frozenset(c))  
     assert_equal(result, ground_truth)
+   
     
-def test_single_node():
+def test_several_communities():
     test = Graph()
     
-    test.add_node('a')
+    # c1
+    test.add_edge('1a', '1b')
+    test.add_edge('1a', '1c')
+    test.add_edge('1b', '1c')
+    
+    # c2
+    test.add_edge('2a', '2b')
+    test.add_edge('2a', '2c')
+    test.add_edge('2b', '2c')
+    
+    # c3
+    test.add_edge('3a', '3b')
+    test.add_edge('3a', '3c')
+    test.add_edge('3b', '3c')
+    
+    # c4
+    test.add_edge('4a', '4b')
+    test.add_edge('4a', '4c')
+    test.add_edge('4b', '4c')
+    
+    # c5
+    test.add_edge('5a', '5b')
+    test.add_edge('5a', '5c')
+    test.add_edge('5b', '5c') 
     
     # ground truth
-    ground_truth = set([frozenset(['a'])])
+    ground_truth = set([frozenset(['1a', '1c', '1b']),
+                        frozenset(['2a', '2c', '2b']),
+                        frozenset(['3a', '3c', '3b']),
+                        frozenset(['4a', '4c', '4b']),
+                        frozenset(['5a', '5c', '5b'])])
                         
     communities = asyn_lpa.asyn_lpa_communities(test)
     result = set()
