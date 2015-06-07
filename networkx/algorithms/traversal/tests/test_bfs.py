5d653919bc81543e2534bdaed751aa3e1ae1e14d
Mridul Seth <seth.mridul@gmail.com>
2015-06-07 14:13:08 +0530

    Add bfs test for directed graph in reverse direction

--- networkx/algorithms/traversal/tests/test_bfs.py
+++ networkx/algorithms/traversal/tests/test_bfs.py
@@ -27,6 +27,12 @@ class TestBFS:
         edges=nx.bfs_edges(self.G,source=0)
         assert_equal(list(edges),[(0, 1), (1, 2), (1, 3), (2, 4)])
 
+    def test_bfs_edges_reverse(self):
+        D = nx.DiGraph()
+        D.add_edges_from([(0,1),(1,2),(1,3),(2,4),(3,4)])
+        edges = nx.bfs_edges(D, source=4, reverse=True)
+        assert_equal(list(edges), [(4, 2), (4, 3), (2, 1), (1, 0)])
+
     def test_bfs_tree_isolates(self):
         G = nx.Graph()
         G.add_node(1)
