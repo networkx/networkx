da72617fc19436ad22f8808bca216f26c1606a18
Russell Jurney <russell.jurney@gmail.com>
2015-06-20 19:57:35 -0700

    Remove tab

--- networkx/algorithms/tests/test_core.py
+++ networkx/algorithms/tests/test_core.py
@@ -71,7 +71,7 @@ class TestCore:
         # k=2
         k_core_subgraph=nx.k_core(self.H,k=2)
         assert_equal(sorted(k_core_subgraph.nodes()),[2,4,5,6])
-    
+
     def test_main_crust(self):
         main_crust_subgraph=nx.k_crust(self.H)
         assert_equal(sorted(main_crust_subgraph.nodes()),[0,1,3])
