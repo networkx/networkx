162d19f0456ab86997b8064dca10f7dae5785dad
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2015-07-30 02:20:19 -0400

    Clarifies documentation for asynchronous LPA.
    Also makes some PEP8 and whitespace fixes.

--- networkx/algorithms/community/asyn_lpa.py
+++ networkx/algorithms/community/asyn_lpa.py
@@ -2,6 +2,7 @@
 #    Copyright (C) 2015
 #    All rights reserved.
 #    BSD license.
+"""Asynchronous label propagation algorithms for community detection."""
 
 import random
 from itertools import groupby
@@ -11,40 +12,40 @@ import networkx as nx
 
 
 def asyn_lpa_communities(G, weight=None):
-    """
-    Asynchronous label propagation algorithm for community detection described
-    on [1]_. The algorithm is probabilistic and the found communities may
+    """Returns communities in ``G`` as detected by asynchronous label
+    propagation.
+
+    The asynchronous label propagation algorithm is described in
+    [1]_. The algorithm is probabilistic and the found communities may
     vary on different executions.
 
-    The algorithm follows these steps:
+    The algorithm proceeds as follows. After initializing each node with
+    a unique label, the algorithm repeatedly sets the label of a node to
+    be the label that appears most frequently among that nodes
+    neighbors. The algorithm halts when each node has the label that
+    appears most frequently among its neighbors. The algorithm is
+    asynchronous because each node is updated without waiting for
+    updates on the remaining nodes.
 
-    1. Initialize the labels at all nodes in the network giving them unique
-    identifiers.
-    2. Set _t=1_.
-    3. Arrange the nodes in the network in a random order and set it to _X_.
-    4. For each _x_ in _X_ chosen in that specific order, get the label
-    occurring with the highest frecuency among neighbors. Ties are broken
-    uniformly randomly.
-    5. If every node has a label that the maximun number of their neighbors
-    have, then stop the algorithm. Else set _t = t+1_ and got to (3).
-
-    Modified to be able to take into account the edge weights when assigning
-    the labels to one node.
+    This generalized version of the algorithm in [1]_ accepts edge
+    weights.
 
     Parameters
     ----------
-    G : graph
-        Network to be processed
+    G : Graph
 
     weight : string
-        If the graph is weighted, the edge parameter representing the weight.
-        If left to None edge weights are not taken into account.
+        The edge attribute representing the weight of an edge. If
+        ``None``, each edge is assumed to have weight one. In this
+        algorithm, the weight of an edge is used in determining the
+        frequency with which a label appears among the neighbors of a
+        node: a higher weight means the label appears more often.
 
     Returns
     -------
-    communities : list
-        List of communities (sets)
-        
+    communities : iterable
+        Iterable of communities given as sets of nodes.
+
     Notes
     ------
     Edge weight attributes must be numerical.
@@ -72,9 +73,8 @@ def asyn_lpa_communities(G, weight=None):
             # Get label frequencies. Depending on the order they are processed
             # in some nodes with be in t and others in t-1, making the
             # algorithm asynchronous.
-            label_freq = Counter()
-            label_freq.update({labels[v]: G.edge[v][node][weight] 
-                                if weight else 1 for v in G[node]})
+            label_freq = Counter({labels[v]: G.edge[v][node][weight]
+                                  if weight else 1 for v in G[node]})
 
             # Choose the label with the highest frecuency. If more than 1 label
             # has the highest frecuency choose one randomly.
@@ -86,6 +86,6 @@ def asyn_lpa_communities(G, weight=None):
             # Continue until all nodes have a label that is better than other
             # neighbour labels (only one label has max_freq for each node).
             cont = cont or len(best_labels) > 1
-   
-    return (set(v) for k, v 
-                in groupby(sorted(labels, key=labels.get), key=labels.get)) 
+
+    return (set(v) for k, v in groupby(sorted(labels, key=labels.get),
+                                       key=labels.get))
