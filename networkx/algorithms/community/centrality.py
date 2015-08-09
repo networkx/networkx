1755093650e3bc5cc056922df9195c203495d9ff
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2015-08-09 00:06:34 -0400

    Allow arbitrary ranking function in girvan_newman.
    This generalizes the `girvan_newman()` function to allow for arbitrary
    ranking functions on the edges during each iteration.

--- networkx/algorithms/community/centrality.py
+++ networkx/algorithms/community/centrality.py
@@ -4,7 +4,7 @@ import networkx as nx
 __all__ = ['girvan_newman']
 
 
-def girvan_newman(G, weight=None):
+def girvan_newman(G, ranking=None):
     """Find communities in graph using Girvan–Newman method.
 
     Parameters
@@ -13,6 +13,17 @@ def girvan_newman(G, weight=None):
 
     weight : string, optional (default=None)
        Edge data key corresponding to the edge weight.
+
+    ranking : function
+        Function that takes a graph as input and outputs a
+        dictionary. The keys in the dictionary must be the edges of the
+        graph and the values must be comparable. The edges with the
+        highest value will be removed at each iteration of the
+        algorithm. The function will be called during each iteration of
+        the algorithm to recompute the edge rankings.
+
+        If not specified, the function
+        :func:`networkx.edge_betweenness_centrality` will be used.
 
     Returns
     -------
@@ -25,6 +36,23 @@ def girvan_newman(G, weight=None):
     >>> comp[0]
     ([0, 1, 2, 3, 4], [8, 9, 5, 6, 7])
 
+    To utilize edge weights when choosing an edge with, for example, the
+    edge betweenness centrality ranking function::
+
+        >>> from functools import partial
+        >>> G = nx.path_graph(10)
+        >>> ranking = partial(nx.edge_betweenness_centrality, weight='weight')
+        >>> comp = girvan_newman(G, ranking=ranking)
+        >>> comp[0]
+        ([0, 1, 2, 3, 4], [8, 9, 5, 6, 7])
+
+    To specify a different ranking algorithm, for example edge current
+    flow betweenness centrality, use the ``ranking`` keyword argument::
+
+        >>> G = nx.path_graph(10)
+        >>> ranking = nx.edge_current_flow_betweenness_centrality
+        >>> comp = girvan_newman(G, ranking=ranking)  # doctest: +SKIP
+
     Notes
     -----
     The Girvan–Newman algorithm detects communities by progressively removing
@@ -33,16 +61,18 @@ def girvan_newman(G, weight=None):
     the tightly knit community structure is exposed and result can be depicted
     as a dendrogram.
     """
+    if ranking is None:
+        ranking = nx.edge_betweenness_centrality
     g = G.copy().to_undirected()
     components = []
     while g.number_of_edges() > 0:
-        _remove_max_edge(g, weight)
+        _remove_max_edge(g, ranking)
         components.append(tuple(list(H)
                                 for H in nx.connected_component_subgraphs(g)))
     return components
 
 
-def _remove_max_edge(G, weight=None):
+def _remove_max_edge(G, ranking):
     """
     Removes edge with the highest value on betweenness centrality.
 
@@ -57,7 +87,7 @@ def _remove_max_edge(G, weight=None):
     """
     number_components = nx.number_connected_components(G)
     while nx.number_connected_components(G) <= number_components:
-        betweenness = nx.edge_betweenness_centrality(G, weight=weight)
+        betweenness = ranking(G)
         max_value = max(betweenness.values())
         # Use a list of edges because G is changed in the loop
         for edge in list(G.edges()):
