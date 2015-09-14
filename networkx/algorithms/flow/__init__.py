2c796990abcfc2e2884813643f67a2e0c19dc7aa
Dan Schult <dschult@colgate.edu>
2015-09-14 10:36:12 -0400

    Make sure __all__ variables loaded in flow __init__.py

--- networkx/algorithms/flow/__init__.py
+++ networkx/algorithms/flow/__init__.py
@@ -1,12 +1,6 @@
-from .maxflow import *
-from .mincost import *
-from .edmondskarp import *
-from .preflowpush import *
-from .shortestaugmentingpath import *
-from .capacityscaling import *
-from .networksimplex import *
-from .utils import build_flow_dict, build_residual_network
-
+# this import is used to combine the __all__ varibles.
+from . import (maxflow, mincost, edmondskarp, preflowpush,
+        shortestaugmentingpath, capacityscaling, networksimplex)
 
 __all__ = sum([maxflow.__all__,
                 mincost.__all__,
@@ -16,3 +10,12 @@ __all__ = sum([maxflow.__all__,
                 capacityscaling.__all__,
                 networksimplex.__all__,
             ], [])
+
+from .maxflow import *
+from .mincost import *
+from .edmondskarp import *
+from .preflowpush import *
+from .shortestaugmentingpath import *
+from .capacityscaling import *
+from .networksimplex import *
+from .utils import build_flow_dict, build_residual_network
