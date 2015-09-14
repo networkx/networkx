4a105978d6529586588aeb4c72a0fd9e3863979c
Aitor Almeida <aitoralmeida@gmail.com>
2015-09-14 16:23:11 +0200

    Simplified some of the code. Now uses nx.coloring.greedy_color instead of the add-hoc coloring algorithm. Improved the documentation.

--- networkx/algorithms/community/label_propagation.py
+++ networkx/algorithms/community/label_propagation.py
@@ -9,15 +9,17 @@ Semi-synchronous label propagation community detection algorithm.
 """
 
 from collections import Counter
+import networkx as nx
 from networkx.utils.decorators import not_implemented_for
+
 
 __all__ = ["label_propagation_communities"]
 
 @not_implemented_for('directed')
 def label_propagation_communities(G):
-    """Finds communities in graph using the label propagation method[1]_. This
-    method uses the diffusion of information in the network to identify
-    communities. Not implemented for directed graphs.
+    """Finds communities in graph using a semi-synchronous label propagation 
+    method[1]_. This method combines the advantages of both the synchronous and  
+    asynchronous models. Not implemented for directed graphs.
     
     Parameters
     ----------
@@ -45,7 +47,7 @@ def label_propagation_communities(G):
     # Create a unique label for each node in the graph
     labeling = {v: k for k, v in enumerate(G)}
     while not _labeling_complete(labeling, G):
-        # Update the labels of every node.
+        # Update the labels of every node with the same color.
         for color, nodes in coloring.items():
             for n in nodes:
                 _update_label(n , labeling, G)
@@ -59,24 +61,12 @@ def _color_network(G):
        a tuple in that order.
     """
     coloring = dict() # color => set(node)
-    lookup = dict() # node => color
-    finally_colored_nodes = set()
-    
-    for n in G:
-        if n not in lookup:
-            color = 0                 
+    colors = nx.coloring.greedy_color(G)
+    for node, color in colors.items():
+        if color in coloring:
+            coloring[color].add(node)
         else:
-            color = lookup[n]
-        lookup[n] = color
-        finally_colored_nodes.add(n)
-        if color not in coloring: 
-            coloring[color] = set()
-        coloring[color].add(n)
-        for q in G.neighbors(n):
-            if q not in finally_colored_nodes: 
-                lookup[q] = lookup[n] + 1
-    
-    return coloring
+            coloring[color] = set([node])
 
 def _labeling_complete(labeling, G):
     """Determines whether or not LPA is done. It is complete when all nodes 
@@ -87,10 +77,10 @@ def _labeling_complete(labeling, G):
        labeled.
     """    
     return all(labeling[v] in 
-                    _select_labels_of_highest_frequency(v, labeling, G) 
+                    _most_frequent_labels(v, labeling, G) 
                         for v in G if len(G[v]) > 0)
 
-def _select_labels_of_highest_frequency(node, labeling, G):
+def _most_frequent_labels(node, labeling, G):
     """Finds all labels of maximum frequency. Specified freqs must be a mapping
        from label to frequency of that label.          
 
@@ -100,11 +90,11 @@ def _select_labels_of_highest_frequency(node, labeling, G):
         # Nodes with no neighbors are themselves a community and are labeled
         # accordingly, hence the immediate if statement.
         return {labeling[node]}
-    else:
-        # Compute the frequencies of each node label in the graph    
-        freqs = dict(Counter(labeling[q] for q in G[node]))
-        max_freq = max(freqs.values())
-        return {label for label, freq in freqs.items() if freq == max_freq}        
+
+    # Compute the frequencies of all neighbours of node    
+    freqs = Counter(labeling[q] for q in G[node])
+    max_freq = max(freqs.values())
+    return {label for label, freq in freqs.items() if freq == max_freq}        
 
            
 def _update_label(node, labeling, G):
@@ -112,9 +102,9 @@ def _update_label(node, labeling, G):
        algorithm, as explained in: 'Community Detection via Semi-Synchronous 
        Label Propagation Algorithms' Cordasco and Gargano, 2011
     """
-    high_labels = _select_labels_of_highest_frequency(node, labeling, G)
+    high_labels = _most_frequent_labels(node, labeling, G)
     if len(high_labels) == 1:
         labeling[node] = high_labels.pop()
     elif len(high_labels) > 1:
         # Prec-Max
-        labeling[node] = labeling[node] if not high_labels or labeling[node] in high_labels else max(high_labels)
+        labeling[node] = labeling[node] if labeling[node] in high_labels else max(high_labels)
