68cbd62c0fda35612d3822dcb2b8456b83fb6e08
Balazs Kossovics <kossovics@gmail.com>
2015-10-27 19:42:18 +0100

    removing prints, adding some error handling

--- networkx/algorithms/isomorphism/tree_isomorph.py
+++ networkx/algorithms/isomorphism/tree_isomorph.py
@@ -4,29 +4,27 @@ Tree isomorphism functions.
 import networkx as nx
 from networkx.exception import NetworkXError
 
-import matplotlib
-# Force matplotlib to not use any Xwindows backend.
-matplotlib.use('Agg')
-
-
-import matplotlib
-import matplotlib.pyplot
-import matplotlib.pyplot as plt
-
 __author__ = """\n""".join(['Balazs Kossovics (kossovics@gmail.com)',
     'Jeffrey Finkelstein (jeffrey.finkelstein@gmail.com)'])
 
 __all__ = ['rooted_tree_is_isomorphic',
            'tree_is_isomorphic']
 
-
-
+# based on http://crypto.cs.mcgill.ca/~crepeau/CS250/2004/HW5+.pdf
+#   and jfinkels' comment
+# usage:
+# T1 = nx.random_powerlaw_tree(10, tries=1000)
+# T2 = nx.random_powerlaw_tree(10, tries=1000)
+# print tree_is_isomorphic(T1, T2)
 
 def canonical_form(T, root, _from=None):
-    children = filter(lambda node: node != _from, T.neighbors(root))
-    if len(children) == 0:
-        return ()
-    return tuple(sorted(canonical_form(T, v, root) for v in children))
+    try:
+        children = filter(lambda node: node != _from, T[root])
+        if len(children) == 0:
+            return ()
+        return tuple(sorted(canonical_form(T, v, root) for v in children))
+    except KeyError:
+        raise NetworkXError("Graph {} contains no node {}".format(T, root))
 
 def rooted_tree_is_isomorphic(T1, T2, r1, r2):
     if not nx.is_tree(T1):
@@ -43,9 +41,8 @@ def tree_is_isomorphic(T1, T2):
         return not T1
 
     r2 = nx.center(T2)[0]
-    print "first", T2, "center", r2
     for r1 in nx.center(T1):
-        print "second", T1, "center", r1
         if rooted_tree_is_isomorphic(T1, T2, r1, r2): 
             return True
     return False
+
