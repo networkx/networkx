ec8dee48ab745094a3da46aab9ec25a3c1a185f8
Kartik Raj <kartikr@iitk.ac.in>
2015-12-29 18:54:58 +0530

    Modified release.py

--- networkx/release.py
+++ networkx/release.py
@@ -210,7 +210,7 @@ classifiers = [
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules',
