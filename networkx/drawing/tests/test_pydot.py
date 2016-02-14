7bfb768e2bea7f08bcd6a907a42e9c5ad2ffaa2a
Michael-E-Rose <Michael.Ernst.Rose@gmail.com>
2016-02-14 20:16:55 +0100

    Improve drawing test scripts (typos, newlines, methods)

--- networkx/drawing/tests/test_pydot.py
+++ networkx/drawing/tests/test_pydot.py
@@ -1,14 +1,11 @@
-"""
-    Unit tests for pydot drawing functions.
-"""
+"""Unit tests for pydot drawing functions."""
 import os
 import tempfile
-
 from nose import SkipTest
 from nose.tools import assert_true
-
 import networkx as nx
 from networkx.testing import assert_graphs_equal
+
 
 class TestPydot(object):
     @classmethod
@@ -20,34 +17,27 @@ class TestPydot(object):
             raise SkipTest('pydotplus not available.')
 
     def pydot_checks(self, G):
-        G.add_edge('A','B')
-        G.add_edge('A','C')
-        G.add_edge('B','C')
-        G.add_edge('A','D')
+        G.add_edges_from([('A','B'),('A','C'),('B','C'),('A','D')])
         G.add_node('E')
         P = nx.nx_pydot.to_pydot(G)
         G2 = G.__class__(nx.nx_pydot.from_pydot(P))
         assert_graphs_equal(G, G2)
 
         fname = tempfile.mktemp()
-        assert_true( P.write_raw(fname) )
-
+        assert_true(P.write_raw(fname))
         Pin = pydotplus.graph_from_dot_file(fname)
 
         n1 = sorted([p.get_name() for p in P.get_node_list()])
         n2 = sorted([p.get_name() for p in Pin.get_node_list()])
-        assert_true( n1 == n2 )
+        assert_true(n1 == n2)
 
-        e1=[(e.get_source(),e.get_destination()) for e in P.get_edge_list()]
-        e2=[(e.get_source(),e.get_destination()) for e in Pin.get_edge_list()]
-        assert_true( sorted(e1)==sorted(e2) )
+        e1 = [(e.get_source(),e.get_destination()) for e in P.get_edge_list()]
+        e2 = [(e.get_source(),e.get_destination()) for e in Pin.get_edge_list()]
+        assert_true(sorted(e1) == sorted(e2))
 
         Hin = nx.nx_pydot.read_dot(fname)
         Hin = G.__class__(Hin)
         assert_graphs_equal(G, Hin)
-
-#        os.unlink(fname)
-
 
     def testUndirected(self):
         self.pydot_checks(nx.Graph())
