f9371a7317088962c192b6bd0715b5b88d3b2474
Ianto Lin Xi <ianto@kite.com>
2016-02-22 17:06:41 -0800

    simple short flask server to deliver example

--- examples/javascript/force.py
+++ examples/javascript/force.py
@@ -11,6 +11,7 @@
 import json
 import networkx as nx
 from networkx.readwrite import json_graph
+import flask
 
 G = nx.barbell_graph(6,3)
 # this d3 example uses the name attribute for the mouse-hover value,
@@ -22,4 +23,12 @@ d = json_graph.node_link_data(G) # node-link format to serialize
 # write json
 json.dump(d, open('force/force.json','w'))
 print('Wrote node-link JSON data to force/force.json')
-print('Open force/force.html in a web browser')
+
+# Serve the file over http to allow for cross origin requests
+app = flask.Flask(__name__, static_folder="force")
+
+@app.route('/<path:path>')
+def static_proxy(path):
+  return app.send_static_file(path)
+print('\nGo to http://localhost:8000/force.html to see the example\n')
+app.run(port=8000)
