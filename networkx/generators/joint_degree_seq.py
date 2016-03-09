862e252adba8a31d36158a635400a54249940e0f
Minas Gjoka <minas.gjoka@gmail.com>
2016-03-08 18:12:09 -0800

    Improved description of docstrings in functions and naming of variables

--- networkx/generators/joint_degree_seq.py
+++ networkx/generators/joint_degree_seq.py
@@ -15,105 +15,114 @@ __all__ = ['is_valid_joint_degree',
            'joint_degree_model']
 
 
-def is_valid_joint_degree(nkk):
-    """ Checks whether the given joint degree dictionary (*nkk*) is realizable
-        as a simple graph by evaluating five necessary and sufficient
-        conditions.
+def is_valid_joint_degree(joint_degrees):
+    """ Checks whether the given joint degree dictionary is realizable
+    as a simple graph.
 
-    Here is the list of five conditions that an input nkk needs to satisfy for
-    simple graph realizability:
-    - Condition    1: nkk[k][l]  is integer for all k,l
-    - Condition    2: sum(nkk[k])/k = number of nodes with degree k, is an
-                      integer
-    - Conditions 3,4: number of edges between k and l cannot exceed maximum
-                      possible number of edges
-    - Condition    5: nkk[k][k] is an even integer i.e. stubs are counted
-                      twice for equal degree pairs. this is an assumption that
-                      the method joint_degree_model expects to be true.
+    A *joint degree dictionary* is a dictionary of dictionaries, in
+    which entry ``joint_degrees[k][l]`` is an integer representing the
+    number of edges joining nodes of degree *k* with nodes of degree
+    *l*. Such a dictionary is realizable as a simple graph if and only
+    if the following conditions are satisfied.
+
+    - each entry must be an integer,
+    - the total number of nodes of degree *k*, computed by
+      ``sum(joint_degrees[k].values()) / k``, must be an integer,
+    - the total number of edges joining nodes of degree *k* with
+       nodes of degree *l* cannot exceed the total number of possible edges,
+    - each diagonal entry ``joint_degrees[k][k]`` must be even (this is
+      a convention assumed by the :func:`joint_degree_model` function).
+
 
     Parameters
     ----------
-    nkk :  dictionary of dictionary of integers
-        joint degree dictionary. for nodes of degree k (1st level of dict) and
-        nodes of degree l (2nd level of dict) describes the number of edges
+    joint_degrees :  dictionary of dictionary of integers
+        A joint degree dictionary in which entry ``joint_degrees[k][l]``
+        is the number of edges joining nodes of degree *k* with nodes of
+        degree *l*.
 
     Returns
     -------
-    boolean:
-        returns true if given joint degree dictionary is realizable, else
-        returns false.
+    bool
+        Whether the given joint degree dictionary is realizable as a
+        simple graph.
 
     References
     ----------
-    ..  [1] M. Gjoka, M. Kurant, A. Markopoulou, "2.5K Graphs: from Sampling
-        to Generation", IEEE Infocom, 2013.
+    .. [1] M. Gjoka, M. Kurant, A. Markopoulou, "2.5K Graphs: from Sampling
+       to Generation", IEEE Infocom, 2013.
+    .. [2] I. Stanton, A. Pinar, "Constructing and sampling graphs with a
+       prescribed joint degree distribution", Journal of Experimental
+       Algorithmics, 2012.
     """
 
-    nk = {}
-    for k in nkk:
+    degree_count = {}
+    for k in joint_degrees:
         if k > 0:
-            k_size = sum(nkk[k].values()) / k
-            if not k_size.is_integer():  # condition 2
+            k_size = sum(joint_degrees[k].values()) / k
+            if not k_size.is_integer():
                 return False
-            nk[k] = k_size
+            degree_count[k] = k_size
 
-    for k in nkk:
-        for l in nkk[k]:
-            if not float(nkk[k][l]).is_integer(): # condition 1
+    for k in joint_degrees:
+        for l in joint_degrees[k]:
+            if not float(joint_degrees[k][l]).is_integer():
                 return False
 
-            if (k != l) and (nkk[k][l] > nk[k]*nk[l]): #condition 3
+            if (k != l) and (joint_degrees[k][l] >
+                             degree_count[k]*degree_count[l]):
                 return False
             elif k == l:
-                if nkk[k][k] > nk[k]*(nk[k]-1): # condition 4
+                if joint_degrees[k][k] > degree_count[k]*(degree_count[k]-1):
                     return False
-                if nkk[k][k] % 2 != 0: # condition 5
+                if joint_degrees[k][k] % 2 != 0:
                     return False
 
 
-    # if all five above conditions have been satisfied then the input nkk is
-    # realizable as a simple graph.
+    # if all above conditions have been satisfied then the input
+    # joint degree is realizable as a simple graph.
     return True
 
 def _neighbor_switch(G, w, unsat, h_node_residual, avoid_node_id=None):
-    """ neighbor_switch  releases one free stub for node *w*, while preserving
-    joint degree in G. First, it selects node w_prime that (1) has the same
-    degree as *w* and (2) is unsaturated. Then, it selects node t, a neighbor
-    of *w*, that is not connected to w_prime and does an edge swap i.e. removes
-    (*w*,t) and adds (w_prime,t). Gjoka et. al. [1] prove that if (1) and (2)
-    are true then such an edge swap is always possible.
+    """ Releases one free stub for saturated node ``w``, while preserving
+    joint degree in graph G.
 
     Parameters
     ----------
-    G : networkx undirected graph
-        graph within which the edge swap will take place
+    G : NetworkX graph
+        Graph in which the neighbor switch will take place.
     w : integer
-        node id for which we need to perform a neighbor switch
-    unsat: set of integers
-        set of node ids that have the same degree as w and are unsaturated
-    h_node_residual: dict of integers
-        for a given node, keeps track of the remaining stubs to be added.
+        Node id for which we will execute this neighbor switch.
+    unsat : set of integers
+        Set of unsaturated node ids that have the same degree as w.
+    h_node_residual: dictionary of integers
+        Keeps track of the remaining stubs  for a given node.
     avoid_node_id: integer
-        node id to avoid when selecting w_prime. used for a rare edge case.
+        Node id to avoid when selecting w_prime.
+
+    Notes
+    -----
+    First, it selects *w_prime*, an  unsaturated node that has the same degree
+    as ``w``. Second, it selects *switch_node*, a neighbor node of ``w`` that
+    is not  connected to *w_prime*. Then it executes an edge swap i.e. removes
+    (``w``,*switch_node*) and adds (*w_prime*,*switch_node*). Gjoka et. al. [1]
+    prove that such an edge swap is always possible.
 
     References
     ----------
-    ..  [1] M. Gjoka, B. Tillman, A. Markopoulou, "Construction of Simple
-        Graphs with a Target Joint Degree Matrix and Beyond", IEEE Infocom, '15
+    .. [1] M. Gjoka, B. Tillman, A. Markopoulou, "Construction of Simple
+       Graphs with a Target Joint Degree Matrix and Beyond", IEEE Infocom, '15
     """
 
-    if (avoid_node_id == None) or (h_node_residual[avoid_node_id] > 1):
-        # select node w_prime that has the same degree as w and is unsatured
+    if (avoid_node_id is None) or (h_node_residual[avoid_node_id] > 1):
+        # select unsatured node w_prime that has the same degree as w
         w_prime = next(iter(unsat))
     else:
-        # assume that inside method joint_degree_model the node pair (v,w) is a
-        # candidate for connection (v=avoid_node_id). if neighbor_switch is
-        # called for node w inside method joint_degree_model and (1) candidate
-        # nodes v=avoid_node_id and w have the same degree i.e.
-        # degree(v)=degree(w) and (2) node v=avoid_node_id is a potential
-        # candidate for w_prime but has only  one stub left i.e.
-        # h_node_residual[v]==1, then prevent v from being selected as w_prime.
-        # This is a rare edge case.
+        # assume that the node pair (v,w) has been selected for connection. if
+        # - neighbor_switch is called for node w,
+        # - nodes v and w have the same degree,
+        # - node v=avoid_node_id has only one stub left,
+        # then prevent v=avoid_node_id from being selected as w_prime.
 
         iter_var = iter(unsat)
         while True:
@@ -121,102 +130,101 @@ def _neighbor_switch(G, w, unsat, h_node_residual, avoid_node_id=None):
             if w_prime != avoid_node_id:
                 break
 
-    # select node t, a neighbor of w, that is not connected to w_prime
+    # select switch_node, a neighbor of w, that is not connected to w_prime
     w_prime_neighbs = G[w_prime] # slightly faster declaring this variable
     for v in G[w]:
         if (v not in w_prime_neighbs) and (v != w_prime):
-            t = v
+            switch_node = v
             break
 
-    # removes (w,t), add (w_prime,t)  and update data structures
-    G.remove_edge(w, t)
-    G.add_edge(w_prime, t)
+    # remove edge (w,switch_node), add edge (w_prime,switch_node) and update
+    # data structures
+    G.remove_edge(w, switch_node)
+    G.add_edge(w_prime, switch_node)
     h_node_residual[w] += 1
     h_node_residual[w_prime] -= 1
     if h_node_residual[w_prime] == 0:
         unsat.remove(w_prime)
 
-def joint_degree_model(nkk, seed=None):
-    """Return a random simple graph with the given joint degree dict (*nkk*).
+def joint_degree_model(joint_degrees, seed=None):
+    """ Generates a random simple graph with the given joint degree dictionary.
 
     Parameters
     ----------
-    nkk :  dictionary of dictionary of integers
-        joint degree dictionary. for nodes of degree k (1st level of dict)
-        and nodes of degree l (2nd level of dict) describes the number of edges
+    joint_degrees :  dictionary of dictionary of integers
+        A joint degree dictionary in which entry ``joint_degrees[k][l]`` is the
+        number of edges joining nodes of degree *k* with nodes of degree *l*.
     seed : hashable object, optional
         Seed for random number generator.
 
     Returns
     -------
     G : Graph
-        A graph with the specified joint degree dict
+        A graph with the specified joint degree dictionary.
 
     Raises
     ------
     NetworkXError
-        If *nkk*, the joint degree dictionary, is not realizable.
-
+        If *joint_degrees* dictionary is not realizable.
 
     Notes
     -----
     In each iteration of the "while loop" the algorithm picks two disconnected
-    nodes v and w, of degree k and l correspondingly,  for which nkk[k][l] has
-    not reached its target yet  i.e. (for given k,l): n_edges_add < nkk[k][l].
-    It then adds edge (v,w) and always increases the number of edges in graph
-    G by one.
+    nodes *v* and *w*, of degree *k* and *l* correspondingly,  for which
+    ``joint_degrees[k][l]`` has not reached its target yet. It then adds
+    edge (*v*, *w*) and increases the number of edges in graph G by one.
 
     The intelligence of the algorithm lies in the fact that  it is always
-    possible to add an edge between disconnected nodes v and w, for which
-    nkk[degree(v)][degree(w)] has not reached its target, even if one or both
-    nodes do not have free stubs. If either node v or w does not have a free
-    stub, we perform a "neighbor switch", an edge rewiring move that releases
-    a free stub while keeping *nkk* the same.
+    possible to add an edge between such disconnected nodes *v* and *w*,
+    even if one or both nodes do not have free stubs. That is made possible by
+    executing a "neighbor switch", an edge rewiring move that releases
+    a free stub while keeping the joint degree of G the same.
 
-    The algorithm continues for E (number of edges in the graph) iterations of
-    the "while loop", at the which point all entries of the given nkk[k][l]
-    have reached their target values and the construction is complete.
+    The algorithm continues for E (number of edges) iterations of
+    the "while loop", at the which point all entries of the given
+    ``joint_degrees[k][l]`` have reached their target values and the
+    construction is complete.
 
     References
     ----------
     ..  [1] M. Gjoka, B. Tillman, A. Markopoulou, "Construction of Simple
-        Graphs with a Target Joint Degree Matrix and Beyond", IEEE Infocom, '15
+        Graphs with a Target Joint Degree Matrix and Beyond", IEEE Infocom, '15.
 
     Examples
     --------
     >>> import networkx as nx
-    >>> joint_degree_dict = {1: {4: 1},
+    >>> joint_degrees = {1: {4: 1},
     ...                      2: {2: 2, 3: 2, 4: 2},
     ...                      3: {2: 2, 4: 1},
     ...                      4: {1: 1, 2: 2, 3: 1}}
-    >>> G=nx.joint_degree_model(joint_degree_dict)
+    >>> G=nx.joint_degree_model(joint_degrees)
     >>>
     """
 
-    if not is_valid_joint_degree(nkk):
+    if not is_valid_joint_degree(joint_degrees):
         msg = 'Input joint degree dict not realizable as a simple graph'
         raise nx.NetworkXError(msg)
 
-    if not seed is None:
+    if seed is not None:
         random.seed(seed)
 
-    # compute degree vector from joint degree nkk
-    nk = {k: sum(l.values())//k for k, l in nkk.items() if k > 0}
+    # compute degree count from joint_degrees
+    degree_count = {k: sum(l.values())//k for k, l in joint_degrees.items() if k > 0}
 
     # start with empty N-node graph
-    N = sum(nk.values())
+    N = sum(degree_count.values())
     G = nx.empty_graph(N)
 
 
     # for a given degree group, keep the list of all node ids
     h_degree_nodelist = {}
 
-    # for a given node, keep track of the remaining stubs to be added.
+    # for a given node, keep track of the remaining stubs
     h_node_residual = {}
 
     # populate h_degree_nodelist and h_node_residual
     nodeid = 0
-    for degree, num_nodes in nk.items():
+    for degree, num_nodes in degree_count.items():
         h_degree_nodelist[degree] = range(nodeid, nodeid+num_nodes)
         for v in h_degree_nodelist[degree]:
             h_node_residual[v] = degree
@@ -224,18 +232,18 @@ def joint_degree_model(nkk, seed=None):
 
     # iterate over every degree pair (k,l) and add the number of edges given
     # for each pair
-    for k in nkk:
-        for l in nkk[k]:
+    for k in joint_degrees:
+        for l in joint_degrees[k]:
 
             # n_edges_add is the number of edges to add for the
             # degree pair (k,l)
-            n_edges_add = nkk[k][l]
+            n_edges_add = joint_degrees[k][l]
 
             if (n_edges_add > 0) and (k >= l):
 
                 # number of nodes with degree k and l
-                k_size = nk[k]
-                l_size = nk[l]
+                k_size = degree_count[k]
+                l_size = degree_count[l]
 
                 # k_nodes and l_nodes consist of all nodes of degree k and l
                 k_nodes = h_degree_nodelist[k]
@@ -249,7 +257,7 @@ def joint_degree_model(nkk, seed=None):
                     l_unsat = set(w for w in l_nodes if h_node_residual[w] > 0)
                 else:
                     l_unsat = k_unsat
-                    n_edges_add = nkk[k][l]//2
+                    n_edges_add = joint_degrees[k][l]//2
 
                 while n_edges_add > 0:
 
--- networkx/generators/tests/test_joint_degree_seq.py
+++ networkx/generators/tests/test_joint_degree_seq.py
@@ -6,36 +6,41 @@ from networkx.generators import powerlaw_cluster_graph
 
 def test_is_valid_joint_degree():
     # valid joint degree that satisfies all five conditions
-    nkk = {1: {4: 1},
-           2: {2: 2, 3: 2, 4: 2},
-           3: {2: 2, 4: 1},
-           4: {1: 1, 2: 2, 3: 1}}
-    assert_true(is_valid_joint_degree(nkk))
+    joint_degrees = {1: {4: 1},
+                     2: {2: 2, 3: 2, 4: 2},
+                     3: {2: 2, 4: 1},
+                     4: {1: 1, 2: 2, 3: 1}}
+    assert_true(is_valid_joint_degree(joint_degrees))
 
     # condition 1
-    nkk_1 = {1: {4: 1.5},          # nkk[1][4] not integer
-             2: {2: 2, 3: 2, 4: 2},
-             3: {2: 2, 4: 1},
-             4: {1: 1.5, 2: 2, 3: 1}}
-    assert_false(is_valid_joint_degree(nkk_1))
+    # joint_degrees_1[1][4] not integer
+    joint_degrees_1 = {1: {4: 1.5},
+                       2: {2: 2, 3: 2, 4: 2},
+                       3: {2: 2, 4: 1},
+                       4: {1: 1.5, 2: 2, 3: 1}}
+    assert_false(is_valid_joint_degree(joint_degrees_1))
 
-    # condition 2
-    nkk_2 = {1: {4: 1},
-             2: {2: 2, 3: 2, 4: 3},   # nk[2] = sum(nkk[2][j)/2, is not an int
-             3: {2: 2, 4: 1},
-             4: {1: 1, 2: 3, 3: 1}}    # nk[4] = sum(nkk[4][j)/4, is not an int
-    assert_false(is_valid_joint_degree(nkk_2))
+    # test condition 2
+    # nk[2] = sum(joint_degrees_2[2][j)/2, is not an int
+    # nk[4] = sum(joint_degrees_2[4][j)/4, is not an int
+    joint_degrees_2 = {1: {4: 1},
+                       2: {2: 2, 3: 2, 4: 3},
+                       3: {2: 2, 4: 1},
+                       4: {1: 1, 2: 3, 3: 1}}
+    assert_false(is_valid_joint_degree(joint_degrees_2))
 
-    # condition 3 and 4
-    nkk_3 = {1: {4: 2},              #nkk[1][4]>nk[1][nk[4] (not possible)
-             2: {2: 2, 3: 2, 4: 2},
-             3: {2: 2, 4: 1},
-             4: {1: 2, 2: 2, 3: 1}}
-    assert_false(is_valid_joint_degree(nkk_3))
+    # test conditions 3 and 4
+    # joint_degrees_3[1][4]>nk[1][nk[4]
+    joint_degrees_3 = {1: {4: 2},
+                       2: {2: 2, 3: 2, 4: 2},
+                       3: {2: 2, 4: 1},
+                       4: {1: 2, 2: 2, 3: 1}}
+    assert_false(is_valid_joint_degree(joint_degrees_3))
 
-    # condition 5
-    nkk_5 = {1:{1:9}}   # nkk[1][1] not even
-    assert_false(is_valid_joint_degree(nkk_5))
+    # test condition 5
+    # joint_degrees_5[1][1] not even
+    joint_degrees_5 = {1:{1:9}}
+    assert_false(is_valid_joint_degree(joint_degrees_5))
 
 def test_joint_degree_model(ntimes=100):
     for _ in range(ntimes):
@@ -45,12 +50,13 @@ def test_joint_degree_model(ntimes=100):
         # generate random graph with model powerlaw_cluster and calculate
         # its joint degree
         g = powerlaw_cluster_graph(n, m, p, seed=seed)
-        nkk_g = degree_mixing_dict(g, normalized=False)
+        joint_degrees_g = degree_mixing_dict(g, normalized=False)
 
-        # generate simple undirected graph with given joint degree nkk_g
-        G = joint_degree_model(nkk_g)
-        nkk_G = degree_mixing_dict(G, normalized=False)
+        # generate simple undirected graph with given joint degree
+        # joint_degrees_g
+        G = joint_degree_model(joint_degrees_g)
+        joint_degrees_G = degree_mixing_dict(G, normalized=False)
 
         # assert that the given joint degree is equal to the generated
         # graph's joint degree
-        assert_true(nkk_g == nkk_G)
+        assert_true(joint_degrees_g == joint_degrees_G)
