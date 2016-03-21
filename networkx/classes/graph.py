1e94d74ac9db0b0d4cb43be9e945e1e82869bee1
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2016-03-21 02:01:16 -0400

    Adds example for using Graph.nodes() with default

--- networkx/classes/graph.py
+++ networkx/classes/graph.py
@@ -656,6 +656,19 @@ class Graph(object):
         [(0, None), (1, '5pm'), (2, None)]
         >>> list(G.nodes(data='time', default='Not Available'))
         [(0, 'Not Available'), (1, '5pm'), (2, 'Not Available')]
+
+        If some of your nodes have an attribute and the rest are assumed
+        to have a default attribute value you can create a dictionary
+        from node/attribute pairs using the `default` keyword argument
+        to guarantee the value is never None::
+
+            >>> G = nx.Graph()
+            >>> G.add_node(0)
+            >>> G.add_node(1, weight=2)
+            >>> G.add_node(2, weight=3)
+            >>> dict(G.nodes(data='weight', default=1))
+            {0: 1, 1: 2, 2: 3}
+
         """
         if data is True:
             for n, ddict in self.node.items():
@@ -667,7 +680,6 @@ class Graph(object):
         else:
             for n in self.node:
                 yield n
-
 
     def number_of_nodes(self):
         """Return the number of nodes in the graph.
