1ac68b6f7b70e65dcd340e95ccf14a147285474c
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2016-04-14 22:37:43 -0400

    Removes if __name__ == '__main__': guard

--- examples/algorithms/kirchhoffs_circuit_laws.py
+++ examples/algorithms/kirchhoffs_circuit_laws.py
@@ -3,7 +3,8 @@
 #
 # kirchhoffs_circuit_laws.py - using NetworkX for Kirchhoff's circuit laws
 #
-# Copyright 2015 NetworkX developers.
+# Copyright 2015 JuanPi Carbajal <juanpi+dev@gmail.com>
+# Copyright 2015–2016 NetworkX developers.
 #
 # This file is part of NetworkX.
 #
@@ -12,47 +13,43 @@
 """Example of creating Kirchhoff's circuit laws from a (directed) multigraph.
 
 """
-__author__ = """\n""".join(['JuanPi Carbajal <juanpi+dev@gmail.com>',
-                            'Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>'])
-
 import networkx as nx
 
-if __name__ == "__main__":
-    # Create a graph representing three resistors in series.
-    resistors = [(1, 2, "R0", {"volt": "V0", "curr": "I0"}),
-                 (1, 2, "R1", {"volt": "V1", "curr": "I1"}),
-                 (1, 2, "R2", {"volt": "V2", "curr": "I2"}),
-                 (1, 3, "c0", {"volt": "Vcc", "curr": "Icc"}),
-                 (3, 2, "c1", {"volt": "Vss", "curr": "Iss"})]
-    G = nx.MultiGraph(resistors)
-    volt = [e[2]["volt"] for e in G.edges(data=True)]
-    curr = [e[2]["curr"] for e in G.edges(data=True)]
+# Create a graph representing three resistors in series.
+resistors = [(1, 2, "R0", {"volt": "V0", "curr": "I0"}),
+             (1, 2, "R1", {"volt": "V1", "curr": "I1"}),
+             (1, 2, "R2", {"volt": "V2", "curr": "I2"}),
+             (1, 3, "c0", {"volt": "Vcc", "curr": "Icc"}),
+             (3, 2, "c1", {"volt": "Vss", "curr": "Iss"})]
+G = nx.MultiGraph(resistors)
+volt = [voltage for u, v, voltage in G.edges(data="volt")]
+curr = [current for u, v, current in G.edges(data="curr")]
 
-    # Kirchhoff's voltage law states that the sum of all voltages in a
-    # resistor network is zero. Each element of the list `KVL` stores a
-    # string representation of an equation demonstrating this law.
-    KVL = []
-    M = nx.cycle_basis_matrix(G).T
-    for row in M:
-        ind = row.nonzero()[0]
-        token = "".join(" {} {}".format("+" if row[j] > 0 else "-", volt[j])
-                        for j in ind)
-        KVL.append(token + " = 0")
+# Kirchhoff's voltage law states that the sum of all voltages in a
+# resistor network is zero. Each element of the list `KVL` stores a
+# string representation of an equation demonstrating this law.
+KVL = []
+M = nx.cycle_basis_matrix(G).T
+for row in M:
+    ind = row.nonzero()[0]
+    token = "".join(" {} {}".format("+" if row[j] > 0 else "-", volt[j])
+                    for j in ind)
+    KVL.append(token + " = 0")
 
-    # Kirchhoff's current law states that the sum of currents flowing
-    # into a node equals the sum of currents flowing out of a node. Each
-    # element of the list `KCL` stores a string representation of an
-    # equation demonstrating this law.
-    KCL = []
-    M = nx.incidence_matrix(G, oriented=True).toarray()[:-1]
-    for row in M:
-        ind = row.nonzero()[0]
-        token = "".join(" {} {}".format("+" if row[j] > 0 else "-", curr[j])
-                        for j in ind)
-        KCL.append(token + " = 0")
+# Kirchhoff's current law states that the sum of currents flowing
+# into a node equals the sum of currents flowing out of a node. Each
+# element of the list `KCL` stores a string representation of an
+# equation demonstrating this law.
+KCL = []
+M = nx.incidence_matrix(G, oriented=True).toarray()[:-1]
+for row in M:
+    ind = row.nonzero()[0]
+    token = "".join(" {} {}".format("+" if row[j] > 0 else "-", curr[j])
+                    for j in ind)
+    KCL.append(token + " = 0")
 
-    print("Kirchhoff's laws for three resistors in series:")
-    for eq in KVL:
-        print(eq)
-    for eq in KCL:
-        print(eq)
+print("Kirchhoff's laws for three resistors in series:")
+for eq in KVL:
+    print(eq)
+for eq in KCL:
+    print(eq)
