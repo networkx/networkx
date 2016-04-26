e24eb6ca984309248619b379bb473ce25fd245ea
thegreathippo <thegreathippo@gmail.com>
2016-04-26 18:02:14 -0400

    Remove attr_dict calls to graph's add_node; replace with **.
    Combing through the code for any point where it pushes a dictionary to
    the add_node method rather than keywords; augmenting these arguments
    with ** to turn them into keyword arguments. NOTE: This was a mistake;
    I later overturned this commit, and squashed it just to get rid of some
    of the nonsense.
    
    Modified all add_node, add_edge, and add_edges_from methods.
    
    I also went through and modified all attr_dict references to either
    directly assign keywords to node/edge attributes, or use the ** syntax
    to unpack the dictionaries. The only case of these I couldn't do is
    networkx.convert.py -- largely because I don't understand how multigraph
    edges work.

--- networkx/classes/graph.py
+++ networkx/classes/graph.py
@@ -853,6 +853,47 @@ class Graph(object):
         >>> G.add_edges_from([(1,2),(2,3)], weight=3)
         >>> G.add_edges_from([(3,4),(1,4)], label='WN2898')
         """
+    def add_edges_from(self, ebunch, attr_dict=None, **attr):
+        """Add all the edges in ebunch.
+
+        Parameters
+        ----------
+        ebunch : container of edges
+            Each edge given in the container will be added to the
+            graph. The edges must be given as as 2-tuples (u,v) or
+            3-tuples (u,v,d) where d is a dictionary containing edge data.
+        attr_dict : dictionary, optional (default= no attributes)
+            Dictionary of edge attributes.  Key/value pairs will
+            update existing data associated with each edge.
+        attr : keyword arguments, optional
+            Edge data (or labels or objects) can be assigned using
+            keyword arguments.
+
+        See Also
+        --------
+        add_edge : add a single edge
+        add_weighted_edges_from : convenient way to add weighted edges
+
+        Notes
+        -----
+        Adding the same edge twice has no effect but any edge data
+        will be updated when each duplicate edge is added.
+
+        Edge attributes specified in edges take precedence
+        over attributes specified generally.
+
+        Examples
+        --------
+        >>> G = nx.Graph()   # or DiGraph, MultiGraph, MultiDiGraph, etc
+        >>> G.add_edges_from([(0,1),(1,2)]) # using a list of edge tuples
+        >>> e = zip(range(0,3),range(1,4))
+        >>> G.add_edges_from(e) # Add the path graph 0-1-2-3
+
+        Associate data to edges
+
+        >>> G.add_edges_from([(1,2),(2,3)], weight=3)
+        >>> G.add_edges_from([(3,4),(1,4)], label='WN2898')
+        """
         # set up attribute dict
         edge_attr = {}
         if attr_dict is not None:
