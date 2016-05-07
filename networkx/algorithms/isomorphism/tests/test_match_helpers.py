cf6075a59e633371687d72e03e142722ead1cefc
René Saitenmacher <saitenmacher@zib.de>
2016-05-07 18:10:46 +0200

    Add unit test for unification of generic_multiedge_match

--- networkx/algorithms/isomorphism/tests/test_match_helpers.py
+++ networkx/algorithms/isomorphism/tests/test_match_helpers.py
@@ -35,7 +35,13 @@ class TestGenericMultiEdgeMatch:
     def test_generic_multiedge_match(self):
         full_match = iso.generic_multiedge_match(['id', 'flowMin', 'flowMax'], [None]*3, [eq]*3)
         flow_match = iso.generic_multiedge_match(['flowMin', 'flowMax'], [None]*2, [eq]*2)
+        min_flow_match = iso.generic_multiedge_match('flowMin', None, eq)
+        id_match = iso.generic_multiedge_match('id', None, eq) 
         assert_true(flow_match(self.G1[1][2], self.G2[2][3]))
+        assert_true(min_flow_match(self.G1[1][2], self.G2[2][3]))
+        assert_true(id_match(self.G1[1][2], self.G2[2][3]))
         assert_true(full_match(self.G1[1][2], self.G2[2][3]))
         assert_true(flow_match(self.G3[3][4], self.G4[4][5]))
+        assert_true(min_flow_match(self.G3[3][4], self.G4[4][5]))
+        assert_false(id_match(self.G3[3][4], self.G4[4][5]))
         assert_false(full_match(self.G3[3][4], self.G4[4][5]))
