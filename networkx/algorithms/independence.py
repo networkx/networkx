5405706df18302a7e0afe2ea21c79c1a011db764
NareshPeshwe <nareshpeshwe12078@gmail.com>
2016-07-21 08:07:06 +0530

    Formatted according to PEP8 conventions, combined both functions to one

--- networkx/algorithms/independence.py
+++ networkx/algorithms/independence.py
@@ -1,52 +1,38 @@
-import networkx as nx
-from networkx.algorithms import *
-
+from networkx.algorithms.clique import enumerate_all_cliques
+from networkx.algorithms.operators.unary import complement
 from networkx.utils import not_implemented_for
-__author__ = """Naresh Peshwe (nareshpeshwe12078@gmail.com)"""
-__all__ = ['enumerate_all_independentSets', 'independence_number']
+
+__all__ = ['independence_number_sets']
 
 
 @not_implemented_for('directed')
-def enumerate_all_independentSets(G):
-	""" Returns an iterator object over the independent sets of nodes of the graph 'G'.
+def independence_number_and_sets(G, independentsets=None):
+    """ Returns the independence number and the independent sets in an undirected graph.
 
-		Parameters
-		----------
-		G : Networkx graph
-			An undirected graph.
+    An independent set is a set of vertices in a graph, no two of which are adjacent. 
+    A maximum independent set is an independent set of largest possible size for a given graph G. 
+    This size is called the independence number of G.
+    
+    Parameters
+    ----------
+    G : NetworkX graph
+        An undirected graph.
 
-		Returns
-		-------
-		Iterator
+    independentsets :	List
+                        A list of independent sets each of which is a list of nodes in
+                        'G'. If not specified, the list of all independent sets is computed.
+    Returns
+    -------
+    tuple : Containing the independence number (int) and the independent sets, a list each of which of is a 
+            list of nodes in 'G'.
 
-		Returns an iterator over the all the independent set of nodes in the given graph 'G'.
+    Notes
+    -----
+    The fact that the cliques of the complement graph of G correspond to the independent sets of the graph G is
+    being used for computing the independent sets.
 
-		Note : For finding the independent sets we use the fact that 
-		independent sets of a graph G are the cliques corresponding 
-		to its complement.
+    """
+    if independentsets is None:
+        independentsets = list(enumerate_all_cliques(complement(G)))
 
-	"""
-	return enumerate_all_cliques(complement(G))
-	
-
-def independence_number(G, independentsets=None):
-	""" Returns the independence number of an undirected graph
-
-	Parameters
-	----------
-	G : NetworkX graph
-		An undirected graph.
-	Returns
-	-------
-	int 
-
-		The size of the largest independent set of nodes of 'G'.
-		A maximum independent set is an independent set of largest possible size for a given graph G. 
-		This size is called the independence number of G.
-
-
-	"""
-	if independentsets == None:
-		independentsets = enumerate_all_independentSets(G)
-	return max([len(i) for i in independentsets])
-
+    return max([len(i) for i in independentsets]), independentsets
