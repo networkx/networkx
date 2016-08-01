381857d9a439b17603e16b524176381df759e2a8
mcognetta <cognetta.marco@gmail.com>
2016-08-01 19:59:56 -0400

    Fixed tests

--- networkx/generators/tests/test_mycielski.py
+++ networkx/generators/tests/test_mycielski.py
@@ -11,27 +11,27 @@
 
 from nose.tools import assert_true, assert_equal, raises
 import networkx as nx
-
+from networkx import *
 
 class TestMycielski(object):
 
     def test_construction(self):
         G = nx.path_graph(2)
-        M = mycielski(G)
+        M = mycielskian(G)
         assert_true(is_isomorphic(M, cycle_graph(5)))
 
     def test_size(self):
         G = nx.path_graph(2)
-        M = mycielski(G, 2)
-        assert_equals(len(M), 11)
-        assert_equals(M.size(), 20)
+        M = mycielskian(G, 2)
+        assert_equal(len(M), 11)
+        assert_equal(M.size(), 20)
 
     def test_mycielski_graph_generator(self):
         G = mycielski_graph(1)
         assert_true(is_isomorphic(G, nx.empty_graph(1)))
-        G = mycielski_graph(2):
+        G = mycielski_graph(2)
         assert_true(is_isomorphic(G, nx.path_graph(2)))
         G = mycielski_graph(3)
-        assert_true(is_isomorphic(M, cycle_graph(5)))
+        assert_true(is_isomorphic(G, cycle_graph(5)))
         G = mycielski_graph(4)
-        assert_true(is_isomorphic(G, mycielski(cycle_graph(5))))
+        assert_true(is_isomorphic(G, mycielskian(cycle_graph(5))))
