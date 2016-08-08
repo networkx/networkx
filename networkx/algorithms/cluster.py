330d48e9fd3e9a505a6c82f40d5f144ad73f215f
Janis Klaise <janis.klaise@gmail.com>
2016-08-08 15:40:58 +0100

    Fix docstring example spanning two lines

--- networkx/algorithms/cluster.py
+++ networkx/algorithms/cluster.py
@@ -393,8 +393,8 @@ def generalized_degree(G, nodes=None):
     >>> print(nx.generalized_degree(G,0))
     Counter({3: 4})
     >>> print(nx.generalized_degree(G))
-    {0: Counter({3: 4}), 1: Counter({3: 4}), 2: Counter({3: 4}),
-        3: Counter({3: 4}), 4: Counter({3: 4})}
+    {0: Counter({3: 4}), 1: Counter({3: 4}), 2: Counter({3: 4}), \
+3: Counter({3: 4}), 4: Counter({3: 4})}
 
     To recover the number of triangles attached to a node:
 
