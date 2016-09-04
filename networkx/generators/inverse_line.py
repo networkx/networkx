0d1f9c28b77f374ab27698a8318e9b82927ac195
James Clough <james.clough91@gmail.com>
2016-09-04 20:38:49 +0100

    Made suggested changes, added more test cases

--- networkx/generators/inverse_line.py
+++ networkx/generators/inverse_line.py
@@ -1,6 +1,4 @@
-"""Functions for generating inverse line graphs.
-
-"""
+"""Functions for generating inverse line graphs."""
 #    Copyright (C) 2016 by
 #    James Clough <james.clough91@gmail.com>
 #    All rights reserved.
@@ -10,12 +8,16 @@ __author__ = "\n".join(["James Clough (james.clough91@gmail.com)"])
 
 __all__ = ['inverse_line_graph']
 
-# TODO - is this the right place to put these?
 from itertools import combinations
 from collections import defaultdict
 
 import networkx as nx
-   
+from networkx.utils import arbitrary_element
+from networkx.utils.decorators import *
+
+
+@not_implemented_for('directed')
+@not_implemented_for('multigraph')
 def inverse_line_graph(G):
     """ Returns the inverse line graph of graph G.
     
@@ -28,13 +30,20 @@ def inverse_line_graph(G):
     Parameters
     ----------
     G : graph
-        A NetworkX Graph, DiGraph, MultiGraph, or MultiDigraph.
+        A NetworkX Graph
         
     Returns
     -------
     H : graph
         The inverse line graph of G.
     
+    Raises
+    ------
+    NetworkXNotImplemented
+        If G is directed or a multigraph
+        
+    NetworkXError
+        If G is not a line graph
     
     Notes
     -----
@@ -46,13 +55,11 @@ def inverse_line_graph(G):
       Information Processing Letters 2, (1973), 108--112. 
     
     """
+    if G.number_of_edges() == 0 or G.number_of_nodes() == 0:
+        raise nx.NetworkXError("G is not a line graph (has zero vertices or edges)")
     
-    # TODO should we have some create_using option here that wraps around the main algorithm?
-    # TODO should we have checks on what kinds of graph are supported or not supported?
     starting_cell = _select_starting_cell(G)
-    
     P = _find_partition(G, starting_cell)
-    
     # count how many times each vertex appears in the partition set
     P_count = {u:0 for u in G.nodes()}
     for p in P:
@@ -82,13 +89,34 @@ def _triangles(G, e):
     return triangle_list           
     
 def _odd_triangle(G, T):
-    """ G - nx graph
-        T - triple of vertices forming triangle in G
+    """ Test whether T is an odd triangle in G
+    
+    Parameters
+    ----------
+    G : NetworkX Graph
+    T : 3-tuple of vertices forming triangle in G
+    
+    Returns
+    -------
+    True is T is an odd triangle
+    False otherwise
+    
+    Raises
+    ------
+    NetworkXError
+        T is not a triangle in G
         
-        Return True is T is an odd triangle, False otherwise
-        
-        An odd triangle is one in which there exists another vertex in G which is adjacent
-        to either exactly one or exactly all three of the vertices in the triangle"""
+    Notes
+    -----
+    An odd triangle is one in which there exists another vertex in G which is adjacent
+    to either exactly one or exactly all three of the vertices in the triangle"""
+    for u in T:
+        if u not in G.nodes():
+            raise nx.NetworkXError("Vertex %s not in graph" % u)
+    for e in list(combinations(T, 2)):
+        if e[0] not in G.neighbors(e[1]):
+            raise nx.NetworkXError("Edge (%s, %s) not in graph" % (e[0], e[1]))
+            
     T_neighbors = defaultdict(int)
     for t in T:
         for v in G.neighbors(t):
@@ -124,15 +152,11 @@ def _select_starting_cell(G, starting_edge=None):
     # If starting edge not specified pick an arbitrary edge - doesn't matter which
     # However, this function may call itself requiring a specific starting edge
     if starting_edge == None:
-        # TODO
-        # JC - is this iterator call a good idea? 
-        # JC - If not whats the best way to select an arbitrary edge?
-        e = G.edges().next()
+        e = arbitrary_element(list(G.edges()))
     else:
         e = starting_edge
         if e[0] not in G.neighbors(e[1]):
-            raise nx.NetworkXError('starting_edge %s is not in the Graph' % e)
-        
+            raise nx.NetworkXError('starting_edge %s is not in the Graph' % e) 
     e_triangles = _triangles(G, e)
     r = len(e_triangles)
     if r == 0:
@@ -159,21 +183,28 @@ def _select_starting_cell(G, starting_edge=None):
                 s += 1
                 odd_triangles.append(T)
                 
-        if (r==2) and (s==0):
+        if r==2 and s==0:
             # in this case it doesn't matter which of our two triangles we choose, so just use T
             starting_cell = T 
-        elif (s==r or s==r-1):
+        elif r-1 <= s <= r:
             # check if odd triangles containing e form complete subgraph
-            # this will be the case if there are s+2 distinct nodes in the triangles
+            # there must be exactly s+2 of them
+            # and they must all be connected
             triangle_nodes = set([])
             for T in odd_triangles:
                 for x in T:
                     triangle_nodes.add(x)
             if len(triangle_nodes) == s+2:
+                for u in triangle_nodes:
+                    for v in triangle_nodes:
+                        if u!=v and (v not in G.neighbors(u)):
+                            raise nx.NetworkXError("G is not a line graph (odd triangles do not form complete subgraph)")
+                # otherwise then we can use this as the starting cell
                 starting_cell = tuple(triangle_nodes)  
+            else:
+                raise nx.NetworkXError("G is not a line graph (odd triangles do not form complete subgraph)")
         else:
             raise nx.NetworkXError("G is not a line graph (incorrect number of odd triangles around starting edge)")
-            # TODO JC - can this logic be made clearer or tidier?
             # TODO JC - are these recursive calls OK or should we do something else?
     return starting_cell    
     
