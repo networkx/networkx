f68016afb6c1c098b808cc40daea7012fe1caf24
rmsyed <rohailsyed@gmail.com>
2016-10-07 22:16:19 -0400

    Update bridges.py

--- networkx/algorithms/bridges.py
+++ networkx/algorithms/bridges.py
@@ -3,18 +3,21 @@ import networkx as nx
 __author__ = "Rohail Syed <rohailsyed@gmail.com>"
 
 __all__ = [
-    'all_bridges',
-    'all_local_bridges',
+    'bridges',
+    'local_bridges',
     'bridges_exist',
     'local_bridges_exist'
 ]
 
 
-def all_local_bridges(G, first_match=False):
-    """ Looks through the graph object G for all local bridges.
-    We formally define a local bridge to be any edge AB such that the removal
+def local_bridges(G, first_match=False):
+    """ Looks through the graph object `G` for all local bridges.
+    We formally define a local bridge to be any edge `AB` such that the removal
     of the edge results in a distance strictly greater than 2 between
-    nodes A and B. Note that all bridges are local bridges.
+    nodes `A` and `B`. We formally define *span* as the distance between two
+    nodes `A` and `B` when the edge connecting them is removed. Note that all
+    bridges are local bridges with span of infinity (outputs span of -1
+    in this function).
 
     Parameters
     ----------
@@ -26,128 +29,56 @@ def all_local_bridges(G, first_match=False):
     Returns
     ----------
     dict
-        edges : List of edges that are local bridges
-        spans : List of corresponding spans of the ith local bridge
+        key : (tuple) edge that is a local bridge
+        value : (int) span of corresponding local bridge
 
     Examples
     --------
-    >>> G=nx.cycle_graph(5)
-    >>> bridges_dict = all_local_bridges(G)
-    >>> bridges_dict["edges"]
-    [[0, 1], [0, 4], [1, 2], [2, 3], [3, 4]]
-    >>> bridges_dict["spans"]
+    >>> G = nx.cycle_graph(5)
+    >>> bridges_dict = local_bridges(G)
+    >>> list(bridges_dict.keys())
+    [(0, 1), (1, 2), (3, 4), (2, 3), (0, 4)]
+    >>> list(bridges_dict.values())
     [4, 4, 4, 4, 4]
 
     Notes
     ----------
     This function can be useful to quickly determine what local bridges exist
     in a given network and what their spans are. The function finds the local
-    bridges as follows. For each node A in G, capture the list of all
-    neighbors it has. For each of these neighbors, remove its edge with A
-    and determine A's new shortest path to that node. If none exists, we have
-    a bridge(which we represent as having span=-1). Otherwise, if the span
-    is strictly greater than 2, that edge is a local bridge. Repeat this
+    bridges as follows. For each edge E in graph G, determine the start and
+    end nodes (A and B) and delete the edge. Attempt to find the new shortest
+    path between A and B. If none exists, we have a bridge(which we represent
+    as having span=-1). Otherwise, if the span is strictly greater than 2,
+    that edge is a local bridge. Add edge E back in the graph and repeat this
     process for all edges.
     """
 
-    nodeset = sorted(G.nodes())  # only need to sort for consistency in output.
-    allBridges = []
-    allSpans = []
-    bridgeObjs = []
-    usedNodes = []
-    for startNode in nodeset:
-        neighbors = sorted(G.neighbors(startNode))  # same as above
-        for endNode in neighbors:
-            if endNode in usedNodes:
-                # this edge has already been checked. move on
-                continue
-            if [startNode, endNode] in allBridges:
-                # this edge is already known to be a local bridge
-                continue
-            G.remove_edge(startNode, endNode)
-            try:
-                pathLength = nx.shortest_path_length(G, startNode, endNode)
-                G.add_edge(startNode, endNode)
-                if pathLength > 2:
-                    # found a local bridge
-                    bridgeObjs.append(
-                        {"edge": [startNode, endNode], "span": pathLength})
-                    if first_match:
-                        allBridges.append(sorted([startNode, endNode]))
-                        allSpans.append(pathLength)
-                        return {"edges": allBridges, "spans": allSpans}
-
-            except nx.NetworkXNoPath:
-                # found a bridge
-                bridgeObjs.append({"edge": [startNode, endNode], "span": -1})
-                G.add_edge(startNode, endNode)
+    # Arya's suggestion
+    bridges = {}
+    for e in G.edges():
+        G.remove_edge(*e)
+        try:
+            (u, v) = e
+            path_length = nx.shortest_path_length(G, u, v)
+        except nx.NetworkXNoPath:
+            bridges[e] = -1  # found a bridge
+            if first_match:
+                return bridges
+        else:
+            if path_length > 2:  # found a local bridge
+                bridges[e] = path_length
                 if first_match:
-                    allBridges.append(sorted([startNode, endNode]))
-                    allSpans.append(-1)
-                    return {"edges": allBridges, "spans": allSpans}
-        usedNodes.append(startNode)
-
-    # sort the list in ascending order (for consistency in output)
-    for bridge in bridgeObjs:
-        bridge["edge"] = sorted(bridge["edge"])
-    bridgeObjs = sorted(bridgeObjs, key=lambda x: x["edge"])
-    allBridges = []
-    allSpans = []
-    for bridge in bridgeObjs:
-        allBridges.append(bridge["edge"])
-        allSpans.append(bridge["span"])
-
-    return {"edges": allBridges, "spans": allSpans}
+                    return bridges
+        finally:
+            G.add_edge(*e)
+    return bridges
 
 
-""" Here, we define the global variables we will need for the Depth-First
-Search used in our implementation of Tarjan's Bridge-finding algorithm. """
+def bridges(G):
+    """ Looks through the graph object `G` for all bridges.
 
-globalGraph = {}
-T = 0
-lowest = {}
-vals = {}
-bridges = []
-
-
-def dfs(currentNode, parentNode):
-    global globalGraph
-    global lowest
-    global vals
-    global T
-    global bridges
-
-    vals[currentNode] = T
-    lowest[currentNode] = T
-    T += 1
-    allNeighbors = globalGraph.neighbors(currentNode)
-    for neighbor in allNeighbors:
-        if not neighbor == parentNode:
-            if vals[neighbor] == 0:
-                # unvisited node
-                dfs(neighbor, currentNode)
-                if lowest[neighbor] > vals[currentNode]:
-                    # found a bridge
-                    bridges.append([currentNode, neighbor])
-                lowest[currentNode] = min(
-                    lowest[currentNode], lowest[neighbor])
-            else:
-                # already visited node
-                lowest[currentNode] = min(lowest[currentNode], vals[neighbor])
-
-
-def all_bridges(G):
-    global globalGraph
-    global lowest
-    global vals
-    global T
-    global bridges
-
-    """ Looks through the graph object G for all bridges.
-    We formally define a bridge to be any edge AB such that the removal
-    of the edge results in a distance of infinite between nodes A and B.
-    Specifically, this means that the total number of connected components
-    should increase by one after the removal of the edge.
+    We formally define a bridge to be any edge `(u, v)` such that the removal
+    of the edge increases the total number of connected components.
 
     Parameters
     ----------
@@ -155,84 +86,71 @@ def all_bridges(G):
 
     Returns
     ----------
-    list
-        List of edges that are bridges
+    iterable
+        Edges that are bridges
 
     Examples
     --------
     >>> G = nx.cycle_graph(5)
-    >>> bridges = all_bridges(G)
-    >>> bridges
+    >>> bridges_list = bridges(G)
+    >>> list(bridges_list)
     []
     >>> G.remove_edge(0,1)
-    >>> all_bridges(G)
-    [[0, 4], [1, 2], [2, 3], [3, 4]]
+    >>> list(bridges(G))
+    [(2, 1), (3, 2), (4, 3), (0, 4)]
 
     Notes
     ----------
     This function can be useful to quickly determine what bridges exist
     in a given network. We use an implemenation of Tarjan's Bridge-finding
-    algorithm to do this. In particular, we start with an initial node from
-    G and from there run Depth-First Search (DFS) where we reach every node
-    once. In each instance, we increment a time variable T, which tells us
-    how many steps of the DFS recursion we have done so far. We assign each
-    node a value, "vals" of T. This much is pretty standard DFS.
+    algorithm to do this.
 
-    Now, we go into Tarjan's algorithm. For each node we reached, we not
-    only assign a time variable but also a "lowest" variable. The lowest
-    variable indicates the distance of a node B to the root node A such
-    that the *current* node has a path to B. For any new (unvisited) node
-    we encounter, we update the current node's lowest variable to be the
-    minimum of their own lowest and that neighbor's lowest since, by
-    definition they have access to that neighbor. Similarly, if we encounter
-    a node that has already been visited (excluding parents), we update
-    the current node's lowest variable to be the minimum of their own
-    lowest and that neighbor's time variable value.
+    The algorithm is described in [1]_.
 
-    From this algorithm, we can see how to identify bridges. Any node that
-    is not a bridge will have at least one non-parent edge that it can use
-    to decrease its lowest variable to at minimum that of its parent.
-    However, a bridge, having exactly one edge, and no other path, to the
-    connected component, will be unable to improve its lowest variable as
-    we didn't allow nodes to update from their parents. Therefore, any
-    pair of nodes where the lowest variable of one node is strictly greater
-    than the value of the other is a bridge.
+    References
+    ----------
+    .. [1] R. Endre Tarjan,
+        "A note on finding the bridges of a graph", *Information Processing
+        Letters*, **2** (6): 160--161
+
     """
 
-    # reset the global variables
-    globalGraph = G
-    T = 0
-    bridges = []
-    allnodes = G.nodes()
-    # The algorithm finds bridges in a connected component. Run this for
-    # all components
-    all_components = (nx.connected_component_subgraphs(G))
-    for comp in all_components:
-        T = 0
-        allnodes = comp.nodes()
-        for node in allnodes:
-            lowest[node] = 0
-            vals[node] = 0
-        # select a starting node
-        startnode = allnodes[0]
-        # run the DFS
-        dfs(startnode, -1)
-    # sort the bridges ascending order (for consistency in output)
-    for edge in bridges:
-        if edge[0] > edge[1]:
-            temp = edge[1]
-            edge[1] = edge[0]
-            edge[0] = temp
-    bridges = sorted(bridges)
-    return bridges
+    visited = set()
+    depths = {}
+    low = {}
+    parent = {node: None for node in G}
+
+    def bridge_util(u, depth):
+        visited.add(u)
+        depth += 1
+        depths[u] = low[u] = depth
+        for v in G[u]:
+            if v not in visited:
+                parent[v] = u
+                for e in bridge_util(v, depth):
+                    yield e
+                # Check if subtree rooted at v has connection
+                # to an ancestor of u.
+                low[u] = min(low[u], low[v])
+                # If the lowest vertex reachable from the subtree under v
+                # is below u in the DFS tree, then `u-v` is a bridge.
+                if low[v] > depths[u]:
+                    yield (u, v)
+            elif v != parent[u]:
+                low[u] = min(low[u], depths[v])
+
+    for u in G:
+        if u not in visited:
+            for e in bridge_util(u, depth=0):
+                yield e
 
 
 def local_bridges_exist(G):
     """ Checks if any local bridges exist in this network. We will simply call
-    the all_local_bridges() function with the first-match stop parameter.
+    the `local_bridges()` function with the first_match stop parameter.
     Since the search for local bridges will terminate after finding one
     instance, this function is faster if we just want to know if at least
-    one local bridge exists in the network and what it is.
+    one local bridge exists in the network.
 
     Parameters
     ----------
@@ -259,8 +177,8 @@ def local_bridges_exist(G):
     bridges exist in a given network.
     """
 
-    results = all_local_bridges(G, first_match=True)
-    if len(results["spans"]) > 0:
+    results = local_bridges(G, first_match=True)
+    if len(results) > 0:
         return True
     else:
         return False
@@ -268,7 +186,7 @@ def local_bridges_exist(G):
 
 def bridges_exist(G):
     """ Checks if any bridges exist in this network. We will simply call the
-    all_bridges() function and look for non-zero list length.
+    `bridges()` function and look for non-zero list length.
 
     Parameters
     ----------
@@ -295,8 +213,8 @@ def bridges_exist(G):
     exist in a given network.
     """
 
-    results = all_bridges(G)
-    if len(results) > 0:
+    results = bridges(G)
+    if len(list(results)) > 0:
         return True
     else:
         return False
