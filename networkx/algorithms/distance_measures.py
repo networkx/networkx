207e58ee6d33ef4d4b81699749fcb91b38e3e365
Dan Schult <dschult@colgate.edu>
2017-01-23 22:16:29 -0500

    Add tests and make silly style changes

--- networkx/algorithms/distance_measures.py
+++ networkx/algorithms/distance_measures.py
@@ -16,7 +16,6 @@ __all__ = ['extrema_bounding', 'eccentricity', 'diameter', 'radius', 'periphery'
 import networkx
 
 def extrema_bounding(G, compute="diameter"):
-
     """Compute requested extreme distance metric of undirected graph G 
     
     Computation is based on smart lower and upper bounds, and in practice 
@@ -47,7 +46,6 @@ def extrema_bounding(G, compute="diameter"):
        
     Notes
     -----       
-    
     This algorithm was proposed in the following papers:
     
     F.W. Takes and W.A. Kosters, Determining the Diameter of Small World 
@@ -65,106 +63,108 @@ def extrema_bounding(G, compute="diameter"):
     doi: http://dx.doi.org/10.1016/j.tcs.2015.02.033   
     """
 
-    # init status variables
-    current = None 
-    ecc_lower = dict((i,0) for i in G.nodes())
-    ecc_upper = dict((i,G.number_of_nodes()) for i in G.nodes())    
-    candidates = dict((i,True) for i in G.nodes())        
-    
+    # init variables
+    degrees = dict(G.degree())  # start with the highest degree node
+    minlowernode = max(degrees, key=lambda i: degrees[i])
+    N=len(degrees)  # number of nodes
     # alternate between smallest lower and largest upper bound
-    high = True
+    high = False
+    # status variables
+    ecc_lower = dict.fromkeys(G.nodes(), 0)
+    ecc_upper = dict.fromkeys(G.nodes(), N)
+    candidates = set(G)
     
     # (re)set bound extremes
-    minlower = G.number_of_nodes()
-    maxlower = 0;
-    minupper = G.number_of_nodes()
-    maxupper = 0;    
+    minlower = N
+    maxlower = 0
+    minupper = N
+    maxupper = 0
     
     # repeat the following until there are no more candidates
-    while(len(candidates) > 0):  
-        high = not high
-        
-        if(current == None):
-            degrees = dict(G.degree())  # start with the highest degree node
-            current = max(degrees, key=lambda i: degrees[i])
-        elif(high): 
+    while candidates:
+        if high:
             current = maxuppernode; # select node with largest upper bound
         else:
-            current = minlowernode; # select node with smallest lower bound            
+            current = minlowernode; # select node with smallest lower bound
+        high = not high
         
         # get distances from/to current node and derive eccentricity
-        d = dict(networkx.single_source_shortest_path_length(G, current))
-        if len(d) != G.order():
+        dist = dict(networkx.single_source_shortest_path_length(G, current))
+        if len(dist) != N:
             msg = ('Cannot compute metric because graph is not connected.')
             raise networkx.NetworkXError(msg)
-        current_ecc = int(max(d.values()))
-      
-        # print("status update")
-#        print ("ecc of " + str(current) + " (" + str(ecc_lower[current]) + "/" 
-#        + str(ecc_upper[current]) + ", deg: " + str(d[current]) + ") is " 
+        current_ecc = int(max(dist.values()))
+
+        # print status update
+#        print ("ecc of " + str(current) + " (" + str(ecc_lower[current]) + "/"
+#        + str(ecc_upper[current]) + ", deg: " + str(dist[current]) + ") is " 
 #        + str(current_ecc))        
 #        print(ecc_upper)
         
         # (re)set bound extremes
         maxuppernode = None;
-        minlowernode = None;           
+        minlowernode = None;
         
         # update node bounds
-        for i in candidates.keys():
-        
+        for i in candidates:
             # update eccentricity bounds
-            ecc_lower[i] = max(ecc_lower[i], max(d[i], (current_ecc - d[i])));
-            ecc_upper[i] = min(ecc_upper[i], current_ecc + d[i]);
+            d = dist[i]
+            ecc_lower[i] = low = max(ecc_lower[i], max(d, (current_ecc - d)))
+            ecc_upper[i] = upp = min(ecc_upper[i], current_ecc + d);
 
             # update min/max values of lower and upper bounds
             minlower = min(ecc_lower[i], minlower);
-            minupper = min(ecc_upper[i], minupper);
             maxlower = max(ecc_lower[i], maxlower);
+            minupper = min(ecc_upper[i], minupper);
             maxupper = max(ecc_upper[i], maxupper);
         
         # update candidate set
-        for i in candidates.keys():
-        
-            # disregard nodes that can no longer contribute to the metric
-            if((ecc_lower[i] == ecc_upper[i]) 
-            or((compute == 'diameter' and 
-                ecc_upper[i] <= maxlower and ecc_lower[i]*2 >= maxupper)
-            or (compute == 'radius' and 
-                ecc_lower[i] >= minupper and (ecc_upper[i] + 1) / 2 <= minlower)
-            or (compute == 'periphery' and ecc_upper[i] < maxlower and 
-                ((maxlower == maxupper) or (ecc_lower[i]*2 > maxupper))) 
-            or (compute == 'center' and ecc_lower[i] > minupper and 
-                ((minlower == minupper) or ((ecc_upper[i] + 1) / 2 < minlower)))
-            or (compute == 'eccentricities' and ecc_lower[i] == ecc_upper[i]))):
-                del candidates[i] 
-#                print("removing %g: ecc_u: %g maxl: %g ecc_l: %g maxu: %g"%(i,ecc_upper[i],maxlower,ecc_lower[i],maxupper))
-#            if i == 4:
-#                print("node %g: ecc_u: %g maxl: %g ecc_l: %g maxu: %g"%(4,ecc_upper[4],maxlower,ecc_lower[4],maxupper))
-#                print("NODE 4: %g"%(ecc_upper[4] <= maxlower))
-#                print("NODE 4: %g"%(2 * ecc_lower[4] >= maxupper))
-#                print("NODE 4: %g"%(ecc_upper[4] <= maxlower and ecc_lower[4]*2 >= maxupper))
+        if compute == 'diameter':
+            ruled_out = {i for i in candidates \
+                if ecc_upper[i] <= maxlower and 2 * ecc_lower[i] >= maxupper}
+
+        elif compute == 'radius':
+            ruled_out = {i for i in candidates if ecc_lower[i] >= minupper \
+                and ecc_upper[i] + 1 <= 2 * minlower}
+
+        elif compute == 'periphery':
+            ruled_out = {i for i in candidates if ecc_upper[i] < maxlower \
+                and (maxlower == maxupper or ecc_lower[i] > maxupper)}
+
+        elif compute == 'center':
+            ruled_out = {i for i in candidates if ecc_lower[i] > minupper \
+                and (minlower == minupper or ecc_upper[i] + 1 < 2 * minlower)}
+
+        elif compute == 'eccentricities':
+            ruled_out = {}
+
+        ruled_out.update(i for i in candidates if ecc_lower[i] == ecc_upper[i])
+        candidates -= ruled_out
+
+#        for i in ruled_out:
+#            print("removing %g: ecc_u: %g maxl: %g ecc_l: %g maxu: %g"%(i,ecc_upper[i],maxlower,ecc_lower[i],maxupper))
+#        print("node %g: ecc_u: %g maxl: %g ecc_l: %g maxu: %g"%(4,ecc_upper[4],maxlower,ecc_lower[4],maxupper))
+#        print("NODE 4: %g"%(ecc_upper[4] <= maxlower))
+#        print("NODE 4: %g"%(2 * ecc_lower[4] >= maxupper))
+#        print("NODE 4: %g"%(ecc_upper[4] <= maxlower and 2 * ecc_lower[4] >= maxupper))
             
-            # updating maxuppernode and minlowernode for selection in next round
-            if(i in candidates):
-                if(minlowernode == None):
-                    minlowernode = i;
-                elif(ecc_lower[i] == ecc_lower[minlowernode] 
-                and G.degree(i) > G.degree(minlowernode)):
-                    minlowernode = i;
-                elif(ecc_lower[i] < ecc_lower[minlowernode]):
-                    minlowernode = i;
-                    
-                if(maxuppernode == None):
-                    maxuppernode = i;
-                elif(ecc_upper[i] == ecc_upper[maxuppernode] 
-                and G.degree(i) > G.degree(maxuppernode)):
-                    maxuppernode = i;
-                elif(ecc_upper[i] > ecc_upper[maxuppernode]):
-                    maxuppernode = i;
-        
-        # print status update    
-#        print (" min=" + str(minlower) + "/" + str(minupper) + 
-#        " max=" + str(maxlower) + "/" + str(maxupper) + 
+        # updating maxuppernode and minlowernode for selection in next round
+        for i in candidates:
+            if minlowernode == None \
+                    or (ecc_lower[i] == ecc_lower[minlowernode] \
+                        and degrees[i] > degrees[minlowernode]) \
+                    or (ecc_lower[i] < ecc_lower[minlowernode]):
+                minlowernode = i;
+
+            if maxuppernode == None \
+                    or (ecc_upper[i] == ecc_upper[maxuppernode] \
+                        and degrees[i] > degrees[maxuppernode]) \
+                    or (ecc_upper[i] > ecc_upper[maxuppernode]):
+                maxuppernode = i;
+    
+        # print status update
+#        print (" min=" + str(minlower) + "/" + str(minupper) +
+#        " max=" + str(maxlower) + "/" + str(maxupper) +
 #        " candidates: " + str(len(candidates)))
 #        print("cand:",candidates)
 #        print("ecc_l",ecc_lower)
@@ -177,12 +177,12 @@ def extrema_bounding(G, compute="diameter"):
     elif(compute == 'radius'):
         return minupper
     elif(compute == 'periphery'):
-        p = [v for v in G.nodes() if ecc_lower[v]==maxlower]
+        p = [v for v in G if ecc_lower[v]==maxlower]
         return p;
     elif (compute == 'center'):
-        c = [v for v in G.nodes() if ecc_upper[v]==minupper]
+        c = [v for v in G if ecc_upper[v]==minupper]
         return c
-    elif (compute == 'eccentricities'):        
+    elif (compute == 'eccentricities'):
         return ecc_lower
     return None;
 
