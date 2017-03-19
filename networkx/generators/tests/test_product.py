74a62ed8ee55ed139944c2517a74997d6037ed85
Nikhil Desai <nikhilarundesai@gmail.com>
2017-03-18 19:15:57 -0700

    Update Kronecker tests counting number of edges

--- networkx/generators/tests/test_product.py
+++ networkx/generators/tests/test_product.py
@@ -13,39 +13,39 @@ class TestGeneratorsProduct():
         m=[[0,0],[0,0]]
         G=kronecker_random_graph(4,m,directed=False)
         assert_equal(len(G),2**4)
-        assert_equal(len(G.edges()),0)
+        assert_equal(sum(1 for _ in G.edges()),0)
 
         m=[[1,1],[1,1]]
         G=kronecker_random_graph(4,m)
         assert_equal(len(G),2**4)
-        assert_equal(len(G.edges()),(2**4)**2)
+        assert_equal(sum(1 for _ in G.edges()),(2**4)**2)
 
         m=[[1,1],[1,1]]
         G=kronecker_random_graph(4,m,directed=False)
         assert_equal(len(G),16)
-        assert_equal(len(G.edges()),136)
+        assert_equal(sum(1 for _ in G.edges()),136)
 
         m=[[1,0],[0,0]]
         G=kronecker_random_graph(4,m)
-        assert_equal(len(G.edges()),1)
+        assert_equal(sum(1 for _ in G.edges()),1)
         assert_true(G.has_edge(0,0))
         assert_true(not G.has_edge(0,1))
 
         m=[[1,1],[0,0]]
         G=kronecker_random_graph(4,m)
-        assert_equal(len(G.edges()),16)
+        assert_equal(sum(1 for _ in G.edges()),16)
         for i in range(16):
             assert_true(G.has_edge(0,i))
 
         m=[[1,1],[1,0]]
         G=kronecker_random_graph(4,m,directed=False)
         assert_equal(len(G),16)
-        assert_equal(len(G.edges()),41)
+        assert_equal(sum(1 for _ in G.edges()),41)
 
         m=[[1,1],[0,0]]
         G=kronecker_random_graph(0,m)
         assert_equal(len(G),1)
-        assert_equal(len(G.edges()),0)
+        assert_equal(sum(1 for _ in G.edges()),0)
 
         m=[[1,1]]
         assert_raises(networkx.exception.NetworkXError,
@@ -64,39 +64,39 @@ class TestGeneratorsProduct():
         m=[[0,0],[0,0]]
         G=kronecker2_random_graph(4,m,directed=False)
         assert_equal(len(G),2**4)
-        assert_equal(len(G.edges()),0)
+        assert_equal(sum(1 for _ in G.edges()),0)
 
         m=[[1,1],[1,1]]
         G=kronecker2_random_graph(4,m)
         assert_equal(len(G),16)
-        assert_equal(len(G.edges()),256)
+        assert_equal(sum(1 for _ in G.edges()),256)
 
         m=[[1,1],[1,1]]
         G=kronecker2_random_graph(4,m,directed=False)
         assert_equal(len(G),16)
-        assert_equal(len(G.edges()),136)
+        assert_equal(sum(1 for _ in G.edges()),136)
 
         m=[[1,0],[0,0]]
         G=kronecker2_random_graph(4,m)
-        assert_equal(len(G.edges()),1)
+        assert_equal(sum(1 for _ in G.edges()),1)
         assert_true(G.has_edge(0,0))
         assert_true(not G.has_edge(0,1))
 
         m=[[1,1],[0,0]]
         G=kronecker2_random_graph(4,m)
-        assert_equal(len(G.edges()),16)
+        assert_equal(sum(1 for _ in G.edges()),16)
         for i in range(16):
             assert_true(G.has_edge(0,i))
 
         m=[[1,1],[0,0]]
         G=kronecker2_random_graph(0,m)
         assert_equal(len(G),1)
-        assert_equal(len(G.edges()),0)
+        assert_equal(sum(1 for _ in G.edges()),0)
 
         m=[[1,1],[1,0]]
         G=kronecker2_random_graph(4,m,directed=False)
         assert_equal(len(G),16)
-        assert_equal(len(G.edges()),41)
+        assert_equal(sum(1 for _ in G.edges()),41)
 
         m=[[1,1]]
         assert_raises(networkx.exception.NetworkXError,
