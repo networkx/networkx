fea77195eb3545607c8c942689dc9934fa98f413
Michael-E-Rose <Michael.Ernst.Rose@gmail.com>
2017-04-27 10:09:47 +0200

    Update test_write_with_node_attributes()

--- networkx/readwrite/tests/test_gexf.py
+++ networkx/readwrite/tests/test_gexf.py
@@ -295,8 +295,12 @@ class TestGEXF(object):
             G.node[i]['label'] = i
             G.node[i]['pid'] = i
 
-        expected = """<gexf version="1.1" xmlns="http://www.gexf.net/1.1draft" xmlns:viz="http://www.gexf.net/1.1draft/viz" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.w3.org/2001/XMLSchema-instance">
-  <graph defaultedgetype="undirected" mode="static">
+        expected = """<gexf version="1.1" xmlns="http://www.gexf.net/1.1draft" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.w3.org/2001/XMLSchema-instance">
+  <graph defaultedgetype="undirected" mode="static" name="">
+    <meta>
+      <creator>NetworkX {}</creator>
+      <lastmodified>{}</lastmodified>
+    </meta>
     <nodes>
       <node id="0" label="0" pid="0" />
       <node id="1" label="1" pid="1" />
@@ -309,7 +313,7 @@ class TestGEXF(object):
       <edge id="2" source="2" target="3" />
     </edges>
   </graph>
-</gexf>"""
+</gexf>""".format(nx.__version__, time.strftime('%d/%m/%Y'))
         obtained = '\n'.join(nx.generate_gexf(G))
         assert_equal( expected, obtained )
 
