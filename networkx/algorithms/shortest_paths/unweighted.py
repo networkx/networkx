c0dbc838194c80382330a617c00331be8c714f7e
Dan Schult <dschult@colgate.edu>
2017-07-01 12:52:51 -0400

    Update docstring for _single_shortest_path

--- networkx/algorithms/shortest_paths/unweighted.py
+++ networkx/algorithms/shortest_paths/unweighted.py
@@ -357,6 +357,10 @@ def _single_shortest_path(adj, firstlevel, paths, cutoff, join):
             paths for starting nodes, e.g. {source: [source]}
         cutoff : int or float
             level at which we stop the process
+        join : function
+            function to construct a path from two partial paths. Requires two
+            list inputs `p1` and `p2`, and returns a list. Usually returns
+            `p1 + p2` (forward from source) or `p2 + p1` (backward from target)
     """
     level = 0                  # the current level
     nextlevel = firstlevel
