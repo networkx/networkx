000cc26edf86bec4da766afcc3f346896ab8a49e
Jarrod Millman <jarrod.millman@gmail.com>
2017-07-25 10:53:06 -0700

    Comply with pep8

--- networkx/tests/test.py
+++ networkx/tests/test.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 import sys
-from os import path,getcwd
+from os import path, getcwd
 
-def run(verbosity=1,doctest=False,numpy=True):
+
+def run(verbosity=1, doctest=False, numpy=True):
     """Run NetworkX tests.
 
     Parameters
     ----------
     verbosity: integer, optional
-      Level of detail in test reports.  Higher numbers provide  more detail.  
+      Level of detail in test reports.  Higher numbers provide more detail.
 
     doctest: bool, optional
       True to run doctests in code modules
@@ -19,27 +20,26 @@ def run(verbosity=1,doctest=False,numpy=True):
     try:
         import nose
     except ImportError:
-        raise ImportError(\
+        raise ImportError(
             "The nose package is needed to run the NetworkX tests.")
 
     sys.stderr.write("Running NetworkX tests:")
-    nx_install_dir=path.join(path.dirname(__file__), path.pardir)
+    nx_install_dir = path.join(path.dirname(__file__), path.pardir)
     # stop if running from source directory
-    if getcwd() == path.abspath(path.join(nx_install_dir,path.pardir)):
+    if getcwd() == path.abspath(path.join(nx_install_dir, path.pardir)):
         raise RuntimeError("Can't run tests from source directory.\n"
                            "Run 'nosetests' from the command line.")
 
-    argv=[' ','--verbosity=%d'%verbosity,
-          '-w',nx_install_dir,
-          '-exe']
+    argv = [' ', '--verbosity=%d' % verbosity,
+            '-w', nx_install_dir,
+            '-exe']
     if doctest:
-        argv.extend(['--with-doctest','--doctest-extension=txt'])
+        argv.extend(['--with-doctest', '--doctest-extension=txt'])
     if not numpy:
         argv.extend(['-A not numpy'])
 
-
     nose.run(argv=argv)
 
-if __name__=="__main__":
-    run()
 
+if __name__ == "__main__":
+    run()
