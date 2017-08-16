0b4edf66a9d81417e03d47f810ebde9ccfba8d2d
Jarrod Millman <jarrod.millman@gmail.com>
2017-08-16 15:28:42 -0700

    Add nx_agraph tests

--- tools/travis/linux_install.sh
+++ tools/travis/linux_install.sh
@@ -11,6 +11,22 @@ if [[ "${OPTIONAL_DEPS}" == pip ]]; then
   export CPLUS_INCLUDE_PATH=/usr/include/gdal
   export C_INCLUDE_PATH=/usr/include/gdal
 
+  # needed for view_graphviz and default_opener
+  DIR=~/.local/share/applications
+  mkdir -p $DIR
+  FILE=$DIR/png.desktop
+  cat <<EOF >$FILE
+[Desktop Entry]
+Name=png
+MimeType=image/png;
+Exec=/usr/bin/file
+Type=Application
+Terminal=true
+NoDisplay=true
+EOF
+
+  xdg-mime default png.desktop image/png
+
 fi
 
 set +ex
