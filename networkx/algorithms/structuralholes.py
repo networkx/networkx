3274c93de27b29a54cb71b60fdab589083db582c
Sanggyu Nam <pokeplus@gmail.com>
2017-08-22 14:10:12 +0900

    Make parameter nodes effective in effective_size()
    * Fixes #2631.

--- networkx/algorithms/structuralholes.py
+++ networkx/algorithms/structuralholes.py
@@ -145,7 +145,7 @@ def effective_size(G, nodes=None, weight=None):
         nodes = G
     # Use Borgatti's simplified formula for unweighted and undirected graphs
     if not G.is_directed() and weight is None:
-        for v in G:
+        for v in nodes:
             # Effective size is not defined for isolated nodes
             if len(G[v]) == 0:
                 effective_size[v] = float('nan')
@@ -153,7 +153,7 @@ def effective_size(G, nodes=None, weight=None):
             E = nx.ego_graph(G, v, center=False, undirected=True)
             effective_size[v] = len(E) - (2 * E.size()) / len(E)
     else:
-        for v in G:
+        for v in nodes:
             # Effective size is not defined for isolated nodes
             if len(G[v]) == 0:
                 effective_size[v] = float('nan')
