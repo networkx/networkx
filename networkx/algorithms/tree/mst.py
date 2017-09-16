0d93bd37e3931acbd4f3921ca8debcf634b0a341
Dan Schult <dschult@colgate.edu>
2017-09-16 19:45:55 -0400

    MT doc Note: isolated nodes with self-loops case (#2670)
    isolated nodes with self-loops included in tree as isolated nodes.
    Fixes #2667

--- networkx/algorithms/tree/mst.py
+++ networkx/algorithms/tree/mst.py
@@ -537,6 +537,8 @@ def minimum_spanning_tree(G, weight='weight', algorithm='kruskal',
     There may be more than one tree with the same minimum or maximum weight.
     See :mod:`networkx.tree.recognition` for more detailed definitions.
 
+    Isolated nodes with self-loops are in the tree as edgeless isolated nodes.
+
     """
     edges = minimum_spanning_edges(G, algorithm, weight, keys=True,
                                    data=True, ignore_nan=ignore_nan)
@@ -595,6 +597,8 @@ def maximum_spanning_tree(G, weight='weight', algorithm='kruskal',
 
     There may be more than one tree with the same minimum or maximum weight.
     See :mod:`networkx.tree.recognition` for more detailed definitions.
+
+    Isolated nodes with self-loops are in the tree as edgeless isolated nodes.
 
     """
     edges = maximum_spanning_edges(G, algorithm, weight, keys=True,
