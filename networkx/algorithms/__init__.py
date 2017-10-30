71784e5c18101453e907fc00ae86b511f337802a
Iacopo Iacopini <iacopiniiacopo@gmail.com>
2017-10-30 10:18:00 +0000

    Update __init__.py

--- networkx/algorithms/__init__.py
+++ networkx/algorithms/__init__.py
@@ -32,6 +32,7 @@ from networkx.algorithms.minors import *
 from networkx.algorithms.mis import *
 from networkx.algorithms.operators import *
 from networkx.algorithms.reciprocity import *
+from networkx.algorithms.rich_core import *
 from networkx.algorithms.richclub import *
 from networkx.algorithms.shortest_paths import *
 from networkx.algorithms.simple_paths import *
