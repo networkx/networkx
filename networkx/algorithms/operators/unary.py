322fa329d6904d6dfc773f7524a814422bde4988
Dan Schult <dschult@colgate.edu>
2017-12-02 16:34:35 -0500

    Add Mycielski Operator (#2785)
    * Added the Mycielski operator to the unary operators file.
    
    * Updated the Mycielski operation to be iterative and not recursive.
    
    Also edited the docstring so that the LaTeX would not fail on square brackets.
    
    * Fixed some spacing in the docstring for mycielski
    
    * Cleaned up docstrings to remove LaTex and removed an unnecessary if statement.
    
    * Updated the mycielski operator code in unary.py.
    
    Removed unnecessary type checking and made some of the code more readable.
    Additionally, removed a recursive step to make the whole mycielski operation iterative.
    
    * Updated tests for Mycielski operator
    
    Removed typechecking tests since they are unnecessary.
    Renamed tests to reflect their function.
    
    * Updated docs for Mycielski.
    
    The docs for the Mycielski operator now contain a description of the operation as well as some external information and properties of the Mycielskian of a graph.
    
    * Fixed spacing for Mycielski to adhere to PEP-8
    
    * moved mycielski to generators instead of operators
    
    Removed mycielski from unary.py and created a new file in generators
    called mycielski.py containing the mycielski operation and a generator
    for the mycielski family of graphs
    
    * removed mycielski tests from operators
    
    * updated docs for mycielski functions and added tests
    
    * Moved mycielski documentation to generators
    
    * Fixed tests
    
    * Adding trailing newline to unary.py
    
    * Updated docs for mycielski to include math tag
    
    * Replace unicode symbols in Latex code with Latex commands

--- networkx/algorithms/operators/unary.py
+++ networkx/algorithms/operators/unary.py
@@ -6,6 +6,7 @@
 #    All rights reserved.
 #    BSD license.
 import networkx as nx
+from networkx.utils import not_implemented_for
 __author__ = """\n""".join(['Aric Hagberg <aric.hagberg@gmail.com>',
                            'Pieter Swart (swart@lanl.gov)',
                            'Dan Schult(dschult@colgate.edu)'])
