bd09a296cd0823ac68eef678ccd32d3cb3ca3d05
rodogi <rodgdor@gmail.com>
2017-12-18 12:31:09 +0100

    add docstring

--- examples/drawing/plot_directed.py
+++ examples/drawing/plot_directed.py
@@ -1,17 +1,26 @@
+#! /usr/bin/env python
+"""
+Directed Graph
+
+Draw a graph with directed edges or arcs using a colormap and different node sizes.
+
+Arcs have different colors.
+"""
+# Author: Rodrigo Dorantes-Gilardi (rodgdor@gmail.com)
+
 import matplotlib.pyplot as plt
 import networkx as nx
 
 G = nx.generators.directed.random_k_out_graph(10, 3, 0.5)
 pos = nx.layout.spring_layout(G)
 
-node_sizes = range(10)
-edge_colors = range(10)
+node_sizes = range(20, 20 + len(G.nodes))
 edge_colors = range(len(G.edges))
 
 nodes = nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color='blue')
 edges = nx.draw_networkx_edges(G, pos, node_size=node_sizes, arrowstyle='->',
                                arrowsize=10, edge_color=edge_colors,
-                               edge_cmap=plt.cm.Blues)
+                               edge_cmap=plt.cm.Blues, width=2)
 ax = plt.gca()
 ax.set_axis_off()
 plt.show()
