59206492a75a9cef05310c409f5d68b4a7303350
Dan Schult <dschult@colgate.edu>
2018-01-09 11:42:19 -0500

    fix pep8 for label_propagation

--- networkx/algorithms/community/label_propagation.py
+++ networkx/algorithms/community/label_propagation.py
@@ -1,46 +1,48 @@
-#-*- coding: utf-8 -*-
-#    Copyright (C) 2015 
+# -*- coding: utf-8 -*-
+#    Copyright (C) 2015-2018 Aitor Almeida
 #    All rights reserved.
 #    BSD license.
-
-
+#
+# Author:   Aitor Almeida <aitoralmeida@gmail.com>
 """
 Semi-synchronous label propagation community detection algorithm.
 """
-
 from collections import Counter
+
 import networkx as nx
 from networkx.utils.decorators import not_implemented_for
 
-
 __all__ = ["label_propagation_communities"]
+
 
 @not_implemented_for('directed')
 def label_propagation_communities(G):
-    """Finds communities in graph using a semi-synchronous label propagation 
-    method[1]_. This method combines the advantages of both the synchronous and  
-    asynchronous models. Not implemented for directed graphs.
-    
+    """Generates community sets determined by label propagation
+
+    Finds communities in `G` using a semi-synchronous label propagation
+    method[1]_. This method combines the advantages of both the synchronous
+    and asynchronous models. Not implemented for directed graphs.
+
     Parameters
     ----------
     G : graph
-    An undirected NetworkX graph.      
-    
-    Returns
-    -------
-    communities : list
-    List of the communities (sets)
-    
+        An undirected NetworkX graph.
+
+    Yields
+    ------
+    communities : generator
+        Yields sets of the nodes in each community.
+
     Raises
     ------
     NetworkXNotImplemented
        If the graph is directed
-  
+
     References
     ----------
-    .. [1] Cordasco, G., & Gargano, L. (2010, December). Community detection 
-       via semi-synchronous label propagation algorithms. In Business 
-       Applications of Social Network Analysis (BASNA), 2010 IEEE International 
+    .. [1] Cordasco, G., & Gargano, L. (2010, December). Community detection
+       via semi-synchronous label propagation algorithms. In Business
+       Applications of Social Network Analysis (BASNA), 2010 IEEE International
        Workshop on (pp. 1-8). IEEE.
     """
     coloring = _color_network(G)
@@ -50,58 +52,60 @@ def label_propagation_communities(G):
         # Update the labels of every node with the same color.
         for color, nodes in coloring.items():
             for n in nodes:
-                _update_label(n , labeling, G)
-        
+                _update_label(n, labeling, G)
+
     for label in set(labeling.values()):
         yield set((x for x in labeling if labeling[x] == label))
-        
+
+
 def _color_network(G):
     """Colors the network so that neighboring nodes all have distinct colors.
-       Returns a dict of set of nodes and also a lookup of nodes to colors, in 
-       a tuple in that order.
+
+       Returns a dict keyed by color to a set of nodes with that color.
     """
-    coloring = dict() # color => set(node)
+    coloring = dict()  # color => set(node)
     colors = nx.coloring.greedy_color(G)
     for node, color in colors.items():
         if color in coloring:
             coloring[color].add(node)
         else:
             coloring[color] = set([node])
-            
+
     return coloring
 
-def _labeling_complete(labeling, G):
-    """Determines whether or not LPA is done. It is complete when all nodes 
-       have a label that is in the set of highest frequency labels amongst its
-       neighbors.
 
-       Nodes with no neighbors are themselves a community and are therefore
-       labeled.
-    """    
-    return all(labeling[v] in 
-                    _most_frequent_labels(v, labeling, G) 
-                        for v in G if len(G[v]) > 0)
+def _labeling_complete(labeling, G):
+    """Determines whether or not LPA is done.
+
+       Label propagation is complete when all nodes have a label that is
+       in the set of highest frequency labels amongst its neighbors.
+
+       Nodes with no neighbors are considered complete.
+    """
+    return all(labeling[v] in _most_frequent_labels(v, labeling, G)
+               for v in G if len(G[v]) > 0)
+
 
 def _most_frequent_labels(node, labeling, G):
-    """Finds all labels of maximum frequency. Specified freqs must be a mapping
-       from label to frequency of that label.          
+    """Returns a set of all labels with maximum frequency in `labeling`.
 
-       Returns a set.
-    """   
-    if not G[node]: 
+       Input `labeling` should be a dict keyed by node to labels.
+    """
+    if not G[node]:
         # Nodes with no neighbors are themselves a community and are labeled
         # accordingly, hence the immediate if statement.
         return {labeling[node]}
 
-    # Compute the frequencies of all neighbours of node    
+    # Compute the frequencies of all neighbours of node
     freqs = Counter(labeling[q] for q in G[node])
     max_freq = max(freqs.values())
-    return {label for label, freq in freqs.items() if freq == max_freq}        
+    return {label for label, freq in freqs.items() if freq == max_freq}
 
-           
+
 def _update_label(node, labeling, G):
-    """Updates the label of a node using the Prec-Max tie breaking 
-       algorithm, as explained in: 'Community Detection via Semi-Synchronous 
+    """Updates the label of a node using the Prec-Max tie breaking algorithm
+
+       The algorithm is explained in: 'Community Detection via Semi-Synchronous
        Label Propagation Algorithms' Cordasco and Gargano, 2011
     """
     high_labels = _most_frequent_labels(node, labeling, G)
@@ -109,4 +113,5 @@ def _update_label(node, labeling, G):
         labeling[node] = high_labels.pop()
     elif len(high_labels) > 1:
         # Prec-Max
-        labeling[node] = labeling[node] if labeling[node] in high_labels else max(high_labels)
+        if labeling[node] not in high_labels:
+            labeling[node] = max(high_labels)
--- networkx/algorithms/community/tests/test_label_propagation.py
+++ networkx/algorithms/community/tests/test_label_propagation.py
@@ -1,27 +1,27 @@
-
 from nose.tools import *
+
 import networkx as nx
-from networkx import label_propagation
+from networkx.algorithms.community import label_propagation_communities
 
 
 @raises(nx.NetworkXNotImplemented)
 def test_directed_not_supported():
     # not supported for directed graphs
     test = nx.DiGraph()
-    test.add_edge('a','b')
-    test.add_edge('a','c')
-    test.add_edge('b','d')    
-    result = label_propagation.label_propagation_communities(test)
+    test.add_edge('a', 'b')
+    test.add_edge('a', 'c')
+    test.add_edge('b', 'd')
+    result = label_propagation_communities(test)
 
-    
+
 def test_one_node():
     test = nx.Graph()
     test.add_node('a')
-    
-    # The expected communities are:    
+
+    # The expected communities are:
     ground_truth = set([frozenset(['a'])])
-    
-    communities = label_propagation.label_propagation_communities(test)
+
+    communities = label_propagation_communities(test)
     result = {frozenset(c) for c in communities}
     assert_equal(result, ground_truth)
 
@@ -36,16 +36,16 @@ def test_unconnected_communities():
     test.add_edge('b', 'e')
     test.add_edge('e', 'f')
     test.add_edge('f', 'b')
-    
-    # The expected communities are:    
-    ground_truth = set([frozenset(['a', 'c', 'd']), 
+
+    # The expected communities are:
+    ground_truth = set([frozenset(['a', 'c', 'd']),
                         frozenset(['b', 'e', 'f'])])
-    
-    communities = label_propagation.label_propagation_communities(test)
-    result = {frozenset(c) for c in communities} 
+
+    communities = label_propagation_communities(test)
+    result = {frozenset(c) for c in communities}
     assert_equal(result, ground_truth)
 
-    
+
 def test_connected_communities():
     test = nx.Graph()
     # community 1
@@ -70,19 +70,19 @@ def test_connected_communities():
     test.add_edge('5', '2')
     test.add_edge('5', '3')
     test.add_edge('5', '4')
-    #edge between community 1 and 2    
+    # edge between community 1 and 2
     test.add_edge('a', '1')
     # community 3
     test.add_edge('x', 'y')
     # community 4 with only a single node
     test.add_node('z')
-    
-    # The expected communities are:    
-    ground_truth = set([frozenset(['a','b','c','d','e']), 
-                        frozenset(['1','2','3','4','5']), 
-                        frozenset(['x', 'y']), 
+
+    # The expected communities are:
+    ground_truth = set([frozenset(['a', 'b', 'c', 'd', 'e']),
+                        frozenset(['1', '2', '3', '4', '5']),
+                        frozenset(['x', 'y']),
                         frozenset(['z'])])
-                        
-    communities = label_propagation.label_propagation_communities(test)
-    result = {frozenset(c) for c in communities} 
+
+    communities = label_propagation_communities(test)
+    result = {frozenset(c) for c in communities}
     assert_equal(result, ground_truth)
