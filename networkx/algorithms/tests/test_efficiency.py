f740bf315358b9673d41349d53b979005df835e7
Aabir Abubaker Kar <aabir22@gmail.com>
2018-01-23 16:59:54 -0500

    Replaced nx.ego_graph() with G.subgraph() and made changes for PEP8

--- networkx/algorithms/tests/test_efficiency.py
+++ networkx/algorithms/tests/test_efficiency.py
@@ -53,7 +53,9 @@ class TestEfficiency:
 
     def test_local_efficiency_complete_graph(self):
         """
-        Test that the local efficiency for a complete graph with at least 3 nodes should be one. For a graph with only 2 nodes, the induced subgraph has no edges.
+        Test that the local efficiency for a complete graph with at least 3
+        nodes should be one. For a graph with only 2 nodes, the induced
+        subgraph has no edges.
         """
         for n in range(3, 10):
             G = nx.complete_graph(n)
