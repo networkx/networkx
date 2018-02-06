1302ce27f379ac3da8d67f0e7a8be138dfa469a9
msramalho <up201403027@fe.up.pt>
2018-02-06 18:39:06 +0000

    typo

--- networkx/algorithms/shortest_paths/dense.py
+++ networkx/algorithms/shortest_paths/dense.py
@@ -86,7 +86,7 @@ def floyd_warshall_predecessor_and_distance(G, weight='weight'):
     >>> G.add_weighted_edges_from([('s', 'u', 10), ('s', 'x', 5),
     ...     ('u', 'v', 1), ('u', 'x', 2), ('v', 'y', 1), ('x', 'u', 3),
     ...     ('x', 'v', 5), ('x', 'y', 2), ('y', 's', 7), ('y', 'v', 6)])
-    >>> predecessors, _ = nx.floyd_warshall_predecessor_and_distance(XG)
+    >>> predecessors, _ = nx.floyd_warshall_predecessor_and_distance(G)
     >>> print(reconstruct_path('s', 'v', predecessors))
     ['s', 'x', 'u', 'v']
 
