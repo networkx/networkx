e0b59af2cad659b1dcd8917f39753741a6b2194f
msramalho <up201403027@fe.up.pt>
2018-02-06 18:13:13 +0000

    updated docstring example

--- networkx/algorithms/shortest_paths/dense.py
+++ networkx/algorithms/shortest_paths/dense.py
@@ -84,10 +84,8 @@ def floyd_warshall_predecessor_and_distance(G, weight='weight'):
     --------
     >>> G = nx.DiGraph()
     >>> G.add_weighted_edges_from([('s', 'u', 10), ('s', 'x', 5),
-                                ('u', 'v', 1), ('u', 'x', 2),
-                                ('v', 'y', 1), ('x', 'u', 3),
-                                ('x', 'v', 5), ('x', 'y', 2),
-                                ('y', 's', 7), ('y', 'v', 6)])
+    ...     ('u', 'v', 1), ('u', 'x', 2), ('v', 'y', 1), ('x', 'u', 3),
+    ...     ('x', 'v', 5), ('x', 'y', 2), ('y', 's', 7), ('y', 'v', 6)])
     >>> predecessors, _ = nx.floyd_warshall_predecessor_and_distance(XG)
     >>> print(reconstruct_path('s', 'v', predecessors))
     ['s', 'x', 'u', 'v']
