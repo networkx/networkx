29580843086f0e9b9ab0af0c262f27f403717e93
Jean-Gabriel Young <jean.gabriel.young@gmail.com>
2018-03-15 13:51:10 -0400

    add SBM

--- networkx/generators/community.py
+++ networkx/generators/community.py
@@ -1,22 +1,24 @@
-"""Generators for classes of graphs used in studying social networks."""
-import itertools
-import math
-import random
-import networkx as nx
-#    Copyright(C) 2011, 2015 by
+#    Copyright(C) 2011, 2015, 2018 by
 #    Ben Edwards <bedwards@cs.unm.edu>
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Konstantinos Karakatsanis <dinoskarakas@gmail.com>
 #    All rights reserved.
 #    BSD license.
-__author__ = """\n""".join(['Ben Edwards (bedwards@cs.unm.edu)',
-                            'Aric Hagberg (hagberg@lanl.gov)',
-                            'Konstantinos Karakatsanis '
-                            '<dinoskarakas@gmail.com>'])
+#
+# Authors:  Ben Edwards (bedwards@cs.unm.edu)
+#           Aric Hagberg (hagberg@lanl.gov)
+#           Konstantinos Karakatsanis (dinoskarakas@gmail.com)
+#           Jean-Gabriel Young (jean.gabriel.young@gmail.com)
+"""Generators for classes of graphs used in studying social networks."""
+import itertools
+import math
+import random
+import networkx as nx
+
 __all__ = ['caveman_graph', 'connected_caveman_graph',
            'relaxed_caveman_graph', 'random_partition_graph',
            'planted_partition_graph', 'gaussian_random_partition_graph',
-           'ring_of_cliques', 'windmill_graph']
+           'ring_of_cliques', 'windmill_graph', 'stochastic_block_model']
 
 
 def caveman_graph(l, k):
@@ -417,14 +419,14 @@ def ring_of_cliques(num_cliques, clique_size):
     Parameters
     ----------
     num_cliques : int
-      Number of cliques
+        Number of cliques
     clique_size : int
-      Size of cliques
+        Size of cliques
 
     Returns
     -------
     G : NetworkX Graph
-      ring of cliques graph
+        ring of cliques graph
 
     Raises
     ------
@@ -474,14 +476,14 @@ def windmill_graph(n, k):
     Parameters
     ----------
     n : int
-      Number of cliques
+        Number of cliques
     k : int
-      Size of cliques
+        Size of cliques
 
     Returns
     -------
     G : NetworkX Graph
-      windmill graph with n cliques of size k
+        windmill graph with n cliques of size k
 
     Raises
     ------
@@ -510,3 +512,177 @@ def windmill_graph(n, k):
                                                for _ in range(n - 1))))
     G.add_edges_from((0, i) for i in range(k, G.number_of_nodes()))
     return G
+
+def stochastic_block_model(sizes, p, nodelist=None, seed=None,
+                           directed=False, selfloops=False, sparse=True):
+    """Return a stochastic block model graph.
+
+    This model partitions the nodes in blocks of arbitrary sizes, and places
+    edges between pairs of nodes independently, with a probability that depends
+    on the blocks.
+
+    Parameters
+    ----------
+    sizes : list of ints
+        Sizes of blocks
+    p : list of list of floats
+        Element (r,s) gives the density of edges going from the nodes
+        of group r to nodes of group s.
+        p must match the number of groups (len(sizes) == len(p)),
+        and it must be symmetric if the graph is undirected.
+    nodelist : list, optional
+        The block tags are assigned according to the node identifiers
+        in nodelist. If nodelist is None, then the ordering is the
+        range [0,sum(sizes)-1].
+    seed : int, optional,  default=None
+        Seed for random number generator.
+    directed : boolean optional, default=False
+        Whether to create a directed graph or not.
+    selfloops : boolean optional, default=False
+        Whether to include self-loops or not.
+    sparse: boolean optional, default=True
+        Use the sparse heuristic to speed up the generator.
+
+    Returns
+    -------
+    g : NetworkX Graph or DiGraph
+        Stochastic block model graph of size sum(sizes)
+
+    Raises
+    ------
+    NetworkXError
+      If probabilities are not in [0,1].
+      If the probability matrix is not square (directed case).
+      If the probability matrix is not symmetric (undirected case).
+      If the sizes list does not match nodelist or the probability matrix.
+      If nodelist contains duplicate.
+
+    Examples
+    --------
+    >>> from __future__ import division
+    >>> import numpy as np
+    >>> import networkx as nx
+    >>>
+    >>> sizes = [75, 75, 300]
+    >>> probs = [[0.25, 0.05, 0.02],
+    ...          [0.05, 0.35, 0.07],
+    ...          [0.02, 0.07, 0.40]]
+    >>> g = nx.stochastic_block_model(sizes, probs)
+    >>> len(g)
+    450
+    >>> H = nx.algorithms.blockmodel(g, g.graph['partition'])
+    >>> for v in H.nodes_iter(data=True):
+    ...     print v[1]['density']
+    ...
+    0.254414414414
+    0.357477477477
+    0.401672240803
+    >>> for v in H.edges_iter(data=True):
+    ...     print v[2]['weight'] / (sizes[v[0]] * sizes[v[1]])
+    ...
+    0.0483555555556
+    0.0195555555556
+    0.0728888888889
+
+    See Also
+    --------
+    random_partition_graph
+    planted_partition_graph
+    gaussian_random_partition_graph
+    gnp_random_graph
+
+    References
+    ----------
+    .. [1] Holland, P. W., Laskey, K. B., & Leinhardt, S.,
+           "Stochastic blockmodels: First steps",
+           Social networks, 5(2), 109-137, 1983.
+    """
+    # Check if dimensions match
+    if len(sizes) != len(p):
+        raise nx.NetworkXException("'sizes' and 'p' do not match.")
+    # Check for probability symmetry (undirected) and shape (directed)
+    for row in p:
+        if len(p) != len(row):
+            raise nx.NetworkXException("'p' must be a square matrix.")
+    if not directed:
+        p_transpose = [list(i) for i in zip(*p)]
+        for i in zip(p, p_transpose):
+            for j in zip(i[0], i[1]):
+                if abs(j[0] - j[1]) > 1e-08:
+                    raise nx.NetworkXException("'p' must be symmetric.")
+    # Check for probability range
+    for row in p:
+        for prob in row:
+            if prob < 0 or prob > 1:
+                raise nx.NetworkXException("Entries of 'p' not in [0,1].")
+    # Check for nodelist consistency
+    if nodelist is not None:
+        if len(nodelist) != sum(sizes):
+            raise nx.NetworkXException("'nodelist' and 'sizes' do not match.")
+        if len(nodelist) != len(set(nodelist)):
+            raise nx.NetworkXException("nodelist contains duplicate.")
+    else:
+        nodelist = range(0, sum(sizes))
+
+    # Setup the graph conditionally to the directed switch.
+    block_range = range(len(sizes))
+    if directed:
+        g = nx.DiGraph()
+        block_iter = itertools.product(block_range, block_range)
+    else:
+        g = nx.Graph()
+        block_iter = itertools.combinations_with_replacement(block_range, 2)
+    # Split nodelist in a partition (list of sets).
+    size_cumsum = [sum(sizes[0:x]) for x in range(0, len(sizes) + 1)]
+    g.graph['partition'] = [set(nodelist[size_cumsum[x]:size_cumsum[x + 1]])
+                            for x in range(0, len(size_cumsum) - 1)]
+    # Setup nodes and graph name
+    for block_id, nodes in enumerate(g.graph['partition']):
+        for node in nodes:
+            g.add_node(node, block=block_id)
+
+    g.name = "stochastic_block_model"
+
+    # Test for edge existence
+    if seed is not None:
+        random.seed(seed)
+
+    for i, j in block_iter:
+        if i == j:
+            if directed:
+                if selfloops:
+                    edges = itertools.product(g.graph['partition'][i],
+                                              g.graph['partition'][i])
+                else:
+                    edges = itertools.permutations(g.graph['partition'][i], 2)
+            else:
+                edges = itertools.combinations(g.graph['partition'][i], 2)
+                if selfloops:
+                    edges = itertools.chain(edges, zip(g.graph['partition'][i],
+                                                       g.graph['partition'][i]))  # noqa
+            for e in edges:
+                if random.random() < p[i][j]:
+                    g.add_edge(*e)
+        else:
+            edges = itertools.product(g.graph['partition'][i],
+                                      g.graph['partition'][j])
+        if sparse:
+            consume = lambda it, n: next(itertools.islice(it, n, n), None)
+            geo = lambda p: math.floor(math.log(random.random()) / math.log(1 - p))  # noqa
+            if p[i][j] == 1:  # Test edges cases p_ij = 0 or 1
+                for e in edges:
+                    g.add_edge(*e)
+            elif p[i][j] > 0:
+                while True:
+                    try:
+                        skip = geo(p[i][j])
+                        consume(edges, skip)
+                        e = next(edges)
+                        g.add_edge(*e)  # __safe
+                    except StopIteration:
+                        break
+        else:
+            for e in edges:
+                if random.random() < p[i][j]:
+                    g.add_edge(*e)  # __safe
+    return g
