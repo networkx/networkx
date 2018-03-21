3ec1dcc86c02ab9565bd7b5780fcc783e0163817
Jonathan Barnoud <jonathan@barnoud.net>
2018-03-21 17:50:07 +0100

    Avoid loosing the order of the nodes in subraphs
    * Make sure the tests catch the issue. The order of the nodes was
      already tested for subgraphs of OrderGraph. However, the nodes in the
      reference were sorted integer; as the faulty behavior was happened to
      be sorting the keys, the issue was not reported. The commit also makes
      the selection in the test redundant, as this was not considered
      before.
    * Fix `classes.filters.show_nodes` to keep the order of the selection.
    
    Fixes #2911

--- networkx/classes/tests/test_ordered.py
+++ networkx/classes/tests/test_ordered.py
@@ -20,19 +20,22 @@ class SmokeTestOrdered(object):
 class TestOrderedFeatures(object):
     def setUp(self):
         self.G = nx.OrderedDiGraph()
-        self.G.add_nodes_from([1, 2, 3])
+        # The nodes should not be in numerical order, some operations may
+        # sort the keys and expecting them sorted masks this unwanted
+        # behavior.
+        self.G.add_nodes_from([2, 3, 1])
         self.G.add_edges_from([(2, 3), (1, 3)])
 
     def test_subgraph_order(self):
         G = self.G
-        G_sub = G.subgraph([1, 2, 3])
+        G_sub = G.subgraph([2, 3, 1, 3])
         assert_equals(list(G.nodes), list(G_sub.nodes))
         assert_equals(list(G.edges), list(G_sub.edges))
         assert_equals(list(G.pred[3]), list(G_sub.pred[3]))
         assert_equals([2, 1], list(G_sub.pred[3]))
         assert_equals([], list(G_sub.succ[3]))
 
-        G_sub = nx.induced_subgraph(G, [1, 2, 3])
+        G_sub = nx.induced_subgraph(G, [2, 3, 1, 3])
         assert_equals(list(G.nodes), list(G_sub.nodes))
         assert_equals(list(G.edges), list(G_sub.edges))
         assert_equals(list(G.pred[3]), list(G_sub.pred[3]))
