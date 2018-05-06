06dddd7319ef26b58af9159aaa6e921b82b6da50
leamingrad <jamesowen@outlook.com>
2018-05-06 14:42:58 +0200

    Add test to check that not preserving attributes still works

--- networkx/algorithms/tree/tests/test_branchings.py
+++ networkx/algorithms/tree/tests/test_branchings.py
@@ -361,3 +361,19 @@ def test_edge_attribute_preservation():
     assert_equal(B[0][1][0]['otherattr'], 1)
     assert_equal(B[0][1][0]['otherattr2'], 3)
 
+
+def test_edge_attribute_discard():
+    # Test that edge attributes are discarded if we do not specify to keep them
+    G = nx.Graph()
+
+    edgelist = [(0, 1, [('weight', 5), ('otherattr', 1), ('otherattr2', 3)]),
+                (0, 2, [('weight', 5), ('otherattr', 2), ('otherattr2', 2)]),
+                (1, 2, [('weight', 6), ('otherattr', 3), ('otherattr2', 1)])]
+    G.add_edges_from(edgelist)
+
+    ed = branchings.Edmonds(G)
+    B = ed.find_optimum('weight', preserve_attrs=False)
+
+    edge_dict = B[0][1]
+    with assert_raises(KeyError):
+        _ = edge_dict['otherattr']
