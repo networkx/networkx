2a014e4e43cf054c402e4ef6f7a6970e43623974
Dan Schult <dschult@colgate.edu>
2018-06-02 18:18:53 -0400

    Comment out tests involving speed due to Travis variability

--- networkx/algorithms/components/tests/test_strongly_connected.py
+++ networkx/algorithms/components/tests/test_strongly_connected.py
@@ -156,27 +156,28 @@ class TestStronglyConnected:
         # deprecated
         assert_raises(NetworkXNotImplemented, nx.strongly_connected_component_subgraphs, G)
 
-    def test_linear_time(self):
-        # See Issue #2831
-        count = 100  # base case
-        dg = nx.DiGraph()
-        dg.add_nodes_from([0, 1])
-        for i in range(2, count):
-            dg.add_node(i)
-            dg.add_edge(i, 1)
-            dg.add_edge(0, i)
-        t = time.time()
-        ret = tuple(nx.strongly_connected_components(dg))
-        dt = time.time() - t
-
-        count = 200
-        dg = nx.DiGraph()
-        dg.add_nodes_from([0, 1])
-        for i in range(2, count):
-            dg.add_node(i)
-            dg.add_edge(i, 1)
-            dg.add_edge(0, i)
-        t = time.time()
-        ret = tuple(nx.strongly_connected_components(dg))
-        dt2 = time.time() - t
-        assert_less(dt2, dt * 2.3)  # should be 2 times longer for this graph
+#    Commented out due to variability on Travis-CI hardware/operating systems
+#    def test_linear_time(self):
+#        # See Issue #2831
+#        count = 100  # base case
+#        dg = nx.DiGraph()
+#        dg.add_nodes_from([0, 1])
+#        for i in range(2, count):
+#            dg.add_node(i)
+#            dg.add_edge(i, 1)
+#            dg.add_edge(0, i)
+#        t = time.time()
+#        ret = tuple(nx.strongly_connected_components(dg))
+#        dt = time.time() - t
+#
+#        count = 200
+#        dg = nx.DiGraph()
+#        dg.add_nodes_from([0, 1])
+#        for i in range(2, count):
+#            dg.add_node(i)
+#            dg.add_edge(i, 1)
+#            dg.add_edge(0, i)
+#        t = time.time()
+#        ret = tuple(nx.strongly_connected_components(dg))
+#        dt2 = time.time() - t
+#        assert_less(dt2, dt * 2.3)  # should be 2 times longer for this graph
