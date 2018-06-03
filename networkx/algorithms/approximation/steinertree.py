df1ea00aec0015dc27df7c93e395dc0f80fae79c
Dan Schult <dschult@colgate.edu>
2018-06-03 00:10:39 -0400

    Add check to metric_closure for is_connected
    Fixes #2979

--- networkx/algorithms/approximation/steinertree.py
+++ networkx/algorithms/approximation/steinertree.py
@@ -25,11 +25,22 @@ def metric_closure(G, weight='weight'):
     """
     M = nx.Graph()
 
-    seen = set()
     Gnodes = set(G)
-    for u, (distance, path) in nx.all_pairs_dijkstra(G, weight=weight):
-        seen.add(u)
-        for v in Gnodes - seen:
+
+    # check for connected graph while processing first node
+    all_paths_iter = nx.all_pairs_dijkstra(G, weight=weight)
+    u, (distance, path) = next(all_paths_iter)
+    if Gnodes - set(distance):
+        msg = "G is not a connected graph. metric_closure is not defined."
+        raise nx.NetworkXError(msg)
+    Gnodes.remove(u)
+    for v in Gnodes:
+        M.add_edge(u, v, distance=distance[v], path=path[v])
+
+    # first node done -- now process the rest
+    for u, (distance, path) in all_paths_iter:
+        Gnodes.remove(u)
+        for v in Gnodes:
             M.add_edge(u, v, distance=distance[v], path=path[v])
 
     return M
