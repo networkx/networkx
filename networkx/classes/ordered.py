451baf29a2646112431178af1d5720874503b88d
Dan Schult <dschult@colgate.edu>
2018-06-16 12:13:28 -0400

    Add docs to orderedgraph module about subgraph
    also add words to make more clear the expectations for these graph classes.
    Fixes #2911

--- networkx/classes/ordered.py
+++ networkx/classes/ordered.py
@@ -1,5 +1,10 @@
 """
 Consistently ordered variants of the default base classes.
+Note that if you are using Python 3.6, you shouldn't need these classes
+because the dicts in Python 3.6 are ordered.
+Note also that there are many differing expectations for the word "ordered"
+and that these classes may not provide the order you expect.
+The intent here is to give a consistent order not a particular order.
 
 The Ordered (Di/Multi/MultiDi) Graphs give a consistent order for reporting of
 nodes and edges.  The order of node reporting agrees with node adding, but for
@@ -8,6 +13,17 @@ edges, the order is not necessarily the order that the edges were added.
 In general, you should use the default (i.e., unordered) graph classes.
 However, there are times (e.g., when testing) when you may need the
 order preserved.
+
+Special care is required when using subgraphs of the Ordered classes.
+The order of nodes in the subclass is not necessarily the same order
+as the original class.  In general it is probably better to avoid using
+subgraphs and replace with code similar to:
+
+    # instead of SG = G.subgraph(ordered_nodes)
+    SG=nx.OrderedGraph()
+    SG.add_nodes_from(ordered_nodes)
+    SG.add_edges_from((u, v) for (u, v) in G.edges() if u in SG if v in SG)
+
 """
 from collections import OrderedDict
 
