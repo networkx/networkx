ea16c9b04b7a3f9a9be16b80895725069b24153e
Dan Schult <dschult@colgate.edu>
2018-07-16 16:18:56 -0400

    add binary mode to view_pygraphviz. (#3069)
    Fixes #3060

--- networkx/drawing/nx_agraph.py
+++ networkx/drawing/nx_agraph.py
@@ -301,7 +301,7 @@ def pygraphviz_layout(G, prog='neato', root=None, args=''):
     return node_pos
 
 
-@nx.utils.open_file(5, 'w')
+@nx.utils.open_file(5, 'w+b')
 def view_pygraphviz(G, edgelabel=None, prog='dot', args='',
                     suffix='', path=None):
     """Views the graph G using the specified layout algorithm.
