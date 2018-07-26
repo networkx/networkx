8a0b5eec9740cd02e9e8e096b5ffbd1a899b1848
Robert Gmyr <robert@gmyr.net>
2018-07-26 09:02:55 -0500

    Add more elaborate tests

--- networkx/algorithms/tests/test_sparsification.py
+++ networkx/algorithms/tests/test_sparsification.py
@@ -2,39 +2,85 @@ import networkx as nx
 import random
 
 
-def test_spanner_unweighted():
-    n = 100
-    stretch = 3
-
-    G = nx.complete_graph(n)
-    spanner = nx.spanner(G, stretch)
-
-    print(G.number_of_edges())
-    print(spanner.number_of_edges())
-
-    length_G = dict(nx.shortest_path_length(G))
-    length_spanner = dict(nx.shortest_path_length(spanner))
-
+def _test_spanner(G, spanner, stretch, weight=None):
+    original_length = dict(nx.shortest_path_length(G, weight=weight))
+    spanner_length = dict(nx.shortest_path_length(spanner, weight=weight))
     for u in G.nodes():
         for v in G.nodes():
-            assert length_spanner[u][v] <= stretch * length_G[u][v]
+            if u in original_length and v in original_length[u]:
+                assert spanner_length[u][v] <= stretch * original_length[u][v]
 
 
-def test_spanner_weighted():
-    n = 1000
-    stretch = 4
+def test_spanner_node_set():
+    G = nx.complete_graph(100)
+    spanner = nx.spanner(G, 4)
+    assert set(G.nodes()) == set(spanner.nodes())
 
-    G = nx.complete_graph(n)
+
+def test_spanner_unweighted_complete_graph():
+    G = nx.complete_graph(100)
+
+    spanner = nx.spanner(G, 1)
+    _test_spanner(G, spanner, 1)
+
+    spanner = nx.spanner(G, 4)
+    _test_spanner(G, spanner, 4)
+
+    spanner = nx.spanner(G, 10)
+    _test_spanner(G, spanner, 10)
+
+
+def test_spanner_weighted_complete_graph():
+    G = nx.complete_graph(100)
     for u, v in G.edges():
         G[u][v]['weight'] = random.random()
-    spanner = nx.spanner(G, stretch, weight='weight')
 
-    print(G.number_of_edges())
-    print(spanner.number_of_edges())
+    spanner = nx.spanner(G, 1, weight='weight')
+    _test_spanner(G, spanner, 1, weight='weight')
 
-    length_G = dict(nx.shortest_path_length(G, weight='weight'))
-    length_spanner = dict(nx.shortest_path_length(spanner, weight='weight'))
+    spanner = nx.spanner(G, 4, weight='weight')
+    _test_spanner(G, spanner, 4, weight='weight')
 
-    for u in G.nodes():
-        for v in G.nodes():
-            assert length_spanner[u][v] <= stretch * length_G[u][v]
+    spanner = nx.spanner(G, 10, weight='weight')
+    _test_spanner(G, spanner, 10, weight='weight')
+
+
+def test_spanner_unweighted_gnp_graph():
+    G = nx.gnp_random_graph(100, 0.2)
+
+    spanner = nx.spanner(G, 1)
+    _test_spanner(G, spanner, 1)
+
+    spanner = nx.spanner(G, 4)
+    _test_spanner(G, spanner, 4)
+
+    spanner = nx.spanner(G, 10)
+    _test_spanner(G, spanner, 10)
+
+
+def test_spanner_weighted_gnp_graph():
+    G = nx.gnp_random_graph(100, 0.2)
+    for u, v in G.edges():
+        G[u][v]['weight'] = random.random()
+
+    spanner = nx.spanner(G, 1)
+    _test_spanner(G, spanner, 1)
+
+    spanner = nx.spanner(G, 4)
+    _test_spanner(G, spanner, 4)
+
+    spanner = nx.spanner(G, 10)
+    _test_spanner(G, spanner, 10)
+
+
+def test_spanner_unweighted_disconnected_graph():
+    G = nx.disjoint_union(nx.complete_graph(50), nx.complete_graph(50))
+
+    spanner = nx.spanner(G, 1)
+    _test_spanner(G, spanner, 1)
+
+    spanner = nx.spanner(G, 4)
+    _test_spanner(G, spanner, 4)
+
+    spanner = nx.spanner(G, 10)
+    _test_spanner(G, spanner, 10)
