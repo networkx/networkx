ebc3bf26d132fa8b4972d894adb16aeab185b789
Graham Fetterman <graham.fetterman@gmail.com>
2018-07-31 19:22:59 -0500

    Re-remove absent-source guards post-#3103

--- networkx/algorithms/shortest_paths/generic.py
+++ networkx/algorithms/shortest_paths/generic.py
@@ -154,8 +154,6 @@ def shortest_path(G, source=None, target=None, weight=None, method='dijkstra'):
                 for target in paths:
                     paths[target] = list(reversed(paths[target]))
     else:
-        if source not in G:
-            raise nx.NodeNotFound("Source {} not in G".format(source))
         if target is None:
             # Find paths to all nodes accessible from the source.
             if method == 'unweighted':
@@ -298,8 +296,6 @@ def shortest_path_length(G,
                     path_length = nx.single_source_bellman_ford_path_length
                     paths = path_length(G, target, weight=weight)
     else:
-        if source not in G:
-            raise nx.NodeNotFound("Source {} not in G".format(source))
         if target is None:
             # Find paths to all nodes accessible from the source.
             if method == 'unweighted':
