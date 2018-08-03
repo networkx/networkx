bd4f28b1f0fdbee953a92624434cfcedfa60091c
Jarrod Millman <jarrod.millman@gmail.com>
2018-08-03 15:43:58 -0500

    Work around for xdg-open on xenial

--- tools/travis/linux_install.sh
+++ tools/travis/linux_install.sh
@@ -12,20 +12,7 @@ if [[ "${OPTIONAL_DEPS}" == 1 ]]; then
   export C_INCLUDE_PATH=/usr/include/gdal
 
   # needed for view_graphviz and default_opener
-  DIR=~/.local/share/applications
-  mkdir -p $DIR
-  FILE=$DIR/png.desktop
-  cat <<EOF >$FILE
-[Desktop Entry]
-Name=png
-MimeType=image/png;
-Exec=/usr/bin/file
-Type=Application
-Terminal=true
-NoDisplay=true
-EOF
-
-  xdg-mime default png.desktop image/png
+  alias xdg-open="file"
 
 fi
 
