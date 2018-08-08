90dbaaf01d82db83f3a5391a3166c6ded292e979
Robert Gmyr <robert@gmyr.net>
2018-08-08 13:52:44 +0200

    Refactoring and comments

--- networkx/algorithms/sparsification.py
+++ networkx/algorithms/sparsification.py
@@ -56,38 +56,43 @@ def baswana_sen_spanner(G, k, weight=None):
                 continue
 
             # step 2: find neighboring (sampled) clusters and lightest edges to them
-            lightest_edge_weight, lightest_edge_neighbor = _lightest_edge_dicts(residual_graph, clustering, v)
+            lightest_edge_neighbor, lightest_edge_weight = _lightest_edge_dicts(residual_graph, clustering, v)
             neighboring_sampled_centers = set(lightest_edge_weight.keys()) & sampled_centers
 
             # step 3: add edges to spanner
             if not neighboring_sampled_centers:
+                # connect to each neighboring center via lightest edge
                 for neighbor in lightest_edge_neighbor.values():
                     _add_edge_to_spanner(H, residual_graph, v, neighbor, weight)
+                # remove all incident edges
                 for neighbor in residual_graph.adj[v]:
                     edges_to_delete.add((v, neighbor))
 
             else:  # there is a neighboring sampled center
-                closest_center = _sampled_center_with_lightest_edge(lightest_edge_weight, neighboring_sampled_centers)
+                closest_center = _closest_sampled_center(lightest_edge_weight, neighboring_sampled_centers)
                 closest_center_weight = lightest_edge_weight[closest_center]
                 closest_center_neighbor = lightest_edge_neighbor[closest_center]
 
                 _add_edge_to_spanner(H, residual_graph, v, closest_center_neighbor, weight)
                 new_clustering[v] = closest_center
 
+                # connect to centers with edge weight less than closest_center_weight
                 for center, edge_weight in lightest_edge_weight.items():
                     if edge_weight < closest_center_weight:
                         neighbor = lightest_edge_neighbor[center]
                         _add_edge_to_spanner(H, residual_graph, v, neighbor, weight)
 
+                # remove edges to centers with edge weight less than closest_center_weight
                 for neighbor in residual_graph.adj[v]:
                     neighbor_cluster = clustering[neighbor]
-                    if neighbor_cluster is closest_center or lightest_edge_weight[neighbor_cluster] < closest_center_weight:
+                    neighbor_weight = lightest_edge_weight[neighbor_cluster]
+                    if neighbor_cluster is closest_center or neighbor_weight < closest_center_weight:
                         edges_to_delete.add((v, neighbor))
 
         # actually delete edges from residual graph
         residual_graph.remove_edges_from(edges_to_delete)
 
-        # update clustering
+        # copy old clustering data to new_clustering
         for node, center in clustering.items():
             if center in sampled_centers:
                 new_clustering[node] = center
@@ -106,7 +111,7 @@ def baswana_sen_spanner(G, k, weight=None):
 
     # phase 2: vertex-cluster joining
     for v in residual_graph.nodes:
-        lightest_edge_weight, lightest_edge_neighbor = _lightest_edge_dicts(residual_graph, clustering, v)
+        lightest_edge_neighbor, _ = _lightest_edge_dicts(residual_graph, clustering, v)
         for neighbor in lightest_edge_neighbor.values():
             _add_edge_to_spanner(H, residual_graph, v, neighbor, weight)
 
@@ -132,27 +137,28 @@ def _setup_residual_graph(G, weight):
 
 
 def _lightest_edge_dicts(residual_graph, clustering, node):
-    lightest_edge_weight = {}
     lightest_edge_neighbor = {}
+    lightest_edge_weight = {}
+    center = clustering[node]
     for neighbor in residual_graph.adj[node]:
-        if clustering[neighbor] is clustering[node]:
+        neighbor_center = clustering[neighbor]
+        if neighbor_center is center:
             continue
         weight = residual_graph[node][neighbor]['weight']
-        neighbor_center = clustering[neighbor]
         if neighbor_center not in lightest_edge_weight or weight < lightest_edge_weight[neighbor_center]:
-            lightest_edge_weight[neighbor_center] = weight
             lightest_edge_neighbor[neighbor_center] = neighbor
-    return lightest_edge_weight, lightest_edge_neighbor
+            lightest_edge_weight[neighbor_center] = weight
+    return lightest_edge_neighbor, lightest_edge_weight
 
 
-def _sampled_center_with_lightest_edge(lightest_edge_to_cluster_weight, neighboring_sampled_centers):
-    lightest_edge_to_sampled_cluster_weight = _infinite_edge_weight()
-    lightest_edge_to_sampled_cluster_center = None
+def _closest_sampled_center(lightest_edge_weight, neighboring_sampled_centers):
+    closest_center = None
+    closest_weight = _infinite_edge_weight()
     for center in neighboring_sampled_centers:
-        if lightest_edge_to_cluster_weight[center] < lightest_edge_to_sampled_cluster_weight:
-            lightest_edge_to_sampled_cluster_weight = lightest_edge_to_cluster_weight[center]
-            lightest_edge_to_sampled_cluster_center = center
-    return lightest_edge_to_sampled_cluster_center
+        if lightest_edge_weight[center] < closest_weight:
+            closest_center = center
+            closest_weight = lightest_edge_weight[center]
+    return closest_center
 
 
 def _add_edge_to_spanner(H, residual_graph, u, v, weight):
