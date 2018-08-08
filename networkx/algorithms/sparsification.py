730e86aa25f16bd1dc394807acb6eb48255e7547
Robert Gmyr <robert@gmyr.net>
2018-08-08 13:14:18 +0200

    Simplify spanner and test code

--- networkx/algorithms/sparsification.py
+++ networkx/algorithms/sparsification.py
@@ -5,27 +5,44 @@ import random
 __all__ = ['spanner']
 
 
-def spanner(G, stretch, weight=None):
+def _stretch_to_k(stretch):
+    return int(math.floor((stretch + 1) / 2))
+
+
+def _spanner_expected_number_of_edges(number_of_nodes, stretch):
+    k = _stretch_to_k(stretch)
+    return k * math.pow(number_of_nodes, 1 + 1 / k)
+
+
+def spanner(G, stretch, weight=None, limit_size=True):
     if stretch < 1:
         raise ValueError('stretch must be at least 1')
-    k = int(math.floor((stretch + 1) / 2))
-    return baswana_sen_spanner(G, k, weight)
+
+    k = _stretch_to_k(stretch)
+    H = baswana_sen_spanner(G, k, weight)
+
+    if limit_size:
+        size_limit = 2 * _spanner_expected_number_of_edges(G.number_of_nodes(), stretch)
+        while H.number_of_edges() > size_limit:
+            # TODO: add reference to paper
+            # this loop runs for O(1) iterations with high probability
+            H = baswana_sen_spanner(G, k, weight)
+
+    return H
 
 
 def baswana_sen_spanner(G, k, weight=None):
-    # initialize empty spanner H
+    # initialize spanner H with empty edge set
     H = nx.empty_graph()
     H.add_nodes_from(G.nodes)
 
     # phase 1: forming the clusters
-    residual_graph = G.copy()
-    _make_edge_weights_distinct(residual_graph, weight)
-
+    residual_graph = _setup_residual_graph(G, weight)
     clustering = {v: v for v in G.nodes}
+    sample_prob = math.pow(G.number_of_nodes(), - 1 / k)
 
     for _ in range(k - 1):
         # step 1: sample centers
-        sample_prob = math.pow(G.number_of_nodes(), - 1 / k)
         sampled_centers = set()
         for center in clustering.values():
             if random.random() < sample_prob:
@@ -39,41 +56,32 @@ def baswana_sen_spanner(G, k, weight=None):
                 continue
 
             # step 2: find neighboring (sampled) clusters and lightest edges to them
-            lightest_edge_to_cluster_weight, lightest_edge_to_cluster_neighbor = _lightest_edge_to_clusters_dicts(residual_graph, clustering, v)
-            neighboring_sampled_centers = set(lightest_edge_to_cluster_weight.keys()) & sampled_centers
+            lightest_edge_weight, lightest_edge_neighbor = _lightest_edge_dicts(residual_graph, clustering, v)
+            neighboring_sampled_centers = set(lightest_edge_weight.keys()) & sampled_centers
 
             # step 3: add edges to spanner
             if not neighboring_sampled_centers:
-                for neighbor in lightest_edge_to_cluster_neighbor.values():
-                    H.add_edge(v, neighbor)
-                    if weight:
-                        H[v][neighbor][weight] = residual_graph[v][neighbor]['weight'][0]
+                for neighbor in lightest_edge_neighbor.values():
+                    _add_edge_to_spanner(H, residual_graph, v, neighbor, weight)
                 for neighbor in residual_graph.adj[v]:
                     edges_to_delete.add((v, neighbor))
 
-            else:
-                lightest_edge_to_sampled_cluster_weight = _infinite_edge_weight()
-                lightest_edge_to_sampled_cluster_center = None
-                for center in neighboring_sampled_centers:
-                    if lightest_edge_to_cluster_weight[center] < lightest_edge_to_sampled_cluster_weight:
-                        lightest_edge_to_sampled_cluster_weight = lightest_edge_to_cluster_weight[center]
-                        lightest_edge_to_sampled_cluster_center = center
-                neighbor = lightest_edge_to_cluster_neighbor[lightest_edge_to_sampled_cluster_center]
-                H.add_edge(v, neighbor)
-                if weight:
-                    H[v][neighbor][weight] = residual_graph[v][neighbor]['weight'][0]
-                new_clustering[v] = lightest_edge_to_sampled_cluster_center
+            else:  # there is a neighboring sampled center
+                closest_center = _sampled_center_with_lightest_edge(lightest_edge_weight, neighboring_sampled_centers)
+                closest_center_weight = lightest_edge_weight[closest_center]
+                closest_center_neighbor = lightest_edge_neighbor[closest_center]
 
-                for center, edge_weight in lightest_edge_to_cluster_weight.items():
-                    if edge_weight < lightest_edge_to_sampled_cluster_weight:
-                        neighbor = lightest_edge_to_cluster_neighbor[center]
-                        H.add_edge(v, neighbor)
-                        if weight:
-                            H[v][neighbor][weight] = residual_graph[v][neighbor]['weight'][0]
+                _add_edge_to_spanner(H, residual_graph, v, closest_center_neighbor, weight)
+                new_clustering[v] = closest_center
 
-                # simplify this
+                for center, edge_weight in lightest_edge_weight.items():
+                    if edge_weight < closest_center_weight:
+                        neighbor = lightest_edge_neighbor[center]
+                        _add_edge_to_spanner(H, residual_graph, v, neighbor, weight)
+
                 for neighbor in residual_graph.adj[v]:
-                    if clustering[neighbor] is clustering[lightest_edge_to_sampled_cluster_center] or lightest_edge_to_cluster_weight[clustering[neighbor]] < lightest_edge_to_sampled_cluster_weight:
+                    neighbor_cluster = clustering[neighbor]
+                    if neighbor_cluster is closest_center or lightest_edge_weight[neighbor_cluster] < closest_center_weight:
                         edges_to_delete.add((v, neighbor))
 
         # actually delete edges from residual graph
@@ -98,29 +106,32 @@ def baswana_sen_spanner(G, k, weight=None):
 
     # phase 2: vertex-cluster joining
     for v in residual_graph.nodes:
-        lightest_edge_to_cluster_weight, lightest_edge_to_cluster_neighbor =\
-            _lightest_edge_to_clusters_dicts(residual_graph, clustering, v)
-        for neighbor in lightest_edge_to_cluster_neighbor.values():
-            H.add_edge(v, neighbor)
-            if weight:
-                H[v][neighbor][weight] = residual_graph[v][neighbor]['weight'][0]
+        lightest_edge_weight, lightest_edge_neighbor = _lightest_edge_dicts(residual_graph, clustering, v)
+        for neighbor in lightest_edge_neighbor.values():
+            _add_edge_to_spanner(H, residual_graph, v, neighbor, weight)
 
     return H
 
 
+# TODO: make this some kind of static variable?
 def _infinite_edge_weight():
     return math.inf, math.inf, math.inf
 
 
-def _make_edge_weights_distinct(G, weight):
+def _setup_residual_graph(G, weight):
+    residual_graph = G.copy()
+
+    # establish unique edge weights, even for unweighted graphs
     for u, v in G.edges():
         if not weight:
-            G[u][v]['weight'] = (id(u), id(v), 0)
+            residual_graph[u][v]['weight'] = (id(u), id(v), 0)
         else:
-            G[u][v]['weight'] = (G[u][v][weight], id(u), id(v))
+            residual_graph[u][v]['weight'] = (G[u][v][weight], id(u), id(v))
+
+    return residual_graph
 
 
-def _lightest_edge_to_clusters_dicts(residual_graph, clustering, node):
+def _lightest_edge_dicts(residual_graph, clustering, node):
     lightest_edge_weight = {}
     lightest_edge_neighbor = {}
     for neighbor in residual_graph.adj[node]:
@@ -132,3 +143,19 @@ def _lightest_edge_to_clusters_dicts(residual_graph, clustering, node):
             lightest_edge_weight[neighbor_center] = weight
             lightest_edge_neighbor[neighbor_center] = neighbor
     return lightest_edge_weight, lightest_edge_neighbor
+
+
+def _sampled_center_with_lightest_edge(lightest_edge_to_cluster_weight, neighboring_sampled_centers):
+    lightest_edge_to_sampled_cluster_weight = _infinite_edge_weight()
+    lightest_edge_to_sampled_cluster_center = None
+    for center in neighboring_sampled_centers:
+        if lightest_edge_to_cluster_weight[center] < lightest_edge_to_sampled_cluster_weight:
+            lightest_edge_to_sampled_cluster_weight = lightest_edge_to_cluster_weight[center]
+            lightest_edge_to_sampled_cluster_center = center
+    return lightest_edge_to_sampled_cluster_center
+
+
+def _add_edge_to_spanner(H, residual_graph, u, v, weight):
+    H.add_edge(u, v)
+    if weight:
+        H[u][v][weight] = residual_graph[u][v]['weight'][0]
--- networkx/algorithms/tests/test_sparsification.py
+++ networkx/algorithms/tests/test_sparsification.py
@@ -4,6 +4,16 @@ import random
 
 
 def _test_spanner(G, spanner, stretch, weight=None):
+    # check node set
+    assert set(G.nodes()) == set(spanner.nodes())
+
+    # check edge set and weights
+    for u, v in spanner.edges():
+        assert G.has_edge(u, v)
+        if weight:
+            assert spanner[u][v][weight] == G[u][v][weight]
+
+    # check connectivity and stretch
     original_length = dict(nx.shortest_path_length(G, weight=weight))
     spanner_length = dict(nx.shortest_path_length(spanner, weight=weight))
     for u in G.nodes():
@@ -12,17 +22,16 @@ def _test_spanner(G, spanner, stretch, weight=None):
                 assert spanner_length[u][v] <= stretch * original_length[u][v]
 
 
-def test_spanner_node_set():
+def test_spanner_trivial():
     G = nx.complete_graph(100)
-    spanner = nx.spanner(G, 4)
-    assert set(G.nodes()) == set(spanner.nodes())
+    spanner = nx.spanner(G, 1)
+
+    for u, v in G.edges:
+        assert(spanner.has_edge(u, v))
 
 
 def test_spanner_unweighted_complete_graph():
     G = nx.complete_graph(100)
-
-    spanner = nx.spanner(G, 1)
-    _test_spanner(G, spanner, 1)
 
     spanner = nx.spanner(G, 4)
     _test_spanner(G, spanner, 4)
@@ -36,9 +45,6 @@ def test_spanner_weighted_complete_graph():
     for u, v in G.edges():
         G[u][v]['weight'] = random.random()
 
-    spanner = nx.spanner(G, 1, weight='weight')
-    _test_spanner(G, spanner, 1, weight='weight')
-
     spanner = nx.spanner(G, 4, weight='weight')
     _test_spanner(G, spanner, 4, weight='weight')
 
@@ -48,9 +54,6 @@ def test_spanner_weighted_complete_graph():
 
 def test_spanner_unweighted_gnp_graph():
     G = nx.gnp_random_graph(100, 0.2)
-
-    spanner = nx.spanner(G, 1)
-    _test_spanner(G, spanner, 1)
 
     spanner = nx.spanner(G, 4)
     _test_spanner(G, spanner, 4)
@@ -64,21 +67,15 @@ def test_spanner_weighted_gnp_graph():
     for u, v in G.edges():
         G[u][v]['weight'] = random.random()
 
-    spanner = nx.spanner(G, 1)
-    _test_spanner(G, spanner, 1)
+    spanner = nx.spanner(G, 4, weight='weight')
+    _test_spanner(G, spanner, 4, weight='weight')
 
-    spanner = nx.spanner(G, 4)
-    _test_spanner(G, spanner, 4)
-
-    spanner = nx.spanner(G, 10)
-    _test_spanner(G, spanner, 10)
+    spanner = nx.spanner(G, 10, weight='weight')
+    _test_spanner(G, spanner, 10, weight='weight')
 
 
 def test_spanner_unweighted_disconnected_graph():
     G = nx.disjoint_union(nx.complete_graph(50), nx.complete_graph(50))
-
-    spanner = nx.spanner(G, 1)
-    _test_spanner(G, spanner, 1)
 
     spanner = nx.spanner(G, 4)
     _test_spanner(G, spanner, 4)
