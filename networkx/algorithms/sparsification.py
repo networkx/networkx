3c30f7f2fe1ab5557ba4c2852395a6b850368b0e
Robert Gmyr <robert@gmyr.net>
2018-08-09 17:29:16 +0200

    Fix critical bug in algorithm and implement better size restriction

--- networkx/algorithms/sparsification.py
+++ networkx/algorithms/sparsification.py
@@ -10,28 +10,7 @@ def spanner(G, stretch, weight=None, limit_size=True):
         raise ValueError('stretch must be at least 1')
 
     k = _stretch_to_k(stretch)
-    H = _baswana_sen_spanner(G, k, weight)
 
-    if limit_size:
-        size_limit = 2 * _spanner_expected_number_of_edges(G.number_of_nodes(), stretch)
-        while H.number_of_edges() > size_limit:
-            # TODO: add reference to paper
-            # this loop runs for O(1) iterations with high probability
-            H = _baswana_sen_spanner(G, k, weight)
-
-    return H
-
-
-def _stretch_to_k(stretch):
-    return int(math.floor((stretch + 1) / 2))
-
-
-def _spanner_expected_number_of_edges(number_of_nodes, stretch):
-    k = _stretch_to_k(stretch)
-    return k * math.pow(number_of_nodes, 1 + 1 / k)
-
-
-def _baswana_sen_spanner(G, k, weight=None):
     # initialize spanner H with empty edge set
     H = nx.empty_graph()
     H.add_nodes_from(G.nodes)
@@ -40,16 +19,19 @@ def _baswana_sen_spanner(G, k, weight=None):
     residual_graph = _setup_residual_graph(G, weight)
     clustering = {v: v for v in G.nodes}
     sample_prob = math.pow(G.number_of_nodes(), - 1 / k)
+    size_limit = 2 * math.pow(G.number_of_nodes(), 1 + 1 / k)
 
-    for _ in range(k - 1):
+    i = 0
+    while i < k - 1:
         # step 1: sample centers
         sampled_centers = set()
-        for center in clustering.values():
+        for center in set(clustering.values()):
             if random.random() < sample_prob:
                 sampled_centers.add(center)
 
         # combined loop for steps 2 and 3
-        edges_to_delete = set()
+        edges_to_add = set()
+        edges_to_remove = set()
         new_clustering = {}
         for v in residual_graph.nodes:
             if clustering[v] in sampled_centers:
@@ -63,34 +45,45 @@ def _baswana_sen_spanner(G, k, weight=None):
             if not neighboring_sampled_centers:
                 # connect to each neighboring center via lightest edge
                 for neighbor in lightest_edge_neighbor.values():
-                    _add_edge_to_spanner(H, residual_graph, v, neighbor, weight)
+                    edges_to_add.add((v, neighbor))
                 # remove all incident edges
                 for neighbor in residual_graph.adj[v]:
-                    edges_to_delete.add((v, neighbor))
+                    edges_to_remove.add((v, neighbor))
 
             else:  # there is a neighboring sampled center
                 closest_center = _closest_sampled_center(lightest_edge_weight, neighboring_sampled_centers)
                 closest_center_weight = lightest_edge_weight[closest_center]
                 closest_center_neighbor = lightest_edge_neighbor[closest_center]
 
-                _add_edge_to_spanner(H, residual_graph, v, closest_center_neighbor, weight)
+                edges_to_add.add((v, closest_center_neighbor))
                 new_clustering[v] = closest_center
 
                 # connect to centers with edge weight less than closest_center_weight
                 for center, edge_weight in lightest_edge_weight.items():
                     if edge_weight < closest_center_weight:
                         neighbor = lightest_edge_neighbor[center]
-                        _add_edge_to_spanner(H, residual_graph, v, neighbor, weight)
+                        edges_to_add.add((v, neighbor))
 
                 # remove edges to centers with edge weight less than closest_center_weight
                 for neighbor in residual_graph.adj[v]:
                     neighbor_cluster = clustering[neighbor]
                     neighbor_weight = lightest_edge_weight[neighbor_cluster]
                     if neighbor_cluster is closest_center or neighbor_weight < closest_center_weight:
-                        edges_to_delete.add((v, neighbor))
+                        edges_to_remove.add((v, neighbor))
+
+        # check whether iteration added too many edges to spanner, if so repeat
+        if limit_size and len(edges_to_add) > size_limit:
+            continue
+
+        # iteration succeeded
+        i = i + 1
+
+        # actually add edges to spanner
+        for u, v in edges_to_add:
+            _add_edge_to_spanner(H, residual_graph, u, v, weight)
 
         # actually delete edges from residual graph
-        residual_graph.remove_edges_from(edges_to_delete)
+        residual_graph.remove_edges_from(edges_to_remove)
 
         # copy old clustering data to new_clustering
         for node, center in clustering.items():
@@ -106,7 +99,7 @@ def _baswana_sen_spanner(G, k, weight=None):
 
         # update residual graph node set
         for v in list(residual_graph.nodes):
-            if v not in clustering and residual_graph.degree[v] == 0:
+            if v not in clustering:
                 residual_graph.remove_node(v)
 
     # phase 2: vertex-cluster joining
@@ -116,6 +109,10 @@ def _baswana_sen_spanner(G, k, weight=None):
             _add_edge_to_spanner(H, residual_graph, v, neighbor, weight)
 
     return H
+
+
+def _stretch_to_k(stretch):
+    return int(math.floor((stretch + 1) / 2))
 
 
 def _setup_residual_graph(G, weight):
