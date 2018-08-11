aeb6ac4940334e79568f87b8705c94792f711e6a
Robert Gmyr <robert@gmyr.net>
2018-08-11 13:37:22 +0200

    Docstrings for tests

--- networkx/algorithms/tests/test_spanner.py
+++ networkx/algorithms/tests/test_spanner.py
@@ -5,6 +5,28 @@ import random
 
 
 def _test_spanner(G, spanner, stretch, weight=None):
+    """Test whether a spanner is valid.
+
+        This function tests whether the given spanner is a subgraph of the given graph G
+        with the same node set. It also tests for all shortest paths whether they adhere
+        to the given stretch.
+
+        Parameters
+        ----------
+
+        G : NetworkX graph
+            The original graph for which the spanner was constructed.
+
+        spanner : NetworkX graph
+            The spanner to be tested.
+
+        stretch : float
+            The proclaimed stretch of the spanner.
+
+        weight : object
+            The edge attribute to use as distance.
+
+    """
     # check node set
     assert set(G.nodes()) == set(spanner.nodes())
 
@@ -24,6 +46,7 @@ def _test_spanner(G, spanner, stretch, weight=None):
 
 
 def test_spanner_trivial():
+    """Test a trivial spanner with stretch 1."""
     G = nx.complete_graph(100)
     spanner = nx.spanner(G, 1)
 
@@ -32,6 +55,7 @@ def test_spanner_trivial():
 
 
 def test_spanner_unweighted_complete_graph():
+    """Test spanner construction on a complete unweighted graph."""
     G = nx.complete_graph(100)
 
     spanner = nx.spanner(G, 4)
@@ -42,6 +66,7 @@ def test_spanner_unweighted_complete_graph():
 
 
 def test_spanner_weighted_complete_graph():
+    """Test spanner construction on a complete weighted graph."""
     G = nx.complete_graph(100)
     for u, v in G.edges():
         G[u][v]['weight'] = random.random()
@@ -54,6 +79,7 @@ def test_spanner_weighted_complete_graph():
 
 
 def test_spanner_unweighted_gnp_graph():
+    """Test spanner construction on an unweighted gnp graph."""
     G = nx.gnp_random_graph(100, 0.2)
 
     spanner = nx.spanner(G, 4)
@@ -64,6 +90,7 @@ def test_spanner_unweighted_gnp_graph():
 
 
 def test_spanner_weighted_gnp_graph():
+    """Test spanner construction on an weighted gnp graph."""
     G = nx.gnp_random_graph(100, 0.2)
     for u, v in G.edges():
         G[u][v]['weight'] = random.random()
@@ -76,6 +103,7 @@ def test_spanner_weighted_gnp_graph():
 
 
 def test_spanner_unweighted_disconnected_graph():
+    """Test spanner construction on a disconnected graph."""
     G = nx.disjoint_union(nx.complete_graph(50), nx.complete_graph(50))
 
     spanner = nx.spanner(G, 4)
@@ -87,5 +115,6 @@ def test_spanner_unweighted_disconnected_graph():
 
 @raises(ValueError)
 def test_spanner_invalid_stretch():
+    """Check whether an invalid stretch is caught."""
     G = nx.empty_graph()
     nx.spanner(G, 0)
