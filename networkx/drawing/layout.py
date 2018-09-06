02d546ad94635e2eb8725fcdf0e39bbd84094c5b
frhyme <freerhein@gmail.com>
2018-09-06 16:48:00 +0900

    add new function rescale_layout_dict again

--- networkx/drawing/layout.py
+++ networkx/drawing/layout.py
@@ -34,6 +34,7 @@ __all__ = ['bipartite_layout',
            'kamada_kawai_layout',
            'random_layout',
            'rescale_layout',
+           'rescale_layout_dict', 
            'shell_layout',
            'spring_layout',
            'spectral_layout',
@@ -882,6 +883,10 @@ def rescale_layout(pos, scale=1):
     pos : numpy array
         scaled positions. Each row is a position.
 
+    See Also
+    --------
+    rescale_layout_dict
+
     """
     # Find max length over all dimensions
     lim = 0  # max coordinate for all axes
@@ -908,9 +913,15 @@ def rescale_layout_dict(pos, scale=1):
     -------
     pos : A dictionary of positions keyed by node
 
+    See Also
+    --------
+    rescale_layout
     """
-    pos_v = np.array([(px, py) for px, py in pos.values()])
-    pos_v = rescale_layout(pos_v, scale=scale)
+    if len(pos)==0:
+        ## the empty graph 
+        return {}
+    pos_v = np.array(list(pos.values()))
+    pos_v = rescale_layout(pos_v, scale=scale
     return {k: tuple(v) for k, v in zip(pos.keys(), pos_v)}
 
 # fixture for nose tests
