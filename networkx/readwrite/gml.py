854e008223d5f12fce50dd051f2eab11ef60db25
Clayton A Davis <cad@clayadavis.net>
2018-10-05 19:30:17 -0400

    Fix reading labels from gml files (#3184)
    * Bring parse_gml() behavior in line with docs when label=None
    
    * Add tests for parse_gml() label= keyword behavior

--- networkx/readwrite/gml.py
+++ networkx/readwrite/gml.py
@@ -415,17 +415,18 @@ def parse_gml_lines(lines, label, destringizer):
 
     nodes = graph.get('node', [])
     mapping = {}
-    labels = set()
+    node_labels = set()
     for i, node in enumerate(nodes if isinstance(nodes, list) else [nodes]):
         id = pop_attr(node, 'node', 'id', i)
         if id in G:
             raise NetworkXError('node id %r is duplicated' % (id,))
-        if label != 'id':
-            label = pop_attr(node, 'node', 'label', i)
-            if label in labels:
-                raise NetworkXError('node label %r is duplicated' % (label,))
-            labels.add(label)
-            mapping[id] = label
+        if label is not None and label != 'id':
+            node_label = pop_attr(node, 'node', label, i)
+            if node_label in node_labels:
+                raise NetworkXError('node label %r is duplicated' %
+                                    (node_label,))
+            node_labels.add(node_label)
+            mapping[id] = node_label
         G.add_node(id, **node)
 
     edges = graph.get('edge', [])
@@ -455,7 +456,7 @@ Hint:  If this is a multigraph, add "multigraph 1" to the header of the file."""
                     (i, source, '->' if directed else '--', target, key))
             G.add_edge(source, target, key, **edge)
 
-    if label != 'id':
+    if label is not None and label != 'id':
         G = nx.relabel_nodes(G, mapping)
     return G
 
