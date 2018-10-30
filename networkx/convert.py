1b0cd4d2835a5b5ce51b6a1b11d5bd38ae8aafb5
stsouko <stsouko@live.ru>
2018-10-30 10:56:49 +0300

    tests and comments added.

--- networkx/convert.py
+++ networkx/convert.py
@@ -79,6 +79,9 @@ def to_networkx_graph(data, create_using=None, multigraph_input=False):
             if hasattr(data, 'graph'):  # data.graph should be dict-like
                 result.graph.update(data.graph)
             if hasattr(data, 'nodes'):  # data.nodes should be dict-like
+                # result.add_node_from(data.nodes.items()) possible but
+                # for custom node_attr_dict_factory which may be hashable
+                # will be unexpected behavior
                 for n, dd in data.nodes.items():
                     result._node[n].update(dd)
             return result
