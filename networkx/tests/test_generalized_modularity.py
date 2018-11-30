b33afb34119becfc59ed333777c7603c4d0316ff
Xiaoyan Lu <xiaoyan.lu.xl@gmail.com>
2018-11-29 19:44:16 -0500

    Add generalized modularity

--- networkx/tests/test_generalized_modularity.py
+++ networkx/tests/test_generalized_modularity.py
@@ -9,11 +9,11 @@ from networkx.algorithms.community import modularity, greedy_modularity_communit
 
 class TestGeneralizedModularity():
     def test_modularity(self):
-        # test that empty graph converts fine for all options
-        G = barbell_graph(3, 0)
-        m = modularity(G, [{0, 1, 2}, {3, 4, 5}])
-
+        # test if the genealized modularity maximization algorithm works fine 
+        # by comparing results 
         G = karate_club_graph()
         c1 = list(greedy_modularity_communities(G, 1.8))
         c2 = list(_naive_greedy_modularity_communities(G, 1.8))
-        assert_equal(sorted(c1[0]), sorted(c2[0]))
+        assert_equal(len(c1), len(c2))
+        for i in range(len(c1)):
+          assert_equal(sorted(c1[i]), sorted(c2[i]))
