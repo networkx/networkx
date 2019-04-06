ead94ef8b27ddf462d74807e9ab0d461e4732622
Jarrod Millman <jarrod.millman@gmail.com>
2019-04-06 10:45:48 -0700

    Add project_urls to setup

--- networkx/release.py
+++ networkx/release.py
@@ -200,7 +200,11 @@ authors = {'Hagberg': ('Aric Hagberg', 'hagberg@lanl.gov'),
 maintainer = "NetworkX Developers"
 maintainer_email = "networkx-discuss@googlegroups.com"
 url = 'http://networkx.github.io/'
-download_url = 'https://github.com/networkx/networkx/releases'
+project_urls={
+    "Bug Tracker": "https://github.com/networkx/networkx/issues",
+    "Documentation": "https://networkx.github.io/documentation/stable/",
+    "Source Code": "https://github.com/networkx/networkx",
+}
 platforms = ['Linux', 'Mac OSX', 'Windows', 'Unix']
 keywords = ['Networks', 'Graph Theory', 'Mathematics',
             'network', 'graph', 'discrete mathematics', 'math']
