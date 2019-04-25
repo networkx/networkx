231d720fddd04ba4e5eab53f53bca235744ae5b0
Jean-Gabriel Young <jean.gabriel.young@gmail.com>
2019-04-25 11:14:50 -0400

    Better coverage

--- networkx/linalg/tests/test_bethehessian.py
+++ networkx/linalg/tests/test_bethehessian.py
@@ -31,11 +31,15 @@ class TestBetheHessian(object):
         H = numpy.array([[ 4, -2,  0],
                           [-2,  5, -2],
                           [ 0, -2,  4]])
-
         permutation = [2, 0, 1]
-        print(nx.bethe_hessian_matrix(self.P, r=2).todense())
+        # Bethe Hessian gives expected form
         assert_equal(nx.bethe_hessian_matrix(self.P, r=2).todense(), H)
+        # nodelist is correctly implemented
         assert_equal(nx.bethe_hessian_matrix(self.P, r=2, nodelist=permutation).todense(),
                      H[numpy.ix_(permutation, permutation)])
+        # Equal to Laplacian matrix when r=1
         assert_equal(nx.bethe_hessian_matrix(self.G, r=1).todense(),
                      nx.laplacian_matrix(self.G).todense())
+        # Correct default for the regularizer r
+        assert_equal(nx.bethe_hessian_matrix(self.G).todense(),
+                     nx.bethe_hessian_matrix(self.G, r=1.25).todense())
