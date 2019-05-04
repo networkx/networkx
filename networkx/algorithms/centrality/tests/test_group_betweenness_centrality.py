c273c4ac10cde84c2c208b1906ec47e888016d13
nandahkrishna <nanda.harishankar@gmail.com>
2019-05-05 02:00:21 +0530

    Added more tests

--- networkx/algorithms/centrality/tests/test_group_betweenness_centrality.py
+++ networkx/algorithms/centrality/tests/test_group_betweenness_centrality.py
@@ -31,3 +31,26 @@ class TestGroupBetweennessCentrality:
                                             weight=None, normalized=True)
         b_answer = 1.0
         assert_equal(b, b_answer)
+
+    def test_group_betweenness_centrality_3(self):
+        """
+            Group betweenness centrality value of 0
+        """
+        G = nx.cycle_graph(6)
+        C = [0, 1, 5]
+        b = nx.group_betweenness_centrality(G, C,
+                                            weight=None, normalized=False)
+        b_answer = 0.0
+        assert_equal(b, b_answer)
+
+    def test_group_betweenness_centrality_4(self):
+        """
+            Group betweenness centrality in a disconnected graph
+        """
+        G = nx.path_graph(5)
+        G.remove_edge(0, 1)
+        C = [0, 1, 2]
+        b = nx.group_betweenness_centrality(G, C,
+                                            weight=None, normalized=False)
+        b_answer = 0.0
+        assert_equal(b, b_answer)
