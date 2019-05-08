ad7573d776cadd208131fd472a147e8fb0cea8d1
haakonhr <haakonhr@gmail.com>
2019-05-08 11:10:13 +0200

    Split is_at_free method
    The previous method had an extra parameter, `certificate`, that could be used
    to decide whether the output should be a boolean value or a tuple with a
    boolean value and a certificate. This has now been split into two functions:
    `is_at_free` and `find_asteroidal_triple`, for more intuitive behaviour.

--- networkx/algorithms/asteroidal.py
+++ networkx/algorithms/asteroidal.py
@@ -116,6 +116,39 @@ def is_at_free(G, certificate=False):
 
     if certificate:
         return (True, None)
+@not_implemented_for("directed")
+@not_implemented_for("multigraph")
+def is_at_free(G):
+    """Check if a graph is AT-free.
+
+    The method uses the `find_asteroidal_triple` method to recognize
+    an AT-free graph. If no asteroidal triple is found the graph is
+    AT-free and True is returned. If at least one asteroidal triple is
+    found the graph is not AT-free and False is returned.
+
+    Parameters
+    ----------
+    G : NetworkX Graph
+        The graph to check whether is AT-free or not.
+
+    Returns
+    -------
+    bool
+        True if G is AT-free and False otherwise.
+
+    Examples
+    --------
+    >>> G = nx.Graph([(0, 1), (0, 2), (1, 2), (1, 3), (1, 4), (4, 5)])
+    >>> nx.is_at_free(G)
+    True
+
+    >>> G = nx.cycle_graph(6)
+    >>> nx.is_at_free(G)
+    False
+    """
+    asteroidal_triple = find_asteroidal_triple(G)
+    if asteroidal_triple:
+        return False
     else:
         return True
 
