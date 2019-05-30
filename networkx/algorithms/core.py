86c5c8c290b70f9de8c11083af632ec685cf0a6b
Antoine Allard <antoine.allard.1@gmail.com>
2019-05-30 16:34:57 -0400

    added details to the documentation ofthe new function onion_layers to core.py

--- networkx/algorithms/core.py
+++ networkx/algorithms/core.py
@@ -433,7 +433,8 @@ def onion_layers(G):
     """Returns the layer of each vertex in the onion decomposition of the graph.
 
     The onion decomposition refines the k-core decomposition by providing
-    information on the internal organization of each k-shell.
+    information on the internal organization of each k-shell. It is usually
+    used alongside the `core numbers`.
 
     Parameters
     ----------
@@ -443,7 +444,8 @@ def onion_layers(G):
     Returns
     -------
     od_layers : dictionary
-       A dictionary keyed by vertex to the onion layer.
+       A dictionary keyed by vertex to the onion layer. The layers are
+       contiguous integers starting at 1.
 
     Raises
     ------
