bfdba362fd300c5d880ba8f133277bb3623bdc04
Antoine Allard <antoine.allard.1@gmail.com>
2019-05-30 17:34:57 -0400

    added the test function test_onion_layers in file test_core.py

--- networkx/algorithms/tests/test_core.py
+++ networkx/algorithms/tests/test_core.py
@@ -141,3 +141,14 @@ class TestCore:
         # k=3
         k_truss_subgraph = nx.k_truss(self.G, 3)
         assert_equal(sorted(k_truss_subgraph.nodes()), [])
+
+    def test_onion_layers(self):
+        layers = nx.onion_layers(self.G)
+        nodes_by_layer = [sorted([n for n in layers if layers[n] == val])
+                          for val in range(1, 7)]
+        assert_nodes_equal(nodes_by_layer[0], [21])
+        assert_nodes_equal(nodes_by_layer[1], [17, 18, 19, 20])
+        assert_nodes_equal(nodes_by_layer[2], [10, 12, 13, 14, 15, 16])
+        assert_nodes_equal(nodes_by_layer[3], [9, 11])
+        assert_nodes_equal(nodes_by_layer[4], [1, 2, 4, 5, 6, 8])
+        assert_nodes_equal(nodes_by_layer[5], [3, 7])
