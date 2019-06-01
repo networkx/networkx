23e1d9cad13f2714229396b8deadf9bbf4b05c40
Brian Kiefer <bkiefer@asu.edu>
2019-06-01 13:16:21 -0400

    Fixed prior merge error

--- networkx/algorithms/distance_measures.py
+++ networkx/algorithms/distance_measures.py
@@ -16,7 +16,8 @@ import networkx as nx
 from networkx.utils import not_implemented_for
 
 __all__ = ['extrema_bounding', 'eccentricity', 'diameter',
-           'radius', 'periphery', 'center', 'resistance_distance']
+           'radius', 'periphery', 'center', 'barycenter',
+           'resistance_distance']
 
 
 def extrema_bounding(G, compute="diameter"):
@@ -368,6 +369,69 @@ def center(G, e=None, usebounds=False):
     radius = min(e.values())
     p = [v for v in e if e[v] == radius]
     return p
+
+
+def barycenter(G, weight=None, attr=None, sp=None):
+    r"""Calculate barycenter of a connected graph, optionally with edge weights.
+    The :dfn:`barycenter` a
+    :func:`connected <networkx.algorithms.components.is_connected>` graph
+    :math:`G` is the subgraph induced by the set of its nodes :math:`v`
+    minimizing the objective function
+    .. math::
+        \sum_{u \in V(G)} d_G(u, v),
+    where :math:`d_G` is the (possibly weighted) :func:`path length
+    <networkx.algorithms.shortest_paths.generic.shortest_path_length>`.
+    The barycenter is also called the :dfn:`median`. See [West01]_, p. 78.
+    Parameters
+    ----------
+    G : :class:`networkx.Graph`
+        The connected graph :math:`G`.
+    weight : :class:`str`, optional
+        Passed through to
+        :func:`~networkx.algorithms.shortest_paths.generic.shortest_path_length`.
+    attr : :class:`str`, optional
+        If given, write the value of the objective function to each node's
+        `attr` attribute. Otherwise do not store the value.
+    sp : dict of dicts, optional
+       All pairs shortest path lengths as a dictionary of dictionaries
+    Returns
+    -------
+    :class:`list`
+        Nodes of `G` that induce the barycenter of `G`.
+    Raises
+    ------
+    :exc:`networkx.NetworkXNoPath`
+        If `G` is disconnected. `G` may appear disconnected to
+        :func:`barycenter` if `sp` is given but is missing shortest path
+        lengths for any pairs.
+    :exc:`ValueError`
+        If `sp` and `weight` are both given.
+    See Also
+    --------
+    center
+    periphery
+    """
+    if sp is None:
+        sp = nx.shortest_path_length(G, weight=weight)
+    else:
+        sp = sp.items()
+        if weight is not None:
+            raise ValueError('Cannot use both sp, weight arguments together')
+    smallest, barycenter_vertices, n = float('inf'), [], len(G)
+    for v, dists in sp:
+        if len(dists) < n:
+            raise nx.NetworkXNoPath(
+                ("Input graph %r is disconnected, so every induced subgraph "
+                 "has infinite barycentricity.") % G)
+        barycentricity = sum(dists.values())
+        if attr is not None:
+            G.nodes[v][attr] = barycentricity
+        if barycentricity < smallest:
+            smallest = barycentricity
+            barycenter_vertices = [v]
+        elif barycentricity == smallest:
+            barycenter_vertices.append(v)
+    return barycenter_vertices
 
 
 def _laplacian_submatrix(node, mat, node_list):
