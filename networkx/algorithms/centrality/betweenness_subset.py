ce1b3b4eb985d263a2aa315a5469cd3cafda5331
nandahkrishna <nanda.harishankar@gmail.com>
2019-06-21 11:51:55 +0530

    Removed 0.5 factor in rescale

--- networkx/algorithms/centrality/betweenness_subset.py
+++ networkx/algorithms/centrality/betweenness_subset.py
@@ -235,12 +235,6 @@ def _rescale(betweenness, n, normalized, directed=False):
             scale = None  # no normalization b=0 for all nodes
         else:
             scale = 1.0 / ((n - 1) * (n - 2))
-    else:  # rescale by 2 for undirected graphs
-        if not directed:
-            scale = 0.5
-        else:
-            scale = None
-    if scale is not None:
         for v in betweenness:
             betweenness[v] *= scale
     return betweenness
