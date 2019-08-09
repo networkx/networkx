d51285d9eb94a3a9a47314d1f34382b8f9ebb75d
Tobias Blass <tobias.blass@de.bosch.com>
2019-08-09 15:00:17 +0200

    When exporting to GML, write non 32-bit numbers as strings.
    According to the GML technical report, integers in GML must fit into a
    signed 32-bit integer. Values outside this range have to be written as
    strings instead.

--- networkx/readwrite/gml.py
+++ networkx/readwrite/gml.py
@@ -667,6 +667,9 @@ def generate_gml(G, stringizer=None):
                     yield indent + key + ' 1'
                 elif value is False:
                     yield indent + key + ' 0'
+                # GML only supports signed 32-bit integers
+                elif value < -2**31 or value >= 2**31:
+                    yield indent + key + ' "' + str(value) + '"'
                 else:
                     yield indent + key + ' ' + str(value)
             elif isinstance(value, float):
