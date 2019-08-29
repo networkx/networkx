48157dec60f79f7dd935b4b14d60379bb2404e67
Zachary Manesiotis <zack.manesiotis@gmail.com>
2019-08-29 12:42:18 -0400

    update str formatting

--- networkx/algorithms/flow/dinitz_alg.py
+++ networkx/algorithms/flow/dinitz_alg.py
@@ -150,9 +150,9 @@ def dinitz(G, s, t, capacity='capacity', residual=None, value_only=False, cutoff
 
 def dinitz_impl(G, s, t, capacity, residual, cutoff):
     if s not in G:
-        raise nx.NetworkXError('node %s not in graph' % str(s))
+        raise nx.NetworkXError('node {} not in graph'.format(str(s)))
     if t not in G:
-        raise nx.NetworkXError('node %s not in graph' % str(t))
+        raise nx.NetworkXError('node {} not in graph'.format(str(t)))
     if s == t:
         raise nx.NetworkXError('source and sink are the same node')
 
