2f98f9b18dd877343416f3cf3acb9db68832bdde
Zachary Manesiotis <zack.manesiotis@gmail.com>
2019-08-29 12:43:17 -0400

    update str formatting

--- networkx/algorithms/flow/edmondskarp.py
+++ networkx/algorithms/flow/edmondskarp.py
@@ -105,9 +105,9 @@ def edmonds_karp_impl(G, s, t, capacity, residual, cutoff):
     """Implementation of the Edmonds-Karp algorithm.
     """
     if s not in G:
-        raise nx.NetworkXError('node %s not in graph' % str(s))
+        raise nx.NetworkXError('node {} not in graph'.format(str(s)))
     if t not in G:
-        raise nx.NetworkXError('node %s not in graph' % str(t))
+        raise nx.NetworkXError('node {} not in graph'.format(str(t)))
     if s == t:
         raise nx.NetworkXError('source and sink are the same node')
 
