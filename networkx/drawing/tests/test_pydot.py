ea944e45e13a483ff91a25e917549bb1abac4e49
PalAditya <adityapal.nghss@gmail.com>
2019-09-29 00:17:32 +0530

    Implementing code feedback

--- networkx/drawing/tests/test_pydot.py
+++ networkx/drawing/tests/test_pydot.py
@@ -78,9 +78,8 @@ class TestPydot(object):
             (e.get_source(), e.get_destination()) for e in P.get_edge_list()])
 
         # Sorted list of all edges in the original "pydot.Dot" instance.
-        e2 = sorted([
-            (e.get_source(), e.get_destination())
-            for e in Pin.get_edge_list()])
+        e2 = sorted([(e.get_source(), e.get_destination())
+                     for e in Pin.get_edge_list()])
 
         # Validate these instances to contain the same edges.
         assert_equal(e1, e2)
