47b1031a45e889034aa7385a97711fb9f97243ca
Dan Schult <dschult@colgate.edu>
2019-09-29 00:41:53 -0400

    Fixes #3211 allowing generators to represent partitions
    Update docs. Add tests of is_partition

--- networkx/algorithms/community/tests/test_utils.py
+++ networkx/algorithms/community/tests/test_utils.py
@@ -19,6 +19,9 @@ from networkx.algorithms.community import is_partition
 def test_is_partition():
     G = nx.empty_graph(3)
     assert_true(is_partition(G, [{0, 1}, {2}]))
+    assert_true(is_partition(G, ({0, 1}, {2})))
+    assert_true(is_partition(G, ([0, 1], [2])))
+    assert_true(is_partition(G, [[0, 1], [2]]))
 
 
 def test_not_covering():
@@ -29,3 +32,8 @@ def test_not_covering():
 def test_not_disjoint():
     G = nx.empty_graph(3)
     assert_false(is_partition(G, [{0, 1}, {1, 2}]))
+
+
+def test_not_node():
+    G = nx.empty_graph(3)
+    assert_false(is_partition(G, [{0, 1}, {3}]))
