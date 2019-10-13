cf3481f7c2f6c706f5e5da4b1dc4d80ba1e6a28c
Jarrod Millman <jarrod.millman@gmail.com>
2019-10-12 18:04:42 -0700

    Announce Python 3.8 support

--- networkx/release.py
+++ networkx/release.py
@@ -209,6 +209,7 @@ classifiers = [
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3 :: Only',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: Scientific/Engineering :: Bio-Informatics',
