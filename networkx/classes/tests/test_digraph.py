e144d5d1f99eaffd7ddb790ea032376f36bf25f1
Dan Schult <dschult@colgate.edu>
2019-10-23 00:10:37 -0400

    fix pycodestyle for test_digraph.py

--- networkx/classes/tests/test_digraph.py
+++ networkx/classes/tests/test_digraph.py
@@ -137,7 +137,8 @@ class BaseDiGraphTester(BaseGraphTester):
 class BaseAttrDiGraphTester(BaseDiGraphTester, BaseAttrGraphTester):
     def test_edges_data(self):
         G = self.K3
-        all_edges = [(0, 1, {}), (0, 2, {}), (1, 0, {}), (1, 2, {}), (2, 0, {}), (2, 1, {})]
+        all_edges = [(0, 1, {}), (0, 2, {}), (1, 0, {}),
+                     (1, 2, {}), (2, 0, {}), (2, 1, {})]
         assert sorted(G.edges(data=True)) == all_edges
         assert sorted(G.edges(0, data=True)) == all_edges[:2]
         assert sorted(G.edges([0, 1], data=True)) == all_edges[:4]
