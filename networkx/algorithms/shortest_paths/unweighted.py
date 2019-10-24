9293e0d18260eb0fd901d6d85808d4ed79429eed
Jonatan Westholm <jonatanwestholm@gmail.com>
2019-10-24 02:41:02 +0200

    Optimize _single_shortest_path_length (#3647)
    * pass nosetests and flake8
    
    * set+found implementation

--- networkx/algorithms/shortest_paths/unweighted.py
+++ networkx/algorithms/shortest_paths/unweighted.py
@@ -79,18 +79,23 @@ def _single_shortest_path_length(adj, firstlevel, cutoff):
         cutoff : int or float
             level at which we stop the process
     """
-    seen = {}                  # level (number of hops) when seen in BFS
-    level = 0                  # the current level
-    nextlevel = firstlevel     # dict of nodes to check at next level
-
+    seen = {}  # level (number of hops) when seen in BFS
+    level = 0  # the current level
+    nextlevel = set(firstlevel)  # set of nodes to check at next level
+    n = len(adj)
     while nextlevel and cutoff >= level:
         thislevel = nextlevel  # advance to next level
-        nextlevel = {}         # and start a new list (fringe)
+        nextlevel = set([])  # and start a new set (fringe)
+        found = []
         for v in thislevel:
             if v not in seen:
                 seen[v] = level  # set the level of vertex v
-                nextlevel.update(adj[v])  # add neighbors of v
+                found.append(v)
                 yield (v, level)
+        if len(seen) == n:
+            return
+        for v in found:
+            nextlevel.update(adj[v])
         level += 1
     del seen
 
