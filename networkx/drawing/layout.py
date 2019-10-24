27fc48ffc6330a582929a5b378f8705269cf3a5c
Don Kirkby <donkirkby@users.noreply.github.com>
2019-10-24 12:04:11 -0700

    Fix typo: Laplacion -> Laplacian
    Thanks for the great library! Here's a little typo fix.

--- networkx/drawing/layout.py
+++ networkx/drawing/layout.py
@@ -765,7 +765,7 @@ def _kamada_kawai_costfn(pos_vec, np, invdist, meanweight, dim):
 def spectral_layout(G, weight='weight', scale=1, center=None, dim=2):
     """Position nodes using the eigenvectors of the graph Laplacian.
 
-    Using the unnormalized Laplacion, the layout shows possible clusters of
+    Using the unnormalized Laplacian, the layout shows possible clusters of
     nodes which are an approximation of the ratio cut. If dim is the number of
     dimensions then the positions are the entries of the dim eigenvectors
     corresponding to the ascending eigenvalues starting from the second one.
