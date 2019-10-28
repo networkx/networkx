42beadf580accceec5e30aa4fd4d2613f62075f4
Stefan van der Walt <stefanv@berkeley.edu>
2019-10-28 11:46:30 -0700

    Remove shim that worked around using starmap

--- examples/advanced/plot_parallel_betweenness.py
+++ examples/advanced/plot_parallel_betweenness.py
@@ -30,26 +30,17 @@ def chunks(l, n):
         yield x
 
 
-def _betmap(G_normalized_weight_sources_tuple):
-    """Pool for multiprocess only accepts functions with one argument.
-    This function uses a tuple as its only argument. We use a named tuple for
-    python 3 compatibility, and then unpack it when we send it to
-    `betweenness_centrality_source`
-    """
-    return nx.betweenness_centrality_source(*G_normalized_weight_sources_tuple)
-
-
 def betweenness_centrality_parallel(G, processes=None):
     """Parallel betweenness centrality  function"""
     p = Pool(processes=processes)
     node_divisor = len(p._pool) * 4
     node_chunks = list(chunks(G.nodes(), int(G.order() / node_divisor)))
     num_chunks = len(node_chunks)
-    bt_sc = p.map(_betmap,
-                  zip([G] * num_chunks,
-                      [True] * num_chunks,
-                      [None] * num_chunks,
-                      node_chunks))
+    bt_sc = p.starmap(nx.betweenness_centrality_source,
+                      zip([G] * num_chunks,
+                        [True] * num_chunks,
+                        [None] * num_chunks,
+                        node_chunks))
 
     # Reduce the partial solutions
     bt_c = bt_sc[0]
