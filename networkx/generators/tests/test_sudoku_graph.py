f7d3cb8f3d1e7fb1d02e11e301590bcead316098
David Radcliffe <dradcliffe@gmail.com>
2019-12-23 21:24:21 -0600

    Parameterize tests for sudoku_graph

--- networkx/generators/tests/test_sudoku_graph.py
+++ networkx/generators/tests/test_sudoku_graph.py
@@ -9,49 +9,29 @@ def test_sudoku_negative():
     pytest.raises(nx.NetworkXError, nx.sudoku_graph, n=-1)
 
 
-def test_sudoku_empty():
-    """Generate an empty Sudoku graph."""
-    G = nx.sudoku_graph(0)
-    assert G.is_directed() is False
-    assert G.is_multigraph() is False
-    assert nx.is_empty(G) is True
+@pytest.mark.parametrize('n', [0, 1, 2, 3, 4])
+def test_sudoku_generator(n):
+    """Generate Sudoku graphs of various sizes and verify that they have the expected properties."""
+    G = nx.sudoku_graph(n)
+    expected_nodes = n ** 4
+    expected_degree = (n - 1) * (3 * n + 1)
+    expected_edges = expected_nodes * expected_degree // 2
+    assert not G.is_directed()
+    assert not G.is_multigraph()
+    assert G.number_of_nodes() == expected_nodes
+    assert G.number_of_edges() == expected_edges
+    assert all(d == expected_degree for _, d in G.degree)
 
-
-def test_sudoku_1x1():
-    """Generate a 1-Sudoku graph (1x1) and verify its properties."""
-    G = nx.sudoku_graph(1)
-    assert G.is_directed() is False
-    assert G.is_multigraph() is False
-    assert G.number_of_nodes() == 1
-    assert G.number_of_edges() == 0
-
-
-def test_sudoku_4x4():
-    """Generate a 2-Sudoku graph (4x4) and verify its properties."""
-    G = nx.sudoku_graph(2)
-    assert G.is_directed() is False
-    assert G.is_multigraph() is False
-    assert G.number_of_nodes() == 16
-    assert G.number_of_edges() == 56
-    assert all(d[1] == 7 for d in G.degree)
-    assert sorted(G.neighbors(6)) == [2, 3, 4, 5, 7, 10, 14]
-
-
-def test_sudoku_9x9():
-    """Generate a 3-Sudoku graph (9x9) and verify its properties."""
-    G = nx.sudoku_graph()
-    assert G.is_directed() is False
-    assert G.is_multigraph() is False
-    assert G.number_of_nodes() == 81
-    assert G.number_of_edges() == 810
-    assert all(d[1] == 20 for d in G.degree)
-    assert sorted(G.neighbors(42)) == [
-        6, 15, 24, 33, 34, 35, 36, 37, 38, 39,
-        40, 41, 43, 44, 51, 52, 53, 60, 69, 78]
-
-
-def test_sudoku_16x16():
-    """Generate a 4-Sudoku graph (16x16) and verify its properties."""
-    G = nx.sudoku_graph(4)
-    assert G.number_of_nodes() == 256
-    assert G.number_of_edges() == 4992
+    if n == 2:
+        assert sorted(G.neighbors(6)) == [2, 3, 4, 5, 7, 10, 14]
+    elif n == 3:
+        assert sorted(G.neighbors(42)) == [
+            6, 15, 24, 33, 34, 35, 36, 37, 38, 39,
+            40, 41, 43, 44, 51, 52, 53, 60, 69, 78
+        ]
+    elif n == 4:
+        assert sorted(G.neighbors(0)) == [
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+            16, 17, 18, 19, 32, 33, 34, 35, 48, 49, 50, 51,
+            64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240
+        ]
