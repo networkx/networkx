b042bc14fe1a0f9f8289367be0b55c2a1d1b119b
KOLANICH <kolan_n@mail.ru>
2020-01-22 14:52:25 +0300

    Moved the metadata into setup.cfg

--- networkx/release.py
+++ networkx/release.py
@@ -180,33 +180,7 @@ description = "Python package for creating and manipulating graphs and networks"
 authors = {'Hagberg': ('Aric Hagberg', 'hagberg@lanl.gov'),
            'Schult': ('Dan Schult', 'dschult@colgate.edu'),
            'Swart': ('Pieter Swart', 'swart@lanl.gov')}
-maintainer = "NetworkX Developers"
-maintainer_email = "networkx-discuss@googlegroups.com"
-url = 'http://networkx.github.io/'
-project_urls = {
-    "Bug Tracker": "https://github.com/networkx/networkx/issues",
-    "Documentation": "https://networkx.github.io/documentation/stable/",
-    "Source Code": "https://github.com/networkx/networkx",
-}
-platforms = ['Linux', 'Mac OSX', 'Windows', 'Unix']
-keywords = ['Networks', 'Graph Theory', 'Mathematics',
-            'network', 'graph', 'discrete mathematics', 'math']
-classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3 :: Only',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    'Topic :: Scientific/Engineering :: Bio-Informatics',
-    'Topic :: Scientific/Engineering :: Information Analysis',
-    'Topic :: Scientific/Engineering :: Mathematics',
-    'Topic :: Scientific/Engineering :: Physics']
+
 
 date, date_info, version, version_info, vcs_info = get_info()
 
