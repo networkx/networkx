4639ab3a10b9c3080b5cbb4b424cb6f61a44ab09
Kang Hong Jin <kanghongjin@gmail.com>
2020-01-25 15:16:27 +0800

    fix pep8 violations

--- networkx/algorithms/isomorphism/tests/test_isomorphvf2.py
+++ networkx/algorithms/isomorphism/tests/test_isomorphvf2.py
@@ -319,19 +319,19 @@ def test_noncomparable_nodes():
     # Just testing some cases
     assert gm.subgraph_is_monomorphic()
 
+
 def test_monomorphism_edge_match():
     G = nx.DiGraph()
     G.add_node(1)
     G.add_node(2)
-    G.add_edge(1,2, label="A")
-    G.add_edge(2,1, label="B")
-    G.add_edge(2,2, label="C")
+    G.add_edge(1, 2, label="A")
+    G.add_edge(2, 1, label="B")
+    G.add_edge(2, 2, label="C")
 
     SG = nx.DiGraph()
-
     SG.add_node(5)
     SG.add_node(6)
-    SG.add_edge(5,6, label="A")
+    SG.add_edge(5, 6, label="A")
 
     gm = iso.DiGraphMatcher(G, SG, edge_match=iso.categorical_edge_match('label', None))
     assert gm.subgraph_is_monomorphic()
