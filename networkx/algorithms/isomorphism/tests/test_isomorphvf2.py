001201c565e9822d9448a1f72b8cd10b84fa1ba6
Kang Hong Jin <kanghongjin@gmail.com>
2020-02-02 12:51:18 +0800

    Prevent KeyError on subgraph_is_monomorphic (#3798)
    Fixes #3775 
    
    
    * add test for subgraph monomorphism using edge_match
    
    * add check to make sure neighbor is in adj
    
    * fix pep8 violations
    
    * Attempt to make cacheing lookups work better in _semantic_feasibility
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- networkx/algorithms/isomorphism/tests/test_isomorphvf2.py
+++ networkx/algorithms/isomorphism/tests/test_isomorphvf2.py
@@ -318,3 +318,20 @@ def test_noncomparable_nodes():
     assert dgm.is_isomorphic()
     # Just testing some cases
     assert gm.subgraph_is_monomorphic()
+
+
+def test_monomorphism_edge_match():
+    G = nx.DiGraph()
+    G.add_node(1)
+    G.add_node(2)
+    G.add_edge(1, 2, label="A")
+    G.add_edge(2, 1, label="B")
+    G.add_edge(2, 2, label="C")
+
+    SG = nx.DiGraph()
+    SG.add_node(5)
+    SG.add_node(6)
+    SG.add_edge(5, 6, label="A")
+
+    gm = iso.DiGraphMatcher(G, SG, edge_match=iso.categorical_edge_match('label', None))
+    assert gm.subgraph_is_monomorphic()
