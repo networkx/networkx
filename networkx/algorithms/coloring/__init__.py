3fd855bb2f6c2762dbc513422d206d296ecce5c2
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-03-07 22:46:10 +0100

    add the new algorithm to the library

--- networkx/algorithms/coloring/__init__.py
+++ networkx/algorithms/coloring/__init__.py
@@ -1,4 +1,4 @@
 from networkx.algorithms.coloring.greedy_coloring import *
 from networkx.algorithms.coloring.equitable_coloring import equitable_color
-from networkx.algorithms.coloring.exact_coloring import backGCP
-__all__ = ['greedy_color', 'equitable_color', 'backGCP']
+from networkx.algorithms.coloring.exact_coloring import exact_color
+__all__ = ['greedy_color', 'equitable_color', 'exact_color']
