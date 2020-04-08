48dc7bf611fd95e865d3a4c68a9f94bd2a317c3f
Lorenzo Cancello <56786819+LorenzoCancello@users.noreply.github.com>
2020-04-08 17:59:44 +0200

    correzione

--- networkx/algorithms/traversal/breadth_first_search.py
+++ networkx/algorithms/traversal/breadth_first_search.py
@@ -62,17 +62,17 @@ def generic_bfs_edges(G, source, neighbors=None, depth_limit=None):
     if depth_limit is None:
         depth_limit = len(G)
     queue = deque([(source, depth_limit, neighbors(source))])
+    print(source)
     while queue:
         parent, depth_now, children = queue[0]
-        print(parent+' parent')
         try:
-            child = next(children) 
-            print(child)         
+            child = next(children)        
             if child not in visited:
                 yield parent, child
                 visited.add(child)
                 if depth_now > 1:
                     queue.append((child, depth_now - 1, neighbors(child)))
+                    print(child)
         except StopIteration:
             queue.popleft()
 
