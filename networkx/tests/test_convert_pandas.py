5db2a12f89ace56a928ed5968bcd8d717f80ec68
iboates <isaac.boates@eifer.uni-karlsruhe.de>
2020-04-26 11:10:37 +0200

    Added exception raise if source or target column name already exists as edge attributes when converting to pandas edgelist

--- networkx/tests/test_convert_pandas.py
+++ networkx/tests/test_convert_pandas.py
@@ -173,22 +173,24 @@ class TestConvertPandas:
         G = nx.Graph()
         G.add_nodes_from(g)
         G.add_weighted_edges_from((u, v, u) for u, v in g.edges())
-        nx.set_edge_attributes(G, 0, name="source_col")
+        nx.set_edge_attributes(G, 0, name="source_co")
         try:
             nx.to_pandas_edgelist(G, source="source_col")
         except NetworkXError:
             assert True
+        assert False
 
     def test_to_edgelist_target_col_exists(self):
         g = nx.cycle_graph(10)
         G = nx.Graph()
         G.add_nodes_from(g)
         G.add_weighted_edges_from((u, v, u) for u, v in g.edges())
-        nx.set_edge_attributes(G, 0, name="target_col")
+        nx.set_edge_attributes(G, 0, name="target_co")
         try:
             nx.to_pandas_edgelist(G, source="target_col")
         except NetworkXError:
             assert True
+        assert False
 
     def test_from_adjacency(self):
         nodelist = [1, 2]
