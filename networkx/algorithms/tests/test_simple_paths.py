c58eeb822d850ac8588be75bade19256a04091d4
Harold Chan <cmkgroup@gmail.com>
2020-05-08 18:12:57 +0800

    Update for PEP8

--- networkx/algorithms/tests/test_simple_paths.py
+++ networkx/algorithms/tests/test_simple_paths.py
@@ -255,17 +255,18 @@ def test_shortest_simple_paths_directed():
 
 
 def test_shortest_simple_paths_directed_with_weight_fucntion():
-    def cost(u,v,x):
+    def cost(u, v, x):
         return 1
     G = cnlti(nx.grid_2d_graph(4, 4), first_label=1, ordering="sorted")
     paths = nx.shortest_simple_paths(G, 1, 12)
     assert next(paths) == [1, 2, 3, 4, 8, 12]
     assert next(paths) == [1, 5, 6, 7, 8, 12]
-    assert ([len(path) for path in nx.shortest_simple_paths(G, 1, 12, weight=cost)] ==
-            sorted([len(path) for path in nx.all_simple_paths(G, 1, 12)]))
+    assert ([len(path) for path in nx.shortest_simple_paths(G, 1, 12, weight=cost)]
+            == sorted([len(path) for path in nx.all_simple_paths(G, 1, 12)]))
+
 
 def test_shortest_simple_paths_with_weight_fucntion():
-    def cost(u,v,x):
+    def cost(u, v, x):
         return 1
     G = nx.cycle_graph(7, create_using=nx.DiGraph())
     paths = nx.shortest_simple_paths(G, 0, 3, weight=cost)
