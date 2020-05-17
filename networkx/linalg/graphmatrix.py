343f45872ecf41cbc9767afc5109e9f865fac72c
muratgu <mgungora@gmail.com>
2020-05-16 18:03:37 -0700

    fix documentation (#3959)
    * fix documentation
    
    * fix dcoumentation
    
    * remove trailing space

--- networkx/linalg/graphmatrix.py
+++ networkx/linalg/graphmatrix.py
@@ -13,8 +13,8 @@ def incidence_matrix(G, nodelist=None, edgelist=None, oriented=False, weight=Non
     For a standard incidence matrix a 1 appears wherever a row's node is
     incident on the column's edge.  For an oriented incidence matrix each
     edge is assigned an orientation (arbitrarily for undirected and aligning to
-    direction for directed).  A -1 appears for the tail of an edge and 1
-    for the head of the edge.  The elements are zero otherwise.
+    direction for directed).  A -1 appears for the source (tail) of an edge and
+    1 for the destination (head) of the edge.  The elements are zero otherwise.
 
     Parameters
     ----------
