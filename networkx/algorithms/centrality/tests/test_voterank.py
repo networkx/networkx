1904c0df7221e102b28ff526a19687bc55360f28
Manas Joshi <manasjoshi241@gmail.com>
2020-05-25 21:10:26 +0530

    Added unit test for VoteRank algorithm on directed graphs

--- networkx/algorithms/centrality/tests/test_voterank.py
+++ networkx/algorithms/centrality/tests/test_voterank.py
@@ -18,3 +18,10 @@ class TestVoteRankCentrality:
         d = nx.voterank(G, 4)
         exact = ['Medici', 'Strozzi', 'Guadagni', 'Castellani']
         assert exact == d
+
+    # Directed graph unit test
+    def test_voterank_centrality_3(self):
+        G = nx.gnc_graph(10, seed=7)
+        d = nx.voterank(G, 4)
+        exact = [3, 6, 8]
+        assert exact == d
