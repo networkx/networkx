2fee8d726733ef6d6ba33f0bcaf6d395c8a29f85
Manas Joshi <manasjoshi241@gmail.com>
2020-05-25 06:48:44 +0530

    Update VoteRank algorithm to work on directed graphs
    Also removed the redundant max_iter parameter

--- networkx/algorithms/centrality/voterank_alg.py
+++ networkx/algorithms/centrality/voterank_alg.py
@@ -4,8 +4,8 @@ from networkx.utils.decorators import not_implemented_for
 __all__ = ['voterank']
 
 
-@not_implemented_for('directed')
-def voterank(G, number_of_nodes=None, max_iter=10000):
+@not_implemented_for('multigraph')
+def voterank(G, number_of_nodes=None):
     """Compute a list of seeds for the nodes in the graph using VoteRank
 
     VoteRank [1]_ computes a ranking of the nodes in the graph G based on a voting
@@ -21,18 +21,11 @@ def voterank(G, number_of_nodes=None, max_iter=10000):
     number_of_nodes : integer, optional
         Number of ranked nodes to extract (default all nodes).
 
-    max_iter : integer, optional
-        Maximum number of iterations to rank nodes.
-
     Returns
     -------
     voterank : list
         Ordered list of computed seeds.
-
-    Raises
-    ------
-    NetworkXNotImplemented
-        If G is digraph.
+        Only nodes with positive number of votes are returned.
 
     References
     ----------
@@ -45,19 +38,27 @@ def voterank(G, number_of_nodes=None, max_iter=10000):
         return voterank
     if number_of_nodes is None or number_of_nodes > len(G):
         number_of_nodes = len(G)
-    avgDegree = sum(deg for _, deg in G.degree()) / float(len(G))
+    avgDegree = 0
+    if (G.is_directed()):
+        # For directed graphs compute average out-degree
+        avgDegree = sum(deg for _, deg in G.out_degree()) / float(len(G))
+    else:
+        # For undirected graphs compute average degree
+        avgDegree = sum(deg for _, deg in G.degree()) / float(len(G))
     # step 1 - initiate all nodes to (0,1) (score, voting ability)
     for _, v in G.nodes(data=True):
         v['voterank'] = [0, 1]
     # Repeat steps 1b to 4 until num_seeds are elected.
-    for _ in range(max_iter):
+    for _ in range(number_of_nodes):
         # step 1b - reset rank
         for _, v in G.nodes(data=True):
             v['voterank'][0] = 0
         # step 2 - vote
         for n, nbr in G.edges():
+            # In directed graphs nodes only vote for their in-neighbors
             G.nodes[n]['voterank'][0] += G.nodes[nbr]['voterank'][1]
-            G.nodes[nbr]['voterank'][0] += G.nodes[n]['voterank'][1]
+            if (not G.is_directed()):
+                G.nodes[nbr]['voterank'][0] += G.nodes[n]['voterank'][1]
         for n in voterank:
             G.nodes[n]['voterank'][0] = 0
         # step 3 - select top node
@@ -66,11 +67,10 @@ def voterank(G, number_of_nodes=None, max_iter=10000):
         if value['voterank'][0] == 0:
             return voterank
         voterank.append(n)
-        if len(voterank) >= number_of_nodes:
-            return voterank
         # weaken the selected node
         G.nodes[n]['voterank'] = [0, 0]
         # step 4 - update voterank properties
+        # Neighbors is same as successors for directed graphs
         for nbr in G.neighbors(n):
             G.nodes[nbr]['voterank'][1] -= 1 / avgDegree
     return voterank
