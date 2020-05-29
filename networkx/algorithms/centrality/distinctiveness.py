341872dc6994ea589cd2d48210b52257fa2534f5
Andrea Fronzetti Colladon <52541209+andreafcdev@users.noreply.github.com>
2020-05-29 11:14:55 +0200

    Update distinctiveness.py

--- networkx/algorithms/centrality/distinctiveness.py
+++ networkx/algorithms/centrality/distinctiveness.py
@@ -103,14 +103,14 @@ def g_preprocess(G, alpha=1):
             if "weight" not in data:
                 data["weight"] = 1
 
-    # Sum weights of all arcs
+    # Sums the weight of all arcs
     totalWEI = 0
     for u, v, data in G.edges(data=True):
         totalWEI += data["weight"]
 
     n1 = nx.number_of_nodes(G) - 1
 
-    # Calculate degree and weighted degree, taking alpha into account
+    # Calculates degree and weighted degree, taking alpha into account
     if type(G) == nx.Graph:
         deg = dict(nx.degree(G))
         indeg = outdeg = wei_insum_alpha_list = wei_outsum_alpha_list = np.nan
@@ -241,15 +241,60 @@ def g_preprocess(G, alpha=1):
     )
 
 
-def distinctiveness(G, alpha=1, normalize=False):
-    r"""Compute the Distinctiveness Centrality for the graph `G`.
+def distinctiveness(G, alpha=1, normalize=False,
+                    measures=["D1", "D2", "D3", "D4", "D5"]):
+    r"""Compute Distinctiveness Centrality for the nodes of graph `G`.
 
     Distinctiveness Centrality is a set of 5 network metrics
-    that attribute larger importance to distinctive connections.
-    The are five formulas that can be used
-    to calculate Distinctiveness Centrality.
-    This function calculates them all
-    and provides results as a list of dictionaries.
+    that attribute larger importance to distinctive connections, i.e.
+    to nodes which have links to loosely connected peers.
+    Revisiting degree and weighted degree centrality, all these
+    metrics penalize connections to hubs or nodes that are
+    very well connected. Distinctiveness measures might serve the
+    identification of strategic social actors, for example those with
+    peripheral connections that keep the network together,
+    avoiding fragmentation.
+
+    The formula of D1 is commented here, whereas complete descriptions
+    can be found in the references.
+
+    .. math::
+
+        D_1 (i) = \sum_{\substack{j=1\\j\neq i}}^{n} w_{ij}
+        \log_{10}\frac{n-1}{{g_{j}}^\alpha}.
+
+    The metric is similar to weighted degree centrality, as it sums the
+    weight of all arcs connected to a node. However, weights are penalized
+    based on the number of connections that a node’s peers have. If a
+    neighbouring node is connected to all the other nodes as well, its
+    contribution to the sum is zero; the rationale is that node for which
+    D1 is calculated ($i$) adds the minimum possible improvement to the
+    reachability of its very well connected peer ($j$), since $j$ is already
+    connected to all other nodes. Instead, if a neighbouring
+    node is connected to node $i$ only, the weight of the arc connecting
+    them is multiplied by the maximum possible factor $\log_{10}(n-1)$
+    (the rationale here is that node $j$ would be unreachable if
+    it were not connected by node $i$).
+    An exponent $α \geq 1$ is used in the formulas to allow an even
+    stronger penalization of connections with highly connected nodes.
+
+    Similarly to the case of in- and out-degree, it is possible to
+    calculate in- and out-distinctiveness on directed graphs.
+    In directed networks, distinctiveness values incoming arcs more, if
+    they originate at nodes with low out-degree. Indeed, a connection from
+    a node sending arcs towards all other nodes is considered of little value.
+    This can be explained through an example of love-letter writing. Consider
+    the case where student A receives a love-letter from student
+    B, who is sending love-letters to all people in the school. The letter
+    sent to A is much less important to A than the case of B sending only
+    one letter (to A). Indeed, B is ‘spamming’ all the network, sending many
+    outgoing arcs, then each of them gives a low contribution to the
+    receiver’s importance. Similarly, distinctiveness values outgoing arcs
+    more when they reach peers with low in-degree. If the arc sent by a node
+    is the only one, or among the few, to reach another node, that arc will be
+    important. Continuing with the example, if student A receives a love letter
+    from student B only, this is much more important than the case of A
+    receiving many love letters.
 
     Parameters
     ----------
@@ -261,36 +306,41 @@ def distinctiveness(G, alpha=1, normalize=False):
         Weights have to be >= 1.
 
     alpha : float or list, optional (default=1)
-        alpha must be a number greater or equal to 1.
+        Alpha must be a number greater or equal to 1.
         It represents the value of the alpha parameter used in
-        the generalized formulas of distinctiveness centrality.
-        If one value is provided it will be used for all the five metrics.
+        the formulas of distinctiveness centrality.
+        If one value is provided, it will be used for all the five metrics.
         Alternatively, alpha can be a list of five numbers,
         used to specify different coefficients
-        for the different metrics (e.g. alpha = [1, 2, 1, 1, 5]).
+        for the different metrics (e.g. alpha = [1, 2, 1, 1, 3]).
 
     normalize : bool, optional (default=False)
-            Normalize can be set to True,
-            to obtain normalized scores for each metric,
-            considering upper and lower bounds. Loose upper
-            and lower bounds are used for D3.
-            Normalization is only carried out for undirected
-            graph measures.
+        Normalize can be set to True,
+        to obtain normalized scores for each metric,
+        considering upper and lower bounds. Loose upper
+        and lower bounds are used for D3.
+        Normalization is only carried out for undirected
+        graph measures.
+
+    measures : list, optional (default=["D1", "D2", "D3", "D4", "D5"])
+        Distinctiveness centrality can be calculated considering five
+        different weighting schemes. This parameter can be adjusted to
+        select which metrics should be computed. The default option is
+        to calculate them all.
 
     Returns
     -------
     nodes : dictionary
-        A dictionary with 15 keys, each one indicating one distinctiveness
-        centrality metric. The value of each key is a dictionary of nodes,
+        A dictionary with 5 or 10 keys, each one indicating a distinctiveness
+        centrality metric, for undirected or directed graphs respectively.
+        The value of each key is a dictionary of nodes,
         with distinctiveness scores. Because distinctiveness can be calculated
-        using five different formulas, the first 5 keys are named as D1, D2,
-        D3, D4 and D5. These measures are calculated if a Graph is
-        given as input (otherwise nans will be generated).
-        The other 10 measures are for the directed networks and will
-        be available if a DiGraph is given as input
-        (otherwise nans will be generated).
-        These are: D1_in, D2_in, D3_in, D4_in,
-        D5_in, D1_out, D2_out, D3_out, D4_out and D5_out.
+        using five different formulas, the 5 keys for undirected networks
+        are named as D1, D2, D3, D4 and D5. These measures can be calculated
+        if a Graph is given as input.
+        The other 10 measures are for directed networks and will
+        be available if a DiGraph is given as input. These are: D1_in, D2_in,
+        D3_in, D4_in, D5_in, D1_out, D2_out, D3_out, D4_out and D5_out.
 
     References
     ----------
@@ -391,13 +441,27 @@ def distinctiveness(G, alpha=1, normalize=False):
     # Computes Distinctiveness Centrality, all 5 metrics
     if type(G) == nx.Graph:
         # Set keys to zero for all nodes (to take isolates into account)
-        d1, d2, d3, d4, d5 = (
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-        )
+        if "D1" in measures:
+            d1 = dict.fromkeys(Glist, 0)
+        else:
+            d1 = np.nan
+        if "D2" in measures:
+            d2 = dict.fromkeys(Glist, 0)
+        else:
+            d2 = np.nan
+        if "D3" in measures:
+            d3 = dict.fromkeys(Glist, 0)
+        else:
+            d3 = np.nan
+        if "D4" in measures:
+            d4 = dict.fromkeys(Glist, 0)
+        else:
+            d4 = np.nan
+        if "D5" in measures:
+            d5 = dict.fromkeys(Glist, 0)
+        else:
+            d5 = np.nan
+
         d1_in = np.nan
         d2_in = np.nan
         d3_in = np.nan
@@ -410,110 +474,148 @@ def distinctiveness(G, alpha=1, normalize=False):
         d5_out = np.nan
 
         for u, v, data in G.edges(data=True):
-            d1[u] += data["weight"] * np.log10(n1 / deg[v] ** alphalist[0])
-            d1[v] += data["weight"] * np.log10(n1 / deg[u] ** alphalist[0])
+            if "D1" in measures:
+                d1[u] += data["weight"] * np.log10(n1 / deg[v] ** alphalist[0])
+                d1[v] += data["weight"] * np.log10(n1 / deg[u] ** alphalist[0])
 
-            d2[u] += 1 * np.log10(n1 / deg[v] ** alphalist[1])
-            d2[v] += 1 * np.log10(n1 / deg[u] ** alphalist[1])
+            if "D2" in measures:
+                d2[u] += 1 * np.log10(n1 / deg[v] ** alphalist[1])
+                d2[v] += 1 * np.log10(n1 / deg[u] ** alphalist[1])
 
-            d3[u] += data["weight"] * np.log10(
-                totalWEI
-                / (wei_sum_alpha_list[2][v]
-                   - data["weight"] ** alphalist[2] + 1)
-            )
-            d3[v] += data["weight"] * np.log10(
-                totalWEI
-                / (wei_sum_alpha_list[2][u]
-                   - data["weight"] ** alphalist[2] + 1)
-            )
+            if "D3" in measures:
+                d3[u] += data["weight"] * np.log10(
+                    totalWEI
+                    / (wei_sum_alpha_list[2][v]
+                       - data["weight"] ** alphalist[2] + 1)
+                )
+                d3[v] += data["weight"] * np.log10(
+                    totalWEI
+                    / (wei_sum_alpha_list[2][u]
+                       - data["weight"] ** alphalist[2] + 1)
+                )
 
-            d4[u] += data["weight"] * (
-                data["weight"] ** alphalist[3] / wei_sum_alpha_list[3][v]
-            )
-            d4[v] += data["weight"] * (
-                data["weight"] ** alphalist[3] / wei_sum_alpha_list[3][u]
-            )
+            if "D4" in measures:
+                d4[u] += data["weight"] * (
+                    data["weight"] ** alphalist[3] / wei_sum_alpha_list[3][v]
+                )
+                d4[v] += data["weight"] * (
+                    data["weight"] ** alphalist[3] / wei_sum_alpha_list[3][u]
+                )
 
-            d5[u] += 1 * (1 / deg[v] ** alphalist[4])
-            d5[v] += 1 * (1 / deg[u] ** alphalist[4])
+            if "D5" in measures:
+                d5[u] += 1 * (1 / deg[v] ** alphalist[4])
+                d5[v] += 1 * (1 / deg[u] ** alphalist[4])
 
         if normalize is True:
-            d1 = {k: (v - D1min) / (D1max - D1min) for k, v in d1.items()}
-            d2 = {k: (v - D2min) / (D2max - D2min) for k, v in d2.items()}
-            d3 = {k: (v - D3min) / (D3max - D3min) for k, v in d3.items()}
-            d4 = {k: (v - D4min) / (D4max - D4min) for k, v in d4.items()}
-            d5 = {k: (v - D5min) / (D5max - D5min) for k, v in d5.items()}
+            if "D1" in measures:
+                d1 = {k: (v - D1min) / (D1max - D1min) for k, v in d1.items()}
+            if "D2" in measures:
+                d2 = {k: (v - D2min) / (D2max - D2min) for k, v in d2.items()}
+            if "D3" in measures:
+                d3 = {k: (v - D3min) / (D3max - D3min) for k, v in d3.items()}
+            if "D4" in measures:
+                d4 = {k: (v - D4min) / (D4max - D4min) for k, v in d4.items()}
+            if "D5" in measures:
+                d5 = {k: (v - D5min) / (D5max - D5min) for k, v in d5.items()}
 
     elif type(G) == nx.DiGraph:
         # Set keys to zero for all nodes
         # (to take isolates into account and nodes with zero in- or out-degree)
-        (d1_in, d2_in, d3_in, d4_in, d5_in, d1_out,
-         d2_out, d3_out, d4_out, d5_out) = (
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-            dict.fromkeys(Glist, 0),
-        )
+        if "D1" in measures:
+            d1_in = dict.fromkeys(Glist, 0)
+            d1_out = dict.fromkeys(Glist, 0)
+        else:
+            d1_in = d1_out = np.nan
+        if "D2" in measures:
+            d2_in = dict.fromkeys(Glist, 0)
+            d2_out = dict.fromkeys(Glist, 0)
+        else:
+            d2_in = d2_out = np.nan
+        if "D3" in measures:
+            d3_in = dict.fromkeys(Glist, 0)
+            d3_out = dict.fromkeys(Glist, 0)
+        else:
+            d3_in = d3_out = np.nan
+        if "D4" in measures:
+            d4_in = dict.fromkeys(Glist, 0)
+            d4_out = dict.fromkeys(Glist, 0)
+        else:
+            d4_in = d4_out = np.nan
+        if "D5" in measures:
+            d5_in = dict.fromkeys(Glist, 0)
+            d5_out = dict.fromkeys(Glist, 0)
+        else:
+            d5_in = d5_out = np.nan
+
         d1 = d2 = d3 = d4 = d5 = np.nan
 
         for u, v, data in G.edges(data=True):
-            d1_in[v] += data["weight"] * np.log10(n1
-                                                  / outdeg[u] ** alphalist[0])
-            d1_out[u] += data["weight"] * np.log10(n1
-                                                   / indeg[v] ** alphalist[0])
 
-            d2_in[v] += 1 * np.log10(n1 / outdeg[u] ** alphalist[1])
-            d2_out[u] += 1 * np.log10(n1 / indeg[v] ** alphalist[1])
+            if "D1" in measures:
+                d1_in[v] += data["weight"] * \
+                    np.log10(n1
+                             / outdeg[u] ** alphalist[0])
+                d1_out[u] += data["weight"] * \
+                    np.log10(n1
+                             / indeg[v] ** alphalist[0])
 
-            d3_in[v] += data["weight"] * np.log10(
-                totalWEI
-                / (wei_outsum_alpha_list[2][u]
-                   - data["weight"] ** alphalist[2] + 1)
-            )
-            d3_out[u] += data["weight"] * np.log10(
-                totalWEI
-                / (wei_insum_alpha_list[2][v]
-                   - data["weight"] ** alphalist[2] + 1)
-            )
+            if "D2" in measures:
+                d2_in[v] += 1 * np.log10(n1 / outdeg[u] ** alphalist[1])
+                d2_out[u] += 1 * np.log10(n1 / indeg[v] ** alphalist[1])
 
-            d4_in[v] += data["weight"] * (
-                data["weight"] ** alphalist[3] / wei_outsum_alpha_list[3][u]
-            )
-            d4_out[u] += data["weight"] * (
-                data["weight"] ** alphalist[3] / wei_insum_alpha_list[3][v]
-            )
+            if "D3" in measures:
+                d3_in[v] += data["weight"] * np.log10(
+                    totalWEI
+                    / (wei_outsum_alpha_list[2][u]
+                       - data["weight"] ** alphalist[2] + 1)
+                )
+                d3_out[u] += data["weight"] * np.log10(
+                    totalWEI
+                    / (wei_insum_alpha_list[2][v]
+                       - data["weight"] ** alphalist[2] + 1)
+                )
 
-            d5_in[v] += 1 * (1 / outdeg[u] ** alphalist[4])
-            d5_out[u] += 1 * (1 / indeg[v] ** alphalist[4])
+            if "D4" in measures:
+                d4_in[v] += data["weight"] * (
+                    data["weight"] ** alphalist[3]
+                    / wei_outsum_alpha_list[3][u]
+                )
+                d4_out[u] += data["weight"] * (
+                    data["weight"] ** alphalist[3]
+                    / wei_insum_alpha_list[3][v]
+                )
+
+            if "D5" in measures:
+                d5_in[v] += 1 * (1 / outdeg[u] ** alphalist[4])
+                d5_out[u] += 1 * (1 / indeg[v] ** alphalist[4])
 
         if normalize is True:
-            print("Normalization is only carried out for undirected graph metrics.")
-            # d1_in = {k: (v - D1min) / (D1max - D1min)
-            #          for k, v in d1_in.items()}
-            # d2_in = {k: (v - D2min) / (D2max - D2min)
-            #          for k, v in d2_in.items()}
-            # d3_in = {k: (v - D3min) / (D3max - D3min)
-            #          for k, v in d3_in.items()}
-            # d4_in = {k: (v - D4min) / (D4max - D4min)
-            #          for k, v in d4_in.items()}
-            # d5_in = {k: (v - D5min) / (D5max - D5min)
-            #          for k, v in d5_in.items()}
-
-            # d1_out = {k: (v - D1min) / (D1max - D1min)
-            #           for k, v in d1_out.items()}
-            # d2_out = {k: (v - D2min) / (D2max - D2min)
+            print("Normalization is only carried out"
+                  " for undirected graph metrics.")
+            # if "D1" in measures:
+            #     d1_in = {k: (v - D1min) / (D1max - D1min)
+            #              for k, v in d1_in.items()}
+            #     d1_out = {k: (v - D1min) / (D1max - D1min)
+            #               for k, v in d1_out.items()}
+            # if "D2" in measures:
+            #     d2_in = {k: (v - D2min) / (D2max - D2min)
+            #              for k, v in d2_in.items()}
+            #     d2_out = {k: (v - D2min) / (D2max - D2min)
             #           for k, v in d2_out.items()}
-            # d3_out = {k: (v - D3min) / (D3max - D3min)
+            # if "D3" in measures:
+            #     d3_in = {k: (v - D3min) / (D3max - D3min)
+            #              for k, v in d3_in.items()}
+            #     d3_out = {k: (v - D3min) / (D3max - D3min)
             #           for k, v in d3_out.items()}
-            # d4_out = {k: (v - D4min) / (D4max - D4min)
+            # if "D4" in measures:
+            #     d4_in = {k: (v - D4min) / (D4max - D4min)
+            #              for k, v in d4_in.items()}
+            #     d4_out = {k: (v - D4min) / (D4max - D4min)
             #           for k, v in d4_out.items()}
-            # d5_out = {k: (v - D5min) / (D5max - D5min)
+            # if "D5" in measures:
+            #     d5_in = {k: (v - D5min) / (D5max - D5min)
+            #              for k, v in d5_in.items()}
+            #     d5_out = {k: (v - D5min) / (D5max - D5min)
             #           for k, v in d5_out.items()}
 
     DC = {
@@ -533,5 +635,6 @@ def distinctiveness(G, alpha=1, normalize=False):
         "D4_out": d4_out,
         "D5_out": d5_out,
     }
+    DC = {k: v for k, v in DC.items() if not isinstance(v, float)}
 
     return DC
