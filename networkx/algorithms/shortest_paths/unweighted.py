79c75f940d2245b5db4cd20e6979c9f9ec45df6b
hcars <hcarscad@gmail.com>
2020-06-17 20:49:23 -0400

    fixed pool

--- networkx/algorithms/shortest_paths/unweighted.py
+++ networkx/algorithms/shortest_paths/unweighted.py
@@ -179,7 +179,7 @@ def all_pairs_shortest_path_length(G, cutoff=None, parallel=False):
     """
     length = single_source_shortest_path_length
     if parallel:
-        with Pool(processes=-1) as pool:
+        with Pool() as pool:
             for n in G:
                 yield (n, pool.apply_async(length, (G, n, cutoff)))
     else:
@@ -456,7 +456,7 @@ def all_pairs_shortest_path(G, cutoff=None, parallel=False):
 
     """
     if parallel:
-        with Pool(processes=-1) as pool:
+        with Pool() as pool:
             for n in G:
                 yield (n, pool.apply_async(single_source_shortest_path, (G, n, cutoff)))
     else:
