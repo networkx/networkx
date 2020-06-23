3d25e5391d7c31d96d57723dcc856baef62cebf6
James Trimble <james.trimble@yahoo.co.uk>
2020-06-23 15:56:48 +0100

    Add a test that max weight clique algorithm is unaffected by loops

--- networkx/algorithms/tests/test_max_weight_clique.py
+++ networkx/algorithms/tests/test_max_weight_clique.py
@@ -27,6 +27,17 @@ class TestMaximumWeightClique:
         with pytest.raises(ValueError):
             nx.algorithms.max_weight_clique(graph)
 
+    def test_unaffected_by_self_loops(self):
+        graph = two_node_graph()
+        graph.add_edge(1, 1)
+        graph.add_edge(2, 2)
+        clique, weight = nx.algorithms.max_weight_clique(graph, 'weight')
+        assert verify_clique(graph, clique, weight, 30, 'weight')
+        graph = three_node_independent_set()
+        graph.add_edge(1, 1)
+        clique, weight = nx.algorithms.max_weight_clique(graph, 'weight')
+        assert verify_clique(graph, clique, weight, 20, 'weight')
+
     def test_30_vertex_prob(self):
         G = nx.Graph()
         G.add_nodes_from(range(1, 31))
