72993e846acccedcb2386caa0477f35ddae87933
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2020-07-06 13:31:06 -0400

    missing parenthesis

--- networkx/algorithms/similarity.py
+++ networkx/algorithms/similarity.py
@@ -159,7 +159,7 @@ def graph_edit_distance(
     >>> G2 = nx.star_graph(5)
     >>> nx.graph_edit_distance(G1, G2, roots=(0,0))
     0.0
-    >>> nx.graph_edit_distance(G1, G2, roots=(1, 0)
+    >>> nx.graph_edit_distance(G1, G2, roots=(1, 0))
     8.0
 
     See Also
