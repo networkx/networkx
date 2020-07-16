968e4af2e27b2aeb028ad2cd224c1d5f9f6b84a1
Jarrod Millman <jarrod.millman@gmail.com>
2020-07-16 13:07:11 -0700

    Install Python after updating brew (#4079)
    MAINT: update CI configuration

--- tools/travis/osx_install.sh
+++ tools/travis/osx_install.sh
@@ -3,16 +3,16 @@ set -ex
 
 section "OSX install section"
 
-# set up Python and virtualenv on OSX
-git clone https://github.com/matthew-brett/multibuild
-source multibuild/osx_utils.sh
-get_macpython_environment $TRAVIS_PYTHON_VERSION venv
-
 if [[ "${OPTIONAL_DEPS}" == 1 ]]; then
     brew install graphviz
     dot -V
     sed -i "" 's/^gdal.*/gdal==3.1.2/' requirements/extras.txt
 fi
+
+# set up Python and virtualenv on OSX
+git clone https://github.com/matthew-brett/multibuild
+source multibuild/osx_utils.sh
+get_macpython_environment $TRAVIS_PYTHON_VERSION venv
 
 section_end "OSX install section"
 
