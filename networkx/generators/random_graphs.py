52d10c1ad887e641cb3b77884aca93d866980122
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2020-07-16 12:42:14 +0600

    Follow np.conj as suggested by jarrodmillman

--- networkx/generators/random_graphs.py
+++ networkx/generators/random_graphs.py
@@ -138,6 +138,14 @@ def gnp_random_graph(n, p, seed=None, directed=False):
     This algorithm [2]_ runs in $O(n^2)$ time.  For sparse graphs (that is, for
     small values of $p$), :func:`fast_gnp_random_graph` is a faster algorithm.
 
+    :func:`binomial_graph` and :func:`erdos_renyi_graph` are
+    aliases for :func:`gnp_random_graph`.
+
+    >>> nx.binomial_graph is nx.gnp_random_graph
+    True
+    >>> nx.erdos_renyi_graph is nx.gnp_random_graph
+    True
+
     References
     ----------
     .. [1] P. Erdős and A. Rényi, On Random Graphs, Publ. Math. 6, 290 (1959).
@@ -161,20 +169,9 @@ def gnp_random_graph(n, p, seed=None, directed=False):
     return G
 
 
-def binomial_graph(n, p, seed=None, directed=False):
-    """Returns a binomial graph.
-    This is an alias for ``gnp_random_graph``.  See the ``gnp_random_graph``
-    docstring for more details.
-    """
-    return gnp_random_graph(n, p, seed, directed)
-
-
-def erdos_renyi_graph(n, p, seed=None, directed=False):
-    """Returns an Erdos-Renyi $G_{n,p}$ random graph.
-    This is an alias for ``gnp_random_graph``.  See the ``gnp_random_graph``
-    docstring for more details.
-    """
-    return gnp_random_graph(n, p, seed, directed)
+# add some aliases to common names
+binomial_graph = gnp_random_graph
+erdos_renyi_graph = gnp_random_graph
 
 
 @py_random_state(2)
