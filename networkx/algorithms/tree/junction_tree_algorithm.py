f6fe7889dae2af3f8ba7ef59ba3c502206f728a2
matthias <matthias.bruhns@uni-jena.de>
2020-07-21 11:45:24 +0200

    Removed example code.

--- networkx/algorithms/tree/junction_tree_algorithm.py
+++ networkx/algorithms/tree/junction_tree_algorithm.py
@@ -89,21 +89,3 @@ def junction_tree(G):
         junction_tree.remove_edge(edge[0], edge[1])
 
     return junction_tree
-
-
-if __name__ == '__main__':
-    B = nx.DiGraph()
-    B.add_nodes_from(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
-    B.add_edges_from([('A', 'B'), ('A', 'F'), ('C', 'B'),
-                      ('B', 'D'), ('F', 'G'), ('E', 'G')])
-    bayes_pos = nx.spring_layout(B, k=3)
-    plt.subplot(1, 2, 1)
-    plt.title('Bayesian Network')
-    nx.draw_networkx(B, with_labels=True)
-
-    jt = junction_tree(B)
-
-    plt.subplot(1, 2, 2)
-    plt.title('Junction Tree')
-    nx.draw_networkx(jt, with_labels=True)
-    plt.show()
