a6793f67acd47eb9ab2edece9408dff6109c56f1
James Trimble <james.trimble@yahoo.co.uk>
2020-07-27 11:26:20 +0100

    Add max_weight_clique to doc
    Also, minor changes to max_weight_clique docstring such as fixing syntax for
    italics and references; added James Trimble to list of contributors.

--- networkx/algorithms/clique.py
+++ networkx/algorithms/clique.py
@@ -662,9 +662,9 @@ class MaxWeightClique(object):
 def max_weight_clique(G, weight='weight'):
     """Find a maximum weight clique in G.
 
-    A _clique_ in a graph is a set of nodes such that every two distinct nodes
-    are adjacent.  The _weight_ of a clique is the sum of the weights of its
-    nodes.  A _maximum weight clique_ of graph G is a clique C in G such that
+    A *clique* in a graph is a set of nodes such that every two distinct nodes
+    are adjacent.  The *weight* of a clique is the sum of the weights of its
+    nodes.  A *maximum weight clique* of graph G is a clique C in G such that
     no clique in G has weight greater than the weight of C.
 
     Parameters
@@ -693,19 +693,19 @@ def max_weight_clique(G, weight='weight'):
     nodes on which to branch.  The algorithm is very similar to the algorithm
     of Tavares et al. [1]_, other than the fact that the NetworkX version does
     not use bitsets.  This style of algorithm for maximum weight clique (and
-    maximum independent set, which is the same problem but on the complement
-    graph) has a decades-long history.  See Algorithm B of Warren and Hicks
-    [2]_ and the references in that paper.
+    maximum weight independent set, which is the same problem but on the
+    complement graph) has a decades-long history.  See Algorithm B of Warren
+    and Hicks [2]_ and the references in that paper.
 
     References
     ----------
-    [1] Tavares, W.A., Neto, M.B.C., Rodrigues, C.D., Michelon, P.: Um
-    algoritmo de branch and bound para o problema da clique máxima ponderada.
-    Proceedings of XLVII SBPO 1 (2015).
+    .. [1] Tavares, W.A., Neto, M.B.C., Rodrigues, C.D., Michelon, P.: Um
+           algoritmo de branch and bound para o problema da clique máxima
+           ponderada.  Proceedings of XLVII SBPO 1 (2015).
 
-    [2] Warrent, Jeffrey S, Hicks, Illya V.: Combinatorial Branch-and-Bound for
-    the Maximum Weight Independent Set Problem.  Technical Report, Texas A&M
-    University (2016).
+    .. [2] Warrent, Jeffrey S, Hicks, Illya V.: Combinatorial Branch-and-Bound
+           for the Maximum Weight Independent Set Problem.  Technical Report,
+           Texas A&M University (2016).
     """
 
     mwc = MaxWeightClique(G, weight)
