045e9717802919494f28641d257842010cd28de5
Ross Barnowski <rossbar@berkeley.edu>
2020-08-14 14:45:28 -0700

    TST: Improve coverage of view_pygv multigraph labels

--- networkx/drawing/tests/test_agraph.py
+++ networkx/drawing/tests/test_agraph.py
@@ -122,6 +122,16 @@ class TestAGraph:
         for edge in A.edges():
             assert edge.attr["label"] == "foo"
 
+    def test_view_pygraphviz_multigraph_edgelabels(self):
+        G = nx.MultiGraph()
+        G.add_edge(0, 1, key=0, name="left_fork")
+        G.add_edge(0, 1, key=1, name="right_fork")
+        path, A = nx.nx_agraph.view_pygraphviz(G, edgelabel="name")
+        edges = A.edges()
+        assert len(edges) == 2
+        for edge in edges:
+            assert edge.attr["label"].strip() in ("left_fork", "right_fork")
+
     def test_graph_with_reserved_keywords(self):
         # test attribute/keyword clash case for #1582
         # node: n
