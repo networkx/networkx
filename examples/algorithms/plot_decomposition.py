0f92cec6a10ade320308f3a4a641855fd064fdf3
Jarrod Millman <jarrod.millman@gmail.com>
2020-08-15 09:57:59 -0700

    Improve example

--- examples/algorithms/plot_decomposition.py
+++ examples/algorithms/plot_decomposition.py
@@ -7,22 +7,34 @@ Example of creating a junction tree from a directed graph.
 """
 
 import networkx as nx
+from networkx.algorithms import moral
 from networkx.algorithms.tree.decomposition import junction_tree
+from networkx.drawing.nx_agraph import graphviz_layout as layout
 import matplotlib.pyplot as plt
 
 B = nx.DiGraph()
-B.add_nodes_from(["A", "B", "C", "D", "E", "F", "G"])
+B.add_nodes_from(["A", "B", "C", "D", "E", "F"])
 B.add_edges_from(
-    [("A", "B"), ("A", "F"), ("C", "B"), ("B", "D"), ("F", "G"), ("E", "G")]
+    [("A", "B"), ("A", "C"), ("B", "D"), ("B", "F"), ("C", "E"), ("E", "F")]
 )
 
-bayes_pos = nx.spring_layout(B, k=3)
-plt.subplot(1, 2, 1)
+options = {"with_labels": True, "node_color": "white", "edgecolors": "blue"}
+
+bayes_pos = layout(B, prog="neato")
+ax1 = plt.subplot(1, 3, 1)
 plt.title("Bayesian Network")
-nx.draw_networkx(B, with_labels=True)
+nx.draw_networkx(B, pos=bayes_pos, **options)
+
+mg = moral.moral_graph(B)
+plt.subplot(1, 3, 2, sharex=ax1, sharey=ax1)
+plt.title("Moralized Graph")
+nx.draw_networkx(mg, pos=bayes_pos, **options)
 
 jt = junction_tree(B)
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 3)
 plt.title("Junction Tree")
-nx.draw_networkx(jt, with_labels=True)
+nsize = [2000 * len(n) for n in list(jt.nodes())]
+nx.draw_networkx(jt, pos=layout(jt, prog="neato"), node_size=nsize, **options)
+
+plt.tight_layout()
 plt.show()
