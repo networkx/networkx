33f13aa5c113c8a05313af813e66618c623115aa
Jarrod Millman <jarrod.millman@gmail.com>
2020-08-16 08:47:04 -0700

    Bump release version

--- networkx/release.py
+++ networkx/release.py
@@ -171,12 +171,12 @@ def get_info(dynamic=True):
 # Version information
 name = "networkx"
 major = "2"
-minor = "5rc1"
+minor = "5rc2"
 
 
 # Declare current release as a development release.
 # Change to False before tagging a release; then change back.
-dev = False
+dev = True
 
 
 description = "Python package for creating and manipulating graphs and networks"
