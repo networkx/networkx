628340c9e9acb844705078d5501d4eaa8e2d60a5
Dan Schult <dschult@colgate.edu>
2020-08-15 20:17:37 -0400

    cite James Trimble for code that finds missing functions

--- doc/release/report_functions_without_rst_generated.sh
+++ doc/release/report_functions_without_rst_generated.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# Authored-by: James Trimble <james.trimble@yahoo.co.uk> 
+# expanded beyond /algorithms/ by Dan Schult <dschult@colgate.edu>
 
-#echo ALGORITHMS
-#find networkx/algorithms -name "*.py" | xargs cat | tr -d ' \n' | grep -o '__all__=\[["a-z0-9_,]*\]' | grep -o '"[a-z0-9_]*"' | tr -d '"' | sort | uniq > tmp_funcs.txt
-#cat doc/reference/algorithms/*.rst | tr -d ' ' | sort | uniq > tmp_doc.txt
-#comm -2 -3 tmp_funcs.txt tmp_doc.txt
-#comm -2 -3 tmp_funcs.txt tmp_doc.txt > functions_possibly_missing_from_doc.txt
-#for f in $(cat functions_possibly_missing_from_doc.txt); do echo $f; grep -l -r "def ${f}" networkx/algorithms | grep -v ".pyc" | sed 's/^/  /'; done
+echo ALGORITHMS
+find networkx/algorithms -name "*.py" | xargs cat | tr -d ' \n' | grep -o '__all__=\[["a-z0-9_,]*\]' | grep -o '"[a-z0-9_]*"' | tr -d '"' | sort | uniq > tmp_funcs.txt
+cat doc/reference/algorithms/*.rst | tr -d ' ' | sort | uniq > tmp_doc.txt
+comm -2 -3 tmp_funcs.txt tmp_doc.txt
+comm -2 -3 tmp_funcs.txt tmp_doc.txt > functions_possibly_missing_from_doc.txt
+for f in $(cat functions_possibly_missing_from_doc.txt); do echo $f; grep -l -r "def ${f}" networkx/algorithms | grep -v ".pyc" | sed 's/^/  /'; done
 
 echo GENERATORS
 find networkx/generators -name "*.py" | xargs cat | tr -d ' \n' | grep -o '__all__=\[["a-z0-9_,]*\]' | grep -o '"[a-z0-9_]*"' | tr -d '"' | sort | uniq > tmp_funcs.txt
