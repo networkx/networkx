12f852f6a686ed32f718125c5965586dd6bb82a9
Jarrod Millman <jarrod.millman@gmail.com>
2020-08-22 13:01:28 -0700

    Designate 2.5 release

--- networkx/release.py
+++ networkx/release.py
@@ -171,12 +171,12 @@ def get_info(dynamic=True):
 # Version information
 name = "networkx"
 major = "2"
-minor = "5rc2"
+minor = "5"
 
 
 # Declare current release as a development release.
 # Change to False before tagging a release; then change back.
-dev = True
+dev = False
 
 
 description = "Python package for creating and manipulating graphs and networks"
