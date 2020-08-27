10a3c3253fd2ae39aedb72c82c27f8e712b9bd4c
Ross Barnowski <rossbar@berkeley.edu>
2020-08-27 13:16:58 -0700

    DOC: Add sections to cytoscape_data docstring

--- networkx/readwrite/json_graph/cytoscape.py
+++ networkx/readwrite/json_graph/cytoscape.py
@@ -11,16 +11,42 @@ def cytoscape_data(G, attrs=None):
     Parameters
     ----------
     G : NetworkX Graph
-
+        The graph to convert to cytoscape format
+    attrs : dict or None (default=None)
+        A dictionary containing the keys 'name' and 'ident' which are mapped to
+        the 'name' and 'id' node elements in cyjs format.All other keys are
+        ignored. Default is `None` which results in the default mapping
+        ``{name="name", ident="id"}``.
 
     Returns
     -------
     data: dict
         A dictionary with cyjs formatted data.
+
     Raises
     ------
     NetworkXError
         If values in attrs are not unique.
+
+    See Also
+    --------
+    cytoscape_graph - convert a dictionary in cyjs format to a graph
+
+    References
+    ----------
+    .. [1] Cytoscape user's manual: 
+       http://manual.cytoscape.org/en/stable/index.html
+
+    Examples
+    --------
+    >>> G = nx.path_graph(2)
+    >>> nx.cytoscape_data(G)  # doctest: +SKIP
+    {'data': [],
+     'directed': False,
+     'multigraph': False,
+     'elements': {'nodes': [{'data': {'id': '0', 'value': 0, 'name': '0'}},
+       {'data': {'id': '1', 'value': 1, 'name': '1'}}],
+      'edges': [{'data': {'source': 0, 'target': 1}}]}}
     """
     if not attrs:
         attrs = _attrs
