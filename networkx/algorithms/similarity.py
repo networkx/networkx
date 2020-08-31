3b3b2856b44ffe3f3d273a058df671a29e72b401
Michael Recachinas <mike@recachinas.dev>
2020-08-30 20:45:55 -0400

    Fix docstring example for generate_random_paths

--- networkx/algorithms/similarity.py
+++ networkx/algorithms/similarity.py
@@ -1639,7 +1639,7 @@ def generate_random_paths(G, sample_size, path_length=5, index_map=None):
         >>> G = nx.star_graph(3)
         >>> index_map = {}
         >>> random_path = nx.generate_random_paths(G, 3, index_map=index_map)
-        >>> paths_containing_node_0 = [random_path[path_idx] for path_idx in index_map[0]]
+        >>> paths_containing_node_0 = [random_path[path_idx] for path_idx in index_map.get(0, [])]
 
     References
     ----------
