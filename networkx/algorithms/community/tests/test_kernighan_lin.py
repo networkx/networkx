1f22c69ba890ac7096026cab587efd047fd31e51
Jarrod Millman <jarrod.millman@gmail.com>
2020-09-12 07:53:17 -0700

    Add tests

--- networkx/algorithms/community/tests/test_kernighan_lin.py
+++ networkx/algorithms/community/tests/test_kernighan_lin.py
@@ -3,6 +3,7 @@ module.
 
 """
 import pytest
+from random import shuffle
 
 import networkx as nx
 from networkx.algorithms.community import kernighan_lin_bisection
@@ -57,3 +58,25 @@ def test_multigraph():
         assert_partition_equal(
             [A, B], [{mapping[0], mapping[1]}, {mapping[2], mapping[3]}]
         )
+
+
+def test_defined_partition_str_nodes():
+    G = nx.Graph([("A", "B"), ("A", "C"), ("B", "C"), ("C", "D")])
+    labels = list(G)
+    shuffle(labels)
+
+    with pytest.raises(KeyError):
+        for i in range(1, len(labels)):
+            k_partition = (set(labels[0:i]), set(labels[i : len(labels)]))
+            kernighan_lin_bisection(G, partition=k_partition)
+
+
+def test_defined_partition_int_nodes():
+    G = nx.Graph([(1, 2), (1, 3), (2, 3), (3, 4)])
+    labels = list(G)
+    shuffle(labels)
+
+    with pytest.raises(KeyError):
+        for i in range(1, len(labels)):
+            k_partition = (set(labels[0:i]), set(labels[i : len(labels)]))
+            kernighan_lin_bisection(G, partition=k_partition)
