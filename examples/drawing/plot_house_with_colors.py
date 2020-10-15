2121986cd58862bccba119150b86b92ec8ff97ed
Ross Barnowski <rossbar@berkeley.edu>
2020-10-15 13:39:53 -0700

    Change node color scheme.

--- examples/drawing/plot_house_with_colors.py
+++ examples/drawing/plot_house_with_colors.py
@@ -12,8 +12,11 @@ G = nx.house_graph()
 # explicitly set positions
 pos = {0: (0, 0), 1: (1, 0), 2: (0, 1), 3: (1, 1), 4: (0.5, 2.0)}
 
-nx.draw_networkx_nodes(G, pos, node_size=2000, nodelist=[4])
-nx.draw_networkx_nodes(G, pos, node_size=3000, nodelist=[0, 1, 2, 3], node_color="b")
+# Plot nodes with different properties for the "wall" and "roof" nodes
+nx.draw_networkx_nodes(
+    G, pos, node_size=3000, nodelist=[0, 1, 2, 3], node_color="#003262"
+)
+nx.draw_networkx_nodes(G, pos, node_size=2000, nodelist=[4], node_color="#fdb515")
 nx.draw_networkx_edges(G, pos, alpha=0.5, width=6)
 # Customize axes
 ax = plt.gca()
