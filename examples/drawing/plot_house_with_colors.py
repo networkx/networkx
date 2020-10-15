99378ef69fa601f484a8f7293a4c09b7a6b1cde3
Ross Barnowski <rossbar@berkeley.edu>
2020-10-15 13:39:37 -0700

    Fix axes so that nodes are fully contained in figure.

--- examples/drawing/plot_house_with_colors.py
+++ examples/drawing/plot_house_with_colors.py
@@ -15,5 +15,9 @@ pos = {0: (0, 0), 1: (1, 0), 2: (0, 1), 3: (1, 1), 4: (0.5, 2.0)}
 nx.draw_networkx_nodes(G, pos, node_size=2000, nodelist=[4])
 nx.draw_networkx_nodes(G, pos, node_size=3000, nodelist=[0, 1, 2, 3], node_color="b")
 nx.draw_networkx_edges(G, pos, alpha=0.5, width=6)
+# Customize axes
+ax = plt.gca()
+ax.margins(0.11)
+plt.tight_layout()
 plt.axis("off")
 plt.show()
