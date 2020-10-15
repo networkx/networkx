6462725818c657e0cb3181d82a4994c3af96a1df
Ross Barnowski <rossbar@berkeley.edu>
2020-10-15 14:46:40 -0700

    Add rank plot and degree hist in one figure
    Condense the degree histogram and degree rank plot examples into a
    single example where the visualization inclues three things:
     1. The subgraph of connected components
     2. The degree rank plot
     3. The degree histogram

--- examples/drawing/plot_degree_rank.py
+++ examples/drawing/plot_degree_rank.py
@@ -7,6 +7,7 @@ Random graph from given degree sequence.
 Draw degree rank plot and graph with matplotlib.
 """
 import networkx as nx
+import numpy as np
 import matplotlib.pyplot as plt
 
 G = nx.gnp_random_graph(100, 0.02)
@@ -14,16 +15,29 @@ G = nx.gnp_random_graph(100, 0.02)
 degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
 dmax = max(degree_sequence)
 
-plt.loglog(degree_sequence, "b-", marker="o")
-plt.title("Degree rank plot")
-plt.ylabel("degree")
-plt.xlabel("rank")
+fig = plt.figure("Degree of a random graph", figsize=(8, 8))
+# Create a gridspec for adding subplots of different sizes
+axgrid = fig.add_gridspec(5, 4)
 
-# draw graph in inset
-plt.axes([0.45, 0.45, 0.45, 0.45])
+ax0 = fig.add_subplot(axgrid[0:3, :])
 Gcc = G.subgraph(sorted(nx.connected_components(G), key=len, reverse=True)[0])
 pos = nx.spring_layout(Gcc)
-plt.axis("off")
-nx.draw_networkx_nodes(Gcc, pos, node_size=20)
-nx.draw_networkx_edges(Gcc, pos, alpha=0.4)
+nx.draw_networkx_nodes(Gcc, pos, ax=ax0, node_size=20)
+nx.draw_networkx_edges(Gcc, pos, ax=ax0, alpha=0.4)
+ax0.set_title("Connected components of G")
+ax0.set_axis_off()
+
+ax1 = fig.add_subplot(axgrid[3:, :2])
+ax1.plot(degree_sequence, "b-", marker="o")
+ax1.set_title("Degree rank plot")
+ax1.set_ylabel("degree")
+ax1.set_xlabel("rank")
+
+ax2 = fig.add_subplot(axgrid[3:, 2:])
+ax2.bar(*np.unique(degree_sequence, return_counts=True))
+ax2.set_title("Degree histogram")
+ax2.set_xlabel("Degree")
+ax2.set_ylabel("# of Nodes")
+
+fig.tight_layout()
 plt.show()
