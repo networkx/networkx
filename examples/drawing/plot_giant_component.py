2281c645ea801985104937b9d547ef500e358fb5
Ross Barnowski <rossbar@berkeley.edu>
2020-10-15 17:27:20 -0700

    Switch from plt to fig,ax approach for subplots

--- examples/drawing/plot_giant_component.py
+++ examples/drawing/plot_giant_component.py
@@ -26,32 +26,31 @@ n = 150  # 150 nodes
 # p value at which giant component (of size log(n) nodes) is expected
 p_giant = 1.0 / (n - 1)
 # p value at which graph is expected to become completely connected
-p_conn = math.log(n) / float(n)
+p_conn = math.log(n) / n
 
 # the following range of p values should be close to the threshold
 pvals = [0.003, 0.006, 0.008, 0.015]
 
-region = 220  # for pylab 2x2 subplot layout
-plt.subplots_adjust(left=0, right=1, bottom=0, top=0.95, wspace=0.01, hspace=0.01)
-for p in pvals:
+fig, axes = plt.subplots(2, 2)
+for p, ax in zip(pvals, axes.ravel()):
     G = nx.binomial_graph(n, p)
     pos = layout(G)
-    region += 1
-    plt.subplot(region)
-    plt.title(f"p = {p:.3f}")
-    nx.draw(G, pos, with_labels=False, node_size=10)
+    ax.set_title(f"p = {p:.3f}")
+    nx.draw(G, pos, ax=ax, with_labels=False, node_size=10)
     # identify largest connected component
     Gcc = sorted(nx.connected_components(G), key=len, reverse=True)
     G0 = G.subgraph(Gcc[0])
-    nx.draw_networkx_edges(G0, pos, edge_color="r", width=6.0)
+    nx.draw_networkx_edges(G0, pos, ax=ax, edge_color="r", width=6.0)
     # show other connected components
     for Gi in Gcc[1:]:
         if len(Gi) > 1:
             nx.draw_networkx_edges(
                 G.subgraph(Gi),
                 pos,
+                ax=ax,
                 edge_color="r",
                 alpha=0.3,
                 width=5.0,
             )
+fig.tight_layout()
 plt.show()
