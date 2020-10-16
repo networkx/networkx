b1e36e512016ff8e40c4f0a4853935bb63e639b9
Ross Barnowski <rossbar@berkeley.edu>
2020-10-16 12:52:59 -0700

    Seeded graph gen and layout for reproduciblity.

--- examples/drawing/plot_degree.py
+++ examples/drawing/plot_degree.py
@@ -17,7 +17,7 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-G = nx.gnp_random_graph(100, 0.02)
+G = nx.gnp_random_graph(100, 0.02, seed=10374196)
 
 degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
 dmax = max(degree_sequence)
@@ -28,7 +28,7 @@ axgrid = fig.add_gridspec(5, 4)
 
 ax0 = fig.add_subplot(axgrid[0:3, :])
 Gcc = G.subgraph(sorted(nx.connected_components(G), key=len, reverse=True)[0])
-pos = nx.spring_layout(Gcc)
+pos = nx.spring_layout(Gcc, seed=10396953)
 nx.draw_networkx_nodes(Gcc, pos, ax=ax0, node_size=20)
 nx.draw_networkx_edges(Gcc, pos, ax=ax0, alpha=0.4)
 ax0.set_title("Connected components of G")
