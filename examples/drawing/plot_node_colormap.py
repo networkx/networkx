1f88d3a242849e4899cf71af80c9b3c6e8bf1f47
Ross Barnowski <rossbar@berkeley.edu>
2020-10-22 18:14:10 -0700

    Switch node cmap example to deterministic layout

--- examples/drawing/plot_node_colormap.py
+++ examples/drawing/plot_node_colormap.py
@@ -10,6 +10,6 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 G = nx.cycle_graph(24)
-pos = nx.spring_layout(G, iterations=200)
+pos = nx.circular_layout(G)
 nx.draw(G, pos, node_color=range(24), node_size=800, cmap=plt.cm.Blues)
 plt.show()
