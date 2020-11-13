de472cc390806c95b021824bf16bf45929368714
Cheeranjeev Purmessur <cpurmessur@colgate.edu>
2020-11-12 20:20:36 -0500

    Improved conciseness of parameterization

--- networkx/algorithms/tests/test_euler.py
+++ networkx/algorithms/tests/test_euler.py
@@ -150,15 +150,13 @@ class TestHasEulerianPath:
         G.add_node(3)
         assert nx.has_eulerian_path(G)
 
-    @pytest.mark.parametrize("graph", (nx.Graph, nx.DiGraph))
-    def test_has_eulerian_path_not_weakly_connected(self, graph):
-        G = graph()
+    @pytest.mark.parametrize("G", (nx.Graph(), nx.DiGraph()))
+    def test_has_eulerian_path_not_weakly_connected(self, G):
         G.add_edges_from([(0, 1), (2, 3), (3, 2)])
         assert not nx.has_eulerian_path(G)
 
-    @pytest.mark.parametrize("graph", (nx.Graph, nx.DiGraph))
-    def test_has_eulerian_path_unbalancedins_more_than_one(self, graph):
-        G = graph()
+    @pytest.mark.parametrize("G", (nx.Graph(), nx.DiGraph()))
+    def test_has_eulerian_path_unbalancedins_more_than_one(self, G):
         G.add_edges_from([(0, 1), (2, 3)])
         assert not nx.has_eulerian_path(G)
 
