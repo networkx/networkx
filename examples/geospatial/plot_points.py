e1ff8dd2069fa98c390e8d10a3fae986201b4b6d
ljwolf <levi.john.wolf@gmail.com>
2020-11-19 15:41:19 +0000

    fixup point plotting path & plot label

--- examples/geospatial/plot_points.py
+++ examples/geospatial/plot_points.py
@@ -16,29 +16,32 @@ import numpy
 import geopandas
 
 # read in example data online
-filepath = examples.get_path("cholera_cases.gpkg")
-cases = geopandas.read_file(filepath)
+cases = geopandas.read_file('cholera_cases.gpkg')
 
+# construct the array of coordinates for the centroid
 coordinates = numpy.column_stack((cases.geometry.x, cases.geometry.y))
 
-# construct the graph
+# construct two different kinds of graphs:
+
+## 3-nearest neighbor graph
 knn3 = weights.KNN.from_dataframe(cases, k=3)
+
+## all pairs within 50 meters 
 dist = weights.DistanceBand.from_array(coordinates, threshold=50)
 
-# convert the graph to networkx
+# convert the graphs to networkx
 knn_graph = knn3.to_networkx()
 dist_graph = dist.to_networkx()
 
-# merge the nodes back to their positions
+# merge the networkx nodes back to their positions
 positions = dict(zip(knn_graph.nodes, coordinates))
-
 
 # plot
 f, ax = plt.subplots(1, 2, figsize=(8, 4))
 for i, facet in enumerate(ax):
     cases.plot(marker=".", color="orangered", ax=facet)
     add_basemap(facet)
-    facet.set_title(("KNN-3", "100-meter Distance Band")[i])
+    facet.set_title(("KNN-3", "50-meter Distance Band")[i])
 nx.draw(knn_graph, positions, ax=ax[0], node_size=5, node_color="b")
 nx.draw(dist_graph, positions, ax=ax[1], node_size=5, node_color="b")
 plt.show()
