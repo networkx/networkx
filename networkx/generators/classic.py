bc2218913303617f83868e53648918b435341cf4
R. Bernstein <rocky@users.noreply.github.com>
2020-12-15 12:08:23 -0500

    binomial_tree() with "create_using parameter (#4461)
    * binomail_tree() with "create_using parameter
    
    Fixes #4460
    
    * Disallow multigraphs for binomial trees
    
    * Make binomial create_using work with multigraphs.
    
    add create_using parameter to docstring
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- networkx/generators/classic.py
+++ networkx/generators/classic.py
@@ -188,7 +188,7 @@ def barbell_graph(m1, m2, create_using=None):
     return G
 
 
-def binomial_tree(n):
+def binomial_tree(n, create_using=None):
     """Returns the Binomial Tree of order n.
 
     The binomial tree of order 0 consists of a single node. A binomial tree of order k
@@ -200,16 +200,21 @@ def binomial_tree(n):
     n : int
         Order of the binomial tree.
 
+    create_using : NetworkX graph constructor, optional (default=nx.Graph)
+       Graph type to create. If graph instance, then cleared before populated.
+
     Returns
     -------
     G : NetworkX graph
         A binomial tree of $2^n$ nodes and $2^n - 1$ edges.
 
     """
-    G = nx.empty_graph(1)
+    G = nx.empty_graph(1, create_using)
+
     N = 1
     for i in range(n):
-        edges = [(u + N, v + N) for (u, v) in G.edges]
+        # Use G.edges() to ensure 2-tuples. G.edges is 3-tuple for MultiGraph
+        edges = [(u + N, v + N) for (u, v) in G.edges()]
         G.add_edges_from(edges)
         G.add_edge(0, N)
         N *= 2
