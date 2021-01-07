367331a5f114316467ae7b280a6033f0ddd05b0a
Cheeranjeev Purmessur <cpurmessur@colgate.edu>
2021-01-07 13:20:24 -0500

    added tests to swap

--- networkx/algorithms/tests/test_swap.py
+++ networkx/algorithms/tests/test_swap.py
@@ -26,12 +26,32 @@ def test_connected_double_edge_swap():
     assert nx.is_connected(graph)
     assert degrees == sorted(d for n, d in graph.degree())
 
-#def test_connected_double_edge_swap2(): ##NEW
-#    graph = nx.barabasi_albert_graph(200, 1)
-#    degrees = sorted(d for n, d in graph.degree())
-#    G = nx.connected_double_edge_swap(graph, 40, _window_threshold=0, seed=1)
-#    assert nx.is_connected(graph)
-#    assert degrees == sorted(d for n, d in graph.degree())
+
+def test_connected_double_edge_swap_low_window_threshold():
+    graph = nx.barabasi_albert_graph(200, 1)
+    degrees = sorted(d for n, d in graph.degree())
+    G = nx.connected_double_edge_swap(graph, 40, _window_threshold=0, seed=1)
+    assert nx.is_connected(graph)
+    assert degrees == sorted(d for n, d in graph.degree())
+
+
+def test_connected_double_edge_swap_star():
+    #Testing ui==xi in connected_double_edge_swap
+    graph = nx.star_graph(40)
+    degrees = sorted(d for n, d in graph.degree())
+    G = nx.connected_double_edge_swap(graph, 1, seed=4)
+    assert nx.is_connected(graph)
+    assert degrees == sorted(d for n, d in graph.degree())
+
+
+def test_connected_double_edge_swap_star_low_window_threshold():
+    #Testing ui==xi in connected_double_edge_swap with low window threshold
+    graph = nx.star_graph(40)
+    degrees = sorted(d for n, d in graph.degree())
+    G = nx.connected_double_edge_swap(graph, 1, _window_threshold=0, seed=4)
+    assert nx.is_connected(graph)
+    assert degrees == sorted(d for n, d in graph.degree())
+
 
 def test_double_edge_swap_small():
     with pytest.raises(nx.NetworkXError):
@@ -42,16 +62,16 @@ def test_double_edge_swap_tries():
     with pytest.raises(nx.NetworkXError):
         G = nx.double_edge_swap(nx.path_graph(10), nswap=1, max_tries=0)
 
-##NEW
+
 def test_double_edge_directed():
     graph = nx.DiGraph([(0, 1), (2, 3)])
-    with pytest.raises(nx.NetworkXError):
+    with pytest.raises(nx.NetworkXError, match="not defined for directed graphs."):
         G = nx.double_edge_swap(graph)
 
-##NEW
-#def test_double_edge_max_tries():
-#    with pytest.raises(nx.NetworkXAlgorithmError):
-#        G = nx.double_edge_swap(nx.path_graph(4), nswap=1, max_tries=5)
+
+def test_double_edge_max_tries():
+    with pytest.raises(nx.NetworkXAlgorithmError):
+        G = nx.double_edge_swap(nx.complete_graph(4), nswap=1, max_tries=5)
 
 
 def test_connected_double_edge_swap_small():
