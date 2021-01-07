6497f4f275c97492d5fb75bbce762f302dab0a83
Cheeranjeev Purmessur <cpurmessur@colgate.edu>
2021-01-07 01:14:48 -0500

    update tests window threshold swap

--- networkx/algorithms/tests/test_swap.py
+++ networkx/algorithms/tests/test_swap.py
@@ -26,6 +26,12 @@ def test_connected_double_edge_swap():
     assert nx.is_connected(graph)
     assert degrees == sorted(d for n, d in graph.degree())
 
+#def test_connected_double_edge_swap2(): ##NEW
+#    graph = nx.barabasi_albert_graph(200, 1)
+#    degrees = sorted(d for n, d in graph.degree())
+#    G = nx.connected_double_edge_swap(graph, 40, _window_threshold=0, seed=1)
+#    assert nx.is_connected(graph)
+#    assert degrees == sorted(d for n, d in graph.degree())
 
 def test_double_edge_swap_small():
     with pytest.raises(nx.NetworkXError):
