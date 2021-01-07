7d9b3d25095d3c09e3c72e1fc79d2b6081a0234e
Cheeranjeev Purmessur <cpurmessur@colgate.edu>
2021-01-06 23:33:40 -0500

    update test for swap

--- networkx/algorithms/tests/test_swap.py
+++ networkx/algorithms/tests/test_swap.py
@@ -36,6 +36,17 @@ def test_double_edge_swap_tries():
     with pytest.raises(nx.NetworkXError):
         G = nx.double_edge_swap(nx.path_graph(10), nswap=1, max_tries=0)
 
+##NEW
+def test_double_edge_directed():
+    graph = nx.DiGraph([(0, 1), (2, 3)])
+    with pytest.raises(nx.NetworkXError):
+        G = nx.double_edge_swap(graph)
+
+##NEW
+#def test_double_edge_max_tries():
+#    with pytest.raises(nx.NetworkXAlgorithmError):
+#        G = nx.double_edge_swap(nx.path_graph(4), nswap=1, max_tries=5)
+
 
 def test_connected_double_edge_swap_small():
     with pytest.raises(nx.NetworkXError):
