b26480b1c236a581296c86c7c077600631d3babe
Ross Barnowski <rossbar@berkeley.edu>
2021-01-27 11:23:12 -0800

    Update documentation and testing of arbitrary_element (#4451)
    * Update documentation for arbitrary_element.
    
     * Add numpydoc sections to docstring and extensive examples.
     * Add to reference documentation.
    
    * Add tests for arbitrary_element.
    
    * Update networkx/utils/misc.py
    
    Co-authored-by: Stefan van der Walt <sjvdwalt@gmail.com>
    
    Co-authored-by: Stefan van der Walt <sjvdwalt@gmail.com>

--- networkx/utils/misc.py
+++ networkx/utils/misc.py
@@ -232,22 +232,63 @@ def arbitrary_element(iterable):
     """Returns an arbitrary element of `iterable` without removing it.
 
     This is most useful for "peeking" at an arbitrary element of a set,
-    but can be used for any list, dictionary, etc., as well::
+    but can be used for any list, dictionary, etc., as well.
 
-        >>> arbitrary_element({3, 2, 1})
+    Parameters
+    ----------
+    iterable : `abc.collections.Iterable` instance
+        Any object that implements ``__iter__``, e.g. set, dict, list, tuple,
+        etc.
+
+    Returns
+    -------
+    The object that results from ``next(iter(iterable))``
+
+    Raises
+    ------
+    ValueError
+        If `iterable` is an iterator (because the current implementation of
+        this function would consume an element from the iterator).
+
+    Examples
+    --------
+    Arbitrary elements from common Iterable objects:
+
+    >>> arbitrary_element([1, 2, 3])  # list
+    1
+    >>> arbitrary_element((1, 2, 3))  # tuple
+    1
+    >>> arbitrary_element({1, 2, 3})  # set
+    1
+    >>> d = {k: v for k, v in zip([1, 2, 3], [3, 2, 1])}
+    >>> arbitrary_element(d)  # dict_keys
+    1
+    >>> arbitrary_element(d.values())   # dict values
+    3
+
+    `str` is also an Iterable:
+
+    >>> arbitrary_element("hello")
+    'h'
+
+    :exc:`ValueError` is raised if `iterable` is an iterator:
+
+    >>> iterator = iter([1, 2, 3])  # Iterator, *not* Iterable
+    >>> arbitrary_element(iterator)
+    Traceback (most recent call last):
+        ...
+    ValueError: cannot return an arbitrary item from an iterator
+
+    Notes
+    -----
+    This function does not return a *random* element. If `iterable` is
+    ordered, sequential calls will return the same value::
+
+        >>> l = [1, 2, 3]
+        >>> arbitrary_element(l)
         1
-        >>> arbitrary_element("hello")
-        'h'
-
-    This function raises a :exc:`ValueError` if `iterable` is an
-    iterator (because the current implementation of this function would
-    consume an element from the iterator)::
-
-        >>> iterator = iter([1, 2, 3])
-        >>> arbitrary_element(iterator)
-        Traceback (most recent call last):
-            ...
-        ValueError: cannot return an arbitrary item from an iterator
+        >>> arbitrary_element(l)
+        1
 
     """
     if isinstance(iterable, Iterator):
