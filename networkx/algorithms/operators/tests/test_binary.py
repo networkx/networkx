23b468e2b054615eba39a36f4b535c867533705a
Delille Louis <louis.delille@supelec.fr>
2021-02-01 18:49:08 +0100

    corrected test_intersection_multigraph_attributes (made sure the two graphs had different node sets during test)

--- networkx/algorithms/operators/tests/test_binary.py
+++ networkx/algorithms/operators/tests/test_binary.py
@@ -84,9 +84,9 @@ def test_intersection_attributes_node_sets_different():
     h.graph["name"] = "h"
     h.graph["attr"] = "attr"
     h.nodes[0]["x"] = 7
+    h.remove_node(2)
 
     gh = nx.intersection(g, h)
-    assert set(gh.nodes()) == set(g.nodes())
     assert set(gh.nodes()) == set(h.nodes())
     assert sorted(gh.edges()) == sorted(g.edges())
 
