6517f1c30438d9c6420a3213e18ab23a56a153b3
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2021-02-03 13:55:07 +0100

    Test writing of DIMACS format

--- networkx/readwrite/tests/test_dimacs.py
+++ networkx/readwrite/tests/test_dimacs.py
@@ -2,6 +2,7 @@
     Unit tests for dimacs.
 """
 import io
+import os
 
 import networkx as nx
 from networkx.testing import assert_edges_equal
@@ -31,3 +32,18 @@ e 3 1
         bytesIO = io.BytesIO(s)
         G = nx.read_dimacs(bytesIO)
         assert_edges_equal(G.edges(), [(1, 2), (2, 3), (3, 1)])
+
+    def test_write_dimacs(self):
+        G = nx.Graph()
+        G.add_node(1, value=2)
+        G.add_node(2, value=3)
+        G.add_edge(1, 2)
+        nx.write_dimacs(G, 'temp')
+        expected_output = """p edge 2 1
+n 1 2
+n 2 3
+e 1 2
+"""
+        output = "".join(open('temp', 'r').readlines())
+        assert output == expected_output
+        os.unlink('temp')
