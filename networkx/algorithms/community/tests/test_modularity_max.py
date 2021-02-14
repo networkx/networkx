a9cbaab8caac9da1b4bd95e010edc1037674a697
Dan Schult <dschult@colgate.edu>
2021-02-13 19:36:28 -0500

    update old tests to simplify and clarify

--- networkx/algorithms/community/tests/test_modularity_max.py
+++ networkx/algorithms/community/tests/test_modularity_max.py
@@ -1,3 +1,5 @@
+import pytest
+
 import networkx as nx
 from networkx.algorithms.community import (
     greedy_modularity_communities,
@@ -5,38 +7,20 @@ from networkx.algorithms.community import (
 )
 
 
-class TestCNM:
-    def setup(self):
-        self.G = nx.karate_club_graph()
+@pytest.mark.parametrize(
+    "func", (greedy_modularity_communities, naive_greedy_modularity_communities)
+)
+def test_modularity_communities(func):
+    G = nx.karate_club_graph()
 
-    def _check_communities(self, expected):
-        communities = set(greedy_modularity_communities(self.G))
-        assert communities == expected
+    john_a = frozenset(
+        [8, 14, 15, 18, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
+    )
+    mr_hi = frozenset([0, 4, 5, 6, 10, 11, 16, 19])
+    overlap = frozenset([1, 2, 3, 7, 9, 12, 13, 17, 21])
+    expected = {john_a, overlap, mr_hi}
 
-    def test_karate_club(self):
-        john_a = frozenset(
-            [8, 14, 15, 18, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
-        )
-        mr_hi = frozenset([0, 4, 5, 6, 10, 11, 16, 19])
-        overlap = frozenset([1, 2, 3, 7, 9, 12, 13, 17, 21])
-        self._check_communities({john_a, overlap, mr_hi})
-
-
-class TestNaive:
-    def setup(self):
-        self.G = nx.karate_club_graph()
-
-    def _check_communities(self, expected):
-        communities = set(naive_greedy_modularity_communities(self.G))
-        assert communities == expected
-
-    def test_karate_club(self):
-        john_a = frozenset(
-            [8, 14, 15, 18, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
-        )
-        mr_hi = frozenset([0, 4, 5, 6, 10, 11, 16, 19])
-        overlap = frozenset([1, 2, 3, 7, 9, 12, 13, 17, 21])
-        self._check_communities({john_a, overlap, mr_hi})
+    assert set(func(G)) == expected
 
 
 def test_generalized_modularity_max_resolution():
