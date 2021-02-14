c7a77395d825666362aa07931e55d99ad95b9176
Dan Schult <dschult@colgate.edu>
2021-02-13 19:26:10 -0500

    Move tests to where they should be

--- networkx/algorithms/community/tests/test_modularity_max.py
+++ networkx/algorithms/community/tests/test_modularity_max.py
@@ -37,3 +37,12 @@ class TestNaive:
         mr_hi = frozenset([0, 4, 5, 6, 10, 11, 16, 19])
         overlap = frozenset([1, 2, 3, 7, 9, 12, 13, 17, 21])
         self._check_communities({john_a, overlap, mr_hi})
+
+
+def test_generalized_modularity_max_resolution():
+    G = nx.karate_club_graph()
+    c1 = list(greedy_modularity_communities(G, 1.8))
+    c2 = list(naive_greedy_modularity_communities(G, 1.8))
+    assert len(c1) == len(c2)
+    for cc1, cc2 in zip(c1, c2):
+        assert sorted(cc1) == sorted(cc2)
