ce50f23c7e3cacb65743eff311f21520eede85d9
Sharwin Bobde <bsharwin@gmail.com>
2021-02-22 15:40:51 +0100

    added vlines and explanations

--- examples/matplotlib-examples/plot_composite_feature_network.py
+++ examples/matplotlib-examples/plot_composite_feature_network.py
@@ -1,9 +1,12 @@
 """
 ===========
-Matplotlib annotations with networkx
+Matplotlib composite plot of feature distribution and network topology
 ===========
 
-Draw a graph with matplotlib.
+Draw a composite plot on matplotlib showing distribution of nodes' features and the network topology at the same time.
+
+This can be useful when you have a dataset with feature information for different items and some links between different
+items.
 """
 
 import matplotlib.pyplot as plt
@@ -13,27 +16,30 @@ import scipy.stats as stats
 
 import networkx as nx
 
+# set seed for the toy example
+np.random.seed(4242)
+
 # Create a dataset with normally distributed feature data (for a nicer final output)
-points = 20
+points = 50
 df = pd.DataFrame(
     {
         "item": np.arange(points).astype(str),
-        "feature": np.random.normal(5, 0.1, points),
+        "feature": np.random.normal(0, 0.05, points),
     }
 )
 
 # Initialise networkX graph
 G = nx.Graph()
 
-# Generate random sparse connections
+# Generate dummy graph edges
 for a in range(points):
-    for b in range(points):
+    for b in np.random.choice(range(points), int(points / 10), replace=False):
         if a == b:
             continue
         # G.add_edge(str(a), str(b), weight=np.random.uniform(1, 10))
         G.add_edge(str(a), str(b), weight=np.random.uniform(1, 10) ** 3)
 
-# Seed layout for reproducibility and retrieve the node positions
+# generate a layout for the graph in 1 dimension (the projection based on graph topology)
 pos = nx.kamada_kawai_layout(G, dim=1)
 df_layout = pd.DataFrame(
     {
@@ -42,25 +48,26 @@ df_layout = pd.DataFrame(
     }
 )
 
+# join the projections with the features dataframe
 df = df.merge(df_layout, how="inner", on="item")
 
 # Normalise projection according to feature
-max_ = df.projection.max()
-min_ = df.projection.min()
-df["projection"] = (df["projection"] - min_) / (max_ - min_) * (
+max_proj = df.projection.max()
+min_proj = df.projection.min()
+
+df["projection"] = (df["projection"] - min_proj) / (max_proj - min_proj) * (
     df.feature.max() - df.feature.min()
 ) + df.feature.min()
 
 # make pos as array of feature and projection
-# df["pos"] = df.apply(lambda x: [x[1], x[2]], axis=1)
 df["pos"] = np.array([df["feature"], df["projection"]]).T.tolist()
 
-
+# create a dict required for networkx.draw_networkx()
 pos = {}
 for index, row in df[["item", "pos"]].iterrows():
     pos[str(row[0])] = row[1]
 
-# Generate figure
+# Generate a new figure
 plt.figure(figsize=(10, 10))
 
 # Define the bounding rectangles for the histogram and network
@@ -75,15 +82,29 @@ ax_net = plt.axes(rect_net)
 # Draw the network graph
 nx.draw_networkx(G, pos=pos, ax=ax_net)
 
+# find plot bounds for scaling everything correctly
+diff_feature = df.feature.max() - df.feature.min()
+max_feature = df.feature.max() + diff_feature * 0.05
+min_feature = df.feature.min() - diff_feature * 0.05
+
+# draw translucent vlines linking nodes and histogram positions
+for index, row in df[["feature"]].iterrows():
+    ax_net.axvline(row[0], alpha=0.2)
+
+# add labels and ticks
 ax_net.set_xlabel("feature axis")
 ax_net.set_ylabel("projection axis")
+ax_net.set_xlim(min_feature, max_feature)
 ax_net.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
 
 # Define the histogram axes and labels
 ax_histx = plt.axes(rect_histx)
 
-ax_histx.tick_params(direction="in", labelbottom=False)
-n, x, _ = ax_histx.hist(df["feature"], bins=points)
+# plot feature histogram
+n, x, _ = ax_histx.hist(df["feature"], bins=100)
+# plot density estimate for the same
 ax_histx.plot(x, stats.gaussian_kde(df["feature"])(x))
+ax_histx.set_xlim(min_feature, max_feature)
+ax_histx.tick_params(left=True, bottom=True)
 
 plt.show()
