fb92d0a441e45acd35c4486abd66420e40d3df5e
Ross Barnowski <rossbar@berkeley.edu>
2021-02-22 20:32:49 -0800

    Rm print statements from tests.

--- networkx/algorithms/community/tests/test_quality.py
+++ networkx/algorithms/community/tests/test_quality.py
@@ -78,10 +78,8 @@ def test_modularity_resolution():
     G = nx.barbell_graph(3, 0)
     C = [{0, 1, 4}, {2, 3, 5}]
     assert modularity(G, C) == pytest.approx(3 / 7 - 100 / 14 ** 2)
-    print(modularity(G, C))
     gamma = 2
     result = modularity(G, C, resolution=gamma)
-    print(result)
     assert result == pytest.approx(3 / 7 - gamma * 100 / 14 ** 2)
     gamma = 0.2
     result = modularity(G, C, resolution=gamma)
@@ -89,59 +87,47 @@ def test_modularity_resolution():
 
     C = [{0, 1, 2}, {3, 4, 5}]
     assert modularity(G, C) == pytest.approx(6 / 7 - 98 / 14 ** 2)
-    print(modularity(G, C))
     gamma = 2
     result = modularity(G, C, resolution=gamma)
-    print(result)
     assert result == pytest.approx(6 / 7 - gamma * 98 / 14 ** 2)
     gamma = 0.2
     result = modularity(G, C, resolution=gamma)
-    print(result)
     assert result == pytest.approx(6 / 7 - gamma * 98 / 14 ** 2)
 
     G = nx.barbell_graph(5, 3)
     C = [frozenset(range(5)), frozenset(range(8, 13)), frozenset(range(5, 8))]
     gamma = 1
     result = modularity(G, C, resolution=gamma)
-    print(result)
     assert result == pytest.approx((22 / 24) - gamma * (918 / (48 ** 2)))
     assert result == pytest.approx(0.518229)  # This C is maximal for gamma=1
     gamma = 2
-    print(result)
     result = modularity(G, C, resolution=gamma)
     assert result == pytest.approx((22 / 24) - gamma * (918 / (48 ** 2)))
     gamma = 0.2
     result = modularity(G, C, resolution=gamma)
-    print(result)
     assert result == pytest.approx((22 / 24) - gamma * (918 / (48 ** 2)))
 
     C = [{0, 1, 2, 3}, {9, 10, 11, 12}, {5, 6, 7}, {4}, {8}]
     gamma = 1
     result = modularity(G, C, resolution=gamma)
-    print(result)
     assert result == pytest.approx((14 / 24) - gamma * (598 / (48 ** 2)))
     gamma = 2.5
     result = modularity(G, C, resolution=gamma)
-    print(result)
     assert result == pytest.approx((14 / 24) - gamma * (598 / (48 ** 2)))
     assert result == pytest.approx(-0.06553819)  # This C is maximal for gamma=2.5
     gamma = 0.2
     result = modularity(G, C, resolution=gamma)
-    print(result)
     assert result == pytest.approx((14 / 24) - gamma * (598 / (48 ** 2)))
 
     C = [frozenset(range(8)), frozenset(range(8, 13))]
     gamma = 1
     result = modularity(G, C, resolution=gamma)
-    print(result)
     assert result == pytest.approx((23 / 24) - gamma * (1170 / (48 ** 2)))
     gamma = 2
     result = modularity(G, C, resolution=gamma)
-    print(result)
     assert result == pytest.approx((23 / 24) - gamma * (1170 / (48 ** 2)))
     gamma = 0.3
     result = modularity(G, C, resolution=gamma)
-    print(result)
     assert result == pytest.approx((23 / 24) - gamma * (1170 / (48 ** 2)))
     assert result == pytest.approx(0.805990)  # This C is maximal for gamma=0.3
 
