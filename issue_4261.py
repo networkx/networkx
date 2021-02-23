a3cb58d03486ff7a9d0ad0145edc3d76e74527ab
YaoM1886 <yaom1886@gmail.com>
2021-02-23 15:33:31 +0800

    add file of fixing issue 4261

---
+++ issue_4261.py
@@ -0,0 +1,82 @@
+"""
+This file provides two functions for adding
+spectral-clustering in scikit-learn to networkx gallery example,
+which is supposed to be useful in two ways:
+
+-- given a spectral clustering, use the output affinity matrix to draw networkx graph.
+-- given a networkx graph, use the graph affinity matrix to do spectral clustering;
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import SpectralClustering
+from sklearn import datasets
+import networkx as nx
+from networkx.drawing.nx_agraph import graphviz_layout
+
+np.random.seed(0)
+
+
+def spectral_clustering_to_graph(clusters, n_clusters):
+    # get affinity matrix from spectral clustering
+    clusters = clusters
+    cluster_affinity_matrix = clusters.fit(X).affinity_matrix_
+
+    pred_labels = clusters.labels_.astype(int)
+    G = nx.from_scipy_sparse_matrix(cluster_affinity_matrix)
+
+    # randomly set n distinct colors for n clusters
+    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % np.random.randint(0, 0xFFFFFF), range(n)))
+    colors = get_colors(n_clusters)
+    color_map = []
+    for i in range(len(G.nodes)):
+        color_map.append(colors[pred_labels[i]])
+
+    # plot two subplots, one for predicted sample labels, one for predicted sample graph
+    plt.subplot(121)
+    plt.scatter(X[:, 0], X[:, 1], s=10, c=pred_labels)
+    plt.subplot(122)
+    nx.draw(G, with_labels=True, font_weight='bold', node_color=color_map, pos=graphviz_layout(G))
+    plt.show()
+
+    return G
+
+
+# given a networkx graph, do spectral clustering
+def graph_to_spectral_clustering(G, clusters, n_clusters):
+    # Extract a graph affinity matrix with each element represents weight between nodes
+    aff_matrix = nx.to_scipy_sparse_matrix(G)
+
+    # Spectral clustering
+    clusters = clusters
+    pred_labels = clusters.fit_predict(aff_matrix).astype(int)
+
+    # randomly set n distinct colors for n clusters
+    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % np.random.randint(0, 0xFFFFFF), range(n)))
+    colors = get_colors(n_clusters)
+    color_map = []
+    for i in range(len(G.nodes)):
+        color_map.append(colors[pred_labels[i]])
+
+    # Draw predicted cluster labels from graphs, graphviz_layout is for better graph visualization
+    nx.draw(G, with_labels=True, font_weight='bold', node_color=color_map, pos=graphviz_layout(G))
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    # toy example for spectral_clustering_to_graph
+    n_samples = 100
+    X, y = datasets.make_moons(n_samples=n_samples)
+    n_clusters = 2
+    known_clusters = SpectralClustering(affinity="nearest_neighbors", n_clusters=n_clusters, random_state=0,
+                                        assign_labels="discretize")
+
+    G = spectral_clustering_to_graph(known_clusters, n_clusters)
+
+    # toy example for graph_to_spectral_clustering
+    derived_clusters = SpectralClustering(affinity='precomputed',
+                                          assign_labels="discretize",
+                                          random_state=0,
+                                          n_clusters=n_clusters)
+    graph_to_spectral_clustering(G, derived_clusters, n_clusters)
