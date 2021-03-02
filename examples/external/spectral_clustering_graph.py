9ba41d866b3c9b23dbca462bdefcb8166abdb04d
Sharwin Bobde <bsharwin@gmail.com>
2021-03-02 15:13:47 +0100

    modifications to show make the demo and use-case clear.

--- examples/external/spectral_clustering_graph.py
+++ examples/external/spectral_clustering_graph.py
@@ -9,78 +9,73 @@ Example of writing creating a graph from a dataset with 2 features using Spectra
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import gridspec
-from sklearn import datasets
+from sklearn.datasets import make_blobs
 from sklearn.cluster import SpectralClustering
+
 import networkx as nx
 
 np.random.seed(0)
 
 
-def spectral_clustering_to_graph(n_clusters, sc: SpectralClustering, true_labels):
+def spectral_clustering_to_graph(X, sc: SpectralClustering):
     # get affinity matrix from spectral clustering
     clusters = sc.fit(X)
     cluster_affinity_matrix = clusters.affinity_matrix_
-    # print(cluster_affinity_matrix)
 
     pred_labels = clusters.labels_.astype(int)
     G = nx.from_scipy_sparse_matrix(cluster_affinity_matrix)
     # remove self edges
     G.remove_edges_from(nx.selfloop_edges(G))
 
-    # randomly set n distinct colors for n clusters
-    def get_colors(n):
-        return list(
-            map(lambda i: "#" + "%06x" % np.random.randint(0, 0xFFFFFF), range(n))
-        )
-
-    colors = get_colors(n_clusters)
-    color_map = []
+    cluser_member = []
     for u in G.nodes:
-        color_map.append(colors[pred_labels[u]])
+        cluser_member.append(pred_labels[u])
 
     # plot two subplots, one for predicted sample labels, one for predicted sample graph
     # Create 2x2 sub plots
     gs = gridspec.GridSpec(2, 2)
-    fig = plt.figure(figsize=(15, 10))
+    fig = plt.figure(figsize=(15, 15))
     fig.suptitle("Feature Space to Spectral Clustering based Graph", fontsize=20)
 
-    ax = plt.subplot(gs[0, 0])
-    ax.set_title("Step 1. True Data")
-    ax.scatter(X[:, 0], X[:, 1], s=26, c=true_labels, cmap="Spectral")
+    ax = plt.subplot(gs[0, 0], projection="3d")
+    ax.set_title("Step 1. Scatterplot for available data")
+    ax.scatter(X[:, 0], X[:, 1], X[:, 2], s=26, alpha=0.3, cmap="cool")
     ax.set_xlabel("feature 0")
     ax.set_ylabel("feature 1")
+    ax.set_zlabel("feature 2")
 
-    ax = plt.subplot(gs[0, 1])
-    ax.set_title("Step 2. Clustering Output")
-    ax.scatter(X[:, 0], X[:, 1], s=26, c=pred_labels, cmap="copper")
+    ax = plt.subplot(gs[0, 1], projection="3d")
+    ax.set_title("Step 2. Scatterplot for the Clustering Output")
+    ax.scatter(X[:, 0], X[:, 1], X[:, 2], s=26, c=pred_labels, alpha=0.3, cmap="cool")
     ax.set_xlabel("feature 0")
     ax.set_ylabel("feature 1")
+    ax.set_zlabel("feature 2")
 
     # draw resulting network according to Force directed layout to keep relative node separations
     # in the visualizations close to the feature space
     ax = plt.subplot(gs[1, :])
-    ax.set_title("Step 3. Resulting Network")
+    ax.set_title("Step 3. Resulting Graph from Affinity Matrix")
+    pos = nx.spring_layout(G, seed=19, iterations=100)
     nx.draw_networkx(
         G,
-        node_color=color_map,
-        pos=nx.kamada_kawai_layout(G),
-        min_source_margin=10,
-        min_target_margin=10,
+        node_color=cluser_member,
+        cmap=plt.cm.cool,
+        pos=pos,
+        alpha=0.5,
+        node_size=100,
+        with_labels=False,
         ax=ax,
     )
+    plt.tight_layout()
     plt.show()
     return G
 
 
 if __name__ == "__main__":
-    # toy example data
-    n_samples = 100
-    X, y = datasets.make_moons(n_samples=n_samples)
+    n_samples = 500
+    X, Y = make_blobs(n_samples=n_samples, centers=3, n_features=3, random_state=4242)
 
-    # add noise to data
-    X = X + np.random.normal(1, 0.1, X.shape)
-
-    num_clusters = 2
+    num_clusters = 3
     spectralClustering = SpectralClustering(
         n_clusters=num_clusters,
         affinity="nearest_neighbors",
@@ -90,4 +85,6 @@ if __name__ == "__main__":
         n_jobs=-1,
     )
 
-    G_output = spectral_clustering_to_graph(num_clusters, spectralClustering, y)
+    G_output = spectral_clustering_to_graph(X, spectralClustering)
+
+    print(G_output.edges.data())
