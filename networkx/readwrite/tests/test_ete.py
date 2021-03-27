40884d1aedfd741eb72b4cdbb49ffc4498e90c4e
Vadim Abzalov <vadim.i.abzalov@gmail.com>
2021-03-27 11:30:15 +0300

    Revised test_ete.py

--- networkx/readwrite/tests/test_ete.py
+++ networkx/readwrite/tests/test_ete.py
@@ -19,30 +19,30 @@ class TestETE:
 
     @classmethod
     def build_graphs(cls):
-        cls.G = nx.Graph(name="test_undirected_graph")
         cls.DG = nx.DiGraph(name="test_directed_graph")
-        cls.MG = nx.MultiGraph(name="test_multi_graph")
+        cls.MDG = nx.MultiDiGraph(name="test_multi_graph")
 
         # Setting nodes with data
         for i in range(0, 42):
             node = str(i)
-            cls.G.add_node(node, data="test_data")
             cls.DG.add_node(node, data="test_data")
-            cls.MG.add_node(node, data="test_data")
+            cls.MDG.add_node(node, data="test_data")
 
         # Setting edges
         for i in range(1, 42):
             u = str(i - 1)
             v = str(i)
 
-            cls.G.add_edge(u, v)
             cls.DG.add_edge(u, v)
-            cls.MG.add_edge(u, v)
+            cls.MDG.add_edge(u, v)
 
     def assert_equal_file_transform(self, G, data=False):
         (fd, fname) = tempfile.mkstemp()
         nx.write_ete(G, fname)
         Gin = nx.read_ete(fname)
+
+        assert nx.is_arborescence(G)
+        assert nx.is_arborescence(Gin)
 
         assert_nodes_equal(list(G), list(Gin))
         assert_edges_equal(G.edges(data=data), Gin.edges(data=data))
@@ -54,23 +54,20 @@ class TestETE:
         T = nx.to_ete(G)
         Gin = nx.from_ete(T)
 
+        assert nx.is_arborescence(G)
+        assert nx.is_arborescence(Gin)
+
         assert_nodes_equal(list(G), list(Gin))
         assert_edges_equal(G.edges(data=data), Gin.edges(data=data))
-
-    def test_undirected_through_file_transformation(self):
-        self.assert_equal_file_transform(self.G, data=True)
 
     def test_directed_through_file_transformation(self):
         self.assert_equal_file_transform(self.DG, data=True)
 
     def test_multigraph_through_file_transformation(self):
-        self.assert_equal_file_transform(self.MG, data=True)
-
-    def test_undirected_through_instance_transformation(self):
-        self.assert_equal_instance_transform(self.G, data=True)
+        self.assert_equal_file_transform(self.MDG, data=True)
 
     def test_directed_through_instance_transformation(self):
         self.assert_equal_instance_transform(self.DG, data=True)
 
     def test_multigraph_through_instance_transformation(self):
-        self.assert_equal_instance_transform(self.MG, data=True)
+        self.assert_equal_instance_transform(self.MDG, data=True)
