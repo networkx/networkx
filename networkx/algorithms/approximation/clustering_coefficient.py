394289203cbf80d55963ce36f8e0dc8cd54928ac
Ross Barnowski <rossbar@berkeley.edu>
2021-04-09 10:14:29 -0700

    rm nx import from docstring example.

--- networkx/algorithms/approximation/clustering_coefficient.py
+++ networkx/algorithms/approximation/clustering_coefficient.py
@@ -39,7 +39,6 @@ def average_clustering(G, trials=1000, seed=None):
 
     Examples
     --------
-    >>> import networkx as nx
     >>> from networkx.algorithms import approximation
     >>> G = nx.erdos_renyi_graph(10, 0.2, seed=10)
     >>> approximation.average_clustering(G, trials=1000, seed=10)
