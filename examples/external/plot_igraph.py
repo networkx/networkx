9dfeb8549c3d7b3d6b435be4ded0215666ded40a
Mridul Seth <seth.mridul@gmail.com>
2021-05-11 16:03:21 +0200

    make plots less dense, enable plotting for igraph

--- examples/external/plot_igraph.py
+++ examples/external/plot_igraph.py
@@ -15,7 +15,7 @@ import igraph as ig
 # NetworkX to igraph
 # ------------------
 
-G = nx.dense_gnm_random_graph(1000, 2000)
+G = nx.dense_gnm_random_graph(50, 100, seed=42)
 
 # largest connected component
 components = nx.connected_components(G)
@@ -29,16 +29,15 @@ plt.show()
 # convert to igraph
 g = ig.Graph.from_networkx(G)
 
-## TODO: uncomment this once python-igraph 0.9 is released
 # igraph draw
-# layout = g.layout()
-# ig.plot(g, layout=layout)
+layout = g.layout()
+ig.plot(g, layout=layout)
 
 # %%
 # igraph to NetworkX
 # ------------------
 
-g = ig.Graph.GRG(100, 0.2)
+g = ig.Graph.GRG(50, 0.2)
 G = g.to_networkx()
 nx.draw(G)
 plt.show()
