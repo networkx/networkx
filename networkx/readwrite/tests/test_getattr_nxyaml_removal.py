d3fb14423190c5024064f33133e782f43cda70aa
Ross Barnowski <rossbar@berkeley.edu>
2021-05-13 20:21:52 -0700

    Add tests for informative messages.

--- networkx/readwrite/tests/test_getattr_nxyaml_removal.py
+++ networkx/readwrite/tests/test_getattr_nxyaml_removal.py
@@ -3,9 +3,36 @@ access nx_yaml."""
 
 import pytest
 
+_msg_stub = "\n.* has been removed from NetworkX"
 
-def test_import_from_module():
-    with pytest.raises(ImportError):
-        import networkx.readwrite.nx_yaml.read_yaml
-    with pytest.raises(ImportError):
-        import networkx.readwrite.nx_yaml.write_yaml
+
+def test_access_from_module():
+    with pytest.raises(ImportError, match=_msg_stub):
+        from networkx.readwrite.nx_yaml import read_yaml
+    with pytest.raises(ImportError, match=_msg_stub):
+        from networkx.readwrite.nx_yaml import write_yaml
+
+
+def test_access_from_nx_namespace():
+    import networkx as nx
+
+    with pytest.raises(ImportError, match=_msg_stub):
+        nx.read_yaml
+    with pytest.raises(ImportError, match=_msg_stub):
+        nx.write_yaml
+
+
+def test_access_from_readwrite_pkg():
+    from networkx import readwrite
+
+    with pytest.raises(ImportError, match=_msg_stub):
+        readwrite.read_yaml
+    with pytest.raises(ImportError, match=_msg_stub):
+        readwrite.write_yaml
+
+
+def test_accessing_nx_yaml():
+    import networkx as nx
+
+    with pytest.raises(ImportError, match=_msg_stub):
+        nx.nx_yaml
