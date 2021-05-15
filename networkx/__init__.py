6774875a1da4136f519052d86bcf1405774cb5f7
Ross Barnowski <rossbar@berkeley.edu>
2021-05-15 13:14:37 -0700

    Add security warning and removal time to message.

--- networkx/__init__.py
+++ networkx/__init__.py
@@ -18,12 +18,15 @@ def __getattr__(name):
             "\nThe nx_yaml module has been removed from NetworkX.\n"
             "Please use the `yaml` package directly for working with yaml data.\n"
             "For example, a networkx.Graph `G` can be written to and loaded\n"
-            "from a yaml file as:\n\n"
+            "from a yaml file with:\n\n"
             "    import yaml\n\n"
             "    with open('path_to_yaml_file', 'w') as fh:\n"
             "        yaml.dump(G, fh)\n"
             "    with open('path_to_yaml_file', 'r') as fh:\n"
-            "        G = yaml.load(fh, Loader=yaml.Loader)\n"
+            "        G = yaml.load(fh, Loader=yaml.Loader)\n\n"
+            "Note that yaml.Loader is considered insecure - see the pyyaml\n"
+            "documentation for further details.\n\n"
+            "This message will be removed in NetworkX 3.0."
         )
     if name == "read_yaml":
         raise ImportError(
@@ -31,15 +34,19 @@ def __getattr__(name):
             "directly:\n\n"
             "    import yaml\n\n"
             "    with open('path', 'r') as fh:\n"
-            "        yaml.load(fh, Loader=yaml.Loader)"
+            "        yaml.load(fh, Loader=yaml.Loader)\n\n"
+            "Note that yaml.Loader is considered insecure - see the pyyaml\n"
+            "documentation for further details.\n\n"
+            "This message will be removed in NetworkX 3.0."
         )
     if name == "write_yaml":
         raise ImportError(
             "\nwrite_yaml has been removed from NetworkX, please use `yaml`\n"
             "directly:\n\n"
             "    import yaml\n\n"
-            "    with oepn('path_for_yaml_output', 'w') as fh:\n"
-            "        yaml.dump(G_to_be_yaml, path_for_yaml_output, **kwds)"
+            "    with open('path_for_yaml_output', 'w') as fh:\n"
+            "        yaml.dump(G_to_be_yaml, path_for_yaml_output, **kwds)\n\n"
+            "This message will be removed in NetworkX 3.0."
         )
     raise AttributeError(f"module {__name__} has no attribute {name}")
 
