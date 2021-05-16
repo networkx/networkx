f4a1ca5c0ff803f852073a5519dc5e9bf666dcbd
Mdontas <mdontas@github.com>
2021-05-16 16:52:47 +0300

    Test cases added for nearest insertion method

--- networkx/algorithms/approximation/tests/test_traveling_salesman.py
+++ networkx/algorithms/approximation/tests/test_traveling_salesman.py
@@ -216,6 +216,55 @@ class TestGreedyTSPMinimumInsertions:
         assert len(cycle) - 1 == len(G) == len(set(cycle))
 
 
+class TestNearestInsertion:
+    setup_class = _setup_class
+
+    def test_nearest_insertion(self):
+        cycle = nx.app.nearest_insertion(self.DG, source="D")
+        cost = sum(self.DG[n][nbr]["weight"] for n, nbr in pairwise(cycle))
+        validate_solution(cycle, cost, ["D", "C", "B", "A", "D"], 31.0)
+
+        cycle = nx.app.nearest_insertion(self.DG2, source="D")
+        cost = sum(self.DG2[n][nbr]["weight"] for n, nbr in pairwise(cycle))
+
+        validate_solution(cycle, cost, ["D", "C", "B", "A", "D"], 78.0)
+
+        cycle = nx.app.nearest_insertion(self.UG, source="D")
+        cost = sum(self.UG[n][nbr]["weight"] for n, nbr in pairwise(cycle))
+        validate_solution(cycle, cost, ["D", "A", "B", "C", "D"], 33.0)
+
+        # cycle = nx_app.greedy_tsp_minimum_insertions(self.UG2, source="D")
+        cycle = nx.app.nearest_insertion(self.UG2, source="D")
+        cost = sum(self.UG2[n][nbr]["weight"] for n, nbr in pairwise(cycle))
+        validate_solution(cycle, cost, ["D", "A", "B", "C", "D"], 25.0)
+
+    def test_not_complete_graph(self):
+        pytest.raises(
+            nx.NetworkXError, nx.app.nearest_insertion, self.incompleteUG
+        )
+        pytest.raises(
+            nx.NetworkXError, nx.app.nearest_insertion, self.incompleteDG
+        )
+
+    def test_not_weighted_graph(self):
+        nx.app.nearest_insertion(self.unweightedUG)
+        nx.app.nearest_insertion(self.unweightedDG)
+
+    def test_two_nodes(self):
+        G = nx.Graph()
+        G.add_weighted_edges_from({(1, 2, 1)})
+        cycle = nx.app.nearest_insertion(G)
+        cost = sum(G[n][nbr]["weight"] for n, nbr in pairwise(cycle))
+
+        validate_solution(cycle, cost, [1, 2, 1], 2)
+
+    def test_ignore_selfloops(self):
+        G = nx.complete_graph(5)
+        G.add_edge(3, 3)
+        cycle = nx.app.nearest_insertion(G)
+        assert len(cycle) - 1 == len(G) == len(set(cycle))
+
+
 class TestSimulatedAnnealingTSP:
     setup_class = _setup_class
     tsp = staticmethod(nx_app.simulated_annealing_tsp)
