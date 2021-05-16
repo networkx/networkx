5358d95039443d3b11e4fbc4685c23ef38a5742c
Jarrod Millman <jarrod.millman@gmail.com>
2021-05-15 22:27:34 -0700

    Reclaim empty sidebar space

--- doc/conf.py
+++ doc/conf.py
@@ -139,8 +139,15 @@ html_theme_options = {
         },
     ],
     "navbar_end": ["navbar-icon-links", "version"],
+    "page_sidebar_items": ["search-field", "page-toc", "edit-this-page"],
 }
-
+html_sidebars = {
+  "**": ["sidebar-nav-bs", "sidebar-ethical-ads"],
+  "index": [],
+  "install": [],
+  "tutorial": [],
+  "auto_examples/index": [],
+}
 html_logo = "_static/networkx_banner.svg"
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
