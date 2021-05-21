d58c0c01e72325f284e6a703e8f4da572754d508
Jarrod Millman <jarrod.millman@gmail.com>
2021-05-21 10:38:51 -0700

    Refactor testing utilities
    Change `assert_edges_equal`, `assert_graphs_equal`, and `assert_nodes_equal`
    to be more pytest-idiomatic.
    For example, `assert_edges_equal` becomes the Boolean function `edges_equal`
    and then the assert is done the testing file
    (i.e., `assert edges_equal(edges1, edges2)`).
    This also makes these utility functions useful in nontesting situations
    where you want to compare edges, but not raise an exception based on the result.

--- networkx/testing/utils.py
+++ networkx/testing/utils.py
@@ -20,6 +20,18 @@ def almost_equal(x, y, places=7):
 
 
 def assert_nodes_equal(nodes1, nodes2):
+    assert nodes_equal(nodes1, nodes2)
+
+
+def assert_edges_equal(edges1, edges2):
+    assert edges_equal(edges1, edges2)
+
+
+def assert_graphs_equal(graph1, graph2):
+    assert graphs_equal(graph1, graph2)
+
+
+def nodes_equal(nodes1, nodes2):
     # Assumes iterables of nodes, or (node,datadict) tuples
     nlist1 = list(nodes1)
     nlist2 = list(nodes2)
@@ -29,10 +41,10 @@ def assert_nodes_equal(nodes1, nodes2):
     except (ValueError, TypeError):
         d1 = dict.fromkeys(nlist1)
         d2 = dict.fromkeys(nlist2)
-    assert d1 == d2
+    return d1 == d2
 
 
-def assert_edges_equal(edges1, edges2):
+def edges_equal(edges1, edges2):
     # Assumes iterables with u,v nodes as
     # edge tuples (u,v), or
     # edge tuples with data dicts (u,v,d), or
@@ -57,18 +69,25 @@ def assert_edges_equal(edges1, edges2):
             data = d2[u][v] + data
         d2[u][v] = data
         d2[v][u] = data
-    assert c1 == c2
+    if c1 != c2:
+        return False
     # can check one direction because lengths are the same.
     for n, nbrdict in d1.items():
         for nbr, datalist in nbrdict.items():
-            assert n in d2
-            assert nbr in d2[n]
+            if n not in d2:
+                return False
+            if nbr not in d2[n]:
+                return False
             d2datalist = d2[n][nbr]
             for data in datalist:
-                assert datalist.count(data) == d2datalist.count(data)
+                if datalist.count(data) != d2datalist.count(data):
+                    return False
+    return True
 
 
-def assert_graphs_equal(graph1, graph2):
-    assert graph1.adj == graph2.adj
-    assert graph1.nodes == graph2.nodes
-    assert graph1.graph == graph2.graph
+def graphs_equal(graph1, graph2):
+    return (
+        graph1.adj == graph2.adj
+        and graph1.nodes == graph2.nodes
+        and graph1.graph == graph2.graph
+    )
