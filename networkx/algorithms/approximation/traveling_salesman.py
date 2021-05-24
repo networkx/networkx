d3a3db8823804faa3edbf8bfa0f4b12459143ac8
mjschwenne <mjschwenne@gmail.com>
2021-05-24 16:58:20 -0400

    Added function stubs and draft docstrings for the Asadpour algorithm

--- networkx/algorithms/approximation/traveling_salesman.py
+++ networkx/algorithms/approximation/traveling_salesman.py
@@ -206,10 +206,7 @@ def traveling_salesman_problem(G, weight="weight", nodes=None, cycle=True, metho
     Second, an algorithm (default: `christofides`) is used to approximate
     the minimal Hamiltonian cycle on this new graph. The available
     algorithms are:
-<<<<<<< HEAD
-=======
 
->>>>>>> 7ebd9f94a8e9ca8e3f08b93cfdb46103b8925cd1
      - christofides
      - greedy_tsp
      - simulated_annealing_tsp
@@ -325,6 +322,174 @@ def traveling_salesman_problem(G, weight="weight", nodes=None, cycle=True, metho
 
 
 def asadpour_tsp(G, weight="weight"):
+    """
+    Returns an O( log n / log log n ) approximate solution to the traveling
+    salesman problem.
+
+    This approximate solution is one of the best known approximations for
+    the asymmetric traveling salesman problem developed by Asadpour et al,
+    [1]_. The algorithm first solves the Held-Karp relaxation to find a
+    lower bound for the weight of the cycle. Next, it constructs an
+    exponential distribution of undirected spanning trees where the
+    probability of an edge being in the tree corresponds to the weight of
+    that edge using a maximum entropy rounding scheme. Next we sample that
+    distribution $2 \\log n$ times and saves the minimum sampled tree once
+    the direction of the arcs is added back to the edges. Finally,
+    we argument then short circuit that graph to find the approximate tour
+    for the salesman.
+
+    Parameters
+    ----------
+    G : nx.DiGraph
+        The graph should be a complete weighted directed graph.
+        The distance between all paris of nodes should be included.
+
+    weight : string, optional (default="weight")
+        Edge data key corresponding to the edge weight.
+        If any edge does not have this attribute the weight is set to 1.
+
+    Returns ------- cycle : list of nodes Returns the cycle (list of nodes)
+    that a salesman can follow to minimize the total weight of the trip.
+
+    Raises
+    ------
+    NetworkXError
+        If `G` is not complete, the algorithm raises an exception.
+
+    References
+    ----------
+    .. [1] A. Asadpour, M. X. Goemans, A. Madry, S. O. Gharan, and A. Saberi,
+       An o(log n/log log n)-approximation algorithm for the asymmetric
+       traveling salesman problem, Operations research, 65 (2017),
+       pp. 1043–1061
+    """
+    pass
+
+
+def _held_karp(G, weight="weight"):
+    """
+    Solves the Held-Karp relaxation of the input complete digraph and scales
+    the output solution for use in the Asadpour [1]_ ASTP algorithm.
+
+    The Held-Karp relaxation defines the lower bound for solutions to the
+    ATSP, although it does return a fractional solution. This is used in the
+    Asadpour algorithm as an initial solution which is later rounded to a
+    integral tree within the spanning tree polytopes. This function solves
+    the relaxation with the ellipsoid method for linear programs.
+
+    Parameters
+    ----------
+    G : nx.DiGraph
+        The graph should be a complete weighted directed graph.
+        The distance between all paris of nodes should be included.
+
+    weight : string, optional (default="weight")
+        Edge data key corresponding to the edge weight.
+        If any edge does not have this attribute the weight is set to 1.
+
+    Returns
+    -------
+    OPT : float
+        The cost for the optimal solution to the Held-Karp relaxation
+    z_star : numpy array
+        A symmetrized and scaled version of the optimal solution to the
+        Held-Karp relaxation for use in the Asadpour algorithm
+
+    References
+    ----------
+    .. [1] A. Asadpour, M. X. Goemans, A. Madry, S. O. Gharan, and A. Saberi,
+       An o(log n/log log n)-approximation algorithm for the asymmetric
+       traveling salesman problem, Operations research, 65 (2017),
+       pp. 1043–1061
+    """
+    def sep_oracle(point):
+        """
+        The separation oracle used in the ellipsoid algorithm to solve the
+        Held-Karp relaxation.
+
+        This 'black-box' takes a point and check to see if it violates any
+        of the Held-Karp constraints, which are defined as
+
+            - The out-degree of all non-empty subsets of $V$ is at lest one.
+            - The in-degree and out-degree of each vertex in $V$ is equal to
+              one. Note that if a vertex has more than one incoming or
+              outgoing arcs the values of each could be less than one so long
+              as they sum to one.
+            - The current value for each arc is greater
+              than zero.
+
+        Parameters
+        ----------
+        point : numpy array
+            The point in n dimensional space we will to test to see if it
+            violations any of the Held-Karp constraints.
+
+        Returns
+        -------
+        numpy array
+            The hyperplane which was the most violated by `point`, i.e the
+            hyperplane defining the polytope of spanning trees which `point`
+            was farthest from, None if no constraints are violated.
+        """
+        pass
+    pass
+
+
+def _spanning_tree_distribution(z_star):
+    """
+    Solves the Maximum Entropy Convex Program in the Asadpour algorithm [1]_
+    using the approach in section 7 to build an exponential distribution of
+    undirected spanning trees.
+
+    This algorithm ensures that the probability of any edge in a spanning
+    tree is proportional to the sum of the probabilities of the tress
+    containing that edge over the sum of the probabilities of all spanning
+    trees of the graph.
+
+    Parameters
+    ----------
+    z_star : numpy array
+        The output of `_held_karp()`, a scaled version of the Held-Karp
+        solution.
+
+    Returns
+    -------
+    gamma : numpy array
+        The probability distribution which approximately preserves the marginal
+        probabilities of `z_star`.
+    """
+    pass
+
+
+def _sample_spanning_tree(G, gamma):
+    """
+    Sample one spanning tree from the distribution defined by `gamma`,
+    roughly using algorithm A8 in [1]_ .
+
+    We 'shuffle' the edges in the graph, and then probabilistically
+    determine weather to add the edge conditioned on all of the previous
+    edges which where added to the tree. Probabilities are calculated using
+    Kirchhoff's Matrix Tree Theorem and a weighted Laplacian matrix.
+
+    Parameters
+    ----------
+    G : nx.Graph
+        An undirected version of the original graph.
+
+    gamma : numpy array
+        The probabilities associated with each of the edges in the undirected
+        graph `G`.
+
+    Returns
+    -------
+    nx.Graph
+        A spanning tree using the distribution defined by `gamma`.
+
+    References
+    ----------
+    .. [1] V. Kulkarni, Generating random combinatorial objects, Journal of
+       algorithms, 11 (1990), pp. 185–207
+    """
     pass
 
 
@@ -421,16 +586,16 @@ def greedy_tsp(G, weight="weight", source=None):
 
 @py_random_state(9)
 def simulated_annealing_tsp(
-    G,
-    init_cycle,
-    weight="weight",
-    source=None,
-    temp=100,
-    move="1-1",
-    max_iterations=10,
-    N_inner=100,
-    alpha=0.01,
-    seed=None,
+        G,
+        init_cycle,
+        weight="weight",
+        source=None,
+        temp=100,
+        move="1-1",
+        max_iterations=10,
+        N_inner=100,
+        alpha=0.01,
+        seed=None,
 ):
     """Returns an approximate solution to the traveling salesman problem.
 
@@ -641,16 +806,16 @@ def simulated_annealing_tsp(
 
 @py_random_state(9)
 def threshold_accepting_tsp(
-    G,
-    init_cycle,
-    weight="weight",
-    source=None,
-    threshold=1,
-    move="1-1",
-    max_iterations=10,
-    N_inner=100,
-    alpha=0.1,
-    seed=None,
+        G,
+        init_cycle,
+        weight="weight",
+        source=None,
+        threshold=1,
+        move="1-1",
+        max_iterations=10,
+        N_inner=100,
+        alpha=0.1,
+        seed=None,
 ):
     """Returns an approximate solution to the traveling salesman problem.
 
@@ -684,10 +849,6 @@ def threshold_accepting_tsp(
     move : "1-1" or "1-0" or function, optional (default="1-1")
         Indicator of what move to use when finding new trial solutions.
         Strings indicate two special built-in moves:
-<<<<<<< HEAD
-=======
-
->>>>>>> 7ebd9f94a8e9ca8e3f08b93cfdb46103b8925cd1
         - "1-1": 1-1 exchange which transposes the position
           of two elements of the current solution.
           The function called is :func:`swap_two_nodes`.
