f71288a6be60772b2b3275601ad510902afa8ee4
Jarrod Millman <jarrod.millman@gmail.com>
2021-05-26 12:10:37 -0700

    Cleanup

--- networkx/conftest.py
+++ networkx/conftest.py
@@ -1,3 +1,16 @@
+"""
+Testing
+=======
+
+General guidelines for writing good tests:
+
+- doctests always assume ``import networkx as nx`` so don't add that
+- prefer pytest fixtures over classes with setup methods.
+- use the ``@pytest.mark.parametrize``  decorator
+- use ``pytest.importskip`` for numpy, scipy, pandas, and matplotlib b/c of PyPy.
+  and add the module to the relevant entries below.
+
+"""
 import pytest
 import networkx
 import sys
--- networkx/testing/__init__.py
+++ networkx/testing/__init__.py
@@ -1,15 +1,2 @@
-"""
-Testing
-=======
-
-General guidelines for writing good tests:
-
-- doctests always assume ``import networkx as nx`` so don't add that
-- prefer pytest fixtures over classes with setup methods.
-- use the ``@pytest.mark.parametrize``  decorator
-- use ``pytest.importskip`` for numpy, scipy, pandas, and matplotlib b/c of PyPy.
-  and add the module to the relevant entries in ``networkx/conftest.py``.
-
-"""
 from networkx.testing.utils import *
 from networkx.testing.test import run
