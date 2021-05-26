34887d15708529b1d711d47613e24a8e43f1d2e0
Jarrod Millman <jarrod.millman@gmail.com>
2021-05-25 18:58:52 -0700

    Update networkx/readwrite/tests/test_sparse6.py
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>

--- networkx/readwrite/tests/test_sparse6.py
+++ networkx/readwrite/tests/test_sparse6.py
@@ -3,8 +3,7 @@ import tempfile
 import pytest
 
 import networkx as nx
-from networkx.utils import edges_equal
-from networkx.utils import nodes_equal
+from networkx.utils import nodes_equal, edges_equal
 
 
 class TestSparseGraph6:
