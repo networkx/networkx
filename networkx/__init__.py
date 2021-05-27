3820c1b63155fe9b239e3f8207015e28a31bd824
Dan Schult <dschult@colgate.edu>
2021-05-27 01:45:58 -0400

    use `from ... import` in all __init__ imports

--- networkx/__init__.py
+++ networkx/__init__.py
@@ -53,36 +53,36 @@ def __getattr__(name):
 
 # These are import orderwise
 from networkx.exception import *
-import networkx.utils
 
-import networkx.classes.filters
-import networkx.classes
+from networkx import utils
+
+from networkx import classes
+from networkx.classes import filters
 from networkx.classes import *
 
-import networkx.convert
+from networkx import convert
 from networkx.convert import *
 
-import networkx.convert_matrix
+from networkx import convert_matrix
 from networkx.convert_matrix import *
 
-
-import networkx.relabel
+from networkx import relabel
 from networkx.relabel import *
 
-import networkx.generators
+from networkx import generators
 from networkx.generators import *
 
-import networkx.readwrite
+from networkx import readwrite
 from networkx.readwrite import *
 
 # Need to test with SciPy, when available
-import networkx.algorithms
+from networkx import algorithms
 from networkx.algorithms import *
 
-import networkx.linalg
+from networkx import linalg
 from networkx.linalg import *
 
 from networkx.testing.test import run as test
 
-import networkx.drawing
+from networkx import drawing
 from networkx.drawing import *
