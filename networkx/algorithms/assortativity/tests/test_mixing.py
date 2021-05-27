Fix assortativity coefficent calculation and tests

networkx/algorithms/assortativity/tests/test_mixing.py
@@ -33,9 +33,8 @@ class TestDegreeMixingDict(BaseTestDegreeMixing):
 class TestDegreeMixingMatrix(BaseTestDegreeMixing):
     def test_degree_mixing_matrix_undirected(self):
         # fmt: off
-        a_result = np.array([[0, 0, 0],
-                             [0, 0, 2],
-                             [0, 2, 2]]
+        a_result = np.array([[0, 2],
+                             [2, 2]]
                             )
         # fmt: on
         a = nx.degree_mixing_matrix(self.P4, normalized=False)
@@ -45,10 +44,9 @@ class TestDegreeMixingMatrix(BaseTestDegreeMixing):
 
     def test_degree_mixing_matrix_directed(self):
         # fmt: off
-        a_result = np.array([[0, 0, 0, 0],
-                             [0, 0, 0, 2],
-                             [0, 1, 0, 1],
-                             [0, 0, 0, 0]]
+        a_result = np.array([[0, 0, 2],
+                             [1, 0, 1],
+                             [0, 0, 0]]
                             )
         # fmt: on
         a = nx.degree_mixing_matrix(self.D, normalized=False)
@@ -58,10 +56,9 @@ class TestDegreeMixingMatrix(BaseTestDegreeMixing):
 
     def test_degree_mixing_matrix_multigraph(self):
         # fmt: off
-        a_result = np.array([[0, 0, 0, 0],
-                             [0, 0, 1, 0],
-                             [0, 1, 0, 3],
-                             [0, 0, 3, 0]]
+        a_result = np.array([[0, 1, 0],
+                             [1, 0, 3],
+                             [0, 3, 0]]
                             )
         # fmt: on
         a = nx.degree_mixing_matrix(self.M, normalized=False)
@@ -71,9 +68,7 @@ class TestDegreeMixingMatrix(BaseTestDegreeMixing):
 
     def test_degree_mixing_matrix_selfloop(self):
         # fmt: off
-        a_result = np.array([[0, 0, 0],
-                             [0, 0, 0],
-                             [0, 0, 2]]
+        a_result = np.array([[2]]
                             )
         # fmt: on
         a = nx.degree_mixing_matrix(self.S, normalized=False)