c444024a550510050b1c17c9fb4baa6a3c7a595d
Jarrod Millman <jarrod.millman@gmail.com>
2021-05-31 13:37:08 -0700

    Bump release version

--- networkx/__init__.py
+++ networkx/__init__.py
@@ -8,7 +8,7 @@ structure, dynamics, and functions of complex networks.
 See https://networkx.org for complete documentation.
 """
 
-__version__ = "2.6rc1"
+__version__ = "2.6rc2.dev0"
 
 
 def __getattr__(name):
