37aaa2858ce1d06a2a5df3adbbba88bda828bfc5
Ross Barnowski <rossbar@berkeley.edu>
2021-05-31 07:52:04 -0500

    Remove "networkx" from top-level networkx namespace (#4840)
    * Add a test to catch importing aliases.
    
    * Add test for another bad import pattern.
    
    * Fix one bad import pattern.
    
    Adds __all__ to some modules where it was missing.
    
    * use `from ... import` in all __init__ imports
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- networkx/tests/test_import.py
+++ networkx/tests/test_import.py
@@ -4,3 +4,8 @@ import pytest
 def test_namespace_alias():
     with pytest.raises(ImportError):
         from networkx import nx
+
+
+def test_namespace_nesting():
+    with pytest.raises(ImportError):
+        from networkx import networkx
