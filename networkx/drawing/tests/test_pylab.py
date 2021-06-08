cef600ff84d7e2832f029112313ac942087225e7
Ross Barnowski <rossbar@berkeley.edu>
2021-06-08 19:41:03 +0300

    Parametrize regression tests to include nx.draw.

--- networkx/drawing/tests/test_pylab.py
+++ networkx/drawing/tests/test_pylab.py
@@ -403,24 +403,27 @@ def test_draw_edges_toggling_with_arrows_kwarg():
     assert isinstance(edges[0], mpl.patches.FancyArrowPatch)
 
 
-def test_draw_networkx_arrows_default_undirected():
+@pytest.mark.parametrize("drawing_func", (nx.draw, nx.draw_networkx))
+def test_draw_networkx_arrows_default_undirected(drawing_func):
     import matplotlib.collections
 
     G = nx.path_graph(3)
     fig, ax = plt.subplots()
-    nx.draw_networkx(G, ax=ax)
+    drawing_func(G, ax=ax)
     assert any(isinstance(c, mpl.collections.LineCollection) for c in ax.collections)
     assert not ax.patches
     plt.delaxes(ax)
 
 
-def test_draw_networkx_arrows_default_directed():
+@pytest.mark.parametrize("drawing_func", (nx.draw, nx.draw_networkx))
+def test_draw_networkx_arrows_default_directed(drawing_func):
     import matplotlib.collections
 
     G = nx.path_graph(3, create_using=nx.DiGraph)
     fig, ax = plt.subplots()
-    nx.draw_networkx(G, ax=ax)
+    drawing_func(G, ax=ax)
     assert not any(
         isinstance(c, mpl.collections.LineCollection) for c in ax.collections
     )
     assert ax.patches
+    plt.delaxes(ax)
