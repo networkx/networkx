d4babbee9f638ed9744dae440938d48f7fee663d
eskountis <lefteris.skountis@gmail.com>
2021-06-18 11:37:05 +0100

    Refactor the plot_tsp drawing
    More specifically, I update the graph structure, so the graph should be more visually friendly by hiding edges that does not matter, the longests.

--- examples/drawing/plot_tsp.py
+++ examples/drawing/plot_tsp.py
@@ -5,58 +5,49 @@ Traveling Salesman Problem
 
 This is an example of a drawing solution of the traveling salesman problem
 
-The function is used to produce the solution is greedy_tsp,
-where given a set of nodes and a source, it calculates the route of the nodes
+The function is used to produce the solution is greedy_tsp (Nearest Neighbor),
+where given a set of nodes, it calculates the route of the nodes
 that the traveler has to follow in order to minimize the total cost.
-The criteria is that in every iteration choose the next node to be the closest one
-to the latest on the current route (Nearest Neighbor).
 """
 
 import matplotlib.pyplot as plt
 import networkx as nx
 import networkx.algorithms.approximation as nx_app
-import random
 import math
 
-G = nx.DiGraph()
+G = nx.random_geometric_graph(20, radius=0.4, seed=3)
+pos = nx.get_node_attributes(G, "pos")
 
-total_nodes = 10
-pos = {}
-random.seed(1)
-for n in range(0, total_nodes):
-    x = random.randint(0, 100)
-    y = random.randint(0, 100)
-    pos.update({n: (x, y)})
-G.add_nodes_from(pos.keys())
+# Depot should be at (0,0)
+pos[0][0], pos[0][1] = 0.5, 0.5
 
-for i in range(0, total_nodes):
-    for j in range(0, total_nodes):
-        if i == j:
-            continue
-        dist = math.sqrt(
-            math.pow(pos[i][0] - pos[j][0], 2) + math.pow(pos[i][1] - pos[j][1], 2)
-        )
-        dist = round(dist)
+H = G.copy()
+
+
+# Calculating the distances between the nodes as edge's weight.
+for i in range(len(pos)):
+    for j in range(i + 1, len(pos)):
+        dist = math.hypot(pos[i][0] - pos[j][0], pos[i][1] - pos[j][1])
+        dist = dist
         G.add_edge(i, j, weight=dist)
-nodes_labels = {x: x for x in pos.keys()}
-nx.draw_networkx_nodes(G, pos, node_size=200)
-nx.draw_networkx_labels(G, pos, labels=nodes_labels)
 
 cycle = nx_app.greedy_tsp(G, source=0)
-
 edge_list = [(x1, x2) for x1, x2 in nx.utils.pairwise(cycle)]
 
-nx.draw_networkx_edges(G, pos, edge_color="blue", width=0.5, arrows=False)
+# Draw closest edges on each node only
+nx.draw_networkx_edges(H, pos, edge_color="blue", width=0.5)
+
+# Draw the route
 nx.draw_networkx(
     G,
     pos,
-    arrows=True,
     with_labels=True,
     nodelist=set(cycle),
     edgelist=edge_list,
     edge_color="red",
     node_size=200,
-    width=4,
+    width=3,
 )
+
 print("The route of the traveller is:", cycle)
 plt.show()
