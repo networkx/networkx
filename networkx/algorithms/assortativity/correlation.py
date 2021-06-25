fe1918b96938977f716832a6660bb7dad30aafe6
vpozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2021-06-25 06:09:09 +0300

    Reading node degrees once

--- networkx/algorithms/assortativity/correlation.py
+++ networkx/algorithms/assortativity/correlation.py
@@ -75,7 +75,7 @@ def degree_assortativity_coefficient(G, x="out", y="in", weight=None, nodes=None
     """
     if nodes is None:
         nodes = G.nodes
-    degrees = set([G.degree(weight=weight)[n] for n in nodes])
+    degrees = set([d for n, d in G.degree(nodes, weight=weight)])
     mapping = {d: i for i, d, in enumerate(degrees)}
     M = degree_mixing_matrix(G, x=x, y=y, nodes=nodes, weight=weight, mapping=mapping)
     return numeric_ac(M, mapping=mapping)
