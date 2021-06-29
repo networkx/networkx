7273f52cd87bc34399dedda2a7b83e578671e4f1
z3y50n <dim_papag@windowslive.com>
2021-06-29 11:48:41 +0300

    add test for partition validation

--- networkx/algorithms/community/tests/test_louvain.py
+++ networkx/algorithms/community/tests/test_louvain.py
@@ -1,7 +1,7 @@
 import pytest
 
 import networkx as nx
-from networkx.algorithms.community import louvain_communities, modularity
+from networkx.algorithms.community import louvain_communities, modularity, is_partition
 
 
 def test_modularity_increase():
@@ -13,3 +13,12 @@ def test_modularity_increase():
     partitions = louvain_communities(G)
 
     assert modularity(G, partitions[-1]) > mod
+
+
+def test_valid_partition():
+    G = nx.LFR_benchmark_graph(
+        250, 3, 1.5, 0.009, average_degree=5, min_community=20, seed=10
+    )
+    partitions = louvain_communities(G)
+
+    assert is_partition(G, partitions[-1])
