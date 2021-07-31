f1f64911a6c8f859c5593b6c7ace73dedfaf2f48
Tanguy Fardet <tanguyfardet@protonmail.com>
2021-07-31 13:29:12 +0200

    added test

--- networkx/algorithms/assortativity/tests/base_test.py
+++ networkx/algorithms/assortativity/tests/base_test.py
@@ -44,6 +44,8 @@ class BaseTestDegreeMixing:
         cls.P4 = nx.path_graph(4)
         cls.D = nx.DiGraph()
         cls.D.add_edges_from([(0, 2), (0, 3), (1, 3), (2, 3)])
+        cls.D2 = nx.DiGraph()
+        cls.D2.add_edges_from([(0, 3), (1, 0), (1, 2), (2, 4), (4, 1), (4, 3), (4, 2)])
         cls.M = nx.MultiGraph()
         nx.add_path(cls.M, range(4))
         cls.M.add_edge(0, 1)
