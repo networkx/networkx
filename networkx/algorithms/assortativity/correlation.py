2f6bf20811969b158deb8b78c8b07533da736876
Tanguy Fardet <tanguyfardet@protonmail.com>
2021-07-31 12:41:06 +0200

    Fix degree_assortativity_coefficient for directed graphs

--- networkx/algorithms/assortativity/correlation.py
+++ networkx/algorithms/assortativity/correlation.py
@@ -75,9 +75,23 @@ def degree_assortativity_coefficient(G, x="out", y="in", weight=None, nodes=None
     """
     if nodes is None:
         nodes = G.nodes
-    degrees = set([d for n, d in G.degree(nodes, weight=weight)])
+
+    degrees = None
+
+    if G.is_directed():
+        degree_method = {"out": G.out_degree, "in": G.in_degree}
+
+        degrees = set([d for _, d in degree_method[x](nodes, weight=weight)])
+
+        if y != x:
+            degrees.update([d for _, d in degree_method[y](nodes, weight=weight)])
+    else:
+        degrees = set([d for _, d in G.degree(nodes, weight=weight)])
+
     mapping = {d: i for i, d, in enumerate(degrees)}
+
     M = degree_mixing_matrix(G, x=x, y=y, nodes=nodes, weight=weight, mapping=mapping)
+
     return numeric_ac(M, mapping=mapping)
 
 
