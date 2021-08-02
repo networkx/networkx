0c6ed4d8a846bc45e461cff94b539c2bc5c5dcc3
Ross Barnowski <rossbar@berkeley.edu>
2021-08-02 14:07:10 +0300

    Minor style changes + add note to test.

--- networkx/algorithms/community/tests/test_modularity_max.py
+++ networkx/algorithms/community/tests/test_modularity_max.py
@@ -25,12 +25,11 @@ def test_modularity_communities(func):
 
 
 def test_greedy_modularity_communities_relabeled():
+    # Test for gh-4966
     G = nx.balanced_tree(2, 2)
     mapping = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h"}
     G = nx.relabel_nodes(G, mapping)
-
     expected = [frozenset({"e", "d", "a", "b"}), frozenset({"c", "f", "g"})]
-
     assert greedy_modularity_communities(G) == expected
 
 
