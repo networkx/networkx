6835dac3c78e0db771d2c6d8d6051b68379485e7
Ross Barnowski <rossbar@berkeley.edu>
2021-08-29 21:33:22 -0500

    if instead of try/except.

--- networkx/readwrite/edgelist.py
+++ networkx/readwrite/edgelist.py
@@ -239,14 +239,12 @@ def parse_edgelist(
 
     G = nx.empty_graph(0, create_using)
     for line in lines:
-        try:  # Ignore case where comments=None
+        if comments is not None:
             p = line.find(comments)
             if p >= 0:
                 line = line[:p]
             if not line:
                 continue
-        except TypeError:
-            pass
         # split line, should have 2 or more
         s = line.strip().split(delimiter)
         if len(s) < 2:
