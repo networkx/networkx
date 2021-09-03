42968daf7e507f834c58a7b5b0b5f43fd9fef082
Ross Barnowski <rossbar@berkeley.edu>
2021-09-03 09:09:39 -0500

    Change naming scheme for backward compatibility.

--- networkx/readwrite/graphml.py
+++ networkx/readwrite/graphml.py
@@ -429,7 +429,7 @@ class GraphML:
                 (np.intp, "int"),
             ] + types
 
-        self._xml_type = dict(types)
+        self.xml_type = dict(types)
         self.python_type = dict(reversed(a) for a in types)
 
     # This page says that data types in GraphML follow Java(TM).
@@ -447,9 +447,12 @@ class GraphML:
         1: True,
     }
 
-    def xml_type(self, key):
+    def get_xml_type(self, key):
+        """Wrapper around the xml_type dict that raises a more informative
+        exception message when a user attempts to use data of a type not
+        supported by GraphML."""
         try:
-            return self._xml_type[key]
+            return self.xml_type[key]
         except KeyError as e:
             raise TypeError(
                 f"GraphML does not support type {type(key)} as data values."
@@ -512,7 +515,7 @@ class GraphMLWriter(GraphML):
             types = self.attribute_types[(name, scope)]
 
             if len(types) > 1:
-                types = {self.xml_type(t) for t in types}
+                types = {self.get_xml_type(t) for t in types}
                 if "string" in types:
                     return str
                 elif "float" in types or "double" in types:
@@ -555,11 +558,11 @@ class GraphMLWriter(GraphML):
         Make a data element for an edge or a node. Keep a log of the
         type in the keys table.
         """
-        if element_type not in self._xml_type:
+        if element_type not in self.xml_type:
             raise nx.NetworkXError(
                 f"GraphML writer does not support {element_type} as data values."
             )
-        keyid = self.get_key(name, self.xml_type(element_type), scope, default)
+        keyid = self.get_key(name, self.get_xml_type(element_type), scope, default)
         data_element = self.myElement("data", key=keyid)
         data_element.text = str(value)
         return data_element
@@ -773,7 +776,7 @@ class GraphMLWriterLxml(GraphMLWriter):
         for k, v in graphdata.items():
             self.attribute_types[(str(k), "graph")].add(type(v))
         for k, v in graphdata.items():
-            element_type = self.xml_type(self.attr_type(k, "graph", v))
+            element_type = self.get_xml_type(self.attr_type(k, "graph", v))
             self.get_key(str(k), element_type, "graph", None)
         # Nodes and data
         for node, d in G.nodes(data=True):
@@ -781,7 +784,7 @@ class GraphMLWriterLxml(GraphMLWriter):
                 self.attribute_types[(str(k), "node")].add(type(v))
         for node, d in G.nodes(data=True):
             for k, v in d.items():
-                T = self.xml_type(self.attr_type(k, "node", v))
+                T = self.get_xml_type(self.attr_type(k, "node", v))
                 self.get_key(str(k), T, "node", node_default.get(k))
         # Edges and data
         if G.is_multigraph():
@@ -790,7 +793,7 @@ class GraphMLWriterLxml(GraphMLWriter):
                     self.attribute_types[(str(k), "edge")].add(type(v))
             for u, v, ekey, d in G.edges(keys=True, data=True):
                 for k, v in d.items():
-                    T = self.xml_type(self.attr_type(k, "edge", v))
+                    T = self.get_xml_type(self.attr_type(k, "edge", v))
                     self.get_key(str(k), T, "edge", edge_default.get(k))
         else:
             for u, v, d in G.edges(data=True):
@@ -798,7 +801,7 @@ class GraphMLWriterLxml(GraphMLWriter):
                     self.attribute_types[(str(k), "edge")].add(type(v))
             for u, v, d in G.edges(data=True):
                 for k, v in d.items():
-                    T = self.xml_type(self.attr_type(k, "edge", v))
+                    T = self.get_xml_type(self.attr_type(k, "edge", v))
                     self.get_key(str(k), T, "edge", edge_default.get(k))
 
         # Now add attribute keys to the xml file
