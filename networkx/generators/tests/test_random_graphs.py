696ad078cca3fdd4e894a9832a769d6119515b47
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-10-14 16:38:25 +0200

    Improve tests for gnp generators aliases
    * Test binomial_graph and erdos_renyi_graph as aliases of gnp_random_graph
    
    * Add tests parametrization for different random seeds

--- networkx/generators/tests/test_random_graphs.py
+++ networkx/generators/tests/test_random_graphs.py
@@ -79,6 +79,21 @@ def test_gnp_generators_edge_probability(generator, p, directed):
                 assert abs(edge_counts[v][w] / float(runs) - p) <= 0.03
 
 
+@pytest.mark.parametrize(
+    "generator", [nx.gnp_random_graph, nx.binomial_graph, nx.erdos_renyi_graph]
+)
+@pytest.mark.parametrize(
+    ("seed", "directed", "expected_num_edges"),
+    [(42, False, 1219), (42, True, 2454), (314, False, 1247), (314, True, 2476)],
+)
+def test_gnp_random_graph_aliases(generator, seed, directed, expected_num_edges):
+    """Test that aliases give the same result with the same seed."""
+    G = generator(100, 0.25, seed=seed, directed=directed)
+    assert len(G) == 100
+    assert G.number_of_edges() == expected_num_edges
+    assert G.is_directed() == directed
+
+
 class TestGeneratorsRandom:
     def test_random_graph(self):
         seed = 42
