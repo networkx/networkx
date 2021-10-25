7deded8bfbebf2401005c6fe2d4fa7f67cf9f74f
Ross Barnowski <rossbar@berkeley.edu>
2021-10-25 09:42:06 -0700

    Refactor node_classification to improve conciseness and readability (#5144)
    * Rm _init_label_matrix indirection.
    
    * Factor out _predict indirection.
    
    * Factor out _propagate one-liner.
    
    * Factor out _build_base_matrix fn.
    
    * Minor cleanups.
    
    * Fix up docstrings.
    
    * Rm internal _build_propagation_matrix internal fn.
    
    * Rm unnecessary binding of local variable.

--- networkx/algorithms/node_classification/utils.py
+++ networkx/algorithms/node_classification/utils.py
@@ -1,24 +1,3 @@
-def _propagate(P, F, B):
-    """Propagate labels by one step
-
-    Parameters
-    ----------
-    P : scipy sparse matrix, shape = [n_samples, n_samples]
-        Propagation matrix
-    F : numpy array, shape = [n_samples, n_classes]
-        Label matrix
-    B : numpy array, shape = [n_samples, n_classes]
-        Base matrix
-
-    Returns
-    ----------
-    F_new : array, shape = [n_samples, n_classes]
-        Label matrix
-    """
-    F_new = (P @ F) + B
-    return F_new
-
-
 def _get_label_info(G, label_name):
     """Get and return information of labels from the input graph
 
@@ -53,47 +32,3 @@ def _get_label_info(G, label_name):
         [label for label, _ in sorted(label_to_id.items(), key=lambda x: x[1])]
     )
     return (labels, label_dict)
-
-
-def _init_label_matrix(n_samples, n_classes):
-    """Create and return zero matrix
-
-    Parameters
-    ----------
-    n_samples : integer
-        The number of nodes (samples) on the input graph
-    n_classes : integer
-        The number of classes (distinct labels) on the input graph
-
-    Returns
-    ----------
-    F : numpy array, shape = [n_samples, n_classes]
-        Label matrix
-    """
-    import numpy as np
-
-    F = np.zeros((n_samples, n_classes))
-    return F
-
-
-def _predict(F, label_dict):
-    """Predict labels by learnt label matrix
-
-    Parameters
-    ----------
-    F : numpy array, shape = [n_samples, n_classes]
-        Learnt (resulting) label matrix
-    label_dict : numpy array, shape = [n_classes]
-        Array of labels
-        i-th element contains the label corresponding label ID `i`
-
-    Returns
-    ----------
-    predicted : numpy array, shape = [n_samples]
-        Array of predicted labels
-    """
-    import numpy as np
-
-    predicted_label_ids = np.argmax(F, axis=1)
-    predicted = label_dict[predicted_label_ids].tolist()
-    return predicted
