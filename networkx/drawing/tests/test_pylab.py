f3f2166ce0853e8095dd6ae16a67b736df79a08e
Ross Barnowski <rossbar@berkeley.edu>
2021-10-27 17:12:29 -0700

    Rm redundant import.

--- networkx/drawing/tests/test_pylab.py
+++ networkx/drawing/tests/test_pylab.py
@@ -489,8 +489,6 @@ def test_draw_networkx_arrowsize_incorrect_size():
 
 @pytest.mark.parametrize("arrowsize", (30, [10, 20, 30]))
 def test_draw_edges_arrowsize(arrowsize):
-    import itertools
-
     G = nx.DiGraph([(0, 1), (0, 2), (1, 2)])
     pos = {0: (0, 0), 1: (0, 1), 2: (1, 0)}
     edges = nx.draw_networkx_edges(G, pos=pos, arrowsize=arrowsize)
