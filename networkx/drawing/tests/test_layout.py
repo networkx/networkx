11db137b93f8f0b605cb8cefbeab21720348b9f7
Daniel Haden <haden.daniel@gmail.com>
2021-11-03 20:30:14 -0600

    Bugfix for issue 5123 (#5153)
    * fixed bug in multipartite_layout so subset labels can be non-numeric
    
    * Added non-numeric partition label test to test_multipartite_layout.
    
    * moved multipartite non-numeric partition label test outside of main test class
    
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>

--- networkx/drawing/tests/test_layout.py
+++ networkx/drawing/tests/test_layout.py
@@ -412,3 +412,15 @@ class TestLayout:
         }
         for k, v in expectation.items():
             assert (s_vpos[k] == v).all()
+
+
+def test_multipartite_layout_nonnumeric_partition_labels():
+    """See gh-5123."""
+    G = nx.Graph()
+    G.add_node(0, subset="s0")
+    G.add_node(1, subset="s0")
+    G.add_node(2, subset="s1")
+    G.add_node(3, subset="s1")
+    G.add_edges_from([(0, 2), (0, 3), (1, 2)])
+    pos = nx.multipartite_layout(G)
+    assert len(pos) == len(G)
