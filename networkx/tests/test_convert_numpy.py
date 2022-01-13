8e196ded4fca88e5d26d40bc15bf538fa79abaa6
Ross Barnowski <rossbar@berkeley.edu>
2022-01-12 22:31:56 -0800

    Add test for more multiweight reduction functions.
    Include arbitrary functions beyond the original set of
    nanmin, nanmax, and nansum.

--- networkx/tests/test_convert_numpy.py
+++ networkx/tests/test_convert_numpy.py
@@ -546,3 +546,21 @@ def test_to_numpy_array_arbitrary_weights():
     A = nx.to_numpy_array(G.to_undirected(), dtype=object)
     expected = np.array([[0, w], [w, 0]], dtype=object)
     npt.assert_array_equal(A, expected)
+
+
+@pytest.mark.parametrize(
+    "func, expected",
+    ((min, -1), (max, 10), (sum, 11), (np.mean, 11 / 3), (np.median, 2)),
+)
+def test_to_numpy_array_multiweight_reduction(func, expected):
+    """Test various functions for reducing multiedge weights."""
+    G = nx.MultiDiGraph()
+    weights = [-1, 2, 10.0]
+    for w in weights:
+        G.add_edge(0, 1, weight=w)
+    A = nx.to_numpy_array(G, multigraph_weight=func, dtype=float)
+    assert np.allclose(A, [[0, expected], [0, 0]])
+
+    # Undirected case
+    A = nx.to_numpy_array(G.to_undirected(), multigraph_weight=func, dtype=float)
+    assert np.allclose(A, [[0, expected], [expected, 0]])
