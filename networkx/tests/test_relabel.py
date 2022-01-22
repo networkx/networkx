6a2e8a46a95c64d8200eadaf90ae043f58e55d3a
Sultan Orazbayev <contact@econpoint.com>
2022-01-22 13:15:56 +0600

    Update test_relabel.py

--- networkx/tests/test_relabel.py
+++ networkx/tests/test_relabel.py
@@ -308,6 +308,14 @@ class TestRelabel:
         new_order = list(H.nodes())
         assert [mapping.get(i, i) for i in original_order] == new_order
 
+    def test_relabel_preserve_node_order_full_mapping_with_copy_false(self):
+        G = nx.path_graph(3)
+        original_order = list(G.nodes())
+        mapping = {2: "a", 1: "b", 0: "c"}  # dictionary keys out of order on purpose
+        H = nx.relabel_nodes(G, mapping, copy=False)
+        new_order = list(H.nodes())
+        assert [mapping.get(i, i) for i in original_order] == new_order
+
     def test_relabel_preserve_node_order_partial_mapping_with_copy_true(self):
         G = nx.path_graph(3)
         original_order = list(G.nodes())
