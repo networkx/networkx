a7c7c9f106cd341dbd9d0f9ef9b0fe8ce4bd9bbe
Lucas <lucashmccabe@gmail.com>
2022-02-20 19:48:04 -0500

    refactor: variable names (style)

--- networkx/algorithms/polynomials/tests/test_tutte.py
+++ networkx/algorithms/polynomials/tests/test_tutte.py
@@ -7,63 +7,63 @@ sympy = pytest.importorskip("sympy")
 class TestTutte:
     def test_tutte_polynomial_K1(self):
         """Check the Tutte polynomial for $K_1$."""
-        G = nx.complete_graph(1)
-        assert nx.tutte_polynomial(G) == 1
+        g = nx.complete_graph(1)
+        assert nx.tutte_polynomial(g) == 1
 
     def test_tutte_polynomial_K4(self):
         """Check the Tutte polynomial for $K_4$."""
-        G = nx.complete_graph(4)
+        g = nx.complete_graph(4)
         t = "x**3 + 3*x**2 + 4*x*y + 2*x + y**3 + 3*y**2 + 2*y"
-        assert str(nx.tutte_polynomial(G)) == t
+        assert str(nx.tutte_polynomial(g)) == t
 
     def test_tutte_polynomial_C5(self):
         """Check the Tutte polynomial for $C_5$."""
-        G = nx.cycle_graph(5)
+        g = nx.cycle_graph(5)
         t = "x**4 + x**3 + x**2 + x + y"
-        assert str(nx.tutte_polynomial(G)) == t
+        assert str(nx.tutte_polynomial(g)) == t
 
     def test_tutte_polynomial_diamond(self):
         """Check the Tutte polynomial for the diamond graph."""
-        G = nx.diamond_graph()
+        g = nx.diamond_graph()
         t = "x**3 + 2*x**2 + 2*x*y + x + y**2 + y"
-        assert str(nx.tutte_polynomial(G)) == t
+        assert str(nx.tutte_polynomial(g)) == t
 
     def test_tutte_polynomial_disjoint_K1(self):
         """Tutte polynomial factors into the Tutte polynomials of its components.
         Verify this property with the disjoint union of two copies of $K_1$.
         """
-        G = nx.complete_graph(1)
-        t_G = nx.tutte_polynomial(G)
-        H = nx.disjoint_union(G, G)
-        t_H = nx.tutte_polynomial(H)
-        assert sympy.simplify(t_G * t_G) == t_H
+        g = nx.complete_graph(1)
+        t_g = nx.tutte_polynomial(g)
+        h = nx.disjoint_union(g, g)
+        t_h = nx.tutte_polynomial(h)
+        assert sympy.simplify(t_g * t_g) == t_h
 
     def test_tutte_polynomial_disjoint_K4(self):
         """Tutte polynomial factors into the Tutte polynomials of its components.
         Verify this property with the disjoint union of two copies of $K_4$.
         """
-        G = nx.complete_graph(4)
-        t_G = nx.tutte_polynomial(G)
-        H = nx.disjoint_union(G, G)
-        t_H = nx.tutte_polynomial(H)
-        assert sympy.simplify(t_G * t_G) == t_H
+        g = nx.complete_graph(4)
+        t_g = nx.tutte_polynomial(g)
+        h = nx.disjoint_union(g, g)
+        t_h = nx.tutte_polynomial(h)
+        assert sympy.simplify(t_g * t_g) == t_h
 
     def test_tutte_polynomial_disjoint_C5(self):
         """Tutte polynomial factors into the Tutte polynomials of its components.
         Verify this property with the disjoint union of two copies of $C_5$.
         """
-        G = nx.cycle_graph(5)
-        t_G = nx.tutte_polynomial(G)
-        H = nx.disjoint_union(G, G)
-        t_H = nx.tutte_polynomial(H)
-        assert sympy.simplify(t_G * t_G) == t_H
+        g = nx.cycle_graph(5)
+        t_g = nx.tutte_polynomial(g)
+        h = nx.disjoint_union(g, g)
+        t_h = nx.tutte_polynomial(h)
+        assert sympy.simplify(t_g * t_g) == t_h
 
     def test_tutte_polynomial_disjoint_diamond(self):
         """Tutte polynomial factors into the Tutte polynomials of its components.
         Verify this property with the disjoint union of two diamond graphs.
         """
-        G = nx.diamond_graph()
-        t_G = nx.tutte_polynomial(G)
-        H = nx.disjoint_union(G, G)
-        t_H = nx.tutte_polynomial(H)
-        assert sympy.simplify(t_G * t_G) == t_H
+        g = nx.diamond_graph()
+        t_g = nx.tutte_polynomial(g)
+        h = nx.disjoint_union(g, g)
+        t_h = nx.tutte_polynomial(h)
+        assert sympy.simplify(t_g * t_g) == t_h
