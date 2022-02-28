e2cce3200856b04795df3b622623f0bf5d32a444
Anutosh Bhat <87052487+anutosh491@users.noreply.github.com>
2022-02-28 08:39:51 +0530

    Updated tests

--- networkx/drawing/tests/test_layout.py
+++ networkx/drawing/tests/test_layout.py
@@ -371,15 +371,14 @@ class TestLayout:
             distances_equidistant[1:], distances_equidistant[-1], atol=0.01
         )
 
-    @pytest.mark.mpl_image_compare
-    def test_paramter_equidistant_for_spiral_layout(self):
-        import matplotlib.pyplot as plt
-
-        fig = plt.figure()
-        G = nx.path_graph(5)
+    def test_spiral_layout_equidistant(self):
+        G = nx.path_graph(10)
         pos = nx.spiral_layout(G, equidistant=True)
-        nx.draw(G, pos=pos)
-        return fig
+        # Extract individual node positions as an array
+        p = np.array(list(pos.values()))
+        # Elementwise-distance between node positions
+        dist = np.linalg.norm(p[1:] - p[:-1], axis=1)
+        assert np.allclose(np.diff(dist), 0, atol=1e-3)
 
     def test_rescale_layout_dict(self):
         G = nx.empty_graph()
