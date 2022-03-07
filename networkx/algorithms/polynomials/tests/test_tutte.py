c67429ca89bb9d6b070ca1d2f98a8fedc5483863
Lucas <lucashmccabe@gmail.com>
2022-03-07 03:27:26 -0500

    feat: iterative implementation of Tutte polynomial

--- networkx/algorithms/polynomials/tests/test_tutte.py
+++ networkx/algorithms/polynomials/tests/test_tutte.py
@@ -5,6 +5,70 @@ sympy = pytest.importorskip("sympy")
 
 
 class TestTutte:
+    def test_tutte_polynomial_recursive_K1(self):
+        """Check the recursive evaluation of the Tutte polynomial for $K_1$."""
+        g = nx.complete_graph(1)
+        assert nx.tutte_polynomial_recursive(g) == 1
+
+    def test_tutte_polynomial_recursive_K4(self):
+        """Check the recursive evaluation of the Tutte polynomial for $K_4$."""
+        g = nx.complete_graph(4)
+        t = "x**3 + 3*x**2 + 4*x*y + 2*x + y**3 + 3*y**2 + 2*y"
+        assert str(nx.tutte_polynomial_recursive(g)) == t
+
+    def test_tutte_polynomial_recursive_C5(self):
+        """Check the recursive evaluation of the Tutte polynomial for $C_5$."""
+        g = nx.cycle_graph(5)
+        t = "x**4 + x**3 + x**2 + x + y"
+        assert str(nx.tutte_polynomial_recursive(g)) == t
+
+    def test_tutte_polynomial_recursive_diamond(self):
+        """Check the recursive evaluation of the Tutte polynomial for the
+        diamond graph."""
+        g = nx.diamond_graph()
+        t = "x**3 + 2*x**2 + 2*x*y + x + y**2 + y"
+        assert str(nx.tutte_polynomial_recursive(g)) == t
+
+    def test_tutte_polynomial_recursive_disjoint_K1(self):
+        """Tutte polynomial factors into the Tutte polynomials of its components.
+        Verify this property with the disjoint union of two copies of $K_1$.
+        """
+        g = nx.complete_graph(1)
+        t_g = nx.tutte_polynomial_recursive(g)
+        h = nx.disjoint_union(g, g)
+        t_h = nx.tutte_polynomial_recursive(h)
+        assert sympy.simplify(t_g * t_g).equals(t_h)
+
+    def test_tutte_polynomial_recursive_disjoint_K4(self):
+        """Tutte polynomial factors into the Tutte polynomials of its components.
+        Verify this property with the disjoint union of two copies of $K_4$.
+        """
+        g = nx.complete_graph(4)
+        t_g = nx.tutte_polynomial_recursive(g)
+        h = nx.disjoint_union(g, g)
+        t_h = nx.tutte_polynomial_recursive(h)
+        assert sympy.simplify(t_g * t_g).equals(t_h)
+
+    def test_tutte_polynomial_recursive_disjoint_C5(self):
+        """Tutte polynomial factors into the Tutte polynomials of its components.
+        Verify this property with the disjoint union of two copies of $C_5$.
+        """
+        g = nx.cycle_graph(5)
+        t_g = nx.tutte_polynomial_recursive(g)
+        h = nx.disjoint_union(g, g)
+        t_h = nx.tutte_polynomial_recursive(h)
+        assert sympy.simplify(t_g * t_g).equals(t_h)
+
+    def test_tutte_polynomial_recursive_disjoint_diamond(self):
+        """Tutte polynomial factors into the Tutte polynomials of its components.
+        Verify this property with the disjoint union of two diamond graphs.
+        """
+        g = nx.diamond_graph()
+        t_g = nx.tutte_polynomial_recursive(g)
+        h = nx.disjoint_union(g, g)
+        t_h = nx.tutte_polynomial_recursive(h)
+        assert sympy.simplify(t_g * t_g).equals(t_h)
+
     def test_tutte_polynomial_K1(self):
         """Check the Tutte polynomial for $K_1$."""
         g = nx.complete_graph(1)
@@ -36,7 +100,7 @@ class TestTutte:
         t_g = nx.tutte_polynomial(g)
         h = nx.disjoint_union(g, g)
         t_h = nx.tutte_polynomial(h)
-        assert sympy.simplify(t_g * t_g) == t_h
+        assert sympy.simplify(t_g * t_g).equals(t_h)
 
     def test_tutte_polynomial_disjoint_K4(self):
         """Tutte polynomial factors into the Tutte polynomials of its components.
@@ -46,7 +110,7 @@ class TestTutte:
         t_g = nx.tutte_polynomial(g)
         h = nx.disjoint_union(g, g)
         t_h = nx.tutte_polynomial(h)
-        assert sympy.simplify(t_g * t_g) == t_h
+        assert sympy.simplify(t_g * t_g).equals(t_h)
 
     def test_tutte_polynomial_disjoint_C5(self):
         """Tutte polynomial factors into the Tutte polynomials of its components.
@@ -56,7 +120,7 @@ class TestTutte:
         t_g = nx.tutte_polynomial(g)
         h = nx.disjoint_union(g, g)
         t_h = nx.tutte_polynomial(h)
-        assert sympy.simplify(t_g * t_g) == t_h
+        assert sympy.simplify(t_g * t_g).equals(t_h)
 
     def test_tutte_polynomial_disjoint_diamond(self):
         """Tutte polynomial factors into the Tutte polynomials of its components.
@@ -66,4 +130,4 @@ class TestTutte:
         t_g = nx.tutte_polynomial(g)
         h = nx.disjoint_union(g, g)
         t_h = nx.tutte_polynomial(h)
-        assert sympy.simplify(t_g * t_g) == t_h
+        assert sympy.simplify(t_g * t_g).equals(t_h)
--- networkx/algorithms/polynomials/tutte.py
+++ networkx/algorithms/polynomials/tutte.py
@@ -4,6 +4,9 @@ The Tutte polynomial `T_G(x, y)` is a fundamental graph polynomial invariant in
 two variables. It encodes a wide array of information related to the
 edge-connectivity of a graph; "Many problems about graphs can be reduced to
 problems of finding and evaluating the Tutte polynomial at certain values" [1]_.
+In fact, every deletion-contraction-expressible feature of a graph is a
+specialization of the Tutte polynomial [2]_.
+
 For instance, at `y=0`, the Tutte polynomial retrieves the chromatic polynomial.
 Some more specializations:
 - `T_G(1, 1)` counts the number of spanning trees of `G`
@@ -15,7 +18,7 @@ Some more specializations:
 Practically, up-front computation of the Tutte polynomial may be useful when
 users wish to repeatedly calculate edge-connectivity-related information about
 one or more graphs. A general treatment of the Tutte polynomial is provided in
-[2]_.
+[3]_.
 
 References
 ----------
@@ -23,19 +26,25 @@ References
    "The Tutte Polynomial."
    Talking About Combinatorial Objects Seminar, 2015
    https://math.berkeley.edu/~brandtm/talks/tutte.pdf
-.. [2] Wikipedia,
+.. [2] A. Björklund, T. Husfeldt, P. Kaski, M. Koivisto,
+   "Computing the Tutte polynomial in vertex-exponential time"
+   49th Annual IEEE Symposium on Foundations of Computer Science, 2008
+   https://ieeexplore.ieee.org/abstract/document/4691000
+.. [3] Wikipedia,
    "Tutte polynomial"
    https://en.wikipedia.org/wiki/Tutte_polynomial#Chromatic_polynomial
 """
+from itertools import chain, combinations
 import networkx as nx
 from networkx.utils import not_implemented_for
 
-__all__ = ["tutte_polynomial"]
+__all__ = ["tutte_polynomial", "tutte_polynomial_recursive"]
 
 
 @not_implemented_for("directed")
-def tutte_polynomial(G, simplify=True):
-    r"""Compute a graph's Tutte polynomial via deletion-contraction.
+def tutte_polynomial_recursive(G, simplify=True):
+    r"""Compute a graph's Tutte polynomial via the classical
+    deletion-contraction algorithm.
 
     The Tutte polynomial `T_G(x, y)` is a fundamental graph polynomial invariant
     in two variables, encoding a wide array of information related to the
@@ -84,11 +93,11 @@ def tutte_polynomial(G, simplify=True):
     Examples
     --------
     >>> C = nx.cycle_graph(5)
-    >>> nx.tutte_polynomial(C)
+    >>> nx.tutte_polynomial_recursive(C)
     x**4 + x**3 + x**2 + x + y
 
     >>> D = nx.diamond_graph()
-    >>> nx.tutte_polynomial(D)
+    >>> nx.tutte_polynomial_recursive(D)
     x**3 + 2*x**2 + 2*x*y + x + y**2 + y
 
     Notes
@@ -126,7 +135,7 @@ def tutte_polynomial(G, simplify=True):
     if len(components) > 1:
         t = 1
         for component in components:
-            t *= tutte_polynomial(G.subgraph(component), False)
+            t *= tutte_polynomial_recursive(G.subgraph(component), False)
         return sympy.simplify(t)
 
     cut_edges = nx.cut_edges(G)
@@ -142,7 +151,160 @@ def tutte_polynomial(G, simplify=True):
     C.remove_edge(*(e[0], e[0]))
     G.remove_edge(*e)
 
-    t = tutte_polynomial(G, False) + tutte_polynomial(C, False)
+    t = tutte_polynomial_recursive(G, False) + tutte_polynomial_recursive(C, False)
     if simplify:
         return sympy.simplify(t)
     return t
+
+
+def _partition_sum(G):
+    r"""Calculates a partition sum from the q-state Potts model via induced
+    subgraphs.
+
+    Def: For `G` an undirected graph with vertex set `V` and edge set `E`, and
+    `c(F)` the number of connected components of the graph with vertex set `V`
+    and edge set `F`, we calculate:
+
+    .. math::
+
+        Z_G(q, v) = \sum_{F \subseteq E} q^{c(F)} v^{|F|}
+
+    Parameters
+    ----------
+    G : NetworkX Graph
+
+    Notes
+    -----
+    The Potts model is introduced in [1]_. A survey of the Potts model partition
+    function and its relation to the Tutte polynomial can be found in [2]_. The
+    partition sum used here is a specialization of equation 1.1.
+
+    References
+    ----------
+    .. [1] R. B. Potts,
+       "Some generalized order-disorder transformations"
+       Mathematical Proceedings of the Cambridge Philosophical Society, 1952
+       https://doi.org/10.1017/S0305004100027419
+    .. [2] A. Sokal,
+       "The multivariate Tutte polynomial (alias Potts model) for graphs and matroids"
+       20th British Combinatorial Conference, 2005
+       https://arxiv.org/abs/math/0503607
+    """
+    import sympy
+
+    q = sympy.Symbol("q")
+    v = sympy.Symbol("v")
+    edges = list(G.edges)
+    result = 0
+    # iterate over all unique subsets of the edge set
+    for f in chain.from_iterable(combinations(edges, r) for r in range(len(edges) + 1)):
+        F = nx.Graph()
+        F.add_nodes_from(G)
+        F.add_edges_from(f)
+        k = nx.number_connected_components(F)
+        if k != 0:
+            result += (q ** k) * (v ** len(f))
+    return result
+
+
+@not_implemented_for("directed")
+def tutte_polynomial(G):
+    r"""Compute a graph's Tutte polynomial via the partition sum of the q-state
+    Potts model.
+
+    The Tutte polynomial `T_G(x, y)` is a fundamental graph polynomial invariant
+    in two variables, encoding a wide array of information related to the
+    edge-connectivity of a graph. There are several equivalent definitions; here
+    are three:
+
+    Def 1 (rank-nullity expansion): For `G` an undirected graph, `n(G)` the
+    number of vertices of `G`, `E` the edge set of `G`, and `c(A)`` the number of
+    connected components of `A` [1]_:
+
+    .. math::
+
+        T_G(x, y) = \sum_{A \in E} (x-1)^{c(A) - c(G)} (y-1)^{c(A) + |A| - n(G)}
+
+    Def 2 (spanning tree expansion): For `G` an undirected graph, `T` a spanning
+    tree of `G`, `i(T)` the internal activity of `T`, and `e(T)` the external
+    activity of `T` [2]_:
+
+    .. math::
+
+        T_G(x, y) = \sum_{T \text{ a spanning tree of } G} x^{i(T)} y^{e(T)}
+
+    Def 3 (deletion-contraction recurrence): For `G` an undirected graph, `G-e`
+    the graph obtained from `G` by deleting edge `e`, `G/e` the graph obtained
+    from `G` by contracting edge `e`, `k(G)` the number of cut-edges of `G`,
+    and `l(G)` the number of loops of `G`:
+
+    .. math::
+        T_G(x, y) = \begin{cases}
+    	   x^{k(G)} y^{l(G)}, & \text{if all edges are cut-edges or loops} \\
+           T_{G-e}(x, y) + T_{G/e}(x, y), & \text{otherwise, for $e$ not a cut-edge or loop}
+        \end{cases}
+
+    Parameters
+    ----------
+    G : NetworkX Graph
+
+    Returns
+    -------
+    instance of :class:`sympy.core.add.Add`
+        A Sympy expression representing the Tutte polynomial for `G`.
+
+    Examples
+    --------
+    >>> C = nx.cycle_graph(5)
+    >>> nx.tutte_polynomial(C)
+    x**4 + x**3 + x**2 + x + y
+
+    >>> D = nx.diamond_graph()
+    >>> nx.tutte_polynomial(D)
+    x**3 + 2*x**2 + 2*x*y + x + y**2 + y
+
+    Notes
+    -----
+    This function obtains the Tutte polynomial by a transformation of its
+    multivariate form (equations 4 and 5 in [3]_). Edge contraction is defined
+    and deletion-contraction is introduced in [4]_. Combinatorial meaning of the
+    coefficients is introduced in [5]_. Universality, properties, and
+    applications are discussed in [6]_.
+
+    See Also
+    --------
+    tutte_polynomial_recursive
+
+    References
+    ----------
+    .. [1] Y. Shi, M. Dehmer, X. Li, I. Gutman,
+       "Graph Polynomials," p. 14
+    .. [2] Y. Shi, M. Dehmer, X. Li, I. Gutman,
+       "Graph Polynomials," p. 46
+    .. [3] A. Björklund, T. Husfeldt, P. Kaski, M. Koivisto,
+       "Computing the Tutte polynomial in vertex-exponential time"
+       49th Annual IEEE Symposium on Foundations of Computer Science, 2008
+       https://ieeexplore.ieee.org/abstract/document/4691000
+    .. [4] D. B. West,
+       "Introduction to Graph Theory," p. 84
+    .. [5] G. Coutinho,
+       "A brief introduction to the Tutte polynomial"
+       Structural Analysis of Complex Networks, 2011
+       https://homepages.dcc.ufmg.br/~gabriel/seminars/coutinho_tuttepolynomial_seminar.pdf
+    .. [6] J. A. Ellis-Monaghan, C. Merino,
+       "Graph polynomials and their applications I: The Tutte polynomial"
+       Structural Analysis of Complex Networks, 2011
+       https://arxiv.org/pdf/0803.3079.pdf
+    """
+    import sympy
+
+    if len(G) == 1:
+        return 1
+    q = sympy.Symbol("q")
+    v = sympy.Symbol("v")
+    x = sympy.Symbol("x")
+    y = sympy.Symbol("y")
+    k = nx.number_connected_components(G)
+    multivariate_tutte = _partition_sum(G).subs([(q, (x - 1) * (y - 1)), (v, y - 1)])
+    tutte = ((x - 1) ** (-k)) * ((y - 1) ** (-len(G))) * multivariate_tutte
+    return sympy.simplify(tutte)
