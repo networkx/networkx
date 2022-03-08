3a8e39f05d652a910ca945a36100c60734d85051
Joshua Eason <jeason15@gmail.com>
2022-03-08 14:59:00 -0600

    fixed broken imports

--- networkx/algorithms/tests/test_randic.py
+++ networkx/algorithms/tests/test_randic.py
@@ -4,22 +4,21 @@
 
 import networkx as nx
 import unittest
-from networkx.algorithms.randic import randic_index
 
 
 def exception_digraph():
     g = nx.DiGraph()
-    return randic_index(g)
+    return nx.randic_index(g)
 
 
 def exception_multigraph():
     g = nx.MultiGraph()
-    return randic_index(g)
+    return nx.randic_index(g)
 
 
 def exception_multiDigraph():
     g = nx.MultiDiGraph()
-    return randic_index(g)
+    return nx.randic_index(g)
 
 
 class TestDegreeCentrality(unittest.TestCase):
@@ -34,7 +33,7 @@ class TestDegreeCentrality(unittest.TestCase):
 
     def test_empty(self):
         g = nx.Graph()
-        assert randic_index(g) == 0.0
+        assert nx.randic_index(g) == 0.0
 
     def test_sparse(self):
         g = nx.Graph()
@@ -44,12 +43,12 @@ class TestDegreeCentrality(unittest.TestCase):
         g.add_edge(7, 8)
         g.add_edge(9, 10)
         g.add_edge(11, 12)
-        assert randic_index(g) == 6.0
+        assert nx.randic_index(g) == 6.0
 
     def test_dense(self):
         g = nx.random_regular_graph(4, 6)
-        assert randic_index(g) == 3
+        assert nx.randic_index(g) == 3
 
     def test_k_5(self):
         g = nx.complete_graph(5)
-        assert randic_index(g) == 2.5
+        assert nx.randic_index(g) == 2.5
