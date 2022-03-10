1c02a08c285180654419e7f0f8c960394bdcf2eb
Ross Barnowski <rossbar@berkeley.edu>
2022-03-10 06:19:48 -0800

    Add Generator support to create_py_random_state. (#5380)
    * Add Generator support to create_py_random_state.
    
    * Add explicit Generator to create_py_randomstate tests.

--- networkx/utils/tests/test_misc.py
+++ networkx/utils/tests/test_misc.py
@@ -239,9 +239,13 @@ def test_create_py_random_state():
     np = pytest.importorskip("numpy")
 
     rs = np.random.RandomState
+    rng = np.random.default_rng(1000)
+    rng_explicit = np.random.Generator(np.random.SFC64())
     nprs = PythonRandomInterface
     assert isinstance(create_py_random_state(np.random), nprs)
     assert isinstance(create_py_random_state(rs(1)), nprs)
+    assert isinstance(create_py_random_state(rng), nprs)
+    assert isinstance(create_py_random_state(rng_explicit), nprs)
     # test default rng input
     assert isinstance(PythonRandomInterface(), nprs)
 
