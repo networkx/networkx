239e8e98a0fa7ddc416f00015a3ac0e14676e5c4
Lucas <lucashmccabe@gmail.com>
2022-03-20 23:15:01 -0400

    refactor: addressing review

--- networkx/algorithms/polynomials.py
+++ networkx/algorithms/polynomials.py
@@ -30,16 +30,27 @@ def tutte_polynomial(G):
     There are several equivalent definitions; here are three:
 
     Def 1 (rank-nullity expansion): For `G` an undirected graph, `n(G)` the
-    number of vertices of `G`, `E` the edge set of `G`, and `c(A)`` the number of
-    connected components of `A` [3]_:
+    number of vertices of `G`, `E` the edge set of `G`, `V` the vertex set of
+    `G`, and `c(A)` the number of connected components of the graph with vertex
+    set `V` and edge set `A` [3]_:
 
     .. math::
 
-        T_G(x, y) = \sum_{A \in E} (x-1)^{c(A) - c(G)} (y-1)^{c(A) + |A| - n(G)}
+        T_G(x, y) = \sum_{A \in E} (x-1)^{c(A) - c(E)} (y-1)^{c(A) + |A| - n(G)}
 
-    Def 2 (spanning tree expansion): For `G` an undirected graph, `T` a spanning
-    tree of `G`, `i(T)` the internal activity of `T`, and `e(T)` the external
-    activity of `T` [4]_:
+    Def 2 (spanning tree expansion): Let `G` be an undirected graph, `T` a spanning
+    tree of `G`, and `E` the edge set of `G`. Let `E` have an arbitrary strict
+    linear order `L`. Let `B_e` be the unique minimal nonempty edge cut of
+    `E \setminus T \cup {e}`. An edge `e` is internally active with respect to
+    `T` and `L` if `e` is the least edge in `B_e` according to the linear order
+    `L`. The internal activity of `T` (denoted `i(T)`) is the number of edges
+    in `E \setminus T` that are internally active with respect to `T` and `L`.
+    Let `P_e` be the unique path in `T \cup {e}` whose source and target vertex
+    are the same. An edge `e` is externally active with respect to `T` and `L`
+    if `e` is the least edge in `P_e` according to the linear order `L`. The
+    external activity of `T` (denoted `e(T)`) is the number of edges in
+    `E \setminus T` that are externally active with respect to `T` and `L`.
+    Then [4, 5]_:
 
     .. math::
 
@@ -48,11 +59,11 @@ def tutte_polynomial(G):
     Def 3 (deletion-contraction recurrence): For `G` an undirected graph, `G-e`
     the graph obtained from `G` by deleting edge `e`, `G/e` the graph obtained
     from `G` by contracting edge `e`, `k(G)` the number of cut-edges of `G`,
-    and `l(G)` the number of loops of `G`:
+    and `l(G)` the number of self-loops of `G`:
 
     .. math::
         T_G(x, y) = \begin{cases}
-    	   x^{k(G)} y^{l(G)}, & \text{if all edges are cut-edges or loops} \\
+    	   x^{k(G)} y^{l(G)}, & \text{if all edges are cut-edges or self-loops} \\
            T_{G-e}(x, y) + T_{G/e}(x, y), & \text{otherwise, for an arbitrary edge $e$ not a cut-edge or loop}
         \end{cases}
 
@@ -84,9 +95,9 @@ def tutte_polynomial(G):
     - `T_G(0, 2)` counts the number of strong orientations of `G`
     - `T_G(2, 0)` counts the number of acyclic orientations of `G`
 
-    Edge contraction is defined and deletion-contraction is introduced in [5]_.
-    Combinatorial meaning of the coefficients is introduced in [6]_.
-    Universality, properties, and applications are discussed in [7]_.
+    Edge contraction is defined and deletion-contraction is introduced in [6]_.
+    Combinatorial meaning of the coefficients is introduced in [7]_.
+    Universality, properties, and applications are discussed in [8]_.
 
     Practically, up-front computation of the Tutte polynomial may be useful when
     users wish to repeatedly calculate edge-connectivity-related information
@@ -106,13 +117,16 @@ def tutte_polynomial(G):
        "Graph Polynomials," p. 14
     .. [4] Y. Shi, M. Dehmer, X. Li, I. Gutman,
        "Graph Polynomials," p. 46
-    .. [5] D. B. West,
+    .. [5] A. Nešetril, J. Goodall,
+       "Graph invariants, homomorphisms, and the Tutte polynomial"
+       https://iuuk.mff.cuni.cz/~andrew/Tutte.pdf
+    .. [6] D. B. West,
        "Introduction to Graph Theory," p. 84
-    .. [6] G. Coutinho,
+    .. [7] G. Coutinho,
        "A brief introduction to the Tutte polynomial"
        Structural Analysis of Complex Networks, 2011
        https://homepages.dcc.ufmg.br/~gabriel/seminars/coutinho_tuttepolynomial_seminar.pdf
-    .. [7] J. A. Ellis-Monaghan, C. Merino,
+    .. [8] J. A. Ellis-Monaghan, C. Merino,
        "Graph polynomials and their applications I: The Tutte polynomial"
        Structural Analysis of Complex Networks, 2011
        https://arxiv.org/pdf/0803.3079.pdf
@@ -127,18 +141,20 @@ def tutte_polynomial(G):
     polynomial = 0
     while stack:
         G = stack.pop()
-        bridges = nx.cut_edges(G)
-        loops = list(nx.selfloop_edges(G, keys=True))
-        edges_not_cuts_loops = [
-            i for i in G.edges if i not in bridges and i not in loops
-        ]
-        if not edges_not_cuts_loops:
+        bridges = set(nx.cut_edges(G))
+
+        e = None
+        for i in G.edges:
+            if i not in bridges and i[0] != i[1]:
+                e = i
+                break
+        if not e:
+            loops = list(nx.selfloop_edges(G, keys=True))
             polynomial += x ** len(bridges) * y ** len(loops)
         else:
-            e = edges_not_cuts_loops[0]
             # deletion-contraction
             C = nx.contracted_edge(G, e, self_loops=True)
-            C.remove_edge(*(e[0], e[0]))
+            C.remove_edge(e[0], e[0])
             G.remove_edge(*e)
             stack.append(G)
             stack.append(C)
