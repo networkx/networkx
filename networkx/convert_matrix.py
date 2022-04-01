f041f5e386ce2fde0ee190401a2e2591d1cd086e
dtuncturk <dilaramemis@sabanciuniv.edu>
2022-04-01 22:03:24 +0300

    Fixing a minor typo in the example

--- networkx/convert_matrix.py
+++ networkx/convert_matrix.py
@@ -1423,11 +1423,11 @@ def to_numpy_array(
     >>> G.add_edge(2, 0, weight =0)
     >>> G.add_edge(2, 1, weight =0)
     >>> G.add_edge(3,0, weight = 1)
-    >>> nx.to_numpy_array(G, nonedge= "nan")
-    array([[nan,  2., nan,  1.],
-           [ 2., nan,  0., nan],
-           [nan,  0., nan,  0.],
-           [ 1., nan,  0., nan]])
+    >>> nx.to_numpy_array(G, nonedge= -1)
+    array([[-1.,  2., -1.,  1.],
+        [ 2., -1.,  0., -1.],
+        [-1.,  0., -1.,  0.],
+        [ 1., -1.,  0., -1.]])
 
     """
     import numpy as np
