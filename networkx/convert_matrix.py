bd6c8ad46d39fe9d769cad561f96538c7cb3b00d
dtuncturk <dilaramemis@sabanciuniv.edu>
2022-04-03 01:28:57 +0300

    Changed nonedge to -1. & update example to match PEP 8

--- networkx/convert_matrix.py
+++ networkx/convert_matrix.py
@@ -1420,14 +1420,14 @@ def to_numpy_array(
 
     >>> G = nx.Graph()
     >>> G.add_edge(3, 1, weight=2)
-    >>> G.add_edge(2, 0, weight =0)
-    >>> G.add_edge(2, 1, weight =0)
-    >>> G.add_edge(3,0, weight = 1)
-    >>> nx.to_numpy_array(G, nonedge= 0.1)
-    array([[0.1, 2. , 0.1, 1. ],
-           [2. , 0.1, 0. , 0.1],
-           [0.1, 0. , 0.1, 0. ],
-           [1. , 0.1, 0. , 0.1]])
+    >>> G.add_edge(2, 0, weight=0)
+    >>> G.add_edge(2, 1, weight=0)
+    >>> G.add_edge(3, 0, weight=1)
+    >>> nx.to_numpy_array(G, nonedge=-1.)
+    array([[-1.,  2., -1.,  1.],
+           [ 2., -1.,  0., -1.],
+           [-1.,  0., -1.,  0.],
+           [ 1., -1.,  0., -1.]])
     """
     import numpy as np
 
