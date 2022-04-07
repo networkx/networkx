7bf614c4e17f474ead61ca0162ddcf0295555f9e
unna97 <31486108+unna97@users.noreply.github.com>
2022-04-08 03:45:12 +0530

    formatting with black for style issue

--- networkx/algorithms/centrality/tests/test_decay_centrality.py
+++ networkx/algorithms/centrality/tests/test_decay_centrality.py
@@ -1,6 +1,6 @@
-'''
+"""
 Unit tests for decay centrality
-'''
+"""
 
 import pytest
 import networkx as nx
@@ -23,8 +23,7 @@ class TestDecayCentrality:
         cls.T.add_edge(5, 6)
         cls.T.add_edge(3, 4)
         cls.Gb = nx.Graph()
-        cls.Gb.add_edges_from(
-            [(0, 1), (0, 2), (1, 3), (2, 3), (2, 4), (4, 5), (3, 5)])
+        cls.Gb.add_edges_from([(0, 1), (0, 2), (1, 3), (2, 3), (2, 4), (4, 5), (3, 5)])
 
         F = nx.florentine_families_graph()
         cls.F = F
@@ -33,22 +32,19 @@ class TestDecayCentrality:
 
     def test_decay_centrality_K(self):
         d = nx.decay_centrality(self.K)
-        assert d == {0: 2.9375,
-                        1: 2.9375,
-                        2: 2.6875,
-                        3: 3.4375,
-                        4: 2.6875,
-                        5: 3.375,
-                        6: 3.375,
-                        7: 3.0,
-                        8: 2.125,
-                        9: 1.3125}
-
-
-    
+        assert d == {
+            0: 2.9375,
+            1: 2.9375,
+            2: 2.6875,
+            3: 3.4375,
+            4: 2.6875,
+            5: 3.375,
+            6: 3.375,
+            7: 3.0,
+            8: 2.125,
+            9: 1.3125,
+        }
 
     def test_decay_centrality_T(self):
         d = nx.decay_centrality(self.T)
         assert d == {0: 2.0, 1: 2.0, 2: 2.0, 3: 1.5, 4: 1.5, 5: 1.5, 6: 1.5}
-
-    
