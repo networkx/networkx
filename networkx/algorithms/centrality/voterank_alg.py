cf0a5d779284591a69053e754e439d41523c317b
dtuncturk <dilaramemis@sabanciuniv.edu>
2022-04-08 15:12:01 +0300

    Add examples & improve documentation of voterank

--- networkx/algorithms/centrality/voterank_alg.py
+++ networkx/algorithms/centrality/voterank_alg.py
@@ -7,11 +7,9 @@ def voterank(G, number_of_nodes=None):
     """Select a list of influential nodes in a graph using VoteRank algorithm
 
     VoteRank [1]_ computes a ranking of the nodes in a graph G based on a
-    voting scheme. With VoteRank, all nodes vote for each of its in-neighbours
-    and the node with the highest votes is elected iteratively. The voting
-    ability of out-neighbors of elected nodes is decreased in subsequent turns.
-
-    Note: We treat each edge independently in case of multigraphs.
+    voting scheme. With VoteRank, every node votes for each of its neighbours
+    and the node with the highest votes is elected iteratively. If a node has been elected,
+    it doesn’t participate in subsequent voting, and the voting ability of its neighbors also be decreased in subsequent turns.
 
     Parameters
     ----------
@@ -26,6 +24,33 @@ def voterank(G, number_of_nodes=None):
     voterank : list
         Ordered list of computed seeds.
         Only nodes with positive number of votes are returned.
+
+    Examples
+    -------
+
+    >>> G= nx.Graph()
+    >>> G.add_edge(0,1)
+    >>> G.add_edge(0,2)
+    >>> G.add_edge(0,3)
+    >>> G.add_edge(1,4)
+    >>> nx.voterank(G)
+    [0, 1]
+
+    The algorithm can be used both for undirected and directed graphs. However, the directed version is different in two ways: 
+    (i) nodes only vote for their in-neighbors and (ii) only the voting ability of elected node and its out-neighbors are updated. 
+    You can see an example below:
+
+    >>> G= nx.DiGraph()
+    >>> G.add_edge(0,1)
+    >>> G.add_edge(2,1)
+    >>> G.add_edge(2,3)
+    >>> G.add_edge(3,4)
+    >>> nx.voterank(G)
+    [2, 3]
+
+    Notes
+    -----
+    Each edge is treated independently in case of multigraphs.
 
     References
     ----------
