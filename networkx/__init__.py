3bf243a47eb6487cea30d6978d4f09d102ce97fb
Jarrod Millman <jarrod.millman@gmail.com>
2022-04-09 15:34:01 -0700

    Designate 2.8 release

--- networkx/__init__.py
+++ networkx/__init__.py
@@ -8,7 +8,7 @@ structure, dynamics, and functions of complex networks.
 See https://networkx.org for complete documentation.
 """
 
-__version__ = "2.8rc2.dev0"
+__version__ = "2.8"
 
 
 def __getattr__(name):
