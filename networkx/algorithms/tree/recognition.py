acead0eb921d45a16a2e6e05bf5cba10d700929d
NikitaSharma1 <nikita07192001@gmail.com>
2022-04-13 14:07:22 +0530

    add_nodes removed

--- networkx/algorithms/tree/recognition.py
+++ networkx/algorithms/tree/recognition.py
@@ -165,7 +165,6 @@ def is_forest(G):
     Examples
     --------
     >>> G= nx.Graph()
-    >>> G.add_nodes_from([1,2,3,4,5])
     >>> G.add_edges_from([(1,2),(1,3),(2,4),(2,5)])
     >>> nx.is_forest(G)
     True
@@ -222,7 +221,6 @@ def is_tree(G):
     Examples
     --------
     >>> G= nx.Graph()
-    >>> G.add_nodes_from([1,2,3,4,5])
     >>> G.add_edges_from([(1,2),(1,3),(2,4),(2,5)])
     >>> nx.is_tree(G) # n-1 edges
     True
