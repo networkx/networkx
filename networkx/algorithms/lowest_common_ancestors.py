a9809333330d6b2ef4613eb7dee4e869ffbb517e
dtuncturk <dilaramemis@sabanciuniv.edu>
2022-04-14 00:45:40 +0300

    Fix output style of examples

--- networkx/algorithms/lowest_common_ancestors.py
+++ networkx/algorithms/lowest_common_ancestors.py
@@ -41,16 +41,7 @@ def tree_all_pairs_lowest_common_ancestor(G, root=None, pairs=None):
     >>> G = nx.DiGraph()
     >>> G.add_edges_from([(1,3),(2,4),(1,2)])
     >>> dict(nx.tree_all_pairs_lowest_common_ancestor(G))
-    {(1, 1): 1,
-     (2, 1): 1,
-     (2, 2): 2,
-     (3, 1): 1,
-     (3, 2): 1,
-     (3, 3): 3,
-     (3, 4): 1,
-     (4, 1): 1,
-     (4, 2): 2,
-     (4, 4): 4}
+    {(3, 3): 3, (3, 4): 1, (4, 4): 4, (3, 2): 1, (2, 2): 2, (4, 2): 2, (1, 1): 1, (3, 1): 1, (2, 1): 1, (4, 1): 1}
 
      We can also use `pairs` argument to specify the pairs of nodes for which we
      want to compute lowest common ancestors. Here is an example:
@@ -214,7 +205,7 @@ def all_pairs_lowest_common_ancestor(G, pairs=None):
     -------
     >>> G = nx.DiGraph([(0,1),(0,3),(1,2)])
     >>> dict(nx.all_pairs_lowest_common_ancestor(G, pairs = [(1,2),(2,3)]))
-    {(1, 2): 1, (2, 3): 0}
+    {(2, 3): 0, (1, 2): 1}
 
     Notes
     -----
