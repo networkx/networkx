2136460f93a30d09979e2e9c7fbdf38c388fcbba
0ddoe_s <45603879+0ddoes@users.noreply.github.com>
2022-04-14 14:12:28 +0530

    Update networkx/algorithms/triads.py
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>

--- networkx/algorithms/triads.py
+++ networkx/algorithms/triads.py
@@ -252,8 +252,7 @@ def is_triad(G):
 
     Examples
     --------
-    >>> G = nx.DiGraph()
-    >>> G.add_edges_from([(1, 2), (2, 3), (3, 1)])
+    >>> G = nx.DiGraph([(1, 2), (2, 3), (3, 1)])
     >>> nx.is_triad(G)
     True
     >>> G.add_edges_from([(3, 4), (4, 6)])
