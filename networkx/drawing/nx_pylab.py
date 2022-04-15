9052187cae7f507b557ae73ff0781a53f3eaff4d
Ross Barnowski <rossbar@berkeley.edu>
2022-04-15 08:54:21 -0700

    Modify default arrowstyle assignment.

--- networkx/drawing/nx_pylab.py
+++ networkx/drawing/nx_pylab.py
@@ -152,7 +152,7 @@ def draw_networkx(G, pos=None, arrows=None, with_labels=True, **kwds):
 
     arrowstyle : str (default='-\|>' for directed graphs)
         For directed graphs, choose the style of the arrowsheads.
-        For undirected graphs default to '-'
+        For undirected graphs default to '<->'
 
         See `matplotlib.patches.ArrowStyle` for more options.
 
@@ -576,7 +576,7 @@ def draw_networkx_edges(
 
     arrowstyle : str (default='-\|>' for directed graphs)
         For directed graphs and `arrows==True` defaults to '-\|>',
-        For undirected graphs default to '-'.
+        For undirected graphs default to '<->'.
 
         See `matplotlib.patches.ArrowStyle` for more options.
 
@@ -681,16 +681,12 @@ def draw_networkx_edges(
     # undirected graphs (for performance reasons) and use FancyArrowPatches
     # for directed graphs.
     # The `arrows` keyword can be used to override the default behavior
-
-    if arrowstyle == None:
-        if G.is_directed():
-            arrowstyle = "-|>"
-        else:
-            arrowstyle = "-"
-
     use_linecollection = not G.is_directed()
     if arrows in (True, False):
         use_linecollection = not arrows
+
+    if arrowstyle == None:
+        arrowstyle = "-|>" if G.is_directed() else "<->"
 
     if ax is None:
         ax = plt.gca()
