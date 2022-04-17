6d983a661d6761415362b176ddfdb8dd53a1d95b
dtuncturk <dilaramemis@sabanciuniv.edu>
2022-04-17 16:20:32 +0300

    Modify related test

--- networkx/algorithms/tests/test_covering.py
+++ networkx/algorithms/tests/test_covering.py
@@ -28,7 +28,7 @@ class TestMinEdgeCover:
         )
         min_cover2 = nx.min_edge_cover(G)
         assert nx.is_edge_cover(G, min_cover)
-        assert len(min_cover) == 8
+        assert len(min_cover) == 4
 
     def test_complete_graph(self):
         G = nx.complete_graph(10)
