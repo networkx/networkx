b742b1550237ad9e4c4e40eacf7048b0a5b104c9
0ddoe_s <45603879+0ddoes@users.noreply.github.com>
2022-04-17 15:37:11 +0530

    Remove list comprehension
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>

--- networkx/algorithms/triads.py
+++ networkx/algorithms/triads.py
@@ -283,8 +283,7 @@ def all_triplets(G):
     Examples
     --------
     >>> G = nx.DiGraph([(1, 2), (2, 3), (3, 4)])
-    >>> triplets = nx.all_triplets(G)
-    >>> print([triplet for triplet in triplets])
+    >>> list(nx.all_triplets(G))
     [(1, 2, 3), (1, 2, 4), (1, 3, 4), (2, 3, 4)]
 
     """
