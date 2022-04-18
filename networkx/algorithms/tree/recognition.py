5a7985fc41bc0c686c035de43c66cf4fb5fcc94f
Nikita Sharma <70018371+NikitaSharma1@users.noreply.github.com>
2022-04-18 23:39:53 +0530

    Added examples in tournament and tree functions (#5536)
    * examples
    
    * examples
    
    * examples
    
    * Example changed
    
    * improved styling
    
    * revised
    
    * edge labels
    
    * improved styling
    
    * spacing
    
    * error testing
    
    * examples
    
    * styling
    
    * add_nodes removed
    
    * spaceing
    
    * spacing
    
    * spacing
    
    * added examples
    
    * removed random_tournament example
    
    * added examples in branching and aborescence
    
    * error removed

--- networkx/algorithms/tree/recognition.py
+++ networkx/algorithms/tree/recognition.py
@@ -96,6 +96,16 @@ def is_arborescence(G):
     b : bool
         A boolean that is True if `G` is an arborescence.
 
+    Examples
+    --------
+    >>> G = nx.DiGraph([(0, 1), (0, 2), (2, 3), (3, 4)])
+    >>> nx.is_arborescence(G)
+    True
+    >>> G.remove_edge(0, 1)
+    >>> G.add_edge(1, 2)  # maximum in-degree is 2
+    >>> nx.is_arborescence(G)
+    False
+
     Notes
     -----
     In another convention, an arborescence is known as a *tree*.
@@ -124,6 +134,16 @@ def is_branching(G):
     -------
     b : bool
         A boolean that is True if `G` is a branching.
+
+    Examples
+    --------
+    >>> G = nx.DiGraph([(0, 1), (1, 2), (2, 3), (3, 4)])
+    >>> nx.is_branching(G)
+    True
+    >>> G.remove_edge(2, 3)
+    >>> G.add_edge(3, 1)  # maximum in-degree is 2
+    >>> nx.is_branching(G)
+    False
 
     Notes
     -----
