2d8da89fd5c78b748dbf7d3e88bc0bfa094e14d6
Dan Schult <dschult@colgate.edu>
2022-04-19 10:26:48 -0400

    Update networkx/algorithms/community/tests/test_modularity_max.py
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>

--- networkx/algorithms/community/tests/test_modularity_max.py
+++ networkx/algorithms/community/tests/test_modularity_max.py
@@ -49,7 +49,7 @@ def test_greedy_modularity_communities_components():
     G = nx.Graph([(0, 1), (2, 3), (4, 5), (5, 6)])
     # usual case with 3 components
     assert greedy_modularity_communities(G) == [{4, 5, 6}, {0, 1}, {2, 3}]
-    # best_n can make the algorithm continue even when modulatiry goes down
+    # best_n can make the algorithm continue even when modularity goes down
     assert greedy_modularity_communities(G, best_n=3) == [{4, 5, 6}, {0, 1}, {2, 3}]
     assert greedy_modularity_communities(G, best_n=2) == [{0, 1, 4, 5, 6}, {2, 3}]
     assert greedy_modularity_communities(G, best_n=1) == [{0, 1, 2, 3, 4, 5, 6}]
