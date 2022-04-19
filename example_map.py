5d0360752df85489885aced123254f9ce3991932
stanyas <stanyas@users.noreply.github.com>
2022-04-19 12:11:58 +0200

    Changing errors in the example graph file

--- example_map.py
+++ example_map.py
@@ -14,7 +14,8 @@ G.add_nodes_from(
         ("6"),
         ("7"),
         ("9"),
-        ("10"),    ]
+        ("10"),    
+    ]
 )
 
 G.add_edges_from(
@@ -44,18 +45,18 @@ M = apxa.min_maximal_matching(G)
 color_map_1 = []
 for node in G:
     if node in I:
-        color_map_1.append('red')
-    else: 
-        color_map_1.append('grey')      
+        color_map_1.append("red")
+    else:
+        color_map_1.append("grey")    
 
 color_map_2 = []
 for edge in G.edges:
     if edge in M:
-        color_map_2.append('red')
-    else: 
-        color_map_2.append('grey')      
+        color_map_2.append("red")
+    else:
+        color_map_2.append("grey")     
 
-fig, axs = plt.subplots(ncols=2, figsize=(3, 2), gridspec_kw={'width_ratios': [1, 1]})
+fig, axs = plt.subplots(ncols=2, figsize=(3, 2), gridspec_kw={"width_ratios": [1, 1]})
 
 nx.draw(
     G,
@@ -64,7 +65,7 @@ nx.draw(
     font_size=25,
     edge_color="grey",
     node_color=color_map_1,
-    ax=axs[0]
+    ax=axs[0],
 )
 
 nx.draw(
@@ -74,9 +75,12 @@ nx.draw(
     font_size=25,
     edge_color=color_map_2,
     node_color='grey',
-    ax=axs[1]
+    ax=axs[1],
 )
 
-axs[0].set_title('Clique - Maximum Independent Set\nMaximum set of (red) vertices with no edge connecting them')
-axs[1].set_title('Matching\nMaximal set of pairwise non-adjacent (red) edges')
+axs[0].set_title(
+    "Clique - Maximum Independent Set\nMaximum set of (red) vertices with no edge connecting them"
+)
+axs[1].set_title("Matching\nMaximal set of pairwise non-adjacent (red) edges")
 plt.show()
+
