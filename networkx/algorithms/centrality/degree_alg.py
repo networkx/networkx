b0d4fb2133b43d6df6ba4104cd60bd3157ae6059
anne <annemachinda@gmail.com>
2022-04-19 22:23:34 +0100

    example on degree centrality

--- networkx/algorithms/centrality/degree_alg.py
+++ networkx/algorithms/centrality/degree_alg.py
@@ -7,31 +7,42 @@ __all__ = ["degree_centrality", "in_degree_centrality", "out_degree_centrality"]
 def degree_centrality(G):
     """Compute the degree centrality for nodes.
 
-    The degree centrality for a node v is the fraction of nodes it
-    is connected to.
+       The degree centrality for a node v is the fraction of nodes it
+       is connected to.
 
-    Parameters
-    ----------
-    G : graph
-      A networkx graph
+       Parameters
+       ----------
+       G : graph
+         A networkx graph
 
-    Returns
-    -------
-    nodes : dictionary
-       Dictionary of nodes with degree centrality as the value.
+       Returns
+       -------
+       nodes : dictionary
+          Dictionary of nodes with degree centrality as the value.
 
-    See Also
-    --------
-    betweenness_centrality, load_centrality, eigenvector_centrality
+       Examples
+       --------
+       >>> G = nx.Graph([(0, 1), (1, 2), (1, 5), (5, 4), (2, 4), (2, 3), (4, 3)])
+       >>> nx.degree_centrality(G)
+       {0: 0.2,
+    1: 0.6000000000000001,
+    2: 0.6000000000000001,
+    5: 0.4,
+    4: 0.6000000000000001,
+    3: 0.4}
 
-    Notes
-    -----
-    The degree centrality values are normalized by dividing by the maximum
-    possible degree in a simple graph n-1 where n is the number of nodes in G.
+       See Also
+       --------
+       betweenness_centrality, load_centrality, eigenvector_centrality
 
-    For multigraphs or graphs with self loops the maximum degree might
-    be higher than n-1 and values of degree centrality greater than 1
-    are possible.
+       Notes
+       -----
+       The degree centrality values are normalized by dividing by the maximum
+       possible degree in a simple graph n-1 where n is the number of nodes in G.
+
+       For multigraphs or graphs with self loops the maximum degree might
+       be higher than n-1 and values of degree centrality greater than 1
+       are possible.
     """
     if len(G) <= 1:
         return {n: 1 for n in G}
