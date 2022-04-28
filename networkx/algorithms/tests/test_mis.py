3ccefe6e787a8479bbc059f3e2bf7225d037dd7e
Ross Barnowski <rossbar@berkeley.edu>
2022-04-28 15:52:25 -0700

    STY: fix black complaint.

--- networkx/algorithms/tests/test_mis.py
+++ networkx/algorithms/tests/test_mis.py
@@ -34,7 +34,12 @@ def test_florentine_family():
     G = nx.florentine_families_graph()
     indep = nx.maximal_independent_set(G, ["Medici", "Bischeri"])
     assert set(indep) == {
-        "Medici", "Bischeri", "Castellani", "Pazzi", "Ginori", "Lamberteschi"
+        "Medici",
+        "Bischeri",
+        "Castellani",
+        "Pazzi",
+        "Ginori",
+        "Lamberteschi",
     }
 
 
