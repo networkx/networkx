44dd91d51da1432f47c348ee61f9427f8b4252a4
0ddoe_s <45603879+0ddoes@users.noreply.github.com>
2022-05-05 11:57:16 +0530

    updated the plot

--- examples/graph/plot_triad_types.py
+++ examples/graph/plot_triad_types.py
@@ -14,21 +14,21 @@ import matplotlib.pyplot as plt
 fig, axes = plt.subplots(4, 4, figsize=(10, 10))
 triads = {
     "003": [],
-    "012": [(1, 2)],
-    "102": [(1, 2), (2, 1)],
-    "021D": [(2, 1), (2, 3)],
-    "021U": [(1, 2), (3, 1)],
-    "021C": [(1, 2), (2, 3)],
+    "012": [(1, 3)],
+    "102": [(1, 3), (3, 1)],
+    "021D": [(3, 1), (3, 2)],
+    "021U": [(1, 3), (2, 3)],
+    "021C": [(1, 3), (3, 2)],
     "111D": [(1, 2), (2, 1), (3, 2)],
     "111U": [(1, 2), (2, 1), (2, 3)],
-    "030T": [(1, 2), (3, 2), (1, 3)],
-    "030C": [(2, 1), (3, 2), (1, 3)],
-    "201": [(1, 2), (2, 1), (2, 3), (3, 2)],
-    "120D": [(2, 1), (2, 3), (1, 3), (3, 1)],
-    "120U": [(1, 2), (3, 2), (1, 3), (3, 1)],
-    "120C": [(1, 2), (2, 3), (1, 3), (3, 1)],
-    "210": [(1, 2), (2, 3), (3, 2), (1, 3), (3, 1)],
-    "300": [(1, 2), (2, 1), (2, 3), (3, 2), (1, 3), (3, 1)],
+    "030T": [(1, 2), (3, 2), (3, 1)],
+    "030C": [(1, 2), (2, 3), (3, 1)],
+    "201": [(1, 2), (2, 1), (3, 1), (1, 3)],
+    "120D": [(1, 2), (2, 1), (3, 1), (3, 2)],
+    "120U": [(1, 2), (2, 1), (1, 3), (2, 3)],
+    "120C": [(1, 2), (2, 1), (1, 3), (3, 2)],
+    "210": [(1, 2), (2, 1), (1, 3), (3, 2), (2, 3)],
+    "300": [(1, 2), (2, 1), (2, 3), (3, 2), (1, 3), (3, 1)]
 }
 
 for (title, triad), ax in zip(triads.items(), axes.flatten()):
@@ -38,15 +38,17 @@ for (title, triad), ax in zip(triads.items(), axes.flatten()):
     nx.draw_networkx(
         G,
         ax=ax,
-        with_labels=True,
-        node_color=["blue", "red", "green"],
+        with_labels=False,
         font_color="white",
-        node_size=500,
-        font_size=15,
-        arrowsize=20,
+        node_size=300,
+        font_size=10,
+        arrowsize=30,
+        width=3,
         pos=nx.planar_layout(G),
         font_weight="bold",
     )
-    ax.set_title(title, fontsize=15)
+    ax.set_title(title, fontsize = 15, fontweight = "bold")
+    ax.set_xlim((val*1.3 for val in ax.get_xlim()))
+    ax.set_ylim((val*1.3 for val in ax.get_ylim()))   
 fig.tight_layout()
 plt.show()
