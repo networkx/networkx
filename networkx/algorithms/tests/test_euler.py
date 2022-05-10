921ebfd321fb03e3f43c7ad2d86a4b2a54b4e349
dtuncturk <dilaramemis@sabanciuniv.edu>
2022-05-10 22:59:45 +0300

    Restore deleted tests for euler path

--- networkx/algorithms/tests/test_euler.py
+++ networkx/algorithms/tests/test_euler.py
@@ -140,6 +140,15 @@ class TestHasEulerianPath:
         G.add_edges_from([(0, 1), (1, 2), (0, 2)])
         assert not nx.has_eulerian_path(G)
 
+        # Test directed graphs without isolated node returns False
+        G = nx.DiGraph()
+        G.add_edges_from([(0, 1), (1, 2), (2, 0)])
+        assert not nx.has_eulerian_path(G)
+
+        # Test directed graphs with isolated node returns False
+        G.add_node(3)
+        assert not nx.has_eulerian_path(G)
+
     @pytest.mark.parametrize("G", (nx.Graph(), nx.DiGraph()))
     def test_has_eulerian_path_not_weakly_connected(self, G):
         G.add_edges_from([(0, 1), (2, 3), (3, 2)])
