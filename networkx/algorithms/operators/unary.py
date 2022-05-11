684e4c55a08e75fe3083aa6fe4dab3bde6599607
anne <annemachinda@gmail.com>
2022-05-11 10:22:52 +0100

    added example on complement

--- networkx/algorithms/operators/unary.py
+++ networkx/algorithms/operators/unary.py
@@ -22,6 +22,14 @@ def complement(G):
     does not produce parallel edges for MultiGraphs.
 
     Graph, node, and edge data are not propagated to the new graph.
+
+    Examples
+    --------
+    >>> G = nx.Graph([(1, 2), (1, 3), (2, 3), (3, 4), (3, 5)])
+    >>> G_complement = nx.complement(G)
+    >>> G_complement.edges() # This shows the edges of the complemented graph
+        EdgeView([(1, 4), (1, 5), (2, 4), (2, 5), (4, 5)])
+
     """
     R = G.__class__()
     R.add_nodes_from(G)
