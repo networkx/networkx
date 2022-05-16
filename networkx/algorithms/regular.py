7e9413555aa7e05ac25df29abd9a11335fd63c8b
anne <annemachinda@gmail.com>
2022-05-16 16:28:18 +0100

    added example on is_k_regular

--- networkx/algorithms/regular.py
+++ networkx/algorithms/regular.py
@@ -55,6 +55,12 @@ def is_k_regular(G, k):
     bool
         Whether the given graph is k-regular.
 
+    Examples
+    --------
+    >>> G = nx.Graph([(1, 2), (2, 3), (3, 4), (4, 1)])
+    >>> nx.is_k_regular(G, k=3)
+    False
+
     """
     return all(d == k for n, d in G.degree)
 
