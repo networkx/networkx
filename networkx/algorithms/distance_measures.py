377a41309a148dc591152cade8dd4bf725b11866
Mridul Seth <seth.mridul@gmail.com>
2022-05-18 17:31:08 +0400

    2022ify hamming distance PR

--- networkx/algorithms/distance_measures.py
+++ networkx/algorithms/distance_measures.py
@@ -11,8 +11,11 @@ __all__ = [
     "periphery",
     "center",
     "barycenter",
-    "resistance_distance",'hamming_distance', 'generalized_hamming_distance', '2d_generalized_hamming_distance',
-        'diversity'
+    "resistance_distance",
+    "hamming_distance",
+    "generalized_hamming_distance",
+    "2d_generalized_hamming_distance",
+    "diversity",
 ]
 
 
@@ -658,26 +661,31 @@ def resistance_distance(G, nodeA, nodeB, weight=None, invert_weight=True):
 
     return rd
 
+
 def hamming_distance(G, H):
-    """Return the Hamming distance between two (possibly directed) graphs.
+    """Return the Hamming distance between two graphs.
+
     The Hamming distance is the number of edges contained in one
     but not the other graph.
+
     Parameters
     ----------
-    G, H : NetworkX graph/digraph
-       The graphs to be compared
+    G : NetworkX graph
+        First Graph
+    H : NetworkX graph
+        Second Graph
     Returns
     -------
     count : integer
       The hamming distance.
     """
     count = 0
-    for e in G.edges_iter():
+    for e in G.edges():
         if not H.has_edge(*e):
-            count+=1
-    for e in H.edges_iter():
+            count += 1
+    for e in H.edges():
         if not G.has_edge(*e):
-            count+=1
+            count += 1
     return count
 
 
@@ -696,23 +704,30 @@ def generalized_hamming_distance(G, H, no_edge_cost=0.5, default=0):
       The generalized hamming distance.
     """
 
-    count=0
-    for e in G.edges_iter():
+    count = 0
+    for e in G.edges():
         try:
             if H.has_edge(*e):
-                count+= abs(nx.get_edge_attributes(G, 'weight')[e]-nx.get_edge_attributes(H, 'weight')[e])
+                count += abs(
+                    nx.get_edge_attributes(G, "weight")[e]
+                    - nx.get_edge_attributes(H, "weight")[e]
+                )
 
             else:
-                count+= (abs(nx.get_edge_attributes(G, 'weight')[e]) + no_edge_cost)
-        except: print '%s does not have a weight!' % str(e)
+                count += abs(nx.get_edge_attributes(G, "weight")[e]) + no_edge_cost
+        except:
+            raise ValueError("%s does not have a weight!" % str(e))
 
-    #And now for the edges that are in H but not in G:
-    for e in H.edges_iter():
+    # And now for the edges that are in H but not in G:
+    for e in H.edges():
         if not G.has_edge(*e):
-            try: count+= (abs(nx.get_edge_attributes(H, 'weight')[e]) + no_edge_cost)
-            except: print '%s does not have a weight!' % str(e)
+            try:
+                count += abs(nx.get_edge_attributes(H, "weight")[e]) + no_edge_cost
+            except:
+                raise ValueError("%s does not have a weight!" % str(e))
 
     return count
+
 
 def two_d_generalized_hamming_distance(G, H, no_edge_params=(0, 2)):
     """Return the Two-Dimensional Generalized Hamming distance between two (possibly directed) graphs, where each link has information on two dimensions (interpreted, for example, as mean and variance).
@@ -730,27 +745,52 @@ def two_d_generalized_hamming_distance(G, H, no_edge_params=(0, 2)):
     """
 
     from numpy import sqrt
-    count=0
-    for e in G.edges_iter():
+
+    count = 0
+    for e in G.edges():
         if H.has_edge(*e):
-            try: count+= sqrt((nx.get_edge_attributes(G, 'mu')[e]-nx.get_edge_attributes(H, 'mu')[e])**2 + (nx.get_edge_attributes(G, 'sigma')[e]-nx.get_edge_attributes(H, 'sigma')[e])**2)
+            try:
+                count += sqrt(
+                    (
+                        nx.get_edge_attributes(G, "mu")[e]
+                        - nx.get_edge_attributes(H, "mu")[e]
+                    )
+                    ** 2
+                    + (
+                        nx.get_edge_attributes(G, "sigma")[e]
+                        - nx.get_edge_attributes(H, "sigma")[e]
+                    )
+                    ** 2
+                )
 
-
-            except: print '%s does not have a mu or a sigma!' % str(e)
+            except:
+                raise ValueError("%s does not have a mu or a sigma!" % str(e))
         else:
-            try: count+= sqrt((nx.get_edge_attributes(G, 'mu')[e]-no_edge_params[0])**2 + (nx.get_edge_attributes(G, 'sigma')[e]-no_edge_params[1])**2)
-            except: print '%s does not have a mu or a sigma!' % str(e)
+            try:
+                count += sqrt(
+                    (nx.get_edge_attributes(G, "mu")[e] - no_edge_params[0]) ** 2
+                    + (nx.get_edge_attributes(G, "sigma")[e] - no_edge_params[1]) ** 2
+                )
+            except:
+                raise ValueError("%s does not have a mu or a sigma!" % str(e))
 
-    #And now for the edges that are in H but not in G:
-    for e in H.edges_iter():
+    # And now for the edges that are in H but not in G:
+    for e in H.edges():
         if not G.has_edge(*e):
-            try: count+= sqrt((nx.get_edge_attributes(H, 'mu')[e]-no_edge_params[0])**2 + (nx.get_edge_attributes(H, 'sigma')[e]-no_edge_params[1])**2)
+            try:
+                count += sqrt(
+                    (nx.get_edge_attributes(H, "mu")[e] - no_edge_params[0]) ** 2
+                    + (nx.get_edge_attributes(H, "sigma")[e] - no_edge_params[1]) ** 2
+                )
 
-            except: print '%s does not have a mu or a sigma!' % str(e)
+            except:
+                raise ValueError("%s does not have a mu or a sigma!" % str(e))
 
     return count
 
+
 # problem with edges not having mu and sigma. Have to attach edges differently!
+
 
 def diversity(obj_set, distance=two_d_generalized_hamming_distance):
     """Return the Weitzman diversity measure (Weitzman 1992) of a set of objects with a distance function defined over any
@@ -768,22 +808,26 @@ def diversity(obj_set, distance=two_d_generalized_hamming_distance):
       The diversity of a collection of objects.
     """
 
-    '''
+    """
     This function calculates the Weitzman diversity measure (Weitzman 1992) of a set of objects with a distance function defined over any
     two objects in the set.
-    '''
-    S=set()
-    divers=0
-    g=obj_set.pop() #Step1: randomly pick an object from the object set
+    """
+    S = set()
+    divers = 0
+    g = obj_set.pop()  # Step1: randomly pick an object from the object set
     S.add(g)
     while obj_set:
-        set_distance=min([distance(g, h) for g in S for h in obj_set])
-        min_elem=[elem for elem in obj_set if min([distance(elem, g) for g in S])==set_distance].pop()
+        set_distance = min(distance(g, h) for g in S for h in obj_set)
+        min_elem = [
+            elem
+            for elem in obj_set
+            if min(distance(elem, g) for g in S) == set_distance
+        ].pop()
 
-        S.add(min_elem) #Step2: add closest member of the object set to the set, S
-        obj_set.remove(min_elem) #and remove it from the object set
-        divers+=set_distance #Step3: increment the diversity by the distance between the set S, and the new member.
+        S.add(min_elem)  # Step2: add closest member of the object set to the set, S
+        obj_set.remove(min_elem)  # and remove it from the object set
+        divers += set_distance  # Step3: increment the diversity by the distance between the set S, and the new member.
 
-    #Normalize the diversity by the number of objects:
-        #make a distance matrix where the distances don't have to be recalculated (memoization)
-    return float(divers)/len(S)
+    # Normalize the diversity by the number of objects:
+    # make a distance matrix where the distances don't have to be recalculated (memoization)
+    return float(divers) / len(S)
