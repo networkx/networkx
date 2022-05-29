995453d0f2caf95657d8acdff34519c74c708062
Erik Welch <erik.n.welch@gmail.com>
2022-05-29 00:05:09 -0500

    And more tests for generalized_degree

--- networkx/algorithms/tests/test_cluster.py
+++ networkx/algorithms/tests/test_cluster.py
@@ -541,3 +541,11 @@ class TestGeneralizedDegree:
         assert nx.generalized_degree(G, 0) == {3: 4}
         G.remove_edge(0, 1)
         assert nx.generalized_degree(G, 0) == {2: 3}
+        assert nx.generalized_degree(G, [1, 2]) == {1: {2: 3}, 2: {2: 2, 3: 2}}
+        assert nx.generalized_degree(G) == {
+            0: {2: 3},
+            1: {2: 3},
+            2: {2: 2, 3: 2},
+            3: {2: 2, 3: 2},
+            4: {2: 2, 3: 2},
+        }
