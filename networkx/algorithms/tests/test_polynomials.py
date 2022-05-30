e5452df82dcebb3374e5cbab8576c06a47a65db6
Lucas <lucashmccabe@gmail.com>
2022-05-30 14:27:56 -0400

    refactor: formatting in chromatic polynomial tests

--- networkx/algorithms/tests/test_polynomials.py
+++ networkx/algorithms/tests/test_polynomials.py
@@ -19,13 +19,14 @@ _test_chromatic_graphs = {
     nx.complete_graph(4): "x*(x**3 - 6*x**2 + 11*x - 6)",
     nx.cycle_graph(5): "((-1)**5)*(x-1)+(x-1)**5",
     nx.diamond_graph(): "x*(x**3 - 5*x**2 + 8*x - 4)",
-    nx.path_graph(5): "x*(x-1)**4"
+    nx.path_graph(5): "x*(x-1)**4",
 }
 
 
 @pytest.mark.parametrize(("G", "expected"), _test_tutte_graphs.items())
 def test_tutte_polynomial(G, expected):
     assert nx.tutte_polynomial(G).equals(expected)
+
 
 @pytest.mark.parametrize("G", _test_tutte_graphs.keys())
 def test_tutte_polynomial_disjoint(G):
@@ -38,10 +39,10 @@ def test_tutte_polynomial_disjoint(G):
     assert sympy.simplify(t_g * t_g).equals(t_h)
 
 
-
 @pytest.mark.parametrize(("G", "expected"), _test_chromatic_graphs.items())
 def test_chromatic_polynomial(G, expected):
     assert nx.chromatic_polynomial(G).equals(expected)
+
 
 @pytest.mark.parametrize("G", _test_chromatic_graphs.keys())
 def test_chromatic_polynomial_disjoint(G):
