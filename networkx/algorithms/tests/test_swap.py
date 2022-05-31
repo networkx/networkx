5f5ec0ebf9297c8eb93db186928943752f9b7652
Ben Heil <ben.jer.heil@gmail.com>
2022-05-31 09:09:39 -0400

    Update tests to use keyword arguments for directed_edge_swap

--- networkx/algorithms/tests/test_swap.py
+++ networkx/algorithms/tests/test_swap.py
@@ -9,7 +9,7 @@ def test_directed_edge_swap():
     graph = nx.gnp_random_graph(200, 0.5, directed=True)
     in_degrees = sorted((n, d) for n, d in graph.in_degree())
     out_degrees = sorted((n, d) for n, d in graph.out_degree())
-    G = nx.directed_edge_swap(graph, 40, max_tries=500)
+    G = nx.directed_edge_swap(graph, nswap=40, max_tries=500)
     assert in_degrees == sorted((n, d) for n, d in G.in_degree())
     assert out_degrees == sorted((n, d) for n, d in G.out_degree())
 
@@ -18,7 +18,7 @@ def test_directed_edge_swap_seed():
     graph = nx.gnp_random_graph(200, 0.5, directed=True)
     in_degrees = sorted((n, d) for n, d in graph.in_degree())
     out_degrees = sorted((n, d) for n, d in graph.out_degree())
-    G = nx.directed_edge_swap(graph, 40, max_tries=500, seed=1)
+    G = nx.directed_edge_swap(graph, nswap=40, max_tries=500, seed=1)
     assert in_degrees == sorted((n, d) for n, d in G.in_degree())
     assert out_degrees == sorted((n, d) for n, d in G.out_degree())
 
