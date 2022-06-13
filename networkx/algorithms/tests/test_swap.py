697bb40dc57942e4828a63a632d0d3e92baafae9
Ben Heil <ben.jer.heil@gmail.com>
2022-06-13 09:38:49 -0400

    Use seed when testing directed edge swap

--- networkx/algorithms/tests/test_swap.py
+++ networkx/algorithms/tests/test_swap.py
@@ -1,20 +1,8 @@
 import pytest
 import networkx as nx
 
-# import random
-# random.seed(0)
-
 
 def test_directed_edge_swap():
-    graph = nx.path_graph(200, create_using=nx.DiGraph)
-    in_degrees = sorted((n, d) for n, d in graph.in_degree())
-    out_degrees = sorted((n, d) for n, d in graph.out_degree())
-    G = nx.directed_edge_swap(graph, nswap=40, max_tries=500)
-    assert in_degrees == sorted((n, d) for n, d in G.in_degree())
-    assert out_degrees == sorted((n, d) for n, d in G.out_degree())
-
-
-def test_directed_edge_swap_seed():
     graph = nx.path_graph(200, create_using=nx.DiGraph)
     in_degrees = sorted((n, d) for n, d in graph.in_degree())
     out_degrees = sorted((n, d) for n, d in graph.out_degree())
