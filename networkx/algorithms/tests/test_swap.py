3a2d1b3d3e6ffc0b27b2ae15bd4bd73fd3d28d3e
Ben Heil <ben.jer.heil@gmail.com>
2022-06-13 09:29:00 -0400

    Update networkx/algorithms/tests/test_swap.py
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- networkx/algorithms/tests/test_swap.py
+++ networkx/algorithms/tests/test_swap.py
@@ -73,7 +73,7 @@ def test_connected_double_edge_swap_star_low_window_threshold():
 
 def test_directed_edge_swap_small():
     with pytest.raises(nx.NetworkXError):
-        G = nx.directed_edge_swap(nx.gnp_random_graph(3, 0.5, directed=True))
+        G = nx.directed_edge_swap(nx.path_graph(3, create_using=nx.DiGraph)
 
 
 def test_directed_edge_swap_tries():
