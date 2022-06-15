5d9054d81d3647c21d71af76fd992badcf3b5773
Mridul Seth <seth.mridul@gmail.com>
2022-06-15 20:57:30 +0400

    Remove deprecated context managers

--- networkx/utils/__init__.py
+++ networkx/utils/__init__.py
@@ -4,4 +4,3 @@ from networkx.utils.random_sequence import *
 from networkx.utils.union_find import *
 from networkx.utils.rcm import *
 from networkx.utils.heaps import *
-from networkx.utils.contextmanagers import *
