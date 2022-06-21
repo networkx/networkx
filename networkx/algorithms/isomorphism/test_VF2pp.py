f133b0d28d9ef369309279e43a6a1e617f1ad82b
kpetridis <petridkon@gmail.com>
2022-06-21 11:40:05 +0300

    Code to verify the ordering

--- networkx/algorithms/isomorphism/test_VF2pp.py
+++ networkx/algorithms/isomorphism/test_VF2pp.py
@@ -1,13 +1,14 @@
 import random
 import time
+
 import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.drawing.nx_agraph import graphviz_layout
 
 
 def main():
-    G = nx.gnp_random_graph(20, 0.25, seed=23)
-    colors = ["blue", "red", "green", "orange", "grey"]
+    G = nx.gnp_random_graph(100, 0.15, seed=19)
+    colors = ["blue", "red", "green", "orange", "grey", "yellow", "purple", "black", "white"]
 
     for i in range(len(G.nodes)):
         G.nodes[i]["label"] = colors[random.randrange(len(colors))]
@@ -21,7 +22,26 @@ def main():
     t0 = time.time()
     M = matching_order(G, G, L)
     print('Elapsed time: ', time.time() - t0)
-    print('Order:', M)
+
+    t0 = time.time()
+    M2 = matching_order2(G, G, L)
+    print('Elapsed time: ', time.time() - t0)
+
+    print("len= ", len(M))
+    print(M)
+    print("len= ", len(M2))
+    print(M2)
+
+    if len(M) != len(M2):
+        print("ERROR")
+        exit(0)
+
+    for m1, m2 in zip(M, M2):
+        if m1 != m2:
+            print("ERROR")
+            exit(0)
+
+    print("CORRECT")
 
 
 def connectivity(G, u, H):
@@ -29,7 +49,11 @@ def connectivity(G, u, H):
 
 
 def node_labels(G):
-    return {n: G.nodes[n]["label"] for n in G.nodes()}
+    return nx.get_node_attributes(G, "label")
+
+
+def label_nodes(labels):
+    return nx.utils.groups(labels)
 
 
 def F(G2, M, L, label):
@@ -54,6 +78,53 @@ def argmax_connectivity(G, Vd, M):
     return [v for v in Vd if connectivity(G, v, M) == max_conn]
 
 
+def process_level2(G1, G2, Vd, M, L):
+    while len(Vd) > 0:
+        # nodes_with_most_ordered_neighbors = [max(G1.nodes, key=lambda n: connectivity(G1, n, Vd))]
+        nodes_with_most_ordered_neighbors = argmax_connectivity(G1, Vd, M)
+        # print("Vd= ", Vd)/
+        print("nds ", nodes_with_most_ordered_neighbors)
+
+        # print(nodes_with_most_ordered_neighbors)
+        max_degree_nodes = [max(nodes_with_most_ordered_neighbors, key=G1.degree)]
+        print("max= ", max_degree_nodes)
+        labels = {node: G1.nodes[node]["label"] for node in max_degree_nodes}
+        print("rarity ", label_nodes(labels).values())
+        rarest_node = min(label_nodes(labels).values(), key=len)
+        # m = argmin_F(G2, M, L, argmax_degree(G1, argmax_connectivity(G1, Vd, M)))
+        print("rare= ", rarest_node)
+        for node in rarest_node:
+            Vd.remove(node)
+            M.append(node)
+    return M
+
+
+def matching_order(G1, G2, L):
+    V1_unordered = set(G1)
+    current_labels = {node: node_labels(G1)[node] for node in V1_unordered}
+    M = []
+    while V1_unordered:
+        rare_nodes = min(label_nodes(current_labels).values(), key=len)
+        best_nodes = max(rare_nodes, key=G1.degree)
+        T, M, V1_unordered = dlevel_bfs_tree(G1, G2, M, L, V1_unordered, best_nodes)
+        del current_labels[best_nodes]
+    return M
+
+
+def dlevel_bfs_tree(G1, G2, M, L, V1_unordered, source):
+    # todo: optimize descendants_at_distance. Compute all levels once. distance(2) re-computes distance(1).
+    d = 0
+    T = []
+    d_level_successors = nx.descendants_at_distance(G1, source=source, distance=d)
+    while len(d_level_successors) > 0:
+        T.append(d_level_successors)
+        M = process_level(G1, G2, d_level_successors, M, L)
+        V1_unordered -= set(M)
+        d += 1
+        d_level_successors = nx.descendants_at_distance(G1, source=source, distance=d)
+    return T, M, V1_unordered
+
+
 def process_level(G1, G2, Vd, M, L):
     while len(Vd) > 0:
         m = argmin_F(G2, M, L, argmax_degree(G1, argmax_connectivity(G1, Vd, M)))
@@ -63,29 +134,33 @@ def process_level(G1, G2, Vd, M, L):
     return M
 
 
-def dlevel_bfs_tree(G, source):
+"""
+Second version
+"""
+def matching_order2(G1, G2, L):
+    V1_unordered = set(G1)
+    current_labels = {node: node_labels(G1)[node] for node in V1_unordered}
+    M = []
+    while V1_unordered:
+        rare_nodes = min(label_nodes(current_labels).values(), key=len)
+        best_nodes = max(rare_nodes, key=G1.degree)
+        T, M, V1_unordered = dlevel_bfs_tree2(G1, G2, M, L, V1_unordered, best_nodes)
+        del current_labels[best_nodes]
+    return M
+
+
+def dlevel_bfs_tree2(G1, G2, M, L, V1_unordered, source):
     # todo: optimize descendants_at_distance. Compute all levels once. distance(2) re-computes distance(1).
     d = 0
     T = []
-    d_level_successors = nx.descendants_at_distance(G, source=source, distance=d)
+    d_level_successors = nx.descendants_at_distance(G1, source=source, distance=d)
     while len(d_level_successors) > 0:
         T.append(d_level_successors)
+        M = process_level2(G1, G2, d_level_successors, M, L)
+        V1_unordered -= set(M)
         d += 1
-        d_level_successors = nx.descendants_at_distance(G, source=source, distance=d)
-    return T
-
-
-def matching_order(G1, G2, L):
-    V1 = G1.nodes
-    M = []
-    while len(set(V1) - set(M)) > 0:
-        S = set(V1) - set(M)
-        r = argmax_degree(G1, argmin_F(G2, M, L, S))
-        for node in r:
-            T = dlevel_bfs_tree(G1, source=node)
-            for Vd in T:
-                M = process_level(G1, G2, Vd, M, L)
-    return M
+        d_level_successors = nx.descendants_at_distance(G1, source=source, distance=d)
+    return T, M, V1_unordered
 
 
 def binary_tree():
