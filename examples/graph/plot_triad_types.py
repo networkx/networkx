6767ba16445edc00b3051614ee983260ce4da90d
Dan Schult <dschult@colgate.edu>
2022-06-22 13:11:15 -0400

    put back fig.tight_layout(), rebase on main, reword, reorder edges

--- examples/graph/plot_triad_types.py
+++ examples/graph/plot_triad_types.py
@@ -2,12 +2,13 @@
 ======
 Triads
 ======
-According to the paper by Snijders, T. (2012). “Transitivity and triads.” 
-University of Oxford, there are 16 Triad types possible. This plot creates 
-a visualization of the 16 Triad Types that can be identified within directed networks.
-Triadic relationships are especially useful when analysing Social Networks. The first 
-three digits refers to numbers of mutual, asymmetric and null dyads and the letter gives 
-the Orientation as Up (U), Down (D) , Cycclical (C) and Transitive (T).
+According to the paper by Snijders, T. (2012). “Transitivity and triads.”
+University of Oxford, there are 16 Triad Types possible. This plot shows
+the 16 Triad Types that can be identified within directed networks.
+Triadic relationships are especially useful when analysing Social Networks.
+The first three digits refer to the number of mutual, asymmetric and null
+dyads (bidirectional, unidirection and nonedges) and the letter gives
+the Orientation as Up (U), Down (D) , Cyclical (C) or Transitive (T).
 """
 
 import networkx as nx
@@ -16,15 +17,15 @@ import matplotlib.pyplot as plt
 fig, axes = plt.subplots(4, 4, figsize=(10, 10))
 triads = {
     "003": [],
-    "012": [(1, 3)],
-    "102": [(1, 3), (3, 1)],
+    "012": [(1, 2)],
+    "102": [(1, 2), (2, 1)],
     "021D": [(3, 1), (3, 2)],
     "021U": [(1, 3), (2, 3)],
     "021C": [(1, 3), (3, 2)],
-    "111D": [(1, 2), (2, 1), (3, 2)],
-    "111U": [(1, 2), (2, 1), (2, 3)],
-    "030T": [(1, 2), (3, 2), (3, 1)],
-    "030C": [(1, 2), (2, 3), (3, 1)],
+    "111D": [(1, 2), (2, 1), (3, 1)],
+    "111U": [(1, 2), (2, 1), (1, 3)],
+    "030T": [(1, 2), (3, 2), (1, 3)],
+    "030C": [(1, 3), (3, 2), (2, 1)],
     "201": [(1, 2), (2, 1), (3, 1), (1, 3)],
     "120D": [(1, 2), (2, 1), (3, 1), (3, 2)],
     "120U": [(1, 2), (2, 1), (1, 3), (2, 3)],
@@ -41,26 +42,22 @@ for (title, triad), ax in zip(triads.items(), axes.flatten()):
         G,
         ax=ax,
         with_labels=False,
-        font_color="white",
-        node_size=300,
-        font_size=10,
-        arrowsize=30,
-        width=3,
+        node_color=["green"],
+        node_size=200,
+        arrowsize=20,
+        width=2,
         pos=nx.planar_layout(G),
-        font_weight="bold",
     )
-    # ax.set_title(title, fontsize=15, fontweight="bold")
-    ax.set_xlim(val * 1.3 for val in ax.get_xlim())
-    ax.set_ylim(val * 1.3 for val in ax.get_ylim())
+    ax.set_xlim(val * 1.2 for val in ax.get_xlim())
+    ax.set_ylim(val * 1.2 for val in ax.get_ylim())
     ax.text(
-        -1.2,
-        0.75,
+        0,
+        0,
         title,
-        color="black",
-        fontweight="bold",
-        fontsize=12,
-        bbox=dict(facecolor="none", edgecolor="blue", pad=3),
+        fontsize=15,
+        fontweight="extra bold",
+        horizontalalignment="center",
+        bbox=dict(boxstyle="square,pad=0.3", fc="none"),
     )
-
-fig.subplots_adjust(left=3, bottom=3, right=4, top=4, wspace=0.5, hspace=0.5)
+fig.tight_layout()
 plt.show()
