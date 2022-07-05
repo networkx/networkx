9dbb52faef011b5bffed84bb9abe5868436b7b59
kpetridis <petridkon@gmail.com>
2022-07-05 21:24:21 +0300

    Create a separate file for the VF2++ ISO algorithm

--- networkx/algorithms/isomorphism/VF2pp.py
+++ networkx/algorithms/isomorphism/VF2pp.py
@@ -1,6 +1,5 @@
 import random
 import time
-
 import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.drawing.nx_agraph import graphviz_layout
@@ -69,45 +68,3 @@ def isomorphic_VF2pp(G1, G2, G1_labels, G2_labels, node_order):
     if len(mapping) == G1.number_of_nodes():
         return mapping
     return False
-
-
-def main():
-    G1 = nx.gnp_random_graph(200, 0.8, 42)
-    G2 = nx.gnp_random_graph(200, 0.8, 42)
-
-    # nx.draw(G1, with_labels=True)
-    # plt.show()
-
-    # G1_edges = [(1, 2), (1, 4), (1, 5), (2, 3), (2, 4), (3, 4), (4, 5), (1, 6), (6, 7), (6, 8), (8, 9), (7, 9)]
-    # G2_edges = [(1, 2), (2, 3), (3, 4), (1, 4), (4, 9), (9, 8), (8, 7), (7, 6), (8, 6), (9, 6), (5, 6), (5, 9)]
-
-    # G1.add_edges_from(G1_edges)
-    # G2.add_edges_from(G2_edges)
-    # G1.add_node(0)  # todo: problem when a node is not connected to the graph. Figure out the problem.
-    # G2.add_node(0)
-
-    # mapped_nodes = {0: 0, 1: 9, 2: 8, 3: 7, 4: 6, 5: 5, 6: 4, 7: 1, 8: 3, 9: 2}
-
-    # colors = ["white", "black", "green", "purple", "orange", "red", "blue", "pink", "yellow", "none"]
-    # for node, color in zip(G1.nodes, colors):
-    #     G1.nodes[node]["label"] = color
-    #     G2.nodes[mapped_nodes[node]]["label"] = color
-
-    # Initialization of VF2++
-    for n in G1.nodes():
-        G1.nodes[n]["label"] = "blue"
-        G2.nodes[n]["label"] = "blue"
-
-    G1_labels = nx.get_node_attributes(G1, "label")
-    G2_labels = nx.get_node_attributes(G2, "label")
-
-    # Node ordering
-    node_order = [n for n in G1.nodes()]  # todo: dummy ordering until the issue with "match_ordering" is fixed
-    # print(f"order: {node_order}")
-
-    # VF2++
-    mapping = isomorphic_VF2pp(G1, G2, G1_labels, G2_labels, node_order)
-    print(mapping)
-
-
-main()
---
+++ networkx/algorithms/isomorphism/demo.py
@@ -0,0 +1,40 @@
+import networkx as nx
+from networkx.algorithms.isomorphism.VF2pp import isomorphic_VF2pp
+
+
+# Graph initialization
+G1 = nx.gnp_random_graph(15, 0.6, 42)
+G2 = nx.gnp_random_graph(15, 0.6, 42)
+
+# nx.draw(G1, with_labels=True)
+# plt.show()
+
+# G1_edges = [(1, 2), (1, 4), (1, 5), (2, 3), (2, 4), (3, 4), (4, 5), (1, 6), (6, 7), (6, 8), (8, 9), (7, 9)]
+# G2_edges = [(1, 2), (2, 3), (3, 4), (1, 4), (4, 9), (9, 8), (8, 7), (7, 6), (8, 6), (9, 6), (5, 6), (5, 9)]
+
+# G1.add_edges_from(G1_edges)
+# G2.add_edges_from(G2_edges)
+# G1.add_node(0)  # todo: problem when a node is not connected to the graph. Figure out the problem.
+# G2.add_node(0)
+
+# mapped_nodes = {0: 0, 1: 9, 2: 8, 3: 7, 4: 6, 5: 5, 6: 4, 7: 1, 8: 3, 9: 2}
+
+# colors = ["white", "black", "green", "purple", "orange", "red", "blue", "pink", "yellow", "none"]
+# for node, color in zip(G1.nodes, colors):
+#     G1.nodes[node]["label"] = color
+#     G2.nodes[mapped_nodes[node]]["label"] = color
+
+# VF2++ initialization
+for n in G1.nodes():
+    G1.nodes[n]["label"] = "blue"
+    G2.nodes[n]["label"] = "blue"
+
+G1_labels = nx.get_node_attributes(G1, "label")
+G2_labels = nx.get_node_attributes(G2, "label")
+
+# Node ordering
+node_order = [n for n in G1.nodes()]  # todo: dummy ordering until the issue with "match_ordering" is fixed
+
+# VF2++
+mapping = isomorphic_VF2pp(G1, G2, G1_labels, G2_labels, node_order)
+print(mapping)
