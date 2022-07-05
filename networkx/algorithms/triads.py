db866121182d20b28decf4991ad02c8d88dfb451
Ross Barnowski <rossbar@berkeley.edu>
2022-07-05 16:14:57 +0300

    Fix rst formatting  * Make bulleted list of triad types  * Add explicit link to relevant example in gallery.

--- networkx/algorithms/triads.py
+++ networkx/algorithms/triads.py
@@ -323,28 +323,29 @@ def all_triads(G):
 @not_implemented_for("undirected")
 def triads_by_type(G):
     """Returns a list of all triads for each triad type in a directed graph.
-    There are exactly 16 different types of Triads possible. Suppose 1, 2, 3 are three
-    nodes, they will be classified as a particular Triad Type if their connection
-    is as follows:
+    There are exactly 16 different types of triads possible. Suppose 1, 2, 3 are three
+    nodes, they will be classified as a particular triad type if their connections
+    are as follows:
 
-    003: 1, 2, 3
-    012: 1 -> 2, 3
-    102: 1 <-> 2, 3
-    021D: 1 <- 2 -> 3
-    021U: 1 -> 2 <- 3
-    021C: 1 -> 2 -> 3
-    111D: 1 <-> 2 <- 3
-    111U: 1 <-> 2 -> 3
-    030T: 1 -> 2 -> 3, 1 -> 3
-    030C: 1 <- 2 <- 3, 1 -> 3
-    201: 1 <-> 2 <-> 3
-    120D: 1 <- 2 -> 3, 1 <-> 3
-    120U: 1 -> 2 <- 3, 1 <-> 3
-    120C: 1 -> 2 -> 3, 1 <-> 3
-    210: 1 -> 2 <-> 3, 1 <-> 3
-    300: 1 <-> 2 <-> 3, 1 <-> 3
+    - 003: 1, 2, 3
+    - 012: 1 -> 2, 3
+    - 102: 1 <-> 2, 3
+    - 021D: 1 <- 2 -> 3
+    - 021U: 1 -> 2 <- 3
+    - 021C: 1 -> 2 -> 3
+    - 111D: 1 <-> 2 <- 3
+    - 111U: 1 <-> 2 -> 3
+    - 030T: 1 -> 2 -> 3, 1 -> 3
+    - 030C: 1 <- 2 <- 3, 1 -> 3
+    - 201: 1 <-> 2 <-> 3
+    - 120D: 1 <- 2 -> 3, 1 <-> 3
+    - 120U: 1 -> 2 <- 3, 1 <-> 3
+    - 120C: 1 -> 2 -> 3, 1 <-> 3
+    - 210: 1 -> 2 <-> 3, 1 <-> 3
+    - 300: 1 <-> 2 <-> 3, 1 <-> 3
 
-    Refer to Gallery Section to view the Triads better.
+    Refer to the :doc:`example gallery <auto_examples/graph/plot_triad_types>`
+    for visual examples of the triad types.
 
     Parameters
     ----------
