4efb72289f31a1193650394a9eef7fabd040e034
Mridul Seth <seth.mridul@gmail.com>
2022-07-06 15:58:42 +0400

    Use py_random_state to control randomness of random_triad

--- networkx/algorithms/triads.py
+++ networkx/algorithms/triads.py
@@ -6,10 +6,9 @@
 
 from collections import defaultdict
 from itertools import combinations, permutations
-from random import sample
 
 import networkx as nx
-from networkx.utils import not_implemented_for
+from networkx.utils import not_implemented_for, py_random_state
 
 __all__ = [
     "triadic_census",
@@ -520,13 +519,17 @@ def triad_type(G):
 
 
 @not_implemented_for("undirected")
-def random_triad(G):
+@py_random_state(1)
+def random_triad(G, seed=None):
     """Returns a random triad from a directed graph.
 
     Parameters
     ----------
     G : digraph
        A NetworkX DiGraph
+    seed : integer, random_state, or None (default)
+        Indicator of random number generation state.
+        See :ref:`Randomness<randomness>`.
 
     Returns
     -------
@@ -536,12 +539,12 @@ def random_triad(G):
     Examples
     --------
     >>> G = nx.DiGraph([(1, 2), (1, 3), (2, 3), (3, 1), (5, 6), (5, 4), (6, 7)])
-    >>> triad = nx.random_triad(G)
+    >>> triad = nx.random_triad(G, seed=1)
     >>> triad.edges
-    OutEdgeView([(1, 3), (3, 1)])
+    OutEdgeView([(1, 2)])
 
     """
-    nodes = sample(list(G.nodes()), 3)
+    nodes = seed.sample(list(G.nodes()), 3)
     G2 = G.subgraph(nodes)
     return G2
 
