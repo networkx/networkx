3bae35ef092b963796b01013795635630013de69
kpetridis <petridkon@gmail.com>
2022-07-07 13:58:51 +0300

    Remove unnecessary class

--- networkx/algorithms/isomorphism/VF2pp_helpers/state.py
+++ networkx/algorithms/isomorphism/VF2pp_helpers/state.py
@@ -1,17 +1,3 @@
-class State:
-    def __init__(self, G1, G2, u, node_order, mapping, reverse_mapping):
-        self.u = u
-        self.node_order = node_order
-        self.mapping = mapping
-        self.reverse_mapping = reverse_mapping
-
-        self.T1 = {nbr for node in mapping for nbr in G1[node] if nbr not in mapping}
-        self.T2 = {nbr for node in reverse_mapping for nbr in G2[node] if nbr not in reverse_mapping}
-
-        self.T1_out = {n1 for n1 in G1.nodes() if n1 not in mapping and n1 not in self.T1}
-        self.T2_out = {n2 for n2 in G2.nodes() if n2 not in reverse_mapping and n2 not in self.T2}
-
-
 def update_Tinout(G1, G2, T1, T2, T1_out, T2_out, new_node1, new_node2, mapping, reverse_mapping):
     # This function should be called right after the feasibility is established and node1 is mapped to node2.
     uncovered_neighbors_G1 = {nbr for nbr in G1[new_node1] if nbr not in mapping}
