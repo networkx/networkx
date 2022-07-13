8522eea3955f5cf3da43cacc27643d93768aeb03
Dimitrios Papageorgiou <dim_papag@windowslive.com>
2022-07-13 20:06:40 +0300

    Correct louvain formula, solve infinite loops (#5713)
    Fixes the formulae used to calculate gain and removal cost in calculation
    of one level of the Louvain partition tree. Errors here were causing infinite loops
    for some cases in the past, see gh-5175 and gh-5704.
    
    This PR also adds test cases to ensure infinite loops are not entered for these cases.
    
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>

--- networkx/algorithms/community/tests/test_louvain.py
+++ networkx/algorithms/community/tests/test_louvain.py
@@ -43,6 +43,59 @@ def test_partition():
     assert part == partition
 
 
+def test_directed_partition():
+    """
+    Test 2 cases that were looping infinitely
+    from issues #5175 and #5704
+    """
+    G = nx.DiGraph()
+    H = nx.DiGraph()
+    G.add_nodes_from(range(10))
+    H.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+    G_edges = [
+        (0, 2),
+        (0, 1),
+        (1, 0),
+        (2, 1),
+        (2, 0),
+        (3, 4),
+        (4, 3),
+        (7, 8),
+        (8, 7),
+        (9, 10),
+        (10, 9),
+    ]
+    H_edges = [
+        (1, 2),
+        (1, 6),
+        (1, 9),
+        (2, 3),
+        (2, 4),
+        (2, 5),
+        (3, 4),
+        (4, 3),
+        (4, 5),
+        (5, 4),
+        (6, 7),
+        (6, 8),
+        (9, 10),
+        (9, 11),
+        (10, 11),
+        (11, 10),
+    ]
+    G.add_edges_from(G_edges)
+    H.add_edges_from(H_edges)
+
+    G_expected_partition = [{0, 1, 2}, {3, 4}, {5}, {6}, {8, 7}, {9, 10}]
+    G_partition = louvain_communities(G, seed=123, weight=None)
+
+    H_expected_partition = [{2, 3, 4, 5}, {8, 1, 6, 7}, {9, 10, 11}]
+    H_partition = louvain_communities(H, seed=123, weight=None)
+
+    assert G_partition == G_expected_partition
+    assert H_partition == H_expected_partition
+
+
 def test_none_weight_param():
     G = nx.karate_club_graph()
     nx.set_edge_attributes(
@@ -84,9 +137,9 @@ def test_quality():
     quality4 = partition_quality(J, partition4)[0]
 
     assert quality >= 0.65
-    assert quality2 >= 0.85
+    assert quality2 >= 0.65
     assert quality3 >= 0.65
-    assert quality4 >= 0.85
+    assert quality4 >= 0.65
 
 
 def test_multigraph():
@@ -120,7 +173,7 @@ def test_threshold():
     G = nx.LFR_benchmark_graph(
         250, 3, 1.5, 0.009, average_degree=5, min_community=20, seed=10
     )
-    partition1 = louvain_communities(G, threshold=0.2, seed=2)
+    partition1 = louvain_communities(G, threshold=0.3, seed=2)
     partition2 = louvain_communities(G, seed=2)
     mod1 = modularity(G, partition1)
     mod2 = modularity(G, partition2)
