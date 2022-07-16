884e0563d434d8a77c528af57fc4ae18f0b96f33
kpetridis <petridkon@gmail.com>
2022-07-16 13:06:34 +0300

    Add documentation for the ordering

--- networkx/algorithms/isomorphism/VF2pp_helpers/node_ordering.py
+++ networkx/algorithms/isomorphism/VF2pp_helpers/node_ordering.py
@@ -3,15 +3,39 @@ import networkx as nx
 
 
 def matching_order(G1, G2, G1_labels, G2_labels):
+    """The node ordering as introduced in VF2++.
+
+    Notes
+    -----
+    Taking into account the structure of the Graph and the node labeling, the nodes are placed in an order such that,
+    most of the unfruitful/infeasible branches of the search space can be pruned on high levels, significantly
+    decreasing the number of visited states. The premise is that, the algorithm will be able to recognize
+    inconsistencies early, proceeding to go deep into the search tree only if it's needed.
+
+    Parameters
+    ----------
+    G1,G2: NetworkX Graph or MultiGraph instances.
+        The two graphs to check for isomorphism or monomorphism.
+
+    G1_labels: dict
+        The label of every node in G1.
+
+    G2_labels: dict
+        The label of every node in G2.
+
+    Returns
+    -------
+    node_order: list
+        The ordering of the nodes.
+    """
     if not G1 and not G2:
         return {}
 
     (
         nodes_of_G1Labels,
         nodes_of_G2Labels,
-        V1_unordered,
-        current_labels,
-    ) = initialise_preprocess(G1, G2, G1_labels, G2_labels)
+        V1_unordered
+    ) = initialise_preprocess(G1, G1_labels, G2_labels)
     label_rarity = {label: len(nodes) for label, nodes in nodes_of_G2Labels.items()}
     used_degrees = {node: 0 for node in G1}
     node_order = []
@@ -34,7 +58,6 @@ def matching_order(G1, G2, G1_labels, G2_labels):
             dlevel_nodes,
             used_degrees,
             V1_unordered,
-            current_labels,
         ) = BFS_levels(
             max_node,
             G1,
@@ -43,31 +66,57 @@ def matching_order(G1, G2, G1_labels, G2_labels):
             label_rarity,
             used_degrees,
             node_order,
-            current_labels,
         )
 
         node_order.append(max_node)
         V1_unordered.discard(max_node)
-        current_labels.pop(max_node)
         label_rarity[G1_labels[max_node]] -= 1  # todo: do we need that?
 
     return node_order
 
 
 def BFS_levels(
-    source_node,
-    G1,
-    G1_labels,
-    V1_unordered,
-    label_rarity,
-    used_degrees,
-    node_order,
-    current_labels,
+        source_node,
+        G1,
+        G1_labels,
+        V1_unordered,
+        label_rarity,
+        used_degree,
+        node_order
 ):
+    """Performs a BFS search, storing and processing each level of the BFS, separately.
+
+    Parameters
+    ----------
+    source_node: int
+        The node from which the BFS starts.
+
+    G1: NetworkX Graph or MultiGraph instances.
+        The graph on which the BFS is performed.
+
+    G1_labels: dict
+        The label of every node in G1.
+
+    V1_unordered: set
+        The nodes from G1 that are not ordered yet.
+
+    label_rarity: dict
+        Contains the number of nodes that have a specific label. Labels are used as keys.
+
+    used_degree: dict
+        Nodes are used as keys. Indicates how many neighbors of each node have been ordered.
+
+    node_order: list
+        Contains all the nodes that have been ordered until now.
+
+    Returns
+    -------
+    @see process_level
+    """
     dlevel_nodes = set()
     for node, nbr in nx.bfs_edges(G1, source_node):
         if (
-            node not in dlevel_nodes
+                node not in dlevel_nodes
         ):  # This checks for when we finish one depth of the BFS
             dlevel_nodes.add(nbr)
             continue
@@ -76,9 +125,8 @@ def BFS_levels(
             node_order,
             label_rarity,
             _,
-            used_degrees,
-            V1_unordered,
-            current_labels,
+            used_degree,
+            V1_unordered
         ) = process_level(
             V1_unordered,
             G1,
@@ -86,8 +134,7 @@ def BFS_levels(
             node_order,
             dlevel_nodes,
             label_rarity,
-            used_degrees,
-            current_labels,
+            used_degree
         )
 
         # initialize next level to indicate that we finished the next depth of the BFS
@@ -101,14 +148,42 @@ def BFS_levels(
         node_order,
         dlevel_nodes,
         label_rarity,
-        used_degrees,
-        current_labels,
+        used_degree
     )
 
 
 def process_level(
-    V1, G1, G1_labels, order, dlevel_nodes, label_rarity, used_degree, current_labels
+        V1_unordered, G1, G1_labels, order, dlevel_nodes, label_rarity, used_degree
 ):
+    """Processes the nodes of a BFS level.
+
+    Parameters
+    ----------
+    V1_unordered: set
+        The nodes from G1 that are not ordered yet.
+
+    G1: NetworkX Graph or MultiGraph instances.
+        The graph on which the BFS is performed.
+
+    G1_labels: dict
+        The label of every node in G1.
+
+    order: list
+        Contains all the nodes that have been ordered until now.
+
+    dlevel_nodes: set
+        Contains the nodes of a BFS level.
+
+    label_rarity: dict
+        Contains the number of nodes that have a specific label. Labels are used as keys.
+
+    used_degree: dict
+        Nodes are used as keys. Indicates how many neighbors of each node have been ordered.
+
+    Returns
+    -------
+    The updated order, label_rarity, dlevel_nodes, used_degree, V1_unordered
+    """
     max_nodes = []
     max_degree = 0
     while dlevel_nodes:
@@ -132,21 +207,32 @@ def process_level(
         ]  # todo: this can be computed on the go
         next_node = min(max_deg_nodes, key=lambda x: label_rarity[G1_labels[x]])
         order.append(next_node)
-        current_labels.pop(next_node)
 
         for node in G1.neighbors(next_node):
             used_degree[node] += 1
 
         dlevel_nodes.remove(next_node)
         label_rarity[G1_labels[next_node]] -= 1
-        V1.discard(next_node)
-    return order, label_rarity, dlevel_nodes, used_degree, V1, current_labels
+        V1_unordered.discard(next_node)
+    return order, label_rarity, dlevel_nodes, used_degree, V1_unordered
 
 
-def initialise_preprocess(G1, G2, G1_labels, G2_labels):
+def initialise_preprocess(G1, G1_labels, G2_labels):
+    """Initializes basic information, needed for the ordering
+
+    Parameters
+    ----------
+    G1: NetworkX Graph or MultiGraph instances.
+        The graph on which the BFS is performed.
+
+    G1_labels: dict
+        The label of every node in G1.
+
+    G2_labels: dict
+        The label of every node in G2.
+    """
     nodes_of_G1Labels = nx.utils.groups(G1_labels)
     nodes_of_G2Labels = nx.utils.groups(G2_labels)
 
     V1_unordered = set(G1)
-    current_labels = {node: G1_labels[node] for node in V1_unordered}
-    return nodes_of_G1Labels, nodes_of_G2Labels, V1_unordered, current_labels
+    return nodes_of_G1Labels, nodes_of_G2Labels, V1_unordered
