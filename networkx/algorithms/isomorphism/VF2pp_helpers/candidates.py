a0f2d5b2bb7d6a496ba54c5f91871dec84197f2b
kpetridis <petridkon@gmail.com>
2022-07-16 13:11:47 +0300

    Add documentation for the candidate selection

--- networkx/algorithms/isomorphism/VF2pp_helpers/candidates.py
+++ networkx/algorithms/isomorphism/VF2pp_helpers/candidates.py
@@ -1,4 +1,31 @@
 def find_candidates(G1, G2, G1_labels, G2_labels, u, mapping, reverse_mapping):
+    """Given a node u of G1, finds the candidates of u from G2.
+
+    Parameters
+    ----------
+    G1,G2: NetworkX Graph or MultiGraph instances.
+        The two graphs to check for isomorphism or monomorphism.
+
+    G1_labels: dict
+        The label of every node in G1.
+
+    G2_labels: dict
+        The label of every node in G2.
+
+    u: int
+        The node from G1 for which to find the candidates from G2.
+
+    mapping: dict
+        The mapping as extended so far. Maps nodes of G1 to nodes of G2.
+
+    reverse_mapping: dict
+        The reverse mapping as extended so far. Maps nodes from G2 to nodes of G1. It's basically "mapping" reversed.
+
+    Returns
+    -------
+    candidates: set
+        The nodes from G2 which are candidates for u.
+    """
     covered_neighbors = {nbr for nbr in G1[u] if nbr in mapping}
     if len(covered_neighbors) == 0:
         return {
