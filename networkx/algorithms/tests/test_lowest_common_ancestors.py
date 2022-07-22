8e377ac4114bfe0ddb66b7a9a4265969cb3a23fd
Ross Barnowski <rossbar@berkeley.edu>
2022-07-22 14:11:55 +0300

    Test cleanup: move new tests into old class.
    Allows us to get rid of duplication/another test setup.

--- networkx/algorithms/tests/test_lowest_common_ancestors.py
+++ networkx/algorithms/tests/test_lowest_common_ancestors.py
@@ -312,51 +312,6 @@ class TestDAGLCA:
         G.add_node(3)
         assert nx.lowest_common_ancestor(G, 3, 3) == 3
 
-
-class TestNaiveLCA:
-    @classmethod
-    def setup_class(cls):
-        cls.DG = nx.DiGraph()
-        cls.DG.add_nodes_from(range(5))
-        cls.DG.add_edges_from([(1, 0), (2, 0), (3, 2), (4, 1), (4, 3)])
-
-        cls.root_distance = nx.shortest_path_length(cls.DG, source=4)
-
-        cls.gold = {
-            (0, 0): 0,
-            (0, 1): 1,
-            (0, 2): 2,
-            (0, 3): 3,
-            (0, 4): 4,
-            (1, 1): 1,
-            (1, 2): 4,
-            (1, 3): 4,
-            (1, 4): 4,
-            (2, 2): 2,
-            (2, 3): 3,
-            (2, 4): 4,
-            (3, 3): 3,
-            (3, 4): 4,
-            (4, 4): 4,
-        }
-
-    def assert_lca_dicts_same(self, d1, d2, G=None):
-        """Checks if d1 and d2 contain the same pairs and
-        have a node at the same distance from root for each.
-        If G is None use self.DG."""
-        if G is None:
-            G = self.DG
-            root_distance = self.root_distance
-        else:
-            roots = [n for n, deg in G.in_degree if deg == 0]
-            assert len(roots) == 1
-            root_distance = nx.shortest_path_length(G, source=roots[0])
-
-        for a, b in ((min(pair), max(pair)) for pair in chain(d1, d2)):
-            assert (
-                root_distance[get_pair(d1, a, b)] == root_distance[get_pair(d2, a, b)]
-            )
-
     def test_naive_lowest_common_ancestor1(self):
         """Test that the one-pair function works for issue #4574."""
         G = nx.DiGraph()
