e3e559510b8ca9099ebb340e128dc352d9a63cce
brocla <github@whidit.com>
2022-07-26 17:03:23 -0600

    added tests for new signature  - kept all the original tests.    Tagged the tests to remove after signature change is complete.
    - leveraged a test from test_cyctoscape.py, checks for warnings.
    
    -  made new versions of tests that use attrs in the signature.

--- networkx/readwrite/json_graph/tests/test_node_link.py
+++ networkx/readwrite/json_graph/tests/test_node_link.py
@@ -7,6 +7,56 @@ from networkx.readwrite.json_graph import node_link_data, node_link_graph
 
 
 class TestNodeLink:
+
+    # TODO: To be removed when signature change complete
+    def test_attrs_deprecation(recwarn):
+        G = nx.path_graph(3)
+
+        # No warnings when `attrs` kwarg not used
+        data = node_link_data(G)
+        H = node_link_graph(data)
+        assert len(recwarn) == 0
+
+        # Future warning raised with `attrs` kwarg
+        attrs = dict(
+            source="source", target="target", name="id", key="key", link="links"
+        )
+        with pytest.warns(DeprecationWarning):
+            data = node_link_data(G, attrs)
+        with pytest.warns(DeprecationWarning):
+            H = node_link_graph(data, attrs)
+
+    # TODO: To be removed when signature change complete
+    def test_custom_attrs_dep(self):
+        G = nx.path_graph(4)
+        G.add_node(1, color="red")
+        G.add_edge(1, 2, width=7)
+        G.graph[1] = "one"
+        G.graph["foo"] = "bar"
+
+        attrs = dict(
+            source="c_source",
+            target="c_target",
+            name="c_id",
+            key="c_key",
+            link="c_links",
+        )
+
+        H = node_link_graph(
+            node_link_data(G, attrs=attrs), multigraph=False, attrs=attrs
+        )
+        assert nx.is_isomorphic(G, H)
+        assert H.graph["foo"] == "bar"
+        assert H.nodes[1]["color"] == "red"
+        assert H[1][2]["width"] == 7
+
+    # TODO: To be removed when signature change complete
+    def test_exception(self):
+        with pytest.raises(nx.NetworkXError):
+            G = nx.MultiDiGraph()
+            attrs = dict(name="node", source="node", target="node", key="node")
+            node_link_data(G, attrs)
+
     def test_graph(self):
         G = nx.path_graph(4)
         H = node_link_graph(node_link_data(G))
@@ -68,7 +118,7 @@ class TestNodeLink:
         with pytest.raises(nx.NetworkXError):
             G = nx.MultiDiGraph()
             attrs = dict(name="node", source="node", target="node", key="node")
-            node_link_data(G, attrs)
+            node_link_data(G, **attrs)
 
     def test_string_ids(self):
         q = "qualité"
@@ -97,9 +147,7 @@ class TestNodeLink:
             link="c_links",
         )
 
-        H = node_link_graph(
-            node_link_data(G, attrs=attrs), multigraph=False, attrs=attrs
-        )
+        H = node_link_graph(node_link_data(G, **attrs), multigraph=False, **attrs)
         assert nx.is_isomorphic(G, H)
         assert H.graph["foo"] == "bar"
         assert H.nodes[1]["color"] == "red"
