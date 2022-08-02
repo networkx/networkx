3fb4afabdae7a3e3c8f341a8c80b5f58b5227346
Ross Barnowski <rossbar@berkeley.edu>
2022-08-02 14:07:00 +0300

    Use mapping ABC instead of hasattr introspection.

--- networkx/relabel.py
+++ networkx/relabel.py
@@ -1,3 +1,5 @@
+from collections.abc import Mapping
+
 import networkx as nx
 
 __all__ = ["convert_node_labels_to_integers", "relabel_nodes"]
@@ -114,13 +116,9 @@ def relabel_nodes(G, mapping, copy=True):
     --------
     convert_node_labels_to_integers
     """
-    # you can pass a function f(old_label) -> new_label
-    # or a class e.g. str(old_label) -> new_label
-    # but we'll just make a dictionary here regardless
-    # To allow classes, we check if __getitem__ is a bound method using __self__
-    if not (
-        hasattr(mapping, "__getitem__") and hasattr(mapping.__getitem__, "__self__")
-    ):
+    # you can pass any callable e.g. f(old_label) -> new_label or
+    # e.g. str(old_label) -> new_label, but we'll just make a dictionary here regardless
+    if not isinstance(mapping, Mapping):
         m = {n: mapping(n) for n in G}
     else:
         m = mapping
