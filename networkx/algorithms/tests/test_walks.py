0e23a854c7074c14aa32ed329b53eb9f053156fe
dtuncturk <dilaramemis@sabanciuniv.edu>
2022-08-18 13:39:10 +0300

    Modify tests

--- networkx/algorithms/tests/test_walks.py
+++ networkx/algorithms/tests/test_walks.py
@@ -19,67 +19,42 @@ class TestNumberOfWalks:
         assert num_walks == expected
 
     def test_source_only(self):
-        num_walks = nx.number_of_walks(self.G, 2, source=2)
+        num_walks = nx.number_of_walks(self.G, 2)
         expected = {0: 0, 1: 1, 2: 0}
-        assert num_walks == expected
-
-    def test_target_only(self):
-        num_walks = nx.number_of_walks(self.G, 2, target=2)
-        expected = {0: 1, 1: 0, 2: 0}
-        assert num_walks == expected
+        assert num_walks[2] == expected
 
     def test_source_and_target(self):
-        num_walks = nx.number_of_walks(self.G, 2, source=0, target=2)
-        assert num_walks == 1
-
-
-class TestSingleSourceNumberOfWalksTestBase:
-    """Unit tests for the
-    :func:`networkx.algorithms.walks.single_source_number_of_walks`
-    function.
-    This is an abstract base class; concrete subclasses must set the
-    :attr:`method` class attribute to be one of the possible values for
-    the `method` keyword argument to the function under test.
-    """
+        num_walks = nx.number_of_walks(self.G, 2)
+        assert num_walks[0][2] == 1
 
     def test_no_target(self):
         G = nx.cycle_graph(4)
         source = 0
         k = 3
-        num_walks = nx.single_source_number_of_walks(G, k, source)
+        num_walks = nx.number_of_walks(G, k)
         expected = {0: 0, 1: 4, 2: 0, 3: 4}
-        assert num_walks == expected
+        assert num_walks[source] == expected
 
     def test_target(self):
         G = nx.cycle_graph(4)
         source = 0
         target = 1
         k = 3
-        num_walks = nx.single_source_number_of_walks(G, k, source=source, target=target)
-        assert num_walks == 4
+        num_walks = nx.number_of_walks(G, k)
+        assert num_walks[source][target] == 4
 
     def test_source_and_target2(self):
         G = nx.DiGraph([("A", "B"), ("A", "C"), ("A", "D"), ("B", "D"), ("C", "D")])
         source = "A"
         target = "D"
         k = 2
-        num_walks = nx.single_source_number_of_walks(G, k, source=source, target=target)
-        assert num_walks == 2
-
-
-class TestAllPairsNumberOfWalksTestBase:
-    """Unit tests for the
-    :func:`networkx.algorithms.walks.all_pairs_number_of_walks`
-    function.
-    This is an abstract base class; concrete subclasses must set the
-    :attr:`method` class attribute to be one of the possible values for
-    the `method` keyword argument to the function under test.
-    """
+        num_walks = nx.number_of_walks(G, k)
+        assert num_walks[source][target] == 2
 
     def test_basic(self):
         G = nx.cycle_graph(4)
         k = 3
-        num_walks = nx.all_pairs_number_of_walks(G, k)
+        num_walks = nx.number_of_walks(G, k)
         expected = {
             0: {0: 0, 1: 4, 2: 0, 3: 4},
             1: {0: 4, 1: 0, 2: 4, 3: 0},
