9c902f776427f22fc8e9d36fd3a4dfc0dafec70c
Adam Li <adam2392@gmail.com>
2022-08-29 12:44:57 -0400

    Fix ci
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- networkx/algorithms/causal/__init__.py
+++ networkx/algorithms/causal/__init__.py
@@ -1,2 +1,2 @@
-from networkx.algorithms.causal.m_separation import *  # noqa: F403
-from networkx.algorithms.causal.convert import *  # noqa: F403
+from .m_separation import *  # noqa: F403
+from .convert import *  # noqa: F403
