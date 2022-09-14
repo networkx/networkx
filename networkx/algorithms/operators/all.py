4a096c1ba007899cbcdbce148bc7735eeca1859c
ddelange <14880945+ddelange@users.noreply.github.com>
2022-09-14 09:52:48 +0200

    Make union_all generator friendly

--- networkx/algorithms/operators/all.py
+++ networkx/algorithms/operators/all.py
@@ -14,8 +14,8 @@ def union_all(graphs, rename=(None,)):
 
     Parameters
     ----------
-    graphs : list of graphs
-       List of NetworkX graphs
+    graphs : iterable
+       Iterable of NetworkX graphs
 
     rename : bool , default=(None, None)
        Node names of G and H can be changed by specifying the tuple
@@ -45,16 +45,8 @@ def union_all(graphs, rename=(None,)):
     union
     disjoint_union_all
     """
-    # collect the graphs in case an iterator was passed
-    graphs = list(graphs)
-
-    if not graphs:
-        raise ValueError("cannot apply union_all to an empty list")
-
-    U = graphs[0]
-
-    if any(G.is_multigraph() != U.is_multigraph() for G in graphs):
-        raise nx.NetworkXError("All graphs must be graphs or multigraphs.")
+    R = None
+    seen_nodes = set()
 
     # rename graph to obtain disjoint node labels
     def add_prefix(graph, prefix):
@@ -70,33 +62,32 @@ def union_all(graphs, rename=(None,)):
 
         return nx.relabel_nodes(graph, label)
 
-    graphs = [add_prefix(G, name) for G, name in zip_longest(graphs, rename)]
+    graphs = (add_prefix(G, name) for G, name in zip_longest(graphs, rename))
 
-    if sum(len(G) for G in graphs) != len(set().union(*graphs)):
-        raise nx.NetworkXError(
-            "The node sets of the graphs are not disjoint.",
-            "Use appropriate rename"
-            "=(G1prefix,G2prefix,...,GNprefix)"
-            "or use disjoint_union(G1,G2,...,GN).",
+    for i, G in enumerate(graphs):
+        G_nodes_set = set(G)
+        if i == 0:
+            # Union is the same type as first graph
+            R = G.__class__()
+        elif G.is_multigraph() != R.is_multigraph():
+            raise nx.NetworkXError("All graphs must be graphs or multigraphs.")
+        elif not seen_nodes.isdisjoint(G_nodes_set):
+            raise nx.NetworkXError(
+                "The node sets of the graphs are not disjoint.",
+                "Use appropriate rename"
+                "=(G1prefix,G2prefix,...,GNprefix)"
+                "or use disjoint_union(G1,G2,...,GN).",
+            )
+
+        seen_nodes |= G_nodes_set
+        R.graph.update(G.graph)
+        R.add_nodes_from(G.nodes(data=True))
+        R.add_edges_from(
+            G.edges(keys=True, data=True) if G.is_multigraph() else G.edges(data=True)
         )
 
-    # Union is the same type as first graph
-    R = U.__class__()
-
-    # add graph attributes, later attributes take precedent over earlier ones
-    for G in graphs:
-        R.graph.update(G.graph)
-
-    # add nodes and attributes
-    for G in graphs:
-        R.add_nodes_from(G.nodes(data=True))
-
-    if U.is_multigraph():
-        for G in graphs:
-            R.add_edges_from(G.edges(keys=True, data=True))
-    else:
-        for G in graphs:
-            R.add_edges_from(G.edges(data=True))
+    if R is None:
+        raise ValueError("cannot apply union_all to an empty list")
 
     return R
 
@@ -230,8 +221,8 @@ def intersection_all(graphs):
         elif G.is_multigraph() != R.is_multigraph():
             raise nx.NetworkXError("All graphs must be graphs or multigraphs.")
 
-        node_intersection &= G.nodes
-        edge_intersection &= G.edges(keys=True) if G.is_multigraph() else G.edges()
+        node_intersection &= set(G.nodes)
+        edge_intersection &= set(G.edges(keys=True) if G.is_multigraph() else G.edges())
 
     if R is None:
         raise ValueError("cannot apply intersection_all to an empty list")
