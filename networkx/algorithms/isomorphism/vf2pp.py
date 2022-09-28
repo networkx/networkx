aa77a026ec329d4c646129258bdc0093373bab46
kpetridis24 <petridkon@gmail.com>
2022-09-28 20:10:34 +0300

    Change default label initialization to None

--- networkx/algorithms/isomorphism/vf2pp.py
+++ networkx/algorithms/isomorphism/vf2pp.py
@@ -154,7 +154,7 @@ def vf2pp_is_isomorphic(G1, G2, node_label=None, default_label=None):
     return False
 
 
-def vf2pp_all_isomorphisms(G1, G2, node_label=None, default_label=-1):
+def vf2pp_all_isomorphisms(G1, G2, node_label=None, default_label=None):
     """Yields all the possible mappings between G1 and G2.
 
     Parameters
