f005f51fd30223b25c7c009f194e875b231feac2
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2022-10-03 10:41:43 +0200

    First rewrite

--- networkx/algorithms/centrality/eigenvector.py
+++ networkx/algorithms/centrality/eigenvector.py
@@ -11,18 +11,33 @@ __all__ = ["eigenvector_centrality", "eigenvector_centrality_numpy"]
 def eigenvector_centrality(G, max_iter=100, tol=1.0e-6, nstart=None, weight=None):
     r"""Compute the eigenvector centrality for the graph `G`.
 
-    Eigenvector centrality computes the centrality for a node based on the
-    centrality of its neighbors. The eigenvector centrality for node $i$ is
-    the $i$-th element of the vector $x$ defined by the equation
+    Eigenvector centrality computes the centrality for a node by adding
+    the centrality of its predecessors. The centrality for node $i$ is the
+    $i$-th element of a left eigenvector associated with the positive
+    eigenvalue $\lamba$ of largest modulus. Such an eigenvector $x$ is
+    defined up to a multiplicative constant by the equation
 
     .. math::
 
-        Ax = \lambda x
+        x^T A = \lambda x^T,
 
-    where $A$ is the adjacency matrix of the graph `G` with eigenvalue
-    $\lambda$. By virtue of the Perron–Frobenius theorem, there is a unique
-    solution $x$, all of whose entries are positive, if $\lambda$ is the
-    largest eigenvalue of the adjacency matrix $A$ ([2]_).
+    where $A$ is the adjacency matrix of the graph `G`. By definition of
+    row-column product, the equation above is equivalent to
+
+    .. math::
+
+        \lambda x_i = \sum {j\to i}x_j.
+
+    That is, adding the eigenvector centrality of the predecessor of $i$
+    one obtains the eigenvector centrality of $i$ multiplied by $\lambda$.
+
+    By virtue of the Perron–Frobenius theorem [1]_, if `G` is strongly
+    connected there is a unique such eigenvector $x$, and all its entries
+    are strictly positive.
+
+    If `G` is not strongly connected there might be several left
+    eigenvectors associated with $\lambda$, and some of their elements
+    might be zero.
 
     Parameters
     ----------
@@ -33,20 +48,26 @@ def eigenvector_centrality(G, max_iter=100, tol=1.0e-6, nstart=None, weight=None
       Maximum number of iterations in power method.
 
     tol : float, optional (default=1.0e-6)
-      Error tolerance used to check convergence in power method iteration.
+      Error tolerance (in Euclidean norm) used to check convergence in
+      power method iteration.
 
     nstart : dictionary, optional (default=None)
-      Starting value of eigenvector iteration for each node.
+      Starting value of eigenvector iteration for each node. Must have a
+      nonzero projection on the desired eigenvector for the power method
+      to converge. If None, this implementation uses an all-ones vector,
+      which is a safe choice.
 
     weight : None or string, optional (default=None)
-      If None, all edge weights are considered equal.
-      Otherwise holds the name of the edge attribute used as weight.
-      In this measure the weight is interpreted as the connection strength.
+      If None, all edge weights are considered equal. Otherwise holds the
+      name of the edge attribute used as weight. In this measure the
+      weight is interpreted as the connection strength.
 
     Returns
     -------
     nodes : dictionary
-       Dictionary of nodes with eigenvector centrality as the value.
+       Dictionary of nodes with eigenvector centrality as the value. The
+       associated vector has unit Euclidian norm and the values are
+       nonegative.
 
     Examples
     --------
@@ -76,30 +97,62 @@ def eigenvector_centrality(G, max_iter=100, tol=1.0e-6, nstart=None, weight=None
 
     Notes
     -----
-    The measure was introduced by [1]_ and is discussed in [2]_.
+    Eigenvector centrality was introduced by Landau [2]_ for chess
+    tournaments. It was later rediscovered by Wei [3]_ and then
+    popularized by Kendall [4]_ in the context of sport ranking. Berge
+    introduced a general definition for graphs based on social connections
+    [5]_. Bonacich [6]_ reintroduced again eigenvector centrality and made
+    it popular in link analysis.
 
-    The power iteration method is used to compute the eigenvector and
-    convergence is **not** guaranteed. Our method stops after ``max_iter``
-    iterations or when the change in the computed vector between two
-    iterations is smaller than an error tolerance of
-    ``G.number_of_nodes() * tol``. This implementation uses ($A + I$)
-    rather than the adjacency matrix $A$ because it shifts the spectrum
-    to enable discerning the correct eigenvector even for networks with
-    multiple dominant eigenvalues.
+    This method compute the left dominant eigenvector, which corresponds
+    to adding the centrality of predecessors: this is the usual approach.
+    To add the centrality of successors first reverse the graph with
+    ``G.reverse()``.
 
-    For directed graphs this is "left" eigenvector centrality which corresponds
-    to the in-edges in the graph. For out-edges eigenvector centrality
-    first reverse the graph with ``G.reverse()``.
+    The implementation uses power iteration [7]_ to compute a dominant
+    eigenvector starting from the provided vector `nstart`. Convergence is
+    guaranteed as long as `nstart` has a nonzero projection on a dominant
+    eigenvector, which certainly happens using the default value.
+
+    The method stops when the change in the computed vector between two
+    iterations is smaller than an error tolerance of ``G.number_of_nodes()
+    * tol`` or after ``max_iter`` iterations, but in the second case it
+    raises an exception.
+
+    This implementation uses $(A + I)$ rather than the adjacency matrix
+    $A$ because the change preserves eigenvectors, but it shifts the
+    spectrum, thus guaranteeing convergence even for networks with
+    negative eigenvalues of maximum modulus.
 
     References
     ----------
-    .. [1] Phillip Bonacich.
-       "Power and Centrality: A Family of Measures."
-       *American Journal of Sociology* 92(5):1170–1182, 1986
-       <http://www.leonidzhukov.net/hse/2014/socialnetworks/papers/Bonacich-Centrality.pdf>
-    .. [2] Mark E. J. Newman.
-       *Networks: An Introduction.*
-       Oxford University Press, USA, 2010, pp. 169.
+    .. [1] Abraham Berman and Robert J. Plemmons.
+       "Nonnegative Matrices in the Mathematical Sciences."
+       Classics in Applied Mathematics. SIAM, 1994.
+
+    .. [2] Edmund Landau.
+       "Zur relativen Wertbemessung der Turnierresultate."
+       Deutsches Wochenschach, 11:366–369, 1895.
+
+    .. [3] Teh-Hsing Wei.
+       "The Algebraic Foundations of Ranking Theory."
+       PhD thesis, University of Cambridge, 1952.
+
+    .. [4] Maurice G. Kendall.
+       "Further contributions to the theory of paired comparisons."
+       Biometrics, 11(1):43–62, 1955.
+       https://www.jstor.org/stable/3001479
+
+    .. [5] Claude Berge
+       "Théorie des graphes et ses applications."
+       Dunod, Paris, France, 1958.
+
+    .. [6] Phillip Bonacich.
+       "Technique for analyzing overlapping memberships."
+       Sociological Methodology, 4:176–185, 1972.
+       https://www.jstor.org/stable/270732
+
+    .. [7] Power iteration:: https://en.wikipedia.org/wiki/Power_iteration
 
     """
     if len(G) == 0:
