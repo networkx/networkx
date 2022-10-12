dbe0599d9126d54a360fd91948fa347c2b1d1dcf
Stefan van der Walt <stefanv@berkeley.edu>
2022-10-12 15:06:54 -0700

    Lazy load top-level namespace

--- networkx/__init__.py
+++ networkx/__init__.py
@@ -10,39 +10,6 @@ See https://networkx.org for complete documentation.
 
 __version__ = "3.0b2.dev0"
 
+import lazy_loader as lazy
 
-# These are imported in order as listed
-from networkx.lazy_imports import _lazy_import
-
-from networkx.exception import *
-
-from networkx import utils
-
-from networkx import classes
-from networkx.classes import filters
-from networkx.classes import *
-
-from networkx import convert
-from networkx.convert import *
-
-from networkx import convert_matrix
-from networkx.convert_matrix import *
-
-from networkx import relabel
-from networkx.relabel import *
-
-from networkx import generators
-from networkx.generators import *
-
-from networkx import readwrite
-from networkx.readwrite import *
-
-# Need to test with SciPy, when available
-from networkx import algorithms
-from networkx.algorithms import *
-
-from networkx import linalg
-from networkx.linalg import *
-
-from networkx import drawing
-from networkx.drawing import *
+__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
