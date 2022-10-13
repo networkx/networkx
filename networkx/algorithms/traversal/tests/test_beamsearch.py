16cf653464783422821f177da8910ecc84976983
Abangma <30361403+Emmanuel-Lud@users.noreply.github.com>
2022-10-13 04:18:52 +0100

    Update test_beamsearch.py

--- networkx/algorithms/traversal/tests/test_beamsearch.py
+++ networkx/algorithms/traversal/tests/test_beamsearch.py
@@ -29,4 +29,4 @@ class TestBeamSearch:
     def test_none(self):
         G = nx.cycle_graph(4)
         edges = nx.bfs_beam_edges(G, 0, identity, width=None)
-        assert list(edges) == [(0, 3), (0, 1), (3, 2)]
+        assert len(edges) == len(G)
