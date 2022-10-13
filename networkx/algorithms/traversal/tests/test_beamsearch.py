b79c8256c6379e9da98b42e494c60dfa7133c263
Jiafei <jiafei.kuang@gmail.com>
2022-10-13 15:37:46 -0700

    revise format

--- networkx/algorithms/traversal/tests/test_beamsearch.py
+++ networkx/algorithms/traversal/tests/test_beamsearch.py
@@ -25,7 +25,7 @@ class TestBeamSearch:
         G = nx.cycle_graph(4)
         edges = nx.bfs_beam_edges(G, 0, identity, width=2)
         assert list(edges) == [(0, 3), (0, 1), (3, 2)]
-    
+
     def test_none(self):
         G = nx.cycle_graph(4)
         edges = nx.bfs_beam_edges(G, 0, identity, width=None)
