e43ada7c0f2fd4016c8012a6b84ad90797c8b283
Qudirah <qudrohbidemi@gmail.com>
2022-10-18 21:20:27 +0100

    test dispersion

--- networkx/algorithms/centrality/tests/test_dispersion.py
+++ networkx/algorithms/centrality/tests/test_dispersion.py
@@ -60,8 +60,8 @@ class TestDispersion:
 
     def test_dispersion_Gh(self):
         G = small_ego_G()
-        disp_Gh = nx.dispersion(G, "h", normalized=False)
-        disp_Gh_normalized = nx.dispersion(G, "h", normalized=True)
+        disp_Gh = nx.dispersion(G, v="h", normalized=False)
+        disp_Gh_normalized = nx.dispersion(G, v="h", normalized=True)
         assert disp_Gh == {"c": 0, "f": 0, "j": 0, "k": 0, "u": 4}
         assert disp_Gh_normalized == {"c": 0.0, "f": 0.0, "j": 0.0, "k": 0.0, "u": 1.0}
 
