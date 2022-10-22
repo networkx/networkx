de76a5f4abb91926777e1593a5377eb3904fce3d
Mjh9122 <mjh9122@rit.edu>
2022-10-22 10:51:55 -0400

    Changed test_covering.py back to main state

--- networkx/algorithms/tests/test_covering.py
+++ networkx/algorithms/tests/test_covering.py
@@ -1,5 +1,3 @@
-from pytest import raises
-
 import networkx as nx
 
 
@@ -14,15 +12,6 @@ class TestMinEdgeCover:
         G = nx.Graph()
         G.add_edge(0, 0)
         assert nx.min_edge_cover(G) == {(0, 0)}
-
-    def test_graph_with_isolated_v(self):
-        with raises(
-            nx.NetworkXException,
-            match="Graph has a node with no edge incident on it, so no edge cover exists.",
-        ):
-            G = nx.Graph()
-            G.add_node(1)
-            nx.min_edge_cover(G)
 
     def test_graph_single_edge(self):
         G = nx.Graph([(0, 1)])
