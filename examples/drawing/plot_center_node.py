6accc08c8c01a937fedb229825b26e5941e29d7d
Douglas K. G. Araujo <dkg.araujo@gmail.com>
2022-10-24 23:44:57 +0200

    clearer comment on custom node position
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>

--- examples/drawing/plot_center_node.py
+++ examples/drawing/plot_center_node.py
@@ -16,5 +16,5 @@ center_node = 5  # Or any other node to be in the center
 edge_nodes = set(G) - {center_node}
 # Ensures the nodes around the circle are evenly distributed
 pos = nx.circular_layout(G.subgraph(edge_nodes))
-pos[center_node] = np.array([0, 0])  # Or off-center - whatever the user needs
+pos[center_node] = np.array([0, 0])  # manually specify node position
 nx.draw(G, pos, with_labels=True)
