3f0df7a5401973bc1d35fd0353257a0a6d3ca3a0
Ross Barnowski <rossbar@berkeley.edu>
2022-10-28 14:43:16 -0700

    Default stringizer doesn't handle frozensets.

--- networkx/readwrite/tests/test_gml.py
+++ networkx/readwrite/tests/test_gml.py
@@ -568,7 +568,7 @@ graph
         assert_generate_error(G)
         G = nx.Graph()
         G.graph["data"] = frozenset([1, 2, 3])
-        assert_generate_error(G, stringizer=literal_stringizer)
+        assert_generate_error(G)
 
     def test_label_kwarg(self):
         G = nx.parse_gml(self.simple_data, label="id")
