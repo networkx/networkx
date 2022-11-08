31736145219076e4075c145c94f4a9fb1987779e
Ross Barnowski <rossbar@berkeley.edu>
2022-11-08 11:29:16 -0800

    Add suggestion for parameter description.

--- networkx/algorithms/centrality/dispersion.py
+++ networkx/algorithms/centrality/dispersion.py
@@ -20,8 +20,12 @@ def dispersion(G, u=None, v=None, normalized=True, alpha=1.0, b=0.0, c=0.0):
     normalized : bool
         If True (default) normalize by the embededness of the nodes (u and v).
     alpha, b, c : float
-        If normalized is True (default), try out different values of alpha, b and, c to obtain maximum
-        performance of normalized dispersion.
+        Parameters for the normalization procedure. When `normalized` is True,
+        the dispersion value is normalized by::
+
+            result = ((dispersion + b) ** alpha) / (embeddedness + c)
+
+        as long as the denominator is nonzero.
 
     Returns
     -------
