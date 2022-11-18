96e38ee8b1b3b05a20029f2a052fee3fbfefbf7e
Lucas <lucas.hedstrom@protonmail.com>
2022-11-18 13:21:25 +0100

    Apply suggestions from code review
    Co-authored-by: Mridul Seth <mail@mriduls.com>

--- networkx/generators/random_graphs.py
+++ networkx/generators/random_graphs.py
@@ -290,7 +290,7 @@ def gnm_random_graph(n, m, seed=None, directed=False):
 @py_random_state(3)
 def newman_watts_strogatz_graph(n, k, p, seed=None):
     """Returns a Newman–Watts–Strogatz small-world graph.
-    
+
     Note: In the original model by Newman and Watts [2]_, self-loops and multiple
     connections were allowed. Here an edge is only added if it's to a different
     unconnected node. However, for large enough `n` and small enough `k` and `p`,
@@ -330,7 +330,7 @@ def newman_watts_strogatz_graph(n, k, p, seed=None):
        Physics Letters A, 263, 341, 1999.
        https://doi.org/10.1016/S0375-9601(99)00757-4
     .. [2] Newman, M. E., & Watts, D. J. (1999).
-       Scaling and percolation in the small-world network model. 
+       Scaling and percolation in the small-world network model.
        hysical review E, 60(6), 7332.
     """
     if k > n:
