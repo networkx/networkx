723d86334ed3f5f3245d90395eb6a3df9fac0943
EranK123 <93202645+EranK123@users.noreply.github.com>
2022-12-05 18:16:48 +0200

    Delete test_two_agents_approx_cake.py

--- test_two_agents_approx_cake.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from networkx.algorithms.approximation import divide_graphical_cake as dgc
-
-
-class TestTwoAgentsCake:
-    def test_agents_approx_algo(self):
-        assert 4 / 9 == dgc.two_agents_approx_cake(2)
-        assert 1 / 3 == dgc.two_agents_approx_cake(1)
-        assert 13 / 27 == dgc.two_agents_approx_cake(3)
