df3c1beca38766def9acaa85920bfd44ad9587ff
Sultan Orazbayev <contact@econpoint.com>
2022-12-11 09:52:29 +0600

    Update cluster.py

--- networkx/algorithms/cluster.py
+++ networkx/algorithms/cluster.py
@@ -52,14 +52,15 @@ def triangles(G, nodes=None):
     Self loops are ignored.
 
     """
-    # If `nodes` represents a single node in the graph, return only its number
-    # of triangles.
-    if nodes in G:
-        return next(_triangles_and_degree_iter(G, nodes))[2] // 2
+    if nodes is not None:
+        # this checks if `nodes` could potentially refer to a single node
+        # If `nodes` represents a single node, return only its number of triangles
+        # (if it's in the graph) or zero (if the node is not in the graph).
+        if callable(getattr(nodes, '__hash__')):
+            return next(_triangles_and_degree_iter(G, nodes))[2] // 2
 
-    # if `nodes` is a container of nodes, then return a
-    # dictionary mapping node to number of triangles.
-    if nodes:
+        # if `nodes` is a container of nodes, then return a
+        # dictionary mapping node to number of triangles.
         return {v: t // 2 for v, d, t, _ in _triangles_and_degree_iter(G, nodes)}
 
     # if nodes is None, then compute triangles for the complete graph
