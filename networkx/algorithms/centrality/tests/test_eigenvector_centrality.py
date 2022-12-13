9374d1ab1cc732a1a86ef1ed2438bc51e834f20d
Alimi Qudirah <qudrohbidemi@gmail.com>
2022-12-13 18:35:33 +0100

    Improve test coverage for Eigenvector centrality  (#6227)
    Fixes: 6226
    
    * Minor formatting fixups.
    
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>

--- networkx/algorithms/centrality/tests/test_eigenvector_centrality.py
+++ networkx/algorithms/centrality/tests/test_eigenvector_centrality.py
@@ -166,3 +166,10 @@ class TestEigenvectorCentralityExceptions:
     def test_empty_numpy(self):
         with pytest.raises(nx.NetworkXException):
             nx.eigenvector_centrality_numpy(nx.Graph())
+
+    def test_zero_nstart(self):
+        G = nx.Graph([(1, 2), (1, 3), (2, 3)])
+        with pytest.raises(
+            nx.NetworkXException, match="initial vector cannot have all zero values"
+        ):
+            nx.eigenvector_centrality(G, nstart={v: 0 for v in G})
