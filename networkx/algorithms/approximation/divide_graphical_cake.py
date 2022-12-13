3f7dfdf4022c173da94e91660e517b4f5dbe9447
EranK123 <93202645+EranK123@users.noreply.github.com>
2022-12-13 13:12:35 +0200

    Add files via upload

--- networkx/algorithms/approximation/divide_graphical_cake.py
+++ networkx/algorithms/approximation/divide_graphical_cake.py
@@ -1,29 +1,87 @@
-def two_agents_approx_cake(k: int) -> float:
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def graph_example1():
     """
- finds the the best approximation for dividing a graphical cake when both of the agents gets overall of
- totatl of at most k + 1 connected pieces.
- Based on the article Dividing a Graphical Cake by Xiaohui Bei and Warut Suksompong from 2019.
- Link to the article - https://arxiv.org/pdf/1910.14129.pdf
- The algorithm is lemma 4.16 in the article.
- Programmer: Eran Katz
- Parameters
- ----------
- k - an integer.
- Represents the amount of connected pieces the agents will get overall, which is total of at most k + 1.
+    Notice that this graph is almost bridgeless, if we add edge (3,6) this graph will be bridgeless
+    """
+    g = nx.Graph()
+    g.add_nodes_from([1, 2, 3, 4, 5, 6])
+    g.add_edges_from([(1, 2), (1, 3), (2, 3), (2, 4), (4, 6), (4, 5), (5, 6)])
+    nx.draw(g)
+    plt.show()
+    return g
 
- Returns
- -------
- The approximation of the algo. It will be at most 1/2 - 1/(2*3^k). This number represents the amount of "cake" both
- agents will get.
 
-Example 1
- >>> two_agents_approx_cake(2)
- 4/9
-Example 2
-  >>> two_agents_approx_cake(1)
-Example 3
- 1/3
-  >>> two_agents_approx_cake(3)
- 13/27
- """
+def graph_example2():
+    """
+    Notice that this graph is almost bridgeless, if we add edge (1,5) this graph will be bridgeless
+    """
+    g = nx.Graph()
+    g.add_nodes_from([1, 2, 3, 4, 5])
+    g.add_edges_from([(1, 2), (2, 3), (2, 5), (5, 4), (4, 3)])
+    nx.draw(g)
+    plt.show()
+    return g
+
+
+def graph_example3():
+    """
+    Notice that this graph is almost bridgeless, if we add edge (1,6) this graph will be bridgeless
+    """
+    g = nx.Graph()
+    g.add_nodes_from([1, 2, 3, 4, 5, 6])
+    g.add_edges_from([(1, 2), (2, 3), (2, 5), (5, 4), (4, 3), (5, 6)])
+    nx.draw(g)
+    plt.show()
+    return g
+
+
+def get_contiguous_oriented_labeling(graph):
+    """
+    This algorithm admits a contiguous oriented labeling in an almost bridgeless graph
+    Parameters
+    ----------
+    graph : NetworkX graph
+        almost bridgeless graph
+
+    Returns
+    -------
+    a contiguous oriented labeling
+
+    Notes
+    -------
+    A bridge of a graph is an edge that is not contained in any cycle.
+    A graph is said to be bridgeless if it contains no bridges.
+    A graph is said to be almost bridgeless if we can add an edge so that the resulting
+    graph is bridgeless.
+    An oriented labeling of a graph with m edges is a labeling of the edges with
+    numbers 1, 2, . . . ,m, using each number exactly once, together with a labeling of one endpoint of
+    each edge i with i− and the other endpoint with i+ (so each vertex receives a number of labels
+    equal to the number of edges adjacent to it). An oriented labeling is said to be contiguous if:
+    • For each 2 ≤ i ≤ m, the edges labeled 1, 2, . . . , i − 1 form a connected subgraph, and the
+    vertex labeled i− belongs to one of these edges.
+    • For each 1 ≤ i ≤ m − 1, the edges labeled i + 1, i + 2, . . . ,m form a connected subgraph, and
+    the vertex labeled i+ belongs to one of these edges.
+
+     References
+    ----------
+    Bases on an article by Xiaohui Bei and Warut Suksompong
+    https://arxiv.org/pdf/1910.14129.pdf#subsection.4.1 - 2019
+
+    Programmer : Eran Katz
+
+    Example 1:
+    >>> get_contiguous_oriented_labeling(graph_example1())
+    1
+    Example 2:
+    >>> get_contiguous_oriented_labeling(graph_example2())
+    1
+    Example 3:
+    >>> get_contiguous_oriented_labeling(graph_example3())
+    1
+
+    """
     pass
+
