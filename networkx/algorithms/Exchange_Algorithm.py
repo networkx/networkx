d3b7155e1a30bb24c69bc15b3b80c64ceb070cb3
TalSomech <talsomech@gmail.com>
2022-12-13 09:09:47 +0200

    Changed Exchange algorithm to fit networkx

--- networkx/algorithms/Exchange_Algorithm.py
+++ networkx/algorithms/Exchange_Algorithm.py
@@ -1,5 +1,6 @@
 import doctest
 import networkx as nx
+
 """REUT HADAD & TAL SOMECH"""
 
 """
@@ -19,30 +20,28 @@ link:http://eprints.gla.ac.uk/25732/
 
 def APXalgorithm(graph: nx.DiGraph, k: int) -> list:
     """
-    "Algorithm 1 - APX-complete algorithm for kidney exchange programs" by Biro, P. and Manlove, D.F. and Rizzi, R.
-    :param graph:a networkx graph representing the said graph
-    :param k: k to which the k-way exchange works in the algorithm
-    :return: a list of matching k-way exchange for kidney exchange program
-    Algorithm - the algorithm finds maximum weight k-way exchanges using reduction from graph to hyper-graph
+    "
+
+
+    A directed weighted graph is a graph in which every edge is one sided and weighted
+    for example an edge from node 1->2 with a weight of 5,an k-way exchange
+    is a circle within a graph containing at most k nodes.
+    max weighted exchange is a circle with the most weighted edges from every node in the circle
+
+    Parameters
+    -----------
+    G : NetworkX DiGraph
+        Directed graph with weights
+
+    Returns
+    -----------
+    Lst: list of lists
+        Each list in lst contaning the nodes which make up the circle with the highest weights sum
+    Examples
+    -----------
     >>> Digraph=nx.DiGraph()
-    >>> Digraph.add_node(1)
-    >>> Digraph.add_node(2)
-    >>> Digraph.add_node(3)
-    >>> Digraph.add_node(4)
-    >>> Digraph.add_node(5)
-    >>> Digraph.add_node(6)
-    >>> Digraph.add_node(7)
-    >>> Digraph.add_node(8)
-    >>> Digraph.add_edge(1,8,weight=2)
-    >>> Digraph.add_edge(8,1,weight=4)
-    >>> Digraph.add_edge(2,1,weight=5)
-    >>> Digraph.add_edge(1,3,weight=4)
-    >>> Digraph.add_edge(3,8,weight=2)
-    >>> Digraph.add_edge(8,2,weight=3)
-    >>> Digraph.add_edge(8,5,weight=4)
-    >>> Digraph.add_edge(5,7,weight=3)
-    >>> Digraph.add_edge(7,6,weight=2)
-    >>> Digraph.add_edge(6,5,weight=4)
+    >>> Digraph.add_nodes_from([1,2,3,4,5,6,7,8])
+    >>> Digraph.add_weighted_edges_from([(1,8,2),(8,1,4),(2,1,5),(1,3,4),(3,8,2),(8,2,3),(8,5,4),(5,7,3),(7,6,2),(6,5,4)])
     >>> print(APXalgorithm(Digraph,3))
     [[5,6,7],[1,8,2]]||[[5,6,7],[1,3,8]]
     >>> Digraph =nx.DiGraph()
@@ -50,97 +49,77 @@ def APXalgorithm(graph: nx.DiGraph, k: int) -> list:
     >>> Digraph.add_node(2)
     >>> Digraph.add_node(3)
     >>> Digraph.add_node(4)
-    >>> Digraph.add_edge(2,1,weight=3)
-    >>> Digraph.add_edge(1,3,weight=1)
-    >>> Digraph.add_edge(3,2,weight=2)
-    >>> Digraph.add_edge(3,4,weight=5)
-    >>> Digraph.add_edge(4,3,weight=9)
+    >>> Digraph.add_weighted_edges_from([(2,1,3),(1,3,1),(3,2,2),(3,4,5),(4,3,9)])
     >>> print(APXalgorithm(Digraph))
     [3,4]
     >>> Digraph=nx.DiGraph()
-    >>> Digraph.add_node(1)
-    >>> Digraph.add_node(2)
-    >>> Digraph.add_node(3)
-    >>> Digraph.add_node(4)
-    >>> Digraph.add_node(5)
-    >>> Digraph.add_node(6)
-    >>> Digraph.add_node(7)
-    >>> Digraph.add_node(8)
-    >>> Digraph.add_edge(8,1,weight=4)
-    >>> Digraph.add_edge(2,1,weight=5)
-    >>> Digraph.add_edge(1,3,weight=4)
-    >>> Digraph.add_edge(3,8,weight=2)
-    >>> Digraph.add_edge(8,2,weight=3)
-    >>> Digraph.add_edge(8,5,weight=4)
-    >>> Digraph.add_edge(5,7,weight=3)
-    >>> Digraph.add_edge(7,6,weight=2)
-    >>> Digraph.add_edge(6,5,weight=4)
+    >>> Digraph.add_nodes_from([1,2,3,4,5,6,7,8])
+    >>> Digraph.add_weighted_edges_from([(8,1,4),(2,1,5),(1,3,4),(3,8,2),(8,2,3),(8,5,4),(5,7,3),(7,6,2),(6,5,4)])
     >>> print(APXalgorithm(Digraph,2))
     []
+    Notes
+    -----------
+    Algorithm - the algorithm finds maximum weight k-way exchanges using reduction from graph to hyper-graph by
+    the algorithm in the published article APX-complete algorithm for kidney exchange programs"
+
+    Refrences
+    ----------
+    Algorithm 1 - 'MAXIMUM WEIGHT CYCLE PACKING IN DIRECTED GRAPHS, WITH APPLICATION TO KIDNEY EXCHANGE PROGRAMS' by Biro, P. and Manlove, D.F. and Rizzi, R. http://eprints.gla.ac.uk/25732/
+
     """
     f = 5
     return [f]
 
 
-def ExactAlgorithm(graph: nx.Graph, k: int) -> list:
+def ExactAlgorithm(graph: nx.DiGraph, k: int) -> list:
     """
-    "Algorithm 2 - Exact algorithm for kidney exchange programs" by Biro, P. and Manlove, D.F. and Rizzi, R.
-    :param graph:a networkx graph representing the said graph
-    :param k: k to which the k-way exchange works in the algorithm
-    :return: a list of matching k-way exchange for kidney exchange program
     Algorithm - the algorithm finds maximum weight k-way exchanges using reduction from directed graph to non directed
     graph
+    "Algorithm 2 - Exact algorithm for kidney exchange programs" by Biro, P. and Manlove, D.F. and Rizzi, R.
+    Returns the list of max weighted exchanges of directed weighted graph 'G'
+
+    A directed weighted graph is a graph in which every edge is one sided and weighted
+    for example an edge from node 1->2 with a weight of 5,an k-way exchange
+    is a circle within a graph containing at most k nodes.
+    max weighted exchange is a circle with the most weighted edges from every node in the circle
+
+    Parameters
+    -----------
+    G : NetworkX DiGraph
+        Directed graph with weights
+
+    Returns
+    -----------
+    Lst: list of lists
+        Each list in lst contaning the nodes which make up the circle with the highest weights sum
+    Examples
+    -----------
+
+
     >>> Digraph=nx.DiGraph()
-    >>> Digraph.add_node(1)
-    >>> Digraph.add_node(2)
-    >>> Digraph.add_node(3)
-    >>> Digraph.add_node(4)
-    >>> Digraph.add_node(5)
-    >>> Digraph.add_node(6)
-    >>> Digraph.add_node(7)
-    >>> Digraph.add_node(8)
-    >>> Digraph.add_edge(1,8,weight=2)
-    >>> Digraph.add_edge(8,1,weight=4)
-    >>> Digraph.add_edge(2,1,weight=5)
-    >>> Digraph.add_edge(1,3,weight=4)
-    >>> Digraph.add_edge(3,8,weight=2)
-    >>> Digraph.add_edge(8,2,weight=3)
-    >>> Digraph.add_edge(8,5,weight=4)
-    >>> Digraph.add_edge(5,7,weight=3)
-    >>> Digraph.add_edge(7,6,weight=2)
-    >>> Digraph.add_edge(6,5,weight=4)
+    >>> Digraph.add_nodes_from([1,2,3,4,5,6,7,8])
+    >>> Digraph.add_weighted_edges_from([(1,8,2),(8,1,4),(2,1,5),(1,3,4),(3,8,2),(8,2,3),(8,5,4),(5,7,3),(7,6,2),(6,5,4)])
     >>> print(APXalgorithm(Digraph,3))
     [[5,6,7],[1,8,2]]||[[5,6,7],[1,3,8]]
     >>> Digraph =nx.DiGraph()
-    >>> Digraph.add_node(1)
-    >>> Digraph.add_node(2)
-    >>> Digraph.add_node(3)
-    >>> Digraph.add_node(4)
-    >>> Digraph.add_edge(2,1,weight=3)
-    >>> Digraph.add_edge(1,3,weight=1)
-    >>> Digraph.add_edge(3,2,weight=2)
-    >>> Digraph.add_edge(3,4,weight=5)
-    >>> Digraph.add_edge(4,3,weight=9)
+    >>> Digraph.add_nodes_from([1,2,3,4])
+    >>> Digraph.add_weighted_edges_from([(2,1,3),(1,3,1),(3,2,2),(3,4,5),(4,3,9)])
     >>> print(APXalgorithm(Digraph))
     [3,4]
     >>> graphEX3 = nx.DiGraph()
-    >>> graphEX3.add_node(10)
-    >>> graphEX3.add_node(11)
-    >>> graphEX3.add_node(12)
-    >>> graphEX3.add_node(13)
-    >>> graphEX3.add_node(14)
-    >>> graphEX3.add_node(15)
-    >>> graphEX3.add_node(16)
-    >>> graphEX3.add_edge(10,11,weight=10)
-    >>> graphEX3.add_edge(11,12,weight=5)
-    >>> graphEX3.add_edge(12,13,weight=6)
-    >>> graphEX3.add_edge(13,10,weight=4)
-    >>> graphEX3.add_edge(11,14,weight=2)
-    >>> graphEX3.add_edge(14,16,weight=3)
-    >>> graphEX3.add_edge(16,15,weight=8)
-    >>> graphEX3.add_edge(15,14,weight=6)
+    >>> graphEX3.add_nodes_from([10,11,12,13,14,15,16])
+    >>> Digraph.add_weighted_edges_from([(10,11,10),(11,12,5),(12,13,6),(13,10,4),(11,14,2),(14,16,3),(16,15,8),(15,14,6)])
     >>> print(ExactAlgorithm(graphEX3, 2))
     []
+    Notes
+    -----------
+    Algorithm - the algorithm finds maximum weight k-way exchanges using reduction from graph to hyper-graph by
+    the algorithm in the published article Exact-complete algorithm for kidney exchange programs"
+
+    Refrences
+    ----------
+    Algorithm 1 - 'MAXIMUM WEIGHT CYCLE PACKING IN DIRECTED GRAPHS, WITH APPLICATION TO KIDNEY EXCHANGE PROGRAMS' by Biro, P. and Manlove, D.F. and Rizzi, R. http://eprints.gla.ac.uk/25732/
+
     """
     f = 5
     return [f]
--- networkx/algorithms/tests/test_exchange.py
+++ networkx/algorithms/tests/test_exchange.py
@@ -5,24 +5,10 @@ from Exchange_Algorithm import APXalgorithm, ExactAlgorithm
 
 def test_1():
     graphEX3 = nx.DiGraph()
-    graphEX3.add_node(1)
-    graphEX3.add_node(2)
-    graphEX3.add_node(3)
-    graphEX3.add_node(4)
-    graphEX3.add_node(5)
-    graphEX3.add_node(6)
-    graphEX3.add_edge(1, 2, weight=4)
-    graphEX3.add_edge(2, 1, weight=3)
-    graphEX3.add_edge(1, 3, weight=5)
-    graphEX3.add_edge(3, 1, weight=6)
-    graphEX3.add_edge(3, 2, weight=1)
-    graphEX3.add_edge(2, 3, weight=2)
-    graphEX3.add_edge(1, 5, weight=9)
-    graphEX3.add_edge(1, 6, weight=8)
-    graphEX3.add_edge(6, 1, weight=7)
-    graphEX3.add_edge(4, 1, weight=11)
-    graphEX3.add_edge(6, 4, weight=10)
-    graphEX3.add_edge(5, 6, weight=12)
+    graphEX3.add_nodes_from([1, 2, 3, 4, 5, 6])
+    graphEX3.add_weighted_edges_from(
+        [(1, 2, 4), (2, 1, 3), (1, 3, 5), (3, 1, 6), (3, 2, 1), (2, 3, 2), (1, 5, 9), (1, 6, 8), (6, 1, 7), (4, 1, 11),
+         (6, 4, 10), (5, 6, 12)])
     assert APXalgorithm(graphEX3, 3) == [[1, 6, 4]]
     assert ExactAlgorithm(graphEX3, 3) == [[1, 6, 4]]
     assert APXalgorithm(graphEX3, 2) == [[1, 6]]
@@ -31,13 +17,8 @@ def test_1():
 
 def test_2():
     graphEX3 = nx.DiGraph()
-    graphEX3.add_node(1)
-    graphEX3.add_node(2)
-    graphEX3.add_node(3)
-    graphEX3.add_edge(1, 2, weight=1)
-    graphEX3.add_edge(2, 1, weight=5)
-    graphEX3.add_edge(2, 3, weight=2)
-    graphEX3.add_edge(3, 1, weight=2)
+    graphEX3.add_nodes_from([1, 2, 3])
+    graphEX3.add_weighted_edges_from([(1, 2, 1), (2, 1, 5), (2, 3, 2), (3, 1, 2)])
     assert APXalgorithm(graphEX3, 3) == [[1, 2]]
     assert ExactAlgorithm(graphEX3, 3) == [[1, 2]]
     assert APXalgorithm(graphEX3, 2) == [[1, 2]]
@@ -46,26 +27,9 @@ def test_2():
 
 def test_3():
     graphEX3 = nx.DiGraph()
-    graphEX3.add_node(1)
-    graphEX3.add_node(2)
-    graphEX3.add_node(3)
-    graphEX3.add_node(4)
-    graphEX3.add_node(5)
-    graphEX3.add_node(6)
-    graphEX3.add_node(7)
-    graphEX3.add_node(8)
-    graphEX3.add_node(9)
-    graphEX3.add_edge(1, 6, weight=11)
-    graphEX3.add_edge(6, 1, weight=10)
-    graphEX3.add_edge(1, 5, weight=3)
-    graphEX3.add_edge(5, 1, weight=2)
-    graphEX3.add_edge(8, 9, weight=11)
-    graphEX3.add_edge(9, 8, weight=20)
-    graphEX3.add_edge(3, 2, weight=6)
-    graphEX3.add_edge(2, 6, weight=5)
-    graphEX3.add_edge(6, 3, weight=8)
-    graphEX3.add_edge(5, 7, weight=6)
-    graphEX3.add_edge(7, 4, weight=11)
-    graphEX3.add_edge(4, 5, weight=5)
+    graphEX3.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    graphEX3.add_weighted_edges_from(
+        [(1, 6, 11), (6, 1, 10), (1, 5, 3), (5, 1, 2), (8, 9, 11), (9, 8, 20), (3, 2, 6), (2, 6, 5), (6, 3, 8),
+         (5, 7, 6), (7, 4, 11), (4, 5, 5)])
     assert APXalgorithm(graphEX3, 3) == [[1, 5], [8, 9], [1, 3, 2]]
     assert ExactAlgorithm(graphEX3, 3) == [[8, 9], [1, 6], [4, 5, 7]]
