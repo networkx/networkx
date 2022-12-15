161b6438a8a2a38aa0d605c247f65f6eb2cd08e1
EranK123 <93202645+EranK123@users.noreply.github.com>
2022-12-15 15:54:10 +0200

    Add files via upload

--- networkx/algorithms/approximation/tests/test_two_agents_approx_cake.py
+++ networkx/algorithms/approximation/tests/test_two_agents_approx_cake.py
@@ -1,9 +1,18 @@
-import pytest
+import unittest
 from networkx.algorithms.approximation import divide_graphical_cake as dgc
 
 
-class TestTwoAgentsCake:
-    def test_agents_approx_algo(self):
-        assert 4 / 9 == dgc.two_agents_approx_cake(2)
-        assert 1 / 3 == dgc.two_agents_approx_cake(1)
-        assert 13 / 27 == dgc.two_agents_approx_cake(3)
+class TestDivideGraphicalCake:
+    def test_get_contiguous_oriented_labeling(self):
+        g1 = dgc.graph_example1()
+        g2 = dgc.graph_example2()
+        g3 = dgc.graph_example3()
+        assert dgc.get_contiguous_oriented_labeling(g1) == {(1, 2): 2, (1, 3): 1, (2, 3): 3, (2, 4): 4, (4, 6): 5,
+                                                            (4, 5): 6, (5, 6): 7}, {1: [1, -2], 2: [2, 3, -4],
+                                                                                    3: [-1, -3], 4: [4, -5, -6],
+                                                                                    5: [6, -7], 6: [5, 7]}
+        assert dgc.get_contiguous_oriented_labeling(g2) == {(1, 2): 5, (2, 3): 3, (2, 5): 4, (5, 4): 1, (4, 3): 2}, {
+            1: [5], 2: [-5, 3, -4], 3: [-3, 2], 4: [-2, 1], 5: [4, -1]}
+        assert dgc.get_contiguous_oriented_labeling(g3) == {(1, 2): 1, (2, 3): 2, (2, 5): 5, (4, 5): 4, (3, 4): 3,
+                                                            (5, 6): 6}, {1: [-1], 2: [1, -2, -5], 3: [2, -3],
+                                                                         4: [3, -4], 5: [4, 5, -6], 6: [6]}
