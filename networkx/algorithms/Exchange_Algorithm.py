e3a437e66b598bf2f1361b18f75bb460a55d741b
TalSomech <talsomech@gmail.com>
2022-12-25 09:26:14 +0200

    changed

--- networkx/algorithms/Exchange_Algorithm.py
+++ networkx/algorithms/Exchange_Algorithm.py
@@ -1,9 +1,11 @@
 import doctest
+import itertools
+
 import networkx as nx
-from itertools import permutations,product
+from itertools import permutations, product
 import numpy as np
 from more_itertools import distinct_permutations as idp
-
+import matplotlib.pyplot as plt
 """REUT HADAD & TAL SOMECH"""
 
 """
@@ -54,7 +56,7 @@ def ExactAlgorithm(graph: nx.DiGraph, k: int) -> list:
     >>> Digraph =nx.DiGraph()
     >>> Digraph.add_nodes_from([1,2,3,4])
     >>> Digraph.add_weighted_edges_from([(2,1,3),(1,3,1),(3,2,2),(3,4,5),(4,3,9)])
-    >>> print(ExactAlgorithm(Digraph))
+    >>> print(ExactAlgorithm(Digraph,3))
     [3,4]
     >>> graphEX3 = nx.DiGraph()
     >>> graphEX3.add_nodes_from([10,11,12,13,14,15,16])
@@ -74,53 +76,70 @@ def ExactAlgorithm(graph: nx.DiGraph, k: int) -> list:
     for cycle in temp_cycles:
         if len(cycle) == k:
             cycles.append(cycle)
-    Y = create_Y(cycles, k)
+    Ys = create_Ys(cycles, k)
 
-    return Y
+    X = []
+    for Y in Ys:
+        ans_graph = nx.Graph()
+
+        for edge in graph.edges:
+            if edge in Y:
+                ans_graph.add_node(f"Y{edge[0]},{edge[1]}")
+            else:
+                if edge[0] not in X:
+                    X.append(edge[0])
+                ans_graph.add_node(f"X{edge[0]}")
+
+        for i in range(len(X)):
+            for edge in Y:
+                if X[i] in edge:
+                    ans_graph.add_edge(f"X{X[i]}",f"Y{edge[0]},{edge[1]}")
+            for j in range(i + 1, len(X)):
+                if (X[i], X[j]) in graph.edges and (X[j], X[i]) in graph.edges:
+                    t = 5
+                    weight = graph.get_edge_data(X[i], X[j])["weight"] + graph.get_edge_data(X[j], X[i])["weight"]
+                    ans_graph.add_edge(f"X{X[i]}", f"X{X[j]}", weight=weight)
+
+        for k in range(len(X)):
+            for j,l in Y:
+                if [j, l, X[k]] in cycles:  # j == X[i] and (l, X[k]) in graph.edges and (X[k], X[i]) in graph.edges:
+                    weight = graph.get_edge_data(j, l)["weight"] + graph.get_edge_data(l, X[k])["weight"] + \
+                             graph.get_edge_data(X[k], j)["weight"]
+                    ans_graph.add_edge(f"X{X[k]}", f"Y{j},{l}", weight=weight)
+        components = [ans_graph.subgraph(c).copy() for c in nx.connected_components(ans_graph)]
+        exchanges=[]
+        for comp in components:
+            exchanges.append(sorted(comp.edges(data=True), key=lambda t: t[2].get('weight', 1),reverse=True)[0])
+        return exchanges
 
 
-def create_Y(cycles, k):
-    arr = [[] for i in range(len(cycles)) for j in range(k)]  # * k * len(cycles)  # np.ndarray(shape=(len(cycles),k))
-    ns = [[None]*k]*len(cycles)**k
-    nk=[]
-    for permu in range(len(cycles)):
-        i = 0
-        # t=idp(cycles[permu], 2)
-        for cyc in permutations(cycles[permu], 2):
-            cyc = sorted(list(cyc))
-            j=0
-            if cyc not in arr[permu * k:permu * k + k]:
-                ns[permu*k:permu*k+k][j]=cyc
-                j=j+1
-                arr[permu * k + i] = cyc
-                #nk.append(cyc)
-                i = i + 1
-        nk.append(arr[permu * k:permu * k + k])
-    #kn=[[i,j,k] for i in nk[0] for j in nk[1] for k in nk[2]]
-    return [[i,j,k] for i in nk[0] for j in nk[1] for k in nk[2]]
-
-
-# def get_pem(cycles,k):
-#     arr = [[]*k**len(cycles)]
-#     # a = list(range(0, 3))
-#     # b = list(range(3, 6))
-#     # c = list(range(6, 9))
-#     j=0
-#     for i in cycles:
-#         for n in range(len(cycles)):
-#             arr[]
-    # for an in permutations([a,b,c]):
-    #     t=a
+def create_Ys(cycles, k):
+    arr2 = np.ndarray(shape=(len(cycles),k), dtype=list)
+    for cyc_idx in range(len(cycles)):
+        cyc = cycles[cyc_idx]
+        for ed_idx in range(len(cyc)):
+            mid = (cyc[ed_idx], cyc[(ed_idx + 1) % len(cyc)])
+            arr2[cyc_idx][ed_idx] = mid
+    temp=arr2[0]
+    for i in range(1,len(arr2)):
+        k=np.ndarray(list(temp))
+        temp=itertools.product(temp,arr2[i])
+    return list(temp) #[[i, j, k] for i in arr2[0] for j in arr2[1] for k in arr2[2]]
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # itertools.ne
     Digraph = nx.DiGraph()
-    # #Digraph.add
-    Digraph.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8])
+    Digraph.add_nodes_from([1, 2, 3, 5, 6, 7, 8])
     Digraph.add_weighted_edges_from(
         [(1, 8, 2), (8, 1, 4), (2, 1, 5), (1, 3, 4), (3, 8, 2), (8, 2, 3), (8, 5, 4), (5, 7, 3), (7, 6, 2), (6, 5, 4)])
+    # t = Digraph.edges
+    # Digraph =nx.DiGraph()
+    # Digraph.add_nodes_from([1,2,3,4])
+    # Digraph.add_weighted_edges_from([(2,1,3),(1,3,1),(3,2,2),(3,4,5),(4,3,9)])
+    # nx.draw_shell(ExactAlgorithm(Digraph, 3))
+    # plt.show()
     print(ExactAlgorithm(Digraph, 3))
     # # for b in permutations([Dx[0:3],Dx[3:6],Dx[6:9]]):
     #     print(b)
@@ -128,6 +147,6 @@ if __name__ == '__main__':
     # print(permutations([1, 2, 3],2))
     # for i in idp([1, 2, 3], 2):
     #     print(i)
-    #get_pem()
+    # get_pem()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
