437c890a11a9949c1c86d285ba6d0e2a7142f501
Matheus Diógenes Andrade <matheusdiogenesandrade@pop-os.localdomain>
2023-01-02 14:52:39 +0100

    New algorithm for calculating longest path between two nodes

--- networkx/algorithms/tests/test_dag.py
+++ networkx/algorithms/tests/test_dag.py
@@ -485,54 +485,6 @@ class TestDagLongestPathBetween:
         assert nx.dag_longest_path_between(G, 1, 3, default_weight=3) == [1, 2, 3]
 
 
-class TestDagLongestPathLength:
-    """Unit tests for computing the length of a longest path in a
-    directed acyclic graph.
-
-    """
-
-    def test_unweighted(self):
-        edges = [(1, 2), (2, 3), (2, 4), (3, 5), (5, 6), (5, 7)]
-        G = nx.DiGraph(edges)
-        assert nx.dag_longest_path_length(G) == 4
-
-        edges = [(1, 2), (2, 3), (3, 4), (4, 5), (1, 3), (1, 5), (3, 5)]
-        G = nx.DiGraph(edges)
-        assert nx.dag_longest_path_length(G) == 4
-
-        # test degenerate graphs
-        G = nx.DiGraph()
-        G.add_node(1)
-        assert nx.dag_longest_path_length(G) == 0
-
-    def test_undirected_not_implemented(self):
-        G = nx.Graph()
-        pytest.raises(nx.NetworkXNotImplemented, nx.dag_longest_path_length, G)
-
-    def test_weighted(self):
-        edges = [(1, 2, -5), (2, 3, 1), (3, 4, 1), (4, 5, 0), (3, 5, 4), (1, 6, 2)]
-        G = nx.DiGraph()
-        G.add_weighted_edges_from(edges)
-        assert nx.dag_longest_path_length(G) == 5
-
-    def test_multigraph_unweighted(self):
-        edges = [(1, 2), (2, 3), (2, 3), (3, 4), (4, 5), (1, 3), (1, 5), (3, 5)]
-        G = nx.MultiDiGraph(edges)
-        assert nx.dag_longest_path_length(G) == 4
-
-    def test_multigraph_weighted(self):
-        G = nx.MultiDiGraph()
-        edges = [
-            (1, 2, 2),
-            (2, 3, 2),
-            (1, 3, 1),
-            (1, 3, 5),
-            (1, 3, 2),
-        ]
-        G.add_weighted_edges_from(edges)
-        assert nx.dag_longest_path_length(G) == 5
-
-
 class TestDagLongestPathBetweenLength:
     """Unit tests for computing the length of a longest path in a
     directed acyclic graph.
