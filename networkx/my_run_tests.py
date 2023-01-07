7cc856336a198e7b8305db10497e50fb57bb6dc7
Benjamin Saldman <benjaminsaldman@gmail.com>
2023-01-08 00:11:31 +0200

    lint failed lines fix

--- networkx/my_run_tests.py
+++ networkx/my_run_tests.py
@@ -6,6 +6,8 @@ from networkx.algorithms.bipartite.matching import (
     minimum_weight_full_matching,
     to_vertex_cover,
 )
+
+
 def main():
     G = nx.complete_bipartite_graph(3, 2)
 
@@ -13,8 +15,9 @@ def main():
     X, Y = nx.bipartite.sets(G)
     unmatched_vertices = set(X) - set(maximum_matching)
     print(maximum_matching)
-    #print(len(unmatched_vertices)==0)\
+    # print(len(unmatched_vertices)==0)\
     print(unmatched_vertices)
     print(G)
+
 
 main()
