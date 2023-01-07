780b8222ab0985987c2be62deda786b7e52eec76
Benjamin Saldman <benjaminsaldman@gmail.com>
2023-01-07 23:09:11 +0200

    lint_check

--- networkx/algorithms/bipartite/tests/test_matching.py
+++ networkx/algorithms/bipartite/tests/test_matching.py
@@ -347,7 +347,20 @@ class TestEnvyFreeMatching:
         matching = envy_free_matching(A)
         assert matching == {0: 3, 3: 0, 1: 4, 4: 1, 2: 5, 5: 2}
 
-        B = nx.Graph([(0, 3), (3, 0), (0, 4), (4, 0), (1, 4), (4, 1), (1, 5), (5, 1), (2, 5), (5, 2)])
+        B = nx.Graph(
+            [
+                (0, 3),
+                (3, 0),
+                (0, 4),
+                (4, 0),
+                (1, 4),
+                (4, 1),
+                (1, 5),
+                (5, 1),
+                (2, 5),
+                (5, 2),
+            ]
+        )
         matching = envy_free_matching(B)
         assert matching == {0: 3, 3: 0, 1: 4, 4: 1, 2: 5, 5: 2}
 
@@ -367,8 +380,23 @@ class TestEnvyFreeMatching:
         matching = envy_free_matching(A)
         assert matching == {0: 3, 3: 0}
 
-        B = nx.Graph([(0, 4), (4, 0), (0, 5), (5, 0), (0, 8), (8, 0), (1, 6), (6, 1), (2, 7), (7, 2), (3, 7), (7, 3)])
-        matching = envy_free_matching(B,top_nodes=[0, 1, 2, 3])
+        B = nx.Graph(
+            [
+                (0, 4),
+                (4, 0),
+                (0, 5),
+                (5, 0),
+                (0, 8),
+                (8, 0),
+                (1, 6),
+                (6, 1),
+                (2, 7),
+                (7, 2),
+                (3, 7),
+                (7, 3),
+            ]
+        )
+        matching = envy_free_matching(B, top_nodes=[0, 1, 2, 3])
         assert matching == {0: 4, 4: 0, 1: 6, 6: 1}
 
     def test_empty_envy_free_matching(self):
@@ -384,14 +412,16 @@ class TestEnvyFreeMatching:
             [(0, 3), (1, 3), (1, 4), (2, 4), (3, 0), (3, 1), (4, 1), (4, 2)]
 
             """
-            if size % 2 == 0: raise Exception
+            if size % 2 == 0:
+                raise Exception
 
             edges = [(0, size)]
 
             actions = {
                 0: lambda: edges.append((edges[-1][0], edges[-1][1] + 1)),
 
-                1: lambda: edges.append((edges[-1][0] + 1, edges[-1][1]))
+
+                1: lambda: edges.append((edges[-1][0] + 1, edges[-1][1])),
             }
 
             for i in range(1, size + 1):
@@ -401,11 +431,34 @@ class TestEnvyFreeMatching:
         A = generate_odd_path(3)  # check small input first
 
         B = nx.Graph(
-            [(0, 6), (6, 0), (1, 6), (6, 1), (1, 7), (7, 1), (2, 6), (6, 2), (2, 8), (8, 2), (3, 9), (9, 3), (3, 6),
-             (6, 3), (4, 8), (8, 4), (4, 7), (7, 4), (5, 9), (9, 5)])  # more intricate graph
+            [
+                (0, 6),
+                (6, 0),
+                (1, 6),
+                (6, 1),
+                (1, 7),
+                (7, 1),
+                (2, 6),
+                (6, 2),
+                (2, 8),
+                (8, 2),
+                (3, 9),
+                (9, 3),
+                (3, 6),
+                (6, 3),
+                (4, 8),
+                (8, 4),
+                (4, 7),
+                (7, 4),
+                (5, 9),
+                (9, 5),
+            ]
+        )  # more intricate graph
 
         C = generate_odd_path(11)  # check big inputs
-        D = generate_odd_path(501)  # check even bigger inputs, may take too long, adjust the given value as needed
+        D = generate_odd_path(
+            501
+        )  # check even bigger inputs, may take too long, adjust the given value as needed
 
         graphs = {A, B, C, D}
         for graph in graphs:
@@ -416,14 +469,35 @@ class TestEnvyFreeMatching:
 class TestMinimumWeightEnvyFreeMatching:
     def test_envy_free_perfect_matching(self):
         G = nx.Graph()
-        weights = [(0, 3, 250), (0, 4, 148), (0, 5, 122), (1, 3, 175), (1, 4, 135), (1, 5, 150), (2, 3, 150),
-                        (2, 4, 125)]
+        weights = [
+            (0, 3, 250),
+            (0, 4, 148),
+            (0, 5, 122),
+            (1, 3, 175),
+            (1, 4, 135),
+            (1, 5, 150),
+            (2, 3, 150),
+            (2, 4, 125),
+        ]
         G.add_weighted_edges_from(weights)
-        assert minimum_weight_envy_free_matching(G) == {0: 5, 1: 4, 2: 3, 5: 0, 4: 1, 3: 2}
+        assert minimum_weight_envy_free_matching(G) == {
+            0: 5,
+            1: 4,
+            2: 3,
+            5: 0,
+            4: 1,
+            3: 2,
+        }
 
 
 
     def test_non_empty_envy_free_matching(self):
         Graph = nx.Graph()
-        Graph.add_weighted_edges_from([(0, 4, 5), (1, 4, 1), (2, 5, 3), (2, 7, 9), (3, 6, 3), (3, 7, 7)])
-        assert minimum_weight_envy_free_matching(Graph, top_nodes=[0, 1, 2, 3]) == {2: 5, 3: 6, 5: 2, 6: 3}
+        Graph.add_weighted_edges_from(
+            [(0, 4, 5), (1, 4, 1), (2, 5, 3), (2, 7, 9), (3, 6, 3), (3, 7, 7)]
+        )
+        assert minimum_weight_envy_free_matching(Graph, top_nodes=[0, 1, 2, 3]) == {
+            2: 5,
+            3: 6,
+            5: 2,
+            6: 3,}
