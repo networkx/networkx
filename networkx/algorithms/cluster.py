59f5b01c80064b3b88e1d044bab1c4cf61fdbc85
Tina Oberoi <55803680+tinaoberoi@users.noreply.github.com>
2023-01-09 13:48:52 -0600

    doc:improve doc of possible values of nodes and expected behaviour (#6333)

--- networkx/algorithms/cluster.py
+++ networkx/algorithms/cluster.py
@@ -329,8 +329,10 @@ def clustering(G, nodes=None, weight=None):
     ----------
     G : graph
 
-    nodes : container of nodes, optional (default=all nodes in G)
-       Compute clustering for nodes in this container.
+    nodes : node, iterable of nodes, or None (default=None)
+        If a singleton node, return the number of triangles for that node.
+        If an iterable, compute the number of triangles for each of those nodes.
+        If `None` (the default) compute the number of triangles for all nodes in `G`.
 
     weight : string or None, optional (default=None)
        The edge attribute that holds the numerical value used as a weight.
