d3e04d01c029e6fbb66c61e2eeb4d333ce7188d9
TalSomech <talsomech@gmail.com>
2023-01-09 09:39:53 +0200

    added doctests

--- networkx/algorithms/maximum_weight_cycle_packing.py
+++ networkx/algorithms/maximum_weight_cycle_packing.py
@@ -156,6 +156,16 @@ def simple_cycles(G, limit):
     >>> a=[y for y in Ys]
     >>> print(a)
     [[16, 15, 14], [1, 3, 2], [3, 4]]
+    >>> graphEX3 = nx.DiGraph()
+    >>> graphEX3.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+    >>> graphEX3.add_weighted_edges_from(
+    ...    [(1, 6, 11), (6, 1, 10), (1, 5, 3), (5, 1, 2), (8, 9, 11), (9, 8, 20), (3, 2, 6), (2, 6, 5), (6, 3, 8),
+    ...     (5, 7, 6), (7, 4, 11), (4, 5, 5), (10, 16, 1), (16, 11, 10), (11, 15, 3), (15, 11, 2), (18, 19, 11),
+    ...     (19, 18, 20), (13, 12, 6), (12, 16, 5), (16, 13, 8)])
+    >>> Ys=simple_cycles(Digraph,3)
+    >>> a=[y for y in Ys]
+    >>> print(a)
+    [[16, 15, 14], [1, 3, 2], [3, 4]]
     """
     subG = type(G)(G.edges())
     sccs = list(nx.strongly_connected_components(subG))
