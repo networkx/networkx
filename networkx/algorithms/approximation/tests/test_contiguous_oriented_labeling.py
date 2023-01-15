b6d9829e7c013ce835bf265345b28473abe4a5ed
erankatz <eranki.katz@gmail.com>
2023-01-15 14:06:46 +0200

    .

--- networkx/algorithms/approximation/tests/test_contiguous_oriented_labeling.py
+++ networkx/algorithms/approximation/tests/test_contiguous_oriented_labeling.py
@@ -10,5 +10,6 @@
 #         g3 = dgc.graph_example3()
 #         print(dgc.get_contiguous_oriented_labeling(g1))
 #         # assert dgc.get_contiguous_oriented_labeling(g1) == ([(1, 3), (3, 2), (1, 2), (2, 4), (4, 6), (6, 5), (5, 4)], InEdgeView([(1, 2), (3, 2), (2, 4), (5, 4), (1, 3), (4, 6), (6, 5)]))
-#         # assert dgc.get_contiguous_oriented_labeling(g2) == ([(1, 2), (2, 5), (5, 4), (4, 3), (2, 3)], InEdgeView([(1, 2), (2, 3), (4, 3), (2, 5), (5, 4)]))
+#
+# assert dgc.get_contiguous_oriented_labeling(g2) == ([(1, 2), (2, 5), (5, 4), (4, 3), (2, 3)], InEdgeView([(1, 2), (2, 3), (4, 3), (2, 5), (5, 4)]))
 #         # assert dgc.get_contiguous_oriented_labeling(g3) == ([(1, 2), (2, 5), (2, 3), (3, 4), (4, 5), (5, 6)], InEdgeView([(1, 2), (2, 3), (3, 4), (4, 5), (2, 5), (5, 6)]))
