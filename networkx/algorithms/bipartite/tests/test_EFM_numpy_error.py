39b4e22c6b19c2bc14096fb24a24b7be0e413df2
Benjamin Saldman <benjaminsaldman@gmail.com>
2023-01-17 01:20:22 +0200

    Lint check

--- networkx/algorithms/bipartite/tests/test_EFM_numpy_error.py
+++ networkx/algorithms/bipartite/tests/test_EFM_numpy_error.py
@@ -10,3 +10,6 @@ class TestEnvyFreeMatching:
         B = nx.complete_bipartite_graph(3, 3)
         matching = envy_free_matching(B)
         assert matching == {0: 3, 1: 4, 2: 5, 3: 0, 4: 1, 5: 2}
+        C = nx.complete_bipartite_graph(3, 3)
+        matching=minimum_weight_envy_free_matching(C)
+        assert matching == {0: 3, 1: 4, 2: 5, 3: 0, 4: 1, 5: 2}
