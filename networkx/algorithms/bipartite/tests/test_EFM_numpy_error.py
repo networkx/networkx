db469f5877210433b83ba9d087d6b37c6c8cd843
Benjamin Saldman <benjaminsaldman@gmail.com>
2023-01-17 00:40:26 +0200

    added error detection

---
+++ networkx/algorithms/bipartite/tests/test_EFM_numpy_error.py
@@ -0,0 +1,12 @@
+import networkx as nx
+from networkx.algorithms.bipartite.EFM_algorithms import *
+
+
+class TestEnvyFreeMatching:
+    def test_envy_free_matching(self):
+        A = nx.complete_bipartite_graph(3, 3)
+        # Fails to import numpy when calling envy_free_matching.
+        assert envy_free_matching(A) == {0: 3, 1: 4, 2: 5, 3: 0, 4: 1, 5: 2}
+        B = nx.complete_bipartite_graph(3, 3)
+        assert envy_free_matching(B) == {0: 3, 1: 4, 2: 5, 3: 0, 4: 1, 5: 2}
+
