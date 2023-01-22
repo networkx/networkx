519f8f4fcdb7bdaea70714892c86e46324a1ed94
Benjamin Saldman <benjaminsaldman@gmail.com>
2023-01-22 18:36:33 +0200

    updated conftest.py

--- networkx/conftest.py
+++ networkx/conftest.py
@@ -182,13 +182,15 @@ try:
 except ImportError:
     has_sympy = False
 
-
 # List of files that pytest should ignore
 
 collect_ignore = []
 
 needs_numpy = [
     "algorithms/approximation/traveling_salesman.py",
+    "algorithms/bipartite/EFM_algorithms.py",
+    "algorithms/bipartite/matching.py",
+    "algorithms/bipartite/tests/test_EFM_numpy_error.py",
     "algorithms/centrality/current_flow_closeness.py",
     "algorithms/node_classification.py",
     "algorithms/non_randomness.py",
@@ -203,6 +205,8 @@ needs_scipy = [
     "algorithms/assortativity/correlation.py",
     "algorithms/assortativity/mixing.py",
     "algorithms/assortativity/pairs.py",
+    "algorithms/bipartite/EFM_algorithms.py",
+    "algorithms/bipartite/matching.py",
     "algorithms/bipartite/matrix.py",
     "algorithms/bipartite/spectral.py",
     "algorithms/centrality/current_flow_betweenness.py",
