479fe072d00e4f2ca3813b5036e81ff56e3a17d5
Lioz Akirav <lioz.akirav@gmail.com>
2023-01-27 18:30:41 +0200

    remove venv

--- venv/lib/python3.8/site-packages/scipy/linalg/_matfuncs_expm.pyi
+++
@@ -1,6 +0,0 @@
-from numpy.typing import NDArray
-from typing import Any, Tuple
-
-def pick_pade_structure(a: NDArray[Any]) -> Tuple[int, int]: ...
-
-def pade_UV_calc(Am: NDArray[Any], n: int, m: int) -> None: ...
