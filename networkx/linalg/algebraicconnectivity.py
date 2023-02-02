8df6ee68bd9d75264ae57c17a9f41bb125275c05
Mridul Seth <git@mriduls.com>
2023-02-02 17:44:17 +0400

    Add tests for spectral bisection

--- networkx/linalg/algebraicconnectivity.py
+++ networkx/linalg/algebraicconnectivity.py
@@ -643,11 +643,10 @@ def spectral_bisection(
     --------
     >>> G = nx.karate_club_graph()
     >>> nx.spectral_bisection(G)
-    [set([9, 14, 15, 18, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]),
-     set([0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 16, 17, 19, 21])]
-    Notes
-    -----
-    This algorithm splits the eigenvector at the value 0.
+    [{0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 16, 17, 19, 21},
+    {8, 9, 14, 15, 18, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33}]
+
+
     References
     ----------
     .. [1] M. E. J Newman 'Networks: An Introduction', pages 364-370
@@ -657,7 +656,7 @@ def spectral_bisection(
 
     v = nx.fiedler_vector(G, weight=weight)
     node_list = list(G)
-    x, y = np.where(v > 0)[0], np.where(v <= 0)[0]
+    x, y = np.where(v < 0)[0], np.where(v >= 0)[0]
 
     return [
         {node_list[i] for i in x},
