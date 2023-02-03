6e157aff1f91333924ac125a0b7a60d6373a0e68
Ross Barnowski <rossbar@berkeley.edu>
2023-02-02 23:25:46 -0800

    Minor formatting nits.

--- networkx/algorithms/approximation/__init__.py
+++ networkx/algorithms/approximation/__init__.py
@@ -1,12 +1,12 @@
 """Approximations of graph properties and Heuristic methods for optimization.
-    
-    The functions in this class are not imported into the top-level
-    :mod:`networkx` namespace so the easiest way to use them is with:
+
+The functions in this class are not imported into the top-level ``networkx``
+namespace so the easiest way to use them is with::
 
     >>> from networkx.algorithms import approximation
 
-    Another option is to import the specific function with 
-    :mod:`from networkx.algorithms.approximation import function_name`.
+Another option is to import the specific function with
+``from networkx.algorithms.approximation import function_name``.
 
 """
 from networkx.algorithms.approximation.clustering_coefficient import *
