b0de1e4923d6b2abd6d014819234f85702aa8b39
Mridul Seth <git@mriduls.com>
2023-02-13 12:23:25 +0100

    update tests

--- networkx/linalg/tests/test_algebraic_connectivity.py
+++ networkx/linalg/tests/test_algebraic_connectivity.py
@@ -50,15 +50,15 @@ def test_spectral_bisection():
     pytest.importorskip("scipy")
     G = nx.barbell_graph(3, 0)
     C = nx.spectral_bisection(G)
-    assert C == [{0, 1, 2}, {3, 4, 5}]
+    assert C == ({0, 1, 2}, {3, 4, 5})
 
     mapping = dict(enumerate("badfec"))
     G = nx.relabel_nodes(G, mapping)
     C = nx.spectral_bisection(G)
-    assert C == [
+    assert C == (
         {mapping[0], mapping[1], mapping[2]},
         {mapping[3], mapping[4], mapping[5]},
-    ]
+    )
 
 
 def check_eigenvector(A, l, x):
