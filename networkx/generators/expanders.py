e7a83be1057b0628eefef32373aa2544593ea8d7
Paula Pérez Bianchi <44149844+paulitapb@users.noreply.github.com>
2023-02-28 10:39:54 -0300

    Doc upgrade paley graph (#6399)
    * Add warning in community doc
    
    * fix unwanted change
    
    * Add latex formatting
    
    * let pre-commit add double backslashes
    
    * Update networkx/generators/expanders.py
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>
    
    * Update networkx/generators/expanders.py
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>
    
    ---------
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- networkx/generators/expanders.py
+++ networkx/generators/expanders.py
@@ -146,22 +146,22 @@ def chordal_cycle_graph(p, create_using=None):
 
 
 def paley_graph(p, create_using=None):
-    """Returns the Paley (p-1)/2-regular graph on p nodes.
+    """Returns the Paley $\\frac{(p-1)}{2}$ -regular graph on $p$ nodes.
 
-    The returned graph is a graph on Z/pZ with edges between x and y
-    if and only if x-y is a nonzero square in Z/pZ.
+    The returned graph is a graph on $\\mathbb{Z}/p\\mathbb{Z}$ with edges between $x$ and $y$
+    if and only if $x-y$ is a nonzero square in $\\mathbb{Z}/p\\mathbb{Z}$.
 
-    If p = 1 mod 4, -1 is a square in Z/pZ and therefore x-y is a square if and
-    only if y-x is also a square, i.e the edges in the Paley graph are symmetric.
+    If $p \\equiv 1  \\pmod 4$, $-1$ is a square in $\\mathbb{Z}/p\\mathbb{Z}$ and therefore $x-y$ is a square if and
+    only if $y-x$ is also a square, i.e the edges in the Paley graph are symmetric.
 
-    If p = 3 mod 4, -1 is not a square in Z/pZ and therefore either x-y or y-x
-    is a square in Z/pZ but not both.
+    If $p \\equiv 3 \\pmod 4$, $-1$ is not a square in $\\mathbb{Z}/p\\mathbb{Z}$ and therefore either $x-y$ or $y-x$
+    is a square in $\\mathbb{Z}/p\\mathbb{Z}$ but not both.
 
     Note that a more general definition of Paley graphs extends this construction
-    to graphs over q=p^n vertices, by using the finite field F_q instead of Z/pZ.
+    to graphs over $q=p^n$ vertices, by using the finite field $F_q$ instead of $\\mathbb{Z}/p\\mathbb{Z}$.
     This construction requires to compute squares in general finite fields and is
-    not what is implemented here (i.e paley_graph(25) does not return the true
-    Paley graph associated with 5^2).
+    not what is implemented here (i.e `paley_graph(25)` does not return the true
+    Paley graph associated with $5^2$).
 
     Parameters
     ----------
