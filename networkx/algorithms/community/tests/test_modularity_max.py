a814601a81db72b8eca9b4bbd62390c3f35a5a96
anne <annemachinda@gmail.com>
2023-03-09 12:49:34 +0100

    changed explicit import from test modularity max

--- networkx/algorithms/community/tests/test_modularity_max.py
+++ networkx/algorithms/community/tests/test_modularity_max.py
@@ -1,9 +1,10 @@
 import pytest
 
 import networkx as nx
-from networkx.algorithms.community import (
-    greedy_modularity_communities,
-    naive_greedy_modularity_communities,
+
+greedy_modularity_communities = nx.community.greedy_modularity_communities
+naive_greedy_modularity_communities = (
+    nx.community.naive_greedy_modularity_communities,
 )
 
 
