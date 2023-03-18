2a6cb28736aa86f61515011bd47e2f4aec1205dc
manny02 <manasviemmadi@gmail.com>
2023-03-18 23:06:09 +0530

    Removed addition of isomorphvf2

--- networkx/algorithms/isomorphism/__init__.py
+++ networkx/algorithms/isomorphism/__init__.py
@@ -5,4 +5,3 @@ from networkx.algorithms.isomorphism.temporalisomorphvf2 import *
 from networkx.algorithms.isomorphism.ismags import *
 from networkx.algorithms.isomorphism.tree_isomorphism import *
 from networkx.algorithms.isomorphism.vf2pp import *
-from networkx.algorithms.isomorphism.isomorphvf2 import *
