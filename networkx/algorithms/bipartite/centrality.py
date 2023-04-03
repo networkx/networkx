b112cd250b13650a065872b93650c6bec9f2bc64
Navya Agarwal <navya.agarwal.in@gmail.com>
2023-04-03 17:48:55 +0530

    Import bipartite module

--- networkx/algorithms/bipartite/centrality.py
+++ networkx/algorithms/bipartite/centrality.py
@@ -24,6 +24,7 @@ def degree_centrality(G, nodes):
 
     Examples
     --------
+    >>> from networkx.algorithms import bipartite
     >>> G = nx.wheel_graph(5)
     >>> bipartite.degree_centrality(G, [0, 1, 2])
     {0: 2.0, 1: 1.5, 2: 1.5, 3: 1.0, 4: 1.0}
@@ -128,6 +129,7 @@ def betweenness_centrality(G, nodes):
 
     Examples
     --------
+    >>> from networkx.algorithms import bipartite
     >>> G = nx.cycle_graph(4)
     >>> bipartite.betweenness_centrality(G, [1, 2])
     {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
@@ -204,6 +206,7 @@ def closeness_centrality(G, nodes, normalized=True):
 
     Examples
     --------
+    >>> from networkx.algorithms import bipartite
     >>> G = nx.wheel_graph(5)
     >>> bipartite.closeness_centrality(G, [0, 1, 2])
     {0: 1.5, 1: 1.2, 2: 1.2, 3: 1.0, 4: 1.0}
