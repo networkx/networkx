f6d058d7c33f4f72d1dc23e1353a2ed7e214a760
Michael Holtz <mjh9122@rit.edu>
2023-04-10 21:07:09 -0400

    Coding Is Fun

--- networkx/algorithms/link_prediction.py
+++ networkx/algorithms/link_prediction.py
@@ -4,11 +4,13 @@ Link prediction algorithms.
 
 from math import log
 
-import networkx as nx
-import scipy as sp
 
-from networkx.utils import not_implemented_for
+import scipy as sp
 from scipy import sparse
+
+import networkx as nx
+from networkx.utils import not_implemented_for
+
 
 __all__ = [
     "resource_allocation_index",
