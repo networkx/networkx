2ccce80c00f602b91bcad9e2f5115f7755265132
evgepab <t8200129@aueb.gr>
2023-05-02 17:56:24 +0300

    Add test and turned time_delta to an interval

--- networkx/algorithms/tests/test_time_dependent.py
+++ networkx/algorithms/tests/test_time_dependent.py
@@ -1,54 +1,44 @@
 """Unit testing for time dependent algorithms."""
 
-import pytest
-import networkx as nx
 from datetime import datetime
 
-class TestCdIndex:
+import pytest
 
-    def test_datetime(self):
+import networkx as nx
+
+
+class TestCdIndex:
+    def test_russellfunk(self):
         G = nx.DiGraph()
-        G.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
-        G.add_edge(1, 2)
-        G.add_edge(1, 3)
-        G.add_edge(1, 4)
-        G.add_edge(1, 5)
-        G.add_edge(6, 1)
-        G.add_edge(7, 1)
-        G.add_edge(8, 1)
+        G.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        G.add_edge(4, 2)
+        G.add_edge(4, 0)
+        G.add_edge(4, 1)
+        G.add_edge(4, 3)
+        G.add_edge(5, 2)
+        G.add_edge(6, 2)
+        G.add_edge(6, 4)
+        G.add_edge(7, 4)
+        G.add_edge(8, 4)
+        G.add_edge(9, 4)
         G.add_edge(9, 1)
-        G.add_edge(10, 1)
-        G.add_edge(11, 1)
+        G.add_edge(9, 3)
+        G.add_edge(10, 4)
 
         node_attrs = {
-            1: {'time': datetime(2022, 1, 1)},
-            2: {'time': datetime(2022, 1, 1)},
-            3: {'time': datetime(2022, 1, 1)},
-            4: {'time': datetime(2022, 1, 1)},
-            5: {'time': datetime(2022, 1, 1)},
-            6: {'time': datetime(2022, 1, 1)},
-            7: {'time': datetime(2022, 1, 1)},
-            8: {'time': datetime(2022, 1, 1)},
-            9: {'time': datetime(2022, 1, 1)},
-            10: {'time': datetime(2022,1, 1)},
-            11: {'time': datetime(2022, 4, 1)}
+            0: {"time": datetime(1992, 1, 1)},
+            1: {"time": datetime(1992, 1, 1)},
+            2: {"time": datetime(1993, 1, 1)},
+            3: {"time": datetime(1993, 1, 1)},
+            4: {"time": datetime(1995, 1, 1)},
+            5: {"time": datetime(1997, 1, 1)},
+            6: {"time": datetime(1998, 1, 1)},
+            7: {"time": datetime(1999, 1, 1)},
+            8: {"time": datetime(1999, 1, 1)},
+            9: {"time": datetime(1998, 1, 1)},
+            10: {"time": datetime(1997, 4, 1)},
         }
 
-        # Set the node attributes for the graph
         nx.set_node_attributes(G, node_attrs)
 
-        print(G.nodes.data('time'))
-
-        assert nx.cd_index(G, 1, datetime(2022,4, 1)) == 1
-
-    def test_extra_nodes(self):
-        G = nx.DiGraph()
-        G.add_nodes_from([1, 2, 3, 4, 5, 6, 7])
-        G.add_edge(1, 2)
-        G.add_edge(1, 3)
-        G.add_edge(4, 1)
-        G.add_edge(5, 1)
-        G.add_edge(6, 2)
-        G.add_edge(7, 4)
-
-        assert nx.cd_index(G, 1) == 0.67
+        assert nx.cd_index(G, 4) == 0.17
--- networkx/algorithms/time_dependent.py
+++ networkx/algorithms/time_dependent.py
@@ -1,55 +1,67 @@
 """Time dependent algorithms."""
 
+from datetime import datetime
+
 import networkx as nx
 from networkx.utils import not_implemented_for
 
 __all__ = ["cd_index"]
 
+
 @not_implemented_for("undirected")
 @not_implemented_for("multigraph")
-def cd_index(G, node, time_delta = None):
-   """Compute the CD index.
+def cd_index(G, node, time_delta=5):
+    """Compute the CD index.
 
-   Calculates the CD index for the graph based on the given "focal patent" node.
+    Calculates the CD index for the graph based on the given "focal patent" node
+    considering the patents after time_delta years.
 
-   Parameters
-   ----------
-   G : graph
-      A directed networkx graph.
-   node : focal node that represents the focal patent
-      Compute triangles for nodes in this container.
+    Parameters
+    ----------
+    G : graph
+       A directed networkx graph whose nodes have datetime 'time' attributes.
+    node : integer
+       Focal node that represents the focal patent.
+    time_delta : integer
+       Number of years after creation of focal patent.
 
-   Returns
-   -------
-   cd : float
-      The CD index calculated for the G graph.
+    Returns
+    -------
+    float
+       The CD index calculated for the G graph.
 
-   Examples
-   --------
+    Examples
+    --------
 
-   Notes
-   -----
-   
+    Notes
+    -----
 
-   """
 
-   suc, pred = list(G.successors(node)), list(G.predecessors(node))
+    """
+    if not (
+        all(
+            G.has_node(n)
+            and "time" in G.nodes[n]
+            and isinstance(G.nodes[n]["time"], datetime)
+            for n in G
+        )
+    ):
+        raise ValueError("Not all nodes have a datetime 'time' attribute.")
 
-   if not suc:
-      raise ValueError("This node has no successors.")
-   
-   if time_delta is not None:
-      for i in pred:
-         if G.nodes[i]['time'] > time_delta:
-            pred.remove(i)
+    target_date = G.nodes[node]["time"].timestamp() + time_delta * 365 * 24 * 60 * 60
+    succ, pred = list(G.successors(node)), list(G.predecessors(node))
 
-   if not pred:
-      raise ValueError("This node has no predecessors.")
+    if not succ:
+        raise ValueError("This node has no successors.")
 
-   b = [int(any((i, j) in G.edges() for j in suc)) for i in pred]
+    actual_pred = [i for i in pred if G.nodes[i]["time"].timestamp() <= target_date]
 
-   n = set(pred)
-   for s in suc:
-      n |= set(G.predecessors(s)) - {node}
+    if not actual_pred:
+        raise ValueError("This node has no predecessors.")
 
-   return round(sum((-2) * bi + 1 for bi in b) / len(n), 2)
+    b = [int(any((i, j) in G.edges() for j in succ)) for i in actual_pred]
+    n = set(actual_pred)
+    for s in succ:
+        n |= set(G.predecessors(s)) - {node}
+
+    return round(sum((-2) * bi + 1 for bi in b) / len(n), 2)
