39be26b38bf00bea3dcc7a74078bab4a5be538c0
Zavins <zywang1@uci.edu>
2023-05-12 12:23:31 -0700

    Fixed the test for num_independent_sets

--- networkx/algorithms/tests/test_num_independent_sets.py
+++ networkx/algorithms/tests/test_num_independent_sets.py
@@ -1,26 +1,26 @@
 """
-Tests for maximum number of independent sets.
+Tests for number of independent sets.
 """
 
 import networkx as nx
-import networkx.algorithms.max_num_ind_sets as ug
+from networkx.algorithms.num_independent_sets import num_independent_sets
 import pytest
 
 def test_singleton_graph():
-    G = ug.UCIGraph()
+    G = nx.Graph()
     G.add_node(1)
-    assert G.num_independent_sets() == 2
+    assert num_independent_sets(G) == 2
 
 def test_len2_path():
-    G = ug.UCIGraph()
+    G = nx.Graph()
     G.add_node(1)
     G.add_node(2)
     G.add_edge(1, 2)
-    assert G.num_independent_sets() == 3
+    assert num_independent_sets(G) == 3
 
 
 def test_simple_path_tree():
-    G = ug.UCIGraph()
+    G = nx.Graph()
     G.add_node(1)#2
     G.add_node(2)#3
     G.add_node(3)#5
@@ -30,10 +30,10 @@ def test_simple_path_tree():
     G.add_edge(2, 3)
     G.add_edge(3, 4)
     G.add_edge(4, 5)
-    assert G.num_independent_sets() == 13
+    assert num_independent_sets(G) == 13
 
 def test_simple_path_tree2():
-    G = ug.UCIGraph()
+    G = nx.Graph()
     G.add_node(1)#2
     G.add_node(2)#3
     G.add_node(3)#5
@@ -45,10 +45,10 @@ def test_simple_path_tree2():
     G.add_edge(3, 4)
     G.add_edge(4, 5)
     G.add_edge(5, 6)
-    assert G.num_independent_sets() == 21
+    assert num_independent_sets(G) == 21
 
 def test_binary_tree_of_depth_3():
-    G = ug.UCIGraph()
+    G = nx.Graph()
     G.add_node(1)
     G.add_node(2)
     G.add_node(3)
@@ -63,10 +63,10 @@ def test_binary_tree_of_depth_3():
     G.add_edge(2, 5)
     G.add_edge(3, 6)
     G.add_edge(3, 7)
-    assert G.num_independent_sets() == 41
+    assert num_independent_sets(G) == 41
 
 def test_tree_of_depth_3():
-    G = ug.UCIGraph()
+    G = nx.Graph()
     G.add_node(1)
     G.add_node(2)
     G.add_node(3)
@@ -83,10 +83,10 @@ def test_tree_of_depth_3():
     G.add_edge(2, 6)
     G.add_edge(3, 7)
     G.add_edge(4, 8)
-    assert G.num_independent_sets() == 61
+    assert num_independent_sets(G) == 61
 
 def test_tree_of_depth_4(): #add a node on top
-    G = ug.UCIGraph()
+    G = nx.Graph()
     G.add_node(0)
     G.add_node(1)
     G.add_edge(0, 1)
@@ -106,5 +106,5 @@ def test_tree_of_depth_4(): #add a node on top
     G.add_edge(2, 6)
     G.add_edge(3, 7)
     G.add_edge(4, 8)
-    assert G.num_independent_sets() == 106
+    assert num_independent_sets(G) == 106
 
