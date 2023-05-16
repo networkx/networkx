c695c6277d7311a61ebad2bce1bc84c08ae908e1
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-05-16 08:07:34 +0200

    Updated documentation of eigenvector_centrality_numpy

--- networkx/algorithms/centrality/eigenvector.py
+++ networkx/algorithms/centrality/eigenvector.py
@@ -45,7 +45,7 @@ def eigenvector_centrality(G, max_iter=100, tol=1.0e-6, nstart=None, weight=None
     Parameters
     ----------
     G : graph
-      A networkx graph
+      A networkx graph.
 
     max_iter : integer, optional (default=100)
       Maximum number of power iterations.
@@ -194,40 +194,61 @@ def eigenvector_centrality(G, max_iter=100, tol=1.0e-6, nstart=None, weight=None
 
 
 def eigenvector_centrality_numpy(G, weight=None, max_iter=50, tol=0):
-    r"""Compute the eigenvector centrality for the graph G.
+    r"""Compute the eigenvector centrality for the graph `G`.
 
-    Eigenvector centrality computes the centrality for a node based on the
-    centrality of its neighbors. The eigenvector centrality for node $i$ is
+    Eigenvector centrality computes the centrality for a node by adding
+    the centrality of its predecessors. The centrality for node $i$ is the
+    $i$-th element of a left eigenvector associated with the eigenvalue $\lambda$
+    of maximum modulus that is positive. Such an eigenvector $x$ is
+    defined up to a multiplicative constant by the equation
 
     .. math::
 
-        Ax = \lambda x
+         \lambda x^T = x^T A,
 
-    where $A$ is the adjacency matrix of the graph G with eigenvalue $\lambda$.
-    By virtue of the Perron–Frobenius theorem, there is a unique and positive
-    solution if $\lambda$ is the largest eigenvalue associated with the
-    eigenvector of the adjacency matrix $A$ ([2]_).
+    where $A$ is the adjacency matrix of the graph `G`. By definition of
+    row-column product, the equation above is equivalent to
+
+    .. math::
+
+        \lambda x_i = \sum_{j\to i}x_j.
+
+    That is, adding the eigenvector centralities of the predecessors of
+    $i$ one obtains the eigenvector centrality of $i$ multiplied by
+    $\lambda$. In the case of undirected graphs, $x$ also solves the familiar
+    right-eigenvector equation $Ax = \lambda x$.
+
+    By virtue of the Perron–Frobenius theorem [1]_, if `G` is strongly
+    connected there is a unique eigenvector $x$, and all its entries
+    are strictly positive.
+
+    If `G` is not strongly connected there might be several left
+    eigenvectors associated with $\lambda$, and some of their elements
+    might be zero.
 
     Parameters
     ----------
     G : graph
-      A networkx graph
+      A networkx graph.
+
+    max_iter : integer, optional (default=50)
+      Maximum number of Arnoldi update iterations allowed.
+
+    tol : float, optional (default=0)
+      Relative accuracy for eigenvalues (stopping criterion).
+      The default value of 0 implies machine precision.
 
     weight : None or string, optional (default=None)
-      The name of the edge attribute used as weight.
-      If None, all edge weights are considered equal.
-      In this measure the weight is interpreted as the connection strength.
-    max_iter : integer, optional (default=100)
-      Maximum number of iterations in power method.
-
-    tol : float, optional (default=1.0e-6)
-       Relative accuracy for eigenvalues (stopping criterion).
-       The default value of 0 implies machine precision.
+      If None, all edge weights are considered equal. Otherwise holds the
+      name of the edge attribute used as weight. In this measure the
+      weight is interpreted as the connection strength.
 
     Returns
     -------
     nodes : dictionary
-       Dictionary of nodes with eigenvector centrality as the value.
+       Dictionary of nodes with eigenvector centrality as the value. The
+       associated vector has unit Euclidian norm and the values are
+       nonegative.
 
     Examples
     --------
@@ -236,37 +257,70 @@ def eigenvector_centrality_numpy(G, weight=None, max_iter=50, tol=0):
     >>> print([f"{node} {centrality[node]:0.2f}" for node in centrality])
     ['0 0.37', '1 0.60', '2 0.60', '3 0.37']
 
+    Raises
+    ------
+    NetworkXPointlessConcept
+        If the graph `G` is the null graph.
+
+    ArpackNoConvergence
+        When the requested convergence is not obtained. The currently
+        converged eigenvalues and eigenvectors can be found as 
+        eigenvalues and eigenvectors attributes of the exception object.
+
     See Also
     --------
+    :func:`scipy.sparse.linalg.eigs`
     eigenvector_centrality
     :func:`~networkx.algorithms.link_analysis.pagerank_alg.pagerank`
     :func:`~networkx.algorithms.link_analysis.hits_alg.hits`
 
     Notes
     -----
-    The measure was introduced by [1]_.
+    Eigenvector centrality was introduced by Landau [2]_ for chess
+    tournaments. It was later rediscovered by Wei [3]_ and then
+    popularized by Kendall [4]_ in the context of sport ranking. Berge
+    introduced a general definition for graphs based on social connections
+    [5]_. Bonacich [6]_ reintroduced again eigenvector centrality and made
+    it popular in link analysis.
 
-    This algorithm uses the SciPy sparse eigenvalue solver (ARPACK) to
+    This method compute the left dominant eigenvector, which corresponds
+    to adding the centrality of predecessors: this is the usual approach.
+    To add the centrality of successors first reverse the graph with
+    ``G.reverse()``.
+
+    This implementation uses the SciPy sparse eigenvalue solver (ARPACK) to
     find the largest eigenvalue/eigenvector pair.
-
-    For directed graphs this is "left" eigenvector centrality which corresponds
-    to the in-edges in the graph. For out-edges eigenvector centrality
-    first reverse the graph with ``G.reverse()``.
-
-    Raises
-    ------
-    NetworkXPointlessConcept
-        If the graph ``G`` is the null graph.
 
     References
     ----------
-    .. [1] Phillip Bonacich:
-       Power and Centrality: A Family of Measures.
-       American Journal of Sociology 92(5):1170–1182, 1986
-       http://www.leonidzhukov.net/hse/2014/socialnetworks/papers/Bonacich-Centrality.pdf
-    .. [2] Mark E. J. Newman:
-       Networks: An Introduction.
-       Oxford University Press, USA, 2010, pp. 169.
+    .. [1] Abraham Berman and Robert J. Plemmons.
+       "Nonnegative Matrices in the Mathematical Sciences."
+       Classics in Applied Mathematics. SIAM, 1994.
+
+    .. [2] Edmund Landau.
+       "Zur relativen Wertbemessung der Turnierresultate."
+       Deutsches Wochenschach, 11:366–369, 1895.
+
+    .. [3] Teh-Hsing Wei.
+       "The Algebraic Foundations of Ranking Theory."
+       PhD thesis, University of Cambridge, 1952.
+
+    .. [4] Maurice G. Kendall.
+       "Further contributions to the theory of paired comparisons."
+       Biometrics, 11(1):43–62, 1955.
+       https://www.jstor.org/stable/3001479
+
+    .. [5] Claude Berge
+       "Théorie des graphes et ses applications."
+       Dunod, Paris, France, 1958.
+
+    .. [6] Phillip Bonacich.
+       "Technique for analyzing overlapping memberships."
+       Sociological Methodology, 4:176–185, 1972.
+       https://www.jstor.org/stable/270732
+
+    .. [7] Power iteration:: https://en.wikipedia.org/wiki/Power_iteration
+
     """
     import numpy as np
     import scipy as sp
