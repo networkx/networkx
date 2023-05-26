c3d75ecf5404d56aecc2a3ac8bb4dd80adeaf6bd
Zavins <zywang1@uci.edu>
2023-05-26 11:41:41 -0700

    Format the files

--- networkx/algorithms/tests/test_num_independent_sets.py
+++ networkx/algorithms/tests/test_num_independent_sets.py
@@ -6,10 +6,12 @@ import networkx as nx
 from networkx.algorithms.num_independent_sets import num_independent_sets
 import pytest
 
+
 def test_singleton_graph():
     G = nx.Graph()
     G.add_node(1)
     assert num_independent_sets(G) == 2
+
 
 def test_len2_path():
     G = nx.Graph()
@@ -21,31 +23,33 @@ def test_len2_path():
 
 def test_simple_path_tree():
     G = nx.Graph()
-    G.add_node(1)#2
-    G.add_node(2)#3
-    G.add_node(3)#5
-    G.add_node(4)#8
-    G.add_node(5)#13
+    G.add_node(1)  # 2
+    G.add_node(2)  # 3
+    G.add_node(3)  # 5
+    G.add_node(4)  # 8
+    G.add_node(5)  # 13
     G.add_edge(1, 2)
     G.add_edge(2, 3)
     G.add_edge(3, 4)
     G.add_edge(4, 5)
     assert num_independent_sets(G) == 13
 
+
 def test_simple_path_tree2():
     G = nx.Graph()
-    G.add_node(1)#2
-    G.add_node(2)#3
-    G.add_node(3)#5
-    G.add_node(4)#8
-    G.add_node(5)#13
-    G.add_node(6)#21
+    G.add_node(1)  # 2
+    G.add_node(2)  # 3
+    G.add_node(3)  # 5
+    G.add_node(4)  # 8
+    G.add_node(5)  # 13
+    G.add_node(6)  # 21
     G.add_edge(1, 2)
     G.add_edge(2, 3)
     G.add_edge(3, 4)
     G.add_edge(4, 5)
     G.add_edge(5, 6)
     assert num_independent_sets(G) == 21
+
 
 def test_binary_tree_of_depth_3():
     G = nx.Graph()
@@ -64,6 +68,7 @@ def test_binary_tree_of_depth_3():
     G.add_edge(3, 6)
     G.add_edge(3, 7)
     assert num_independent_sets(G) == 41
+
 
 def test_tree_of_depth_3():
     G = nx.Graph()
@@ -85,7 +90,8 @@ def test_tree_of_depth_3():
     G.add_edge(4, 8)
     assert num_independent_sets(G) == 61
 
-def test_tree_of_depth_4(): #add a node on top
+
+def test_tree_of_depth_4():  # add a node on top
     G = nx.Graph()
     G.add_node(0)
     G.add_node(1)
@@ -107,4 +113,3 @@ def test_tree_of_depth_4(): #add a node on top
     G.add_edge(3, 7)
     G.add_edge(4, 8)
     assert num_independent_sets(G) == 106
-
