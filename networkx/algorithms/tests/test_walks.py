133663690a591de2f575fee8b1578eba049b3e74
Dan Schult <dschult@colgate.edu>
2023-05-26 23:31:25 -0700

    use dense matrix power and fix walk_length definition

--- networkx/algorithms/tests/test_walks.py
+++ networkx/algorithms/tests/test_walks.py
@@ -8,26 +8,39 @@ pytest.importorskip("numpy")
 pytest.importorskip("scipy")
 
 
-def test_neither_source_nor_target():
+def test_directed():
     G = nx.DiGraph([(0, 1), (1, 2), (2, 0)])
     num_walks = nx.number_of_walks(G, 3)
     expected = {0: {0: 1, 1: 0, 2: 0}, 1: {0: 0, 1: 1, 2: 0}, 2: {0: 0, 1: 0, 2: 1}}
     assert num_walks == expected
 
 
-def test_no_target():
-    G = nx.cycle_graph(4)
-    source = 0
-    k = 3
-    num_walks = nx.number_of_walks(G, k)
-    expected = {0: 0, 1: 4, 2: 0, 3: 4}
-    assert num_walks[source] == expected
+def test_undirected():
+    G = nx.cycle_graph(3)
+    num_walks = nx.number_of_walks(G, 3)
+    expected = {0: {0: 2, 1: 3, 2: 3}, 1: {0: 3, 1: 2, 2: 3}, 2: {0: 3, 1: 3, 2: 2}}
+    assert num_walks == expected
 
 
-def test_source_and_target():
-    G = nx.DiGraph([("A", "B"), ("A", "C"), ("A", "D"), ("B", "D"), ("C", "D")])
-    source = "A"
-    target = "D"
-    k = 2
-    num_walks = nx.number_of_walks(G, k)
-    assert num_walks[source][target] == 2
+def test_non_integer_nodes():
+    G = nx.DiGraph([("A", "B"), ("B", "C"), ("C", "A")])
+    num_walks = nx.number_of_walks(G, 2)
+    expected = {
+        "A": {"A": 0, "B": 0, "C": 1},
+        "B": {"A": 1, "B": 0, "C": 0},
+        "C": {"A": 0, "B": 1, "C": 0}
+    }
+    assert num_walks == expected
+
+
+def test_zero_length():
+    G = nx.cycle_graph(3)
+    num_walks = nx.number_of_walks(G, 0)
+    expected = {0: {0: 1, 1: 0, 2: 0}, 1: {0: 0, 1: 1, 2: 0}, 2: {0: 0, 1: 0, 2: 1}}
+    assert num_walks == expected
+
+
+def test_negative_length_exception():
+    G = nx.cycle_graph(3)
+    with pytest.raises(ValueError):
+        nx.number_of_walks(G, -1)
--- networkx/algorithms/walks.py
+++ networkx/algorithms/walks.py
@@ -9,11 +9,17 @@ __all__ = ["number_of_walks"]
 
 
 def number_of_walks(G, walk_length):
-    """Returns the number of walks connecting each pair of nodes in
-    `G`.
+    """Returns the number of walks connecting each pair of nodes in `G`
 
     A *walk* is a sequence of nodes in which each adjacent pair of nodes
-    in the sequence is adjacent in the graph.
+    in the sequence is adjacent in the graph. A walk can repeat the same
+    edge and go in the opposite direction just as people can walk on a
+    set of paths, but standing still is not counted as part of the walk.
+
+    This function only counts the walks with `walk_length` edges. Note that
+    the number of nodes in the walk sequence is one more than `walk_length`.
+    The number of walks can grow very quickly on a larger graph
+    and with a larger walk length.
 
     Parameters
     ----------
@@ -37,16 +43,21 @@ def number_of_walks(G, walk_length):
     Examples
     --------
 
-    >>> G = nx.DiGraph([(0, 1), (1, 2)])
-    >>> nx.number_of_walks(G, 2)
-    {0: {0: 0, 1: 0, 2: 1}, 1: {0: 0, 1: 0, 2: 0}, 2: {0: 0, 1: 0, 2: 0}}
+    >>> G = nx.Graph([(0, 1), (1, 2)])
+    >>> walks = nx.number_of_walks(G, 2)
+    >>> walks
+    {0: {0: 1, 1: 0, 2: 1}, 1: {0: 0, 1: 2, 2: 0}, 2: {0: 1, 1: 0, 2: 1}}
+    >>> total_walks = sum(sum(tgts.values()) for _, tgts in walks.items())
 
-    You can also reach number of walks from a specific source node using the returned dictionary.
-    For example, number of walks of length 1 from node 0 can be found as follows:
+    You can also get the number of walks from a specific source node using the
+    returned dictionary. For example, number of walks of length 1 from node 0
+    can be found as follows:
 
     >>> walks = nx.number_of_walks(G, 1)
     >>> walks[0]
     {0: 0, 1: 1, 2: 0}
+    >>> sum(walks[0].values())  # walks from 0 of length 1
+    1
 
     Similarly, a target node can also be specified:
 
@@ -54,8 +65,14 @@ def number_of_walks(G, walk_length):
     1
 
     """
+    import numpy as np
+
+    if walk_length < 0:
+        raise ValueError(f"`walk_length` cannot be negative: {walk_length}")
+
     A = nx.adjacency_matrix(G)
-    power = A**walk_length
+    #power = sp.sparse.linalg.matrix_power(A, walk_length)
+    power = np.linalg.matrix_power(A.toarray(), walk_length)
     result = {
         u: {v: power[u_idx, v_idx] for v_idx, v in enumerate(G)}
         for u_idx, u in enumerate(G)
