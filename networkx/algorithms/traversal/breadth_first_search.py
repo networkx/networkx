af0a6e22fe13226174ad970b41d08f34d2742377
Anthony Labarre <48905616+alabarre@users.noreply.github.com>
2023-05-28 17:49:56 +0200

    Added girth computation function (#6633)
    * added girth computation function
    
    * added tests for girth
    
    * removed useless method and added seed to random call as recommended
    
    * Add numpydoc reference formatting to docstring.
    
    * Apply suggestions from code review
    
    * Update networkx/algorithms/tests/test_cycles.py
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>
    
    * Update networkx/algorithms/tests/test_cycles.py
    
    * Blacken.
    
    * Add girth fn to reference.
    
    * avoid frozenset and include other comments
    
    * fix import order
    
    * performance improvement to girth; added test for isolated nodes
    
    * Update networkx/algorithms/cycles.py
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>
    
    * took Dan's suggestion to use nx.predecessor to obviate nx.single_source...
    
    * reordered computation to only find one min-length (even/odd) cycle per root
    
    * copying is no longer worthwhile for runtime
    
    * rewrote girth for speed and hopefully clarity
    * added new helper function bfs_labeled_edges
    * added support for even girth and odd girth
    
    * doctest typos
    
    * doctest fix
    
    * doctests were a mistake
    
    * removed faulty parity argument
    
    * patched up un-tested unit test
    
    * unified directed & undirected bfs_labeled_edges
    
    * touchup on bfs_labeled_edges; added _dispatch
    
    * more performance improvements
    
    ---------
    
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>
    Co-authored-by: Mridul Seth <mail@mriduls.com>
    Co-authored-by: Dan Schult <dschult@colgate.edu>
    Co-authored-by: Kelly Boothby <boothby@dwavesys.com>

--- networkx/algorithms/traversal/breadth_first_search.py
+++ networkx/algorithms/traversal/breadth_first_search.py
@@ -1,4 +1,5 @@
 """Basic algorithms for breadth-first searching the nodes of a graph."""
+import math
 from collections import deque
 
 import networkx as nx
@@ -10,6 +11,7 @@ __all__ = [
     "bfs_successors",
     "descendants_at_distance",
     "bfs_layers",
+    "bfs_labeled_edges",
 ]
 
 
@@ -429,6 +431,87 @@ def bfs_layers(G, sources):
                     visited.add(child)
                     next_layer.append(child)
         current_layer = next_layer
+
+
+REVERSE_EDGE = "reverse"
+TREE_EDGE = "tree"
+FORWARD_EDGE = "forward"
+LEVEL_EDGE = "level"
+
+
+@nx._dispatch
+def bfs_labeled_edges(G, sources):
+    """Iterate over edges in a breadth-first search (BFS) labeled by type.
+
+    We generate triple of the form (*u*, *v*, *d*), where (*u*, *v*) is the
+    edge being explored in the breadth-first search and *d* is one of the
+    strings 'tree', 'forward', 'level', or 'reverse'.  A 'tree' edge is one in
+    which *v* is first discovered and placed into the layer below *u*.  A
+    'forward' edge is one in which *u* is on the layer above *v* and *v* has
+    already been discovered.  A 'level' edge is one in which both *u* and *v*
+    occur on the same layer.  A 'reverse' edge is one in which *u* is on a layer
+    below *v*.
+
+    We emit each edge exactly once.  In an undirected graph, 'reverse' edges do
+    not occur, because each is discovered either as a 'tree' or 'forward' edge.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        A graph over which to find the layers using breadth-first search.
+
+    sources : node in `G` or list of nodes in `G`
+        Starting nodes for single source or multiple sources breadth-first search
+
+    Yields
+    ------
+    edges: generator
+       A generator of triples (*u*, *v*, *d*) where (*u*, *v*) is the edge being
+       explored and *d* is described above.
+
+    Examples
+    --------
+    >>> G = nx.cycle_graph(4, create_using = nx.DiGraph)
+    >>> list(nx.bfs_labeled_edges(G, 0))
+    [(0, 1, 'tree'), (1, 2, 'tree'), (2, 3, 'tree'), (3, 0, 'reverse')]
+    >>> G = nx.complete_graph(3)
+    >>> list(nx.bfs_labeled_edges(G, 0))
+    [(0, 1, 'tree'), (0, 2, 'tree'), (1, 2, 'level')]
+    >>> list(nx.bfs_labeled_edges(G, [0, 1]))
+    [(0, 1, 'level'), (0, 2, 'tree'), (1, 2, 'forward')]
+    """
+    if sources in G:
+        sources = [sources]
+
+    neighbors = G._adj
+    directed = G.is_directed()
+    visited = set()
+    visit = visited.discard if directed else visited.add
+    # We use visited in a negative sense, so the visited set stays empty for the
+    # directed case and level edges are reported on their first occurrence in
+    # the undirected case.  Note our use of visited.discard -- this is built-in
+    # thus somewhat faster than a python-defined def nop(x): pass
+    depth = {s: 0 for s in sources}
+    queue = deque(depth.items())
+    push = queue.append
+    pop = queue.popleft
+    while queue:
+        u, du = pop()
+        for v in neighbors[u]:
+            if v not in depth:
+                depth[v] = dv = du + 1
+                push((v, dv))
+                yield u, v, TREE_EDGE
+            else:
+                dv = depth[v]
+                if du == dv:
+                    if v not in visited:
+                        yield u, v, LEVEL_EDGE
+                elif du < dv:
+                    yield u, v, FORWARD_EDGE
+                elif directed:
+                    yield u, v, REVERSE_EDGE
+        visit(u)
 
 
 @nx._dispatch
