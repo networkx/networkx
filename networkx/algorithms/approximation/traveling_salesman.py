a56309ce2a9ede8e7bbb55de3650373438daf04e
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-18 12:24:56 -0400

    spelling: arborescence
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- networkx/algorithms/approximation/traveling_salesman.py
+++ networkx/algorithms/approximation/traveling_salesman.py
@@ -643,7 +643,7 @@ def held_karp_ascent(G, weight="weight"):
         for n in G:
             d[n] = 0
         del n
-        # 2. Find a 1-Aborescence T^k such that k is in K(pi, d).
+        # 2. Find a 1-Arborescence T^k such that k is in K(pi, d).
         minimum_1_arborescences = k_pi()
         while True:
             # Reduce K(pi) to K(pi, d)
