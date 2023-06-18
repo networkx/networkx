a7b48398a69e3ae279ad03e2cf3c00f1b879a525
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-18 12:24:57 -0400

    spelling: ancestors
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- networkx/algorithms/tests/test_dag.py
+++ networkx/algorithms/tests/test_dag.py
@@ -746,7 +746,7 @@ class TestDagToBranching:
 
 
 def test_ancestors_descendants_undirected():
-    """Regression test to ensure anscestors and descendants work as expected on
+    """Regression test to ensure ancestors and descendants work as expected on
     undirected graphs."""
     G = nx.path_graph(5)
     nx.ancestors(G, 2) == nx.descendants(G, 2) == {0, 1, 3, 4}
