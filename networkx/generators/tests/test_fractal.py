ae5c57ede7a1563c94247162fd2d75e00ec53d4c
Paul Brodersen <paulbrodersen@gmail.com>
2023-06-22 16:24:39 +0100

    Additional code formatting as the linter cannot make up its mind.

--- networkx/generators/tests/test_fractal.py
+++ networkx/generators/tests/test_fractal.py
@@ -14,9 +14,7 @@ def test_laakso_graph(create_using):
     G = nx.laakso_graph(1, create_using)
     H = nx.from_edgelist([(0, 1)], create_using)
     assert nx.is_isomorphic(G, H)
-    returned_pos = {
-        tuple(arr) for arr in nx.get_node_attributes(G, "pos").values()
-    }
+    returned_pos = {tuple(arr) for arr in nx.get_node_attributes(G, "pos").values()}
     expected_pos = {(0, 0), (0, 1)}
     assert returned_pos == expected_pos
 
@@ -30,13 +28,18 @@ def test_laakso_graph(create_using):
             (3, 4),
             (4, 5),
         ],
-        create_using
+        create_using,
     )
     assert nx.is_isomorphic(G, H)
-    returned_pos = {
-        tuple(arr) for arr in nx.get_node_attributes(G, "pos").values()
+    returned_pos = {tuple(arr) for arr in nx.get_node_attributes(G, "pos").values()}
+    expected_pos = {
+        (-0.25, 0.5),
+        (0.0, 0.0),
+        (0.0, 0.25),
+        (0.0, 0.75),
+        (0.0, 1.0),
+        (0.25, 0.5),
     }
-    expected_pos = {(-0.25, 0.5), (0.0, 0.0), (0.0, 0.25), (0.0, 0.75), (0.0, 1.0), (0.25, 0.5)}
     assert returned_pos == expected_pos
 
 
@@ -48,10 +51,8 @@ def test_sierpinski_gasket_graph(create_using):
     G = nx.sierpinski_gasket_graph(1, create_using)
     H = nx.from_edgelist([(0, 1), (1, 2), (2, 0)], create_using)
     assert nx.is_isomorphic(G, H)
-    returned_pos = {
-        tuple(arr) for arr in nx.get_node_attributes(G, "pos").values()
-    }
-    expected_pos = {(0.0, 0.0), (1.0, 0.0), (0.5, np.sqrt(3)/2)}
+    returned_pos = {tuple(arr) for arr in nx.get_node_attributes(G, "pos").values()}
+    expected_pos = {(0.0, 0.0), (1.0, 0.0), (0.5, np.sqrt(3) / 2)}
     assert returned_pos == expected_pos
 
     G = nx.sierpinski_gasket_graph(2, create_using)
@@ -67,7 +68,7 @@ def test_sierpinski_gasket_graph(create_using):
             (4, 5),
             (5, 1),
         ],
-        create_using
+        create_using,
     )
     assert nx.is_isomorphic(G, H)
     returned_pos = {tuple(arr) for arr in nx.get_node_attributes(G, "pos").values()}
@@ -77,7 +78,7 @@ def test_sierpinski_gasket_graph(create_using):
         (0.5, np.sqrt(3) / 2),
         (-0.5, -np.sqrt(3) / 2),
         (0.5, -np.sqrt(3) / 2),
-        (1.5, -np.sqrt(3) / 2)
+        (1.5, -np.sqrt(3) / 2),
     }
     # normalize
     expected_pos = {(x / 2, (y + np.sqrt(3) / 2) / 2) for (x, y) in expected_pos}
