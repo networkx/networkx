8e7955503ca472dc6782ec8c709e6735f1c5b1b8
evgepab <t8200129@aueb.gr>
2023-06-23 15:25:10 +0300

    Add more tests for arbitary time values

--- networkx/algorithms/tests/test_time_dependent.py
+++ networkx/algorithms/tests/test_time_dependent.py
@@ -45,6 +45,76 @@ class TestCdIndex:
 
         assert nx.cd_index(G, 4) == 0.17
 
+    def test_common_graph_with_int_attributes(self):
+        G = nx.DiGraph()
+        G.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        G.add_edge(4, 2)
+        G.add_edge(4, 0)
+        G.add_edge(4, 1)
+        G.add_edge(4, 3)
+        G.add_edge(5, 2)
+        G.add_edge(6, 2)
+        G.add_edge(6, 4)
+        G.add_edge(7, 4)
+        G.add_edge(8, 4)
+        G.add_edge(9, 4)
+        G.add_edge(9, 1)
+        G.add_edge(9, 3)
+        G.add_edge(10, 4)
+
+        node_attrs = {
+            0: {"time": 20},
+            1: {"time": 20},
+            2: {"time": 30},
+            3: {"time": 30},
+            4: {"time": 50},
+            5: {"time": 70},
+            6: {"time": 80},
+            7: {"time": 90},
+            8: {"time": 90},
+            9: {"time": 80},
+            10: {"time": 74},
+        }
+
+        nx.set_node_attributes(G, node_attrs)
+
+        assert nx.cd_index(G, 4, 50) == 0.17
+
+    def test_common_graph_with_float_attributes(self):
+        G = nx.DiGraph()
+        G.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        G.add_edge(4, 2)
+        G.add_edge(4, 0)
+        G.add_edge(4, 1)
+        G.add_edge(4, 3)
+        G.add_edge(5, 2)
+        G.add_edge(6, 2)
+        G.add_edge(6, 4)
+        G.add_edge(7, 4)
+        G.add_edge(8, 4)
+        G.add_edge(9, 4)
+        G.add_edge(9, 1)
+        G.add_edge(9, 3)
+        G.add_edge(10, 4)
+
+        node_attrs = {
+            0: {"time": 20.2},
+            1: {"time": 20.2},
+            2: {"time": 30.7},
+            3: {"time": 30.7},
+            4: {"time": 50.9},
+            5: {"time": 70.1},
+            6: {"time": 80.6},
+            7: {"time": 90.7},
+            8: {"time": 90.7},
+            9: {"time": 80.6},
+            10: {"time": 74.2},
+        }
+
+        nx.set_node_attributes(G, node_attrs)
+
+        assert nx.cd_index(G, 4, 50) == 0.17
+
     def test_common_graph_with_weights(self):
         G = nx.DiGraph()
         G.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
@@ -177,6 +247,41 @@ class TestCdIndex:
         with pytest.raises(ValueError, match="The cd index cannot be defined.") as ve:
             nx.cd_index(G, 4)
 
+    def test_time_timedelta_compatibility(self):
+        G = nx.DiGraph()
+        G.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        G.add_edge(4, 2)
+        G.add_edge(4, 0)
+        G.add_edge(4, 3)
+        G.add_edge(6, 4)
+        G.add_edge(7, 4)
+        G.add_edge(8, 4)
+        G.add_edge(9, 4)
+        G.add_edge(9, 1)
+        G.add_edge(10, 4)
+
+        node_attrs = {
+            0: {"time": 20.2},
+            1: {"time": 20.2},
+            2: {"time": 30.7},
+            3: {"time": 30.7},
+            4: {"time": 50.9},
+            5: {"time": 70.1},
+            6: {"time": 80.6},
+            7: {"time": 90.7},
+            8: {"time": 90.7},
+            9: {"time": 80.6},
+            10: {"time": 74.2},
+        }
+
+        nx.set_node_attributes(G, node_attrs)
+
+        with pytest.raises(
+            ValueError,
+            match="Addition and comparison are not supported between time_delta and time attribute types, default time_delta = datetime.timedelta",
+        ) as ve:
+            nx.cd_index(G, 4)
+
     def test_node_with_no_time(self):
         G = nx.DiGraph()
         G.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
@@ -209,7 +314,7 @@ class TestCdIndex:
         nx.set_node_attributes(G, node_attrs)
 
         with pytest.raises(
-            ValueError, match="Not all nodes have a datetime 'time' attribute."
+            ValueError, match="Not all nodes have a 'time' attribute."
         ) as ve:
             nx.cd_index(G, 4)
 
