d929c045dd52bd05a506ac72458599a30ba6c15e
Paul Brodersen <paulbrodersen@gmail.com>
2023-06-26 11:21:04 +0100

    Add generators/fractal.py to list of numpy dependees

--- networkx/conftest.py
+++ networkx/conftest.py
@@ -154,6 +154,7 @@ needs_numpy = [
     "linalg/laplacianmatrix.py",
     "utils/misc.py",
     "algorithms/centrality/laplacian.py",
+    "generators/fractal.py",
 ]
 needs_scipy = [
     "algorithms/approximation/traveling_salesman.py",
