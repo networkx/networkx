026a64d62cd1690fd0080e66fb94e68b724e6aa7
Paul Brodersen <paulbrodersen@gmail.com>
2023-06-26 12:37:55 +0100

    Don't import networkx in doc tests

--- networkx/generators/fractal.py
+++ networkx/generators/fractal.py
@@ -52,7 +52,6 @@ def laakso_graph(k, create_using=nx.Graph):
     Create and plot a Laakso graph order 5:
 
     >>> import matplotlib.pyplot as plt
-    >>> import networkx as nx
     >>> G = nx.laakso_graph(5)
     >>> pos = nx.get_node_attributes(G, "pos")
     >>> fig, ax = plt.subplots()
@@ -135,7 +134,6 @@ def sierpinski_gasket_graph(n, create_using=nx.Graph):
     Create and plot a Sierpinski gasket graph order 5:
 
     >>> import matplotlib.pyplot as plt
-    >>> import networkx as nx
     >>> G = nx.sierpinski_gasket_graph(5)
     >>> pos = nx.get_node_attributes(G, "pos")
     >>> fig, ax = plt.subplots()
