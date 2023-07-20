ac96644d6a36123dc0c85b85b6bbef21d8012bc5
Erik Welch <erik.n.welch@gmail.com>
2023-07-19 22:07:37 -0500

    Use default of 0 for "flow" attribute in edmonds_karp_core

--- networkx/algorithms/flow/edmondskarp.py
+++ networkx/algorithms/flow/edmondskarp.py
@@ -10,7 +10,7 @@ __all__ = ["edmonds_karp"]
 
 @nx._dispatch(
     graphs="R",
-    preserve_edge_attrs={"R": {"capacity": float("inf"), "flow": None}},
+    preserve_edge_attrs={"R": {"capacity": float("inf"), "flow": 0}},
     preserve_graph_attrs=True,
 )
 def edmonds_karp_core(R, s, t, cutoff):
