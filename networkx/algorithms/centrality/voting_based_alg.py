7c1d27a02aedb1c6f16eec155b5253541452a836
Christian Laußmann <christian@laussmann.me>
2023-07-20 12:27:40 +0200

    UPDATE: Descriptions, References

--- networkx/algorithms/centrality/voting_based_alg.py
+++ networkx/algorithms/centrality/voting_based_alg.py
@@ -4,23 +4,29 @@ centrality using voting methods from Computational Social Choice.
 
 import itertools
 import networkx as nx
-from networkx.utils import not_implemented_for
 
 __all__ = ["sav_voting", "copeland_voting", "spav_voting"]
 
 
-@not_implemented_for("directed")
+@nx.not_implemented_for("directed")
+@nx.not_implemented_for("multigraph")
 def sav_voting(G):
     """Rank nodes using Satisfaction Approval Voting (SAV).
 
-    SAV [1]_ [2]_ lets each node vote on the centrality of its neighbors. Thereby, each node x
-    contributes 1/|degree(x)| points to the total score of each neighbor. In other words: node
-    x distributes a total of 1 point equally to all neighbors.
-    
-    One interpretation is that a node is more central according to SAV if it has many neighbors
-    which highly rely on the connection to this node (they have no, or only a few, other neighbors).
+    SAV centrality (introduced in [2]_ and based on the framework in [3]_ ) lets each node vote
+    on the centrality of its neighbors. Thereby, each node x contributes 1/|degree(x)| points
+    to the total score of each neighbor. In other words: node x distributes a total of 1 point
+    equally to all neighbors. One interpretation is that a node is more central according to
+    SAV centrality if it has many neighbors which highly rely on the connection to this node
+    (they have no, or only a few, other neighbors).
 
-    SAV only works with undirected networks. Weights are ignored.
+    For a detailed description of the SAV voting system which is the base of this centrality
+    method, and a study on its properties, see [1]_ and
+    https://en.wikipedia.org/wiki/Satisfaction_approval_voting
+
+    Notes
+    -----
+    SAV centrality only works with undirected networks (no multigraph). Weights are ignored.
 
     Parameters
     ----------
@@ -44,16 +50,22 @@ def sav_voting(G):
 
     References
     ----------
-    .. [1] Brams, S. and Kilgour, D. (2014).
-        Satisfaction Approval Voting.
+    .. [1] Brams, S. J. and Kilgour, D. M. (2014).
+        "Satisfaction Approval Voting"
         In: Voting Power and Procedures.
         Publisher: Springer
         Pages 323-346.
+        https://link.springer.com/chapter/10.1007/978-3-319-05158-1_18
        [2] Laussmann, C. (2023).
-        Network Centrality Through Voting Rules.
+        "Network Centrality Through Voting Rules"
         In: COMSOC Methods in Real-World Applications (Dissertation).
         Publisher: Universitaets- und Landesbibliothek der Heinrich-Heine-Universitaet Duesseldorf
         Pages 27-45.
+       [3] Brandes, U. and Laussmann, C. and Rothe, J. (2022).
+        "Voting for Centrality (Extended Abstract)"
+        In: Proceedings of the 21st International Conference on Autonomous Agents and Multiagent Systems (AAMAS)
+        Publisher: IFAAMAS
+        https://www.ifaamas.org/Proceedings/aamas2022/pdfs/p1554.pdf
     """
 
     # Compute Scores
@@ -68,27 +80,33 @@ def sav_voting(G):
 
 
 
-@not_implemented_for("directed")
+@nx.not_implemented_for("directed")
+@nx.not_implemented_for("multigraph")
 def copeland_voting(G):
     """Rank nodes using the Condorcet-consistent voting rule due to Copeland.
 
     According to Condorcet, a candidate should win an election whenever it is preferred
     to each other candidate by a majority of voters in direct comparison. Although this
     is a very reasonable criterion, it doesn't tell us what to do when such a candidate
-    doesn't exist (which happens most of the time). Arthur Copeland described a method
-    which selects the Condorcet-winner whenever it exists, but chooses a winner which
-    is close to being a Condorcet winner otherwise.
+    doesn't exist (which happens most of the time). Arthur Copeland (1951) described a
+    method which selects the Condorcet-winner whenever it exists, but chooses a candidate
+    which is close to being a Condorcet winner otherwise.
 
-    This centrality index is based on Copelands method. Each node is compared with every
+    This centrality index is based on Copeland's method. Each node is compared with every
     other node in direct comparison. Say nodes A and B are compared. If there are more
     nodes strictly closer to A than to B, A receives one point (wins the pairwise
     comparison), and B loses one point. The same holds vice versa when B wins the pairwise
     comparison. If A and B are tied (neither A nor B wins), both receive no points. The
     Copeland centrality index is achieved by summing up all the points from all comparisons.
     
+    The Copeland centrality index was introduced in [1]_ . Although the Copeland voting
+    method is mostly known under Copeland's name, it dates back at least to Llull (1299).
+    See also https://en.wikipedia.org/wiki/Copeland%27s_method
+
     Notes
     -----
-    The Copeland centrality index was introduced in [1]_ .
+    Copeland centrality only works with undirected networks (no multigraph).
+    Weights are ignored.
 
     Parameters
     ----------
@@ -113,9 +131,10 @@ def copeland_voting(G):
     References
     ----------
     .. [1] Brandes, U. and Laussmann, C. and Rothe, J. (2022).
-        Voting for Centrality (Extended Abstract).
-        Proceedings of the 21st International Conference on Autonomous Agents and Multiagent Systems (AAMAS)
+        "Voting for Centrality (Extended Abstract)"
+        In: Proceedings of the 21st International Conference on Autonomous Agents and Multiagent Systems (AAMAS)
         Publisher: IFAAMAS
+        https://www.ifaamas.org/Proceedings/aamas2022/pdfs/p1554.pdf
     """
 
     shortest_paths_len = dict()
@@ -142,19 +161,29 @@ def copeland_voting(G):
     return scores
 
 
-@not_implemented_for("directed")
+@nx.not_implemented_for("directed")
+@nx.not_implemented_for("multigraph")
 def spav_voting(G, number_of_nodes=None, voting_ability_fn=None):
     """Select a set of influential nodes using Sequential Proportional Approval Voting (SPAV).
 
-    SPAV [1]_ [2]_ lets each node vote on the centrality of its neighbors in multiple rounds. In each
-    round, the node with the highest voting score is elected where each node contributes the same
-    points to each of its neighbors. Thereby, whenever a node is elected, all neighbors' voting ability
-    is reduced so that in the next round they contribute less points to their neighbors (similar to VoteRank).
-    By how far the voting ability is reduced depends on the voting_ability_fn, which maps an integer
-    (how many nodes from the neighborhood of a "voter" are already selected) to a real number (voting ability).
-    By default, voting_ability_fn(0) = 1, voting_ability_fn(1) = 1/2, voting_ability_fn(2) = 1/3, etc., is
-    used, as it war originally proposed by Thiele in 1895.
+    SPAV centrality (introduced in [2]_ and based on the framework in [3]_ ) lets each node vote
+    on the centrality of its neighbors in multiple rounds. In each round, the node with the highest
+    voting score is elected where each node contributes the same points to each of its neighbors.
+    Thereby, whenever a node is elected, all neighbors' voting ability is reduced so that in the
+    next round they contribute less points to their neighbors (similar to VoteRank). By how far the
+    voting ability is reduced depends on the voting_ability_fn, which maps an integer (how many nodes
+    from the neighborhood of a "voter" are already selected) to a real number (voting ability).
+    By default, voting_ability_fn(0) = 1, voting_ability_fn(1) = 1/2, voting_ability_fn(2) = 1/3,
+    etc., is used.
+    
+    The original voting method SPAV was first proposed by Thorvald Thiele around 1900. For an
+    introduction, and astudy on SPAV's properties, see e.g. [1]_ as well as
+    https://en.wikipedia.org/wiki/Sequential_proportional_approval_voting
 
+    Notes
+    -----
+    SPAV centrality only works with undirected networks (no multigraph).
+    Weights are ignored.
 
     Parameters
     ----------
@@ -183,17 +212,30 @@ def spav_voting(G, number_of_nodes=None, voting_ability_fn=None):
     >>> nx.spav_voting(G, number_of_nodes=2)
     [1, 7]
 
+    >>> G = nx.Graph([(1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (7, 6), (7, 8), (7, 9), (10, 6), (10, 1),])
+    >>> nx.spav_voting(G, number_of_nodes=2, voting_ability_fn=lambda x : 1 if x==0 else 0)
+    [6, 7]
+
     References
     ----------
     .. [1] Aziz, H. et al. (2017).
-        Justified representation in approval-based committee voting.
+        "Justified Representation in Approval-based Committee Voting"
         In: Social Choice and Welfare 48
         Pages 461-485.
        [2] Laussmann, C. (2023).
-        Network Centrality Through Voting Rules.
+        "Network Centrality Through Voting Rules"
         In: COMSOC Methods in Real-World Applications (Dissertation).
         Publisher: Universitaets- und Landesbibliothek der Heinrich-Heine-Universitaet Duesseldorf
         Pages 27-45.
+       [3] Brandes, U. and Laussmann, C. and Rothe, J. (2022).
+        "Voting for Centrality (Extended Abstract)"
+        In: Proceedings of the 21st International Conference on Autonomous Agents and Multiagent Systems (AAMAS)
+        Publisher: IFAAMAS
+        https://www.ifaamas.org/Proceedings/aamas2022/pdfs/p1554.pdf
+       [1] Zhang, J.-X. et al. (2016).
+        "Identifying a set of influential spreaders in complex networks."
+        In: Sci. Rep. 6, 27823.
+        https://doi.org/10.1038/srep27823
     """
 
     # Set default values
