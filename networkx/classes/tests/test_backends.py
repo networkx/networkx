588326364848a165b045e419a63a4e62d568c615
Erik Welch <erik.n.welch@gmail.com>
2023-08-24 16:21:52 -0500

    Change `_dispatch` to a class instead of a closure (#6840)
    * Change `_dispatch` to a class instead of a closure
    
    * pickle pickle!
    
    * Support `can_run(name, args, kwargs)` function for backends
    
    - Remove `is_testing=` keyword in `convert_from_nx` (for now)
    - Delay computing `__signature__` of dispatched functions
    - Fix `name` -> `self.name` in f-string
    
    * Move dispatch type checking into constructor
    
    * Slightly faster import time
    
    * Fast path when dispatching if no backends are installed
    
    This requires "nx-loopback" to only be available when it's being tested.
    
    * Always include nx-loopback plugin when testing
    
    * Does this work? (trying to fix tests on Python <3.10)
    
    * This should work
    
    * better
    
    * Add `NETWORKX_AUTOMATIC_BACKENDS` to enable automatic conversions to backends
    
    `NETWORKX_AUTOMATIC_BACKENDS` is a comma-separated environment variable, so multiple
    backends can be specified in priority order.
    
    Rename `NETWORKX_TEST_FALLBACK_TO_NX` to `NETWORKX_FALLBACK_TO_NX`, since the latter
    can be used with `NETWORKX_AUTOMATIC_BACKENDS` environment variable.
    
    Refactor methods on `_dispatch` class:
    - add `_can_backend_run` helper function
      - should we expose this to users?
    - add `_convert_arguments` to convert `*args` and `**kwargs`
      - should we expose this to users? If so, should we use `*args` and `**kwargs` in the signature?
    - remove `is_testing=` keyword argument from `_convert_and_call`
    - add `_convert_and_call_for_tests` to use when testing
    
    Also, add a few `# Future work:` code comments to be more clear about development progress.
    
    * haha, oops
    
    * Automagically add `backend=` keyword argument to dispatched functions
    
    * Clean up exception raising
    
    * Also catch `NotImplementedError` from converting graphs when dispatching

--- networkx/classes/tests/test_backends.py
+++ networkx/classes/tests/test_backends.py
@@ -1,3 +1,5 @@
+import pickle
+
 import pytest
 
 import networkx as nx
@@ -12,3 +14,9 @@ def test_dispatch_kwds_vs_args():
     nx.pagerank(G=G)
     with pytest.raises(TypeError):
         nx.pagerank()
+
+
+def test_pickle():
+    for name, func in nx.utils.backends._registered_algorithms.items():
+        assert pickle.loads(pickle.dumps(func)) is func
+    assert pickle.loads(pickle.dumps(nx.inverse_line_graph)) is nx.inverse_line_graph
