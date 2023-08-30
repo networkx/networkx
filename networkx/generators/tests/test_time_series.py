1d60a7fb097c6c4fd6ec121058273484e75430e6
Eran Rivlis <eran.rivlis@yahoo.com>
2023-08-30 23:53:39 +0300

    refactor unit tests

--- networkx/generators/tests/test_time_series.py
+++ networkx/generators/tests/test_time_series.py
@@ -1,30 +1,30 @@
 """Unit tests for the :mod:`networkx.generators.time_series` module."""
-import networkx
+import itertools
+
+import networkx as nx
 
 
-class TestVisibilityGraph:
-    """Unit tests for :func:`networkx.generators.time_series.visibility_graph`"""
+def test_visibility_graph_static_series():
+    # arrange
+    series = range(10)
+    expected_series_length = len(series)
 
-    def test_static_series(self):
-        # arrange
-        series = range(10)
-        expected_series_length = len(series)
+    # act
+    actual_graph = nx.visibility_graph(series)
 
-        # act
-        actual_graph = networkx.generators.time_series.visibility_graph(series)
+    # assert
+    assert actual_graph.number_of_nodes() == expected_series_length
+    assert actual_graph.number_of_edges() == 9
 
-        # assert
-        assert actual_graph.number_of_nodes() == expected_series_length
-        assert actual_graph.number_of_edges() == 9
 
-    def test_cyclic_series(self):
-        # arrange
-        series = [2, 1, 3, 2, 1, 3, 2, 1, 3, 2, 1, 3]
-        expected_series_length = len(series)
+def test_visibility_graph_cyclic_series():
+    # arrange
+    series = list(itertools.islice(itertools.cycle((2, 1, 3)), 12))
+    expected_series_length = len(series)
 
-        # act
-        actual_graph = networkx.generators.time_series.visibility_graph(series)
+    # act
+    actual_graph = nx.visibility_graph(series)
 
-        # assert
-        assert actual_graph.number_of_nodes() == expected_series_length
-        assert actual_graph.number_of_edges() == 18
+    # assert
+    assert actual_graph.number_of_nodes() == expected_series_length
+    assert actual_graph.number_of_edges() == 18
