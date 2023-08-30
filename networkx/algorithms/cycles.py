4f9905f0aca3d219b74eda6d7e681aa3f8ecb171
Mridul Seth <seth.mridul@gmail.com>
2023-08-30 22:04:45 +0400

    Revert "Clarify that basis generates simple cycles only" (#6885)
    Revert "Clarify that basis generates simple cycles only (#6882)"
    
    This reverts commit 7febe9690adfce0f4922f58bc42420a309da6bdf.

--- networkx/algorithms/cycles.py
+++ networkx/algorithms/cycles.py
@@ -29,11 +29,11 @@ def cycle_basis(G, root=None):
     """Returns a list of cycles which form a basis for cycles of G.
 
     A basis for cycles of a network is a minimal collection of
-    cycles  such that any  simple cycle in  the network can be
-    written as a sum of cycles in the basis. Here summation of
-    cycles  is defined as "exclusive or" of  the edges.  Cycle
-    bases   are  useful,  e.g.  when  deriving  equations  for
-    electric circuits using Kirchhoff's Laws.
+    cycles such that any cycle in the network can be written
+    as a sum of cycles in the basis.  Here summation of cycles
+    is defined as "exclusive or" of the edges. Cycle bases are
+    useful, e.g. when deriving equations for electric circuits
+    using Kirchhoff's Laws.
 
     Parameters
     ----------
