b6498eabd1fb1cfe948fb075f7de58997bd2779c
OrionSehn <OTSehn@pcl.com>
2023-08-30 12:05:54 -0600

    added additional comments

--- networkx/algorithms/shortest_paths/ada_star.py
+++ networkx/algorithms/shortest_paths/ada_star.py
@@ -1,146 +1,240 @@
 import math
-import networkx as nx
 import random
 import time
+import networkx as nx
 import numpy as np
 
 __all__ = ["ADA_star"]
 
 class ADA_star:
+    """A dynamic anytime path planning algorithm.
+    
+    Class is responsible for the implementation of the Dynamic Anytime
+    A* algorithm. This algorithm returns a path from a start state to a
+    goal state. The algorithm is able to return a suboptimal path in a
+    short amount of time. The algorithm is able to iteratively improve 
+    the path over time by calling the compute_or_improve_path function.
+    The algorithm is also able to cope with dynamic environments by 
+    calling the update_graph function, which updates the graph with new
+    weights. The algorithm makes use of previous work done when there
+    are changes to the graph.
+
+    Creates a search object, which can be used to find a path from a
+    start state to a goal state in a graph. The search object can be 
+    used to iteratively improve the path over time, as well as being
+    called to update the graph with new weights. The search object, will
+    then use previous work to find a new path in the updated graph
+    
+    Parameters
+    ----------
+    s_start : node
+        Starting node for the path
+    
+    s_goal : node
+        Goal node for the path
+    
+    G : networkx graph
+        Graph to find the path in
+
+    heursistic : function
+        A function to evaluate the estimate of the distance
+    from the a node to the target.  The function takes
+    two node labels as arguments and must return a number.
+    If the heuristic is inadmissible (if it might
+    overestimate the cost of reaching the goal from a node),
+    the result may not be a shortest path.
+
+    weight : string, default='weight'
+        The edge weights will be accessed via the edge attribute with 
+    this key (that is, the weight of the edge joining `u` to `v` 
+    will be ``G.edges[u, v][weight]``). If no such edge attribute
+    exists, the weight of the edge is assumed to be one.
+
+    initial_epsilon : float, default=1000
+        The suboptimality bound, epsilon >= 1. The algorithm will
+    return a path that is suboptimal by this bound. The algorithm
+    will return a path that is suboptimal by this bound. The path
+    is accessable through the extract_path function.
+
+
+    Methods
+    -------
+    compute_or_improve_path(epsilon)
+        Compute or improve the path from the start state to the goal state.
+
+    update_graph(changes)
+        Update the graph with new edge weights
+
+    extract_path()
+        Extract the path based on current potentially suboptimal solution.
+        
+
+    Raises
+    ------
+    NetworkXNoPath
+        If no path exists between source and target.
+    NodeNotFound
+        If either source or target is not in G.
+
+    See Also
+    --------
+    shortest_path, astar_path, dijkstra_path    
+
+    References
+    ----------
+    [1] Likhachev, Maxim, David I. Ferguson, Geoffrey J. Gordon, Anthony
+    Stentz, and Sebastian Thrun. "Anytime dynamic A*: An anytime,
+    replanning algorithm." In ICAPS, vol. 5, pp. 262-271. 2005.
+
+    Examples
+    --------
+    >>> random.seed(1)
+    >>> G = nx.random_geometric_graph(100, 0.20, seed=896803)
+    >>> for (u, v, w) in G.edges(data=True): #Euclidean distance between nodes
+    ...     w['weight'] = np.sqrt((G.nodes[v]["pos"][0] - G.nodes[u]["pos"][0])**2 + (G.nodes[v]["pos"][1] - G.nodes[u]["pos"][1])**2)
+    >>> s_start, s_goal = 42, 25       
+    >>> def heursistic(u, v): #Euclidean distance between nodes
+    >>> return np.sqrt((G.nodes[v]["pos"][0] - G.nodes[u]["pos"][0])**2 + (G.nodes[v]["pos"][1] - G.nodes[u]["pos"][1])**2)
+        
+    >>> # A* search for comparison
+    >>> start_time = time.time()
+    >>> path = nx.astar_path(G, s_start, s_goal, heursistic)
+    >>> print("A* time: ", time.time() - start_time)
+    A* time:  0.00012803077697753906
+    >>> print("A* path: ", path)
+    A* path:  [42, 32, 19, 72, 49, 29, 31, 94, 35, 25]
+
+    >>> #create search object
+    >>> search = ADA_star(s_start, s_goal, G, heursistic)
+
+    >>> #compute first suboptimal path epsilon = 2
+    >>> start_time = time.time()
+    >>> search.compute_or_improve_path(epsilon=2)
+    >>> path = search.extract_path()
+    >>> print("ADA* epsilon = 2 time: ", time.time() - start_time)
+    ADA* epsilon = 2 time:  0.0009984970092773438
+    >>> print("epsilon = 2 path: ", path)
+    epsilon = 2 path:  [42, 32, 24, 40, 59, 4, 66, 27, 35, 25]
+    >>> print("epsilon = 2 path_weight: ", nx.path_weight(G, search.extract_path(), "weight"))
+    epsilon = 2 path_weight:  1.4679609830956495
+
+    >>> #compute second (better) suboptimal path
+    >>> search.compute_or_improve_path(epsilon=1.2)
+    >>> path = search.extract_path()
+    >>> print("epsilon = 1.2 path: ", path)
+    epsilon = 1.2 path:  [42, 32, 24, 12, 59, 4, 1, 27, 35, 25]
+    >>> print("epsilon = 1.2 path_weight: ", nx.path_weight(G, path, "weight"))
+    epsilon = 1.2 path_weight:  1.3335657361796027
+
+    >>> #compute third (best) suboptimal path
+    >>> search.compute_or_improve_path(epsilon=1)
+    >>> path = search.extract_path()
+    >>> print("epsilon = 1 path: ", path)
+    epsilon = 1 path:  [42, 32, 19, 72, 49, 29, 31, 94, 35, 25]
+    >>> print("epsilon = 1 path_weight: ", nx.path_weight(G, path, "weight"))
+    epsilon = 1 path_weight:  1.29129785933092
+
+    >>> #change graph edge weight
+    >>> print("changing graph weight for edge (49, 97)")
+    changing graph weight for edge (49, 97)
+    >>> search.update_graph([[49, 97, 0]]) #add edge between 77 and 15 with weight 0
+    >>> search.compute_or_improve_path(epsilon=1)
+    >>> path = search.extract_path()
+    >>> print("changed epsilon = 1 path: ", path)
+    changed epsilon = 1 path:  [42, 32, 19, 72, 49, 97, 11, 31, 94, 35, 25]
+    >>> print("changed epsilon = 1 path_weight: ", nx.path_weight(G, path, "weight"))
+    changed epsilon = 1 path_weight:  1.1428811257616596
+
+
+
+    """
+
     def __init__(self, s_start, s_goal, G, heursistic, weight = "weight", initial_epsilon = 1000):
-        """Returns a search object, which can be used to find a path from a
+        """ initializer for ADA* algorithm     
+
+        Returns a search object, which can be used to find a path from a
         start state to a goal state in a graph. The search object can be 
         used to iteratively improve the path over time, as well as being
         called to update the graph with new weights. The search object, will
         then use previous work to find a new path in the updated graph.
+
+
+        Raises
+        ------
+        NetworkXNoPath
+            If no path exists between source and target.
         
-        Class is responsible for the implementation of the Dynamic Anytime
-        A* algorithm. This algorithm returns a path from a start state to a
-        goal state. The algorithm is able to return a suboptimal path in a
-        short amount of time. The algorithm is able to iteratively improve 
-        the path over time by calling the compute_or_improve_path function.
-        The algorithm is also able to cope with dynamic environments by 
-        calling the update_graph function, which updates the graph with new
-        weights. The algorithm makes use of previous work done when there
-        are changes to the graph.
-
-
-        Parameters
-        ----------
-        s_start : node
-            Starting node for the path
+        NodeNotFound
+            If either source or target is not in G.
         
-        s_goal : node
-            Goal node for the path
-        
-        G : networkx graph
-            Graph to find the path in
 
-        heursistic : function
-            A function to evaluate the estimate of the distance
-        from the a node to the target.  The function takes
-        two node labels as arguments and must return a number.
-        If the heuristic is inadmissible (if it might
-        overestimate the cost of reaching the goal from a node),
-        the result may not be a shortest path.
 
-        weight : string
-            The edge weights will be accessed via the edge attribute with 
-        this key (that is, the weight of the edge joining `u` to `v` 
-        will be ``G.edges[u, v][weight]``). If no such edge attribute
-        exists, the weight of the edge is assumed to be one.
-        
-        Examples
-        --------
-        >>> random.seed(1)
-        >>> G = nx.random_geometric_graph(100, 0.20, seed=896803)
-        >>> for (u, v, w) in G.edges(data=True): #Euclidean distance between nodes
-        ...     w['weight'] = np.sqrt((G.nodes[v]["pos"][0] - G.nodes[u]["pos"][0])**2 + (G.nodes[v]["pos"][1] - G.nodes[u]["pos"][1])**2)
-        >>> s_start, s_goal = 42, 25       
-        >>> def heursistic(u, v): #Euclidean distance between nodes
-        >>> return np.sqrt((G.nodes[v]["pos"][0] - G.nodes[u]["pos"][0])**2 + (G.nodes[v]["pos"][1] - G.nodes[u]["pos"][1])**2)
-         
-        >>> # A* search for comparison
-        >>> start_time = time.time()
-        >>> path = nx.astar_path(G, s_start, s_goal, heursistic)
-        >>> print("A* time: ", time.time() - start_time)
-        A* time:  0.00012803077697753906
-        >>> print("A* path: ", path)
-        A* path:  [42, 32, 19, 72, 49, 29, 31, 94, 35, 25]
-
-        >>> #create search object
-        >>> search = ADA_star(s_start, s_goal, G, heursistic)
- 
-        >>> #compute first suboptimal path epsilon = 2
-        >>> start_time = time.time()
-        >>> search.compute_or_improve_path(epsilon=2)
-        >>> path = search.extract_path()
-        >>> print("ADA* epsilon = 2 time: ", time.time() - start_time)
-        ADA* epsilon = 2 time:  0.0009984970092773438
-        >>> print("epsilon = 2 path: ", path)
-        epsilon = 2 path:  [42, 32, 24, 40, 59, 4, 66, 27, 35, 25]
-        >>> print("epsilon = 2 path_weight: ", nx.path_weight(G, search.extract_path(), "weight"))
-        epsilon = 2 path_weight:  1.4679609830956495
-
-        >>> #compute second (better) suboptimal path
-        >>> search.compute_or_improve_path(epsilon=1.2)
-        >>> path = search.extract_path()
-        >>> print("epsilon = 1.2 path: ", path)
-        epsilon = 1.2 path:  [42, 32, 24, 12, 59, 4, 1, 27, 35, 25]
-        >>> print("epsilon = 1.2 path_weight: ", nx.path_weight(G, path, "weight"))
-        epsilon = 1.2 path_weight:  1.3335657361796027
-
-        >>> #compute third (best) suboptimal path
-        >>> search.compute_or_improve_path(epsilon=1)
-        >>> path = search.extract_path()
-        >>> print("epsilon = 1 path: ", path)
-        epsilon = 1 path:  [42, 32, 19, 72, 49, 29, 31, 94, 35, 25]
-        >>> print("epsilon = 1 path_weight: ", nx.path_weight(G, path, "weight"))
-        epsilon = 1 path_weight:  1.29129785933092
- 
-        >>> #change graph edge weight
-        >>> print("changing graph weight for edge (49, 97)")
-        changing graph weight for edge (49, 97)
-        >>> search.update_graph([[49, 97, 0]]) #add edge between 77 and 15 with weight 0
-        >>> search.compute_or_improve_path(epsilon=1)
-        >>> path = search.extract_path()
-        >>> print("changed epsilon = 1 path: ", path)
-        changed epsilon = 1 path:  [42, 32, 19, 72, 49, 97, 11, 31, 94, 35, 25]
-        >>> print("changed epsilon = 1 path_weight: ", nx.path_weight(G, path, "weight"))
-        changed epsilon = 1 path_weight:  1.1428811257616596
         """
+
         if s_start not in G or s_goal not in G:
             msg = f"Either source {s_start} or target {s_goal} is not in G"
             raise nx.NodeNotFound(msg)
         
         self.s_start, self.s_goal = s_start, s_goal
-
         self.heursistic = heursistic
         self.weight = weight
         self.G = G
-
         self.g, self.rhs, self.OPEN = {}, {}, {}
 
         # estimate g(s) of the cost from each state to the goal
         self.g = {s:math.inf for s in G.nodes()}
 
-        # one-step lookahead cost
+        # one-step lookahead cost, 0 is s is the goal, otherwise, is the 
+        # minimum of the following sum: 
+            # The cost between s and its neighbours plus the g value 
+            # of that neighbour
         self.rhs = {s:math.inf for s in G.nodes()}
 
         self.rhs[self.s_goal] = 0.0
         self.epsilon = initial_epsilon
+
+        #initialize OPEN, CLOSED, INCONS 
         self.OPEN[self.s_goal] = self.key(self.s_goal)
         self.CLOSED, self.INCONS = set(), dict()
 
+        #keep track of visited states
         self.visited = set()
+        #flag to check if the algorithm is being called for the first time
         self.initialize = True
+        #compute first suboptimal path
         self.compute_or_improve_path(self.epsilon)
 
     
     def compute_or_improve_path(self, epsilon) -> None:
         """
-        Computes or 
+        Compute or improve the path from the start state to the goal state.
+        Takes a suboptimality bound as input, and calculates a path that is
+        suboptimal by that bound. The path is accessable through the 
+        extract_path function.
 
+
+        Parameters
+        ----------        
+
+        epsilon : float
+            The suboptimality bound, epsilon >= 1
+
+        :return: None
+
+        Examples
+        --------        
+        >>> #create search object
+        >>> search = ADA_star(s_start, s_goal, G, heursistic)
+ 
+        >>> #compute first suboptimal path epsilon = 2
+        >>> search.compute_or_improve_path(epsilon=2)
+        >>> path = search.extract_path()
+        >>> print("epsilon = 2 path: ", path)
+        epsilon = 2 path:  [42, 32, 24, 40, 59, 4, 66, 27, 35, 25]
+        >>> search.compute_or_improve_path(epsilon=1)
+        >>> path = search.extract_path()
+        >>> print("epsilon = 1 path: ", path)
+        epsilon = 1 path:  [42, 32, 19, 72, 49, 29, 31, 94, 35, 25]
 
         """
         if self.initialize: 
@@ -238,16 +332,27 @@ class ADA_star:
         return self.G[s].keys()
 
 
-    def extract_path(self):
-        """
-        Extract the path based on the PARENT set.
-        :return: The planning path
+    def extract_path(self) -> list:
+        """Extract the path based on current potentially suboptimal solution.
+
+        Returns a path from the start state to the goal state. The path is
+        suboptimal by the bound given in the compute_or_improve_path function.
+
+        Returns
+        -------
+        list of nodes in the path
+
+        Raises
+        -------
+        NetworkXNoPath
+            If no path exists between source and target.
+
         """
 
         path = [self.s_start]
         s = self.s_start
 
-        while True:
+        while True: #TODO raise exception if no path exists
             neighbours = self.get_neighbor(s)
             # find neighbour with lowest g value
             s = min(neighbours, key=lambda x: self.g[x] + self.cost(s, x))
@@ -258,11 +363,42 @@ class ADA_star:
         return list(path)
 
 
-    def update_graph(self, changes):
-        """
-        Update the graph with new edge weights
-        :param changes: list of changes, each change is a list of [node1, node2, new_weight]
-        :return: None        
+    def update_graph(self, changes) -> None:
+        """Update the graph with new edge weights
+
+        Updates the graph with new edge weights. The algorithm will use
+        previous work to find a new path in the updated graph. 
+
+        Parameters
+        ---------
+        changes : list of changes, each change is a list of [node1, node2, new_weight]
+
+        Returns
+        ---------
+        None
+
+        Examples
+        --------
+
+            >>> random.seed(1)
+            >>> G = nx.random_geometric_graph(100, 0.20, seed=896803)
+            >>> for (u, v, w) in G.edges(data=True): #Euclidean distance between nodes
+            ...     w['weight'] = np.sqrt((G.nodes[v]["pos"][0] - G.nodes[u]["pos"][0])**2 + (G.nodes[v]["pos"][1] - G.nodes[u]["pos"][1])**2)
+            >>> s_start, s_goal = 42, 25       
+            >>> def heursistic(u, v): #Euclidean distance between nodes
+            >>> return np.sqrt((G.nodes[v]["pos"][0] - G.nodes[u]["pos"][0])**2 + (G.nodes[v]["pos"][1] - G.nodes[u]["pos"][1])**2)
+
+            >>> #change graph edge weight
+            >>> print("changing graph weight for edge (49, 97)")
+            changing graph weight for edge (49, 97)
+            >>> search.update_graph([[49, 97, 0]]) #add edge between 77 and 15 with weight 0
+            >>> search.compute_or_improve_path(epsilon=1)
+            >>> path = search.extract_path()
+            >>> print("changed epsilon = 1 path: ", path)
+            changed epsilon = 1 path:  [42, 32, 19, 72, 49, 97, 11, 31, 94, 35, 25]
+            >>> print("changed epsilon = 1 path_weight: ", nx.path_weight(G, path, "weight"))
+            changed epsilon = 1 path_weight:  1.1428811257616596
+        
         """
         for change in changes:
             self.G[change[0]][change[1]][self.weight] = change[2]
