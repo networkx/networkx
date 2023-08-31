e5ddd5123b6ff620552eae86b1525f6de0f3809d
Florian2Richter <florian2richter@gmail.com>
2023-08-31 19:16:25 +0200

    Changed name

--- examples/3d_drawing/plot_facebook_3d.py
+++ examples/3d_drawing/plot_facebook_3d.py
@@ -1,6 +1,6 @@
 """
 ================
-3D Facebook Network Plot 
+3D Facebook Network
 ================
 
 3D spring plot for the Facebook graph investigated at https://networkx.org/nx-guides/content/exploratory_notebooks/facebook_notebook.html.
