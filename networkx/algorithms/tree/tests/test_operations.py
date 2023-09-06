88097f7d7f798ec49eb868691dde77cf791a67ec
Siri <88175932+sirichandana-v@users.noreply.github.com>
2023-09-07 01:38:58 +0530

    Added test cases for join operation and fixed join operation to handle label_attributes (#6503)
    * updating (#6488)
    
    * added test to check label attribute
    
    * use sets to compare
    
    * names list -> set
    
    ---------
    
    Co-authored-by: Mridul Seth <git@mriduls.com>
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- networkx/algorithms/tree/tests/test_operations.py
+++ networkx/algorithms/tree/tests/test_operations.py
@@ -2,8 +2,18 @@
 
 """
 
+from itertools import chain
+
 import networkx as nx
 from networkx.utils import edges_equal, nodes_equal
+
+
+def _check_label_attribute(input_trees, res_tree, label_attribute="_old"):
+    res_attr_dict = nx.get_node_attributes(res_tree, label_attribute)
+    res_attr_set = set(res_attr_dict.values())
+    input_label = (list(tree[0].nodes()) for tree in input_trees)
+    input_label_set = set(chain.from_iterable(input_label))
+    return res_attr_set == input_label_set
 
 
 class TestJoin:
@@ -24,14 +34,18 @@ class TestJoin:
 
         """
         T = nx.empty_graph(1)
-        actual = nx.join([(T, 0)])
+        trees = [(T, 0)]
+        actual = nx.join(trees)
         expected = nx.path_graph(2)
         assert nodes_equal(list(expected), list(actual))
         assert edges_equal(list(expected.edges()), list(actual.edges()))
+        assert _check_label_attribute(trees, actual)
 
     def test_basic(self):
         """Tests for joining multiple subtrees at a root node."""
         trees = [(nx.full_rary_tree(2, 2**2 - 1), 0) for i in range(2)]
-        actual = nx.join(trees)
+        label_attribute = "old_values"
+        actual = nx.join(trees, label_attribute)
         expected = nx.full_rary_tree(2, 2**3 - 1)
         assert nx.is_isomorphic(actual, expected)
+        assert _check_label_attribute(trees, actual, label_attribute)
