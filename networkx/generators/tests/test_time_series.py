2b51debe83a30b56fd8267e23d969ea7ef68b177
Eran Rivlis <erivlis@users.noreply.github.com>
2023-09-11 09:56:04 +0300

    Apply suggestions from code review
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- networkx/generators/tests/test_time_series.py
+++ networkx/generators/tests/test_time_series.py
@@ -6,7 +6,7 @@ import networkx as nx
 
 def test_visibility_graph_static_series():
     # arrange
-    series = range(10)
+    series = [i ** 2 for i in range(10)]  # no obstructions
     expected_series_length = len(series)
 
     # act
@@ -14,12 +14,24 @@ def test_visibility_graph_static_series():
 
     # assert
     assert actual_graph.number_of_nodes() == expected_series_length
-    assert actual_graph.number_of_edges() == 9
+    assert actual_graph.number_of_edges() == 45
+
+
+def test_visibility_graph_corner_cases():
+    edge_graph = nx.visibility_graph([10, 20])
+    assert list(edge_graph.edges) == [(0, 1)]
+
+    node_graph = nx.visibility_graph([10])
+    assert node_graph.number_of_nodes() == 1
+    assert node_graph.number_of_edges() == 0
+
+    null_graph = nx.visibility_graph([])
+    assert node_graph.number_of_nodes() == 0
 
 
 def test_visibility_graph_cyclic_series():
     # arrange
-    series = list(itertools.islice(itertools.cycle((2, 1, 3)), 12))
+    series = list(itertools.islice(itertools.cycle((2, 1, 3)), 15))
     expected_series_length = len(series)
 
     # act
@@ -27,4 +39,4 @@ def test_visibility_graph_cyclic_series():
 
     # assert
     assert actual_graph.number_of_nodes() == expected_series_length
-    assert actual_graph.number_of_edges() == 18
+    assert actual_graph.number_of_edges() == 23
--- networkx/generators/time_series.py
+++ networkx/generators/time_series.py
@@ -12,14 +12,22 @@ def visibility_graph(series):
     """
     Return a Visibility Graph of an input Time Series.
 
-    It converts a time series into a graph. The constructed graph inherits several properties of the series in its
-    structure. Thereby, periodic series convert into regular graphs, and random series do so into random graphs.
-    Moreover, fractal series convert into scale-free networks [1]_.
+    A visibility graph converts a time series into a graph. The constructed graph
+    uses integer nodes to indicate which event in the series the node represents. 
+    Edges are formed as follows: consider a bar plot of the series and view that
+    as a side view of a landscape with a node at the top of each bar. An edge
+    means that the nodes can be connected by a straight "line-of-sight" without
+    being obscured by any bars between the nodes. 
+    
+    The resulting graph inherits several properties of the series in its structure.
+    Thereby, periodic series convert into regular graphs, random series convert
+    into random graphs, and fractal series convert into scale-free networks [1]_.
 
     Parameters
     ----------
-    series: list[float] | tuple[float] | list[int] | tuple[int]
-       Time Series iterable of float or int values
+    series : Sequence[Number]
+       A Time Series sequence (iterable and slicable) of numeric values
+       representing times.
 
     Returns
     -------
@@ -28,7 +36,6 @@ def visibility_graph(series):
 
     Examples
     --------
-    >>> import networkx as nx
     >>> series_list = [range(10), [2, 1, 3, 2, 1, 3, 2, 1, 3, 2, 1, 3]]
     >>> for s in series_list:
     ...     g = nx.visibility_graph(s)
@@ -44,21 +51,12 @@ def visibility_graph(series):
            https://www.pnas.org/doi/10.1073/pnas.0709247105
     """
 
-    # TODO: Consider adding support for generator, numpy array, pandas series etc.
-    if not isinstance(series, (list, tuple, range)):
-        raise nx.NetworkXError(
-            "Input series must be a sliceable Iterable, "
-            "i.e. of one the following types: list, tuple or range"
-        )
-
     # Sequential values are always connected
     G = nx.path_graph(len(series))
     nx.set_node_attributes(G, dict(enumerate(series)), "value")
 
     # Check all combinations of nodes n series
-    for s1, s2 in itertools.combinations(enumerate(series), 2):
-        n1, t1 = s1
-        n2, t2 = s2
+    for (n1, t1), (n2, t2) in itertools.combinations(enumerate(series), 2):
         # check if any value between obstructs line of sight
         slope = (t2 - t1) / (n2 - n1)
         constant = t2 - slope * n2
