cc6f31b46e64aecc16c95ed4d842a9e45764915d
Eran Rivlis <eran.rivlis@yahoo.com>
2023-09-11 10:25:58 +0300

    update unit tests

--- networkx/generators/tests/test_time_series.py
+++ networkx/generators/tests/test_time_series.py
@@ -4,9 +4,25 @@ import itertools
 import networkx as nx
 
 
-def test_visibility_graph_static_series():
+def test_visibility_graph__empty_ts__empty_graph():
+    null_graph = nx.visibility_graph([])
+    assert null_graph.number_of_nodes() == 0
+
+
+def test_visibility_graph__single_value_ts__single_node_graph():
+    node_graph = nx.visibility_graph([10])
+    assert node_graph.number_of_nodes() == 1
+    assert node_graph.number_of_edges() == 0
+
+
+def test_visibility_graph__two_values_ts__single_edge_graph():
+    edge_graph = nx.visibility_graph([10, 20])
+    assert list(edge_graph.edges) == [(0, 1)]
+
+
+def test_visibility_graph_convex_series():
     # arrange
-    series = [i ** 2 for i in range(10)]  # no obstructions
+    series = [i**2 for i in range(10)]  # no obstructions
     expected_series_length = len(series)
 
     # act
@@ -15,18 +31,6 @@ def test_visibility_graph_static_series():
     # assert
     assert actual_graph.number_of_nodes() == expected_series_length
     assert actual_graph.number_of_edges() == 45
-
-
-def test_visibility_graph_corner_cases():
-    edge_graph = nx.visibility_graph([10, 20])
-    assert list(edge_graph.edges) == [(0, 1)]
-
-    node_graph = nx.visibility_graph([10])
-    assert node_graph.number_of_nodes() == 1
-    assert node_graph.number_of_edges() == 0
-
-    null_graph = nx.visibility_graph([])
-    assert node_graph.number_of_nodes() == 0
 
 
 def test_visibility_graph_cyclic_series():
