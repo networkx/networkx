8cd6a214380a94b65ccfd560a0443be0a2caac74
peijenburg <mas8berg@gmail.com>
2023-09-13 15:05:23 +0200

    style fixes

--- networkx/algorithms/distance_measures.py
+++ networkx/algorithms/distance_measures.py
@@ -904,7 +904,7 @@ def kirchhoff_index(G, weight=None, invert_weight=True):
     mu = nx.laplacian_spectrum(G, weight=weight)
     mu = sorted(mu)
     for i in range(1, G.number_of_nodes()):
-        Kf += 1/mu[i]
+        Kf += 1 / mu[i]
     return Kf * G.number_of_nodes()
 
 
