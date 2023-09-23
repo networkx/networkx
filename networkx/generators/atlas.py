65b2d25a1c9e7b119fc725dd297eaa33879db885
Mridul Seth <seth.mridul@gmail.com>
2023-09-23 04:18:01 +0400

    MAINT: Use importlib.resources instead of file dunder to access files (#6936)

--- networkx/generators/atlas.py
+++ networkx/generators/atlas.py
@@ -2,6 +2,7 @@
 Generators for the small graph atlas.
 """
 import gzip
+import importlib.resources
 import os
 import os.path
 from itertools import islice
@@ -15,9 +16,6 @@ __all__ = ["graph_atlas", "graph_atlas_g"]
 #: The graphs are labeled starting from 0 and extending to (but not
 #: including) this number.
 NUM_GRAPHS = 1253
-
-#: The absolute path representing the directory containing this file.
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 #: The path to the data file containing the graph edge lists.
 #:
@@ -51,7 +49,9 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 #:             f.write(bytes(f'NODES {len(G)}\n', encoding='utf-8'))
 #:             write_edgelist(G, f, data=False)
 #:
-ATLAS_FILE = os.path.join(THIS_DIR, "atlas.dat.gz")
+
+# Path to the atlas file
+ATLAS_FILE = importlib.resources.files("networkx.generators") / "atlas.dat.gz"
 
 
 def _generate_graphs():
