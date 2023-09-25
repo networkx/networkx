cfefd88cf96a698d9995b515a707527fe5ec2560
Ross Barnowski <rossbar@berkeley.edu>
2023-09-25 10:18:38 -0700

    Add support for tuple nodes to gml stringizer.

--- networkx/readwrite/gml.py
+++ networkx/readwrite/gml.py
@@ -749,6 +749,8 @@ def generate_gml(G, stringizer=None):
                 for key, value in value.items():
                     yield from stringize(key, value, (), next_indent)
                 yield indent + "]"
+            elif isinstance(value, tuple) and key == "label":
+                yield indent + key + f" \"({','.join(repr(v) for v in value)})\""
             elif isinstance(value, (list, tuple)) and key != "label" and not in_list:
                 if len(value) == 0:
                     yield indent + key + " " + f'"{value!r}"'
