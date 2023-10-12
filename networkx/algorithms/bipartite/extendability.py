659138a45bca53c3f27d0f5e7f6e04f6a57b380a
Ross Barnowski <rossbar@caltech.edu>
2023-10-12 12:41:36 -0700

    Docstring formatting touchups.

--- networkx/algorithms/bipartite/extendability.py
+++ networkx/algorithms/bipartite/extendability.py
@@ -20,50 +20,45 @@ def maximal_extendability(G):
 
     Parameters
     ----------
-
     G : NetworkX Graph
         A fully-connected bipartite graph without self-loops
 
     Returns
     -------
-
     extendability : int
 
     Raises
     ------
-
     NetworkXError
-       If the graph G is disconnected.
-       If the graph G is not bipartite.
-       If the graph G does not contain a perfect matching.
-       If the residual graph of G is not strongly connected.
+       If the graph `G` is disconnected.
+       If the graph `G` is not bipartite.
+       If the graph `G` does not contain a perfect matching.
+       If the residual graph of `G` is not strongly connected.
 
     Notes
     -----
-
     Definition:
-    Let G be a simple, connected, undirected and bipartite graph with a perfect
-    matching M and bipartition (U,V). The residual graph of G, denoted by $G_M$,
+    Let `G` be a simple, connected, undirected and bipartite graph with a perfect
+    matching M and bipartition (U,V). The residual graph of `G`, denoted by $G_M$,
     is the graph obtained from G by directing the edges of M from V to U and the
     edges that do not belong to M from U to V.
 
     Lemma([1]_):
-    Let M be a perfect matching of G. G is $k$-extendable if and only if its residual
+    Let M be a perfect matching of `G`. `G` is $k$-extendable if and only if its residual
     graph $G_M$ is strongly connected and there are $k$ vertex-disjoint directed
     paths between every vertex of U and every vertex of V.
 
-    Assuming that input graph G is undirected, simple, connected, bipartite and contains
+    Assuming that input graph `G` is undirected, simple, connected, bipartite and contains
     a perfect matching M, this function constructs the residual graph $G_M$ of G and
     returns the minimum value among the maximum vertex-disjoint directed paths between
     every vertex of U and every vertex of V in $G_M$. By combining the definitions
-    and the lemma, this value represents the extendability of the graph G.
+    and the lemma, this value represents the extendability of the graph `G`.
 
     Time complexity O($n^3$ $m^2$)) where $n$ is the number of vertices
     and $m$ is the number of edges.
 
     References
     ----------
-
     ..[1] "A polynomial algorithm for the extendability problem in bipartite graphs",
           J. Lakhal, L. Litzler, Information Processing Letters, 1998.
     ..[2] "On n-extendible graphs", M. D. Plummer, Discrete Mathematics, 31:201–210, 1980
