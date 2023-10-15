7be2a737193e3cc6d09ece6ed7dfa66723d046c3
Simon Forbat <sforbat@students.uni-mainz.de>
2023-10-15 18:27:20 +0200

    Change class-based tests into function-based tests

--- networkx/algorithms/centrality/tests/test_rumor_centrality.py
+++ networkx/algorithms/centrality/tests/test_rumor_centrality.py
@@ -8,101 +8,109 @@ import pytest
 import networkx as nx
 
 
-class TestRumorCentrality:
-    def setup_method(self):
-        self.K2 = nx.complete_graph(2)
-        self.P3 = nx.path_graph(3)
-        self.P5 = nx.path_graph(5)
-        self.S4 = nx.star_graph(4)
-        self.S9 = nx.star_graph(9)
+def test_rumor_centrality_1():
+    K2 = nx.complete_graph(2)
+    d = nx.rumor_centrality(K2)
+    exact = dict(zip(range(2), [1] * 2))
+    for n, rc in d.items():
+        assert exact[n] == pytest.approx(rc, abs=1e-7)
 
-        G = nx.Graph()
-        G.add_nodes_from(range(5))
-        G.add_edge(0, 1)
-        G.add_edge(1, 2)
-        G.add_edge(2, 3)
-        G.add_edge(1, 4)
-        self.G = G
-        # 0 -- 1 -- 2 -- 3
-        #      |
-        #      4
 
-    def test_rumor_centrality_1(self):
-        d = nx.rumor_centrality(self.K2)
-        exact = dict(zip(range(2), [1] * 2))
-        for n, rc in d.items():
-            assert exact[n] == pytest.approx(rc, abs=1e-7)
+def test_rumor_centrality_2():
+    P3 = nx.path_graph(3)
+    d = nx.rumor_centrality(P3)
+    exact = {0: 1, 1: 2, 2: 1}
+    for n, rc in d.items():
+        assert exact[n] == pytest.approx(rc, abs=1e-7)
 
-    def test_rumor_centrality_2(self):
-        d = nx.rumor_centrality(self.P3)
-        exact = {0: 1, 1: 2, 2: 1}
-        for n, rc in d.items():
-            assert exact[n] == pytest.approx(rc, abs=1e-7)
 
-    def test_rumor_centrality_3(self):
-        d = nx.rumor_centrality(self.P5)
-        exact = {0: 1, 1: 4, 2: 6, 3: 4, 4: 1}
-        for n, rc in d.items():
-            assert exact[n] == pytest.approx(rc, abs=1e-7)
+def test_rumor_centrality_3():
+    P5 = nx.path_graph(5)
+    d = nx.rumor_centrality(P5)
+    exact = {0: 1, 1: 4, 2: 6, 3: 4, 4: 1}
+    for n, rc in d.items():
+        assert exact[n] == pytest.approx(rc, abs=1e-7)
 
-    def test_rumor_centrality_4(self):
-        d = nx.rumor_centrality(self.S4)
-        exact = {0: 24, 1: 6, 2: 6, 3: 6, 4: 6}
-        for n, rc in d.items():
-            assert exact[n] == pytest.approx(rc, abs=1e-7)
 
-    def test_rumor_centrality_5(self):
-        d = nx.rumor_centrality(self.S9)
-        exact = {
-            0: math.factorial(9),
-            1: math.factorial(8),
-            2: math.factorial(8),
-            3: math.factorial(8),
-            4: math.factorial(8),
-            5: math.factorial(8),
-            6: math.factorial(8),
-            7: math.factorial(8),
-            8: math.factorial(8),
-            9: math.factorial(8),
-        }
-        for n, rc in d.items():
-            assert exact[n] == pytest.approx(rc, abs=1e-7)
+def test_rumor_centrality_4():
+    S4 = nx.star_graph(4)
+    d = nx.rumor_centrality(S4)
+    exact = {0: 24, 1: 6, 2: 6, 3: 6, 4: 6}
+    for n, rc in d.items():
+        assert exact[n] == pytest.approx(rc, abs=1e-7)
 
-    def test_rumor_centrality_6(self):
-        d = nx.rumor_centrality(self.G)
-        exact = {0: 3, 1: 12, 2: 8, 3: 2, 4: 3}
-        for n, rc in d.items():
-            assert exact[n] == pytest.approx(rc, abs=1e-7)
 
-    def test_rumor_centrality_7_weighted_graph(self):
-        G = nx.Graph()
-        G.add_nodes_from(range(5))
-        G.add_edge(0, 1, weight=1)
-        G.add_edge(1, 2, weight=2)
-        G.add_edge(2, 3, weight=3)
-        G.add_edge(1, 4, weight=4)
+def test_rumor_centrality_5():
+    S9 = nx.star_graph(9)
+    d = nx.rumor_centrality(S9)
+    exact = {
+        0: math.factorial(9),
+        1: math.factorial(8),
+        2: math.factorial(8),
+        3: math.factorial(8),
+        4: math.factorial(8),
+        5: math.factorial(8),
+        6: math.factorial(8),
+        7: math.factorial(8),
+        8: math.factorial(8),
+        9: math.factorial(8),
+    }
+    for n, rc in d.items():
+        assert exact[n] == pytest.approx(rc, abs=1e-7)
 
-        d = nx.rumor_centrality(self.G)
-        exact = {0: 3, 1: 12, 2: 8, 3: 2, 4: 3}
-        for n, rc in d.items():
-            assert exact[n] == pytest.approx(rc, abs=1e-7)
 
-    def test_rumor_centrality_8_multigraph(self):
-        e = [(1, 2), (1, 2), (2, 3), (3, 4)]
-        G = nx.MultiGraph(e)
+def test_rumor_centrality_6():
+    G = nx.Graph()
+    G.add_nodes_from(range(5))
+    G.add_edge(0, 1)
+    G.add_edge(1, 2)
+    G.add_edge(2, 3)
+    G.add_edge(1, 4)
+    # 0 -- 1 -- 2 -- 3
+    #      |
+    #      4
 
-        with pytest.raises(nx.NetworkXNotImplemented):
-            nx.rumor_centrality(G)
+    d = nx.rumor_centrality(G)
+    exact = {0: 3, 1: 12, 2: 8, 3: 2, 4: 3}
+    for n, rc in d.items():
+        assert exact[n] == pytest.approx(rc, abs=1e-7)
 
-    def test_rumor_centrality_9_directed(self):
-        e = [(1, 2), (2, 3), (3, 4)]
-        G = nx.DiGraph(e)
 
-        with pytest.raises(nx.NetworkXNotImplemented):
-            nx.rumor_centrality(G)
+def test_rumor_centrality_7_weighted_graph():
+    G = nx.Graph()
+    G.add_nodes_from(range(5))
+    G.add_edge(0, 1)
+    G.add_edge(1, 2)
+    G.add_edge(2, 3)
+    G.add_edge(1, 4)
+    # 0 -- 1 -- 2 -- 3
+    #      |
+    #      4
 
-    def test_rumor_centrality_10_cycles(self):
-        G = nx.cycle_graph(3)
+    d = nx.rumor_centrality(G)
+    exact = {0: 3, 1: 12, 2: 8, 3: 2, 4: 3}
+    for n, rc in d.items():
+        assert exact[n] == pytest.approx(rc, abs=1e-7)
 
-        with pytest.raises(nx.NetworkXNotImplemented):
-            nx.rumor_centrality(G)
+
+def test_rumor_centrality_8_multigraph():
+    e = [(1, 2), (1, 2), (2, 3), (3, 4)]
+    G = nx.MultiGraph(e)
+
+    with pytest.raises(nx.NetworkXNotImplemented):
+        nx.rumor_centrality(G)
+
+
+def test_rumor_centrality_9_directed():
+    e = [(1, 2), (2, 3), (3, 4)]
+    G = nx.DiGraph(e)
+
+    with pytest.raises(nx.NetworkXNotImplemented):
+        nx.rumor_centrality(G)
+
+
+def test_rumor_centrality_10_cycles():
+    G = nx.cycle_graph(3)
+
+    with pytest.raises(nx.NetworkXNotImplemented):
+        nx.rumor_centrality(G)
