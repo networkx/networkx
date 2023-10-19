aa2de1adecea09f7b86ff6093b212ca86f22b3ef
Jarrod Millman <jarrod.millman@gmail.com>
2023-10-18 20:49:10 -0700

    Designate 3.2 release

--- networkx/__init__.py
+++ networkx/__init__.py
@@ -8,7 +8,7 @@ structure, dynamics, and functions of complex networks.
 See https://networkx.org for complete documentation.
 """
 
-__version__ = "3.2rc1.dev0"
+__version__ = "3.2"
 
 
 # These are imported in order as listed
