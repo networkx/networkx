b680c7da06aedb51f473fa9821185f5bc3a81919
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2023-10-21 23:40:17 -0400

    remove text 3d_rotation_anime.py
    [skip active]

--- examples/3d_drawing/3d_rotation_anime.py
+++ examples/3d_drawing/3d_rotation_anime.py
@@ -1,18 +1,1 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
-# Adapted from
-# https://matplotlib.org/gallery/animation/basic_example.html
-
-
-def _update_line(num):
-    line.set_data(data[..., :num])
-    return (line,)
-
-
-fig, ax = plt.subplots()
-data = np.random.RandomState(0).rand(2, 25)
-(line,) = ax.plot([], [], "r-")
-ax.set(xlim=(0, 1), ylim=(0, 1))
-ani = animation.FuncAnimation(fig, _update_line, 25, interval=100, blit=True)
