75137cbd41de26ba8ca2ffa0c49d79c68ba7b024
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2023-10-22 17:49:16 -0400

    actual example added 3d_rotation_anime.py

--- examples/3d_drawing/3d_rotation_anime.py
+++ examples/3d_drawing/3d_rotation_anime.py
@@ -43,3 +43,32 @@ data = np.random.RandomState(0).rand(2, 25)
 (line,) = ax.plot([], [], "r-")
 ax.set(xlim=(0, 1), ylim=(0, 1))
 ani = animation.FuncAnimation(fig, _update_line, 25, interval=100, blit=True)
+
+###############################################################################
+# Rotating 3D graph animation.
+# ----------------------------
+#
+# In this example, the frame update performs only a rotation of a 3D plot
+# of a given graph.
+
+
+def _frame_update(index):
+    ax.view_init(index * 0.2, index * 0.5)
+    return
+
+
+G = nx.dodecahedral_graph()
+pos = nx.spectral_layout(G, dim=3)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+pos = nx.spectral_layout(G, dim=3)
+nodes = np.array([pos[v] for v in G])
+edges = np.array([(pos[u], pos[v]) for u, v in G.edges()])
+ax.scatter(*nodes.T, alpha=0.5, s=100)
+for vizedge in edges:
+    ax.plot(*vizedge.T, color="tab:gray")
+plt.box(False)
+ax.grid(False)
+ax.set_axis_off()
+plt.tight_layout()
+ani = animation.FuncAnimation(fig, _frame_update, interval=20)
