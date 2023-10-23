d0e27c79d514ce0033c48a2ae11b1e87165eecfd
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2023-10-23 11:42:13 -0400

    final example added 3d_rotation_anime.py

--- examples/3d_drawing/3d_rotation_anime.py
+++ examples/3d_drawing/3d_rotation_anime.py
@@ -3,14 +3,11 @@
 3D Rotating Graph Animation
 ===========================
 
-3D spring plot.
-
-Animation is adapted from
-https://matplotlib.org/gallery/animation/basic_example.html
-https://sphinx-gallery.github.io/stable/auto_examples/plot_8_animations.html
-
+3D animations illustrated.
 """
 import numpy as np
+import networkx as nx
+import random
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
@@ -31,6 +28,9 @@ plt.show()
 # --------------------------
 #
 # Test animation plot for testing doc generation for animation.
+# Animation is copy/pasted from
+# https://matplotlib.org/gallery/animation/basic_example.html
+# https://sphinx-gallery.github.io/stable/auto_examples/plot_8_animations.html
 
 
 def _update_line(num):
@@ -43,6 +43,7 @@ data = np.random.RandomState(0).rand(2, 25)
 (line,) = ax.plot([], [], "r-")
 ax.set(xlim=(0, 1), ylim=(0, 1))
 ani = animation.FuncAnimation(fig, _update_line, 25, interval=100, blit=True)
+# plt.show()
 
 ###############################################################################
 # Rotating 3D graph animation.
@@ -71,4 +72,36 @@ plt.box(False)
 ax.grid(False)
 ax.set_axis_off()
 plt.tight_layout()
-ani = animation.FuncAnimation(fig, _frame_update, interval=20)
+ani = animation.FuncAnimation(fig, _frame_update, interval=20, cache_frame_data=False)
+# plt.show()
+
+###############################################################################
+# Random jumping on rotating 3D graph animation.
+# ----------------------------------------------
+#
+# The frame update can also draw a new plot in every frame giving the ultimate
+# flexibility at the cost of performance loss.
+
+
+def _frame_update(index):
+    ax.clear()
+    ax.scatter(*nodes.T, alpha=0.5, s=100)
+    for vizedge in edges:
+        ax.plot(*vizedge.T, color="tab:gray")
+    node = random.choice(nodes)
+    ax.scatter(*node, alpha=1, marker="s", color="red", s=100)
+    ax.view_init(index * 0.2, index * 0.5)
+    plt.box(False)
+    ax.grid(False)
+    ax.set_axis_off()
+    return
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+plt.box(False)
+ax.grid(False)
+ax.set_axis_off()
+plt.tight_layout()
+ani = animation.FuncAnimation(fig, _frame_update, interval=20, cache_frame_data=False)
+# plt.show()
