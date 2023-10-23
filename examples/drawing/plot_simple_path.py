dc9dc598ccf1b717d2fa397e7d3cfef767cd3412
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2023-10-23 13:00:58 -0400

    minor plot_simple_path.py

--- examples/drawing/plot_simple_path.py
+++ examples/drawing/plot_simple_path.py
@@ -8,7 +8,7 @@ Draw a graph with matplotlib.
 import matplotlib.pyplot as plt
 import networkx as nx
 
-G = nx.path_graph(8)
+G = nx.path_graph(9)
 pos = nx.spring_layout(G, seed=47)  # Seed layout for reproducibility
 nx.draw(G, pos=pos)
 plt.show()
