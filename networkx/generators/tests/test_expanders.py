6f6ad4767f02b676848e81768339d186a5fb4561
fcrozatier <frederic.crozatier@protonmail.com>
2023-10-29 11:31:26 +0100

    fix tests

--- networkx/generators/tests/test_expanders.py
+++ networkx/generators/tests/test_expanders.py
@@ -97,7 +97,7 @@ def test_random_regular_expander(d, n):
 def test_random_regular_expander_explicit_construction():
     pytest.importorskip("numpy")
     pytest.importorskip("scipy")
-    G = nx.random_regular_expander_graph(4, 5)
+    G = nx.random_regular_expander_graph(d=4, n=5)
 
     assert len(G) == 5 and len(G.edges) == 10, "Should be a complete graph"
 
