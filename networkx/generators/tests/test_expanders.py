e58e41f730df8a1d87fb8d12987c63cd6cbf118a
fcrozatier <frederic.crozatier@protonmail.com>
2023-11-02 18:40:38 +0100

    fix tests

--- networkx/generators/tests/test_expanders.py
+++ networkx/generators/tests/test_expanders.py
@@ -135,7 +135,7 @@ def test_maybe_regular_expander_badinput():
     with pytest.raises(nx.NetworkXError, match="d must be greater than or equal to 2"):
         nx.maybe_regular_expander(n=10, d=0)
 
-    with pytest.raises(nx.NetworkXError, match="There is not enough room"):
+    with pytest.raises(nx.NetworkXError, match="Need n-1>= d to have room room"):
         nx.maybe_regular_expander(n=5, d=6)
 
 
@@ -157,7 +157,7 @@ def test_random_regular_expander_badinput():
     with pytest.raises(nx.NetworkXError, match="d must be greater than or equal to 2"):
         nx.random_regular_expander_graph(n=10, d=0)
 
-    with pytest.raises(nx.NetworkXError, match="There is not enough room"):
+    with pytest.raises(nx.NetworkXError, match="Need n-1>= d to have room"):
         nx.random_regular_expander_graph(n=5, d=6)
 
     with pytest.raises(nx.NetworkXError, match="epsilon must be non negative"):
