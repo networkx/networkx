e06647e64c6a58d79c0265cda73bf6a426f879dd
fcrozatier <frederic.crozatier@protonmail.com>
2023-11-02 18:44:41 +0100

    typo

--- networkx/generators/tests/test_expanders.py
+++ networkx/generators/tests/test_expanders.py
@@ -135,7 +135,7 @@ def test_maybe_regular_expander_badinput():
     with pytest.raises(nx.NetworkXError, match="d must be greater than or equal to 2"):
         nx.maybe_regular_expander(n=10, d=0)
 
-    with pytest.raises(nx.NetworkXError, match="Need n-1>= d to have room room"):
+    with pytest.raises(nx.NetworkXError, match="Need n-1>= d to have room"):
         nx.maybe_regular_expander(n=5, d=6)
 
 
