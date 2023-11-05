483098a7be6a27a8513cac98306e96d5e1ab87cf
Ross Barnowski <rossbar@caltech.edu>
2023-11-04 18:27:53 -0700

    Split out walk animation example.

--- examples/3d_drawing/plot_3d_animation_walk.py
+++ examples/3d_drawing/plot_3d_animation_walk.py
@@ -1,16 +1,11 @@
 """
 =========================================
-Animations of 3D rotation and random walk
+Animation of 3D rotation and random walk.
 =========================================
-Examples of 3D plots of a graph in the 3D spectral layout with animation.
-Following
-https://sphinx-gallery.github.io/stable/auto_examples/plot_8_animations.html
-using frame rotation of an initial plot of a graph as in
-https://matplotlib.org/stable/api/animation_api.html
-or complete frame redraw to plot a random walk on the graph.
 
-The commented out line with 'plt.show()' needs to be commented back in
-in both examples when running locally.
+Another animation example, illustrating a walk along the nodes as the view
+rotates.
+
 """
 
 import numpy as np
@@ -23,47 +18,12 @@ from matplotlib import animation
 # Define a graph to plot.
 # -----------------------
 #
-# Pick up a graph to look good in 3D.
+# Pick a graph for visualization in 3D, such as one of the Platonic solids
 
 G = nx.dodecahedral_graph()
-pos = nx.spectral_layout(G, dim=3)
+pos = nx.spring_layout(G, dim=3)
 nodes = np.array([pos[v] for v in G])
 edges = np.array([(pos[u], pos[v]) for u, v in G.edges()])
-
-###############################################################################
-# Rotating 3D graph animation.
-# ----------------------------
-#
-# In this example, a frame update is only a rotation of a given 3D graph.
-
-
-def init():
-    ax.scatter(*nodes.T, alpha=0.2, s=100, color="blue")
-    for vizedge in edges:
-        ax.plot(*vizedge.T, color="gray")
-    ax.grid(False)
-    ax.set_axis_off()
-    plt.tight_layout()
-    return
-
-
-def _frame_update(index):
-    ax.view_init(index * 0.2, index * 0.5)
-    return
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-
-ani = animation.FuncAnimation(
-    fig,
-    _frame_update,
-    init_func=init,
-    interval=50,
-    cache_frame_data=False,
-    frames=100,
-)
-# plt.show()
 
 ###############################################################################
 # Random walk on rotating 3D graph animation.
@@ -103,4 +63,5 @@ ani = animation.FuncAnimation(
     cache_frame_data=False,
     frames=100,
 )
-# plt.show()
+
+plt.show()
