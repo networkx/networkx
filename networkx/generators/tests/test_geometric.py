d22e47829f0822168e8e725bad130f9bdfee448c
Ross Barnowski <rossbar@berkeley.edu>
2023-11-07 09:13:30 -0800

    Add test for general edge scaling with increasing r.

--- networkx/generators/tests/test_geometric.py
+++ networkx/generators/tests/test_geometric.py
@@ -268,6 +268,20 @@ class TestNavigableSmallWorldGraph:
         expected = nx.grid_2d_graph(3, 3, create_using=nx.DiGraph)
         assert nx.utils.graphs_equal(G, expected)
 
+    @pytest.mark.parametrize("seed", range(2478, 2578, 10))
+    def test_r_general_scaling(self, seed):
+        """The probability of adding a long-range edge scales with `1 / dist**r`,
+        so a navigable_small_world graph created with r < 1 should generally
+        result in more edges than a navigable_small_world graph with r >= 1
+        (for 0 < q << n).
+
+        N.B. this is probabilistic, so this test may not hold for all seeds."""
+        G1 = nx.navigable_small_world_graph(7, q=3, r=0.5, seed=seed)
+        G2 = nx.navigable_small_world_graph(7, q=3, r=1, seed=seed)
+        G3 = nx.navigable_small_world_graph(7, q=3, r=2, seed=seed)
+        assert G1.number_of_edges() > G2.number_of_edges()
+        assert G2.number_of_edges() > G3.number_of_edges()
+
 
 class TestThresholdedRandomGeometricGraph:
     """Unit tests for :func:`~networkx.thresholded_random_geometric_graph`"""
