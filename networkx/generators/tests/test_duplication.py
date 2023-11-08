4c451f4abf1e06e717c725ef99db1b4422582ef6
Chiranjeevi Karthik Kuruganti <35001420+karthikchiru12@users.noreply.github.com>
2023-11-08 23:27:08 +0530

    Added few tests for /generators/duplication.py and /generators/geomet… (#6976)
    * Added few tests for /generators/duplication.py and /generators/geometric.py. Corrected an exception statement in geometric.py (r must be >=0 instead of r must be >= 1)
    
    * Added kargs to make it more readable. And added few more tests.
    
    * STY: Minor signature cleanup.
    
    * Update test for r in range [0, 1].
    
    * Add test for general edge scaling with increasing r.
    
    ---------
    
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>

--- networkx/generators/tests/test_duplication.py
+++ networkx/generators/tests/test_duplication.py
@@ -18,18 +18,29 @@ class TestDuplicationDivergenceGraph:
     """
 
     def test_final_size(self):
-        G = duplication_divergence_graph(3, 1)
+        G = duplication_divergence_graph(3, p=1)
         assert len(G) == 3
-        G = duplication_divergence_graph(3, 1, seed=42)
+        G = duplication_divergence_graph(3, p=1, seed=42)
         assert len(G) == 3
 
     def test_probability_too_large(self):
         with pytest.raises(NetworkXError):
-            duplication_divergence_graph(3, 2)
+            duplication_divergence_graph(3, p=2)
 
     def test_probability_too_small(self):
         with pytest.raises(NetworkXError):
-            duplication_divergence_graph(3, -1)
+            duplication_divergence_graph(3, p=-1)
+
+    def test_non_extreme_probability_value(self):
+        G = duplication_divergence_graph(6, p=0.3, seed=42)
+        assert len(G) == 6
+        assert list(G.degree()) == [(0, 2), (1, 3), (2, 2), (3, 3), (4, 1), (5, 1)]
+
+    def test_minimum_desired_nodes(self):
+        with pytest.raises(
+            NetworkXError, match=".*n must be greater than or equal to 2"
+        ):
+            duplication_divergence_graph(1, p=1)
 
 
 class TestPartialDuplicationGraph:
