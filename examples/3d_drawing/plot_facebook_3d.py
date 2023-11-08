cfa85449bbae8697db1f8f980189d9a2e1bc71b2
florian2richter <florian2richter@gmail.com>
2023-11-08 08:11:24 +0100

    added dynamic camera positions

--- examples/3d_drawing/plot_facebook_3d.py
+++ examples/3d_drawing/plot_facebook_3d.py
@@ -10,6 +10,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import pandas as pd
+import random
 
 # The graph to plot
 facebook = pd.read_csv(
@@ -18,8 +19,16 @@ facebook = pd.read_csv(
     sep=" ",
     names=["start_node", "end_node"],
 )
+
+# random sampling from graph
+n = 2000 # n_max=5000
+random.seed(n)
+random_indices = [random.randint(0, 5000) for _ in range(n)]
 # Generate graph from CSV data
-G = nx.from_pandas_edgelist(facebook, "start_node", "end_node")
+print(facebook)
+facebook_random = facebook.loc[random_indices]
+
+G = nx.from_pandas_edgelist(facebook_random, "start_node", "end_node")
 
 # Generate layout of the graph using spring_layout in 3D
 pos = nx.spring_layout(
@@ -34,7 +43,30 @@ nodes = np.array([pos[v] for v in G])
 edges = np.array([(pos[u], pos[v]) for u, v in G.edges()])
 point_size = int(1000 / np.sqrt(len(nodes)))
 
-# Rotating 3D graph animation.
+#camera drive parameters
+frames = 60
+
+azimuthal_min = 0
+azimuthal_max = 100
+
+elevation_min = 0
+elevation_max = 40
+
+# Calculate the step size of camera angles for the first half
+step_size_azimuthal = (azimuthal_max - azimuthal_min) / (frames // 2 - 1)
+step_size_elevation = (elevation_max - elevation_min) / (frames // 2 - 1)
+
+# Create the angles for the first half
+azimuthal_angles_first_half = [azimuthal_min + i * step_size_azimuthal for i in range(frames // 2)]
+elevation_angles_first_half = [elevation_min + i * step_size_elevation for i in range(frames // 2)]
+
+# Reverse the first half angles to create the second half
+azimuthal_angles_second_half = azimuthal_angles_first_half[::-1]
+elevation_angles_second_half = elevation_angles_first_half[::-1]
+
+# Combine the two halves to create the complete angles
+azimuthal_angles = azimuthal_angles_first_half + azimuthal_angles_second_half
+elevation_angles = elevation_angles_first_half + elevation_angles_second_half
 
 
 def init():
@@ -49,7 +81,11 @@ def init():
 
 
 def _frame_update(index):
-    ax.view_init(index * 0.2, index * 0.5)
+    ax.view_init(elevation_angles[index], azimuthal_angles[index])
+    print('index')
+    print(index)
+    print(elevation_angles[index])
+    print(azimuthal_angles[index])
     return
 
 
@@ -62,6 +98,6 @@ ani = animation.FuncAnimation(
     init_func=init,
     interval=50,
     cache_frame_data=False,
-    frames=50,
+    frames=frames,
 )
 plt.show()
