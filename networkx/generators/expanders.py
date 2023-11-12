f510439a09a625e1167fe9ac06e3432cd72084f6
fcrozatier <frederic.crozatier@protonmail.com>
2023-11-12 11:12:02 +0100

    escape all latex named symbols

--- networkx/generators/expanders.py
+++ networkx/generators/expanders.py
@@ -244,7 +244,7 @@ def maybe_regular_expander(n, d, *, create_using=None, max_tries=100, seed=None)
 
     Joel Friedman proved that in this model the resulting
     graph is an expander with probability
-    $1 - O(n^{-\\tau})$ where $\\tau = \\lceil (\sqrt{d - 1}) / 2 \\rceil - 1$. [1]_
+    $1 - O(n^{-\\tau})$ where $\\tau = \\lceil (\\sqrt{d - 1}) / 2 \\rceil - 1$. [1]_
 
     Examples
     --------
@@ -336,11 +336,11 @@ def is_regular_expander(G, *, epsilon=0):
     An expander graph is a sparse graph with strong connectivity properties.
 
     More precisely, this helper checks whether the graph is a
-    regular $(n, d, \lambda)$-expander with $\lambda$ close to
+    regular $(n, d, \\lambda)$-expander with $\\lambda$ close to
     the Alon-Boppana bound and given by
-    $\lambda = 2 \sqrt{d - 1} + \epsilon$. [2]_
+    $\\lambda = 2 \\sqrt{d - 1} + \\epsilon$. [2]_
 
-    In the case where $\epsilon = 0 $ then if the graph successfully passes the test
+    In the case where $\\epsilon = 0 $ then if the graph successfully passes the test
     it is a Ramanujan graph. [3]_
 
     A Ramanujan graph has spectral gap almost as large as possible, which makes them
@@ -354,8 +354,8 @@ def is_regular_expander(G, *, epsilon=0):
     Returns
     -------
     bool
-        Whether the given graph is a regular $(n, d, \lambda)$-expander
-        where $\lambda = 2 \sqrt{d - 1} + \epsilon$.
+        Whether the given graph is a regular $(n, d, \\lambda)$-expander
+        where $\\lambda = 2 \\sqrt{d - 1} + \\epsilon$.
 
     Examples
     --------
