1d79a33608f02cd2257c9567a47e21050fea7b23
AKSHAYA MADHURI <76612327+akshayamadhuri@users.noreply.github.com>
2023-11-18 22:41:59 +0530

    Update similarity.py

--- networkx/algorithms/similarity.py
+++ networkx/algorithms/similarity.py
@@ -336,12 +336,12 @@ def optimal_edit_paths(
     5.0
 
     Note 
-    --------
-    When the cost is zero, it indicates that G1 and G2 are 
-    isomorphic, and the optimal paths represent different isomorphic 
-    mappings. To transform G1 into a graph isomorphic to G2, apply the
-    node and edge edits in the optimal paths. In the case of isomorphic graphs,
-    these edits involve renaming nodes and edges to match the structure of G2.
+    -------
+        When the cost is zero, it indicates that G1 and G2 are 
+        isomorphic, and the optimal paths represent different isomorphic 
+        mappings. To transform G1 into a graph isomorphic to G2, apply the
+        node and edge edits in the optimal paths. In the case of isomorphic graphs,
+        these edits involve renaming nodes and edges to match the structure of G2.
 
     See Also
     --------
