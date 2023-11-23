1257b7b0ddce91402ae88620bfae4270054a7b62
pmlpm1986 <96069104+pmlpm1986@users.noreply.github.com>
2023-11-23 14:17:06 +0100

    Update test_function.py
    Included a test case for the is_path method with single nodes.

--- networkx/classes/tests/test_function.py
+++ networkx/classes/tests/test_function.py
@@ -753,13 +753,15 @@ def test_pathweight():
     "G", (nx.Graph(), nx.DiGraph(), nx.MultiGraph(), nx.MultiDiGraph())
 )
 def test_ispath(G):
-    G.add_edges_from([(1, 2), (2, 3), (1, 2), (3, 4)])
+    G.add_edges_from([(1, 2), (2, 3), (1, 2), (3, 4), (3, 6)])
     valid_path = [1, 2, 3, 4]
     invalid_path = [1, 2, 4, 3]  # wrong node order
     another_invalid_path = [1, 2, 3, 4, 5]  # contains node not in G
+    yet_another_invalid_path = [2] # a single node is not a path
     assert nx.is_path(G, valid_path)
     assert not nx.is_path(G, invalid_path)
     assert not nx.is_path(G, another_invalid_path)
+    assert not nx.is_path(G, yet_another_invalid_path)
 
 
 @pytest.mark.parametrize("G", (nx.Graph(), nx.DiGraph()))
