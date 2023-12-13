4da3e3b1c46b85f19b20be1cc8cd749b4ea0f2ef
Schefflera-Arboricola <1509aditi@gmail.com>
2023-12-13 11:13:27 +0530

    added Raises sec to all funcs and updated error msg

--- networkx/algorithms/link_prediction.py
+++ networkx/algorithms/link_prediction.py
@@ -78,6 +78,14 @@ def resource_allocation_index(G, ebunch=None):
         An iterator of 3-tuples in the form (u, v, p) where (u, v) is a
         pair of nodes and p is their resource allocation index.
 
+    Raises
+    ------
+    NetworkXNotImplemented
+        If `G` is a `DiGraph` or a `Multigraph`.
+
+    NodeNotFound
+        If `ebunch` has a node that is not in `G`.
+
     Examples
     --------
     >>> G = nx.complete_graph(5)
@@ -132,6 +140,14 @@ def jaccard_coefficient(G, ebunch=None):
     piter : iterator
         An iterator of 3-tuples in the form (u, v, p) where (u, v) is a
         pair of nodes and p is their Jaccard coefficient.
+
+    Raises
+    ------
+    NetworkXNotImplemented
+        If `G` is a `DiGraph` or a `Multigraph`.
+
+    NodeNotFound
+        If `ebunch` has a node that is not in `G`.
 
     Examples
     --------
@@ -191,6 +207,14 @@ def adamic_adar_index(G, ebunch=None):
     piter : iterator
         An iterator of 3-tuples in the form (u, v, p) where (u, v) is a
         pair of nodes and p is their Adamic-Adar index.
+
+    Raises
+    ------
+    NetworkXNotImplemented
+        If `G` is a `DiGraph` or a `Multigraph`.
+
+    NodeNotFound
+        If `ebunch` has a node that is not in `G`.
 
     Examples
     --------
@@ -270,6 +294,17 @@ def common_neighbor_centrality(G, ebunch=None, alpha=0.8):
         pair of nodes and p is their Common Neighbor and Centrality based
         Parameterized Algorithm(CCPA) score.
 
+    Raises
+    ------
+    NetworkXNotImplemented
+        If `G` is a `DiGraph` or a `Multigraph`.
+    
+    NetworkXAlgorithmError
+        If self loops exsists in `ebunch` or in `G` (if `ebunch` is `None`).
+
+    NodeNotFound
+        If `ebunch` has a node that is not in `G`.
+
     Examples
     --------
     >>> G = nx.complete_graph(5)
@@ -292,7 +327,7 @@ def common_neighbor_centrality(G, ebunch=None, alpha=0.8):
 
         def predict(u, v):
             if u == v:
-                raise nx.NetworkXAlgorithmError("Self links are not supported")
+                raise nx.NetworkXAlgorithmError("Self loops are not supported")
 
             return sum(1 for _ in nx.common_neighbors(G, u, v))
 
@@ -302,7 +337,7 @@ def common_neighbor_centrality(G, ebunch=None, alpha=0.8):
 
         def predict(u, v):
             if u == v:
-                raise nx.NetworkXAlgorithmError("Self links are not supported")
+                raise nx.NetworkXAlgorithmError("Self loops are not supported")
             path_len = spl[u].get(v, inf)
 
             return alpha * sum(1 for _ in nx.common_neighbors(G, u, v)) + (
@@ -343,6 +378,14 @@ def preferential_attachment(G, ebunch=None):
     piter : iterator
         An iterator of 3-tuples in the form (u, v, p) where (u, v) is a
         pair of nodes and p is their preferential attachment score.
+
+    Raises
+    ------
+    NetworkXNotImplemented
+        If `G` is a `DiGraph` or a `Multigraph`.
+
+    NodeNotFound
+        If `ebunch` has a node that is not in `G`.
 
     Examples
     --------
@@ -407,6 +450,18 @@ def cn_soundarajan_hopcroft(G, ebunch=None, community="community"):
     piter : iterator
         An iterator of 3-tuples in the form (u, v, p) where (u, v) is a
         pair of nodes and p is their score.
+
+    Raises
+    ------
+    NetworkXNotImplemented
+        If `G` is a `DiGraph` or a `Multigraph`.
+
+    NetworkXAlgorithmError
+        If no community information is available for a node in `ebunch` or 
+        in `G` (if `ebunch` is `None`).
+
+    NodeNotFound
+        If `ebunch` has a node that is not in `G`.
 
     Examples
     --------
@@ -482,6 +537,18 @@ def ra_index_soundarajan_hopcroft(G, ebunch=None, community="community"):
     piter : iterator
         An iterator of 3-tuples in the form (u, v, p) where (u, v) is a
         pair of nodes and p is their score.
+
+    Raises
+    ------
+    NetworkXNotImplemented
+        If `G` is a `DiGraph` or a `Multigraph`.
+
+    NetworkXAlgorithmError
+        If no community information is available for a node in `ebunch` or 
+        in `G` (if `ebunch` is `None`).
+
+    NodeNotFound
+        If `ebunch` has a node that is not in `G`.
 
     Examples
     --------
@@ -559,6 +626,19 @@ def within_inter_cluster(G, ebunch=None, delta=0.001, community="community"):
         An iterator of 3-tuples in the form (u, v, p) where (u, v) is a
         pair of nodes and p is their WIC measure.
 
+    Raises
+    ------
+    NetworkXNotImplemented
+        If `G` is a `DiGraph` or a `Multigraph`.
+
+    NetworkXAlgorithmError
+        - If `delta` is less than or equal to zero.
+        - If no community information is available for a node in `ebunch` or 
+        in `G` (if `ebunch` is `None`).
+
+    NodeNotFound
+        If `ebunch` has a node that is not in `G`.
+
     Examples
     --------
     >>> G = nx.Graph()
@@ -607,4 +687,6 @@ def _community(G, u, community):
     try:
         return node_u[community]
     except KeyError as err:
-        raise nx.NetworkXAlgorithmError("No community information") from err
+        raise nx.NetworkXAlgorithmError(
+            f"No community information available for Node {u}"
+        ) from err
