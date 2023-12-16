62887bd1875a292d42b8632054f9c1a510fe3923
Ross Barnowski <rossbar@berkeley.edu>
2023-12-16 11:02:56 -0800

    Rm extra unicode string indicators.

--- networkx/readwrite/tests/test_gml.py
+++ networkx/readwrite/tests/test_gml.py
@@ -554,8 +554,8 @@ graph
             "directed 1 multigraph 1 ]"
         )
         assert_parse_error(
-            "graph [edge [ source u'\u4200' target u'\u4200' ] "
-            + "node [ id u'\u4200' label b ] ]"
+            "graph [edge [ source '\u4200' target '\u4200' ] "
+            + "node [ id '\u4200' label b ] ]"
         )
 
         def assert_generate_error(*args, **kwargs):
