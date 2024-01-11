4bda30f68541c88de0b4b8ddd978794791cc5ae7
Nihal John George <nihalgeorge01@gmail.com>
2024-01-11 10:50:28 -0500

    ensure test consistency for normalized and un-normalized

--- networkx/algorithms/tests/test_richclub.py
+++ networkx/algorithms/tests/test_richclub.py
@@ -91,38 +91,42 @@ def test_rich_club_selfloop():
         nx.rich_club_coefficient(G)
 
 
-def test_rich_club_leq_3_nodes():
+@pytest.mark.parametrize("normalized", (True, False))
+def test_rich_club_leq_3_nodes(normalized):
     # edgeless graphs upto 3 nodes
     G = nx.Graph()
-    rc = nx.rich_club_coefficient(G)
+    rc = nx.rich_club_coefficient(G, normalized)
     assert rc == {}
 
     for i in range(3):
         G.add_node(i)
-        rc = nx.rich_club_coefficient(G)
+        rc = nx.rich_club_coefficient(G, normalized)
         assert rc == {}
 
     # 2 nodes, single edge
     G = nx.Graph()
     G.add_edge(0, 1)
-    rc = nx.rich_club_coefficient(G)
+    rc = nx.rich_club_coefficient(G, normalized)
     assert rc == {0: 1}
 
     # 3 nodes, single edge
     G = nx.Graph()
     G.add_nodes_from([0, 1, 2])
     G.add_edge(0, 1)
-    rc = nx.rich_club_coefficient(G)
+    rc = nx.rich_club_coefficient(G, normalized)
     assert rc == {0: 1}
 
     # 3 nodes, 2 edges
     G.add_edge(1, 2)
-    rc = nx.rich_club_coefficient(G)
-    assert rc == {0: 1}
+    rc = nx.rich_club_coefficient(G, normalized)
+    if normalized:
+        assert rc == {0: 1}
+    else:
+        assert rc == {0: 2 / 3}
 
     # 3 nodes, 3 edges
     G.add_edge(0, 2)
-    rc = nx.rich_club_coefficient(G)
+    rc = nx.rich_club_coefficient(G, normalized)
     assert rc == {0: 1, 1: 1}
 
 
