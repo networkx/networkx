906691c8238e19ec19233c73cbc89126c8840ad4
Max Seeliger <maximilian.seeliger@gmail.com>
2024-01-15 18:13:03 +0100

    Implementation of modular product

--- networkx/algorithms/operators/product.py
+++ networkx/algorithms/operators/product.py
@@ -14,6 +14,7 @@ __all__ = [
     "power",
     "rooted_product",
     "corona_product",
+    "modular_product",
 ]
 _G_H = {"G": 0, "H": 1}
 
@@ -530,5 +531,82 @@ def corona_product(G, H):
 
         # creating new edges between H_i and a G's node
         GH.add_edges_from((G_node, (G_node, H_node)) for H_node in H)
+
+    return GH
+
+
+@not_implemented_for("directed")
+@not_implemented_for("multigraph")
+@nx._dispatchable(graphs=_G_H)
+def modular_product(G, H):
+    r"""Returns the Modular product of G and H.
+
+    The modular product of $G$ and $H$ is the graph $M = G \nabla H$,
+    consisting of the node set $V(M) = V(G) \times V(H)$ that is the Cartesian
+    product of the node sets of $G$ and $H$. M has an edge $((u, v), (x, y))$:
+
+    - if $u$ is adjacent to $x$ in $G$ and $v$ is adjacent to $y$ in $H$, or
+    - if $u$ is not adjacent to $x$ in $G$ and $v$ is not adjacent to $y$ in $H$.
+
+    More formally, $E(M) = \{((u, v), (x, y)) \mid (u, x) \in E(G) \text{ and } (v, y) \in E(H)\}
+    \cup \{((u, v), (x, y)) \mid (u, x) \notin E(G) \text{ and } (v, y) \notin E(H)\}$
+
+    Parameters
+    ----------
+    G, H: NetworkX graphs
+        The graphs to take the modular product of.
+
+    Returns
+    -------
+    M: NetworkX graph
+        The Modular product of G and H.
+
+    Raises
+    ------
+    NetworkXNotImplemented
+        If `G` is not a simple graph.
+
+    Examples
+    --------
+    >>> G = nx.cycle_graph(4)
+    >>> H = nx.path_graph(2)
+    >>> M = nx.modular_product(G, H)
+    >>> list(M)
+    [(0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1), (3, 0), (3, 1)]
+    >>> print(M)
+    Graph with 8 nodes and 8 edges
+
+    References
+    ----------
+    [1] R. Hammack, W. Imrich, and S. Klavžar, "Handbook of Product Graphs",
+        CRC Press, 2011.
+    [2] H. G. Barrow and R. Burstall, "Subgraph isomorphism, matching
+        relational structures and maximal cliques", Information Processing
+        Letters, vol. 4, issue 4, pp. 83-84, 1976,
+        doi: 10.1016/0020-0190(76)90049-1.
+    [3] V. G. Vizing, "Reduction of the problem of isomorphism and isomorphic
+        entrance to the task of finding the nondensity of a graph." Proc. Third
+        All-Union Conference on Problems of Theoretical Cybernetics. 1974.
+
+    Notes
+    -----
+    This definition of "modular product" comes from [1] and was first
+    introduced as the "weak modular product".
+
+    The modular product reduces the problem of counting isomorphic subgraphs
+    in $G$ and $H$ to the problem of counting cliques in $M$. The subgraphs of
+    $G$ and $H$ that are induced by the nodes of a clique in $M$ are
+    isomorphic.
+    """
+    GH = _init_product_graph(G, H)
+    GH.add_nodes_from(_node_product(G, H))
+
+    GH.add_edges_from(_directed_edges_cross_edges(G, H))
+    GH.add_edges_from(_undirected_edges_cross_edges(G, H))
+
+    G = nx.complement(G)
+    H = nx.complement(H)
+    GH.add_edges_from(_directed_edges_cross_edges(G, H))
+    GH.add_edges_from(_undirected_edges_cross_edges(G, H))
 
     return GH
