8bd17a40018361daaa622bea2273100afcac47b7
Max Seeliger <maximilian.seeliger@gmail.com>
2024-01-15 21:26:45 +0100

    Fixed typo

--- networkx/algorithms/operators/product.py
+++ networkx/algorithms/operators/product.py
@@ -128,7 +128,7 @@ def tensor_product(G, H):
     r"""Returns the tensor product of G and H.
 
     The tensor product $P$ of the graphs $G$ and $H$ has a node set that
-    is the tensor product of the node sets, $V(P)=V(G) \times V(H)$.
+    is the Cartesian product of the node sets, $V(P)=V(G) \times V(H)$.
     $P$ has an edge $((u,v), (x,y))$ if and only if $(u,x)$ is an edge in $G$
     and $(v,y)$ is an edge in $H$.
 
