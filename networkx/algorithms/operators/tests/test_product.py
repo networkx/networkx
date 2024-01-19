0d9fd48c2b0554ce1bedb06c582cc10e9ab66480
Maximilian Seeliger <maximilian.seeliger@gmail.com>
2024-01-19 22:25:45 +0100

    Added tests for combinations of accepted and non-accepted graph type
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- networkx/algorithms/operators/tests/test_product.py
+++ networkx/algorithms/operators/tests/test_product.py
@@ -467,12 +467,25 @@ def test_modular_product():
 
 
 def test_modular_product_raises():
+    G = nx.Graph([(0, 1), (1, 2), (2, 0)])
+    H = nx.Graph([(0, 1), (1, 2), (2, 0)])
+    DG = nx.DiGraph([(0, 1), (1, 2), (2, 0)])
+    DH = nx.DiGraph([(0, 1), (1, 2), (2, 0)])
     with pytest.raises(nx.NetworkXNotImplemented):
-        G = nx.DiGraph([(0, 1), (1, 2), (2, 0)])
-        H = nx.DiGraph([(0, 1), (1, 2), (2, 0)])
-        nx.modular_product(G, H)
+        nx.modular_product(G, DH)
+    with pytest.raises(nx.NetworkXNotImplemented):
+        nx.modular_product(DG, H)
+    with pytest.raises(nx.NetworkXNotImplemented):
+        nx.modular_product(DG, DH)
 
+    MG = nx.MultiGraph([(0, 1), (1, 2), (2, 0), (0, 1)])
+    MH = nx.MultiGraph([(0, 1), (1, 2), (2, 0), (0, 1)])
     with pytest.raises(nx.NetworkXNotImplemented):
-        G = nx.MultiGraph([(0, 1), (1, 2), (2, 0), (0, 1)])
-        H = nx.MultiGraph([(0, 1), (1, 2), (2, 0), (0, 1)])
-        nx.modular_product(G, H)
+        nx.modular_product(G, MH)
+    with pytest.raises(nx.NetworkXNotImplemented):
+        nx.modular_product(MG, H)
+    with pytest.raises(nx.NetworkXNotImplemented):
+        nx.modular_product(MG, MH)
+    with pytest.raises(nx.NetworkXNotImplemented):
+        # check multigraph with no multiedges
+        nx.modular_product(nx.MultiGraph(G), H)
