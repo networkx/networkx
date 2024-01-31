c51e08bdfd5794fac6bd13441efdb259aa3cbad5
Ross Barnowski <rossbar@berkeley.edu>
2024-01-31 09:19:52 -0800

    Add test degree_histogram empty input.

--- networkx/classes/tests/test_function.py
+++ networkx/classes/tests/test_function.py
@@ -6,6 +6,11 @@ import networkx as nx
 from networkx.utils import edges_equal, nodes_equal
 
 
+def test_degree_histogram_empty():
+    G = nx.Graph()
+    assert nx.degree_histogram(G) == []
+
+
 class TestFunction:
     def setup_method(self):
         self.G = nx.Graph({0: [1, 2, 3], 1: [1, 2, 0], 4: []}, name="Test")
