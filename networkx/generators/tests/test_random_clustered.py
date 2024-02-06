c2607e1697ac2eda1eaa3741a5013ec00ea0c81a
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2024-02-06 11:20:00 +0530

    Add match
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>

--- networkx/generators/tests/test_random_clustered.py
+++ networkx/generators/tests/test_random_clustered.py
@@ -32,9 +32,8 @@ class TestRandomClusteredGraph:
         )
 
     def test_directed_graph_raises_error(self):
-        pytest.raises(
-            nx.NetworkXError,
-            nx.random_clustered_graph,
-            [(1, 2), (2, 1), (1, 1), (1, 1), (1, 1), (2, 0)],
-            create_using=nx.DiGraph,
-        )
+        with pytest.raises(nx.NetworkXError, match="Directed Graph not supported"):
+            nx.random_clustered_graph(
+                [(1, 2), (2, 1), (1, 1), (1, 1), (1, 1), (2, 0)],
+                create_using=nx.DiGraph,
+            )
