007fed70c9098f0934335061542d9ffddb7e8ad9
joncrall <erotemic@gmail.com>
2024-02-14 10:44:03 -0500

    Fix typo

--- networkx/algorithms/tree/branchings.py
+++ networkx/algorithms/tree/branchings.py
@@ -1196,7 +1196,7 @@ def minimum_branching(
 
 @nx._dispatchable(
     edge_attrs={"attr": "default", "partition": None},
-    preserve_edge_attrs="preserve_edge_attrsserve_attrs",
+    preserve_edge_attrs="preserve_attrs",
 )
 def minimal_branching(
     G, /, *, attr="weight", default=1, preserve_attrs=False, partition=None
