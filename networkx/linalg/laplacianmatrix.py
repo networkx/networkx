7bf536eea9e199480da20ffbbcbd336b94a73b78
peijenburg <mas8berg@gmail.com>
2024-03-02 09:28:47 +0100

    Update laplacianmatrix.py

--- networkx/linalg/laplacianmatrix.py
+++ networkx/linalg/laplacianmatrix.py
@@ -344,8 +344,6 @@ def total_spanning_tree_weight(G, weight=None, root=None):
     # Determinant ignoring first row and column
     return float(abs(np.linalg.det(G_laplacian[1:, 1:])))
 
-    return nx.number_of_spanning_trees(G, weight=weight, root=root)
-
 
 ###############################################################################
 # Code based on work from https://github.com/bjedwards
