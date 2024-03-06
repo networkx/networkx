fbf09803cf42c543be2ffe31bf50d9dadcadfc5f
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-03-06 17:52:21 +0100

    Excape regex pattern

--- networkx/classes/tests/test_reportviews.py
+++ networkx/classes/tests/test_reportviews.py
@@ -1,4 +1,5 @@
 import pickle
+import re
 from copy import deepcopy
 
 import pytest
@@ -596,15 +597,18 @@ class TestEdgeView:
         assert ev[0, 1] == {"foo": "bar"}
 
         # slicing
-        with pytest.raises(nx.NetworkXError) as e:
+        with pytest.raises(
+            nx.NetworkXError,
+            match=re.escape(
+                f"{type(G.edges).__name__} does not support slicing, try list(G.edges)[0:5:None]"
+            ),
+        ):
             G.edges[0:5]
-        assert (
-            str(e.value)
-            == f"{type(G.edges).__name__} does not support slicing, try list(G.edges)[0:5:None]"
-        )
 
         # Invalid edge
-        with pytest.raises(KeyError, match="The edge (0, 9) is not in the graph."):
+        with pytest.raises(
+            KeyError, match=re.escape("'The edge (0, 9) is not in the graph.'")
+        ):
             G.edges[0, 9]
 
     def test_call(self):
