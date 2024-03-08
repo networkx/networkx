3795fc3ab3c6963471a09c9f483d471be1029051
Ross Barnowski <rossbar@berkeley.edu>
2024-03-07 20:45:59 -0800

    Add test for multigraph with non-default edge attr.

--- networkx/algorithms/approximation/tests/test_steinertree.py
+++ networkx/algorithms/approximation/tests/test_steinertree.py
@@ -198,3 +198,13 @@ def test_steiner_tree_weight_attribute(method):
     nx.set_edge_attributes(G, {e: 10 for e in G.edges}, name="distance")
     H = nx.approximation.steiner_tree(G, [1, 3], method=method, weight="distance")
     assert nx.utils.edges_equal(H.edges, [(0, 1), (0, 3)])
+
+
+@pytest.mark.parametrize("method", ("kou", "mehlhorn"))
+def test_steiner_tree_multigraph_weight_attribute(method):
+    G = nx.cycle_graph(3, create_using=nx.MultiGraph)
+    nx.set_edge_attributes(G, {e: 10 for e in G.edges}, name="distance")
+    G.add_edge(2, 0, distance=5)
+    H = nx.approximation.steiner_tree(G, list(G), method=method, weight="distance")
+    assert len(H.edges) == 2 and H.has_edge(2, 0, key=1)
+    assert sum(dist for *_, dist in H.edges(data="distance")) == 15
