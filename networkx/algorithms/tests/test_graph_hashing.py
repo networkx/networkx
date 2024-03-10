ef5f9acb5b711346d6eb92b4bf01d32a75d4f57c
Aaron Z <40212329+aaronzo@users.noreply.github.com>
2024-03-10 16:44:03 +0000

    Option to include initial labels in `weisfeiler_lehman_subgraph_hashes` (#6601)
    * docstring
    
    * change n -> u for node in docstring
    
    * fix issue 7330
    
    * Update networkx/algorithms/graph_hashing.py
    
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>
    
    ---------
    
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>

--- networkx/algorithms/tests/test_graph_hashing.py
+++ networkx/algorithms/tests/test_graph_hashing.py
@@ -655,3 +655,32 @@ def test_digest_size_subgraph_hash():
 
         assert hexdigest_sizes_correct(digest_size16_hashes, 16)
         assert hexdigest_sizes_correct(digest_size32_hashes, 32)
+
+
+def test_initial_node_labels_subgraph_hash():
+    """
+    Including the hashed initial label prepends an extra hash to the lists
+    """
+    G = nx.path_graph(5)
+    nx.set_node_attributes(G, {i: int(0 < i < 4) for i in G}, "label")
+    # initial node labels:
+    # 0--1--1--1--0
+
+    without_initial_label = nx.weisfeiler_lehman_subgraph_hashes(G, node_attr="label")
+    assert all(len(v) == 3 for v in without_initial_label.values())
+    # 3 different 1 hop nhds
+    assert len({v[0] for v in without_initial_label.values()}) == 3
+
+    with_initial_label = nx.weisfeiler_lehman_subgraph_hashes(
+        G, node_attr="label", include_initial_labels=True
+    )
+    assert all(len(v) == 4 for v in with_initial_label.values())
+    # 2 different initial labels
+    assert len({v[0] for v in with_initial_label.values()}) == 2
+
+    # check hashes match otherwise
+    for u in G:
+        for a, b in zip(
+            with_initial_label[u][1:], without_initial_label[u], strict=True
+        ):
+            assert a == b
