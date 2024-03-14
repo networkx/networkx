15a52b666c94320d9ebe738f9a9877cab1ad2983
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2024-03-14 13:20:56 +0530

    Update test_steinertree.py

--- networkx/algorithms/approximation/tests/test_steinertree.py
+++ networkx/algorithms/approximation/tests/test_steinertree.py
@@ -214,7 +214,7 @@ def test_steiner_tree_multigraph_weight_attribute(method):
 def test_steiner_tree_methods(method):
     G = nx.star_graph(4)
     expected = nx.Graph([(0, 1), (0, 3)])
-    st = nx.approximation.steinter_tree(G, [1, 3], method=method)
+    st = nx.approximation.steiner_tree(G, [1, 3], method=method)
     assert nx.utils.edges_equal(H.edges, expected.edges)
 
 
