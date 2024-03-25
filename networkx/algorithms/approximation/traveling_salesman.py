7fd277519d111afb5521c3bbf49a3fb7642d2184
Matt Schwennesen <mjschwenne@gmail.com>
2024-03-25 16:01:40 -0500

    format doctest

--- networkx/algorithms/approximation/traveling_salesman.py
+++ networkx/algorithms/approximation/traveling_salesman.py
@@ -298,7 +298,13 @@ def traveling_salesman_problem(
 
     Otherwise, pass other keyword arguments directly into the tsp function.
 
-    >>> path = tsp(G, cycle=False, method=nx.approximation.simulated_annealing_tsp, init_cycle="greedy", temp=500)
+    >>> path = tsp(
+    ...     G,
+    ...     cycle=False,
+    ...     method=nx.approximation.simulated_annealing_tsp,
+    ...     init_cycle="greedy",
+    ...     temp=500,
+    ... )
     >>> path in ([4, 3, 2, 1, 0, 8, 7, 6, 5], [5, 6, 7, 8, 0, 1, 2, 3, 4])
     True
     """
