edac98548c896f5b51d2aa1f79380dcd6227c910
Sreelakshmi <sreeluvkv@gmail.com>
2024-04-09 20:36:52 +0530

    fixing style/format error

--- networkx/algorithms/coloring/tests/test_edgecoloring.py
+++ networkx/algorithms/coloring/tests/test_edgecoloring.py
@@ -1,6 +1,8 @@
 """Unit tests for the :mod:`networkx.algorithms.coloring.edge_coloring` module."""
 from collections import defaultdict
+
 import pytest
+
 import networkx as nx
 from networkx.algorithms.coloring.edge_coloring import edge_coloring
 
@@ -21,11 +23,11 @@ def _is_proper_edge_coloring(coloring):
 
     return True
 
+
 class TestEdgeColoring:
     """Tests for edge coloring algorithm"""
 
     def test_complete_graph(self):
-
         G = nx.complete_graph(13)
         coloring = edge_coloring(G)
 
@@ -38,9 +40,8 @@ class TestEdgeColoring:
         assert len(used_colors) <= (max_degree + 1)
 
     def test_odd_graph(self):
-
         n = 3
-        G = nx.kneser_graph(2*n-1, n-1)
+        G = nx.kneser_graph(2 * n - 1, n - 1)
         coloring = edge_coloring(G)
 
         # Check that no vertex has two edges with the same color
@@ -52,22 +53,46 @@ class TestEdgeColoring:
         assert len(used_colors) <= (max_degree + 1)
 
     def test_13(self):
-
         # Create a graph
         G = nx.Graph()
 
         edge_list = [
-        (0, 1), (0, 2), (0, 3), (0, 4), (0, 12),
-        (1, 2), (1, 3), (1, 11), (1, 5),
-        (2, 3), (2, 4), (2, 5),
-        (3, 4), (3, 5),
-        (4, 5),
-        (6, 7), (3, 8), (6, 9), (6, 10), (6, 11), (6, 12),
-        (7, 8), (7, 9), (7, 10), (7, 11), (7, 12),
-        (8, 9), (8, 10), (5, 11), (8, 12),
-        (2, 10), (9, 11), (9, 12),
-        (10, 11), (10, 12),
-        (11, 12)
+            (0, 1),
+            (0, 2),
+            (0, 3),
+            (0, 4),
+            (0, 12),
+            (1, 2),
+            (1, 3),
+            (1, 11),
+            (1, 5),
+            (2, 3),
+            (2, 4),
+            (2, 5),
+            (3, 4),
+            (3, 5),
+            (4, 5),
+            (6, 7),
+            (3, 8),
+            (6, 9),
+            (6, 10),
+            (6, 11),
+            (6, 12),
+            (7, 8),
+            (7, 9),
+            (7, 10),
+            (7, 11),
+            (7, 12),
+            (8, 9),
+            (8, 10),
+            (5, 11),
+            (8, 12),
+            (2, 10),
+            (9, 11),
+            (9, 12),
+            (10, 11),
+            (10, 12),
+            (11, 12),
         ]
 
         G.add_edges_from(edge_list)
@@ -83,16 +108,10 @@ class TestEdgeColoring:
         assert len(used_colors) <= (max_degree + 1)
 
     def test_disconnected_graph(self):
-
         # Create a graph
         G = nx.Graph()
 
-        edge_list = [
-        (0, 1), (0, 2), (0, 3),
-        (1, 2),
-        (4, 5), (4, 6),
-        (6, 7)
-        ]
+        edge_list = [(0, 1), (0, 2), (0, 3), (1, 2), (4, 5), (4, 6), (6, 7)]
 
         G.add_edges_from(edge_list)
 
