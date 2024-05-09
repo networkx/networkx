bd9d2e7e67e26a3066aff4648d9acc7cab4944e3
Rhyd-Lewis <163303122+Rhyd-Lewis@users.noreply.github.com>
2024-05-09 09:50:27 +0100

    Update greedy_coloring.py

--- networkx/algorithms/coloring/greedy_coloring.py
+++ networkx/algorithms/coloring/greedy_coloring.py
@@ -223,7 +223,7 @@ def strategy_saturation_largest_first(G, colors):
     algorithm is **exact** for such graphs).
 
     This implementation of the DSatur algorithm has a complexity of
-    $O(n lg n + m lg m)$. See [1]_ for further details.
+    $O(n log n + m log m)$. See [1]_ for further details.
 
     References
     ----------
@@ -532,14 +532,14 @@ def greedy_color(G, strategy="largest_first", tabu=0, interchange=False):
        If ``strategy`` is a string, it must be one of the following.
        Each of these represents one of the built-in strategy functions.
 
-       * ``'largest_first'`` (vertices are ordered by degree, largest first, complexity $O(n lg n)$)
+       * ``'largest_first'`` (vertices are ordered by degree, largest first, complexity $O(n log n)$)
        * ``'random_sequential'`` (vertices are randomly ordered, complexity $O(n)$)
        * ``'smallest_last'``
        * ``'independent_set'``
        * ``'connected_sequential_bfs'``
        * ``'connected_sequential_dfs'``
        * ``'connected_sequential'`` (alias for the previous strategy)
-       * ``'saturation_largest_first'`` (uses the Dsatur algorithm, complexity $O(n lg n + m lg m)$)
+       * ``'saturation_largest_first'`` (uses the Dsatur algorithm, complexity $O(n log n + m log m)$)
        * ``'DSATUR'`` (alias for the previous strategy)
        * ``'rlf'`` (recursive largest first algorithm, complexity $O(nm)$)
 
