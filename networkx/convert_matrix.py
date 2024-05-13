f3ad4d79bfc8599cb2ec186180f017c4b4e735d9
inbalh1 <inbal12343@gmail.com>
2024-05-13 11:13:37 +0300

    Add edge_key to reserved_colomns
    	modified:   networkx/convert_matrix.py

--- networkx/convert_matrix.py
+++ networkx/convert_matrix.py
@@ -348,7 +348,7 @@ def from_pandas_edgelist(
     edge_attr : str or int, iterable, True, or None
         A valid column name (str or int) or iterable of column names that are
         used to retrieve items and add them to the graph as edge attributes.
-        If `True`, all of the remaining columns will be added.
+        If `True`, all of the remaining columns will be added (not including edge_key).
         If `None`, no edge attributes are added to the graph.
 
     create_using : NetworkX graph constructor, optional (default=nx.Graph)
@@ -436,6 +436,8 @@ def from_pandas_edgelist(
         return g
 
     reserved_columns = [source, target]
+    if g.is_multigraph() and edge_key is not None:
+        reserved_columns.append(edge_key)
 
     # Additional columns requested
     attr_col_headings = []
