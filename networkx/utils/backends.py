656c09400789836ea649a43258da2b99e5f8308c
Erik Welch <erik.n.welch@gmail.com>
2024-05-20 16:24:42 -0400

    Always cache graph attrs for better cache behavior
    This should slightly optimize caching for little cost.
    It reduces the number of possible cache keys by half.
    Also, add a few comments for clarity.

--- networkx/utils/backends.py
+++ networkx/utils/backends.py
@@ -1059,15 +1059,13 @@ class _dispatchable:
             # node_attrs: dict | None
             # preserve_edge_attrs: bool (False if edge_attrs is not None)
             # preserve_node_attrs: bool (False if node_attrs is not None)
-            # preserve_graph_attrs: bool
-            key = edge_key, node_key, graph_key = (
+            key = edge_key, node_key = (
                 frozenset(edge_attrs.items())
                 if edge_attrs is not None
                 else preserve_edge_attrs,
                 frozenset(node_attrs.items())
                 if node_attrs is not None
                 else preserve_node_attrs,
-                preserve_graph_attrs,
             )
             if cache:
                 warning_message = (
@@ -1085,8 +1083,8 @@ class _dispatchable:
                     "will correctly clear the cache to keep it consistent. "
                     "You may also use `G.__networkx_cache__.clear()` to "
                     "manually clear the cache, or set `G.__networkx_cache__` "
-                    "to None to disable caching for G. Enable or disable "
-                    "caching via `nx.config.cache_converted_graphs` config."
+                    "to None to disable caching for G. Enable or disable caching "
+                    "globally via `nx.config.cache_converted_graphs` config."
                 )
                 # Do a simple search for a cached graph with compatible data.
                 # For example, if we need a single attribute, then it's okay
@@ -1095,7 +1093,6 @@ class _dispatchable:
                 for compat_key in itertools.product(
                     (edge_key, True) if edge_key is not True else (True,),
                     (node_key, True) if node_key is not True else (True,),
-                    (graph_key, True) if graph_key is not True else (True,),
                 ):
                     if (rv := cache.get(compat_key)) is not None:
                         warnings.warn(warning_message)
@@ -1106,25 +1103,23 @@ class _dispatchable:
                     # with any edge attribute will suffice. We use the same logic
                     # below (but switched) to clear unnecessary items from the cache.
                     # Use `list(cache.items())` to be thread-safe.
-                    for (ekey, nkey, gkey), val in list(cache.items()):
+                    for (ekey, nkey), val in list(cache.items()):
                         if edge_key is False or ekey is True:
-                            pass
+                            pass  # Cache works for edge data!
                         elif (
                             edge_key is True
                             or ekey is False
                             or not edge_key.issubset(ekey)
                         ):
-                            continue
+                            continue  # Cache missing required edge data; does not work
                         if node_key is False or nkey is True:
-                            pass
+                            pass  # Cache works for node data!
                         elif (
                             node_key is True
                             or nkey is False
                             or not node_key.issubset(nkey)
                         ):
-                            continue
-                        if graph_key and not gkey:
-                            continue
+                            continue  # Cache missing required node data; does not work
                         warnings.warn(warning_message)
                         return val
 
@@ -1135,7 +1130,10 @@ class _dispatchable:
             node_attrs=node_attrs,
             preserve_edge_attrs=preserve_edge_attrs,
             preserve_node_attrs=preserve_node_attrs,
-            preserve_graph_attrs=preserve_graph_attrs,
+            # Always preserve graph attrs when we are caching b/c this should be
+            # cheap and may help prevent extra (unnecessary) conversions. Because
+            # we do this, we don't need `preserve_graph_attrs` in the cache key.
+            preserve_graph_attrs=preserve_graph_attrs or use_cache,
             name=self.name,
             graph_name=graph_name,
         )
@@ -1147,7 +1145,7 @@ class _dispatchable:
             for cur_key in list(cache):
                 if cur_key == key:
                     continue
-                ekey, nkey, gkey = cur_key
+                ekey, nkey = cur_key
                 if ekey is False or edge_key is True:
                     pass
                 elif ekey is True or edge_key is False or not ekey.issubset(edge_key):
@@ -1155,8 +1153,6 @@ class _dispatchable:
                 if nkey is False or node_key is True:
                     pass
                 elif nkey is True or node_key is False or not nkey.issubset(node_key):
-                    continue
-                if gkey and not graph_key:
                     continue
                 cache.pop(cur_key, None)  # Use pop instead of del to be thread-safe
 
