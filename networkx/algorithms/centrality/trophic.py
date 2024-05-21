c4e815e08e0d7360303e41f4efda929a99b53b9a
Maverick18 <115613425+Aditya-Shandiya1812@users.noreply.github.com>
2024-05-21 10:52:30 +0530

    Simplified the implementation

--- networkx/algorithms/centrality/trophic.py
+++ networkx/algorithms/centrality/trophic.py
@@ -42,33 +42,25 @@ def trophic_levels(G, weight="weight"):
     """
 
     def bfs_reachable_from_basal(basal_nodes, graph):
-        visited = set()
-        reachable_nodes = set()  # Initialize an empty set to store reachable nodes
-        for node in basal_nodes:
-            queue = [node]
-            while queue:
-                current = queue.pop(0)
-                if current not in visited:
-                    visited.add(current)
-                    reachable_nodes.add(
-                        current
-                    )  # Add the current node to reachable_nodes
-                    for neighbor in graph.successors(current):
-                        if neighbor not in visited:
-                            queue.append(neighbor)
-        return reachable_nodes  # Return the set of reachable nodes
+        reachable_nodes = {
+            node
+            for layer in nx.bfs_layers(graph, sources=basal_nodes)
+            for node in layer
+        }
+        return reachable_nodes
 
     basal_nodes = [n for n in G if G.in_degree(n) == 0]
     if not basal_nodes:
         raise nx.NetworkXError(
-            "Trophic levels are only defined for graphs where every node has a path from a basal node (basal nodes are nodes with no incoming edges)."
+            "Trophic levels are only defined for graphs where every node has a path "
+            "from a basal node (basal nodes are nodes with no incoming edges)."
         )
 
     reachable_nodes = bfs_reachable_from_basal(basal_nodes, G)
     if len(reachable_nodes) != len(G.nodes):
         raise nx.NetworkXError(
-            "Trophic levels are only defined for graphs where every node has a"
-            + " path from a basal node (basal nodes are nodes with no incoming edges)."
+            "Trophic levels are only defined for graphs where every node has a path "
+            "from a basal node (basal nodes are nodes with no incoming edges)."
         )
 
     import numpy as np
