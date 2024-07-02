675c26c88c8671e388db11d141a5ab16d4211636
Mridul Seth <git@mriduls.com>
2024-07-02 22:47:54 +0200

    FIX: scipy 1d indexing tripped up numpy?

--- networkx/algorithms/centrality/flow_matrix.py
+++ networkx/algorithms/centrality/flow_matrix.py
@@ -71,7 +71,11 @@ class InverseLaplacian:
         m = 0
         for i, row in enumerate(L):
             w = 0
-            x, y = np.nonzero(row)
+            # scipy.sparse indexing for 1D has changed https://github.com/scipy/scipy/pull/20120
+            try:
+                x, y = np.nonzero(row)
+            except ValueError:
+                y = np.nonzero(row)[0]
             if len(y) > 0:
                 v = y - i
                 w = v.max() - v.min() + 1
