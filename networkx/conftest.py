4fa22011df857a86423a63df0396f0796dabbd5d
Ross Barnowski <rossbar@caltech.edu>
2024-07-04 08:26:15 +0300

    Minor updates to colliders v_structures tests (#7539)
    * Fix warnings filter to suppress test warnings.
    
    * Parametrize v_structures test.
    
    * Parametrize colliders tests.
    
    * Add back reference for test case.
    
    Co-authored-by: Gilles Peiffer <gilles.peiffer.yt@gmail.com>
    
    * Apply suggestions from code review re: whitespace for readable long tuples
    
    ---------
    
    Co-authored-by: Gilles Peiffer <gilles.peiffer.yt@gmail.com>
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- networkx/conftest.py
+++ networkx/conftest.py
@@ -139,7 +139,7 @@ def set_warnings():
         "ignore", category=DeprecationWarning, message=r"\n\nThe 'create=matrix'"
     )
     warnings.filterwarnings(
-        "ignore", category=DeprecationWarning, message="\n\ncompute_v_structures"
+        "ignore", category=DeprecationWarning, message="\n\n\`compute_v_structures"
     )
 
 
