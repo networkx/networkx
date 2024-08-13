4da8205ce86650979127cb4548147a4a57c03d73
Ross Barnowski <rossbar@berkeley.edu>
2024-08-12 22:28:55 -0700

    Add directed graphs to harmonic centrality benchmarks.

--- benchmarks/benchmarks/benchmark_harmonic_centrality.py
+++ benchmarks/benchmarks/benchmark_harmonic_centrality.py
@@ -6,13 +6,23 @@ import networkx as nx
 class HarmonicCentralityBenchmarks:
     timeout = 120
     nodes = [10, 100, 1000]
-    _graphs = [nx.wheel_graph(n) for n in nodes]
-    params = [f"wheel_graph({i})" for i in nodes]
-
+    params = [f"wheel_graph({i})" for i in nodes] + [
+        f"directed_wheel({i})" for i in nodes
+    ]
     param_names = ["graph"]
 
     def setup(self, graph):
-        self.graphs_dict = dict(zip(self.params, self._graphs))
+        def directed_wheel(n):
+            # bidirectional edges on the rim with directed edges to the central node
+            G = nx.DiGraph(nx.cycle_graph(range(1, n)))
+            G.add_node(0)
+            G.add_edges_from((0, i) for i in range(1, n))
+            return G
+
+        self.graphs_dict = {}
+        for n in self.nodes:
+            self.graphs_dict[f"wheel_graph({n})"] = nx.wheel_graph(n)
+            self.graphs_dict[f"directed_wheel({n})"] = directed_wheel(n)
 
     def time_harmonic_centrality(self, graph):
         _ = nx.harmonic_centrality(self.graphs_dict[graph])
