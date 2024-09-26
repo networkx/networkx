045b2eb39148f849fe336ed6cf81f802040ed74e
Matthew Thorne <mthorne@MacBookPro.powerhub>
2024-09-26 22:00:44 +0100

    initial commit of adding clausets algorithm

---
+++ networkx/algorithms/community/clauset.py
@@ -0,0 +1,13 @@
+
+
+def clauset_local_community_detection(G):
+    """
+    Local community detection starts from a single node an iterative expands until it finds a minimal community
+
+    https://journals.aps.org/pre/abstract/10.1103/PhysRevE.72.026132
+
+    https://ieeexplore.ieee.org/document/9916271
+    
+    """
+
+    return []
