af36e57c12988d622a140e38a960ccbcee293ae0
rlratzel <rratzel@nvidia.com>
2024-10-08 01:08:54 -0500

    Removes unsupported config example.

--- networkx/utils/configs.py
+++ networkx/utils/configs.py
@@ -324,7 +324,6 @@ class NetworkXConfig(Config):
 
     and can be used for finer control of ``backend_priority`` such as:
 
-    - ``NETWORKX_BACKEND_PRIORITY_PAGERANK``: set ``backend_priority.pagerank`` from comma-separated names.
     - ``NETWORKX_BACKEND_PRIORITY_ALGOS``: same as ``NETWORKX_BACKEND_PRIORITY`` to set ``backend_priority.algos`.
 
     This is a global configuration. Use with caution when using from multiple threads.
