fd27267ea13182ef027fc90f8a4cb578640fea98
Ross Barnowski <rossbar@berkeley.edu>
2024-10-16 21:57:10 -0700

    Apply updates to 3d anim examples.

--- examples/3d_drawing/plot_3d_animation_basic.py
+++ examples/3d_drawing/plot_3d_animation_basic.py
@@ -1,21 +1,17 @@
 """
-=========================================
-Animations of 3D rotation and random walk
-=========================================
-Examples of 3D plots of a graph in the 3D spectral layout with animation.
-Following
-https://sphinx-gallery.github.io/stable/auto_examples/plot_8_animations.html
-using frame rotation of an initial plot of a graph as in
-https://matplotlib.org/stable/api/animation_api.html
-or complete frame redraw to plot a random walk on the graph.
+===============
+Basic Animation
+===============
 
-The commented out line with 'plt.show()' needs to be commented back in
-in both examples when running locally.
+NetworkX supports several 3D layout functions for visualization.
+These layouts can be combined with :external+matplotlib:doc:`api/animation_api`
+for 3D graph visualization.
+
+This example shows a basic animation incrementing the camera view.
 """
 
 import numpy as np
 import networkx as nx
-import random
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
@@ -23,10 +19,10 @@ from matplotlib import animation
 # Define a graph to plot.
 # -----------------------
 #
-# Pick up a graph to look good in 3D.
+# Pick a graph for visualization in 3D, such as one of the Platonic solids
 
 G = nx.dodecahedral_graph()
-pos = nx.spectral_layout(G, dim=3)
+pos = nx.spring_layout(G, dim=3, seed=2734)
 nodes = np.array([pos[v] for v in G])
 edges = np.array([(pos[u], pos[v]) for u, v in G.edges()])
 
@@ -38,22 +34,21 @@ edges = np.array([(pos[u], pos[v]) for u, v in G.edges()])
 
 
 def init():
+    ax.clear()
     ax.scatter(*nodes.T, alpha=0.2, s=100, color="blue")
     for vizedge in edges:
         ax.plot(*vizedge.T, color="gray")
     ax.grid(False)
     ax.set_axis_off()
-    plt.tight_layout()
-    return
 
 
 def _frame_update(index):
     ax.view_init(index * 0.2, index * 0.5)
-    return
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
+fig.tight_layout()
 
 ani = animation.FuncAnimation(
     fig,
@@ -63,44 +58,5 @@ ani = animation.FuncAnimation(
     cache_frame_data=False,
     frames=100,
 )
-# plt.show()
 
-###############################################################################
-# Random walk on rotating 3D graph animation.
-# -------------------------------------------
-#
-# The frame update can also draw a new plot in every frame giving the ultimate
-# flexibility at the cost of performance loss.
-
-
-def _frame_update(index):
-    ax.clear()
-    ax.scatter(*nodes.T, alpha=0.2, s=100, color="blue")
-    for vizedge in edges:
-        ax.plot(*vizedge.T, color="gray")
-    neighbors = list(G.neighbors(node[0]))
-    if index % 5 == 0:
-        node[0] = random.choice(neighbors)
-    node0 = nodes[node[0]]
-    ax.scatter(*node0, alpha=1, marker="s", color="red", s=100)
-    ax.view_init(index * 0.2, index * 0.5)
-    ax.grid(False)
-    ax.set_axis_off()
-    plt.tight_layout()
-    return
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-ax.grid(False)
-ax.set_axis_off()
-plt.tight_layout()
-node = [0]
-ani = animation.FuncAnimation(
-    fig,
-    _frame_update,
-    interval=50,
-    cache_frame_data=False,
-    frames=100,
-)
-# plt.show()
+plt.show()
--- examples/3d_drawing/plot_3d_animation_walk.py
+++ examples/3d_drawing/plot_3d_animation_walk.py
@@ -1,16 +1,11 @@
 """
-=========================================
-Animations of 3D rotation and random walk
-=========================================
-Examples of 3D plots of a graph in the 3D spectral layout with animation.
-Following
-https://sphinx-gallery.github.io/stable/auto_examples/plot_8_animations.html
-using frame rotation of an initial plot of a graph as in
-https://matplotlib.org/stable/api/animation_api.html
-or complete frame redraw to plot a random walk on the graph.
+====================
+Animation: Node Walk
+====================
 
-The commented out line with 'plt.show()' needs to be commented back in
-in both examples when running locally.
+Another animation example, illustrating a walk along the nodes as the view
+rotates.
+
 """
 
 import numpy as np
@@ -23,38 +18,49 @@ from matplotlib import animation
 # Define a graph to plot.
 # -----------------------
 #
-# Pick up a graph to look good in 3D.
+# Pick a graph for visualization in 3D, such as one of the Platonic solids
 
 G = nx.dodecahedral_graph()
-pos = nx.spectral_layout(G, dim=3)
+pos = nx.spring_layout(G, dim=3, seed=2734)
 nodes = np.array([pos[v] for v in G])
 edges = np.array([(pos[u], pos[v]) for u, v in G.edges()])
 
 ###############################################################################
-# Rotating 3D graph animation.
-# ----------------------------
+# Random walk on rotating 3D graph animation.
+# -------------------------------------------
 #
-# In this example, a frame update is only a rotation of a given 3D graph.
+# Modify the plot itself along with the view, showing a wandering node that
+# travels to a new neighbor every 5 frames
 
 
 def init():
+    ax.clear()
     ax.scatter(*nodes.T, alpha=0.2, s=100, color="blue")
     for vizedge in edges:
         ax.plot(*vizedge.T, color="gray")
+    # Intialize the "walking" node
+    ax.plot(*nodes[0], alpha=1, marker="s", color="red")
     ax.grid(False)
     ax.set_axis_off()
-    plt.tight_layout()
-    return
 
 
 def _frame_update(index):
+    global node
+    # Update "current" node every 5 frames
+    neighbors = list(G.neighbors(node))
+    if index % 5 == 0:
+        # Update node
+        node = random.choice(neighbors)
+        # Update the last line object, which corresponds to the walking node
+        _ = ax.lines[-1].set_data_3d(nodes[node][:, np.newaxis])
+    # Update view
     ax.view_init(index * 0.2, index * 0.5)
-    return
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
-
+fig.tight_layout()
+node = 0  # Initialize the walking node
 ani = animation.FuncAnimation(
     fig,
     _frame_update,
@@ -63,44 +69,5 @@ ani = animation.FuncAnimation(
     cache_frame_data=False,
     frames=100,
 )
-# plt.show()
 
-###############################################################################
-# Random walk on rotating 3D graph animation.
-# -------------------------------------------
-#
-# The frame update can also draw a new plot in every frame giving the ultimate
-# flexibility at the cost of performance loss.
-
-
-def _frame_update(index):
-    ax.clear()
-    ax.scatter(*nodes.T, alpha=0.2, s=100, color="blue")
-    for vizedge in edges:
-        ax.plot(*vizedge.T, color="gray")
-    neighbors = list(G.neighbors(node[0]))
-    if index % 5 == 0:
-        node[0] = random.choice(neighbors)
-    node0 = nodes[node[0]]
-    ax.scatter(*node0, alpha=1, marker="s", color="red", s=100)
-    ax.view_init(index * 0.2, index * 0.5)
-    ax.grid(False)
-    ax.set_axis_off()
-    plt.tight_layout()
-    return
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-ax.grid(False)
-ax.set_axis_off()
-plt.tight_layout()
-node = [0]
-ani = animation.FuncAnimation(
-    fig,
-    _frame_update,
-    interval=50,
-    cache_frame_data=False,
-    frames=100,
-)
-# plt.show()
+plt.show()
