a18c7d823feb9077f732208aba456b944a7ee737
Thomas Louf <31036680+TLouf@users.noreply.github.com>
2024-11-07 09:31:45 +0100

    Simplify the self-loop conditional branches by merging them
    Co-authored-by: Ross Barnowski <rossbar@caltech.edu>

--- networkx/generators/directed.py
+++ networkx/generators/directed.py
@@ -506,16 +506,13 @@ def _random_k_out_graph_numpy(n, k, alpha, self_loops=True, seed=None):
     for i in range(k * n):
         u = seed.choice(nodes[remaining_mask])
 
-        if not self_loops:
-            u_weight = weights[u]
-            weights[u] = 0
-            total_weight -= u_weight
-
-        v = seed.choice(nodes, p=weights / total_weight)
-
-        if not self_loops:
+        if self_loops:                                                          
+            v = seed.choice(nodes, p=weights / total_weight)                    
+        else:  # Ignore weight of u when selecting v                            
+            u_weight = weights[u]                                               
+            weights[u] = 0                                                      
+            v = seed.choice(nodes, p=weights / (total_weight - u_weight))       
             weights[u] = u_weight
-            total_weight += u_weight
 
         G.add_edge(u, v)
         weights[v] += 1
