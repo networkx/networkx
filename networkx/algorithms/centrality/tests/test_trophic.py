c3da99c6102d2038c349d7f6596911886752d579
Ross Barnowski <rossbar@caltech.edu>
2024-11-12 15:04:46 -0800

    Fix match statement.

--- networkx/algorithms/centrality/tests/test_trophic.py
+++ networkx/algorithms/centrality/tests/test_trophic.py
@@ -149,14 +149,8 @@ def test_trophic_levels_singular_matrix():
     """Should raise an error with graphs with only non-basal nodes"""
     matrix = np.identity(4)
     G = nx.from_numpy_array(matrix, create_using=nx.DiGraph)
-    with pytest.raises(nx.NetworkXError) as e:
+    with pytest.raises(nx.NetworkXError, match="no basal nodes"):
         nx.trophic_levels(G)
-    msg = (
-        "Trophic levels are only defined for graphs where every node "
-        + "has a path from a basal node (basal nodes are nodes with no "
-        + "incoming edges)."
-    )
-    assert msg in str(e.value)
 
 
 def test_trophic_levels_singular_with_basal():
