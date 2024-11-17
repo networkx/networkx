5c345021f47bf8eaac8b3a4830ed5216ebfc3b6a
Evans Samuel Biju <evanssamuelbiju@gmail.com>
2024-11-17 20:22:46 +0530

    formatted file

--- networkx/algorithms/k_vertex_cover/bipartite_get_top_nodes.py
+++ networkx/algorithms/k_vertex_cover/bipartite_get_top_nodes.py
@@ -14,6 +14,7 @@ def get_top_nodes(G: nx.Graph) -> list:
     bipartite graph
     """
     assert nx.is_bipartite(G), "Graph must be bipartite"
+
     top_nodes = []
     graph_coloring = color(G)
     for node in G.nodes:
