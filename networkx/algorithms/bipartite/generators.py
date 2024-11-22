f2691de54c2703d34b41e81bbd80a5e04411e2a5
Erik Welch <erik.n.welch@gmail.com>
2024-11-21 19:41:32 -0600

    Fix code formatting of some examples

--- networkx/algorithms/bipartite/generators.py
+++ networkx/algorithms/bipartite/generators.py
@@ -551,8 +551,8 @@ def gnmk_random_graph(n, m, k, seed=None, directed=False):
 
     Examples
     --------
-    from nx.algorithms import bipartite
-    G = bipartite.gnmk_random_graph(10,20,50)
+    >>> from nx.algorithms import bipartite
+    >>> G = bipartite.gnmk_random_graph(10, 20, 50)
 
     See Also
     --------
