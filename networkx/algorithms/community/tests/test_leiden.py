5b4a5ed3465953efa52854dc0a93cdb33ce7296b
Erik Welch <erik.n.welch@gmail.com>
2025-01-26 11:14:22 +0100

    Trim trailing whitespace

--- networkx/algorithms/community/tests/test_leiden.py
+++ networkx/algorithms/community/tests/test_leiden.py
@@ -18,8 +18,10 @@ def test_leiden_with_nx_backend():
     G = nx.karate_club_graph()
     with pytest.raises(NotImplementedError):
         nx.community.leiden_partitions(G, backend="networkx")
- 
- 
+    with pytest.raises(NotImplementedError):
+        nx.community.leiden_communities(G, backend="networkx")
+
+
 @pytest.mark.skipif(no_backends_for_leiden_communities)
 def test_modularity_increase():
     G = nx.LFR_benchmark_graph(
