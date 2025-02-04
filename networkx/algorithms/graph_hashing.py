567712615ec70d4cb7a1b94aaca48599709be955
Thijs Leon Vromen <tlvromen@gmail.com>
2025-02-04 22:17:14 +0100

    fmt

--- networkx/algorithms/graph_hashing.py
+++ networkx/algorithms/graph_hashing.py
@@ -33,11 +33,27 @@ def _neighborhood_aggregate(G, node, node_labels, edge_attr=None):
     Compute new labels for given node by aggregating
     the labels of each node's neighbors.
     """
-    label_list = []
-    for nbr in G.neighbors(node):
-        prefix = "" if edge_attr is None else str(G[node][nbr][edge_attr])
-        label_list.append(prefix + node_labels[nbr])
-    return node_labels[node] + "".join(sorted(label_list))
+    if nx.is_directed(G):
+        successor_labels = []
+        for nbr in G.successors(node):
+            prefix = "" if edge_attr is None else str(G[node][nbr][edge_attr])
+            successor_labels.append(prefix + node_labels[nbr])
+
+        predecessor_labels = []
+        for nbr in G.predecessors(node):
+            prefix = "" if edge_attr is None else str(G[nbr][node][edge_attr])
+            predecessor_labels.append(prefix + node_labels[nbr])
+        return (
+            node_labels[node]
+            + "".join(sorted(successor_labels))
+            + "".join(sorted(predecessor_labels))
+        )
+    else:
+        label_list = []
+        for nbr in G.neighbors(node):
+            prefix = "" if edge_attr is None else str(G[node][nbr][edge_attr])
+            label_list.append(prefix + node_labels[nbr])
+        return node_labels[node] + "".join(sorted(label_list))
 
 
 @nx.utils.not_implemented_for("multigraph")
