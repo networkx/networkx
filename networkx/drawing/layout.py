64ce49c9744722399ca51713eba3b1b41ef14dee
Ross Barnowski <rossbar@caltech.edu>
2025-02-04 16:46:06 -0800

    Bipartite layout nodes optional (#7756)
    * Make nodes param optional in bipartite layout.
    
    * Add tests for new behavior.
    
    * Update docstring parameter description and examples.
    
    * Add draw_bipartite convenience func.
    
    * Implement draw_bipartite.
    
    ---------
    
    Co-authored-by: Mridul Seth <git@mriduls.com>

--- networkx/drawing/layout.py
+++ networkx/drawing/layout.py
@@ -305,7 +305,7 @@ def shell_layout(
 
 def bipartite_layout(
     G,
-    nodes,
+    nodes=None,
     align="vertical",
     scale=1,
     center=None,
@@ -319,9 +319,10 @@ def bipartite_layout(
     G : NetworkX graph or list of nodes
         A position will be assigned to every node in G.
 
-    nodes : list or container
-        Nodes in one node set of the bipartite graph.
-        This set will be placed on left or top.
+    nodes : collection of nodes
+        Nodes in one node set of the graph. This set will be placed on
+        left or top. If `None` (the default), a node set is chosen arbitrarily
+        if the graph if bipartite.
 
     align : string (default='vertical')
         The alignment of nodes. Vertical or horizontal.
@@ -345,15 +346,35 @@ def bipartite_layout(
     pos : dict
         A dictionary of positions keyed by node.
 
+    Raises
+    ------
+    NetworkXError
+        If ``nodes=None`` and `G` is not bipartite.
+
     Examples
     --------
-    >>> G = nx.bipartite.gnmk_random_graph(3, 5, 10, seed=123)
-    >>> top = nx.bipartite.sets(G)[0]
-    >>> pos = nx.bipartite_layout(G, top)
-    >>> # supress the returned dict and store on the graph directly
-    >>> _ = nx.bipartite_layout(G, top, store_pos_as="pos")
+    >>> G = nx.complete_bipartite_graph(3, 3)
+    >>> pos = nx.bipartite_layout(G)
+
+    The ordering of the layout (i.e. which nodes appear on the left/top) can
+    be specified with the `nodes` parameter:
+
+    >>> top, bottom = nx.bipartite.sets(G)
+    >>> pos = nx.bipartite_layout(G, nodes=bottom)  # "bottom" set appears on the left
+
+    `store_pos_as` can be used to store the node positions for the computed layout
+    directly on the nodes:
+
+    >>> _ = nx.bipartite_layout(G, nodes=bottom, store_pos_as="pos")
     >>> nx.get_node_attributes(G, "pos")
-    {0: array([-1. , -0.6]), 1: array([-1.,  0.]), 2: array([-1. ,  0.6]), 3: array([ 0.6, -0.6]), 4: array([ 0.6, -0.3]), 5: array([0.6, 0. ]), 6: array([0.6, 0.3]), 7: array([0.6, 0.6])}
+    {0: array([ 1.  , -0.75]), 1: array([1., 0.]), 2: array([1.  , 0.75]), 3: array([-1.  , -0.75]), 4: array([-1.,  0.]), 5: array([-1.  ,  0.75])}
+
+
+    The ``bipartite_layout`` function can be used with non-bipartite graphs
+    by explicitly specifying how the layout should be partitioned with `nodes`:
+
+    >>> G = nx.complete_graph(5)  # Non-bipartite
+    >>> pos = nx.bipartite_layout(G, nodes={0, 1, 2})
 
     Notes
     -----
@@ -376,9 +397,14 @@ def bipartite_layout(
     width = aspect_ratio * height
     offset = (width / 2, height / 2)
 
-    top = dict.fromkeys(nodes)
-    bottom = [v for v in G if v not in top]
-    nodes = list(top) + bottom
+    if nodes is None:
+        top, bottom = nx.bipartite.sets(G)
+        nodes = list(G)
+    else:
+        top = set(nodes)
+        bottom = set(G) - top
+        # Preserves backward-compatible node ordering in returned pos dict
+        nodes = list(top) + list(bottom)
 
     left_xs = np.repeat(0, len(top))
     right_xs = np.repeat(width, len(bottom))
