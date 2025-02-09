1526acc676a1dabb5f50055f85b4e80115a6950b
Erik Welch <erik.n.welch@gmail.com>
2025-02-09 19:36:29 +0100

    Fix docstring example of `nx.generate_random_paths(index_map=...)` (#7832)
    * Fix docstring example of `nx.generate_random_paths(index_map=...)`
    
    * Using list is simpler/better

--- networkx/algorithms/similarity.py
+++ networkx/algorithms/similarity.py
@@ -1711,9 +1711,9 @@ def generate_random_paths(
 
     >>> G = nx.star_graph(3)
     >>> index_map = {}
-    >>> random_path = nx.generate_random_paths(G, 3, index_map=index_map)
+    >>> random_paths = list(nx.generate_random_paths(G, 3, index_map=index_map))
     >>> paths_containing_node_0 = [
-    ...     random_path[path_idx] for path_idx in index_map.get(0, [])
+    ...     random_paths[path_idx] for path_idx in index_map.get(0, [])
     ... ]
 
     References
