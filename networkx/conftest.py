88de938f25426d37b597dd4665cb435dbbd23af2
Ross Barnowski <rossbar@caltech.edu>
2025-02-10 15:17:05 -0800

    Rm total_spanning_tree_weight.

--- networkx/conftest.py
+++ networkx/conftest.py
@@ -112,9 +112,6 @@ def set_warnings():
         "ignore", category=DeprecationWarning, message="\n\nThe `normalized`"
     )
     warnings.filterwarnings(
-        "ignore", category=DeprecationWarning, message="\n\ntotal_spanning_tree_weight"
-    )
-    warnings.filterwarnings(
         "ignore", category=DeprecationWarning, message=r"\n\nThe 'create=matrix'"
     )
     warnings.filterwarnings(
