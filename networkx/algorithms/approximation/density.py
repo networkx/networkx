cc8cb958f2cc5e9357aab840a969a7d6f8de6d6f
Ross Barnowski <rossbar@caltech.edu>
2025-02-10 13:29:41 -0800

    Update private fn docstring formatting.

--- networkx/algorithms/approximation/density.py
+++ networkx/algorithms/approximation/density.py
@@ -67,16 +67,25 @@ def _fractional_peeling(G, b, x, node_to_idx, edge_to_idx):
     """
     Optimized fractional peeling using NumPy arrays.
 
-    Parameters:
-        G (networkx.Graph): The input graph.
-        b (numpy.ndarray): Induced load vector.
-        x (numpy.ndarray): Fractional edge values.
-        node_to_idx (dict): Mapping from node to index.
-        edge_to_idx (dict): Mapping from edge to index.
+    Parameters
+    ----------
+        G : networkx.Graph
+            The input graph.
+        b : numpy.ndarray
+            Induced load vector.
+        x : numpy.ndarray
+            Fractional edge values.
+        node_to_idx : dict
+            Mapping from node to index.
+        edge_to_idx : dict
+            Mapping from edge to index.
 
-    Returns:
-        best_density (float): The best density found.
-        best_subgraph (set): The subset of nodes defining the densest subgraph.
+    Returns
+    -------
+        best_density : float
+            The best density found.
+        best_subgraph : set
+            The subset of nodes defining the densest subgraph.
     """
     heap = nx.utils.BinaryHeap()
 
