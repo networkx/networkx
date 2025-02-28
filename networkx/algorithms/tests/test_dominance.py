aca1e95c27f191bddbb49ba09e4d295948f890ff
Erik Welch <erik.n.welch@gmail.com>
2025-02-28 11:05:38 -0600

    Remove unnecessary `dict(...)` for SSSP algos that return dicts (#7878)
    * Remove unnecessary `dict(...)` for SSSP algos that return dicts
    
    * Also update for `shortest_path` and `shortest_path_length`

--- networkx/algorithms/tests/test_dominance.py
+++ networkx/algorithms/tests/test_dominance.py
@@ -120,7 +120,7 @@ class TestDominanceFrontiers:
         """
         edges = [(1, 2), (2, 1), (3, 2), (4, 1), (5, 3), (5, 4)]
         G = nx.DiGraph(edges)
-        assert dict(nx.dominance_frontiers(G, 5).items()) == {
+        assert nx.dominance_frontiers(G, 5) == {
             1: {2},
             2: {1},
             3: {2},
