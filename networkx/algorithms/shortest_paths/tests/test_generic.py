dedb04576ca7ef8811ddde98f16498ff6a4f3193
Erik Welch <erik.n.welch@gmail.com>
2025-02-27 22:04:37 -0600

    Remove unnecessary `dict(...)` for SSSP algos that return dicts

--- networkx/algorithms/shortest_paths/tests/test_generic.py
+++ networkx/algorithms/shortest_paths/tests/test_generic.py
@@ -149,13 +149,13 @@ class TestGenericPath:
     def test_single_source_shortest_path_length(self):
         ans = dict(nx.shortest_path_length(self.cycle, 0))
         assert ans == {0: 0, 1: 1, 2: 2, 3: 3, 4: 3, 5: 2, 6: 1}
-        assert ans == dict(nx.single_source_shortest_path_length(self.cycle, 0))
+        assert ans == nx.single_source_shortest_path_length(self.cycle, 0)
         ans = dict(nx.shortest_path_length(self.grid, 1))
         assert ans[16] == 6
         # now with weights
         ans = dict(nx.shortest_path_length(self.cycle, 0, weight="weight"))
         assert ans == {0: 0, 1: 1, 2: 2, 3: 3, 4: 3, 5: 2, 6: 1}
-        assert ans == dict(nx.single_source_dijkstra_path_length(self.cycle, 0))
+        assert ans == nx.single_source_dijkstra_path_length(self.cycle, 0)
         ans = dict(nx.shortest_path_length(self.grid, 1, weight="weight"))
         assert ans[16] == 6
         # weights and method specified
@@ -163,14 +163,14 @@ class TestGenericPath:
             nx.shortest_path_length(self.cycle, 0, weight="weight", method="dijkstra")
         )
         assert ans == {0: 0, 1: 1, 2: 2, 3: 3, 4: 3, 5: 2, 6: 1}
-        assert ans == dict(nx.single_source_dijkstra_path_length(self.cycle, 0))
+        assert ans == nx.single_source_dijkstra_path_length(self.cycle, 0)
         ans = dict(
             nx.shortest_path_length(
                 self.cycle, 0, weight="weight", method="bellman-ford"
             )
         )
         assert ans == {0: 0, 1: 1, 2: 2, 3: 3, 4: 3, 5: 2, 6: 1}
-        assert ans == dict(nx.single_source_bellman_ford_path_length(self.cycle, 0))
+        assert ans == nx.single_source_bellman_ford_path_length(self.cycle, 0)
 
     def test_single_source_all_shortest_paths(self):
         cycle_ans = {0: [[0]], 1: [[0, 1]], 2: [[0, 1, 2], [0, 3, 2]], 3: [[0, 3]]}
