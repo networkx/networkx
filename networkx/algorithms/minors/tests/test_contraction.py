61f3b695d9eb5a4c156fe20a2948739ed592cf51
Ross Barnowski <rossbar@caltech.edu>
2025-03-08 14:15:12 -0800

    Parametrize node attr test on store_contraction.

--- networkx/algorithms/minors/tests/test_contraction.py
+++ networkx/algorithms/minors/tests/test_contraction.py
@@ -380,26 +380,33 @@ def test_multigraph_keys():
     assert edges_equal(actual.edges, expected.edges)
 
 
+@pytest.mark.parametrize("store_contraction", (True, False))
 @pytest.mark.parametrize("copy", (True, False))
 @pytest.mark.parametrize("selfloops", (True, False))
-def test_node_attributes(copy, selfloops):
+def test_node_attributes(store_contraction, copy, selfloops):
     """Tests that node contraction preserves node attributes."""
     G = nx.cycle_graph(4)
     # Add some data to the two nodes being contracted.
     G.nodes[0]["foo"] = "bar"
     G.nodes[1]["baz"] = "xyzzy"
-    actual = nx.contracted_nodes(G, 0, 1, copy=copy, self_loops=selfloops)
+    actual = nx.contracted_nodes(
+        G, 0, 1, copy=copy, self_loops=selfloops, store_contraction=store_contraction
+    )
     # We expect that contracting the nodes 0 and 1 in C_4 yields K_3, but
     # with nodes labeled 0, 2, and 3.
     expected = nx.complete_graph(3)
     expected = nx.relabel_nodes(expected, {1: 2, 2: 3})
+    expected.nodes[0]["foo"] = "bar"
     # ... and a self-loop (0, 0), if self_loops=True
     if selfloops:
         expected.add_edge(0, 0)
-    cdict = {1: {"baz": "xyzzy"}}
-    expected.nodes[0].update({"foo": "bar", "contraction": cdict})
+
+    if store_contraction:
+        cdict = {1: {"baz": "xyzzy"}}
+        expected.nodes[0].update({"foo": "bar", "contraction": cdict})
+
     assert nx.is_isomorphic(actual, expected)
-    assert actual.nodes == expected.nodes
+    assert actual.nodes(data=True) == expected.nodes(data=True)
     if not copy:
         assert actual is G
 
