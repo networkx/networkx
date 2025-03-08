e4b42ec072b3fb4166e459d516696e1651635de5
Ross Barnowski <rossbar@caltech.edu>
2025-03-08 14:16:49 -0800

    Rm redundant test_without_selfloops - covered by parametrization.

--- networkx/algorithms/minors/tests/test_contraction.py
+++ networkx/algorithms/minors/tests/test_contraction.py
@@ -430,14 +430,6 @@ def test_edge_attributes():
     assert len(H.edges(("src1", "dest"))) == 2
 
 
-def test_without_self_loops():
-    """Tests for node contraction without preserving -loops."""
-    G = nx.cycle_graph(4)
-    actual = nx.contracted_nodes(G, 0, 1, self_loops=False)
-    expected = nx.complete_graph(3)
-    assert nx.is_isomorphic(actual, expected)
-
-
 def test_contract_loop_graph():
     """Tests for node contraction when nodes have loops."""
     G = nx.cycle_graph(4)
@@ -445,11 +437,9 @@ def test_contract_loop_graph():
     actual = nx.contracted_nodes(G, 0, 1)
     expected = nx.complete_graph([0, 2, 3])
     expected.add_edge(0, 0)
-    expected.add_edge(0, 0)
     assert edges_equal(actual.edges, expected.edges)
     actual = nx.contracted_nodes(G, 1, 0)
     expected = nx.complete_graph([1, 2, 3])
-    expected.add_edge(1, 1)
     expected.add_edge(1, 1)
     assert edges_equal(actual.edges, expected.edges)
 
