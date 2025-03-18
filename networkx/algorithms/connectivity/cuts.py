547c830d2d7ce1b3e9585682431dce1447e6f834
Sure akshita <sureakshita23@gmail.com>
2025-03-18 15:22:31 +0530

    Fixed the return type of minimum_st_node_cut

--- networkx/algorithms/connectivity/cuts.py
+++ networkx/algorithms/connectivity/cuts.py
@@ -206,6 +206,9 @@ def minimum_st_node_cut(G, s, t, flow_func=None, auxiliary=None, residual=None):
     cutset : set
         Set of nodes that, if removed, would destroy all paths between
         source and target in G.
+        
+        Returns an empty set if source and target are directly connected
+        by an edge in G, as no nodes can destroy their path.  
 
     Examples
     --------
@@ -291,7 +294,7 @@ def minimum_st_node_cut(G, s, t, flow_func=None, auxiliary=None, residual=None):
     if mapping is None:
         raise nx.NetworkXError("Invalid auxiliary digraph.")
     if G.has_edge(s, t) or G.has_edge(t, s):
-        return {}
+        return set()
     kwargs = {"flow_func": flow_func, "residual": residual, "auxiliary": H}
 
     # The edge cut in the auxiliary digraph corresponds to the node cut in the
