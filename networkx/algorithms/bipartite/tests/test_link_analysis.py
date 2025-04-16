37649eebc8ccac3b837281f3d51425daa948beff
Colman Bouton <cole.bouton@gmail.com>
2025-04-15 17:57:59 -0700

    Add unit tests for BiRank

---
+++ networkx/algorithms/bipartite/tests/test_link_analysis.py
@@ -0,0 +1,108 @@
+import pytest
+
+import networkx as nx
+from networkx.algorithms import bipartite
+
+np = pytest.importorskip("numpy")
+sp = pytest.importorskip("scipy")
+
+
+class TestBipartiteLinkAnalysis:
+    def test_collaborative_filtering_birank(self):
+        elist = [
+            ("u1", "p1", 5),
+            ("u2", "p1", 5),
+            ("u2", "p2", 4),
+            ("u3", "p1", 3),
+            ("u3", "p3", 2),
+        ]
+        item_recommendation_graph = nx.DiGraph()
+        item_recommendation_graph.add_weighted_edges_from(elist, weight="rating")
+        product_nodes = ("p1", "p2", "p3")
+        u1_query = {
+            product: rating
+            for _, product, rating in item_recommendation_graph.edges(
+                nbunch="u1", data="rating"
+            )
+        }
+        u1_birank_results = bipartite.birank(
+            item_recommendation_graph,
+            product_nodes,
+            alpha=0.8,
+            beta=1.0,
+            top_personalization=u1_query,
+            weight="rating",
+        )
+        u1_recommendations = dict(
+            filter(
+                lambda item: item[0][0] == "p"
+                and "u1" not in item_recommendation_graph.neighbors(item[0]),
+                u1_birank_results.items(),
+            )
+        )
+        assert u1_recommendations["p2"] > u1_recommendations["p3"] + 2 * 1e-6
+
+        u1_birank_results_unweighted = bipartite.birank(
+            item_recommendation_graph,
+            product_nodes,
+            alpha=0.8,
+            beta=1.0,
+            top_personalization=u1_query,
+            weight=None,
+        )
+
+        u1_recommendations_unweighted = dict(
+            filter(
+                lambda item: item[0][0] == "p"
+                and "u1" not in item_recommendation_graph.neighbors(item[0]),
+                u1_birank_results_unweighted.items(),
+            )
+        )
+
+        assert u1_recommendations["p2"] == pytest.approx(
+            u1_recommendations["p3"], rel=1e-6
+        )
+
+    def test_davis_birank(self):
+        G = nx.davis_southern_women_graph()
+        women = set(
+            [node for node, bipartite in G.nodes(data="bipartite") if bipartite == 0]
+        )
+        scores = bipartite.birank(G, women)
+        answer = {
+            "Nora Fayette": 0.08,
+            "Flora Price": 0.04,
+            "Helen Lloyd": 0.06,
+            "Verne Sanderson": 0.05,
+            "Brenda Rogers": 0.07,
+            "Myra Liddel": 0.05,
+            "Dorothy Murchison": 0.04,
+            "Eleanor Nye": 0.05,
+            "Theresa Anderson": 0.08,
+            "Ruth DeSand": 0.05,
+            "Pearl Oglethorpe": 0.05,
+            "Charlotte McDowd": 0.05,
+            "Laura Mandeville": 0.07,
+            "Katherina Rogers": 0.07,
+            "Sylvia Avondale": 0.07,
+            "Frances Anderson": 0.05,
+            "Evelyn Jefferson": 0.08,
+            "Olivia Carleton": 0.04,
+            "E1": 0.05,
+            "E2": 0.05,
+            "E13": 0.05,
+            "E12": 0.07,
+            "E4": 0.05,
+            "E14": 0.05,
+            "E11": 0.05,
+            "E3": 0.07,
+            "E7": 0.09,
+            "E10": 0.06,
+            "E8": 0.1,
+            "E9": 0.09,
+            "E6": 0.08,
+            "E5": 0.08,
+        }
+
+        for node, value in answer.items():
+            assert value == pytest.approx(scores[node], abs=1e-2)
