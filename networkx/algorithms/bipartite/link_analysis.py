76de90229c49f56df7c702a4afbe4a0a10c08dff
Colman Bouton <cole.bouton@gmail.com>
2025-04-18 15:02:49 -0700

    Adjust docstring line lengths to <88 chars

--- networkx/algorithms/bipartite/link_analysis.py
+++ networkx/algorithms/bipartite/link_analysis.py
@@ -19,40 +19,51 @@ def birank(
 ):
     r"""Compute the BiRank score for nodes in a bipartite network.
 
-    Given the bipartite sets $U$ and $P$, the BiRank algorithm seeks to satisfy the following
-    recursive relationships between the scores of nodes $j \in P$ and $i \in U$:
+    Given the bipartite sets $U$ and $P$, the BiRank algorithm seeks to satisfy
+    the following recursive relationships between the scores of nodes $j \in P$
+    and $i \in U$:
 
     .. math::
 
-       p_j = \alpha \sum_{i \in U} \frac{w_{ij}}{\sqrt{d_i}\sqrt{d_j}} u_i + (1 - \alpha) p_j^0
+       p_j = \alpha \sum_{i \in U} \frac{w_{ij}}{\sqrt{d_i}\sqrt{d_j}} u_i
+       + (1 - \alpha) p_j^0
 
-       u_i = \beta \sum_{j \in P} \frac{w_{ij}}{\sqrt{d_i}\sqrt{d_j}} p_j + (1 - \beta) u_i^0
+       u_i = \beta \sum_{j \in P} \frac{w_{ij}}{\sqrt{d_i}\sqrt{d_j}} p_j
+       + (1 - \beta) u_i^0
 
     where
 
     * $p_j$ and $u_i$ are the BiRank scores of nodes $j \in P$ and $i \in U$.
-    * $w_{ij}$ is the weight of the edge between nodes $i \in U$ and $j \in P$ (With a value of 0 if no edge exists).
-    * $d_i$ and $d_j$ are the weighted degrees of nodes $i \in U$ and $j \in P$, respectively.
-    * $p_j^0$ and $u_i^0$ are personalization values that can encode a priori weights for the nodes $j \in P$ and $i \in U$, respectively. Akin to the personalization vector used by PageRank.
-    * $\alpha$ and $\beta$ are damping hyperparameters applying to nodes in $P$ and $U$ respectively. They have values in the interval $[0, 1]$ and are analogous to those used by PageRank.
+    * $w_{ij}$ is the weight of the edge between nodes $i \in U$ and $j \in P$
+      (With a value of 0 if no edge exists).
+    * $d_i$ and $d_j$ are the weighted degrees of nodes $i \in U$ and $j \in P$,
+      respectively.
+    * $p_j^0$ and $u_i^0$ are personalization values that can encode a priori
+      weights for the nodes $j \in P$ and $i \in U$, respectively. Akin to the
+      personalization vector used by PageRank.
+    * $\alpha$ and $\beta$ are damping hyperparameters applying to nodes in $P$
+      and $U$ respectively. They can take values in the interval $[0, 1]$, and
+      are analogous to those used by PageRank.
 
     Below are two use cases for this algorithm.
 
     1. Personalized Recommendation System
-        Given a bipartite graph representing users
-        and items, BiRank can be used as a collaborative filtering algorithm
-        to recommend items to users. Previous ratings are encoded as edge weights,
-        and the specific ratings of an individual user on a set of items is used as
-        the personalization vector over items. See the example below for an implementation of
-        this on a toy dataset provided in [1]_.
+        Given a bipartite graph representing users and items, BiRank can be used
+        as a collaborative filtering algorithm to recommend items to users.
+        Previous ratings are encoded as edge weights, and the specific ratings
+        of an individual user on a set of items is used as the personalization
+        vector over items. See the example below for an implementation of this
+        on a toy dataset provided in [1]_.
 
     2. Popularity Prediction
-        Given a bipartite graph representing user interactions with items,
-        e.g. commits to a GitHub repository, BiRank can be used to predict the popularity of a given
-        item. Edge weights should encode the strength of the interaction signal. This could be a raw
-        count, or weighted by a time decay function like that specified in Eq. (15) of [1]_. The
-        personalization vectors can be used to encode existing popularity signals, for example, the
-        monthly download count of a repository's package.
+        Given a bipartite graph representing user interactions with items, e.g.
+        commits to a GitHub repository, BiRank can be used to predict the
+        popularity of a given item. Edge weights should encode the strength of
+        the interaction signal. This could be a raw count, or weighted by a time
+        decay function like that specified in Eq. (15) of [1]_. The
+        personalization vectors can be used to encode existing popularity
+        signals, for example, the monthly download count of a repository's
+        package.
 
     Parameters
     ----------
@@ -60,32 +71,37 @@ def birank(
        A bipartite network
 
     nodes : list or container
-      Container with all nodes belonging to the first bipartite node set ('top'). The nodes
-      in this set use the hyperparameter `alpha`, and the personalization
-      dictionary `top_personalization`. The nodes in the second bipartite node set ('bottom') are
-      automatically determined by taking the complement of 'top' with respect to the graph `G`.
+      Container with all nodes belonging to the first bipartite node set
+      ('top'). The nodes in this set use the hyperparameter `alpha`, and the
+      personalization dictionary `top_personalization`. The nodes in the second
+      bipartite node set ('bottom') are automatically determined by taking the
+      complement of 'top' with respect to the graph `G`.
 
-    alpha : float, optional (default=0.80 if top_personalization is not empty, 1 otherwise)
+    alpha : float, optional (default=0.80 if top_personalization not empty, else 1)
+
       Damping factor for the 'top' nodes. Must be in the interval $[0, 1]$.
 
-    beta : float, optional (default=0.80 if bottom_personalization is not empty, 1 otherwise)
+    beta : float, optional (default=0.80 if bottom_personalization not empty, else 1)
+
       Damping factor for the 'bottom' nodes. Must be in the interval $[0, 1]$.
 
     top_personalization : dict, optional (default={})
-      Dictionary keyed by nodes in 'top' with the personalization value as the value. Unspecified
-      nodes will be assigned a personalization value of 0.
+      Dictionary keyed by nodes in 'top' with the personalization value as the
+      value. Unspecified nodes will be assigned a personalization value of 0.
 
     bottom_personalization : dict, optional (default={})
-      Dictionary keyed by nodes in 'bottom' with the personalization value as the value. Unspecified
-      nodes will be assigned a personalization value of 0.
+      Dictionary keyed by nodes in 'bottom' with the personalization value as
+      the value. Unspecified nodes will be assigned a personalization value of
+      0.
 
     max_iter : int, optional (default=100)
       Maximum number of iterations in power method eigenvalue solver.
 
     tol : float, optional (default=1.0e-6)
-      Error tolerance used to check convergence in power method solver. The iteration will stop
-      after a tolerance of both ``len(top) * tol`` and ``len(bottom) * tol`` is
-      reached for nodes in 'top' and 'bottom' respectively.
+      Error tolerance used to check convergence in power method solver. The
+      iteration will stop after a tolerance of both ``len(top) * tol`` and
+      ``len(bottom) * tol`` is reached for nodes in 'top' and 'bottom'
+      respectively.
 
     weight : string or None, optional (default='weight')
       Edge data key to use as weight.
@@ -109,8 +125,8 @@ def birank(
     Examples
      --------
      Construct a bipartite graph with user-item ratings and use BiRank to
-     recommend items to a user (user 1). The example below uses the `rating` edge
-     attribute as the weight of the edges. The `top_personalization` vector
+     recommend items to a user (user 1). The example below uses the `rating`
+     edge attribute as the weight of the edges. The `top_personalization` vector
      is used to encode the user's previous ratings on items.
 
      Creation of graph, bipartite sets for the example.
@@ -127,8 +143,9 @@ def birank(
      >>> product_nodes = ("p1", "p2", "p3")
      >>> user = "u1"
 
-     First, we create a personalization vector for the user based on on their ratings of past items.
-     In this case they have only rated one item (p1, with a rating of 5) in the past.
+     First, we create a personalization vector for the user based on on their
+     ratings of past items. In this case they have only rated one item (p1, with
+     a rating of 5) in the past.
 
      >>> user_personalization = {
      ...  product: rating \
@@ -138,8 +155,8 @@ def birank(
      >>> user_personalization
      {'p1': 5}
 
-     Calculate the BiRank score of all nodes in the graph, filter for the items that the
-     user has not rated yet, and sort the results by score.
+     Calculate the BiRank score of all nodes in the graph, filter for the items
+     that the user has not rated yet, and sort the results by score.
 
      >>> user_birank_results = nx.bipartite.birank(
      ...  G, product_nodes, \
@@ -163,9 +180,10 @@ def birank(
      >>> user_recommendations
      {'p2': 1.44818, 'p3': 1.04811}
 
-     We find that user 1 should be recommended item p2 over item p3. This is due to the fact that user 2
-     rated also rated p1 highly, while user 3 did not. Thus user 2's tastes are inferred to be similar
-     to user 1's, and carry more weight in the recommendation.
+     We find that user 1 should be recommended item p2 over item p3. This is due
+     to the fact that user 2 rated also rated p1 highly, while user 3 did not.
+     Thus user 2's tastes are inferred to be similar to user 1's, and carry more
+     weight in the recommendation.
 
     See Also
     --------
@@ -207,7 +225,8 @@ def birank(
 
     if top_count == 0 or bottom_count == 0:
         raise nx.NetworkXAlgorithmError(
-            "The BiRank algorithm requires a bipartite graph with at least one node in each set."
+            "The BiRank algorithm requires a bipartite graph with at least one"
+            "node in each set."
         )
 
     # Set default values for alpha and beta if not provided
