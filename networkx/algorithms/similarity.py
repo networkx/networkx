813ce78a34990d9029d07ce589f52e45662f7e4b
Michael Recachinas <mrecachinas@github.com>
2025-05-04 15:48:30 -0400

    Update networkx/algorithms/similarity.py
    Co-authored-by: Alejandro Candioti <amcandio@gmail.com>

--- networkx/algorithms/similarity.py
+++ networkx/algorithms/similarity.py
@@ -1607,11 +1607,10 @@ def _prepare_panther_paths(
     index_map = {}
 
     # Generate the random paths and populate the index_map
-    list(
-        generate_random_paths(
-            G, sample_size, path_length=path_length, index_map=index_map, weight=weight
-        )
-    )
+        for _ in generate_random_paths( 
+             G, sample_size, path_length=path_length, index_map=index_map, weight=weight
+        ):
+            pass
 
     # Return a named tuple with all the required data
     return PantherPaths(
