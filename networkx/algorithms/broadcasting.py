fdf57d9a5bea9d52fe155de90ba621ab607d676e
Gilles Peiffer <gilles.peiffer.yt@gmail.com>
2025-05-31 19:59:39 +0200

    Clean up `tree_broadcast_time` docstring

--- networkx/algorithms/broadcasting.py
+++ networkx/algorithms/broadcasting.py
@@ -110,32 +110,38 @@ def tree_broadcast_center(G):
 @not_implemented_for("multigraph")
 @nx._dispatchable
 def tree_broadcast_time(G, node=None):
-    """Return the Broadcast Time of the tree `G`.
+    """Return the broadcast time of the tree `G`.
 
     The minimum broadcast time of a node is defined as the minimum amount
-    of time required to complete broadcasting starting from the
-    originator. The broadcast time of a graph is the maximum over
+    of time required to complete broadcasting starting from that node.
+    The broadcast time of a graph is the maximum over
     all nodes of the minimum broadcast time from that node [1]_.
     This function returns the minimum broadcast time of `node`.
-    If `node` is None the broadcast time for the graph is returned.
+    If `node` is `None`, the broadcast time for the graph is returned.
 
     Parameters
     ----------
-    G : undirected graph
-        The graph should be an undirected tree
-    node: int, optional
-        index of starting node. If `None`, the algorithm returns the broadcast
-        time of the tree.
+    G : Graph
+        The graph should be an undirected tree.
+
+    node: node, optional (default=None)
+        Starting node for the broadcasting. If `None`, the algorithm returns the broadcast
+        time of the graph instead.
 
     Returns
     -------
-    BT : int
-        Broadcast Time of a node in a tree
+    int
+        Broadcast time of `node` in `G`, or broadcast time of `G` if no node is provided.
 
     Raises
     ------
     NetworkXNotImplemented
-        If the graph is directed or is a multigraph.
+        If `G` is directed or is a multigraph.
+
+    NetworkXError
+        If `G` is not a tree.
+
+        If `node` is not a node in `G`.
 
     References
     ----------
