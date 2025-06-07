4e1064f8e280156794841069d634e72502a13858
Ross Barnowski <rossbar@caltech.edu>
2025-06-07 11:02:55 -0700

    TST: tweak regex.

--- networkx/tests/test_removed_functions_exception_messages.py
+++ networkx/tests/test_removed_functions_exception_messages.py
@@ -4,5 +4,5 @@ import networkx as nx
 
 
 def test_random_tree():
-    with pytest.raises(AttributeError, match=".*use nx.random_labeled_tree instead"):
+    with pytest.raises(AttributeError, match=".*Use `nx.random_labeled_tree` instead"):
         nx.random_tree(3)
