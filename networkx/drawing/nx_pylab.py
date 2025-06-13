5a31d44018c6f222f1a459bdb00a87ce687b9545
Gilles Peiffer <gilles.peiffer.yt@gmail.com>
2025-06-13 15:07:34 +0200

    Also update `display_*` version

--- networkx/drawing/nx_pylab.py
+++ networkx/drawing/nx_pylab.py
@@ -720,26 +720,80 @@ def display(
 
         def _update_text_pos_angle(self, arrow):
             # Fractional label position
-            path_disp = self._get_arrow_path_disp(arrow)
-            (x1, y1), (cx, cy), (x2, y2) = path_disp.vertices
             # Text position at a proportion t along the line in display coords
             # default is 0.5 so text appears at the halfway point
             t = self.label_pos
             tt = 1 - t
-            x = tt**2 * x1 + 2 * t * tt * cx + t**2 * x2
-            y = tt**2 * y1 + 2 * t * tt * cy + t**2 * y2
+            path_disp = self._get_arrow_path_disp(arrow)
+            conn = arrow.get_connectionstyle()
+            is_line = True
+            # 1. Calculate x and y
+            points = path_disp.vertices
+            if isinstance(
+                conn,
+                mpl.patches.ConnectionStyle.Arc3 | mpl.patches.ConnectionStyle.Angle3,
+            ):
+                # Arc3 or Angle3 type Connection Styles - Bezier curve
+                (x1, y1), (cx, cy), (x2, y2) = points
+                x = tt**2 * x1 + 2 * t * tt * cx + t**2 * x2
+                y = tt**2 * y1 + 2 * t * tt * cy + t**2 * y2
+                is_line = False
+            else:
+                if isinstance(conn, mpl.patches.ConnectionStyle.Bar):
+                    # Bar Connection Style - straight line
+                    assert len(points) == 4
+                    lines = [points[1:3]]
+                else:
+                    if isinstance(conn, mpl.patches.ConnectionStyle.Angle):
+                        assert len(points) in {3, 5}
+                        lines = [points[:2], points[-2:]]
+                    else:
+                        assert isinstance(conn, mpl.patches.ConnectionStyle.Arc)
+                        if len(points) == 2:
+                            lines = [points]
+                        elif len(points) == 5:
+                            lines = [points[:2], points[-2:]]
+                        else:
+                            assert len(points) == 8
+                            lines = [points[:2], points[3:5], points[-2:]]
+
+                    if len(lines) != 1:
+                        assert len(lines) in {2, 3}
+                        dists = [math.dist(*line) for line in lines]
+                        dist_tot = sum(dists)
+                        cdist = 0
+                        last_cut = 0
+                        last_i = len(dists) - 1
+                        for i, dist in enumerate(dists):
+                            cdist += dist
+                            cut = cdist / dist_tot
+                            if i == last_i or t <= cut:
+                                t = (t - last_cut) / (dist / dist_tot)
+                                tt = 1 - t
+                                lines = [lines[i]]
+                                break
+                            last_cut = cut
+                [[(cx1, cy1), (cx2, cy2)]] = lines
+                x = cx1 * tt + cx2 * t
+                y = cy1 * tt + cy2 * t
+
+            # 2. Calculate Angle
             if self.labels_horizontal:
                 # Horizontal text labels
                 angle = 0
             else:
                 # Labels parallel to curve
-                change_x = 2 * tt * (cx - x1) + 2 * t * (x2 - cx)
-                change_y = 2 * tt * (cy - y1) + 2 * t * (y2 - cy)
-                angle = (np.arctan2(change_y, change_x) / (2 * np.pi)) * 360
+                if is_line:
+                    change_x = (cx2 - cx1) / 2
+                    change_y = (cy2 - cy1) / 2
+                else:
+                    change_x = 2 * tt * (cx - x1) + 2 * t * (x2 - cx)
+                    change_y = 2 * tt * (cy - y1) + 2 * t * (y2 - cy)
+                angle = np.arctan2(change_y, change_x) / (2 * np.pi) * 360
                 # Text is "right way up"
                 if angle > 90:
                     angle -= 180
-                if angle < -90:
+                elif angle < -90:
                     angle += 180
             (x, y) = self.ax.transData.inverted().transform((x, y))
             return x, y, angle
