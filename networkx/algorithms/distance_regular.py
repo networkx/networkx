54702916ae76a8fdc9844c1fa9734d798565c93e
Anthony Labarre <48905616+alabarre@users.noreply.github.com>
2025-07-21 14:05:58 +0200

    Update networkx/algorithms/distance_regular.py
    Co-authored-by: Gilles Peiffer <gilles.peiffer.yt@gmail.com>

--- networkx/algorithms/distance_regular.py
+++ networkx/algorithms/distance_regular.py
@@ -176,7 +176,7 @@ def intersection_array(G):
     max_diameter_for_dr_graphs = (8 * log(len(G), 2)) / 3
     for u, v in combinations_with_replacement(G, 2):
         # compute needed shortest path lengths
-        if u not in path_length or v not in path_length[u]:
+        if v not in path_length[u]:
             path_length[u].update(nx.single_source_shortest_path_length(G, u))
             for x, distance in path_length[u].items():
                 path_length[x][u] = distance
