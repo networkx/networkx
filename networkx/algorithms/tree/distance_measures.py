dda22e21ff6a2ada2e2303892f97e9200857a887
Peiffap <gilles.peiffer@uclouvain.be>
2025-08-01 15:55:36 +0200

    api: move `tree.center` to new `tree.distance_measures` subpackage

---
+++ networkx/algorithms/tree/distance_measures.py
@@ -0,0 +1,81 @@
+"""
+Algorithms for computing distance measures on trees.
+"""
+
+import networkx as nx
+from networkx.utils import not_implemented_for
+
+__all__ = [
+    "center",
+]
+
+
+@not_implemented_for("directed")
+def center(G):
+    """Returns the center of an undirected tree graph.
+
+    The center of a tree consists of nodes that minimize the maximum eccentricity.
+    That is, these nodes minimize the maximum distance to all other nodes.
+    This implementation currently only works for unweighted edges.
+
+    If the input graph is not a tree, results are not guaranteed to be correct and while
+    some non-trees will raise a ``NetworkXError`` not all non-trees will be discovered.
+    Thus, this function should not be used if caller is unsure whether the input graph
+    is a tree. Use ``networkx.is_tree(G)`` to check.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        A tree graph (undirected, acyclic graph).
+
+    Returns
+    -------
+    center : list
+        A list of nodes in the center of the tree. This could be one or two nodes.
+
+    Raises
+    ------
+    NetworkXError
+        If algorithm detects input graph is not a tree. There is no guarantee
+        this error will always raise if a non-tree is passed.
+
+    Notes
+    -----
+    This algorithm iteratively removes leaves (nodes with degree 1) from the tree until
+    there are only 1 or 2 nodes left. The remaining nodes form the center of the tree.
+
+    This algorithm's time complexity is O(N) where N is the number of nodes in the tree.
+
+    Examples
+    --------
+    >>> G = nx.Graph([(1, 2), (1, 3), (2, 4), (2, 5)])
+    >>> nx.tree.center(G)
+    [1, 2]
+
+    >>> G = nx.Graph([(1, 2), (2, 3), (3, 4), (4, 5)])
+    >>> nx.tree.center(G)
+    [3]
+    """
+    center_candidates_degree = dict(G.degree)
+    leaves = {node for node, degree in center_candidates_degree.items() if degree == 1}
+
+    # It's better to fail than an infinite loop, so check leaves to ensure progress
+    while len(center_candidates_degree) > 2 and leaves:
+        new_leaves = set()
+        for leaf in leaves:
+            del center_candidates_degree[leaf]
+            for neighbor in G.neighbors(leaf):
+                if neighbor not in center_candidates_degree:
+                    continue
+                center_candidates_degree[neighbor] -= 1
+                if center_candidates_degree[neighbor] == 1:
+                    new_leaves.add(neighbor)
+        leaves = new_leaves
+
+    if not leaves and len(center_candidates_degree) >= 2:
+        # We detected graph is not a tree. This check does not cover all cases.
+        # For example, it does not cover the case where we have two islands (A-B) and (B-C)
+        # where we might eliminate (B-C) leaves and return [A, B] as centers.
+        raise nx.NetworkXError("Input graph is not a tree")
+
+    return list(center_candidates_degree)
