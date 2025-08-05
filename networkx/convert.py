42b96871b29664dc98d46b566485792727a8fbd0
Peiffap <gilles.peiffer@uclouvain.be>
2025-08-05 16:23:25 +0200

    add edge data immediately

--- networkx/convert.py
+++ networkx/convert.py
@@ -439,8 +439,7 @@ def from_dict_of_dicts(d, create_using=None, multigraph_input=False):
             for u, nbrs in d.items():
                 for v, data in nbrs.items():
                     if (u, v) not in seen:
-                        G.add_edge(u, v, key=0)
-                        G[u][v][0].update(data)
+                        G.add_edge(u, v, key=0, data=data)
                     seen.add((v, u))
         else:
             G.add_edges_from(
