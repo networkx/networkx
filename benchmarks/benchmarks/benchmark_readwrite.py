8cd96aac2b7e0264a9f1cbabb2c1ac76356af0ee
Peiffap <gilles.peiffer.yt@gmail.com>
2025-08-07 02:00:11 +0200

    add benchmarks for other changed functions

--- benchmarks/benchmarks/benchmark_readwrite.py
+++ benchmarks/benchmarks/benchmark_readwrite.py
@@ -1,3 +1,5 @@
+from asv_runner.benchmarks.mark import SkipNotImplemented
+
 import networkx as nx
 
 
@@ -14,6 +16,11 @@ class AdjlistCompleteGraph:
     def time_generate_adjlist(self, graph_type, n):
         _ = list(nx.generate_adjlist(self.G))
 
+    def time_write_multiline_adjlist(self, graph_type, n):
+        _ = nx.write_multiline_adjlist(
+            self.G, path="benchmark_write_multiline_adjlist.txt"
+        )
+
 
 class EdgelistCompleteGraph:
     params = [
@@ -26,14 +33,28 @@ class EdgelistCompleteGraph:
     def setup(self, graph_type, n, data):
         self.create_using = getattr(nx, graph_type)
         self.G = nx.complete_graph(n, create_using=self.create_using)
-        if self.G.is_multigraph():
-            self.G.add_edges_from(self.G.edges)
-            for u, v, k in self.G.edges(keys=True):
-                self.G[u][v][k].update({"weight": 1})
-        else:
-            for u, v in self.G.edges():
-                self.G[u][v].update({"weight": 1})
+        graphs = [self.G]
+        if not self.G.is_directed():
+            self.B = nx.bipartite.complete_bipartite_graph(
+                n // 2, n // 2, create_using=self.create_using
+            )
+            graphs.append(self.B)
+        for graph in graphs:
+            if graph.is_multigraph():
+                graph.add_edges_from(graph.edges)
+                for u, v, k in graph.edges(keys=True):
+                    graph[u][v][k].update({"weight": 1})
+            else:
+                for u, v in graph.edges():
+                    graph[u][v].update({"weight": 1})
 
     def time_write_edgelist(self, graph_type, n, data):
         # data = ("weight",) corresponds to `write_weighted_edgelist`.
-        _ = nx.write_edgelist(self.G, path="benchmark_edgelist.txt", data=data)
+        _ = nx.write_edgelist(self.G, path="benchmark_write_edgelist.txt", data=data)
+
+    def time_bipartite_write_edgelist(self, graph_type, n, data):
+        if graph_type in {"DiGraph", "MultiDiGraph"}:
+            raise SkipNotImplemented(f"bipartite graph not defined for {graph_type}")
+        _ = nx.bipartite.write_edgelist(
+            self.B, path="benchmark_write_bipartite_edgelist.txt", data=data
+        )
