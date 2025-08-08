be6cfab271c4031414fd418606d2e7786da383c1
Peiffap <gilles.peiffer@uclouvain.be>
2025-08-08 22:32:21 +0200

    update docstring for hnm

--- networkx/generators/harary_graph.py
+++ networkx/generators/harary_graph.py
@@ -25,29 +25,28 @@ __all__ = ["hnm_harary_graph", "hkn_harary_graph"]
 
 @nx._dispatchable(graphs=None, returns_graph=True)
 def hnm_harary_graph(n, m, create_using=None):
-    """Returns the Harary graph with given numbers of nodes and edges.
+    r"""Return the Harary graph with given numbers of nodes and edges.
 
-    The Harary graph $H_{n,m}$ is the graph that maximizes node connectivity
+    The Harary graph $H_{n, m}$ is the graph that maximizes node connectivity
     with $n$ nodes and $m$ edges.
 
-    This maximum node connectivity is known to be floor($2m/n$). [1]_
+    This maximum node connectivity is known to be $\lfloor 2m/n \rfloor$. [1]_
 
     Parameters
     ----------
     n: integer
-       The number of nodes the generated graph is to contain
+        The number of nodes the generated graph is to contain.
 
     m: integer
-       The number of edges the generated graph is to contain
+        The number of edges the generated graph is to contain.
 
-    create_using : NetworkX graph constructor, optional Graph type
-     to create (default=nx.Graph). If graph instance, then cleared
-     before populated.
+    create_using : NetworkX graph constructor, optional (default=nx.Graph)
+        Graph type to create. If graph instance, then cleared before populated.
 
     Returns
     -------
     NetworkX graph
-        The Harary graph $H_{n,m}$.
+        The Harary graph $H_{n, m}$.
 
     See Also
     --------
@@ -91,6 +90,7 @@ def hnm_harary_graph(n, m, create_using=None):
         )
 
         if d % 2 == 1:
+            # If d is odd, n must be even.
             half = n // 2
             # Add edges diagonally.
             H.add_edges_from((i, i + half) for i in range(half))
