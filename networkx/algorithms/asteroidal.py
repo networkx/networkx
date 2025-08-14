b5cd9652ed06501dc6764e6c7f5ee188abcdc3c0
Peiffap <gilles.peiffer.yt@gmail.com>
2025-08-14 16:39:05 +0200

    update docstrings for `asteroidal.py`

--- networkx/algorithms/asteroidal.py
+++ networkx/algorithms/asteroidal.py
@@ -1,14 +1,14 @@
 """
 Algorithms for asteroidal triples and asteroidal numbers in graphs.
 
-An asteroidal triple in a graph G is a set of three non-adjacent vertices
-u, v and w such that there exist a path between any two of them that avoids
-closed neighborhood of the third. More formally, v_j, v_k belongs to the same
-connected component of G - N[v_i], where N[v_i] denotes the closed neighborhood
-of v_i. A graph which does not contain any asteroidal triples is called
+An asteroidal triple in a graph ``G`` is a set of three non-adjacent nodes
+``u``, ``v``, and ``w`` such that there exists a path between any two of them that avoids
+the closed neighborhood of the third. More formally, ``v_j``, ``v_k`` belong to the same
+connected component of ``G - N[v_i]``, where ``N[v_i]`` denotes the closed neighborhood
+of ``v_i``. A graph which does not contain any asteroidal triples is called
 an AT-free graph. The class of AT-free graphs is a graph class for which
-many NP-complete problems are solvable in polynomial time. Amongst them,
-independent set and coloring.
+many NP-complete problems such as independent set and coloring are solvable in polynomial time.
+.
 """
 
 import networkx as nx
@@ -21,41 +21,45 @@ __all__ = ["is_at_free", "find_asteroidal_triple"]
 @not_implemented_for("multigraph")
 @nx._dispatchable
 def find_asteroidal_triple(G):
-    r"""Find an asteroidal triple in the given graph.
+    r"""Find an asteroidal triple in a graph.
 
-    An asteroidal triple is a triple of non-adjacent vertices such that
-    there exists a path between any two of them which avoids the closed
-    neighborhood of the third. It checks all independent triples of vertices
-    and whether they are an asteroidal triple or not. This is done with the
-    help of a data structure called a component structure.
-    A component structure encodes information about which vertices belongs to
-    the same connected component when the closed neighborhood of a given vertex
-    is removed from the graph. The algorithm used to check is the trivial
-    one, outlined in [1]_, which has a runtime of
-    :math:`O(|V||\overline{E} + |V||E|)`, where the second term is the
-    creation of the component structure.
+    An asteroidal triple is a triple of non-adjacent nodes such that
+    there exists a path between any two of them that avoids the closed
+    neighborhood of the third.
 
     Parameters
     ----------
     G : NetworkX Graph
-        The graph to check whether is AT-free or not
+        The graph to find an asteroidal triple in.
 
     Returns
     -------
     list or None
         An asteroidal triple is returned as a list of nodes. If no asteroidal
-        triple exists, i.e. the graph is AT-free, then None is returned.
+        triple exists, i.e. the graph is AT-free, then `None` is returned.
+
+    Raises
+    ------
+    NetworkXNotImplemented
+        If the graph is directed or a multigraph.
 
     Notes
     -----
-    The component structure and the algorithm is described in [1]_. The current
-    implementation implements the trivial algorithm for simple graphs.
+    This implements the trivial algorithm for simple graphs in [1]_.
+    It checks all independent triples of nodes and whether they are an
+    asteroidal triple or not. This is done with the help of a data structure
+    called a component structure. The component structure encodes information
+    about which nodes belong to the same connected component when the closed
+    neighborhood of a third node is removed from the graph.
+
+    The algorithm has a runtime of :math:`O(|V||\overline{E}| + |V||E|)`,
+    where the second term is the creation of the component structure.
 
     References
     ----------
     .. [1] Ekkehard Köhler,
-       "Recognizing Graphs without asteroidal triples",
-       Journal of Discrete Algorithms 2, pages 439-452, 2004.
+       "Recognizing graphs without asteroidal triples",
+       Journal of Discrete Algorithms 2, pages 439--452, 2004.
        https://www.sciencedirect.com/science/article/pii/S157086670400019X
     """
     V = set(G)
@@ -91,8 +95,8 @@ def is_at_free(G):
 
     The method uses the `find_asteroidal_triple` method to recognize
     an AT-free graph. If no asteroidal triple is found the graph is
-    AT-free and True is returned. If at least one asteroidal triple is
-    found the graph is not AT-free and False is returned.
+    AT-free and `True` is returned. If at least one asteroidal triple is
+    found the graph is not AT-free and `False` is returned.
 
     Parameters
     ----------
@@ -102,7 +106,7 @@ def is_at_free(G):
     Returns
     -------
     bool
-        True if G is AT-free and False otherwise.
+        `True` if `G` is AT-free and `False` otherwise.
 
     Examples
     --------
@@ -121,17 +125,19 @@ def is_at_free(G):
 @not_implemented_for("multigraph")
 @nx._dispatchable
 def _create_component_structure(G):
-    r"""Create component structure for G.
+    r"""Create a modified component structure for a graph.
 
-    A *component structure* is an `nxn` array, denoted `c`, where `n` is
-    the number of vertices,  where each row and column corresponds to a vertex.
+    A *component structure* is an ``n x n`` array, denoted ``c``, where ``n`` is
+    the number of vertices, where each row and column corresponds to a node.
 
     .. math::
-        c_{uv} = \begin{cases} 0, if v \in N[u] \\
-            k, if v \in component k of G \setminus N[u] \end{cases}
+        c_{uv} = \begin{cases} 0, \text{if} v \in N[u] \\
+            k, \text{if} v \in \text{component} k \text{of} G \setminus N[u] \end{cases}
 
-    Where `k` is an arbitrary label for each component. The structure is used
-    to simplify the detection of asteroidal triples.
+    where ``k` is an arbitrary label for each component. The structure is used
+    to simplify the detection of asteroidal triples. Our implementation uses a
+    modified version of the component structure that excludes ``v`` from ``c_u``
+    if ``v \in N[u]``.
 
     Parameters
     ----------
@@ -141,7 +147,7 @@ def _create_component_structure(G):
     Returns
     -------
     component_structure : dictionary
-        A dictionary of dictionaries, keyed by pairs of vertices.
+        A dictionary of dictionaries, keyed by pairs of nodes.
 
     """
     V = set(G)
