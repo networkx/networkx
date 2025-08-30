94ac0ed77478db6e47b4a3d3cac5dfdd1f060f64
Ross Barnowski <rossbar@caltech.edu>
2025-08-30 14:40:21 -0700

    MAINT: Rm print from threshold_graph (#8212)
    * MAINT: print -> raise in internal threshold_graph fn.
    
    * Rm un-hittable code branch.
    
    * Remove extra return
    
    ---------
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- networkx/algorithms/threshold.py
+++ networkx/algorithms/threshold.py
@@ -329,8 +329,7 @@ def threshold_graph(creation_sequence, create_using=None):
         cs = uncompact(creation_sequence)
         ci = list(enumerate(cs))
     else:
-        print("not a valid creation sequence type")
-        return None
+        raise ValueError("not a valid creation sequence")
 
     G = nx.empty_graph(0, create_using)
     if G.is_directed():
@@ -546,9 +545,6 @@ def degree_correlation(creation_sequence):
     ds = degree_sequence(cs)
     for i, sym in enumerate(cs):
         if sym == "d":
-            if i != rdi[0]:
-                print("Logic error in degree_correlation", i, rdi)
-                raise ValueError
             rdi.pop(0)
         degi = ds[i]
         for dj in rdi:
