f23d3c8d1d8fd2d104401db1be2dd4d534378a3e
sean-chernov <seanchernov@gmail.com>
2025-09-01 16:31:24 +0300

    Update random_graphs.py

--- networkx/generators/random_graphs.py
+++ networkx/generators/random_graphs.py
@@ -1458,16 +1458,12 @@ def random_k_lift(G, k, seed=None):
     - For each edge (``u``, ``v``) in `G`, a random permutation σ is applied to determine new edges:
       if σ(``i``) = ``j``, then ((``u``, ``i``), (``v``, ``j``)) is added to ``H``.
       The permutation is simulated by creating a shuffled list ``permutation`` of values 0 to `k`-1.
-      Each ``i``-th copy of ``u`` is then connected to the ``permutation[``i``]``-th copy of ``v``.
-
-      
+      Each ``i``-th copy of ``u`` is then connected to the ``permutation[``i``]``-th copy of ``v``. 
     This operation is often used in the construction of expander graphs.
     If the base graph is a decent expander (i.e., has a good spectral gap),
     then its `k`-lifts are also expanders, with the spectral gap preserved
     (or slightly reduced) with high probability, while producing a larger
     graph of the same degree.
-    
-    
     For arbitrary input graphs, the lifted graph may be disconnected.
     Disconnected lifts occur more often when the base graph has poor
     expansion. Since enforcing connectivity in such cases is unlikely to
