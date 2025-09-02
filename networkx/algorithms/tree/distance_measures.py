6f5d4d2f3cb383e2a338ac1f9b93aec422bd71ab
Gilles Peiffer <gilles.peiffer.yt@gmail.com>
2025-09-02 17:45:11 +0200

    feat(api): update non-tree check in `_tree_center`  and move to `tree` subpackage (#8174)
    * api: move `tree.center` to new `tree.distance_measures` subpackage
    
    * clean up docstring
    
    * Use `NotATree` exception and improve nontree check
    
    * add tests for `nx.tree.center`
    
    * add benchmarks for `nx(.tree).center`
    
    * additional nontree check
    
    Co-authored-by: Alejandro Candioti <amcandio@gmail.com>
    
    * fail on self-loops
    
    * remove pointless benchmarking imports
    
    
    Co-authored-by: Ross Barnowski <rossbar@caltech.edu>
    
    * remove walrus operator
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>
    
    ---------
    
    Co-authored-by: Alejandro Candioti <amcandio@gmail.com>
    Co-authored-by: Ross Barnowski <rossbar@caltech.edu>
    Co-authored-by: Dan Schult <dschult@colgate.edu>

---
+++ networkx/algorithms/tree/distance_measures.py
@@ -0,0 +1,87 @@
+"""
+Algorithms for computing distance measures on trees.
+"""
+
+import networkx as nx
+from networkx.utils import not_implemented_for
+
+__all__ = [
+    "center",
+]
+
+
+@not_implemented_for("directed")
+def center(G):
+    """Returns the center of an undirected tree graph.
+
+    The center of a tree consists of nodes that minimize the maximum eccentricity.
+    That is, these nodes minimize the maximum distance to all other nodes.
+    This implementation currently only works for unweighted edges.
+
+    If the input graph is not a tree, results are not guaranteed to be correct and while
+    some non-trees will raise an ``nx.NotATree`` exception, not all non-trees will be discovered.
+    Thus, this function should not be used if caller is unsure whether the input graph
+    is a tree. Use ``nx.is_tree(G)`` to check.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        A tree graph (undirected, acyclic graph).
+
+    Returns
+    -------
+    center : list
+        A list of nodes forming the center of the tree. This can be one or two nodes.
+
+    Raises
+    ------
+    NetworkXNotImplemented
+        If the input graph is directed.
+
+    NotATree
+        If the algorithm detects the input graph is not a tree. There is no guarantee
+        this error will always raise if a non-tree is passed.
+
+    Notes
+    -----
+    This algorithm iteratively removes leaves (nodes with degree 1) from the tree until
+    there are only 1 or 2 nodes left. The remaining nodes form the center of the tree.
+
+    This algorithm's time complexity is ``O(N)`` where ``N`` is the number of nodes in the tree.
+
+    Examples
+    --------
+    >>> G = nx.Graph([(1, 2), (1, 3), (2, 4), (2, 5)])
+    >>> nx.tree.center(G)
+    [1, 2]
+
+    >>> G = nx.path_graph(5)
+    >>> nx.tree.center(G)
+    [2]
+    """
+    center_candidates_degree = dict(G.degree)
+    leaves = {node for node, degree in center_candidates_degree.items() if degree == 1}
+
+    # It's better to fail than an infinite loop, so check leaves to ensure progress.
+    while len(center_candidates_degree) > 2 and leaves:
+        new_leaves = set()
+        for leaf in leaves:
+            del center_candidates_degree[leaf]
+            for neighbor in G.neighbors(leaf):
+                if neighbor not in center_candidates_degree:
+                    continue
+                center_candidates_degree[neighbor] -= 1
+                if (cddn := center_candidates_degree[neighbor]) == 1:
+                    new_leaves.add(neighbor)
+                elif cddn == 0 and len(center_candidates_degree) != 1:
+                    raise nx.NotATree("input graph is not a tree")
+        leaves = new_leaves
+
+    n = len(center_candidates_degree)
+    # check disconnected or cyclic
+    if (n == 2 and not leaves) or n not in {1, 2}:
+        # We have detected graph is not a tree. This check does not cover all cases.
+        # For example, `nx.Graph([(0, 0)])` will not raise an error.
+        raise nx.NotATree("input graph is not a tree")
+
+    return list(center_candidates_degree)
