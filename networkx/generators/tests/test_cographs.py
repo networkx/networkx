e5a7011dba6f713d34d5dbc70984a5a629cca6f1
Peiffap <gilles.peiffer.yt@gmail.com>
2025-09-04 13:37:42 +0200

    parametrize on `n` and `seed`

--- networkx/generators/tests/test_cographs.py
+++ networkx/generators/tests/test_cographs.py
@@ -1,11 +1,18 @@
 """Unit tests for the :mod:`networkx.generators.cographs` module."""
 
+import pytest
+
 import networkx as nx
 
 
-def test_random_cograph():
-    n = 3
-    G = nx.random_cograph(n, seed=42)
+@pytest.mark.parametrize("n", [3, 4, 5])
+@pytest.mark.parametrize("seed", [42, 43])
+def test_random_cograph(n, seed):
+    """Test the generation of random cographs.
+
+    Parametrized on `seed` to ensure we hit all code branches.
+    """
+    G = nx.random_cograph(n, seed=seed)
 
     assert len(G) == 2**n
 
