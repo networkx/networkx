cb1080948eb27ae770182828b7c5f16c434ab48e
sean-chernov <seanchernov@gmail.com>
2025-09-06 00:09:41 +0300

    Update test_random_graphs.py

--- networkx/generators/tests/test_random_graphs.py
+++ networkx/generators/tests/test_random_graphs.py
@@ -484,7 +484,7 @@ def test_random_kernel_disallow_directed_and_multigraph(graphtype):
     [
         (4, 8, 4),  # Balanced
         (1, 2, 1),  # Tiny graph
-        (3, 20, 3),  # Higher n with moderate d
+        (6, 20, 3),  # Higher n with moderate d
         (40, 60, 6),  # Large-scale performance & correctness
     ],
 )
