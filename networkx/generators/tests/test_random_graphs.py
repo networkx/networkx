595f68578953a7da77ca5fa9f8724966fa8d4114
Peiffap <gilles.peiffer.yt@gmail.com>
2025-09-06 00:11:04 +0200

    TST: add test for default `kernel_root` in `random_kernel_graph`

--- networkx/generators/tests/test_random_graphs.py
+++ networkx/generators/tests/test_random_graphs.py
@@ -308,8 +308,16 @@ class TestGeneratorsRandom:
             return r / c + w
 
         c = 1
-        graph = nx.random_kernel_graph(1000, integral, root)
         graph = nx.random_kernel_graph(1000, integral, root, seed=42)
+        assert len(graph) == 1000
+
+    def test_random_kernel_graph_default_root(self):
+        def integral(u, w, z):
+            return c * (z - w)
+
+        c = 1
+        pytest.importorskip("scipy")
+        graph = nx.random_kernel_graph(1000, integral, seed=42)
         assert len(graph) == 1000
 
 
