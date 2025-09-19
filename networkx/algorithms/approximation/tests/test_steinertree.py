06b279b11a8b74f58ce3417f03a0e1abed09175a
Aka2210 <qoo0973215128@gmail.com>
2025-09-19 18:43:08 +0800

    Add tests for directed_steiner_tree
    Covers valid cases, cutoff behavior, invalid input, and
    unfeasible instances.

--- networkx/algorithms/approximation/tests/test_steinertree.py
+++ networkx/algorithms/approximation/tests/test_steinertree.py
@@ -3,6 +3,7 @@ import pytest
 import networkx as nx
 from networkx.algorithms.approximation.steinertree import (
     _remove_nonterminal_leaves,
+    directed_steiner_tree,
     metric_closure,
     steiner_tree,
 )
@@ -301,3 +302,96 @@ def test_steiner_tree_non_terminal_leaves_multigraph_self_loop_edges():
 
     # Only the terminal nodes should be left
     assert list(G) == [4, 5, 6, 7]
+
+
+class TestDirectedSteinerTree:
+    @classmethod
+    def setup_class(cls):
+        G1 = nx.DiGraph()
+        G1.add_edge(0, 1, weight=1)
+        G1.add_edge(0, 2, weight=2)
+        G1.add_edge(2, 1, weight=0.5)
+        G1.add_node(3)
+
+        G2 = nx.DiGraph()
+        G2.add_edge(0, 2, weight=1)
+        G2.add_edge(2, 1, weight=1)
+
+        cls.G1 = G1
+        cls.G2 = G2
+
+    def test_undirected_graph_raises(self):
+        G = nx.Graph()
+        G.add_edge(0, 1, weight=1)
+        with pytest.raises(nx.NetworkXError, match="only works for directed graphs"):
+            directed_steiner_tree(G, 0, {1})
+
+    def test_invaild_root(self):
+        with pytest.raises(nx.NetworkXError):
+            directed_steiner_tree(self.G1, 5, {1})
+
+    def test_invaild_terminals(self):
+        with pytest.raises(nx.NetworkXUnfeasible):
+            directed_steiner_tree(self.G1, 0, {})
+
+    @pytest.mark.parametrize(
+        "cutoff, should_raise",
+        [
+            (None, False),
+            (float("inf"), False),
+            (999999, False),
+            (0, True),
+            (-5, True),
+        ],
+    )
+    def test_cutoff_values(self, cutoff, should_raise):
+        if should_raise:
+            with pytest.raises(nx.NetworkXError):
+                directed_steiner_tree(self.G2, 0, {1}, cutoff=cutoff)
+        else:
+            H = directed_steiner_tree(self.G2, 0, {1}, cutoff=cutoff)
+            assert (0, 2) and (2, 1) in H.edges
+
+    @pytest.mark.parametrize(
+        "cutoff, should_raise",
+        [
+            (2, False),
+            (1, True),
+        ],
+    )
+    def test_cutoff_limited_depth(self, cutoff, should_raise):
+        if should_raise:
+            with pytest.raises(nx.NetworkXUnfeasible):
+                directed_steiner_tree(self.G2, 0, {1}, cutoff=cutoff)
+        else:
+            H = directed_steiner_tree(self.G2, 0, {1}, cutoff=cutoff)
+            assert 2 in H.nodes
+
+    @pytest.mark.parametrize(
+        "min_terminals, should_raise", [(-1, True), (0, True), (2, True), (1, False)]
+    )
+    def test_min_terminals_values(self, min_terminals, should_raise):
+        if should_raise:
+            with pytest.raises(nx.NetworkXError):
+                directed_steiner_tree(self.G2, 0, {1}, min_terminals=min_terminals)
+        else:
+            H = directed_steiner_tree(self.G2, 0, {1}, min_terminals=min_terminals)
+            assert 1 in H.nodes
+
+    def test_no_reachable_nodes(self):
+        with pytest.raises(nx.NetworkXUnfeasible):
+            directed_steiner_tree(self.G1, 0, {3})
+
+    def test_basic_single_terminal(self):
+        H = directed_steiner_tree(self.G1, 0, {1})
+        assert set(H.nodes) == {0, 1}
+        assert (0, 1) in H.edges
+
+    def test_multiple_terminals(self):
+        H = directed_steiner_tree(self.G1, 0, {1, 2})
+        assert set(H.nodes) == {0, 1, 2}
+        assert (0, 1) and (2, 1) in H.edges
+
+    def test_cannot_cover_min_terminals_raises(self):
+        with pytest.raises(nx.NetworkXError):
+            directed_steiner_tree(self.G2, 0, {1, 2, 3, 4}, min_terminals=4)
