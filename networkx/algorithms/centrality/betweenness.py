0748d8e840f6016d1986499c05f1ba8a1b64fa96
Alain Kadar <73320455+AlainKadar@users.noreply.github.com>
2025-12-16 15:43:36 +0000

    Betweenness documentation definitions should match default behavior (#8380)
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- networkx/algorithms/centrality/betweenness.py
+++ networkx/algorithms/centrality/betweenness.py
@@ -25,15 +25,18 @@ def betweenness_centrality(
 
     .. math::
 
-       c_B(v) = \sum_{s, t \in V} \frac{\sigma(s, t | v)}{\sigma(s, t)}
+       c_B(v) = \frac{1}{P} \sum_{s, t \in V} \frac{\sigma(s, t | v)}{\sigma(s, t)}
 
     where $V$ is the set of nodes, $\sigma(s, t)$ is the number of
     shortest $(s, t)$-paths, and $\sigma(s, t | v)$ is the number of
     those paths passing through some node $v$ other than $s$ and $t$.
     If $s = t$, $\sigma(s, t) = 1$, and if $v \in \{s, t\}$,
-    $\sigma(s, t | v) = 0$ [2]_.
-    The denominator $\sigma(s, t)$ is a normalization factor that can be
-    turned off to get the raw path counts.
+    $\sigma(s, t | v) = 0$ (when `endpoints` takes its default value of
+    `False`) [2]_.
+    $P$ is a normalization factor representing the number of pairs of nodes
+    that have counted shortest paths. Its value depends on the values of `normalized`
+    and `endpoints`, and on whether the graph is directed (see Notes). It can
+    be set to ``1`` with ``normalized=False``.
 
     Parameters
     ----------
