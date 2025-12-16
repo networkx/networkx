6d769b4a590f81af22281b7e962605430c0af409
Gulshan Kumar <gksept2005@gmail.com>
2025-12-16 11:13:31 +0530

    Updated docs:
    1. regarding weight parameter: copied from other distance func
    2. in fw_pred_dist: return value needed more specification, so, taken
       from tree version

--- networkx/algorithms/shortest_paths/dense.py
+++ networkx/algorithms/shortest_paths/dense.py
@@ -105,8 +105,18 @@ def floyd_warshall_tree(G, weight="weight"):
     ----------
     G : NetworkX graph
 
-    weight : string, optional (default= 'weight')
-        Edge data key corresponding to the edge weight.
+    weight : string or function, optional (default = 'weight')
+        If this is a string, then edge weights will be accessed via the
+        edge attribute with this key (that is, the weight of the edge
+        joining `u` to `v` will be ``G.edges[u, v][weight]``). If no
+        such edge attribute exists, the weight of the edge is assumed to
+        be one.
+
+        If this is a function, the weight of an edge is the value
+        returned by the function. The function must accept exactly three
+        positional arguments: the two endpoints of an edge and the
+        dictionary of edge attributes for that edge. The function must
+        return a number or None to indicate a hidden edge.
 
     Returns
     -------
@@ -237,14 +247,26 @@ def floyd_warshall_predecessor_and_distance(G, weight="weight"):
     ----------
     G : NetworkX graph
 
-    weight: string, optional (default= 'weight')
-       Edge data key corresponding to the edge weight.
+    weight : string or function, optional (default = 'weight')
+        If this is a string, then edge weights will be accessed via the
+        edge attribute with this key (that is, the weight of the edge
+        joining `u` to `v` will be ``G.edges[u, v][weight]``). If no
+        such edge attribute exists, the weight of the edge is assumed to
+        be one.
+
+        If this is a function, the weight of an edge is the value
+        returned by the function. The function must accept exactly three
+        positional arguments: the two endpoints of an edge and the
+        dictionary of edge attributes for that edge. The function must
+        return a number or None to indicate a hidden edge.
 
     Returns
     -------
-    predecessor,distance : dictionaries
-       Dictionaries, keyed by source and target, of predecessors and distances
-       in the shortest path.
+    predecessor, distance : dict and dict-of-dict
+        Predecessor is a dict keyed by node to the predecessor node in
+        the shortest path. The distance output is a dict keyed by source
+        node to a dict keyed by target node to the distance value of the
+        shortest path between the source and target.
 
     Examples
     --------
