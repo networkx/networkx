11124f3e98272c668b8700146b333e3bacac8d6c
Sonucode-prog <so9336232018@gmail.com>
2025-12-16 23:33:23 +0530

    matching the style formates of NetworkX

--- examples/basic/basic_bfs.py
+++ examples/basic/basic_bfs.py
@@ -6,12 +6,14 @@ import networkx as nx
 
 # Create a simple graph
 G = nx.Graph()
-G.add_edges_from([
-    (1, 2),
-    (1, 3),
-    (2, 4),
-    (3, 5),
-])
+G.add_edges_from(
+    [
+        (1, 2),
+        (1, 3),
+        (2, 4),
+        (3, 5),
+    ]
+)
 
 # Perform BFS starting from node 1
 bfs_nodes = list(nx.bfs_tree(G, source=1))
