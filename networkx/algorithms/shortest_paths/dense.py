6b93b95fa619a0ea959325f0935ba8149462df88
Gulshan Kumar <gksept2005@gmail.com>
2025-12-18 21:23:06 +0530

    update docs respecting earlier indentation

--- networkx/algorithms/shortest_paths/dense.py
+++ networkx/algorithms/shortest_paths/dense.py
@@ -127,8 +127,18 @@ def floyd_warshall_tree(G, weight="weight"):
     ----------
     G : NetworkX graph
 
-    weight : string, optional (default= 'weight')
-        Edge data key corresponding to the edge weight.
+    weight : string or function (default= 'weight')
+        If this is a string, then edge weights will be accessed via the
+        edge attribute with this key (that is, the weight of the edge
+        joining `u` to `v` will be ``G.edges[u, v][weight]``). If no
+        such edge attribute exists, the weight of the edge is assumed to
+        be one.
+
+        If this is a function, the weight of an edge is the value
+        returned by the function. The function must accept exactly three
+        positional arguments: the two endpoints of an edge and the
+        dictionary of edge attributes for that edge. The function must
+        return a number or None to indicate a hidden edge.
 
     Returns
     -------
@@ -244,8 +254,18 @@ def floyd_warshall_predecessor_and_distance(G, weight="weight"):
     ----------
     G : NetworkX graph
 
-    weight: string, optional (default= 'weight')
-       Edge data key corresponding to the edge weight.
+    weight : string or function (default= 'weight')
+       If this is a string, then edge weights will be accessed via the
+       edge attribute with this key (that is, the weight of the edge
+       joining `u` to `v` will be ``G.edges[u, v][weight]``). If no
+       such edge attribute exists, the weight of the edge is assumed to
+       be one.
+
+       If this is a function, the weight of an edge is the value
+       returned by the function. The function must accept exactly three
+       positional arguments: the two endpoints of an edge and the
+       dictionary of edge attributes for that edge. The function must
+       return a number or None to indicate a hidden edge.
 
     Returns
     -------
@@ -353,8 +373,18 @@ def floyd_warshall(G, weight="weight"):
     ----------
     G : NetworkX graph
 
-    weight: string, optional (default= 'weight')
-       Edge data key corresponding to the edge weight.
+    weight : string or function (default= 'weight')
+       If this is a string, then edge weights will be accessed via the
+       edge attribute with this key (that is, the weight of the edge
+       joining `u` to `v` will be ``G.edges[u, v][weight]``). If no
+       such edge attribute exists, the weight of the edge is assumed to
+       be one.
+
+       If this is a function, the weight of an edge is the value
+       returned by the function. The function must accept exactly three
+       positional arguments: the two endpoints of an edge and the
+       dictionary of edge attributes for that edge. The function must
+       return a number or None to indicate a hidden edge.
 
 
     Returns
