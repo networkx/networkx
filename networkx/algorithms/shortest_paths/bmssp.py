4ee4b2671aa1600c7ad1accc4406c923e4178031
Ramji Purwar <ramji.purwar@iitgn.ac.in>
2025-12-22 14:35:10 +0000

    made doc string of bmssp

--- networkx/algorithms/shortest_paths/bmssp.py
+++ networkx/algorithms/shortest_paths/bmssp.py
@@ -5,7 +5,13 @@ from itertools import count
 
 import networkx as nx
 
-__all__ = ["bmssp_path" ,"bmssp_path_lenght"]
+__all__ = [
+    "single_source_bmssp_path", 
+    "single_source_bmssp_path_lenght",
+    "multi_source_bmssp_path",
+    "multi_source_bmssp_path_lenght",
+    "bmssp"
+]
 
 def single_source_bmssp_path(G, source, target, weight="weight"):
     """Returns the shortest weighted path in G from source to target
@@ -154,5 +160,182 @@ def single_source_bmssp_path_lenght(G, source, target, weight="weight"):
     except KeyError as err:
         raise nx.NetworkXNoPath(f"Node {target} not reachable from {source}") from err
 
-def bmssp():
+def multi_source_bmssp_path(G, sources, weight="weight"):
+    """ Find shortest weighted paths in G from a given set of soource
+    nodes.
+
+    Compute shortest path between any of the source nodes and all other
+    reachable nodes for a weighted graph.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+
+    sources : non-empty set of nodes
+        Starting nodes for paths. If this is just a set containing a 
+        single node, then all paths computed by this function will start
+        from that node. If there are two or more nodes in the set, the 
+        computed paths may begin from from any one of the start nodes.
+
+    weight : string or function
+        If there is a string, then edge weights will be accessed via the 
+        edge attribute with this key (that is, the weight of the edge
+        joining `u` to `v` will be ``G.edges[u, v][weight]``). If no 
+        such edge attribute exists, the weight of the edge is assumed to 
+        be one.
+
+        If this is a function, the weight of an edge is the value
+        returned by the function. The function must accept exactly three
+        positional arguments: the two endpoints of an edge and the 
+        dictionary of edge attributes for that edge. The function must 
+        return a number or None to indicate a hidden edge.
+
+    Returns
+    ------- 
+    path : dictionary
+        Dictionary of shortest paths keyed by target.
+
+    Examples
+    --------
+    >>> G = nx.path_graph(5)
+    >>> path = nx.multi_source_bmssp_path(G, {0, 4})
+    >>> path[1]
+    [0, 1]
+    >>> path[3]
+    [4, 3]
+
+    Notes
+    -----
+    Edge weight attributes must be numerical.
+    Distances are calculated as sums of weighted edges traversed.
+
+    The weight function can be used to hide edges by returning None.
+    So ``weight = lambda u, v, d: 1 if d['color']=="red" else None``
+    will find the shortest red path.
+
+    Raises
+    ------
+    ValueError
+        If `sources` is empty.
+    NodeNotFound
+        If any of `sources` is not in `G`.
+
+    """
+    _, path = bmssp(G, sources, weight=weight)
+    return path
+
+def multi_source_bmssp_path_lenght(G, sources, weight="weight"):
+    """Find shortest weighted path lenghts in G from a given set of
+    source nodes.
+
+    Compute the shortest path lenght between any of the source nodes and 
+    all other reachable nodes for a weighted graph.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+
+    sources : non-empty set of nodes
+        Starting nodes for paths. If this is just a set containing a
+        single node, then all paths computed by this function will start 
+        from that node. If there are two or more nodes in the set, the 
+        computed paths may begin from any one of the start nodes.
+
+    weight : string or function
+        If this is a string, then edge weights will be accessed via the 
+        edge attribute with this key (that is, the weight of the edge 
+        joining `u` to `v` will be ``G.edges[u, v][weight]``). If no 
+        such attribute exists, the weight of the edge is asssumed to 
+        be one.
+
+        If this is a function, the weight of an edge is the value
+        returned by the function. The function must accept exactly three
+        positional arguments: the two endpoints of an edge and the 
+        dictionary of edge attributes for that edge. The function must
+        return a number or None to indicate a hidden edge.
+
+    Returns
+    -------
+    lenght : dict
+        Dict keyed by node to shortest path lenght to nearest source.
+
+    Examples
+    --------
+    >>> G = nx.path_graph(5)
+    >>> length = nx.multi_source_bmssp_path_lenght(G, {0, 4})
+    >>> for node in [0, 1, 2, 3, 4]:
+    ...     print(f"{node}: {length[node]}")
+    0: 0
+    1: 1
+    2: 2
+    3: 1
+    4: 0
+
+    Notes
+    -----
+    Edge weight attributes must be numerical.
+    Distances are calculated as sums of weighted edges traversed.
+
+    The weight function can be used to hide edge by returning None.
+    So ``weight = lambda u, v, d: 1 if d['color']=="red" else None``
+    will find the shortest red path.
+
+    Raises
+    ------
+    ValueError
+        If `source` is empty.
+    NodeNotFound
+        If any of `sources` is not a `G`.
+
+    """
+    if not sources:
+        raise ValueError("Sources must not be empty")
+    for s in sources:
+        if s not in G:
+            raise nx.NodeNotFound(f"Node {s} not found in graph")
+    length, _ = bmssp(G, sources, weight=weight)
+    return length
+
+def bmssp(
+    G, sources, pred=None, paths=None
+):
+    """Uses BMSSP algorithm to find shortest weighted paths
+
+    Parameters
+    ----------
+    G : NetworkX graph
+
+    sources : non-empty iterable of nodes
+        Starting nodes for paths. If this is just an iterable containing 
+        a single node, then all paths computed by this function will 
+        start from that node. If there are two or more nodes in this 
+        iterable, the computed paths may begin from any one of the start
+        nodes.
+
+    weight : function
+        Function with (u, v, data) input that returns that edge's weight
+        or None to indicate a hidden edge
+    
+    pred : dictionary of list, optional (default=None)
+        dictionary to store a list of predecessors keyed by that node
+        If None, predecessors are not stored.
+    
+    paths: dictionary, optional (default=None)
+        dict to store the path list from source to each node, keyed by node.
+        If None, paths are not stored.
+
+    Returns
+    -------
+    distance : dictionary 
+        A mapping from node to shortest distances to that node from one 
+        of the source nodes.
+
+    Raises 
+    ------
+    NodeNotFound
+        If any of `sources` is not in `G`.
+    
+    """
+    pred_dict = pred if paths is None or pred is not None else {}
+    
     return
