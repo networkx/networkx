0ecaf9327846748c13b82a3165f4dc3310630635
Ramji Purwar <ramji.purwar@iitgn.ac.in>
2025-12-23 10:54:42 +0000

    included precion in function calling in test_bmssp.py

--- networkx/algorithms/shortest_paths/tests/test_bmssp.py
+++ networkx/algorithms/shortest_paths/tests/test_bmssp.py
@@ -13,7 +13,7 @@ from networkx.algorithms.shortest_paths.bmssp import (
 class TestSingleSourceBMSSPPath:
     def test_simple_path(self):
         G = nx.DiGraph(nx.path_graph(5))
-        path = single_source_bmssp_path(G, 0, 4)
+        path = single_source_bmssp_path(G, 0, 4, precision=0)
         assert path == [0, 1, 2, 3, 4]
 
     def test_weighted_graph(self):
@@ -21,7 +21,7 @@ class TestSingleSourceBMSSPPath:
         G.add_edge(0, 1, weight=1)
         G.add_edge(1, 2, weight=2)
         G.add_edge(0, 2, weight=5)
-        path = single_source_bmssp_path(G, 0, 2)
+        path = single_source_bmssp_path(G, 0, 2, precision=0)
         assert path == [0, 1, 2]
 
     def test_string_nodes(self):
@@ -29,7 +29,7 @@ class TestSingleSourceBMSSPPath:
         G.add_edge("A", "B", weight=1)
         G.add_edge("B", "C", weight=2)
         G.add_edge("A", "C", weight=10)
-        path = single_source_bmssp_path(G, "A", "C")
+        path = single_source_bmssp_path(G, "A", "C", precision=0)
         assert path == ["A", "B", "C"]
 
     def test_source_not_in_graph(self):
@@ -52,12 +52,12 @@ class TestSingleSourceBMSSPPath:
 class TestSingleSourceBMSSPPathLength:
     def test_simple_path(self):
         G = nx.DiGraph(nx.path_graph(5))
-        length = single_source_bmssp_path_length(G, 0, 4)
+        length = single_source_bmssp_path_length(G, 0, 4, precision=0)
         assert length == 4
 
     def test_same_node(self):
         G = nx.DiGraph(nx.path_graph(5))
-        length = single_source_bmssp_path_length(G, 2, 2)
+        length = single_source_bmssp_path_length(G, 2, 2, precision=0)
         assert length == 0
 
     def test_weighted_graph(self):
@@ -65,7 +65,7 @@ class TestSingleSourceBMSSPPathLength:
         G.add_edge(0, 1, weight=3)
         G.add_edge(1, 2, weight=4)
         G.add_edge(0, 2, weight=10)
-        length = single_source_bmssp_path_length(G, 0, 2)
+        length = single_source_bmssp_path_length(G, 0, 2, precision=0)
         assert length == 7
 
     def test_source_not_in_graph(self):
@@ -86,12 +86,12 @@ class TestMultiSourceBMSSPPath:
         G.add_edge(0, 2, weight=3)
         G.add_edge(1, 2, weight=1)
         G.add_edge(2, 3, weight=2)
-        paths = multi_source_bmssp_path(G, {0, 1})
+        paths = multi_source_bmssp_path(G, {0, 1}, precision=0)
         assert paths[2] == [1, 2]
 
     def test_single_source(self):
         G = nx.DiGraph(nx.path_graph(4))
-        paths = multi_source_bmssp_path(G, {0})
+        paths = multi_source_bmssp_path(G, {0}, precision=0)
         assert paths[3] == [0, 1, 2, 3]
 
     def test_empty_sources(self):
@@ -111,7 +111,7 @@ class TestMultiSourceBMSSPPathLength:
         G.add_edge(0, 2, weight=5)
         G.add_edge(1, 2, weight=2)
         G.add_edge(2, 3, weight=3)
-        lengths = multi_source_bmssp_path_length(G, {0, 1})
+        lengths = multi_source_bmssp_path_length(G, {0, 1}, precision=0)
         assert lengths[0] == 0
         assert lengths[1] == 0
         assert lengths[2] == 2
@@ -129,7 +129,7 @@ class TestBMSSP:
         G.add_edge(0, 1, weight=1)
         G.add_edge(1, 2, weight=2)
         G.add_edge(0, 2, weight=4)
-        distances, paths = bmssp(G, {0})
+        distances, paths = bmssp(G, {0}, precision=0)
         assert distances[0] == 0
         assert distances[1] == 1
         assert distances[2] == 3
@@ -137,7 +137,7 @@ class TestBMSSP:
 
     def test_with_target(self):
         G = nx.DiGraph(nx.path_graph(10))
-        distances, paths = bmssp(G, {0}, target=3)
+        distances, paths = bmssp(G, {0}, target=3, precision=0)
         assert 3 in distances
         assert distances[3] == 3
 
@@ -146,7 +146,7 @@ class TestBMSSP:
         G.add_edge("A", "B", cost=2)
         G.add_edge("B", "C", cost=3)
         G.add_edge("A", "C", cost=10)
-        distances, paths = bmssp(G, {"A"}, weight="cost")
+        distances, paths = bmssp(G, {"A"}, weight="cost", precision=0)
         assert distances["C"] == 5
         assert paths["C"] == ["A", "B", "C"]
 
@@ -163,7 +163,7 @@ class TestBMSSP:
     def test_cycle_graph(self):
         G = nx.DiGraph()
         nx.add_cycle(G, [0, 1, 2, 3])
-        distances, paths = bmssp(G, {0})
+        distances, paths = bmssp(G, {0}, precision=0)
         assert distances[0] == 0
         assert distances[1] == 1
         assert distances[2] == 2
@@ -174,7 +174,7 @@ class TestBMSSP:
         G.add_weighted_edges_from(
             [[0, 1, 2], [1, 2, 12], [2, 3, 1], [3, 4, 5], [4, 5, 1], [5, 0, 10]]
         )
-        distances, paths = bmssp(G, {0})
+        distances, paths = bmssp(G, {0}, precision=0)
         assert distances[3] == 15
 
     def test_weighted_two_edges(self):
@@ -191,7 +191,7 @@ class TestBMSSP:
                 [7, 0, 1],
             ]
         )
-        distances, paths = bmssp(G, {0})
+        distances, paths = bmssp(G, {0}, precision=0)
         assert distances[2] == 4
 
 
@@ -205,7 +205,7 @@ class TestBMSSPWeightFunction:
             if d.get("color") == "red":
                 return d.get("weight", 1)
             return None
-        distances, paths = bmssp(G, {0}, weight=red_only)
+        distances, paths = bmssp(G, {0}, weight=red_only, precision=0)
         assert paths[2] == [0, 1, 2]
         assert distances[2] == 2
 
@@ -221,7 +221,7 @@ class TestBMSSPWeightFunction:
             node_v_wt = G.nodes[v].get("node_weight", 1)
             edge_wt = d.get("weight", 1)
             return node_u_wt / 2 + node_v_wt / 2 + edge_wt
-        distances, paths = bmssp(G, {0}, weight=func)
+        distances, paths = bmssp(G, {0}, weight=func, precision=0)
         # Edge 0->1: 2/2 + 4/2 + 1 = 4
         # Edge 1->2: 4/2 + 6/2 + 1 = 6
         # Total 0->2: 10
@@ -237,7 +237,7 @@ class TestBMSSPValidation:
         G = nx.gnm_random_graph(50, 150, directed=True, seed=42)
         for u, v in G.edges():
             G[u][v]["weight"] = random.randint(1, 10)
-        distances, paths = bmssp(G, {0})
+        distances, paths = bmssp(G, {0}, precision=0)
         for target in distances:
             if target == 0:
                 continue
@@ -255,7 +255,7 @@ class TestBMSSPValidation:
         G = nx.gnm_random_graph(100, 400, directed=True, seed=123)
         for u, v in G.edges():
             G[u][v]["weight"] = random.randint(1, 20)
-        distances, paths = bmssp(G, {0})
+        distances, paths = bmssp(G, {0}, precision=0)
         assert len(distances) > 0
         # Spot check a few nodes
         for target in [10, 25, 50]:
