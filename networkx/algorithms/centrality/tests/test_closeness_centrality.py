c671664d3b65eb6ac6c4056e8782055d003c057f
Ross Barnowski <rossbar@caltech.edu>
2026-03-02 16:25:58 -0800

    Parametrize florentine, les mis, weighted.

--- networkx/algorithms/centrality/tests/test_closeness_centrality.py
+++ networkx/algorithms/centrality/tests/test_closeness_centrality.py
@@ -82,10 +82,12 @@ class TestClosenessCentrality:
         }
         assert all(c[n] == pytest.approx(expected[n], abs=1e-3) for n in G)
 
-    def test_florentine_families_closeness(self):
+    @pytest.mark.parametrize("precompute_sp", (True, False))
+    def test_florentine_families_closeness(self, precompute_sp):
         G = nx.florentine_families_graph()
-        c = nx.closeness_centrality(G)
-        d = {
+        sp = dict(nx.all_pairs_shortest_path_length(G)) if precompute_sp else None
+        c = nx.closeness_centrality(G, sp=sp)
+        expected = {
             "Acciaiuoli": 0.368,
             "Albizzi": 0.483,
             "Barbadori": 0.4375,
@@ -102,13 +104,14 @@ class TestClosenessCentrality:
             "Strozzi": 0.4375,
             "Tornabuoni": 0.483,
         }
-        for n in G:
-            assert c[n] == pytest.approx(d[n], abs=1e-3)
+        assert all(c[n] == pytest.approx(expected[n], abs=1e-3) for n in G)
 
-    def test_les_miserables_closeness(self):
+    @pytest.mark.parametrize("precompute_sp", (True, False))
+    def test_les_miserables_closeness(self, precompute_sp):
         G = nx.les_miserables_graph()
-        c = nx.closeness_centrality(G)
-        d = {
+        sp = dict(nx.all_pairs_shortest_path_length(G)) if precompute_sp else None
+        c = nx.closeness_centrality(G, sp=sp)
+        expected = {
             "Napoleon": 0.302,
             "Myriel": 0.429,
             "MlleBaptistine": 0.413,
@@ -187,10 +190,10 @@ class TestClosenessCentrality:
             "Brujon": 0.380,
             "MmeHucheloup": 0.353,
         }
-        for n in G:
-            assert c[n] == pytest.approx(d[n], abs=1e-3)
+        assert all(c[n] == pytest.approx(expected[n], abs=1e-3) for n in G)
 
-    def test_weighted_closeness(self):
+    @pytest.mark.parametrize("precompute_sp", (True, False))
+    def test_weighted_closeness(self, precompute_sp):
         edges = [
             ("s", "u", 10),
             ("s", "x", 5),
@@ -203,144 +206,16 @@ class TestClosenessCentrality:
             ("y", "s", 7),
             ("y", "v", 6),
         ]
-        XG = nx.Graph()
-        XG.add_weighted_edges_from(edges)
-        c = nx.closeness_centrality(XG, distance="weight")
-        d = {"y": 0.200, "x": 0.286, "s": 0.138, "u": 0.235, "v": 0.200}
-        for n in sorted(XG):
-            assert c[n] == pytest.approx(d[n], abs=1e-3)
-
-    # ----------- Tests closeness centrality using pre-calculated shortest path lengths ------------------- #
-    def test_florentine_families_closeness_sp(self):
-        G = nx.florentine_families_graph()
-        sp = dict(nx.all_pairs_shortest_path_length(G))
-        c = nx.closeness_centrality(G, sp=sp)
-        d = {
-            "Acciaiuoli": 0.368,
-            "Albizzi": 0.483,
-            "Barbadori": 0.4375,
-            "Bischeri": 0.400,
-            "Castellani": 0.389,
-            "Ginori": 0.333,
-            "Guadagni": 0.467,
-            "Lamberteschi": 0.326,
-            "Medici": 0.560,
-            "Pazzi": 0.286,
-            "Peruzzi": 0.368,
-            "Ridolfi": 0.500,
-            "Salviati": 0.389,
-            "Strozzi": 0.4375,
-            "Tornabuoni": 0.483,
-        }
-        for n in G:
-            assert c[n] == pytest.approx(d[n], abs=1e-3)
-
-    def test_les_miserables_closeness_sp(self):
-        G = nx.les_miserables_graph()
-        sp = dict(nx.all_pairs_shortest_path_length(G))
-        c = nx.closeness_centrality(G, sp=sp)
-        d = {
-            "Napoleon": 0.302,
-            "Myriel": 0.429,
-            "MlleBaptistine": 0.413,
-            "MmeMagloire": 0.413,
-            "CountessDeLo": 0.302,
-            "Geborand": 0.302,
-            "Champtercier": 0.302,
-            "Cravatte": 0.302,
-            "Count": 0.302,
-            "OldMan": 0.302,
-            "Valjean": 0.644,
-            "Labarre": 0.394,
-            "Marguerite": 0.413,
-            "MmeDeR": 0.394,
-            "Isabeau": 0.394,
-            "Gervais": 0.394,
-            "Listolier": 0.341,
-            "Tholomyes": 0.392,
-            "Fameuil": 0.341,
-            "Blacheville": 0.341,
-            "Favourite": 0.341,
-            "Dahlia": 0.341,
-            "Zephine": 0.341,
-            "Fantine": 0.461,
-            "MmeThenardier": 0.461,
-            "Thenardier": 0.517,
-            "Cosette": 0.478,
-            "Javert": 0.517,
-            "Fauchelevent": 0.402,
-            "Bamatabois": 0.427,
-            "Perpetue": 0.318,
-            "Simplice": 0.418,
-            "Scaufflaire": 0.394,
-            "Woman1": 0.396,
-            "Judge": 0.404,
-            "Champmathieu": 0.404,
-            "Brevet": 0.404,
-            "Chenildieu": 0.404,
-            "Cochepaille": 0.404,
-            "Pontmercy": 0.373,
-            "Boulatruelle": 0.342,
-            "Eponine": 0.396,
-            "Anzelma": 0.352,
-            "Woman2": 0.402,
-            "MotherInnocent": 0.398,
-            "Gribier": 0.288,
-            "MmeBurgon": 0.344,
-            "Jondrette": 0.257,
-            "Gavroche": 0.514,
-            "Gillenormand": 0.442,
-            "Magnon": 0.335,
-            "MlleGillenormand": 0.442,
-            "MmePontmercy": 0.315,
-            "MlleVaubois": 0.308,
-            "LtGillenormand": 0.365,
-            "Marius": 0.531,
-            "BaronessT": 0.352,
-            "Mabeuf": 0.396,
-            "Enjolras": 0.481,
-            "Combeferre": 0.392,
-            "Prouvaire": 0.357,
-            "Feuilly": 0.392,
-            "Courfeyrac": 0.400,
-            "Bahorel": 0.394,
-            "Bossuet": 0.475,
-            "Joly": 0.394,
-            "Grantaire": 0.358,
-            "MotherPlutarch": 0.285,
-            "Gueulemer": 0.463,
-            "Babet": 0.463,
-            "Claquesous": 0.452,
-            "Montparnasse": 0.458,
-            "Toussaint": 0.402,
-            "Child1": 0.342,
-            "Child2": 0.342,
-            "Brujon": 0.380,
-            "MmeHucheloup": 0.353,
-        }
-        for n in G:
-            assert c[n] == pytest.approx(d[n], abs=1e-3)
-
-    def test_weighted_closeness_sp(self):
-        edges = [
-            ("s", "u", 10),
-            ("s", "x", 5),
-            ("u", "v", 1),
-            ("u", "x", 2),
-            ("v", "y", 1),
-            ("x", "u", 3),
-            ("x", "v", 5),
-            ("x", "y", 2),
-            ("y", "s", 7),
-            ("y", "v", 6),
-        ]
-        XG = nx.Graph()
-        XG.add_weighted_edges_from(edges)
-        sp = dict(nx.all_pairs_dijkstra_path_length(XG), distance="weight")
-        c = nx.closeness_centrality(XG, distance="weight", sp=sp)
-        d = {"y": 0.200, "x": 0.286, "s": 0.138, "u": 0.235, "v": 0.200}
-        for n in sorted(XG):
-            assert c[n] == pytest.approx(d[n], abs=1e-3)
+        G = nx.Graph()
+        G.add_weighted_edges_from(edges)
+        sp = (
+            dict(nx.all_pairs_dijkstra_path_length(G), distance="weight")
+            if precompute_sp
+            else None
+        )
+        c = nx.closeness_centrality(G, distance="weight", sp=sp)
+        expected = {"y": 0.200, "x": 0.286, "s": 0.138, "u": 0.235, "v": 0.200}
+        assert all(c[n] == pytest.approx(expected[n], abs=1e-3) for n in G)
 
     def test_directed_cyclic_sp(self):
         edges = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 0)]
