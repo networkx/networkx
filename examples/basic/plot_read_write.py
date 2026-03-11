9cba67940350c57d715227ba367d6927aa93a860
Ross Barnowski <rossbar@caltech.edu>
2026-03-10 23:09:56 -0700

    Replace dummy file with BytesIO.

--- examples/basic/plot_read_write.py
+++ examples/basic/plot_read_write.py
@@ -6,18 +6,22 @@ Read and write graphs.
 Read and write graphs.
 """
 
+import io
 import matplotlib.pyplot as plt
 import networkx as nx
 
 G = nx.grid_2d_graph(5, 5)  # 5x5 grid
 
-# print the adjacency list
-for line in nx.generate_adjlist(G):
-    print(line)
-# write edgelist to grid.edgelist
-nx.write_edgelist(G, path="grid.edgelist", delimiter=":")
-# read edgelist from grid.edgelist
-H = nx.read_edgelist(path="grid.edgelist", delimiter=":")
+# Use BytesIO as a virtual file handle.
+# Replace with: `with open(<filename>, "wb") as fh:` to save to disk
+fh = io.BytesIO()
+# write edgelist to file
+nx.write_edgelist(G, path=fh, delimiter=":")
+# View file contents
+print(fh.getvalue().decode())
+# read edgelist from file
+fh.seek(0)
+H = nx.read_edgelist(path=fh, delimiter=":")
 
 pos = nx.spring_layout(H, seed=200)
 nx.draw(H, pos)
