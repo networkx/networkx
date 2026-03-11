acb3f08fac6ab4c449e97a80dcde13c69a52bf66
Ross Barnowski <rossbar@caltech.edu>
2026-03-10 21:45:05 -0700

    Pull directed drawing into separate example.
    This way it gets its own thumbnail.

--- examples/basic/plot_simple_graph.py
+++ examples/basic/plot_simple_graph.py
@@ -23,26 +23,4 @@ options = {
     "width": 5,
 }
 nx.draw(G, pos, with_labels=True, **options)
-
-# %%
-# A directed graph
-
-G = nx.DiGraph([(0, 3), (1, 3), (2, 4), (3, 5), (3, 6), (4, 6), (5, 6)])
-
-# group nodes by column
-left_nodes = [0, 1, 2]
-middle_nodes = [3, 4]
-right_nodes = [5, 6]
-
-# set the position according to column (x-coord)
-pos = {n: (0, i) for i, n in enumerate(left_nodes)}
-pos.update({n: (1, i + 0.5) for i, n in enumerate(middle_nodes)})
-pos.update({n: (2, i + 0.5) for i, n in enumerate(right_nodes)})
-
-nx.draw_networkx(G, pos, **options)
-
-# Set margins for the axes so that nodes aren't clipped
-ax = plt.gca()
-ax.margins(0.20)
-plt.axis("off")
 plt.show()
