5f982ccbfa50ce82e6a68ef220baac9ca8f8258b
Ross Barnowski <rossbar@caltech.edu>
2026-03-10 21:39:17 -0700

    Condense graph construction from edge list.

--- examples/basic/plot_simple_graph.py
+++ examples/basic/plot_simple_graph.py
@@ -9,13 +9,7 @@ Draw simple graph with manual layout.
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.Graph()
-G.add_edge(1, 2)
-G.add_edge(1, 3)
-G.add_edge(1, 5)
-G.add_edge(2, 3)
-G.add_edge(3, 4)
-G.add_edge(4, 5)
+G = nx.Graph([(1, 2), (1, 3), (1, 5), (2, 3), (3, 4), (4, 5)])
 
 # explicitly set positions
 pos = {1: (0, 0), 2: (-1, 0.3), 3: (2, 0.17), 4: (4, 0.255), 5: (5, 0.03)}
