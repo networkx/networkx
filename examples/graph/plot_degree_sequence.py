fcc07ad765ecd232bfdbb3111500345ddeccf504
Ross Barnowski <rossbar@caltech.edu>
2026-03-10 23:59:09 -0700

    Remove unnecessary comments, more context for print.

--- examples/graph/plot_degree_sequence.py
+++ examples/graph/plot_degree_sequence.py
@@ -15,12 +15,12 @@ seed = 668273
 z = [5, 3, 3, 3, 3, 2, 2, 2, 1, 1, 1]
 print(f"Sequence {z} is a valid degree sequence: {nx.is_graphical(z)}")
 
-print("Configuration model")
-G = nx.configuration_model(
-    z, seed=seed
-)  # configuration model, seed for reproducibility
-degree_sequence = [d for n, d in G.degree()]  # degree sequence
-print(f"Degree sequence {degree_sequence}")
+G = nx.configuration_model(z, seed=seed)
+degree_sequence = [d for _, d in G.degree()]
+
+# The degree sequence for the random graph computed using configuration_model
+# should match the degree sequence used to generate it
+print(f"Degree sequence {degree_sequence} identical to z: {degree_sequence == z}")
 print("Degree histogram")
 hist = {}
 for d in degree_sequence:
