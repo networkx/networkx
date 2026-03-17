45801aeb560abbf8420ee2aac389da7dd81b46bd
duriantaco <aaronoh2015@gmail.com>
2026-03-17 11:40:04 +0800

    MAINT: remove unused functions and imports

--- networkx/algorithms/threshold.py
+++ networkx/algorithms/threshold.py
@@ -759,27 +759,6 @@ def eigenvectors(creation_sequence):
     return (val, vec)
 
 
-def spectral_projection(u, eigenpairs):
-    """
-    Returns the coefficients of each eigenvector
-    in a projection of the vector u onto the normalized
-    eigenvectors which are contained in eigenpairs.
-
-    eigenpairs should be a list of two objects.  The
-    first is a list of eigenvalues and the second a list
-    of eigenvectors.  The eigenvectors should be lists.
-
-    There's not a lot of error checking on lengths of
-    arrays, etc. so be careful.
-    """
-    coeff = []
-    evect = eigenpairs[1]
-    for ev in evect:
-        c = sum(evv * uv for (evv, uv) in zip(ev, u))
-        coeff.append(c)
-    return coeff
-
-
 def eigenvalues(creation_sequence):
     """
     Return sequence of eigenvalues of the Laplacian of the threshold
