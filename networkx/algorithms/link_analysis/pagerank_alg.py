65d1ce36b5f9518b55c58f390b956501384a0a4c
Ross Barnowski <rossbar@caltech.edu>
2026-03-24 10:09:10 -0700

    MAINT: Rm transpose of 1D array.

--- networkx/algorithms/link_analysis/pagerank_alg.py
+++ networkx/algorithms/link_analysis/pagerank_alg.py
@@ -460,7 +460,7 @@ def _pagerank_scipy(
     A = nx.to_scipy_sparse_array(G, nodelist=nodelist, weight=weight, dtype=float)
     S = A.sum(axis=1)
     S[S != 0] = 1.0 / S[S != 0]
-    Q = sp.sparse.dia_array((S.T, 0), shape=A.shape).tocsr()
+    Q = sp.sparse.dia_array((S, 0), shape=A.shape).tocsr()
     A = Q @ A
 
     # initial vector
