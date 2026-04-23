891ce43288a7c1be594675cb65759ffe8a85add0
Ross Barnowski <rossbar@caltech.edu>
2026-04-23 16:57:57 -0700

    MAINT: triad_type with match-case.
    Keep switching logic, but move 2-edge vals to case guards.

--- networkx/algorithms/triads.py
+++ networkx/algorithms/triads.py
@@ -453,48 +453,45 @@ def triad_type(G):
         Oxford.
         https://web.archive.org/web/20170830032057/http://www.stats.ox.ac.uk/~snijders/Trans_Triads_ha.pdf
     """
-    if not is_triad(G):
-        raise nx.NetworkXAlgorithmError("G is not a triad (order-3 DiGraph)")
-    num_edges = len(G.edges())
-    if num_edges == 0:
-        return "003"
-    elif num_edges == 1:
-        return "012"
-    elif num_edges == 2:
-        e1, e2 = G.edges()
-        if set(e1) == set(e2):
+    match list(G.edges):
+        case []:  # No edges
+            return "003"
+        case [single_edge]:  # One edge
+            return "012"
+        # Triads with 2 edges
+        case e1, e2 if set(e1) == set(e2):
             return "102"
-        elif e1[0] == e2[0]:
+        case e1, e2 if e1[0] == e2[0]:
             return "021D"
-        elif e1[1] == e2[1]:
+        case e1, e2 if e1[1] == e2[1]:
             return "021U"
-        elif e1[1] == e2[0] or e2[1] == e1[0]:
+        case e1, e2:  # Remaining 2-edge case
             return "021C"
-    elif num_edges == 3:
-        for e1, e2, e3 in permutations(G.edges(), 3):
-            if set(e1) == set(e2):
-                if e3[0] in e1:
-                    return "111U"
-                # e3[1] in e1:
-                return "111D"
-            elif set(e1).symmetric_difference(set(e2)) == set(e3):
-                if {e1[0], e2[0], e3[0]} == {e1[0], e2[0], e3[0]} == set(G.nodes()):
-                    return "030C"
-                # e3 == (e1[0], e2[1]) and e2 == (e1[1], e3[1]):
-                return "030T"
-    elif num_edges == 4:
-        for e1, e2, e3, e4 in permutations(G.edges(), 4):
-            if set(e1) == set(e2):
-                # identify pair of symmetric edges (which necessarily exists)
-                if set(e3) == set(e4):
-                    return "201"
-                if {e3[0]} == {e4[0]} == set(e3).intersection(set(e4)):
-                    return "120D"
-                if {e3[1]} == {e4[1]} == set(e3).intersection(set(e4)):
-                    return "120U"
-                if e3[1] == e4[0]:
-                    return "120C"
-    elif num_edges == 5:
-        return "210"
-    elif num_edges == 6:
-        return "300"
+        # Triads with 3 edges
+        case e1, e2, e3:
+            for e1, e2, e3 in permutations([e1, e2, e3]):
+                if set(e1) == set(e2):
+                    return "111U" if e3[0] in e1 else "111D"
+                elif set(e1) ^ set(e2) == set(e3):
+                    if {e1[0], e2[0], e3[0]} == set(G):
+                        return "030C"
+                    return "030T"
+        # Triads with 4 edges
+        case e1, e2, e3, e4:
+            for e1, e2, e3, e4 in permutations([e1, e2, e3, e4]):
+                # Identify pair of symmetric edges (which necessarily exists)
+                if set(e1) == set(e2):
+                    if set(e3) == set(e4):
+                        return "201"
+                    if {e3[0]} == {e4[0]} == set(e3) & set(e4):
+                        return "120D"
+                    if {e3[1]} == {e4[1]} == set(e3) & set(e4):
+                        return "120U"
+                    if e3[1] == e4[0]:
+                        return "120C"
+        case e1, e2, e3, e4, e5:  # 5 edges
+            return "210"
+        case e1, e2, e3, e4, e5, e6:  # All 6 edges
+            return "300"
+        case _:
+            raise nx.NetworkXAlgorithmError("G is not a triad (order-3 DiGraph)")
