2152e0bb0bd4a9eff77f35adceb3d65afa70a5e1
uwezkhan <uwezkhan053@gmail.com>
2026-06-22 19:14:58 +0530

    graphml: derive numeric-bool directed tests from generated graph

--- networkx/readwrite/tests/test_graphml.py
+++ networkx/readwrite/tests/test_graphml.py
@@ -427,41 +427,23 @@ class TestReadGraphML(BaseGraphML):
         pytest.raises(nx.NetworkXError, nx.parse_graphml, s)
 
     def test_directed_edge_in_undirected_numeric_bool(self):
-        # directed="1" is xsd:boolean true, same as directed="true"
-        s = """<?xml version="1.0" encoding="UTF-8"?>
-<graphml xmlns="http://graphml.graphdrawing.org/xmlns"
-         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
-         http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
-  <graph id="G">
-    <node id="n0"/>
-    <node id="n1"/>
-    <node id="n2"/>
-    <edge source="n0" target="n1"/>
-    <edge source="n1" target="n2" directed='1'/>
-  </graph>
-</graphml>"""
-        fh = io.BytesIO(s.encode("UTF-8"))
-        pytest.raises(nx.NetworkXError, nx.read_graphml, fh)
+        # `directed` is xsd:boolean, so "1" is a valid spelling of "true".
+        # NetworkX only writes `edgedefault`, never a per-edge `directed`, so
+        # mark one edge directed by hand on an otherwise NX-generated document.
+        G = nx.Graph([("n0", "n1")])
+        s = "\n".join(nx.generate_graphml(G)).replace(
+            "<edge ", '<edge directed="1" ', 1
+        )
         pytest.raises(nx.NetworkXError, nx.parse_graphml, s)
 
     def test_undirected_edge_in_directed_numeric_bool(self):
-        # directed="0" is xsd:boolean false, same as directed="false"
-        s = """<?xml version="1.0" encoding="UTF-8"?>
-<graphml xmlns="http://graphml.graphdrawing.org/xmlns"
-         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
-         http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
-  <graph id="G" edgedefault='directed'>
-    <node id="n0"/>
-    <node id="n1"/>
-    <node id="n2"/>
-    <edge source="n0" target="n1"/>
-    <edge source="n1" target="n2" directed='0'/>
-  </graph>
-</graphml>"""
-        fh = io.BytesIO(s.encode("UTF-8"))
-        pytest.raises(nx.NetworkXError, nx.read_graphml, fh)
+        # `directed` is xsd:boolean, so "0" is a valid spelling of "false".
+        # NetworkX only writes `edgedefault`, never a per-edge `directed`, so
+        # mark one edge undirected by hand on an otherwise NX-generated document.
+        G = nx.DiGraph([("n0", "n1")])
+        s = "\n".join(nx.generate_graphml(G)).replace(
+            "<edge ", '<edge directed="0" ', 1
+        )
         pytest.raises(nx.NetworkXError, nx.parse_graphml, s)
 
     def test_key_raise(self):
