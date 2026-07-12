5a8ef36a59d3d563b44e17b217d47014ad5b1f9e
Dan Schult <dschult@colgate.edu>
2026-07-12 12:03:48 -0400

    fix infinite loop

--- networkx/algorithms/community/louvain.py
+++ networkx/algorithms/community/louvain.py
@@ -298,18 +298,18 @@ def _one_level(G, partition, m, resolution, is_directed, seed):
                 degree = degrees[u]
                 Stot[u_com] -= degree
                 x = Stot[u_com] * degree
-            remove_cost = -u_deg_by_com[u_com] / m + gamma * x / m**2
+            move_cost = u_deg_by_com[u_com] * m - gamma * x
 
-            best_mod = 0
+            best_gain = 0
             best_com = u_com
             for nbr_com, wt in u_deg_by_com.items():
                 if is_directed:
                     x = out_degree * Stot_in[nbr_com] + in_degree * Stot_out[nbr_com]
                 else:
                     x = Stot[nbr_com] * degree
-                gain = remove_cost + wt / m - gamma * x / m**2
-                if gain > best_mod:
-                    best_mod = gain
+                gain = wt * m  - gamma * x
+                if gain > best_gain:
+                    best_gain = gain
                     best_com = nbr_com
 
             if is_directed:
