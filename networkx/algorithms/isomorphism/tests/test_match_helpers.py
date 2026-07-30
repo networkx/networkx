2e07b0cbc47ce3f686d0645b2817742ac7d1db75
Dan Schult <dschult@colgate.edu>
2026-07-29 23:54:24 -0400

    Add match helper input types to be lists or not, as described in docs

--- networkx/algorithms/isomorphism/tests/test_match_helpers.py
+++ networkx/algorithms/isomorphism/tests/test_match_helpers.py
@@ -1,27 +1,74 @@
 from operator import eq
 
+import pytest
+
 import networkx as nx
 from networkx.algorithms import isomorphism as iso
 
 
 def test_categorical_node_match():
-    nm = iso.categorical_node_match(["x", "y", "z"], [None] * 3)
+    nm = iso.categorical_node_match(["x", "y", "z"], None)
     assert nm({"x": 1, "y": 2, "z": 3}, {"x": 1, "y": 2, "z": 3})
     assert not nm({"x": 1, "y": 2, "z": 2}, {"x": 1, "y": 2, "z": 1})
 
 
+def test_input_length_exceptions():
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.categorical_node_match([0, 1, 2], [None])
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.categorical_node_match([0, 1, 2], [None] * 4)
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.categorical_multiedge_match([0, 1, 2], [None])
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.categorical_multiedge_match([0, 1, 2], [None] * 4)
+
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.numerical_node_match([0, 1, 2], [None])
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.numerical_node_match([0, 1, 2], [None] * 4)
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.numerical_multiedge_match([0, 1, 2], [None])
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.numerical_multiedge_match([0, 1, 2], [None] * 4)
+
+    # non-lists pass with value spread across attributes
+    iso.generic_node_match([0, 1, 2], None, lambda x: x)
+    iso.generic_node_match("color", None, lambda x: x)
+    iso.generic_node_match("color", [None], [lambda x: x])
+
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.generic_node_match([0, 1, 2], [None], [lambda x: x] * 3)
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.generic_node_match([0, 1, 2], [None] * 3, [lambda x: x])
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.generic_node_match([0, 1, 2], [None] * 4, [lambda x: x] * 3)
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.generic_node_match([0, 1, 2], [None] * 3, [lambda x: x] * 4)
+
+    # non-lists pass with value spread across attributes
+    iso.generic_multiedge_match([0, 1, 2], None, lambda x: x)
+    iso.generic_multiedge_match("color", None, lambda x: x)
+    iso.generic_multiedge_match("color", [None], [lambda x: x])
+
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.generic_multiedge_match([0, 1, 2], [None], [lambda x: x] * 3)
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.generic_multiedge_match([0, 1, 2], [None] * 3, [lambda x: x])
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.generic_multiedge_match([0, 1, 2], [None] * 4, [lambda x: x] * 3)
+    with pytest.raises(ValueError, match="zip.. argument "):
+        iso.generic_multiedge_match([0, 1, 2], [None] * 3, [lambda x: x] * 4)
+
+
 class TestGenericMultiEdgeMatch:
     def setup_method(self):
-        self.G1 = nx.MultiDiGraph()
-        self.G2 = nx.MultiDiGraph()
-        self.G3 = nx.MultiDiGraph()
-        self.G4 = nx.MultiDiGraph()
-        attr_dict1 = {"id": "edge1", "minFlow": 0, "maxFlow": 10}
-        attr_dict2 = {"id": "edge2", "minFlow": -3, "maxFlow": 7}
-        attr_dict3 = {"id": "edge3", "minFlow": 13, "maxFlow": 117}
-        attr_dict4 = {"id": "edge4", "minFlow": 13, "maxFlow": 117}
-        attr_dict5 = {"id": "edge5", "minFlow": 8, "maxFlow": 12}
-        attr_dict6 = {"id": "edge6", "minFlow": 8, "maxFlow": 12}
+        self.G = nx.MultiDiGraph()
+        attr_dict1 = {"id": "edge1", "minFlow": 0, "maxFlow": 10, "pos": (0, 10)}
+        attr_dict2 = {"id": "edge2", "minFlow": -3, "maxFlow": 7, "pos": (3, 10)}
+        attr_dict3 = {"id": "edge3", "minFlow": 13, "maxFlow": 117, "pos": (13, 17)}
+        attr_dict4 = {"id": "edge4", "minFlow": 13, "maxFlow": 117, "pos": (13, 17)}
+        attr_dict5 = {"id": "edge5", "minFlow": 8, "maxFlow": 12, "pos": (8, 12)}
+        attr_dict6 = {"id": "edge6", "minFlow": 8, "maxFlow": 12, "pos": (8, 12)}
         for attr_dict in [
             attr_dict1,
             attr_dict2,
@@ -30,7 +77,7 @@ class TestGenericMultiEdgeMatch:
             attr_dict5,
             attr_dict6,
         ]:
-            self.G1.add_edge(1, 2, **attr_dict)
+            self.G.add_edge(1, 2, **attr_dict)
         for attr_dict in [
             attr_dict5,
             attr_dict3,
@@ -39,26 +86,38 @@ class TestGenericMultiEdgeMatch:
             attr_dict4,
             attr_dict2,
         ]:
-            self.G2.add_edge(2, 3, **attr_dict)
+            self.G.add_edge(2, 3, **attr_dict)
         for attr_dict in [attr_dict3, attr_dict5]:
-            self.G3.add_edge(3, 4, **attr_dict)
+            self.G.add_edge(3, 4, **attr_dict)
         for attr_dict in [attr_dict6, attr_dict4]:
-            self.G4.add_edge(4, 5, **attr_dict)
+            self.G.add_edge(4, 5, **attr_dict)
 
     def test_generic_multiedge_match(self):
-        full_match = iso.generic_multiedge_match(
-            ["id", "flowMin", "flowMax"], [None] * 3, [eq] * 3
-        )
-        flow_match = iso.generic_multiedge_match(
-            ["flowMin", "flowMax"], [None] * 2, [eq] * 2
-        )
-        min_flow_match = iso.generic_multiedge_match("flowMin", None, eq)
-        id_match = iso.generic_multiedge_match("id", None, eq)
-        assert flow_match(self.G1[1][2], self.G2[2][3])
-        assert min_flow_match(self.G1[1][2], self.G2[2][3])
-        assert id_match(self.G1[1][2], self.G2[2][3])
-        assert full_match(self.G1[1][2], self.G2[2][3])
-        assert flow_match(self.G3[3][4], self.G4[4][5])
-        assert min_flow_match(self.G3[3][4], self.G4[4][5])
-        assert not id_match(self.G3[3][4], self.G4[4][5])
-        assert not full_match(self.G3[3][4], self.G4[4][5])
+        gmatch = iso.generic_multiedge_match
+        id_match = gmatch("id", None, eq)
+        full_match = gmatch(["id", "flowMin", "flowMax"], None, eq)
+        flow_match = gmatch(["flowMin", "flowMax"], None, eq)
+        flow_matcx = gmatch(["flowMin", "flowMax"], [None] * 2, [eq] * 2)
+        minf_match = gmatch("flowMin", None, eq)
+
+        def all_eq(x, y):
+            return all(a == b for a, b in zip(x, y))
+
+        diffops_id = gmatch(["id", "pos"], None, [eq, all_eq])
+        diffops_fl = gmatch(["pos", "flowMin"], None, [all_eq, eq])
+
+        assert id_match(self.G[1][2], self.G[2][3])
+        assert flow_match(self.G[1][2], self.G[2][3])
+        assert minf_match(self.G[1][2], self.G[2][3])
+        assert full_match(self.G[1][2], self.G[2][3])
+        assert diffops_id(self.G[1][2], self.G[2][3])
+        assert diffops_fl(self.G[1][2], self.G[2][3])
+
+        assert flow_match(self.G[3][4], self.G[4][5])
+        assert flow_matcx(self.G[3][4], self.G[4][5])
+        assert minf_match(self.G[3][4], self.G[4][5])
+        assert diffops_fl(self.G[3][4], self.G[4][5])
+
+        assert not id_match(self.G[3][4], self.G[4][5])
+        assert not full_match(self.G[3][4], self.G[4][5])
+        assert not diffops_id(self.G[3][4], self.G[4][5])
