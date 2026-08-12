c8da2cbe7ed859469e3688056058750f548493c1
Ross Barnowski <rossbar@caltech.edu>
2026-08-12 14:16:22 -0700

    Comment wording update.

--- examples/algorithms/plot_parallel_betweenness.py
+++ examples/algorithms/plot_parallel_betweenness.py
@@ -17,8 +17,8 @@ faster. This is a limitation of our CI/CD pipeline.
 Depending on your setup, you will likely observe a speedup.
 """
 
-# Prevent this example from running in a parallel sphinx-gallery run to prevent
-# resource oversubscription, since this example itself makes uses of parallelism
+# Don't run this example in parallel via sphinx-gallery to prevent
+# resource oversubscription. This example itself makes uses of parallelism.
 # sphinx_gallery_parallel = False
 
 from multiprocessing import Pool
