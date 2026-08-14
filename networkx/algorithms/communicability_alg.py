69e56a590cbda5e57ba588fa4dbb8344a61fdd11
Ross Barnowski <rossbar@caltech.edu>
2026-08-14 07:53:35 -0700

    DOC: Fixup communicability docstring.

--- networkx/algorithms/communicability_alg.py
+++ networkx/algorithms/communicability_alg.py
@@ -12,18 +12,18 @@ __all__ = ["communicability", "communicability_exp"]
 @not_implemented_for("multigraph")
 @nx._dispatchable
 def communicability(G):
-    r"""Returns communicability between all pairs of nodes in G.
+    r"""Returns communicability between all pairs of nodes in `G`.
 
-    The communicability between pairs of nodes in G is the sum of
-    walks of different lengths starting at node u and ending at node v.
+    The communicability between pairs of nodes in `G` is the sum of
+    walks of different lengths starting at node ``u`` and ending at node ``v``.
 
     Parameters
     ----------
-    G: graph
+    G : graph
 
     Returns
     -------
-    comm: dictionary of dictionaries
+    comm : dictionary of dictionaries
         Dictionary of dictionaries keyed by nodes with communicability
         as the value.
 
@@ -35,25 +35,24 @@ def communicability(G):
     See Also
     --------
     communicability_exp:
-       Communicability between all pairs of nodes in G  using spectral
-       decomposition.
+       Communicability between all pairs of nodes in `G` using matrix exponentiation
     communicability_betweenness_centrality:
-       Communicability betweenness centrality for each node in G.
+       Communicability betweenness centrality for each node in `G`.
 
     Notes
     -----
     This algorithm uses a spectral decomposition of the adjacency matrix.
-    Let G=(V,E) be a simple undirected graph.  Using the connection between
-    the powers  of the adjacency matrix and the number of walks in the graph,
-    the communicability  between nodes `u` and `v` based on the graph spectrum
-    is [1]_
+    Let $G=(V,E)$ be a simple undirected graph.  Using the connection between
+    the powers of the adjacency matrix and the number of walks in the graph,
+    the communicability  between nodes ``u`` and ``v`` based on the graph spectrum
+    is:
 
     .. math::
-        C(u,v)=\sum_{j=1}^{n}\phi_{j}(u)\phi_{j}(v)e^{\lambda_{j}},
+        C(u,v)=\sum_{j=1}^{n}\phi_{j}(u)\phi_{j}(v)e^{\lambda_{j}}
 
-    where `\phi_{j}(u)` is the `u\rm{th}` element of the `j\rm{th}` orthonormal
+    where $\phi_{j}(u)$ is the $u\rm{th}$ element of the $j\rm{th}$ orthonormal
     eigenvector of the adjacency matrix associated with the eigenvalue
-    `\lambda_{j}`.
+    $\lambda_{j}$ [1]_.
 
     References
     ----------
