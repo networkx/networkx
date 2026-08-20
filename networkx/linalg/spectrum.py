dd4dacd35d61d7249b8b8360ad8791cf18e4baee
Ross Barnowski <rossbar@caltech.edu>
2026-08-20 15:18:33 -0700

    MAINT: Make magnetic_spectrum params kwarg-only.
    Matches the params that are kwarg-only in magnetic_laplacian.

--- networkx/linalg/spectrum.py
+++ networkx/linalg/spectrum.py
@@ -125,7 +125,7 @@ def adjacency_spectrum(G, weight="weight"):
 
 
 @nx._dispatchable(edge_attrs="weight")
-def magnetic_spectrum(G, q=0.25, weight="weight", normalized=False):
+def magnetic_spectrum(G, *, q=0.25, weight="weight", normalized=False):
     """Returns eigenvalues of the magnetic laplacian matrix with phase `q`.
 
     Parameters
