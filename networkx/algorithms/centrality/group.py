35d9e7c7dd3f5b51216b8ef336374dcdb8dd4fd2
Dan Schult <dschult@colgate.edu>
2026-09-22 22:10:59 -0400

    remove arbitrary 2-digit rounding in prominent_group centrality measure

--- networkx/algorithms/centrality/group.py
+++ networkx/algorithms/centrality/group.py
@@ -451,7 +451,6 @@ def prominent_group(
     # If undirected then count only the undirected edges
     elif not G.is_directed():
         max_GBC /= 2
-    max_GBC = float(f"{max_GBC:.2f}")
     return max_GBC, max_group
 
 
