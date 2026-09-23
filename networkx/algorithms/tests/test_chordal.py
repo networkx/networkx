00386e3cc92efbb3f9d87738f3eb263f08734393
Ross Barnowski <rossbar@caltech.edu>
2026-09-23 15:23:00 -0700

    TST: Rm special test case for graph with single self loop.

--- networkx/algorithms/tests/test_chordal.py
+++ networkx/algorithms/tests/test_chordal.py
@@ -88,9 +88,7 @@ class TestMCS:
         non_chordal_G.add_edges_from([(1, 2), (1, 3), (2, 4), (2, 5), (3, 4), (3, 5)])
         cls.non_chordal_G = non_chordal_G
 
-        self_loop_G = nx.Graph()
-        self_loop_G.add_edges_from([(1, 1)])
-        cls.self_loop_G = self_loop_G
+        cls.self_loop_G = nx.Graph([(1, 1)])
 
     @pytest.mark.parametrize("G", (nx.DiGraph(), nx.MultiGraph(), nx.MultiDiGraph()))
     def test_is_chordal_not_implemented(self, G):
@@ -136,8 +134,6 @@ class TestMCS:
         assert set(nx.chordal_graph_cliques(self.chordal_G)) == cliques
         with pytest.raises(nx.NetworkXError, match="Input graph is not chordal"):
             set(nx.chordal_graph_cliques(self.non_chordal_G))
-        with pytest.raises(nx.NetworkXError, match="Input graph is not chordal"):
-            set(nx.chordal_graph_cliques(self.self_loop_G))
 
     def test_chordal_find_cliques_path(self):
         G = nx.path_graph(10)
