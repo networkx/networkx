242465fb053ec4cbba452f46b43edde77bd650d1
Eric Larson <larson.eric.d@gmail.com>
2026-09-25 11:45:19 -0400

    Make pygraphviz PNG scraper parallel-safe

--- doc/conf.py
+++ doc/conf.py
@@ -60,13 +60,29 @@ rst_epilog = """
 .. |dijkstra| replace:: :doc:`/reference/algorithms/shortest_paths/dijkstra`
 """
 
-# Add pygraphviz png scraper, if available
-try:
-    from pygraphviz.scraper import PNGScraper
 
-    sphinx_gallery_conf["image_scrapers"] += (PNGScraper(),)
-except ImportError:
-    pass
+def png_scraper(block, block_vars, gallery_conf):
+    """Collect .png files written by pygraphviz examples (e.g. ``A.draw("k5.png")``).
+
+    Unlike ``pygraphviz.scraper.PNGScraper``, only collect files whose names appear
+    in the current code block, so that examples in the same directory running in
+    parallel do not grab each other's (possibly partially written) images.
+    """
+    import glob
+    import shutil
+    from sphinx_gallery.scrapers import figure_rst
+
+    example_dir = os.path.dirname(block_vars["src_file"])
+    image_names = []
+    for png in sorted(glob.glob(os.path.join(example_dir, "*.png"))):
+        if os.path.basename(png) in block.content:
+            image_path = next(block_vars["image_path_iterator"])
+            shutil.move(png, image_path)
+            image_names.append(image_path)
+    return figure_rst(image_names, gallery_conf["src_dir"])
+
+
+sphinx_gallery_conf["image_scrapers"] += (png_scraper,)
 
 # generate autosummary pages
 autosummary_generate = True
